981242a9b8d80d2111e882d0d02cd8ec418fe0c4
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-10 17:30:27 -0600

    add white spacing in cells to show full data

--- apps/genome-page/src/components/go/All.js
+++ apps/genome-page/src/components/go/All.js
@@ -3,23 +3,20 @@ import { Table, Column, InfiniteLoader } from 'react-virtualized'
 import 'react-virtualized/styles.css'
 import '../../styles/index.css'
 
-const BASE_ROW_HEIGHT = 30;
-const MAX_NAME_CHARS_PER_LINE = 20;
+const BASE_ROW_HEIGHT = 30
+const MAX_NAME_CHARS_PER_LINE = 20
 
 export default class All extends Component {
-    state = { 
+    state = {
         comments: []
-     }
-
+    }
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/comments')
             .then(res => res.json())
             .then(comments => this.setState({ comments }))
     }
-
     getRowHeight = ({ index }) => {
         const data = this.state.comments[index]
-    
         const numLines = Math.ceil(data.name.length / MAX_NAME_CHARS_PER_LINE)
 
         // if (numLines > MAX_NAME_CHARS_PER_LINE) {
@@ -27,10 +24,9 @@ export default class All extends Component {
         // } else {
         //     return BASE_ROW_HEIGHT
         // }
-    
-        return numLines * BASE_ROW_HEIGHT
-      }
 
+        return numLines * BASE_ROW_HEIGHT
+    }
     getRowStyle = ({ index }) => {
         const data = this.state.comments
         if (index === -1) {
@@ -52,27 +48,34 @@ export default class All extends Component {
             }
         }
     }
-
     isRowLoaded = ({ index }) => {
         return !!this.state.comments[index]
     }
-
     loadMoreRows = ({ startIndex, stopIndex }) => {
         return fetch(`/data?startIndex=${startIndex}&stopIndex=${stopIndex}`)
             .then(res => res.json())
             .then(comments => this.setState({ comments }))
     }
-
     rowGetter = ({ index }) => {
         const data = this.state.comments
         if (data[index]) {
             return data[index]
         }
     }
+    cellDataGetter = ({ rowData, dataKey }) => {
+        if (rowData) {
+            return rowData[dataKey]
+        }
+    }
+    descriptorRenderer = ({ rowData, cellData }) => {
+        if (rowData) {
+            return <div style={{ whiteSpace: 'normal' }}>{cellData}</div>
+        }
+    }
 
     render() {
         const rowCount = this.state.comments.length
-        const { cellWidth, height } = this.props
+        const { cellWidth, cellHeight } = this.props
 
         return (
             <div className="wrapper">
@@ -81,53 +84,53 @@ export default class All extends Component {
                     isRowLoaded={this.isRowLoaded}
                     rowCount={rowCount}
                     loadMoreRows={this.loadMoreRows}
-                    threshold={10}
-                >        
-                {({onRowsRendered, registerChild}) =>
-                <Table
-                    ref={ registerChild }
-                    headerHeight={50}
-                    height={height}
-                    width={cellWidth * 6 + 100}
-                    rowCount={rowCount}
-                    rowGetter={this.rowGetter}
-                    rowHeight={this.getRowHeight}
-                    rowStyle={this.getRowStyle}
-                    onRowsRendered={onRowsRendered}
-                >
-                    <Column
-                        label="GO term + Extension"
-                        dataKey="name"
-                        width={ 250 }
-                    />
-                    <Column
-                        label="Evidence"
-                        dataKey="postId"
-                        width={ 100 }
-                    />
-                    <Column
-                        label="With"
-                        dataKey="email"
-                        width={ 100 }
-                    />
-                    <Column
-                        label="Reference"
-                        dataKey="name"
-                        width={ 250 }
-                    />
-                    <Column
-                        label="Date"
-                        dataKey="id"
-                        width={ 75 }
-                    />
-                    <Column
-                        label="Source"
-                        dataKey="postId"
-                        width={ 75 }
-                    />
-                </Table>
-            }
-            </InfiniteLoader>
+                    threshold={10}>
+                    {({ onRowsRendered, registerChild }) => (
+                        <Table
+                            ref={registerChild}
+                            headerHeight={50}
+                            height={cellHeight * 7}
+                            width={cellWidth * 3 + 250 + 250}
+                            rowCount={rowCount}
+                            rowGetter={this.rowGetter}
+                            rowHeight={this.getRowHeight}
+                            rowStyle={this.getRowStyle}
+                            onRowsRendered={onRowsRendered}>
+                            <Column
+                                label="GO term + Extension"
+                                dataKey="name"
+                                width={250}
+                                cellDataGetter={this.attributeGetter}
+                                cellRenderer={this.descriptorRenderer}
+                            />
+                            <Column
+                                label="Evidence"
+                                dataKey="postId"
+                                width={100}
+                            />
+                            <Column
+                                label="With"
+                                dataKey="email"
+                                width={150}
+                                cellDataGetter={this.attributeGetter}
+                                cellRenderer={this.descriptorRenderer}
+                            />
+                            <Column
+                                label="Reference"
+                                dataKey="name"
+                                width={250}
+                                cellDataGetter={this.attributeGetter}
+                                cellRenderer={this.descriptorRenderer}
+                            />
+                            <Column label="Date" dataKey="id" width={75} />
+                            <Column
+                                label="Source"
+                                dataKey="postId"
+                                width={75}
+                            />
+                        </Table>
+                    )}
+                </InfiniteLoader>
             </div>
         )
     }
