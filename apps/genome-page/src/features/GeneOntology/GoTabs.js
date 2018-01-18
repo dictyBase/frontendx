dedc9948eee50ec72fadb537505d9ca2353d7f33
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-17 19:27:39 -0600

    rewrite tabs as array and add active tab styling

--- apps/genome-page/src/features/GeneOntology/GoTabs.js
+++ apps/genome-page/src/features/GeneOntology/GoTabs.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Link as RouterLink } from 'react-router-dom'
-import { Tabs, TabItem } from 'rebass'
+import React, { Component } from 'react'
+import { Flex, Tabs, TabItem } from 'rebass'
 import styled, { ThemeProvider } from 'styled-components'
 import { lighten } from 'polished'
+
+const TabList = ['All GO', 'Manual GO', 'Experimental GO', 'Electronic GO']
 
 const theme = {
     primary: '#15317e',
@@ -15,7 +16,7 @@ const TabRow = styled(Tabs)`
 `
 
 const Tab = styled(TabItem)`
-    background: #A3BAE9;
+    background: ${ props => !props.active && props.theme.secondary ? '#A3BAE9' : '#15317e' };
     border-top: 1px solid black;
     border-right: 1px solid black;
     border-left: 1px solid black;
@@ -24,37 +25,56 @@ const Tab = styled(TabItem)`
     margin-right: 3px;
     margin-top: 1px;
     padding: 5px;
+    width: 150px;
+    color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };
+    border-color: ${ props => props.active ? 'black' : ''};
 
     &:hover {
-    background: ${ props => !props.active && props.theme.secondary ? lighten(0.1, props.theme.secondary) : '#15317e' };
-  }
+        background: ${ props => !props.active && props.theme.secondary ? lighten(0.1, props.theme.secondary) : '#15317e' };
+        color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };;
+        cursor: pointer;
+    }
 `
 
-const Link = styled(RouterLink)`
-    text-decoration: none;
-    padding: 10px 15px;
-    color: black;
-`
+class GoTabs extends Component {
+    state = {
+        active: 0
+    }
 
-const GoTabs = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <TabRow>
-                <Tab>
-                    <Link to="/gene/:id/go">All GO</Link>
-                </Tab>
-                <Tab>
-                    <Link to="/gene/:id/go">Manual GO</Link>
-                </Tab>
-                <Tab>
-                    <Link to="/gene/:id/go">Experimental GO</Link>
-                </Tab>
-                <Tab>
-                    <Link to="/gene/:id/go">Electronic GO</Link>
-                </Tab>
-            </TabRow>
-        </ThemeProvider>
-    )
+    setActive = active => {
+        this.setState({ active })
+    }
+
+    renderTabs = ({ value, index }) => {
+        const { active } = this.state
+        const isActive = index === active
+
+        return (
+            <Tab
+                key={index}
+                active={isActive}
+                onClick={e => {
+                    this.setActive(index)
+                }}
+            >
+                { value }
+            </Tab>
+        )
+    }
+
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <TabRow>
+                    <Flex justify='center' mx='auto'>
+                        {TabList.map((value, index) =>
+                            this.renderTabs({ value, index })
+                        )}
+                    </Flex>
+                </TabRow>
+            </ThemeProvider>
+        )
+    }
 }
 
 export default GoTabs
