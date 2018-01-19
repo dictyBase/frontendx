3a00ca40a089df4bb6806d2be76a94e445c4db68
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-19 17:20:50 -0600

    move custom styled-components to their own file

--- apps/genome-page/src/features/GeneOntology/GoTabs.js
+++ apps/genome-page/src/features/GeneOntology/GoTabs.js
@@ -3,9 +3,9 @@ import AllGO from './AllGO'
 import ElectronicGO from './ElectronicGO'
 import ExperimentalGO from './ExperimentalGO'
 import ManualGO from './ManualGO'
-import { Flex, Tabs, TabItem } from 'rebass'
-import styled, { ThemeProvider } from 'styled-components'
-import { lighten } from 'polished'
+import { TabContainer, Tab } from '../../styles/style'
+import { Flex } from 'rebass'
+import { ThemeProvider } from 'styled-components'
 
 const TabList = ['All GO', 'Experimental GO', 'Manual GO', 'Electronic GO']
 
@@ -14,31 +14,6 @@ const theme = {
     secondary: '#A3BAE9',
     tabText: 'white'
 }
-
-const TabContainer = styled(Tabs)`
-    border-bottom: 0px;
-`
-
-const Tab = styled(TabItem)`
-    background: ${ props => !props.active && props.theme.secondary ? '#A3BAE9' : '#15317e' };
-    border-top: 1px solid white;
-    border-right: 1px solid black;
-    border-left: 1px solid black;
-    border-top-right-radius: 2px;
-    border-top-left-radius: 2px;
-    margin-right: 3px;
-    margin-top: 1px;
-    padding: 5px;
-    width: 150px;
-    color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };
-    border-color: ${ props => props.active ? 'black' : ''};
-
-    &:hover {
-        background: ${ props => !props.active && props.theme.secondary ? lighten(0.1, props.theme.secondary) : '#15317e' };
-        color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };;
-        cursor: pointer;
-    }
-`
 
 class GoTabs extends Component {
     state = {
