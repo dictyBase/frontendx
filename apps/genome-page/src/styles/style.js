c16040f1ce4b9db088daf838bd6ae57977e05981
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-19 17:20:50 -0600

    move custom styled-components to their own file

--- apps/genome-page/src/styles/style.js
+++ apps/genome-page/src/styles/style.js
@@ -1,4 +1,6 @@
 import styled, { injectGlobal } from 'styled-components'
+import { Tabs, TabItem } from 'rebass'
+import { lighten } from 'polished'
 
 injectGlobal([`
     html {
@@ -25,4 +27,29 @@ export const ReactVirtualizedTable = styled.div`
 `
 export const Wrapper = styled.div`
     text-align: center;
+`
+
+export const TabContainer = styled(Tabs)`
+    border-bottom: 0px;
+`
+
+export const Tab = styled(TabItem)`
+    background: ${ props => !props.active && props.theme.secondary ? '#A3BAE9' : '#15317e' };
+    border-top: 1px solid white;
+    border-right: 1px solid black;
+    border-left: 1px solid black;
+    border-top-right-radius: 2px;
+    border-top-left-radius: 2px;
+    margin-right: 3px;
+    margin-top: 1px;
+    padding: 5px;
+    width: 150px;
+    color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };
+    border-color: ${ props => props.active ? 'black' : ''};
+
+    &:hover {
+        background: ${ props => !props.active && props.theme.secondary ? lighten(0.1, props.theme.secondary) : '#15317e' };
+        color: ${ props => !props.active && props.theme.secondary ? 'black' : 'white' };;
+        cursor: pointer;
+    }
 `
