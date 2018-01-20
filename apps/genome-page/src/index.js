64e5a50b39eda40339a064921e3fa7291f208f13
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-20 11:14:52 -0600

    set up basic redux implementation

--- apps/genome-page/src/index.js
+++ apps/genome-page/src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/layout/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app/layout/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './app/rootReducer'
+import registerServiceWorker from './registerServiceWorker'
 import './styles/style.css'
 import './styles/style.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
+registerServiceWorker()
