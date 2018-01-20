c21d6fe480089666c94f41113daab8644d5e5858
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-19 19:05:15 -0600

    remove redundant code and add redux dependencies

--- apps/genome-page/src/features/GeneOntology/ElectronicGO.js
+++ apps/genome-page/src/features/GeneOntology/ElectronicGO.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react'
 import DisplayTable from './DisplayTable'
 
 export default class ElectronicGO extends Component {
-    state = {
-        data: []
-    }
-    componentDidMount() {
-        fetch('https://api.myjson.com/bins/6vbot')
-            .then(res => res.json())
-            .then(data => this.setState({ data }))
-    }
-
     render() {
-        const data = this.state.data.filter(code => code.evidence === 'IEA')
+        const data = this.props.data.filter(code => code.evidence === 'IEA')
         return (
             <div className="wrapper">
                 <DisplayTable data={data} />
--- apps/genome-page/src/features/GeneOntology/ExperimentalGO.js
+++ apps/genome-page/src/features/GeneOntology/ExperimentalGO.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react'
 import DisplayTable from './DisplayTable'
 
 export default class ExperimentalGO extends Component {
-    state = {
-        data: []
-    }
-    componentDidMount() {
-        fetch('https://api.myjson.com/bins/erxtx')
-            .then(res => res.json())
-            .then(data => this.setState({ data }))
-    }
-
     render() {
-        const data = this.state.data.filter(
+        const data = this.props.data.filter(
             code =>
                 code.evidence === 'IMP' ||
                 code.evidence === 'IGI' ||
