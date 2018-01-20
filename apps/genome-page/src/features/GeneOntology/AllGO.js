87ad2f63ed4ebc67d767bece71343f950caf0606
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-19 19:05:15 -0600

    remove redundant code and add redux dependencies

--- apps/genome-page/src/features/GeneOntology/AllGO.js
+++ apps/genome-page/src/features/GeneOntology/AllGO.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react'
 import DisplayTable from './DisplayTable'
 
 export default class AllGO extends Component {
-    state = {
-        data: []
-    }
-    componentDidMount() {
-        fetch('https://api.myjson.com/bins/erxtx')
-            .then(res => res.json())
-            .then(data => this.setState({ data }))
-    }
-
     render() {
-        const data = this.state.data
+        const data = this.props.data
         return (
             <div className="wrapper">
                 <DisplayTable data={data} />
