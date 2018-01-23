43da3d75ca6a1fc4ebc90a2bbdec4bdb40a0afc8
wildlifehexagon <crossthebreeze@gmail.com>
2018-01-23 11:47:21 -0600

    move store and rootReducer files to appropriate folders

--- apps/genome-page/src/app/store/configureStore.js
+++ apps/genome-page/src/app/store/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from './rootReducer'
+import rootReducer from 'app/reducers/rootReducer'
 
 export default function configureStore(initialState) {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
