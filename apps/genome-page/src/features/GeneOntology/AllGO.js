e9b27c69234ae293cba36c8e05988d8f27f0ca85
wildlifehexagon <crossthebreeze@gmail.com>
2018-02-13 15:29:46 -0600

    add flow type checking for GO components

--- apps/genome-page/src/features/GeneOntology/AllGO.js
+++ apps/genome-page/src/features/GeneOntology/AllGO.js
@@ -1,9 +1,17 @@
+// @flow
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchData } from './goActions'
 import DisplayTable from './DisplayTable'
 
-class AllGO extends Component {
+type Props = {
+    data: Object,
+    hasErrored: boolean,
+    isLoading: boolean,
+    fetchData: Function
+}
+
+class AllGO extends Component<Props> {
     componentDidMount() {
         this.props.fetchData('https://api.myjson.com/bins/6vbot')
     }
