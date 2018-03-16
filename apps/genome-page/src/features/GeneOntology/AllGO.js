85d2abc8d3e389ea5cd84b24d547b2656b07a0c8
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-16 17:33:30 -0500

    add multiple tables to GO tab

--- apps/genome-page/src/features/GeneOntology/AllGO.js
+++ apps/genome-page/src/features/GeneOntology/AllGO.js
@@ -3,6 +3,7 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import { fetchData } from "./goActions"
 import DisplayTable from "./DisplayTable"
+import { Wrapper, GoHeaderStyle } from "styles/style"
 
 type Props = {
     /** The data fetched from the API */
@@ -34,9 +35,22 @@ export class AllGO extends Component<Props> {
         }
 
         return (
-            <div className="wrapper">
+            <Wrapper>
+                <GoHeaderStyle>
+                    <h3>Molecular Function</h3>
+                </GoHeaderStyle>
                 <DisplayTable data={this.props.data} />
-            </div>
+                <br />
+                <GoHeaderStyle>
+                    <h3>Biological Process</h3>
+                </GoHeaderStyle>
+                <DisplayTable data={this.props.data} />
+                <br />
+                <GoHeaderStyle>
+                    <h3>Cellular Composition</h3>
+                </GoHeaderStyle>
+                <DisplayTable data={this.props.data} />
+            </Wrapper>
         )
     }
 }
