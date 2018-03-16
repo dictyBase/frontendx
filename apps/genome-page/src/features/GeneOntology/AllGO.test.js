e182d61714dfc6295e6c65cd33c153f5d272a457
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-16 16:23:02 -0500

    add jest snapshots

---
+++ apps/genome-page/src/features/GeneOntology/AllGO.test.js
@@ -0,0 +1,32 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import { AllGO } from "./AllGO"
+jest.mock("react-virtualized")
+
+test("matching a snapshot of AllGO", () => {
+    const data = [
+        {
+            term: "ATP-gated ion channel activity",
+            evidence: "IMP",
+            with: "",
+            reference: "Sivaramakrishnan & Fountain (2012)",
+            date: "21-02-2014",
+            source: "DDB"
+        },
+        {
+            term: "calcium ion transmembrane transport requires ATP",
+            evidence: "IGI",
+            with: "p2xA p2xC p2xD p2xE",
+            reference: "Sivaramakrishnan & Fountain (2012)",
+            date: "21-02-2014",
+            source: "DDB"
+        }
+    ]
+    const fetchData = () => {}
+
+    const component = renderer.create(
+        <AllGO data={data} fetchData={fetchData} />
+    )
+    let tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+})
