9d563a9abd90cdee45fb384fb13e2e7b1ee8eb03
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-30 17:11:26 -0500

    add loading skeleton

--- apps/genome-page/src/features/GeneOntology/goReducer.js
+++ apps/genome-page/src/features/GeneOntology/goReducer.js
@@ -1,8 +1,8 @@
 // @flow
 import {
-    DATA_HAS_ERRORED,
-    DATA_IS_LOADING,
-    FETCH_DATA_SUCCESS
+  DATA_HAS_ERRORED,
+  DATA_IS_LOADING,
+  FETCH_DATA_SUCCESS
 } from "./goConstants"
 
 /**
@@ -10,44 +10,44 @@ import {
  */
 
 const initialState = {
-    data: []
+  data: []
 }
 
 export function dataHasErrored(
-    state: boolean = false,
-    action: { type: string, hasErrored: boolean }
+  state: boolean = false,
+  action: { type: string, hasErrored: boolean }
 ) {
-    switch (action.type) {
-        case DATA_HAS_ERRORED:
-            return action.hasErrored
+  switch (action.type) {
+    case DATA_HAS_ERRORED:
+      return action.hasErrored
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
 export function dataIsLoading(
-    state: boolean = false,
-    action: { type: string, isLoading: boolean }
+  state: boolean = false,
+  action: { type: string, isLoading: boolean }
 ) {
-    switch (action.type) {
-        case DATA_IS_LOADING:
-            return action.isLoading
+  switch (action.type) {
+    case DATA_IS_LOADING:
+      return action.isLoading
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
 
 export function data(
-    state: Object = initialState,
-    action: { type: string, data: Array<Object> }
+  state: Object = initialState,
+  action: { type: string, data: Array<Object> }
 ) {
-    switch (action.type) {
-        case FETCH_DATA_SUCCESS:
-            return action.data
+  switch (action.type) {
+    case FETCH_DATA_SUCCESS:
+      return action.data
 
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
