df72b93934dbef9349c3ea1aadd268104bd13007
wildlifehexagon <crossthebreeze@gmail.com>
2018-04-04 16:56:52 -0500

    first attempt at using HOC with react-redux connect

---
+++ apps/genome-page/src/features/GeneOntology/WithGoDataRendering.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import Skeleton from "react-loading-skeleton"
+import { fetchData } from "features/GeneOntology/goActions"
+
+const WithGoDataRendering = WrappedComponent => props => {
+  class WithGoDataRenderingComponent extends Component {
+    componentDidMount() {
+      this.props.fetchData("https://api.myjson.com/bins/6vbot")
+    }
+    render() {
+      if (this.props.hasErrored) {
+        return (
+          <p>
+            Sorry! There was an error loading the items:{" "}
+            {this.props.hasErrored.message}
+          </p>
+        )
+      }
+
+      if (this.props.isLoading) {
+        return (
+          <div>
+            <Skeleton count={10} />
+          </div>
+        )
+      }
+
+      return <WrappedComponent {...this.props} />
+    }
+  }
+  const mapStateToProps = state => {
+    return {
+      data: state.data,
+      hasErrored: state.dataHasErrored,
+      isLoading: state.dataIsLoading
+    }
+  }
+
+  const mapDispatchToProps = dispatch => {
+    return {
+      fetchData: url => dispatch(fetchData(url))
+    }
+  }
+  return connect(mapStateToProps, mapDispatchToProps)(
+    WithGoDataRenderingComponent
+  )
+}
+
+export default WithGoDataRendering
