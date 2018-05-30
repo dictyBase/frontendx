3667ef7221e402bc65c39c2aa5cf50ece68d69b7
wildlifehexagon <crossthebreeze@gmail.com>
2018-05-30 14:07:29 -0500

    update react and add initial protein information tab

--- apps/genome-page/src/Routes.js
+++ apps/genome-page/src/Routes.js
@@ -1,68 +1,67 @@
 // @flow
-import React from 'react'
+import React from "react"
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect
-} from 'react-router-dom'
-import Tabs from 'dicty-components-tab'
-import GeneOntologyContainer from 'features/GeneOntology/GeneOntologyContainer'
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom"
+import Tabs from "dicty-components-tab"
+import GeneOntologyContainer from "features/GeneOntology/GeneOntologyContainer"
+import ProteinInformationContainer from "features/ProteinInformation/ProteinInformationContainer"
 
 const tabs: Array<Object> = [
-    {
-        title: 'Gene Summary',
-        element: <div>Gene Summary</div>,
-        link: 'summary'
-    },
-    {
-        title: 'Protein Information',
-        element: <div>Protein Information</div>,
-        link: 'proteininformation'
-    },
-    {
-        title: 'Gene Ontology',
-        element: <GeneOntologyContainer />,
-        link: 'go'
-    },
-    {
-        title: 'Orthologs',
-        element: <div>Orthologs</div>,
-        link: 'orthologs'
-    },
-    {
-        title: 'Phenotypes',
-        element: <div>Phenotypes</div>,
-        link: 'phenotypes'
-    },
-    {
-        title: 'Reference',
-        element: <div>Reference</div>,
-        link: 'reference'
-    },
-    {
-        title: 'BLAST',
-        element: <div>BLAST</div>,
-        link: 'blast'
-    }
+  {
+    title: "Gene Summary",
+    element: <div>Gene Summary</div>,
+    link: "summary",
+  },
+  {
+    title: "Protein Information",
+    element: <ProteinInformationContainer />,
+    link: "proteininformation",
+  },
+  {
+    title: "Gene Ontology",
+    element: <GeneOntologyContainer />,
+    link: "go",
+  },
+  {
+    title: "Orthologs",
+    element: <div>Orthologs</div>,
+    link: "orthologs",
+  },
+  {
+    title: "Phenotypes",
+    element: <div>Phenotypes</div>,
+    link: "phenotypes",
+  },
+  {
+    title: "Reference",
+    element: <div>Reference</div>,
+    link: "reference",
+  },
+  {
+    title: "BLAST",
+    element: <div>BLAST</div>,
+    link: "blast",
+  },
 ]
 
+// route needs to be updated with non-hardcoded version
+
 const Routes = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route
-                    exact
-                    path="/"
-                    component={() => <Redirect to="/gene/p2xA" />}
-                />
-                <Route
-                    path="/gene/:id"
-                    component={props => <Tabs {...props} tabs={tabs} />}
-                />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <Redirect to="/gene/p2xA" />} />
+        <Route
+          path="/gene/:id"
+          component={props => <Tabs {...props} tabs={tabs} />}
+        />
+      </Switch>
+    </Router>
+  )
 }
 
 export default Routes
