66fd8074b4bc1e66913f24092cae0e6627179ffe
wildlifehexagon <crossthebreeze@gmail.com>
2018-05-30 09:28:52 -0500

    reconfigure HOC to only fetch data once and convert tabs to presentational

--- apps/genome-page/src/features/GeneOntology/ExperimentalGO.js
+++ apps/genome-page/src/features/GeneOntology/ExperimentalGO.js
@@ -1,87 +1,49 @@
 // @flow
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
-import Skeleton from "react-loading-skeleton"
-import { fetchData } from "./goActions"
 import DisplayTable from "./DisplayTable"
+import WithGoDataRendering from "./WithGoDataRendering"
 import { Wrapper, GoHeaderStyle } from "styles/style"
 
 type Props = {
   /** The data fetched from the API */
   data: Object,
-  /** Check if items could not be loaded */
-  hasErrored: boolean,
-  /** Check if page is loading */
-  isLoading: boolean,
-  /** Action creator that accepts URL as parameter */
-  fetchData: Function
 }
 
 /**
- * Fetches and shares only the Experimental GO data (IMP, IGI, IDA)
+ * Filters and shares only the Experimental GO data (IMP, IGI, IDA)
  */
 
-export class ExperimentalGO extends Component<Props> {
-  componentDidMount() {
-    this.props.fetchData("https://api.myjson.com/bins/6vbot")
-  }
+const ExperimentalGO = (props: Props) => {
+  const data = props.data.filter(
+    code =>
+      code.evidence === "IMP" ||
+      code.evidence === "IGI" ||
+      code.evidence === "IDA",
+  )
 
-  render() {
-    const data = this.props.data.filter(
-      code =>
-        code.evidence === "IMP" ||
-        code.evidence === "IGI" ||
-        code.evidence === "IDA"
-    )
-    if (this.props.hasErrored) {
-      return (
-        <p>
-          Sorry! There was an error loading the items:{" "}
-          {this.props.hasErrored.message}
-        </p>
-      )
-    }
-
-    if (this.props.isLoading) {
-      return (
-        <div>
-          <Skeleton count={10} />
-        </div>
-      )
-    }
-    return (
-      <Wrapper>
-        <GoHeaderStyle>
-          <h3>Molecular Function</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-        <br />
-        <GoHeaderStyle>
-          <h3>Biological Process</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-        <br />
-        <GoHeaderStyle>
-          <h3>Cellular Composition</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-      </Wrapper>
-    )
-  }
+  return (
+    <Wrapper>
+      <GoHeaderStyle>
+        <h3>Molecular Function</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+      <br />
+      <GoHeaderStyle>
+        <h3>Biological Process</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+      <br />
+      <GoHeaderStyle>
+        <h3>Cellular Composition</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+    </Wrapper>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    data: state.data,
-    hasErrored: state.dataHasErrored,
-    isLoading: state.dataIsLoading
-  }
-}
+const mapStateToProps = ({ data }) => ({ data })
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchData: url => dispatch(fetchData(url))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ExperimentalGO)
+export default connect(mapStateToProps, null)(
+  WithGoDataRendering(ExperimentalGO),
+)
--- apps/genome-page/src/features/GeneOntology/ManualGO.js
+++ apps/genome-page/src/features/GeneOntology/ManualGO.js
@@ -1,88 +1,47 @@
 // @flow
-import React, { Component } from "react"
+import React from "react"
 import { connect } from "react-redux"
-import Skeleton from "react-loading-skeleton"
-import { fetchData } from "./goActions"
 import DisplayTable from "./DisplayTable"
+import WithGoDataRendering from "./WithGoDataRendering"
 import { Wrapper, GoHeaderStyle } from "styles/style"
 
 type Props = {
   /** The data fetched from the API */
   data: Object,
-  /** Check if items could not be loaded */
-  hasErrored: boolean,
-  /** Check if page is loading */
-  isLoading: boolean,
-  /** Action creator that accepts URL as parameter */
-  fetchData: Function
 }
 
 /**
- * Fetches and shares only the Manual GO data (IMP, IGI, IDA, IBA)
+ * Filters and shares only the Manual GO data (IMP, IGI, IDA, IBA)
  */
 
-export class ManualGO extends Component<Props> {
-  componentDidMount() {
-    this.props.fetchData("https://api.myjson.com/bins/6vbot")
-  }
-
-  render() {
-    const data = this.props.data.filter(
-      code =>
-        code.evidence === "IMP" ||
-        code.evidence === "IGI" ||
-        code.evidence === "IDA" ||
-        code.evidence === "IBA"
-    )
-    if (this.props.hasErrored) {
-      return (
-        <p>
-          Sorry! There was an error loading the items:{" "}
-          {this.props.hasErrored.message}
-        </p>
-      )
-    }
-
-    if (this.props.isLoading) {
-      return (
-        <div>
-          <Skeleton count={10} />
-        </div>
-      )
-    }
-    return (
-      <Wrapper>
-        <GoHeaderStyle>
-          <h3>Molecular Function</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-        <br />
-        <GoHeaderStyle>
-          <h3>Biological Process</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-        <br />
-        <GoHeaderStyle>
-          <h3>Cellular Composition</h3>
-        </GoHeaderStyle>
-        <DisplayTable data={data} />
-      </Wrapper>
-    )
-  }
+const ManualGO = (props: Props) => {
+  const data = props.data.filter(
+    code =>
+      code.evidence === "IMP" ||
+      code.evidence === "IGI" ||
+      code.evidence === "IDA" ||
+      code.evidence === "IBA",
+  )
+  return (
+    <Wrapper>
+      <GoHeaderStyle>
+        <h3>Molecular Function</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+      <br />
+      <GoHeaderStyle>
+        <h3>Biological Process</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+      <br />
+      <GoHeaderStyle>
+        <h3>Cellular Composition</h3>
+      </GoHeaderStyle>
+      <DisplayTable data={data} />
+    </Wrapper>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    data: state.data,
-    hasErrored: state.dataHasErrored,
-    isLoading: state.dataIsLoading
-  }
-}
+const mapStateToProps = ({ data }) => ({ data })
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchData: url => dispatch(fetchData(url))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ManualGO)
+export default connect(mapStateToProps, null)(WithGoDataRendering(ManualGO))
