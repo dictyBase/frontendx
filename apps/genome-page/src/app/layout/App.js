01baadcfb78db63ee1de365fd681569dc5cfc9bb
wildlifehexagon <crossthebreeze@gmail.com>
2018-05-30 18:47:33 -0500

    add page header and router wrapper

--- apps/genome-page/src/app/layout/App.js
+++ apps/genome-page/src/app/layout/App.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import { Header, Footer } from 'dicty-components-header-footer'
-import { Navbar } from 'dicty-components-navbar'
-import { FooterLinks } from 'common/constants/Footer'
-import { NavbarLinks } from 'common/constants/Navbar'
-import Routes from 'Routes'
+import React from "react"
+import { Header, Footer } from "dicty-components-header-footer"
+import { Navbar } from "dicty-components-navbar"
+import { FooterLinks } from "common/constants/Footer"
+import { NavbarLinks } from "common/constants/Navbar"
+import Routes from "Routes"
 
-const App = () => {
-    return (
-        <div className="wrapper">
-            <Header
-                downloads="/downloads"
-                info="/information"
-                cite="/citation"
-            />
-            <Navbar items={NavbarLinks} />
-            <Routes />
-            <Footer items={FooterLinks} />
-        </div>
-    )
+const App = props => {
+  return (
+    <div className="wrapper">
+      <Header downloads="/downloads" info="/information" cite="/citation" />
+      <Navbar items={NavbarLinks} />
+      <h1>Gene Information for p2xA</h1>
+      <Routes />
+      <Footer items={FooterLinks} />
+    </div>
+  )
 }
 
 export default App
