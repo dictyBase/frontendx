438c789239c70942bcaf0ba54a808ce5514aa3b3
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-05 16:58:56 -0500

    set up store for auth

--- apps/genome-page/src/app/store/configureStore.js
+++ apps/genome-page/src/app/store/configureStore.js
@@ -1,12 +1,40 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from 'app/reducers/rootReducer'
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import { routerMiddleware } from "react-router-redux"
+import { manageStateStorage } from "dicty-components-redux"
+import createHistory from "history/createBrowserHistory"
+import callAPI from "common/middlewares/callAPI"
+import rootReducer from "app/reducers/rootReducer"
+import { LOGIN_SUCCESS, LOGOUT_SUCCESS } from "common/constants/types"
 
-export default function configureStore(initialState) {
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-    return createStore(
-        rootReducer,
-        initialState,        
-        composeEnhancers(applyMiddleware(thunk))
-    )
+export const history = createHistory()
+
+const authArg = {
+  save_action: LOGIN_SUCCESS,
+  remove_action: LOGOUT_SUCCESS,
+  key: "auth",
+  namespace: "auth",
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const enhancer = composeEnhancers(
+  applyMiddleware(
+    routerMiddleware(history),
+    thunk,
+    callAPI,
+    manageStateStorage(authArg),
+  ),
+)
+
+export default function configureStore(initialState: Object) {
+  const store = createStore(rootReducer, initialState, enhancer)
+  if (process.env.NODE_ENV === "development") {
+    if (module.hot) {
+      module.hot.accept("app/reducers/rootReducer", () =>
+        store.replaceReducer(require("app/reducers/rootReducer").default),
+      )
+    }
+  }
+  return store
 }
--- apps/genome-page/src/index.js
+++ apps/genome-page/src/index.js
@@ -2,25 +2,25 @@ import "whatwg-fetch"
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
-import configureStore from "app/store/configureStore"
+import { ConnectedRouter } from "react-router-redux"
+import { hydrateStore } from "dicty-components-redux"
+import configureStore, { history } from "app/store/configureStore"
 import App from "app/layout/App"
 import registerServiceWorker from "./registerServiceWorker"
 
 import "styles/style.css"
 import "styles/style.js"
 
-const initialState = {}
+// load state from localStorage(if any) to set the initial state for the store
+const initialState = hydrateStore({ key: "auth", namespace: "auth" })
 
 const store = configureStore(initialState)
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </div>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root"),
 )
