396a3f17eb1c4d44d34944d669616c85f2fa8548
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-18 14:49:33 -0500

    update login and header-footer components to latest versions

--- apps/genome-page/src/styles/style.js
+++ apps/genome-page/src/styles/style.js
@@ -3,7 +3,7 @@ import { Tabs, TabItem } from "rebass"
 import { lighten } from "polished"
 
 injectGlobal([
-    `
+  `
     html {
         height: 100%;
     }
@@ -16,60 +16,60 @@ injectGlobal([
         color: #333;
         background-color: #fff;
     }
-`
+`,
 ])
 
 export const Root = styled.div`
-    height: 100vh;
+  height: 100vh;
 `
 
 export const ReactVirtualizedTable = styled.div`
-    .ReactVirtualized__Table__headerRow {
-        background-color: #e5efff;
-    }
+  .ReactVirtualized__Table__headerRow {
+    background-color: #e5efff;
+  }
 
-    .ReactVirtualized__Table__Grid {
-        margin: 0px auto;
-    }
+  .ReactVirtualized__Table__Grid {
+    margin: 0px auto;
+  }
 `
 export const Wrapper = styled.div`
-    text-align: center;
+  text-align: center;
 `
 
 export const GoHeaderStyle = styled.div`
-    text-align: left;
-    background: #15317e;
-    color: #fff;
-    padding: 1px 10px 1px;
+  text-align: left;
+  background: #15317e;
+  color: #fff;
+  padding: 1px 10px 1px;
 `
 
 export const TabContainer = styled(Tabs)`
-    border-bottom: 0px;
+  border-bottom: 0px;
 `
 
 export const Tab = styled(TabItem)`
-    background: ${props =>
-        !props.active && props.theme.secondary ? "#A3BAE9" : "#15317e"};
-    border-top: 1px solid white;
-    border-right: 1px solid black;
-    border-left: 1px solid black;
-    border-top-right-radius: 2px;
-    border-top-left-radius: 2px;
-    margin-right: 3px;
-    margin-top: 1px;
-    padding: 5px;
-    width: 150px;
-    color: ${props =>
-        !props.active && props.theme.secondary ? "black" : "white"};
-    border-color: ${props => (props.active ? "black" : "")};
+  background: ${props =>
+    !props.active && props.theme.secondary ? "#A3BAE9" : "#15317e"};
+  border-top: 1px solid white;
+  border-right: 1px solid black;
+  border-left: 1px solid black;
+  border-top-right-radius: 2px;
+  border-top-left-radius: 2px;
+  margin-right: 3px;
+  margin-top: 1px;
+  padding: 5px;
+  width: 150px;
+  color: ${props =>
+    !props.active && props.theme.secondary ? "black" : "white"};
+  border-color: ${props => (props.active ? "black" : "")};
 
-    &:hover {
-        background: ${props =>
-            !props.active && props.theme.secondary
-                ? lighten(0.1, props.theme.secondary)
-                : "#15317e"};
-        color: ${props =>
-            !props.active && props.theme.secondary ? "black" : "white"};
-        cursor: pointer;
-    }
+  &:hover {
+    background: ${props =>
+      !props.active && props.theme.secondary
+        ? lighten(0.1, props.theme.secondary)
+        : "#15317e"};
+    color: ${props =>
+      !props.active && props.theme.secondary ? "black" : "white"};
+    cursor: pointer;
+  }
 `
