1ecae29212a5895ad5714db947dbe10a5af4d8c0
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-25 13:39:45 -0500

    fix formatting of example page links

--- apps/genome-page/src/features/MainPage/MainPage.js
+++ apps/genome-page/src/features/MainPage/MainPage.js
@@ -6,23 +6,21 @@ const MainPage = () => {
     <div>
       <h1>Genomes</h1>
       <div>Example genome pages:</div>
-      <ul>
-        <li>
-          <Link to="/DDB_G0272608">DDB_G0272608</Link>
-        </li>
-        <li>
-          <Link to="/DDB_G0288511">DDB_G0288511</Link>
-        </li>
-        <li>
-          <Link to="/DDB_G0269114">DDB_G0269114</Link>
-        </li>
-        <li>
-          <Link to="/DDB_G0286355">DDB_G0286355</Link>
-        </li>
-        <li>
-          <Link to="/DDB_G0277399">DDB_G0277399</Link>
-        </li>
-      </ul>
+      <p>
+        <Link to="/DDB_G0272608">DDB_G0272608</Link>
+      </p>
+      <p>
+        <Link to="/DDB_G0288511">DDB_G0288511</Link>
+      </p>
+      <p>
+        <Link to="/DDB_G0269114">DDB_G0269114</Link>
+      </p>
+      <p>
+        <Link to="/DDB_G0286355">DDB_G0286355</Link>
+      </p>
+      <p>
+        <Link to="/DDB_G0277399">DDB_G0277399</Link>
+      </p>
     </div>
   )
 }
