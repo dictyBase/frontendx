69ec44e22ae8c3f94ea521d51cb872660aff82da
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-26 17:37:11 -0500

    remove old tabs code

--- apps/genome-page/src/common/components/tabs/tabSelectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect'
-
-export const selectTabs = state => state.tabs
-
-export const selectCurrentTab = createSelector(
-    selectTabs,
-    tabs => tabs.currentTab
-)
