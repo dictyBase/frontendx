e00fc805dce86d6b2cbb1ae9a6f2fc9fb025c142
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-26 15:34:47 -0500

    remove old dicty dependencies

--- apps/genome-page/flow-typed/Types.js
+++ apps/genome-page/flow-typed/Types.js
@@ -1,32 +1,23 @@
 // node_modules
-declare module 'react-virtualized' {
-    declare module.exports: any;
+declare module "styled-components" {
+  declare module.exports: any
 }
-declare module 'dicty-components-tab' {
-    declare module.exports: any;
-}
-declare module 'styled-components' {
-    declare module.exports: any;
-}
-declare module 'react-toggle-display' {
-    declare module.exports: any;
-}
-declare module 'rebass' {
-    declare module.exports: any;
+declare module "rebass" {
+  declare module.exports: any
 }
 
 // styles
-declare module 'react-virtualized/styles.css' {
-    declare module.exports: any;
+declare module "react-virtualized/styles.css" {
+  declare module.exports: any
 }
 
 // components
-declare module 'features/GeneOntology/GeneOntologyContainer' {
-    declare module.exports: any;
+declare module "features/GeneOntology/GeneOntologyContainer" {
+  declare module.exports: any
 }
-declare module 'styles/style' {
-    declare module.exports: any;
+declare module "styles/style" {
+  declare module.exports: any
 }
-declare module 'common/utils/reducerUtils' {
-    declare module.exports: any;
+declare module "common/utils/reducerUtils" {
+  declare module.exports: any
 }
