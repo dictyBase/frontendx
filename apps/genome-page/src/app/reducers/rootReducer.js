3715854b687e741a58d94c3243690745ac31c470
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-26 17:37:11 -0500

    remove old tabs code

--- apps/genome-page/src/app/reducers/rootReducer.js
+++ apps/genome-page/src/app/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux"
 import goDataReducer from "features/GeneOntology/goReducer"
-import tabReducer from "common/components/tabs/tabReducer"
 import proteinDataReducer from "features/ProteinInformation/proteinReducer"
 import authReducer from "features/Authentication/authReducer"
 
 const rootReducer = combineReducers({
-  tabs: tabReducer,
   goData: goDataReducer,
   proteinData: proteinDataReducer,
   auth: authReducer,
