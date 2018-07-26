8f9445c797cc96c231f651d87b7b810aacc83cfd
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-26 16:53:41 -0500

    add basic panel and gene summary container components

---
+++ apps/genome-page/src/features/GeneSummary/GeneSummaryContainer.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Panel from "common/components/Panel"
+
+const GeneSummaryContainer = () => {
+  return (
+    <div>
+      <Panel title="General Information" data="test" />
+    </div>
+  )
+}
+
+export default GeneSummaryContainer
