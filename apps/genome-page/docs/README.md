075e8ef77c1527241170c1abb1e59c873f0b9f98
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-27 15:10:43 -0500

    add shape of state documentation

---
+++ apps/genome-page/docs/README.md
@@ -0,0 +1,7 @@
+# Dicty Stock Center
+
+## Table of Contents
+
+- [Readme](/README.md)
+- Redux
+  - [Shape of the state](/docs/redux/state.md)
--- apps/genome-page/flow-typed/Types.js
+++ apps/genome-page/flow-typed/Types.js
@@ -6,18 +6,10 @@ declare module "rebass" {
   declare module.exports: any
 }
 
-// styles
-declare module "react-virtualized/styles.css" {
-  declare module.exports: any
-}
-
 // components
 declare module "features/GeneOntology/GeneOntologyContainer" {
   declare module.exports: any
 }
 declare module "styles/style" {
-  declare module.exports: any
-}
-declare module "common/utils/reducerUtils" {
   declare module.exports: any
 }
