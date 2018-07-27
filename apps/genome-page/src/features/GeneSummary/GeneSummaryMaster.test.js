607207163de446321d15ba1c808faa585348c171
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-27 14:48:58 -0500

    set up basic tests for GeneSummaryMaster

--- apps/genome-page/src/features/GeneSummary/GeneSummaryMaster.test.js
+++ apps/genome-page/src/features/GeneSummary/GeneSummaryMaster.test.js
@@ -2,7 +2,56 @@ import React from "react"
 import { shallow } from "enzyme"
 import "../../setupTests"
 import { GeneSummaryMaster } from "./GeneSummaryMaster"
+import Tab from "@material-ui/core/Tab"
 
 describe("GeneSummary/GeneSummaryMaster", () => {
-  // tests go here
+  const json = {
+    data: {
+      attributes: {
+        group: ["protein", "goa", "orthologs", "phenotypes", "references"],
+        subgroup: [
+          "general",
+          "genomic",
+          "protein",
+          "goa",
+          "dbxrefs",
+          "summary",
+          "publication",
+        ],
+      },
+    },
+  }
+
+  const props = {
+    classes: {
+      root: {},
+      tabs: {},
+      tab: {},
+    },
+    match: {
+      params: {
+        id: "id number",
+      },
+    },
+  }
+  const wrapper = shallow(<GeneSummaryMaster {...props} />)
+  it("renders without crashing", () => {
+    shallow(<GeneSummaryMaster {...props} />)
+  })
+
+  describe("generateTabs method", () => {
+    const generateTabs = wrapper.instance().generateTabs(json)
+
+    it("should produce an array of five items", () => {
+      expect(generateTabs.length).toBe(5)
+    })
+  })
+
+  describe("generatePanels method", () => {
+    const generatePanels = wrapper.instance().generatePanels(json)
+
+    it("should produce an array of seven items", () => {
+      expect(generatePanels.length).toBe(7)
+    })
+  })
 })
