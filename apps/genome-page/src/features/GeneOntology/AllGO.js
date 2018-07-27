3ac7b0c0e9da275944f633f87d3e816db5f7ef72
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-27 11:56:31 -0500

    remove unnecessary components and old snapshot tests

--- apps/genome-page/src/features/GeneOntology/AllGO.js
+++ apps/genome-page/src/features/GeneOntology/AllGO.js
@@ -3,7 +3,6 @@ import React, { Component } from "react"
 import { connect } from "react-redux"
 import Grid from "@material-ui/core/Grid"
 import { fetchData } from "./goaActions"
-import DisplayTable from "./DisplayTable"
 import WithGoDataRendering from "./WithGoDataRendering"
 import Panel from "common/components/Panel"
 
@@ -28,13 +27,13 @@ export class AllGO extends Component<Props> {
       <Grid container>
         <Grid item sm={12} md={12} lg={12} xl={12}>
           <Panel title="Molecular Function">
-            <DisplayTable data={data} />
+            <div>data table coming later</div>
           </Panel>
           <Panel title="Biological Process">
-            <DisplayTable data={data} />
+            <div>data table coming later</div>
           </Panel>
           <Panel title="Cellular Composition">
-            <DisplayTable data={data} />
+            <div>data table coming later</div>
           </Panel>
         </Grid>
       </Grid>
