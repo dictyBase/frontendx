a02ac51f2a921fedb4df7d5c489ca591bc9946af
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-27 16:54:46 -0500

    change fetch_data to fetch_goa_data

--- apps/genome-page/src/features/GeneOntology/goaActions.js
+++ apps/genome-page/src/features/GeneOntology/goaActions.js
@@ -1,57 +1,57 @@
 // @flow
 import {
-  FETCH_DATA_REQUEST,
-  FETCH_DATA_FAILURE,
-  FETCH_DATA_SUCCESS,
+  FETCH_GOA_DATA_REQUEST,
+  FETCH_GOA_DATA_FAILURE,
+  FETCH_GOA_DATA_SUCCESS,
 } from "./goaConstants"
 
 /**
  * All of the Redux actions related to the GO tab
  */
 
-export function fetchDataRequest(bool: boolean) {
+export function fetchGoaDataRequest(bool: boolean) {
   return {
-    type: FETCH_DATA_REQUEST,
+    type: FETCH_GOA_DATA_REQUEST,
     payload: {
       isLoading: true,
     },
   }
 }
 
-export function fetchDataFailure(bool: boolean) {
+export function fetchGoaDataFailure(bool: boolean) {
   return {
-    type: FETCH_DATA_FAILURE,
+    type: FETCH_GOA_DATA_FAILURE,
     payload: {
       hasErrored: true,
     },
   }
 }
 
-export function fetchDataSuccess(data: Array<Object>) {
+export function fetchGoaDataSuccess(data: Array<Object>) {
   return {
-    type: FETCH_DATA_SUCCESS,
+    type: FETCH_GOA_DATA_SUCCESS,
     payload: {
       data,
     },
   }
 }
 
-export function fetchData(url: string) {
+export function fetchGoaData(url: string) {
   return async (dispatch: Function) => {
     try {
       const res = await fetch(url)
       const data = await res.json()
       if (res.ok) {
-        dispatch(fetchDataSuccess(data))
+        dispatch(fetchGoaDataSuccess(data))
       } else {
-        dispatch(fetchDataRequest(false))
+        dispatch(fetchGoaDataRequest(false))
         if (process.env.NODE_ENV !== "production") {
           console.error(res.statusText)
         }
         throw Error(res.statusText)
       }
     } catch (error) {
-      dispatch(fetchDataFailure(true))
+      dispatch(fetchGoaDataFailure(true))
       if (process.env.NODE_ENV !== "production") {
         console.error(`Network error: ${error}`)
       }
