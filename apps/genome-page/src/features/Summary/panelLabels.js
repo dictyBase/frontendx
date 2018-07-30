b769421f02b19cc13316652643ca8bc6e3e1ffaf
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-30 13:57:22 -0500

    refactor panel labels function

---
+++ apps/genome-page/src/features/Summary/panelLabels.js
@@ -0,0 +1,9 @@
+export const panelLabels = {
+  general: "General Information",
+  genomic: "Genomic Information",
+  protein: "Gene Product Information",
+  goa: "Gene Ontology Annotations",
+  dbxrefs: "Links",
+  summary: "Summary",
+  publication: "Latest References",
+}
