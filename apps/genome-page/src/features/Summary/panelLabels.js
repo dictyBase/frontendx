887148acb703869530047928e4c95d4e56b3e62d
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-31 13:38:32 -0500

    reconfigure panelLabels to include custom components

--- apps/genome-page/src/features/Summary/panelLabels.js
+++ apps/genome-page/src/features/Summary/panelLabels.js
@@ -1,3 +1,5 @@
+import GoaPanel from "features/Summary/Panels/GoaPanel"
+
 export const panelLabels = {
   general: "General Information",
   genomic: "Genomic Information",
@@ -6,4 +8,35 @@ export const panelLabels = {
   dbxrefs: "Links",
   summary: "Summary",
   publication: "Latest References",
+}
+
+export const panelLabelsWithComponents = {
+  general: {
+    title: "General Information",
+    component: GoaPanel,
+  },
+  genomic: {
+    title: "Genomic Information",
+    component: GoaPanel,
+  },
+  protein: {
+    title: "Gene Product Information",
+    component: GoaPanel,
+  },
+  goa: {
+    title: "Gene Ontology Annotations",
+    component: GoaPanel,
+  },
+  dbxrefs: {
+    title: "Links",
+    component: GoaPanel,
+  },
+  summary: {
+    title: "Summary",
+    component: GoaPanel,
+  },
+  publication: {
+    title: "Latest References",
+    component: GoaPanel,
+  },
 }
