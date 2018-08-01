5ffcd721dd9814246797e12a120d6f8535e2e36e
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-01 14:11:44 -0500

    add table styling and remove panels if no data

--- apps/genome-page/src/features/Ontology/InnerGoPanel.js
+++ apps/genome-page/src/features/Ontology/InnerGoPanel.js
@@ -15,7 +15,6 @@ type Props = {
 
 const InnerGoPanel = (props: Props) => {
   const { goaData } = props
-  console.log(goaData)
 
   // set variables that represent filtered arrays for use in each row
   const molecular = goaData.filter(item => item.type === "molecular_function")
@@ -25,15 +24,21 @@ const InnerGoPanel = (props: Props) => {
   return (
     <Grid container>
       <Grid item sm={12} md={12} lg={12} xl={12}>
-        <PanelWrapper title="Molecular Function">
-          <DisplayTable goaData={molecular} />
-        </PanelWrapper>
-        <PanelWrapper title="Biological Process">
-          <DisplayTable goaData={biological} />
-        </PanelWrapper>
-        <PanelWrapper title="Cellular Composition">
-          <DisplayTable goaData={cellular} />
-        </PanelWrapper>
+        {molecular.length > 0 && (
+          <PanelWrapper title="Molecular Function">
+            <DisplayTable goaData={molecular} />
+          </PanelWrapper>
+        )}
+        {biological.length > 0 && (
+          <PanelWrapper title="Biological Process">
+            <DisplayTable goaData={biological} />
+          </PanelWrapper>
+        )}
+        {cellular.length > 0 && (
+          <PanelWrapper title="Cellular Composition">
+            <DisplayTable goaData={cellular} />
+          </PanelWrapper>
+        )}
       </Grid>
     </Grid>
   )
