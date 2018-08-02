290d1dbb4cb0dc814fe1495038b59d09db3e493a
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-02 17:11:59 -0500

    add more basic unit tests

--- apps/genome-page/src/features/Ontology/OntologyContainer.test.js
+++ apps/genome-page/src/features/Ontology/OntologyContainer.test.js
@@ -2,6 +2,7 @@ import React from "react"
 import { shallow } from "enzyme"
 import "../../setupTests"
 import OntologyContainer from "./OntologyContainer"
+import AppBar from "@material-ui/core/AppBar"
 
 describe("Ontology/OntologyContainer", () => {
   const wrapper = shallow(<OntologyContainer />)
@@ -9,5 +10,9 @@ describe("Ontology/OntologyContainer", () => {
     it("renders without crashing", () => {
       wrapper
     })
+
+    // it("always renders one AppBar", () => {
+    //   expect(wrapper.find(AppBar).length).toBe(1)
+    // })
   })
 })
