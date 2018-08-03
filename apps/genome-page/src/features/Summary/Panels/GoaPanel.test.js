bf0c03e8521c39c76ba25f5c324909d986a00050
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-03 08:54:26 -0500

    fix broken testsS

--- apps/genome-page/src/features/Summary/Panels/GoaPanel.test.js
+++ apps/genome-page/src/features/Summary/Panels/GoaPanel.test.js
@@ -10,78 +10,82 @@ import TableCell from "@material-ui/core/TableCell"
 describe("Summary/Panels/GoaPanel", () => {
   const props = {
     classes: {},
-    data: [
-      {
-        type: "biological_process",
-        id: "GO:0000281",
-        attributes: {
-          date: "20120522",
-          evidence_code: "IMP",
-          goterm: "mitotic cytokinesis",
-          qualifier: "acts_upstream_of_or_within",
-          publication: "PMID:12499361",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
+    panelData: {
+      data: {
+        data: [
+          {
+            type: "biological_process",
+            id: "GO:0000281",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IMP",
+              goterm: "mitotic cytokinesis",
+              qualifier: "acts_upstream_of_or_within",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "biological_process",
+            id: "GO:0000902",
+            attributes: {
+              date: "20051219",
+              evidence_code: "IMP",
+              goterm: "cell morphogenesis",
+              qualifier: "acts_upstream_of_or_within",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "molecular_function",
+            id: "GO:0005515",
+            attributes: {
+              date: "20110913",
+              evidence_code: "IPI",
+              goterm: "protein binding",
+              qualifier: "enables",
+              publication: "PMID:21441344",
+              with: [{ connectedXrefs: [{ db: "UniProtKB", id: "Q54HG2" }] }],
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "cellular_component",
+            id: "GO:0005938",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IDA",
+              goterm: "cell cortex",
+              qualifier: "part_of",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "cellular_component",
+            id: "GO:0015629",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IDA",
+              goterm: "actin cytoskeleton",
+              qualifier: "part_of",
+              publication: "PMID:21441344",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+        ],
       },
-      {
-        type: "biological_process",
-        id: "GO:0000902",
-        attributes: {
-          date: "20051219",
-          evidence_code: "IMP",
-          goterm: "cell morphogenesis",
-          qualifier: "acts_upstream_of_or_within",
-          publication: "PMID:12499361",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-      {
-        type: "molecular_function",
-        id: "GO:0005515",
-        attributes: {
-          date: "20110913",
-          evidence_code: "IPI",
-          goterm: "protein binding",
-          qualifier: "enables",
-          publication: "PMID:21441344",
-          with: [{ connectedXrefs: [{ db: "UniProtKB", id: "Q54HG2" }] }],
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-      {
-        type: "cellular_component",
-        id: "GO:0005938",
-        attributes: {
-          date: "20120522",
-          evidence_code: "IDA",
-          goterm: "cell cortex",
-          qualifier: "part_of",
-          publication: "PMID:12499361",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-      {
-        type: "cellular_component",
-        id: "GO:0015629",
-        attributes: {
-          date: "20120522",
-          evidence_code: "IDA",
-          goterm: "actin cytoskeleton",
-          qualifier: "part_of",
-          publication: "PMID:21441344",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-    ],
+    },
   }
   const wrapper = mount(<GoaPanel {...props} />)
 
@@ -108,53 +112,85 @@ describe("Summary/Panels/GoaPanel", () => {
   })
 })
 
-test("matching a snapshot of InfoPage/InfoPage", () => {
+test("matching a snapshot of Summary/Panels/GoaPanel", () => {
   const props = {
     classes: {},
-    data: [
-      {
-        type: "biological_process",
-        id: "GO:0000281",
-        attributes: {
-          date: "20120522",
-          evidence_code: "IMP",
-          goterm: "mitotic cytokinesis",
-          qualifier: "acts_upstream_of_or_within",
-          publication: "PMID:12499361",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
+    panelData: {
+      data: {
+        data: [
+          {
+            type: "biological_process",
+            id: "GO:0000281",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IMP",
+              goterm: "mitotic cytokinesis",
+              qualifier: "acts_upstream_of_or_within",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "biological_process",
+            id: "GO:0000902",
+            attributes: {
+              date: "20051219",
+              evidence_code: "IMP",
+              goterm: "cell morphogenesis",
+              qualifier: "acts_upstream_of_or_within",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "molecular_function",
+            id: "GO:0005515",
+            attributes: {
+              date: "20110913",
+              evidence_code: "IPI",
+              goterm: "protein binding",
+              qualifier: "enables",
+              publication: "PMID:21441344",
+              with: [{ connectedXrefs: [{ db: "UniProtKB", id: "Q54HG2" }] }],
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "cellular_component",
+            id: "GO:0005938",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IDA",
+              goterm: "cell cortex",
+              qualifier: "part_of",
+              publication: "PMID:12499361",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+          {
+            type: "cellular_component",
+            id: "GO:0015629",
+            attributes: {
+              date: "20120522",
+              evidence_code: "IDA",
+              goterm: "actin cytoskeleton",
+              qualifier: "part_of",
+              publication: "PMID:21441344",
+              with: null,
+              extensions: null,
+              assigned_by: "dictyBase",
+            },
+          },
+        ],
       },
-      {
-        type: "molecular_function",
-        id: "GO:0005515",
-        attributes: {
-          date: "20110913",
-          evidence_code: "IPI",
-          goterm: "protein binding",
-          qualifier: "enables",
-          publication: "PMID:21441344",
-          with: [{ connectedXrefs: [{ db: "UniProtKB", id: "Q54HG2" }] }],
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-      {
-        type: "cellular_component",
-        id: "GO:0005938",
-        attributes: {
-          date: "20120522",
-          evidence_code: "IDA",
-          goterm: "cell cortex",
-          qualifier: "part_of",
-          publication: "PMID:12499361",
-          with: null,
-          extensions: null,
-          assigned_by: "dictyBase",
-        },
-      },
-    ],
+    },
   }
 
   const component = renderer.create(<GoaPanel {...props} />)
