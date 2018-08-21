ebcfdf94995459c86f7c8d617969faad6b9844e0
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-21 15:02:28 -0500

    update navbar and footer to use json files and redux

--- apps/genome-page/src/app/layout/App.js
+++ apps/genome-page/src/app/layout/App.js
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import { withRouter } from "react-router-dom"
 import { Header, Footer } from "dicty-components-header-footer"
 import { Navbar } from "dicty-components-navbar"
-import { FooterLinks } from "common/constants/Footer"
-import { NavbarLinks } from "common/constants/Navbar"
+
+import fetchNavbar from "app/actions/navbarActions"
+import fetchFooter from "app/actions/footerActions"
+import footerItems from "common/constants/Footer"
+import navbarItems from "common/constants/Navbar"
 import {
   headerItems,
   loggedHeaderItems,
@@ -12,29 +15,84 @@ import {
 } from "common/utils/headerItems"
 import Routes from "app/routes/Routes"
 import MainBodyContainer from "styles/MainBodyContainer"
-import type { MapStateToProps } from "react-redux"
 
-export const App = props => {
-  return (
-    <div>
-      {props.auth.isAuthenticated ? (
-        <Header items={loggedHeaderItems}>
-          {items => items.map(generateLinks)}
-        </Header>
-      ) : (
-        <Header items={headerItems}>{items => items.map(generateLinks)}</Header>
-      )}
-      <Navbar items={NavbarLinks} />
-      <MainBodyContainer>
-        <main>
-          <Routes />
-        </main>
-      </MainBodyContainer>
-      <Footer items={FooterLinks} />
-    </div>
-  )
+type Props = {
+  /** Object representing auth part of state */
+  auth: Object,
+  /** Object representing navbar part of state */
+  navbar: Object,
+  /** Object representing footer part of state */
+  footer: Object,
+  /** Action creator to fetch navbar content */
+  fetchNavbarAction: Function,
+  /** Action creator to fetch footer content */
+  fetchFooterAction: Function,
 }
 
-const mapStateToProps: MapStateToProps<*, *, *> = ({ auth }) => ({ auth })
+export class App extends Component<Props> {
+  componentDidMount() {
+    const { fetchNavbarAction, fetchFooterAction } = this.props
+    fetchNavbarAction()
+    fetchFooterAction()
+  }
 
-export default withRouter(connect(mapStateToProps)(App))
+  render() {
+    const { auth, navbar, footer } = this.props
+
+    // if any errors, fall back to old link setup
+    if (navbar.error || !navbar.links || footer.error || !footer.links) {
+      return (
+        <div>
+          {auth.isAuthenticated ? (
+            <Header items={loggedHeaderItems}>
+              {items => items.map(generateLinks)}
+            </Header>
+          ) : (
+            <Header items={headerItems}>
+              {items => items.map(generateLinks)}
+            </Header>
+          )}
+          <Navbar items={navbarItems} />
+          <MainBodyContainer>
+            <main>
+              <Routes />
+            </main>
+          </MainBodyContainer>
+          <Footer items={footerItems} />
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {auth.isAuthenticated ? (
+          <Header items={loggedHeaderItems}>
+            {items => items.map(generateLinks)}
+          </Header>
+        ) : (
+          <Header items={headerItems}>
+            {items => items.map(generateLinks)}
+          </Header>
+        )}
+        <Navbar items={navbar.links} />
+        <MainBodyContainer>
+          <main>
+            <Routes />
+          </main>
+        </MainBodyContainer>
+        <Footer items={footer.links} />
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ auth, navbar, footer }) => ({ auth, navbar, footer })
+
+// why rename action creator?
+// https://stackoverflow.com/questions/37682705/avoid-no-shadow-eslint-error-with-mapdispatchtoprops/42337137#42337137
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { fetchNavbarAction: fetchNavbar, fetchFooterAction: fetchFooter },
+  )(App),
+)
