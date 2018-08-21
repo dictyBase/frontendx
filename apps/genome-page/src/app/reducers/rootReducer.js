38697f33485bf7803eaab006e82227dd041792dc
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-21 15:02:28 -0500

    update navbar and footer to use json files and redux

--- apps/genome-page/src/app/reducers/rootReducer.js
+++ apps/genome-page/src/app/reducers/rootReducer.js
@@ -3,12 +3,16 @@ import summaryReducer from "features/Summary/summaryReducer"
 import goaReducer from "features/Ontology/goaReducer"
 import proteinReducer from "features/ProteinInformation/proteinReducer"
 import authReducer from "features/Authentication/authReducer"
+import footerReducer from "app/reducers/footerReducer"
+import navbarReducer from "app/reducers/navbarReducer"
 
 const rootReducer = combineReducers({
   general: summaryReducer,
   goa: goaReducer,
   protein: proteinReducer,
   auth: authReducer,
+  footer: footerReducer,
+  navbar: navbarReducer,
 })
 
 export default rootReducer
