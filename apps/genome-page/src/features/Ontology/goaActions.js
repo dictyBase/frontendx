c0ade3285405a7254a854a00a4b3c3525a16e127
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-21 15:08:41 -0500

    fix flow issues

--- apps/genome-page/src/features/Ontology/goaActions.js
+++ apps/genome-page/src/features/Ontology/goaActions.js
@@ -1,70 +1,65 @@
 // @flow
+import printError from "common/utils/printError"
 import {
   FETCH_GOA_REQUEST,
   FETCH_GOA_FAILURE,
   FETCH_GOA_SUCCESS,
 } from "./goaConstants"
-import printError from "common/utils/printError"
 
 /**
  * All of the Redux actions related to GOA data
  */
 
-const fetchGoaRequest = () => {
-  return {
-    type: FETCH_GOA_REQUEST,
-    payload: {
-      isFetching: true,
-    },
-  }
-}
+const fetchGoaRequest = () => ({
+  type: FETCH_GOA_REQUEST,
+  payload: {
+    isFetching: true,
+  },
+})
 
-const fetchGoaFailure = error => {
-  return {
-    type: FETCH_GOA_FAILURE,
-    payload: {
-      isFetching: false,
-      error: error,
-    },
-  }
-}
+const fetchGoaFailure = error => ({
+  type: FETCH_GOA_FAILURE,
+  payload: {
+    isFetching: false,
+    error,
+  },
+})
 
-const fetchGoaSuccess = (data: Object) => {
-  return {
-    type: FETCH_GOA_SUCCESS,
-    payload: {
-      isFetching: false,
-      data,
-    },
-  }
-}
+const fetchGoaSuccess = (data: Object) => ({
+  type: FETCH_GOA_SUCCESS,
+  payload: {
+    isFetching: false,
+    data,
+  },
+})
 
-export const fetchGoa = (url: string) => {
-  return async (dispatch: Function, getState) => {
-    if (getState().goa.data) {
-      return
-    }
-    try {
-      dispatch(fetchGoaRequest())
-      const res = await fetch(url, {
-        headers: { Accept: "application/json" },
-      })
-      const json = await res.json()
-      if (res.ok) {
-        if (json.status >= 300) {
-          dispatch(fetchGoaFailure(json.title))
-        }
-        dispatch(fetchGoaSuccess(json))
-      } else {
+export const fetchGoa = (url: string) => async (
+  dispatch: Function,
+  getState: Function,
+) => {
+  if (getState().goa.data) {
+    return
+  }
+  try {
+    dispatch(fetchGoaRequest())
+    const res = await fetch(url, {
+      headers: { Accept: "application/json" },
+    })
+    const json = await res.json()
+    if (res.ok) {
+      if (json.status >= 300) {
         dispatch(fetchGoaFailure(json.title))
-        if (process.env.NODE_ENV !== "production") {
-          printError(res, json)
-        }
       }
-    } catch (error) {
+      dispatch(fetchGoaSuccess(json))
+    } else {
+      dispatch(fetchGoaFailure(json.title))
       if (process.env.NODE_ENV !== "production") {
-        console.error(`Network error: ${error.message}`)
+        printError(res, json)
       }
+    }
+  } catch (error) {
+    if (process.env.NODE_ENV !== "production") {
+      console.error(`Network error: ${error.message}`)
     }
   }
 }
--- apps/genome-page/src/features/Summary/summaryActions.js
+++ apps/genome-page/src/features/Summary/summaryActions.js
@@ -1,71 +1,66 @@
 // @flow
 // import { push } from "connected-react-router"
+import printError from "common/utils/printError"
 import {
   FETCH_GENERAL_DATA_REQUEST,
   FETCH_GENERAL_DATA_FAILURE,
   FETCH_GENERAL_DATA_SUCCESS,
 } from "./summaryConstants"
-import printError from "common/utils/printError"
 
 /**
  * All of the Redux actions related to the Summary tab
  */
 
-const fetchGeneralDataRequest = () => {
-  return {
-    type: FETCH_GENERAL_DATA_REQUEST,
-    payload: {
-      isFetching: true,
-    },
-  }
-}
+const fetchGeneralDataRequest = () => ({
+  type: FETCH_GENERAL_DATA_REQUEST,
+  payload: {
+    isFetching: true,
+  },
+})
 
-const fetchGeneralDataFailure = error => {
-  return {
-    type: FETCH_GENERAL_DATA_FAILURE,
-    payload: {
-      isFetching: false,
-      error: error,
-    },
-  }
-}
+const fetchGeneralDataFailure = error => ({
+  type: FETCH_GENERAL_DATA_FAILURE,
+  payload: {
+    isFetching: false,
+    error,
+  },
+})
 
-const fetchGeneralDataSuccess = (data: Array<Object>) => {
-  return {
-    type: FETCH_GENERAL_DATA_SUCCESS,
-    payload: {
-      isFetching: false,
-      data,
-    },
-  }
-}
+const fetchGeneralDataSuccess = (data: Array<Object>) => ({
+  type: FETCH_GENERAL_DATA_SUCCESS,
+  payload: {
+    isFetching: false,
+    data,
+  },
+})
 
-export const fetchGeneralData = (url: string) => {
-  return async (dispatch: Function, getState) => {
-    if (getState().general.data) {
-      return
-    }
-    try {
-      dispatch(fetchGeneralDataRequest())
-      const res = await fetch(url, {
-        headers: { Accept: "application/json" },
-      })
-      const json = await res.json()
-      if (res.ok) {
-        if (json.status >= 300) {
-          dispatch(fetchGeneralDataFailure(json.title))
-        }
-        dispatch(fetchGeneralDataSuccess(json))
-      } else {
+export const fetchGeneralData = (url: string) => async (
+  dispatch: Function,
+  getState: Function,
+) => {
+  if (getState().general.data) {
+    return
+  }
+  try {
+    dispatch(fetchGeneralDataRequest())
+    const res = await fetch(url, {
+      headers: { Accept: "application/json" },
+    })
+    const json = await res.json()
+    if (res.ok) {
+      if (json.status >= 300) {
         dispatch(fetchGeneralDataFailure(json.title))
-        if (process.env.NODE_ENV !== "production") {
-          printError(res, json)
-        }
       }
-    } catch (error) {
+      dispatch(fetchGeneralDataSuccess(json))
+    } else {
+      dispatch(fetchGeneralDataFailure(json.title))
       if (process.env.NODE_ENV !== "production") {
-        console.error(`Network error: ${error.message}`)
+        printError(res, json)
       }
+    }
+  } catch (error) {
+    if (process.env.NODE_ENV !== "production") {
+      console.error(`Network error: ${error.message}`)
     }
   }
 }
