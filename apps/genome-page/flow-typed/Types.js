fb98247e94e7739316b6d874ba4031290a686068
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-21 15:08:41 -0500

    fix flow issues

--- apps/genome-page/flow-typed/Types.js
+++ apps/genome-page/flow-typed/Types.js
@@ -5,3 +5,12 @@ declare module "ProtVista/style/main.css" {
 declare module "font-awesome/css/font-awesome.min.css" {
   declare module.exports: any
 }
+
+class process {
+  static env: {
+    REACT_APP_API_SERVER: string,
+    REACT_APP_BASENAME: string,
+    REACT_APP_NAVBAR_JSON: string,
+    REACT_APP_FOOTER_JSON: string,
+  }
+}
