00a222f3093d6a0a19436ffb756e745f0e5155dd
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-18 11:00:01 -0500

    refactor: create custom loading and error components for summary and go tabs
    re #32

---
+++ apps/genome-page/src/features/Ontology/OntologyLoader.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
+import AppBar from "@material-ui/core/AppBar"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles"
+import PageHeader from "common/components/PageHeader"
+
+const skeletonTheme = createMuiTheme({
+  overrides: {
+    MuiTab: {
+      root: {
+        textTransform: "none",
+      },
+    },
+    MuiTabs: {
+      root: {
+        backgroundColor: "#DFE8F6",
+        color: "#000",
+      },
+    },
+  },
+})
+
+const OntologyLoader = () => (
+  <div>
+    <PageHeader />
+    <AppBar position="static">
+      <Tabs value="goa">
+        <Tab label="Gene Summary" />
+        <Tab label="Gene Ontology" />
+      </Tabs>
+    </AppBar>
+    <MuiThemeProvider theme={skeletonTheme}>
+      <AppBar position="static">
+        <Tabs value="goa">
+          <Tab label="All GO" />
+          <Tab label="Experimental GO" />
+          <Tab label="Manual GO" />
+          <Tab label="Electronic GO" />
+        </Tabs>
+      </AppBar>
+    </MuiThemeProvider>
+    <SkeletonTheme color="#d1d1d1">
+      <Skeleton count={5} />
+      <br />
+      <br />
+      <Skeleton count={5} />
+      <br />
+      <br />
+      <Skeleton count={5} />
+    </SkeletonTheme>
+  </div>
+)
+
+export default OntologyLoader
