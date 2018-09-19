c929592d8793ad3a1d7bf2af2d2d7238ea033d5a
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-19 10:44:22 -0500

    feat: add ErrorPage component, convert PageNotReady to material-ui
    re #21

--- apps/genome-page/src/common/components/PageNotReady.js
+++ apps/genome-page/src/common/components/PageNotReady.js
@@ -1,33 +1,71 @@
 // @flow
 import React from "react"
-import { Flex, Box } from "rebass"
+import { Link } from "react-router-dom"
+import { withStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Button from "@material-ui/core/Button"
 import FontAwesome from "react-fontawesome"
-import { SaveButton, Jumbotron, RouterLink } from "styles/style"
+import sadDicty from "images/sad-dicty.png"
+
+// eslint-disable-next-line
+const styles = theme => ({
+  container: {
+    backgroundColor: "#eff8fb",
+    textAlign: "center",
+    paddingTop: 30,
+    paddingBottom: 30,
+    marginBottom: 30,
+    borderRadius: 5,
+  },
+  button: {
+    width: "25%",
+    textTransform: "none",
+  },
+  mainGrid: {
+    marginTop: "40px",
+  },
+  paragraph: {
+    paddingLeft: "10px",
+    paddingRight: "10px",
+  },
+  routerLink: {
+    color: "#428bca",
+    textDecoration: "none",
+  },
+})
 
 /**
- * General 404 error page, currently designated as "Page Not Ready"/"Under Construction"
+ * Fallback component for non-existent routes -- "Page Not Ready"/"Under Construction"
  */
 
-const PageNotReady = () => (
-  <Flex justify="center" style={{ marginTop: "20px" }}>
-    <Box w="60%">
-      <Jumbotron>
-        <h1>
-          <FontAwesome name="wrench" /> Under Construction
-        </h1>
-        <p>This page is not ready yet.</p>
-        <Flex justify="center">
-          <Box w={["90%", "60%", "25%"]}>
-            <RouterLink to="/">
-              <SaveButton size="small" variant="contained" color="primary">
-                Dictybase Home
-              </SaveButton>
-            </RouterLink>
-          </Box>
-        </Flex>
-      </Jumbotron>
-    </Box>
-  </Flex>
-)
+const PageNotReady = props => {
+  const { classes } = props
+  return (
+    <Grid container className={classes.mainGrid} justify="center">
+      <Grid item xs={10} md={8}>
+        <div className={classes.container}>
+          <img src={sadDicty} alt="Sad Dicty Logo" />
+          <h1>
+            <FontAwesome name="wrench" /> Content Not Ready
+          </h1>
+          <p className={classes.paragraph}>This page is not ready yet.</p>
+          <p className={classes.paragraph}>
+            We are constantly adding content to our new website so check back
+            soon!
+          </p>
+          <Link className={classes.routerLink} to="/">
+            <Button
+              className={classes.button}
+              size="small"
+              variant="contained"
+              color="primary">
+              Back to homepage
+            </Button>
+          </Link>
+        </div>
+      </Grid>
+    </Grid>
+  )
+}
 
-export default PageNotReady
+export default withStyles(styles)(PageNotReady)
