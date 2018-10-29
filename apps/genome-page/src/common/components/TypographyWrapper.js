ec8986af0ad337d2e00efd73842c6f42318c94f1
wildlifehexagon <crossthebreeze@gmail.com>
2018-10-29 16:16:18 -0500

    refactor: rename TabContainer to TypographyWrapper

---
+++ apps/genome-page/src/common/components/TypographyWrapper.js
@@ -0,0 +1,9 @@
+// @flow
+import React from "react"
+import Typography from "@material-ui/core/Typography"
+
+const TypographyWrapper = ({ children }: any) => (
+  <Typography component="div">{children}</Typography>
+)
+
+export default TypographyWrapper
