79aa702a4781d8496d51f1e9ddd7745a354a57cb
wildlifehexagon <crossthebreeze@gmail.com>
2018-10-30 17:19:21 -0500

    refactor: rename to LeftDisplay and RightDisplay

---
+++ apps/genome-page/src/common/components/panels/LeftDisplay.md
@@ -0,0 +1,3 @@
+```js
+<LeftDisplay title="Molecular Function" />
+```
--- apps/genome-page/src/common/components/panels/LeftDisplay.test.js
+++ apps/genome-page/src/common/components/panels/LeftDisplay.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { shallow } from "enzyme"
-import PanelListItemLeft from "./PanelListItemLeft"
+import LeftDisplay from "./LeftDisplay"
 import TableCell from "@material-ui/core/TableCell"
 
 // two options for tests for components that use Material-UI withStyles:
@@ -8,11 +8,11 @@ import TableCell from "@material-ui/core/TableCell"
 // 2) use mount instead of shallow
 // https://github.com/mui-org/material-ui/issues/9266
 
-describe("PanelListItemLeft", () => {
+describe("LeftDisplay", () => {
   const props = {
     title: "Molecular Function",
   }
-  const wrapper = shallow(<PanelListItemLeft {...props} />)
+  const wrapper = shallow(<LeftDisplay {...props} />)
   const cell = wrapper.dive().find(TableCell)
 
   it("always renders one TableCell component", () => {
