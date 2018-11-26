48da8862282b3703f9a8c00d515dfc7023ea95a1
wildlifehexagon <crossthebreeze@gmail.com>
2018-11-26 11:52:36 -0600

    refactor: create AppErrorFallback component for main app error display
    #62

---
+++ apps/genome-page/src/app/layout/AppErrorFallback.js
@@ -0,0 +1,63 @@
+import React from "react"
+import { connect } from "react-redux"
+import { withRouter } from "react-router-dom"
+import { withStyles } from "@material-ui/core/styles"
+import { Header, Footer } from "dicty-components-header-footer"
+import { Navbar } from "dicty-components-navbar"
+
+import ErrorBoundary from "common/components/ErrorBoundary"
+import footerItems from "common/constants/Footer"
+import navbarItems from "common/constants/Navbar"
+import {
+  headerItems,
+  loggedHeaderItems,
+  generateLinks,
+} from "common/utils/headerItems"
+import Routes from "app/routes/Routes"
+import { appStyles as styles, navTheme } from "./appStyles"
+
+type Props = {
+  /** Object representing auth part of state */
+  auth: Object,
+  /** Action that fetches both navbar and footer content */
+  fetchNavbarAndFooter: Function,
+}
+
+/**
+ * If there's an error fetching navbar and/or footer data,
+ * this component is rendered with the static link data.
+ */
+
+const AppErrorFallback = (props: Props) => {
+  const { auth, classes } = props
+
+  return (
+    <div className={classes.body}>
+      {auth.isAuthenticated ? (
+        <Header items={loggedHeaderItems}>
+          {items => items.map(generateLinks)}
+        </Header>
+      ) : (
+        <Header items={headerItems}>{items => items.map(generateLinks)}</Header>
+      )}
+      <Navbar theme={navTheme} items={navbarItems} />
+
+      <main className={classes.main}>
+        <ErrorBoundary>
+          <Routes />
+        </ErrorBoundary>
+      </main>
+
+      <Footer items={footerItems} />
+    </div>
+  )
+}
+
+const mapStateToProps = ({ auth }) => ({ auth })
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    null,
+  )(withStyles(styles)(AppErrorFallback)),
+)
