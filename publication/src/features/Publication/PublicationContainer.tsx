604ad89011d8236feeedbc23cc6938e228b02c88
wildlifehexagon <crossthebreeze@gmail.com>
2019-03-29 16:52:55 -0500

    refactor: change width and layout of main container

--- publication/src/features/Publication/PublicationContainer.tsx
+++ publication/src/features/Publication/PublicationContainer.tsx
@@ -2,21 +2,30 @@ import React from "react"
 import Grid from "@material-ui/core/Grid"
 import { withStyles } from "@material-ui/core/styles"
 import createStyles from "@material-ui/core/styles/createStyles"
+import { Theme } from "@material-ui/core/styles/createMuiTheme"
 import LeftSidebar from "./LeftSidebar"
 import PublicationDisplay from "./PublicationDisplay"
 
-const styles = createStyles({
-  root: {
-    padding: "20px",
-  },
-  title: {
-    textAlign: "center",
-  },
-})
+const styles = (theme: Theme) =>
+  createStyles({
+    layout: {
+      width: "auto",
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+        width: 900,
+        marginLeft: "auto",
+        marginRight: "auto",
+      },
+    },
+    title: {
+      textAlign: "center",
+    },
+  })
 
 interface Props {
   classes: {
-    root: string
+    layout: string
     title: string
   }
 }
@@ -24,7 +33,7 @@ interface Props {
 export const PublicationContainer = (props: Props) => {
   const { classes } = props
   return (
-    <Grid container spacing={16} className={classes.root}>
+    <Grid container spacing={16} className={classes.layout}>
       <Grid item xs={12}>
         <h1 className={classes.title}>dictyBase Literature</h1>
       </Grid>
