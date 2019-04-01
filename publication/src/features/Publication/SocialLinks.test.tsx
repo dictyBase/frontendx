46e0e0e27ec97550dc2483469a78dc02e07cd2bd
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-01 14:41:21 -0500

    test: add tests for publication field components

---
+++ publication/src/features/Publication/SocialLinks.test.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { shallow } from "enzyme"
+import "../../setupTests"
+import SocialLinks from "./SocialLinks"
+import FontAwesome from "react-fontawesome"
+
+describe("Publication/SocialLinks", () => {
+  const props = {
+    title: "this is a test title",
+  }
+  const wrapper = shallow(<SocialLinks {...props} />)
+  describe("initial render", () => {
+    it("renders without crashing", () => {
+      wrapper
+    })
+    it("always renders one <div> element", () => {
+      expect(wrapper.dive().find("div")).toHaveLength(1)
+    })
+    it("always renders two <FontAwesome> elements", () => {
+      expect(wrapper.dive().find(FontAwesome)).toHaveLength(2)
+    })
+    it("matches title prop value", () => {
+      expect(wrapper.prop("title")).toEqual("this is a test title")
+    })
+  })
+})
---
+++ publication/src/features/Publication/Title.test.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { shallow } from "enzyme"
+import "../../setupTests"
+import Title from "./Title"
+
+describe("Publication/Title", () => {
+  const props = {
+    title: "this is a test title",
+  }
+  const wrapper = shallow(<Title {...props} />)
+  describe("initial render", () => {
+    it("renders without crashing", () => {
+      wrapper
+    })
+    it("always renders one <div> element", () => {
+      expect(wrapper.dive().find("div")).toHaveLength(1)
+    })
+    it("always renders one <h2> element", () => {
+      expect(wrapper.dive().find("h2")).toHaveLength(1)
+    })
+    it("matches title prop value", () => {
+      expect(wrapper.prop("title")).toEqual("this is a test title")
+    })
+  })
+})
