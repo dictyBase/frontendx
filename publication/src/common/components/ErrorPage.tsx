b58e474ac3cc0449a850c65c70c4b637266ae024
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-02 17:33:10 -0500

    refactor: remove unnecessary styles from ErrorPage

--- publication/src/common/components/ErrorPage.tsx
+++ publication/src/common/components/ErrorPage.tsx
@@ -10,22 +10,13 @@ import sadDicty from "../assets/sad-dicty.png"
 
 const styles = (theme: Theme) =>
   createStyles({
-    error400: {
+    error: {
       backgroundColor: "#eff8fb",
       textAlign: "center",
       paddingTop: 30,
       paddingBottom: 30,
       marginBottom: 30,
       borderRadius: 5,
-    },
-    error500: {
-      backgroundColor: "#a63232",
-      textAlign: "center",
-      paddingTop: 40,
-      paddingBottom: 40,
-      marginBottom: 30,
-      borderRadius: 5,
-      color: "#e3e3e3",
     },
     backButton: {
       width: "25%",
@@ -40,20 +31,10 @@ const styles = (theme: Theme) =>
     mainGrid: {
       marginTop: "40px",
     },
-    paragraph: {
-      padding: "10px",
-    },
+
     link: {
       color: "#428bca",
       textDecoration: "none",
-    },
-    link500: {
-      color: "#e0e0e0",
-      textDecoration: "none",
-    },
-    list: {
-      margin: "0 auto",
-      display: "table",
     },
   })
 
@@ -61,13 +42,9 @@ interface Props {
   /** Material-UI styling */
   classes: {
     mainGrid: string
-    link500: string
-    error500: string
+    error: string
     backButton: string
-    list: string
-    error400: string
     link: string
-    paragraph: string
   }
   error: object
 }
@@ -84,7 +61,7 @@ export const ErrorPage = (props: Props) => {
   return (
     <Grid container className={classes.mainGrid} justify="center">
       <Grid item xs={10} md={8}>
-        <div className={classes.error400}>
+        <div className={classes.error}>
           <img src={sadDicty} alt="Sad Dicty -- HTTP Error" />
           <h1>
             <FontAwesome name="exclamation-circle" /> Error
