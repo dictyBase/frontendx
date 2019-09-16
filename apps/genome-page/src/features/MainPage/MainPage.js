1a84beca30504c8f70d9de41b99592ce7d076582
wildlifehexagon <crossthebreeze@gmail.com>
2019-09-16 14:18:57 -0500

    feat: add react-helmet to provide useful page titles, fixes #65

--- apps/genome-page/src/features/MainPage/MainPage.js
+++ apps/genome-page/src/features/MainPage/MainPage.js
@@ -1,92 +1,41 @@
 // @flow
 import React from "react"
 import { Link } from "react-router-dom"
-
-const links = [
-  {
-    type: "regular",
-    id: "DDB_G0272608",
-  },
-  {
-    type: "regular",
-    id: "DDB_G0288511",
-  },
-  {
-    type: "regular",
-    id: "DDB_G0269114",
-  },
-  {
-    type: "regular",
-    id: "DDB_G0286355",
-  },
-  {
-    type: "regular",
-    id: "DDB_G0277399",
-  },
-  {
-    type: "complex",
-    id: "DDB_G0280531",
-  },
-  {
-    type: "complex",
-    id: "DDB_G0276267",
-  },
-  {
-    type: "complex",
-    id: "DDB_G0281211",
-  },
-  {
-    type: "complex",
-    id: "DDB_G0277589",
-  },
-  {
-    type: "name",
-    id: "sadA",
-  },
-  {
-    type: "name",
-    id: "piaA",
-  },
-  {
-    type: "name",
-    id: "svkA",
-  },
-  {
-    type: "name",
-    id: "ripA",
-  },
-  {
-    type: "name",
-    id: "far1",
-  },
-]
+import { Helmet } from "react-helmet"
+import links from "./linksData"
 
 /**
- * This provides a list of example gene pages to view when you hit the index route.
+ * MainPage provides a list of example gene pages to view when you hit the index route.
  */
 
 const MainPage = () => (
-  <center>
-    <h1>Genomes</h1>
-    <div>Example genome pages:</div>
-    {links.filter(n => n.type === "regular").map((item, index) => (
-      <p key={index}>
-        <Link to={`/${item.id}`}>{item.id}</Link>
-      </p>
-    ))}
-    <div>Complex examples:</div>
-    {links.filter(n => n.type === "complex").map((item, index) => (
-      <p key={index}>
-        <Link to={`/${item.id}`}>{item.id}</Link>
-      </p>
-    ))}
-    <div>Gene name examples:</div>
-    {links.filter(n => n.type === "name").map((item, index) => (
-      <p key={index}>
-        <Link to={`/${item.id}`}>{item.id}</Link>
-      </p>
-    ))}
-  </center>
+  <div>
+    <Helmet>
+      <title>dictyBase Genomes</title>
+      <meta name="description" content="dictyBase gene page examples" />
+    </Helmet>
+    <center>
+      <h1>Genomes</h1>
+      <div>Example genome pages:</div>
+      {links.filter(n => n.type === "regular").map((item, index) => (
+        <p key={index}>
+          <Link to={`/${item.id}`}>{item.id}</Link>
+        </p>
+      ))}
+      <div>Complex examples:</div>
+      {links.filter(n => n.type === "complex").map((item, index) => (
+        <p key={index}>
+          <Link to={`/${item.id}`}>{item.id}</Link>
+        </p>
+      ))}
+      <div>Gene name examples:</div>
+      {links.filter(n => n.type === "name").map((item, index) => (
+        <p key={index}>
+          <Link to={`/${item.id}`}>{item.id}</Link>
+        </p>
+      ))}
+    </center>
+  </div>
 )
 
 export default MainPage
