88eb60dc24b90251aee2549136da653a5f3fb28b
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-17 11:53:04 -0500

    fix: fix type issue related to error notifications

--- publication/src/features/Authentication/ErrorNotification.tsx
+++ publication/src/features/Authentication/ErrorNotification.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { withStyles } from "@material-ui/core/styles"
+import { makeStyles } from "@material-ui/core/styles"
 import SnackbarContent from "@material-ui/core/SnackbarContent"
 
-const styles = () => ({
+const useStyles = makeStyles({
   message: {
     backgroundColor: "#cc0000",
   },
@@ -10,25 +10,24 @@ const styles = () => ({
 
 interface Props {
   /** The error message to display */
-  error: object
-  /** Material-UI styling */
-  classes: {
-    message: string
+  error: {
+    status: number
+    title: string
   }
 }
 
 /** Notification snackbar-style message if user hits some type of error */
 
-export const ErrorNotification = (props: Props) => {
-  const { classes, error } = props
+export const ErrorNotification = ({ error }: Props) => {
+  const classes = useStyles()
 
   return (
     <div style={{ textAlign: "center" }}>
-      <SnackbarContent className={classes.message} message={error} />
+      <SnackbarContent className={classes.message} message={error.title} />
       <br />
       <br />
     </div>
   )
 }
 
-export default withStyles(styles)(ErrorNotification)
+export default ErrorNotification
