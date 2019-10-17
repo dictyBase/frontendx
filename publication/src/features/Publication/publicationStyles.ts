71ce0abf6321c6d7440709ca611c18316e363874
wildlifehexagon <crossthebreeze@gmail.com>
2019-10-17 14:05:38 -0500

    refactor: switch from withStyles to useStyles hook

--- publication/src/features/Publication/publicationStyles.ts
+++ publication/src/features/Publication/publicationStyles.ts
@@ -1,31 +1,30 @@
-import createStyles from "@material-ui/core/styles/createStyles"
+import { makeStyles } from "@material-ui/core/styles"
 import { Theme } from "@material-ui/core/styles/createMuiTheme"
 
-const styles = (theme: Theme) =>
-  createStyles({
-    layout: {
-      width: "75%",
+const useStyles = makeStyles((theme: Theme) => ({
+  layout: {
+    width: "75%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    [theme.breakpoints.up(1300 + theme.spacing(3) * 2)]: {
+      width: 1300,
       marginLeft: "auto",
       marginRight: "auto",
-      [theme.breakpoints.up(1300 + theme.spacing(3) * 2)]: {
-        width: 1300,
-        marginLeft: "auto",
-        marginRight: "auto",
-      },
-      [theme.breakpoints.down("sm")]: {
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto",
-      },
     },
-    title: {
+    [theme.breakpoints.down("sm")]: {
+      width: "90%",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  title: {
+    textAlign: "center",
+  },
+  sidebar: {
+    [theme.breakpoints.down("sm")]: {
       textAlign: "center",
     },
-    sidebar: {
-      [theme.breakpoints.down("sm")]: {
-        textAlign: "center",
-      },
-    },
-  })
+  },
+}))
 
-export default styles
+export default useStyles
