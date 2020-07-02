562fba1ad28134e6b671c0832b09986d1010d9a8
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-02 16:41:00 -0500

    refactor: remove old redux auth content

--- publication/src/app/reducers/rootReducer.ts
+++ publication/src/app/reducers/rootReducer.ts
@@ -1,13 +1,11 @@
 import { combineReducers } from "redux"
 import { History } from "history"
 import { connectRouter } from "connected-react-router"
-import authReducer from "../../features/Authentication/authReducer"
 import footerReducer from "./footerReducer"
 import navbarReducer from "./navbarReducer"
 
 const rootReducer = (history: History) =>
   combineReducers({
-    auth: authReducer,
     footer: footerReducer,
     navbar: navbarReducer,
     router: connectRouter(history),
--- publication/src/features/Authentication/Login.md
+++
@@ -1,9 +0,0 @@
-```js
-const Provider = require("react-redux").Provider
-const configureStore = require("../../app/store/configureStore").default
-const reducers = require("../../app/reducers/rootReducer").default
-
-;<Provider store={configureStore({})}>
-  <Login location={{}} />
-</Provider>
-```
