b93ad9ba22b838bb23c97dc48c85970a5c56aafe
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-24 15:21:20 -0500

    refactor: return graphql data on summary page

--- apps/genome-page/src/features/Summary/Panels/GoaPanelContent.js
+++ apps/genome-page/src/features/Summary/Panels/GoaPanelContent.js
@@ -1,73 +1,33 @@
-// @flow
 import React, { Fragment } from "react"
+import { makeStyles } from "@material-ui/core/styles"
 import withLinkGenerator from "features/Ontology/utils/withLinkGenerator"
-import { withStyles } from "@material-ui/core/styles"
+import withDataFilter from "../utils/withDataFilter"
 
-// function to filter the With data
-const withDataFilter = (arr) => {
-  // filter With array by db type
-  const dictyChecker = arr.filter((item) => item.db === "dictyBase").slice(0, 2)
-  const uniprotChecker = arr
-    .filter((item) => item.db === "UniProtKB")
-    .slice(0, 2)
-  const mgiChecker = arr.filter((item) => item.db === "MGI").slice(0, 2)
-  const rgdChecker = arr.filter((item) => item.db === "RGD").slice(0, 2)
-  const sgdChecker = arr.filter((item) => item.db === "SGD").slice(0, 2)
-  const pomChecker = arr.filter((item) => item.db === "PomBase").slice(0, 2)
-
-  // order of preference to display With data
-  // dicty => UniProt => MGI => RGD => SGD => PomBase
-  if (!Array.isArray(dictyChecker) || !dictyChecker.length) {
-    if (!Array.isArray(uniprotChecker) || !uniprotChecker.length) {
-      if (!Array.isArray(mgiChecker) || !mgiChecker.length) {
-        if (!Array.isArray(rgdChecker) || !rgdChecker.length) {
-          if (!Array.isArray(sgdChecker) || !sgdChecker.length) {
-            return pomChecker
-          }
-          return sgdChecker
-        }
-        return rgdChecker
-      }
-      return mgiChecker
-    }
-    return uniprotChecker
-  }
-
-  return dictyChecker
-}
-
-// Material-UI stylings
-const styles = (theme) => ({
-  link: {
-    textDecoration: "none",
-    color: "#4C5E81",
-    "&:visited": {
+const useStyles = (theme) =>
+  makeStyles({
+    link: {
+      textDecoration: "none",
       color: "#4C5E81",
+      "&:visited": {
+        color: "#4C5E81",
+      },
+      "&:hover": {
+        textDecoration: "underline",
+      },
     },
-    "&:hover": {
-      textDecoration: "underline",
-    },
-  },
-})
-
-type Props = {
-  /** Material-UI styling */
-  classes: Object,
-  /** The mapped item to display on a line in GoaPanel */
-  item: Object,
-}
+  })
 
 /**
  * The content that goes in the right side of the GOA panel on the summary page.
  */
 
-const GoaPanelContent = (props: Props) => {
-  const { item, classes } = props
+const GoaPanelContent = ({ item }) => {
+  const classes = useStyles()
 
   return (
     <Fragment>
       <span>
-        {item.goterm}
+        {item.go_term}
         {item.with !== null &&
           withDataFilter(item.with).map((xref: Object, i: string) => (
             <Fragment key={i}>
@@ -127,4 +87,4 @@ const GoaPanelContent = (props: Props) => {
   )
 }
 
-export default withStyles(styles)(GoaPanelContent)
+export default GoaPanelContent
---
+++ apps/genome-page/src/features/Summary/utils/withDataFilter.js
@@ -0,0 +1,34 @@
+// function to filter the With data
+const withDataFilter = (arr) => {
+  // filter With array by db type
+  const dictyChecker = arr.filter((item) => item.db === "dictyBase").slice(0, 2)
+  const uniprotChecker = arr
+    .filter((item) => item.db === "UniProtKB")
+    .slice(0, 2)
+  const mgiChecker = arr.filter((item) => item.db === "MGI").slice(0, 2)
+  const rgdChecker = arr.filter((item) => item.db === "RGD").slice(0, 2)
+  const sgdChecker = arr.filter((item) => item.db === "SGD").slice(0, 2)
+  const pomChecker = arr.filter((item) => item.db === "PomBase").slice(0, 2)
+
+  // order of preference to display With data
+  // dicty => UniProt => MGI => RGD => SGD => PomBase
+  if (!Array.isArray(dictyChecker) || !dictyChecker.length) {
+    if (!Array.isArray(uniprotChecker) || !uniprotChecker.length) {
+      if (!Array.isArray(mgiChecker) || !mgiChecker.length) {
+        if (!Array.isArray(rgdChecker) || !rgdChecker.length) {
+          if (!Array.isArray(sgdChecker) || !sgdChecker.length) {
+            return pomChecker
+          }
+          return sgdChecker
+        }
+        return rgdChecker
+      }
+      return mgiChecker
+    }
+    return uniprotChecker
+  }
+
+  return dictyChecker
+}
+
+export default withDataFilter
