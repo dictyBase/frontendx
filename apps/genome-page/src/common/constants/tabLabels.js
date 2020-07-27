3be92bdd985864dc1cd4b13b48f1223582391663
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-27 16:55:58 -0500

    refactor: convert gql and constants to typescript

--- apps/genome-page/src/common/constants/tabLabels.js
+++
@@ -1,8 +0,0 @@
-export const tabLabels = {
-  protein: "Protein Information",
-  goa: "Gene Ontology",
-  orthologs: "Orthologs",
-  phenotypes: "Phenotypes",
-  publications: "Publications",
-  blast: "BLAST",
-}
