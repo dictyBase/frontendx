4233b3f9b8e6ed1dbbde849f114e58f374a7d303
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-27 17:21:52 -0500

    refactor: replace navbar and footer redux code

--- apps/genome-page/src/app/reducers/rootReducer.js
+++ apps/genome-page/src/app/reducers/rootReducer.js
@@ -1,11 +1,7 @@
 import { combineReducers } from "redux"
 import authReducer from "features/Authentication/authReducer"
-import footerReducer from "app/reducers/footerReducer"
-import navbarReducer from "app/reducers/navbarReducer"
 
 export default (history) =>
   combineReducers({
     auth: authReducer,
-    footer: footerReducer,
-    navbar: navbarReducer,
   })
