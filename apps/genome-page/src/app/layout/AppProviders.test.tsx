782f8f436c46a037e7d082bd3fff7295dcaf5b3b
wildlifehexagon <crossthebreeze@gmail.com>
2020-07-28 14:39:55 -0500

    test: fix broken test

--- apps/genome-page/src/app/layout/AppProviders.test.tsx
+++ apps/genome-page/src/app/layout/AppProviders.test.tsx
@@ -2,7 +2,7 @@ import { isMutation } from "./AppProviders"
 
 describe("isMutation function", () => {
   it("should return true for mutation", () => {
-    expect(isMutation("CreateContent")).toBeTruthy()
+    expect(isMutation("Logout")).toBeTruthy()
   })
   it("should return false for query", () => {
     expect(isMutation("GetRefreshToken")).toBeFalsy()
