7d43a2e8ed0be6c8e3502bbbdea8c5fb64c75316
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-04 16:32:25 -0500

    refactor: use one query to retrieve gene data

--- apps/genome-page/src/features/Ontology/OntologyContainer.tsx
+++ apps/genome-page/src/features/Ontology/OntologyContainer.tsx
@@ -1,51 +1,36 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { useParams, useRouteMatch } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { useQuery } from "@apollo/client"
 import Typography from "@material-ui/core/Typography"
 import OntologyTabLayout from "./OntologyTabLayout"
 import OntologyLoader from "./OntologyLoader"
 import Layout from "app/layout/Layout"
-import { GET_GENE_BY_ID, GET_GENE_BY_NAME } from "common/graphql/query"
+import { GET_GENE } from "common/graphql/query"
 
 const OntologyContainer = () => {
-  // detect if route contains a gene ID then update graphql query accordingly
-  const match = useRouteMatch("/:id([A-Z]{3}_G[0-9]{4,})")
-  const { id } = useParams()
-
-  let query = GET_GENE_BY_ID
-  let variables = {
-    id,
-  } as any
-
-  if (match === null) {
-    query = GET_GENE_BY_NAME
-    variables = {
-      name: id,
-    }
-  }
-
-  const { loading, error, data } = useQuery(query, {
-    variables,
+  const { gene } = useParams()
+  const { loading, error, data } = useQuery(GET_GENE, {
+    variables: {
+      gene,
+    },
   })
 
   if (loading) return <OntologyLoader />
 
   if (error) return <div>got an error...</div>
 
-  let geneData = data.geneByID ? data.geneByID : data.geneByName
-
   return (
     <Layout>
       <Helmet>
-        <title>GO Annotations for {geneData.name} - dictyBase</title>
+        <title>GO Annotations for {data.gene.name} - dictyBase</title>
         <meta
           name="description"
-          content={`Gene Ontology Annotations for ${geneData.name} at dictyBase`}
+          content={`Gene Ontology Annotations for ${data.gene.name} at dictyBase`}
         />
       </Helmet>
       <Typography component="div">
-        <OntologyTabLayout data={geneData.goas} />
+        <OntologyTabLayout data={data.gene.goas} />
       </Typography>
     </Layout>
   )
--- apps/genome-page/src/features/Summary/SummaryContainer.tsx
+++ apps/genome-page/src/features/Summary/SummaryContainer.tsx
@@ -1,55 +1,40 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import { useParams, useRouteMatch } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { useQuery } from "@apollo/client"
 import Typography from "@material-ui/core/Typography"
 import PanelWrapper from "common/components/panels/PanelWrapper"
 import SummaryLoader from "./SummaryLoader"
 import Layout from "app/layout/Layout"
 import GoaPanel from "features/Summary/Panels/GoaPanel"
-import { GET_GENE_BY_ID, GET_GENE_BY_NAME } from "common/graphql/query"
+import { GET_GENE } from "common/graphql/query"
 
 const SummaryContainer = () => {
-  // detect if route contains a gene ID then update graphql query accordingly
-  const match = useRouteMatch("/:id([A-Z]{3}_G[0-9]{4,})")
-  const { id } = useParams()
-
-  let query = GET_GENE_BY_ID
-  let variables = {
-    id,
-  } as any
-
-  if (match === null) {
-    query = GET_GENE_BY_NAME
-    variables = {
-      name: id,
-    }
-  }
-
-  const { loading, error, data } = useQuery(query, {
-    variables,
+  const { gene } = useParams()
+  const { loading, error, data } = useQuery(GET_GENE, {
+    variables: {
+      gene,
+    },
   })
 
-  if (loading) return <SummaryLoader id={id} />
+  if (loading) return <SummaryLoader gene={gene} />
 
   if (error) return <div>got an error...</div>
-
-  let geneData = data.geneByID ? data.geneByID : data.geneByName
 
   return (
     <Layout>
       <Helmet>
-        <title>Gene Summary for {geneData.name} - dictyBase</title>
+        <title>Gene Summary for {data.gene.name} - dictyBase</title>
         <meta
           name="description"
-          content={`Gene information for ${geneData.name} at dictyBase`}
+          content={`Gene information for ${data.gene.name} at dictyBase`}
         />
       </Helmet>
       <Typography component="div">
         <PanelWrapper
           title="Latest Gene Ontology Annotations"
-          route={`/${id}/goannotations`}>
-          <GoaPanel data={geneData.goas} />
+          route={`/${gene}/goannotations`}>
+          <GoaPanel data={data.gene.goas} />
         </PanelWrapper>
       </Typography>
     </Layout>
