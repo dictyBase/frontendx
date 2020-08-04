dab5253f55a96b9b3f5b4a6bfad3598a7b80ff45
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-04 13:42:14 -0500

    test: add tests for other common components

---
+++ apps/genome-page/src/common/components/panels/PanelWrapper.test.tsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import PanelWrapper from "./PanelWrapper"
+import { BrowserRouter } from "react-router-dom"
+
+describe("common/components/panels/PanelWrapper", () => {
+  const Child = () => <React.Fragment>child component</React.Fragment>
+  const renderComponent = () => {
+    render(
+      <PanelWrapper title="GO Annotations">
+        <Child />
+      </PanelWrapper>,
+    )
+  }
+
+  it("should render children", () => {
+    renderComponent()
+    expect(screen.getByText(/child component/)).toBeInTheDocument()
+  })
+
+  it("should display title", () => {
+    renderComponent()
+    expect(screen.getByText(/GO Annotations/)).toBeInTheDocument()
+  })
+
+  it("should display link when passed a route", () => {
+    const route = "/goannotations"
+    render(
+      <BrowserRouter>
+        <PanelWrapper title="GO Annotations" route={route}>
+          <Child />
+        </PanelWrapper>
+      </BrowserRouter>,
+    )
+
+    expect(screen.getByText(/View All/)).toBeInTheDocument()
+    expect(screen.getByRole("link")).toHaveAttribute("href", route)
+  })
+
+  it("should not display link when not passed a route", () => {
+    renderComponent()
+    expect(screen.queryByText(/View All/)).toBeNull()
+    expect(screen.queryByRole("link")).toBeNull()
+  })
+})
