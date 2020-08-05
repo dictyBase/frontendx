888beb69e37ca712b25edded72739065e8d4033e
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-05 14:44:14 -0500

    fix: add table sorting back to go tab, fixes #303

--- apps/genome-page/src/features/Ontology/Table/utils/getSorting.ts
+++ apps/genome-page/src/features/Ontology/Table/utils/getSorting.ts
@@ -3,10 +3,7 @@ type Order = "asc" | "desc"
 function getSorting<Key extends keyof any>(
   order: Order,
   orderBy: Key,
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string },
-) => number {
+): (a: { [key in Key]: any }, b: { [key in Key]: any }) => number {
   return order === "desc"
     ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
     : (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1)
