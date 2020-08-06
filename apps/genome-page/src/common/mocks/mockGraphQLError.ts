e7cf651938839b4d841828b367fefd7ec8ce15b5
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-06 14:16:04 -0500

    test: add more tests for error displays

--- apps/genome-page/src/common/mocks/mockGraphQLError.ts
+++ apps/genome-page/src/common/mocks/mockGraphQLError.ts
@@ -14,4 +14,36 @@ const mockNotFoundError = {
   ],
 }
 
-export { mockNotFoundError }
+const mockUnavailableError = {
+  errors: [
+    {
+      message: "server is unavailable",
+      path: ["gene"],
+      extensions: { code: "Unavailable" },
+      locations: undefined,
+      nodes: undefined,
+      source: undefined,
+      positions: undefined,
+      originalError: undefined,
+      name: "",
+    },
+  ],
+}
+
+const mockOtherError = {
+  errors: [
+    {
+      message: "unknown test error",
+      path: ["gene"],
+      extensions: { code: "Unknown" },
+      locations: undefined,
+      nodes: undefined,
+      source: undefined,
+      positions: undefined,
+      originalError: undefined,
+      name: "",
+    },
+  ],
+}
+
+export { mockNotFoundError, mockOtherError, mockUnavailableError }
