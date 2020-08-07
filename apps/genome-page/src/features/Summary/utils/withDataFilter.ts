cbb5a02a1dc6c202c45ea290bb5e675902b7b254
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-07 11:09:20 -0500

    refactor: remove nested conditionals #302

--- apps/genome-page/src/features/Summary/utils/withDataFilter.ts
+++ apps/genome-page/src/features/Summary/utils/withDataFilter.ts
@@ -4,34 +4,23 @@ const emptyArray = (arr: Array<With>) => !Array.isArray(arr) || !arr.length
 
 // function to filter the With data
 const withDataFilter = (arr: Array<With>) => {
-  // filter With array by db type
-  const dictyChecker = arr.filter((item) => item.db === "dictyBase").slice(0, 2)
-  const uniprotChecker = arr
-    .filter((item) => item.db === "UniProtKB")
-    .slice(0, 2)
-  const mgiChecker = arr.filter((item) => item.db === "MGI").slice(0, 2)
-  const rgdChecker = arr.filter((item) => item.db === "RGD").slice(0, 2)
-  const sgdChecker = arr.filter((item) => item.db === "SGD").slice(0, 2)
-  const pomChecker = arr.filter((item) => item.db === "PomBase").slice(0, 2)
+  // filter by db type
+  const dictyBase = arr.filter((item) => item.db === "dictyBase").slice(0, 2)
+  const uniProtKB = arr.filter((item) => item.db === "UniProtKB").slice(0, 2)
+  const MGI = arr.filter((item) => item.db === "MGI").slice(0, 2)
+  const RGD = arr.filter((item) => item.db === "RGD").slice(0, 2)
+  const SGD = arr.filter((item) => item.db === "SGD").slice(0, 2)
+  const PomBase = arr.filter((item) => item.db === "PomBase").slice(0, 2)
 
   // order of preference to display With data
-  // dicty => UniProt => MGI => RGD => SGD => PomBase
-  if (emptyArray(dictyChecker)) {
-    if (emptyArray(uniprotChecker)) {
-      if (emptyArray(mgiChecker)) {
-        if (emptyArray(rgdChecker)) {
-          if (emptyArray(sgdChecker)) {
-            return pomChecker
-          }
-          return sgdChecker
-        }
-        return rgdChecker
-      }
-      return mgiChecker
-    }
-    return uniprotChecker
-  }
-  return dictyChecker
+  // dictyBase => UniProt => MGI => RGD => SGD => PomBase
+  if (!emptyArray(dictyBase)) return dictyBase
+  if (!emptyArray(uniProtKB)) return uniProtKB
+  if (!emptyArray(MGI)) return MGI
+  if (!emptyArray(RGD)) return RGD
+  if (!emptyArray(SGD)) return SGD
+
+  return PomBase
 }
 
 export { emptyArray }
