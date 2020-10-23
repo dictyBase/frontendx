edea2a37cea2a5b3226b1dc94245df2ddaba2fd7
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-23 16:21:30 -0500

    test: use correct mutation name in test

--- apps/genome-page/src/common/hooks/useApolloClient.test.tsx
+++ apps/genome-page/src/common/hooks/useApolloClient.test.tsx
@@ -2,7 +2,7 @@ import { isMutation, getGraphQLServer } from "./useApolloClient"
 
 describe("isMutation function", () => {
   it("should return true for mutation", () => {
-    expect(isMutation("CreateContent")).toBeTruthy()
+    expect(isMutation("Logout")).toBeTruthy()
   })
   it("should return false for query", () => {
     expect(isMutation("GetRefreshToken")).toBeFalsy()
