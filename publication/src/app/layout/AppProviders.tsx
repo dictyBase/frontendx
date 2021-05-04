45ca9a2175717f929090c9933f6984be3f0685a4
wildlifehexagon <hartline.eric@gmail.com>
2021-05-04 10:11:00 -0500

    refactor: add custom mui theme

--- publication/src/app/layout/AppProviders.tsx
+++ publication/src/app/layout/AppProviders.tsx
@@ -5,21 +5,48 @@ import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
 import useApolloClient from "common/hooks/useApolloClient"
 
 const muiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#004080",
+      light: "#476ab0",
+      dark: "#001b53",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#008080",
+      light: "#4cb0af",
+      dark: "#005354",
+    },
+    error: {
+      main: "#b2102f",
+      light: "#ea4f58",
+      dark: "#7b0008",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    h1: {
+      fontSize: "2.00em",
+    },
+    h2: {
+      fontSize: "1.50em",
+    },
+    h3: {
+      fontSize: "1.17em",
+    },
+    h4: {
+      fontSize: "1.00em",
+    },
+    h5: {
+      fontSize: "0.83em",
+    },
+    h6: {
+      fontSize: "0.67em",
+    },
+  },
   overrides: {
-    MuiTab: {
-      root: {
-        textTransform: "none",
-      },
-    },
-    MuiTabs: {
-      root: {
-        backgroundColor: "#cce6ff",
-        color: "#000",
-      },
-      indicator: {
-        backgroundColor: "#858780",
-      },
-    },
     MuiCssBaseline: {
       "@global": {
         a: {
