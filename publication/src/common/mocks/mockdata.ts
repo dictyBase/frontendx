11a0514b842a93211d122eb3fc7eb34f5879ddae
wildlifehexagon <hartline.eric@gmail.com>
2021-05-04 15:08:54 -0500

    test: add tests for publication query

--- publication/src/common/mocks/mockdata.ts
+++ publication/src/common/mocks/mockdata.ts
@@ -1,29 +1,26 @@
-export const data = {
-  data: {
-    publication: {
-      abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      doi: "9.0909/j.diff.1964.02.01",
-      journal:
-        "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
-      pages: "71-79",
-      pub_date: "1964-01-29",
-      id: "12345678",
-      title:
-        "This is a fake publication title that should be at least ten words",
-      authors: [
-        {
-          first_name: "George",
-          last_name: "Costanza",
-          full_name: "Costanza G",
-          initials: "GC",
-        },
-        {
-          first_name: "Cosmo",
-          last_name: "Kramer",
-          full_name: "Kramer C",
-          initials: "CK",
-        },
-      ],
-    },
+const data = {
+  publication: {
+    id: "12345678",
+    doi: "9.0909/j.diff.1964.02.01",
+    title: "This is a fake publication title that should be at least ten words",
+    abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    journal:
+      "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb",
+    pub_date: "1964-01-29",
+    pages: "71-79",
+    issue: "101",
+    volume: "999",
+    authors: [
+      {
+        last_name: "Costanza",
+        initials: "GC",
+      },
+      {
+        last_name: "Kramer",
+        initials: "CK",
+      },
+    ],
   },
 }
+
+export { data }
