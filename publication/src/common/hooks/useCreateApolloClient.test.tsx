6e75c3957d59a60f19e6adbee9ae68202f9f70ab
wildlifehexagon <hartline.eric@gmail.com>
2021-05-04 14:06:15 -0500

    test: use correct mutation

--- publication/src/common/hooks/useCreateApolloClient.test.tsx
+++ publication/src/common/hooks/useCreateApolloClient.test.tsx
@@ -2,7 +2,7 @@ import { isMutation, getGraphQLServer } from "./useCreateApolloClient"
 
 describe("isMutation function", () => {
   it("should return true for mutation", () => {
-    expect(isMutation("CreateContent")).toBeTruthy()
+    expect(isMutation("Logout")).toBeTruthy()
   })
   it("should return false for query", () => {
     expect(isMutation("GetRefreshToken")).toBeFalsy()
