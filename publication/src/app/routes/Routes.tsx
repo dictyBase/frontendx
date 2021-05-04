6c6ff21c99c3fcfddc6033fcabf89c59f87d2040
wildlifehexagon <hartline.eric@gmail.com>
2021-05-04 10:06:11 -0500

    refactor: add custom hook for google analytics

--- publication/src/app/routes/Routes.tsx
+++ publication/src/app/routes/Routes.tsx
@@ -5,25 +5,30 @@ import Login from "features/Authentication/Login"
 import OauthCallback from "features/Authentication/OauthCallback"
 import AuthLoader from "features/Authentication/AuthLoader"
 import Logout from "features/Authentication/Logout"
+import useGoogleAnalytics from "common/hooks/useGoogleAnalytics"
 
 /**
  * Component for all of our React Router routes.
  */
 
-export const Routes = () => (
-  <Switch>
-    <Route path="/login" component={Login} />
-    <Route path="/:provider/callback" component={OauthCallback} />
-    <Route path="/load/auth" component={AuthLoader} />
-    <Route path="/logout" component={Logout} />
-    <Route exact path="/:id" component={PublicationContainer} />
-    <Redirect
-      from="/"
-      to={{
-        pathname: "/26088819",
-      }}
-    />
-  </Switch>
-)
+export const Routes = () => {
+  useGoogleAnalytics()
+
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/:provider/callback" component={OauthCallback} />
+      <Route path="/load/auth" component={AuthLoader} />
+      <Route path="/logout" component={Logout} />
+      <Route exact path="/:id" component={PublicationContainer} />
+      <Redirect
+        from="/"
+        to={{
+          pathname: "/26088819",
+        }}
+      />
+    </Switch>
+  )
+}
 
 export default Routes
