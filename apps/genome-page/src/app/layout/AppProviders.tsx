7bbecb84c39c872f3fd96ff6dfdf568eb6d63bce
wildlifehexagon <hartline.eric@gmail.com>
2021-05-05 09:52:30 -0500

    feat: add apollo cache persistence

--- apps/genome-page/src/app/layout/AppProviders.tsx
+++ apps/genome-page/src/app/layout/AppProviders.tsx
@@ -2,9 +2,51 @@ import React from "react"
 import { ApolloProvider } from "@apollo/client"
 import { BrowserRouter } from "react-router-dom"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import useApolloClient from "common/hooks/useApolloClient"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import useCreateApolloClient from "common/hooks/useCreateApolloClient"
 
 const muiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#004080",
+      light: "#476ab0",
+      dark: "#001b53",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#008080",
+      light: "#4cb0af",
+      dark: "#005354",
+    },
+    error: {
+      main: "#b2102f",
+      light: "#ea4f58",
+      dark: "#7b0008",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    h1: {
+      fontSize: "2.00em",
+    },
+    h2: {
+      fontSize: "1.50em",
+    },
+    h3: {
+      fontSize: "1.17em",
+    },
+    h4: {
+      fontSize: "1.00em",
+    },
+    h5: {
+      fontSize: "0.83em",
+    },
+    h6: {
+      fontSize: "0.67em",
+    },
+  },
   overrides: {
     MuiTab: {
       root: {
@@ -35,10 +77,14 @@ const muiTheme = createMuiTheme({
 })
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
-  const apolloClient = useApolloClient()
+  const { client, cacheInitializing } = useCreateApolloClient()
+
+  if (cacheInitializing) {
+    return <CircularProgress />
+  }
 
   return (
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <MuiThemeProvider theme={muiTheme}>
         <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
           {children}
