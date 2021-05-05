6325c0e1883cc4faabd4103a6039f6746bb0e71d
wildlifehexagon <hartline.eric@gmail.com>
2021-05-05 09:19:11 -0500

    refactor: use custom hook for google analytics

--- apps/genome-page/src/app/routes/Routes.tsx
+++ apps/genome-page/src/app/routes/Routes.tsx
@@ -7,6 +7,7 @@ import OauthCallback from "features/Authentication/OauthCallback"
 import AuthLoader from "features/Authentication/AuthLoader"
 import Logout from "features/Authentication/Logout"
 import PageNotReady from "common/components/PageNotReady"
+import useGoogleAnalytics from "common/hooks/useGoogleAnalytics"
 
 /**
  * List of routes used with React Router.
@@ -14,23 +15,27 @@ import PageNotReady from "common/components/PageNotReady"
 
 // TODO: Consider useRouteMatch instead of identifier
 
-const Routes = () => (
-  <Switch>
-    <Route path="/login" component={Login} />
-    <Route path="/:provider/callback" component={OauthCallback} />
-    <Route path="/load/auth" component={AuthLoader} />
-    <Route path="/logout" component={Logout} />
-    <Route path="/:gene/goannotations" component={OntologyContainer} />
-    <Route path="/:gene/*" component={PageNotReady} />
-    <Route path="/:gene" component={SummaryContainer} />
-    <Redirect
-      from="/"
-      to={{
-        pathname: "/sadA",
-      }}
-    />
-    <Route path="*" component={PageNotReady} />
-  </Switch>
-)
+const Routes = () => {
+  useGoogleAnalytics()
+
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/:provider/callback" component={OauthCallback} />
+      <Route path="/load/auth" component={AuthLoader} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/:gene/goannotations" component={OntologyContainer} />
+      <Route path="/:gene/*" component={PageNotReady} />
+      <Route path="/:gene" component={SummaryContainer} />
+      <Redirect
+        from="/"
+        to={{
+          pathname: "/sadA",
+        }}
+      />
+      <Route path="*" component={PageNotReady} />
+    </Switch>
+  )
+}
 
 export default Routes
