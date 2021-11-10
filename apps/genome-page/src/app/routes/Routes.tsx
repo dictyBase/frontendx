3f51899dbe18a1691c3b72d4f078017721fc1a03
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-10 12:55:46 -0600

    fix: remove duplicate BrowserRouter

--- apps/genome-page/src/app/routes/Routes.tsx
+++ apps/genome-page/src/app/routes/Routes.tsx
@@ -1,10 +1,5 @@
 import React from "react"
-import {
-  Route,
-  Routes as ReactRoutes,
-  BrowserRouter,
-  Navigate,
-} from "react-router-dom"
+import { Route, Routes as ReactRoutes, Navigate } from "react-router-dom"
 import SummaryContainer from "features/Summary/SummaryContainer"
 import OntologyContainer from "features/Ontology/OntologyContainer"
 import Login from "features/Authentication/Login"
@@ -24,20 +19,18 @@ const Routes = () => {
   useGoogleAnalytics()
 
   return (
-    <BrowserRouter>
-      <ReactRoutes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/:provider/callback" element={<OauthCallback />} />
-        <Route path="/load/auth" element={<AuthLoader />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/:gene/goannotations" element={<OntologyContainer />} />
-        <Route path="/:gene/*" element={<PageNotReady />} />
-        <Route path="/:gene" element={<SummaryContainer />} />
-        {/* Since react-router v6 has removed Redirect we have to use Navigate instead. See https://gist.github.com/mjackson/b5748add2795ce7448a366ae8f8ae3bb#not-server-rendering */}
-        <Route path="/" element={<Navigate replace to="/sadA" />} />
-        <Route path="*" element={<PageNotReady />} />
-      </ReactRoutes>
-    </BrowserRouter>
+    <ReactRoutes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/:provider/callback" element={<OauthCallback />} />
+      <Route path="/load/auth" element={<AuthLoader />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="/:gene/goannotations" element={<OntologyContainer />} />
+      <Route path="/:gene/*" element={<PageNotReady />} />
+      <Route path="/:gene" element={<SummaryContainer />} />
+      {/* Since react-router v6 has removed Redirect we have to use Navigate instead. See https://gist.github.com/mjackson/b5748add2795ce7448a366ae8f8ae3bb#not-server-rendering */}
+      <Route path="/" element={<Navigate replace to="/sadA" />} />
+      <Route path="*" element={<PageNotReady />} />
+    </ReactRoutes>
   )
 }
 
