22cc40fb37ed6efdd0ef2471f756dd7ba4c86616
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-19 15:11:40 -0600

    refactor: reuse Layout component instead of redefining gene tabs

--- apps/genome-page/src/features/Summary/SummaryLoader.tsx
+++ apps/genome-page/src/features/Summary/SummaryLoader.tsx
@@ -1,9 +1,6 @@
 import React from "react"
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import AppBar from "@material-ui/core/AppBar"
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import PageHeader from "common/components/PageHeader"
+import Layout from "app/layout/Layout"
 
 type Props = {
   /** Gene name */
@@ -16,19 +13,11 @@ type Props = {
 
 const SummaryLoader = ({ gene }: Props) => (
   <div data-testid="skeleton-loader">
-    <PageHeader gene={gene} />
-    <AppBar position="static">
-      <Tabs value={0}>
-        <Tab label="Gene Summary" />
-        <Tab label="Gene Ontology" />
-      </Tabs>
-    </AppBar>
-    <SkeletonTheme baseColor="#d1d1d1">
-      <Skeleton count={10} />
-      {/* <br />
-      <br />
-      <Skeleton count={10} /> */}
-    </SkeletonTheme>
+    <Layout gene={gene}>
+      <SkeletonTheme baseColor="#d1d1d1">
+        <Skeleton count={10} />
+      </SkeletonTheme>
+    </Layout>
   </div>
 )
 
