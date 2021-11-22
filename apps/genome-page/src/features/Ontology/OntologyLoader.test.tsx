69e6724a5a024b65e7bd0f77521e6429c46eda29
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-22 13:44:10 -0600

    test: mock useLocation params

--- apps/genome-page/src/features/Ontology/OntologyLoader.test.tsx
+++ apps/genome-page/src/features/Ontology/OntologyLoader.test.tsx
@@ -1,29 +1,22 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import OntologyLoader from "./OntologyLoader"
-import { useLocation } from "react-router-dom"
+
+const gene = "DDB_G123456"
 
 jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom")
   return {
-    ...originalModule,
-    useLocation: jest.fn(),
-    useParams: jest.fn(),
+    useLocation: () => ({
+      pathname: `gene/${gene}/goannotations`,
+    }),
   }
 })
 
 describe("common/components/OntologyLoader", () => {
-  const gene = "DDB_G123456"
-  beforeEach(() =>
-    (useLocation as jest.Mock).mockReturnValue({
-      pathname: `testdb.dictybase.org/gene/${gene}/goannotations`,
-    }),
-  )
-
   it("should render correct page header", () => {
     render(<OntologyLoader gene={gene} />)
     expect(
-      screen.getByText(/Gene Ontology Annotations for DDB_G123456/),
+      screen.getByText(`Gene Ontology Annotations for ${gene}`),
     ).toBeInTheDocument()
   })
 
--- apps/genome-page/src/features/Summary/SummaryLoader.test.tsx
+++ apps/genome-page/src/features/Summary/SummaryLoader.test.tsx
@@ -1,29 +1,20 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import SummaryLoader from "./SummaryLoader"
-import { useLocation } from "react-router-dom"
 
+const gene = "DDB_G123456"
 jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom")
   return {
-    ...originalModule,
-    useLocation: jest.fn(),
-    useParams: jest.fn(),
-    useRouteMatch: jest.fn(),
+    useLocation: () => ({
+      pathname: `gene/${gene}`,
+    }),
   }
 })
 
 describe("common/components/SummaryLoader", () => {
-  const gene = "DDB_G123456"
-  beforeEach(() =>
-    (useLocation as jest.Mock).mockReturnValue({
-      pathname: `testdb.dictybase.org/gene/${gene}`,
-    }),
-  )
-
   it("should render correct page header", () => {
     render(<SummaryLoader gene={gene} />)
-    expect(screen.getByText(/Gene Summary for DDB_G123456/)).toBeInTheDocument()
+    expect(screen.getByText(`Gene Summary for ${gene}`)).toBeInTheDocument()
   })
 
   it("should render skeleton loader", () => {
