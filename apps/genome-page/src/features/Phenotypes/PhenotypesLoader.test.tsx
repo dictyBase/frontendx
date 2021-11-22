7f752b2a8da401f43d776022444aafb454277d27
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-22 12:33:55 -0600

    test: add PhenotypesLoader test cases

---
+++ apps/genome-page/src/features/Phenotypes/PhenotypesLoader.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react"
+import React from "react"
+import PhenotypesLoader from "./PhenotypesLoader"
+
+const gene = "sadA"
+
+jest.mock("react-router-dom", () => {
+  return {
+    useLocation: () => ({
+      pathname: "gene/sadA/phenotypes",
+    }),
+  }
+})
+
+describe("features/Phenotypes/PhenotypesLoader", () => {
+  it("should render loader", () => {
+    render(<PhenotypesLoader gene={gene} />)
+
+    expect(screen.getByText(`Gene Phenotypes for ${gene}`)).toBeInTheDocument()
+  })
+})
