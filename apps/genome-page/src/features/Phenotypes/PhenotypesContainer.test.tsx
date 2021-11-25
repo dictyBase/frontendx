3aefb382c07ff82379605ce3d887f9bcfa5eb329
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-24 18:00:35 -0600

    test: write unit tests for PhenotypesContainer

--- apps/genome-page/src/features/Phenotypes/PhenotypesContainer.test.tsx
+++ apps/genome-page/src/features/Phenotypes/PhenotypesContainer.test.tsx
@@ -1,23 +1,11 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import PhenotypesContainer from "./PhenotypesContainer"
-import { MockedProvider } from "@apollo/client/testing"
-import { GeneDocument } from "dicty-graphql-schema"
+import { useGeneQuery } from "dicty-graphql-schema"
 import mockGraphQLData from "common/mocks/mockGraphQLData"
 
 const gene = "sadA"
 const pathname = `gene/${gene}/phenotypes`
-const mocks = [
-  {
-    request: {
-      query: GeneDocument,
-      variables: {
-        gene: gene,
-      },
-    },
-    result: mockGraphQLData,
-  },
-]
 
 jest.mock("react-router-dom", () => {
   const useParams = () => ({ gene })
@@ -27,14 +15,33 @@ jest.mock("react-router-dom", () => {
   }
 })
 
+jest.mock("dicty-graphql-schema", () => {
+  const useGeneQuery = jest.fn()
+  return { useGeneQuery }
+})
+
 describe("features/Phenotypes/PhenotypesContainer", () => {
+  beforeEach(() => jest.clearAllMocks())
+
   it("should render phenotypes page", () => {
-    render(
-      <MockedProvider mocks={mocks} addTypename={false}>
-        <PhenotypesContainer />
-      </MockedProvider>,
-    )
+    ;(useGeneQuery as jest.Mock).mockReturnValue({
+      loading: false,
+      error: undefined,
+      data: { ...mockGraphQLData.data.gene },
+    })
+    render(<PhenotypesContainer />)
 
     expect(screen.getByText(`Gene Phenotypes for ${gene}`)).toBeInTheDocument()
+    expect(
+      screen.getByText("aberrant actin filament organization"),
+    ).toBeInTheDocument()
+  })
+
+  it("should render loading", () => {
+    ;(useGeneQuery as jest.Mock).mockReturnValue({
+      loading: true,
+    })
+    render(<PhenotypesContainer />)
+    expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
   })
 })
