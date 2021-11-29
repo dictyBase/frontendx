30cba18a052f2abe126c0cfd55c78037148376ee
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-29 16:16:01 -0600

    feat: remove references to mock data and types

--- apps/genome-page/src/features/Phenotypes/PhenotypesContainer.tsx
+++ apps/genome-page/src/features/Phenotypes/PhenotypesContainer.tsx
@@ -4,36 +4,27 @@ import { useParams } from "react-router-dom"
 import Typography from "@material-ui/core/Typography"
 import GraphQLErrorPage from "common/components/errors/GraphQLErrorPage"
 import Layout from "app/layout/Layout"
-import { useGeneQuery } from "dicty-graphql-schema"
+import { Strain, useGeneQuery } from "dicty-graphql-schema"
 import PhenotypesLoader from "./PhenotypesLoader"
 import PhenotypesDataTable from "./PhenotypesDataTable"
-import { IMockGeneData } from "mocks/mockGene"
-import mockPhenotypesData from "mocks/mockPhenotypesData"
 
 const PhenotypesContainer = () => {
   let { gene } = useParams()
   if (!gene) gene = ""
 
-  // TODO: Change to const once graphql-schema is updated...
   const { loading, error, data } = useGeneQuery({
     variables: {
       gene,
     },
     fetchPolicy: "cache-and-network",
   })
-  let updatedData = data as IMockGeneData
-  if (data) {
-    updatedData = {
-      ...updatedData,
-      phenotypes: [...mockPhenotypesData.phenotypes],
-    }
-  }
 
   if (loading) return <PhenotypesLoader gene={gene} />
 
-  if (error) return <GraphQLErrorPage error={error} />
+  if (error || !data?.allStrains) return <GraphQLErrorPage error={error} />
 
   const geneName = gene as string
+  const strains = data.allStrains.strains as Strain[]
 
   return (
     <Layout gene={geneName}>
@@ -46,7 +37,7 @@ const PhenotypesContainer = () => {
       </Helmet>
 
       <Typography component="div">
-        <PhenotypesDataTable data={updatedData?.phenotypes} />
+        <PhenotypesDataTable data={strains} />
       </Typography>
     </Layout>
   )
