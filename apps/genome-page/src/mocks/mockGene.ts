2a58a8d2981354cfbb47ea60746486b3d13f3202
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-30 12:27:32 -0600

    fix: add allStrains field with mock data

--- apps/genome-page/src/mocks/mockGene.ts
+++ apps/genome-page/src/mocks/mockGene.ts
@@ -2,12 +2,17 @@ import { Gene } from "dicty-graphql-schema"
 import mockOntologyData from "./mockOntologyData"
 import mockPhenotypesData from "./mockPhenotypesData"
 
-const mockGene: { gene: Gene } = {
+const mockGene: { gene: Gene; allStrains: Gene } = {
   gene: {
     __typename: "Gene",
     id: "DDB_G0288511",
     name: "sadA",
     goas: { ...mockOntologyData.goas },
+  },
+  allStrains: {
+    __typename: "Gene",
+    id: "DDB_G0288511",
+    name: "sadA",
     strains: { ...mockPhenotypesData.strains },
   },
 }
