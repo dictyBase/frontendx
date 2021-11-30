de06aabcaa9344c88ceb9f490cad9940e407cdcc
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-30 11:42:41 -0600

    feat: add table row component

---
+++ apps/genome-page/src/features/Phenotypes/PhenotypeRow.tsx
@@ -0,0 +1,38 @@
+import { TableRow, TableCell, Box } from "@material-ui/core"
+import { Phenotype } from "dicty-graphql-schema"
+
+interface PhenotypeRowProps {
+  id: string
+  strain?: string
+  characteristics?: string[]
+  phenotype: Phenotype
+}
+
+const PhenotypeRow = ({
+  id,
+  strain,
+  characteristics,
+  phenotype,
+}: PhenotypeRowProps) => {
+  return (
+    <TableRow>
+      <TableCell>
+        {strain ? (
+          <>
+            <Box>
+              <a href={`/stockcenter/strains/${id}`}>{strain}</a>
+            </Box>
+            <Box>({id})</Box>
+          </>
+        ) : (
+          <></>
+        )}
+      </TableCell>
+      <TableCell>{characteristics ? characteristics : ""}</TableCell>
+      <TableCell>{phenotype.phenotype}</TableCell>
+      <TableCell></TableCell>
+    </TableRow>
+  )
+}
+
+export default PhenotypeRow
