044e2a5f503707604db3854f36c593fbb65f7a10
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-30 14:10:49 -0600

    fix: add full sadA mock data

--- apps/genome-page/src/mocks/mockPhenotypesData.tsx
+++ apps/genome-page/src/mocks/mockPhenotypesData.tsx
@@ -22,7 +22,7 @@ const mockPhenotypesData: { strains: Strain[] } = {
             ],
             title: "SadA, a novel adhesion receptor in Dictyostelium.",
             journal: "J Cell Biol",
-            pub_date: "159:1109-19",
+            issue: "159:1109-19",
           },
         },
         {
@@ -37,7 +37,7 @@ const mockPhenotypesData: { strains: Strain[] } = {
             ],
             title: "SadA, a novel adhesion receptor in Dictyostelium.",
             journal: "J Cell Biol",
-            pub_date: "159:1109-19",
+            issue: "159:1109-19",
           },
         },
         {
@@ -52,7 +52,7 @@ const mockPhenotypesData: { strains: Strain[] } = {
             ],
             title: "SadA, a novel adhesion receptor in Dictyostelium.",
             journal: "J Cell Biol",
-            pub_date: "159:1109-19",
+            issue: "159:1109-19",
           },
         },
         {
@@ -70,138 +70,236 @@ const mockPhenotypesData: { strains: Strain[] } = {
             title:
               "Cell Substratum Adhesion during Early Development of Dictyostelium discoideum.",
             journal: "PLoS ONE",
-            pub_date: "9:e106574",
+            issue: "9:e106574",
+          },
+        },
+        {
+          phenotype: "decreased phagocytosis",
+          publication: {
+            id: "25247557",
+            authors: [
+              { last_name: "Fey" },
+              { last_name: "Stephens" },
+              { last_name: "Titus" },
+              { last_name: "Chisholm" },
+            ],
+            title: "SadA, a novel adhesion receptor in Dictyostelium.",
+            journal: "J Cell Biol",
+            issue: "159:1109-19",
+          },
+        },
+        {
+          phenotype: "increased growth rate",
+          publication: {
+            id: "25247557",
+            authors: [
+              { last_name: "Fey" },
+              { last_name: "Stephens" },
+              { last_name: "Titus" },
+              { last_name: "Chisholm" },
+            ],
+            title: "SadA, a novel adhesion receptor in Dictyostelium.",
+            journal: "J Cell Biol",
+            issue: "159:1109-19",
+          },
+        },
+      ],
+      in_stock: true,
+    } as Strain,
+
+    // Second
+    {
+      id: "DBS0309040",
+      label: "sadA",
+      characteristics: ["null mutant"],
+      phenotypes: [
+        {
+          phenotype: "abolished protein localization to cell surface",
+          publication: {
+            id: "12499361",
+            authors: [
+              { last_name: "Froquet" },
+              { last_name: "le Coadic" },
+              { last_name: "Perrin" },
+              { last_name: "Cherix" },
+              { last_name: "Cornillon" },
+              { last_name: "Cosson" },
+            ],
+            title:
+              "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
+            journal: "Molecular biology of the cell",
+            issue: ":",
+          },
+        },
+        {
+          phenotype: "decreased phagocytosis",
+          publication: {
+            id: "12499361",
+            authors: [
+              { last_name: "Froquet" },
+              { last_name: "le Coadic" },
+              { last_name: "Perrin" },
+              { last_name: "Cherix" },
+              { last_name: "Cornillon" },
+              { last_name: "Cosson" },
+            ],
+            title:
+              "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
+            journal: "Molecular biology of the cell",
+            issue: ":",
+          },
+        },
+        {
+          phenotype: "decreased protein stabilization",
+          publication: {
+            id: "25247557",
+            authors: [
+              { last_name: "Froquet" },
+              { last_name: "le Coadic" },
+              { last_name: "Perrin" },
+              { last_name: "Cherix" },
+              { last_name: "Cornillon" },
+              { last_name: "Cosson" },
+            ],
+            title:
+              "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
+            journal: "Molecular biology of the cell",
+            issue: ":",
           },
         },
       ],
       in_stock: false,
     } as Strain,
-    // {
-    //   id: "DBS0236921",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "abolished cell-substrate adhesion",
-    //   reference: {
-    //     author: "Fey, Stephens, Titus & Chisholm (2002)",
-    //     title: "SadA, a novel adhesion receptor in Dictyostelium.",
-    //     name: "J Cell Biol 159:1109-19",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/2172",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/12499361",
-    //       fullText: "http://www.jcb.org/cgi/pmidlookup?view=full&pmid=12499361",
-    //     },
-    //   },
-    // },
-    // {
-    //   id: "DBS0236921",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "decreased phagocytosis",
-    //   reference: {
-    //     author: "Fey, Stephens, Titus & Chisholm (2002)",
-    //     title: "SadA, a novel adhesion receptor in Dictyostelium.",
-    //     name: "J Cell Biol 159:1109-19",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/2172",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/12499361",
-    //       fullText: "http://www.jcb.org/cgi/pmidlookup?view=full&pmid=12499361",
-    //     },
-    //   },
-    // },
-    // {
-    //   id: "DBS0236921",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "increased growth rate",
-    //   reference: {
-    //     author: "Fey, Stephens, Titus & Chisholm (2002)",
-    //     title: "SadA, a novel adhesion receptor in Dictyostelium.",
-    //     name: "J Cell Biol 159:1109-19",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/2172",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/12499361",
-    //       fullText: "http://www.jcb.org/cgi/pmidlookup?view=full&pmid=12499361",
-    //     },
-    //   },
-    // },
 
-    // // Second...
-    // {
-    //   id: "DBS0309040",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "abolished protein localization to cell surface",
-    //   reference: {
-    //     author: "Froquet, le Coadic, Perrin, Cherix, Cornillon & Cosson (2012)",
-    //     title:
-    //       "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
-    //     name: "Molecular biology of the cell :",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/11946",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/22219373",
-    //       fullText:
-    //         "http://www.molbiolcell.org/cgi/pmidlookup?view=long&pmid=22219373",
-    //     },
-    //   },
-    // },
-    // {
-    //   id: "DBS0309040",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "decreased phagocytosis",
-    //   reference: {
-    //     author: "Froquet, le Coadic, Perrin, Cherix, Cornillon & Cosson (2012)",
-    //     title:
-    //       "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
-    //     name: "Molecular biology of the cell :",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/11946",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/22219373",
-    //       fullText:
-    //         "http://www.molbiolcell.org/cgi/pmidlookup?view=long&pmid=22219373",
-    //     },
-    //   },
-    // },
-    // {
-    //   id: "DBS0309040",
-    //   strain: "sadA",
-    //   characteristics: "null mutant",
-    //   phenotype: "decreased protein stabilization",
-    //   reference: {
-    //     author: "Froquet, le Coadic, Perrin, Cherix, Cornillon & Cosson (2012)",
-    //     title:
-    //       "TM9/Phg1 and SadA proteins control surface expression and stability of SibA adhesion molecules in Dictyostelium.",
-    //     name: "Molecular biology of the cell :",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/11946",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/22219373",
-    //       fullText:
-    //         "http://www.molbiolcell.org/cgi/pmidlookup?view=long&pmid=22219373",
-    //     },
-    //   },
-    // },
+    // Third
+    {
+      id: "DBS0350974",
+      label: "sadA-/[act15]:gpaH",
+      characteristics: ["multiple mutant", "overexpressor", "null mutant"],
+      phenotypes: [
+        {
+          phenotype: "aberrant cytokinesis",
+          publication: {
+            id: "12499361",
+            authors: [{ last_name: "Wu" }, { last_name: "Janetopoulos" }],
+            title:
+              "The G alpha subunit G?8 inhibits proliferation, promotes adhesion and regulates cell differentiation.",
+            journal: "Dev. Biol.",
+            issue: ":",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
 
-    // // Third...
-    // {
-    //   id: "DBS0350974",
-    //   strain: "sadA-/[act15]:gpaH",
-    //   characteristics: "multiple mutant, overexpressor, null mutant",
-    //   phenotype: "aberrant cytokinesis",
-    //   reference: {
-    //     author: "Wu & Janetopoulos (2013)",
-    //     title:
-    //       "The G alpha subunit G?8 inhibits proliferation, promotes adhesion and regulates cell differentiation.",
-    //     name: "Dev. Biol. :",
-    //     links: {
-    //       dictyBase: "http://dictybase.org/publication/13217",
-    //       pubMed: "http://view.ncbi.nlm.nih.gov/pubmed/23665473",
-    //       fullText:
-    //         "http://linkinghub.elsevier.com/retrieve/pii/S0012-1606(13)00229-7",
-    //     },
-    //   },
-    // },
+    // Fourth
+    {
+      id: "DBS0349790",
+      label: "sadA-/[act15]:sadA(1-923):GFP",
+      characteristics: ["null mutant", "multiple mutant"],
+      phenotypes: [
+        {
+          phenotype: "decreased cell-substrate adhesion",
+          publication: {
+            id: "12499361",
+            authors: [{ last_name: "Kowal" }, { last_name: "Chisholm" }],
+            title:
+              "Uncovering a role for the Dictyostelium discoideum SadA protein in cell-substrate adhesion: A Role for the Tail.",
+            journal: "Eukaryotic cell",
+            issue: ":",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
 
-    // // Fourth...
+    // Fifth
+    {
+      id: "DBS0349792",
+      label: "sadA-/[act15]:sadA(S924A/S925A):GFP",
+      characteristics: ["null mutant", "multiple mutant"],
+      phenotypes: [
+        {
+          phenotype: "wild type",
+          publication: {
+            id: "12499361",
+            authors: [{ last_name: "Kowal" }, { last_name: "Chisholm" }],
+            title:
+              "Uncovering a role for the Dictyostelium discoideum SadA protein in cell-substrate adhesion: A Role for the Tail.",
+            journal: "Eukaryotic cell",
+            issue: ":",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
+
+    // Sixth
+    {
+      id: "DBS0349791",
+      label: "sadA-/[act15]:sadA(S924E/S925E):GFP",
+      characteristics: ["null mutant", "multiple mutant"],
+      phenotypes: [
+        {
+          phenotype: "decreased cell-substrate adhesion",
+          publication: {
+            id: "12499361",
+            authors: [{ last_name: "Kowal" }, { last_name: "Chisholm" }],
+            title:
+              "Uncovering a role for the Dictyostelium discoideum SadA protein in cell-substrate adhesion: A Role for the Tail.",
+            journal: "Eukaryotic cell",
+            issue: ":",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
+
+    // Seventh
+    {
+      id: "DBS0236922",
+      label: "sadA-/[act15]:sadA:GFP",
+      characteristics: ["null mutant", "rescued"],
+      phenotypes: [
+        {
+          phenotype: "wild type",
+          publication: {
+            id: "12499361",
+            authors: [
+              { last_name: "Fey" },
+              { last_name: "Stephens" },
+              { last_name: "Titus" },
+              { last_name: "Chisholm" },
+            ],
+            title: "SadA, a novel adhesion receptor in Dictyostelium.",
+            journal: " J Cell Biol",
+            issue: "159:1109-19",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
+
+    // Eighth
+    {
+      id: "DBS0349793",
+      label: "[act15]:sadA(921-952)",
+      characteristics: ["dominant negative mutant", "overexpressor"],
+      phenotypes: [
+        {
+          phenotype: "decreased cell-substrate adhesion",
+          publication: {
+            id: "12499361",
+            authors: [{ last_name: "Kowal" }, { last_name: "Chisholm" }],
+            title:
+              "Uncovering a role for the Dictyostelium discoideum SadA protein in cell-substrate adhesion: A Role for the Tail.",
+            journal: "Eukaryotic cell",
+            issue: ":",
+          },
+        },
+      ],
+      in_stock: false,
+    } as Strain,
   ],
 }
 
