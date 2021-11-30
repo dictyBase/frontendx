2e63fa9a591f307d7dceb4e338e947cd61801254
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-30 12:12:29 -0600

    feat: update gene object to include strains field

--- apps/genome-page/src/mocks/mockGene.ts
+++ apps/genome-page/src/mocks/mockGene.ts
@@ -1,24 +1,14 @@
-import { GoAnnotation } from "dicty-graphql-schema"
+import { Gene } from "dicty-graphql-schema"
 import mockOntologyData from "./mockOntologyData"
-import mockPhenotypesData, { IMockPhenotypesData } from "./mockPhenotypesData"
+import mockPhenotypesData from "./mockPhenotypesData"
 
-interface IMockGeneData {
-  __typename?: string
-  id: string
-  name: string
-  goas?: GoAnnotation[]
-  phenotypes?: IMockPhenotypesData[]
-}
-
-const mockGene: { gene: IMockGeneData } = {
+const mockGene: { gene: Gene } = {
   gene: {
     __typename: "Gene",
     id: "DDB_G0288511",
     name: "sadA",
-    goas: [...mockOntologyData.goas],
-    phenotypes: [...mockPhenotypesData.phenotypes],
+    goas: { ...mockOntologyData.goas },
+    strains: { ...mockPhenotypesData.strains },
   },
 }
-
-export type { IMockGeneData }
 export default mockGene
