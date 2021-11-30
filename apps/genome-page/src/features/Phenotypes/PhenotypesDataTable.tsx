4c853ded876ee87a41fbacb2af6485a53428fe1c
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-11-30 11:44:04 -0600

    refactor: simplify PhenotypesDataTable component

--- apps/genome-page/src/features/Phenotypes/PhenotypesDataTable.tsx
+++ apps/genome-page/src/features/Phenotypes/PhenotypesDataTable.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import {
-  Box,
   makeStyles,
   Paper,
   Table,
@@ -10,7 +9,8 @@ import {
   TableHead,
   TableRow,
 } from "@material-ui/core"
-import { IMockPhenotypesData } from "mocks/mockPhenotypesData"
+import { Phenotype, Strain } from "dicty-graphql-schema"
+import PhenotypeRow from "./PhenotypeRow"
 
 const useStyles = makeStyles({
   root: {
@@ -29,32 +29,28 @@ const useStyles = makeStyles({
 })
 
 interface PhenotypesDataTableProps {
-  data?: IMockPhenotypesData[]
+  data: Strain[]
 }
 
-/**
- * Checks if the field was repeated, so we can give a "nested" table effect
- */
-const isRepeated = (
-  data: IMockPhenotypesData[],
-  i: number,
-  field: "strain" | "characteristics" | "phenotype",
-) => {
-  if (i === 0) return false
-
-  const cur = data[i]
-  const prev = data[i - 1]
-  const strainIsRepeated = cur.strain === prev.strain && cur.id === prev.id
-  switch (field) {
-    case "strain":
-      return strainIsRepeated
-    case "characteristics":
-      return strainIsRepeated && cur.characteristics === prev.characteristics
-    case "phenotype":
-      return strainIsRepeated && cur.phenotype === prev.phenotype
-    default:
-      return false
-  }
+const StrainRow = ({ strain }: { strain: Strain }) => {
+  const phenotypes = strain.phenotypes as Phenotype[]
+  const characteristics = strain.characteristics as string[] | undefined
+  return (
+    <>
+      {phenotypes.map((phenotype, index) =>
+        index === 0 ? (
+          <PhenotypeRow
+            id={strain.id}
+            strain={strain.label}
+            characteristics={characteristics}
+            phenotype={phenotype}
+          />
+        ) : (
+          <PhenotypeRow id={strain.id} phenotype={phenotype} />
+        ),
+      )}
+    </>
+  )
 }
 
 const PhenotypesDataTable = ({ data }: PhenotypesDataTableProps) => {
@@ -68,44 +64,12 @@ const PhenotypesDataTable = ({ data }: PhenotypesDataTableProps) => {
             <TableCell>Strain</TableCell>
             <TableCell>Characteristics</TableCell>
             <TableCell>Phenotype</TableCell>
-            <TableCell>Assay &amp; Environment</TableCell>
             <TableCell>Reference(s)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data?.map((item, i) => (
-            <TableRow key={`${item}#${i}`}>
-              <TableCell>
-                {isRepeated(data, i, "strain") ? (
-                  <></>
-                ) : (
-                  <>
-                    <Box>
-                      <a href={`/stockcenter/strains/${item.id}`}>
-                        {item.strain}
-                      </a>
-                    </Box>
-                    <Box>({item.id})</Box>
-                  </>
-                )}
-              </TableCell>
-              <TableCell>
-                {isRepeated(data, i, "characteristics")
-                  ? ""
-                  : item.characteristics}
-              </TableCell>
-              <TableCell>
-                {isRepeated(data, i, "phenotype") ? "" : item.phenotype}
-              </TableCell>
-              <TableCell>
-                <i>To be implemented</i>
-              </TableCell>
-              <TableCell>
-                <b>{item.reference.author}</b>
-                &nbsp; '{item.reference.title}' &nbsp;
-                <i>{item.reference.name}</i>
-              </TableCell>
-            </TableRow>
+          {data.map((strain) => (
+            <StrainRow strain={strain} />
           ))}
         </TableBody>
       </Table>
