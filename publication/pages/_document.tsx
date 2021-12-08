c5e38f7e1168d7b7b14b726f425095641a1a2a38
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-08 15:09:33 -0600

    feat: add _document.tsx which replaces public/index.html

---
+++ publication/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+
+class PublicationDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <meta name="theme-color" content="#000000" />
+
+          <link rel="manifest" href="manifest.json" />
+
+          <title>dictyBase Literature</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default PublicationDocument
