2b96f1ed981d986ca162793c69799f2051c8ebe9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-09 13:39:48 -0600

    feat: migrate publication page route

--- publication/pages/[publicationId].tsx
+++ publication/pages/[publicationId].tsx
@@ -1,7 +1,25 @@
-import PublicationContainer from "../src/features/Publication/PublicationContainer"
+import React from "react"
+import { useRouter } from "next/router"
+import { Publication, usePublicationQuery } from "dicty-graphql-schema"
+import ErrorPage from "src/common/components/ErrorPage"
+import PublicationContent from "components/PublicationContent"
+import PublicationLoader from "src/features/Publication/PublicationLoader"
 
 function PublicationPage() {
-  return <PublicationContainer />
+  const { query } = useRouter()
+  const id = query.publicationId as string
+
+  const { loading, error, data } = usePublicationQuery({
+    variables: { id },
+  })
+
+  if (loading) return <PublicationLoader />
+  if (error || !data) return <ErrorPage />
+
+  const publication = data.publication as Publication
+  const title = publication.title as string
+
+  return <PublicationContent title={title} data={publication} />
 }
 
 export default PublicationPage
