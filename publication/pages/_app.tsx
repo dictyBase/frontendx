39a61d5a9d8accb9a81ca2ace1a7a01053784fc5
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-09 16:04:43 -0600

    refactor: organize layout components + remove old Routes component

--- publication/pages/_app.tsx
+++ publication/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from "next/app"
 import { AuthProvider } from "../src/features/Authentication/AuthStore"
-import AppProviders from "../src/app/layout/AppProviders"
+import AppProviders from "components/layout/AppProviders"
 import CssBaseline from "@material-ui/core/CssBaseline"
-import App from "../src/app/layout/App"
+import App from "components/layout/App"
 import Head from "next/head"
 
 const PublicationApp = ({ Component, pageProps }: AppProps) => {
