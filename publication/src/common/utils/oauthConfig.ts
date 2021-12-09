ce621e6ddebf092884ff9482bcd22758b5c74864
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-09 14:13:44 -0600

    feat: migrate /login related components

--- publication/src/common/utils/oauthConfig.ts
+++ publication/src/common/utils/oauthConfig.ts
@@ -1,4 +1,4 @@
-import clientConfig from "common/utils/clientConfig"
+import clientConfig from "src/common/utils/clientConfig"
 
 type Config = {
   name: string
@@ -23,13 +23,15 @@ type Auth = {
   [index: string]: any
 }
 
+const url = typeof window !== "undefined" ? window.location.origin : ""
+
 const oauthConfig: Auth = {
   google: {
     name: "Google",
     url: "/auth/google",
     authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
     clientId: clientConfig.google.clientId,
-    redirectUrl: `${window.location.origin}/google/callback`,
+    redirectUrl: `${url}/google/callback`,
     requiredUrlParams: [["response_type", "code"]],
     scopes: ["email"],
     scopeDelimiter: " ",
@@ -41,7 +43,7 @@ const oauthConfig: Auth = {
     url: "/auth/linkedin",
     authorizationEndpoint: "https://www.linkedin.com/oauth/v2/authorization",
     clientId: clientConfig.linkedin.clientId,
-    redirectUrl: `${window.location.origin}/linkedin/callback`,
+    redirectUrl: `${url}/linkedin/callback`,
     scopes: ["r_emailaddress"],
     scopeDelimiter: " ",
     requiredUrlParams: [
@@ -55,7 +57,7 @@ const oauthConfig: Auth = {
     url: "/auth/orcid",
     authorizationEndpoint: "https://orcid.org/oauth/authorize",
     clientId: clientConfig.orcid.clientId,
-    redirectUrl: `${window.location.origin}/orcid/callback`,
+    redirectUrl: `${url}/orcid/callback`,
     scopes: ["/authenticate"],
     scopeDelimiter: " ",
     requiredUrlParams: [["response_type", "code"]],
