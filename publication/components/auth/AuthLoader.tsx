a1feb5540f2bd27d45b8defbbf2a2dcb00d37020
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-10 18:38:36 -0600

    fix: remove incorrect fortawesome import

--- publication/components/auth/AuthLoader.tsx
+++ publication/components/auth/AuthLoader.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
-import Typography from "@material-ui/core/Typography"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { CircularProgress, Typography } from "@material-ui/core"
 
 /**
  * Loading screen during the login process
  */
-
 const AuthLoader = () => {
   return (
     <Box display="flex" flexDirection="column" justifyContent="center">
@@ -14,7 +12,7 @@ const AuthLoader = () => {
         Logging in...
       </Typography>
       <Box textAlign="center" mt={3}>
-        <FontAwesomeIcon icon="spinner" size="5x" pulse fixedWidth />
+        <CircularProgress />
       </Box>
     </Box>
   )
