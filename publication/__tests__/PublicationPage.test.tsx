667199404b6fe610742341118a9417138b835d07
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-13 12:21:10 -0600

    test: pass publication page tests

---
+++ publication/__tests__/PublicationPage.test.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { data } from "./mocks/mockdata"
+import PublicationPage from "../components/PublicationPage"
+
+describe("components/PublicationPage", () => {
+  describe("initial render", () => {
+    it("displays expected data", () => {
+      render(<PublicationPage data={data.publication} />)
+
+      expect(
+        screen.getByText(
+          "This is a fake publication title that should be at least ten words",
+        ),
+      ).toBeInTheDocument()
+      // shows depositor
+      const { abstract } = data.publication
+      expect(screen.getByText(abstract)).toBeInTheDocument()
+    })
+  })
+})
