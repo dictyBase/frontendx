522d831092cbd7dc345b2dfd8bb7a2eb41ff55da
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-13 11:41:09 -0600

    docs: explain use of jsdom for testing

--- publication/__tests__/ErrorNotification.test.tsx
+++ publication/__tests__/ErrorNotification.test.tsx
@@ -2,10 +2,6 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import ErrorNotification from "../components/errors/ErrorNotification"
 
-/**
- * @jest-environment jsdom
- */
-
 describe("components/errors/ErrorNotification", () => {
   it("should display provided error message", () => {
     render(<ErrorNotification error="not a marine biologist" />)
