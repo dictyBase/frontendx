c5f3118ab15cf5753d4cb2c168c2abf33be1ec95
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-13 11:37:57 -0600

    fix: ErrorNotification test with jsdom

--- publication/__tests__/ErrorNotification.test.tsx
+++ publication/__tests__/ErrorNotification.test.tsx
@@ -2,10 +2,13 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import ErrorNotification from "../components/errors/ErrorNotification"
 
-describe("features/Authentication/ErrorNotification", () => {
-  render(<ErrorNotification error="not a marine biologist" />)
+/**
+ * @jest-environment jsdom
+ */
 
+describe("components/errors/ErrorNotification", () => {
   it("should display provided error message", () => {
+    render(<ErrorNotification error="not a marine biologist" />)
     expect(screen.getByText(/not a marine biologist/)).toBeInTheDocument()
   })
 })
