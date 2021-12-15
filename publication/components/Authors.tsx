ce6c1953045125aa75b9473931da35c661bd13ff
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-15 15:26:37 -0600

    refactor: use Maybe<Author> type instead of making type conversion

--- publication/components/Authors.tsx
+++ publication/components/Authors.tsx
@@ -1,20 +1,20 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
-import { Author } from "dicty-graphql-schema"
+import { Author, Maybe } from "dicty-graphql-schema"
 
-type Props = {
-  authors: Author[]
+interface AuthorsProps {
+  authors: Maybe<Author>[]
 }
 
 /**
  * Authors displays an inline list of the authors of the publication.
  */
-const Authors = ({ authors }: Props) => {
+const Authors = ({ authors }: AuthorsProps) => {
   return (
     <Box pb={2}>
       {authors.map((author, index) => (
         <span key={index}>
-          {(index ? ", " : "") + author.last_name + " " + author.initials}
+          {(index ? ", " : "") + author?.last_name + " " + author?.initials}
         </span>
       ))}
     </Box>
