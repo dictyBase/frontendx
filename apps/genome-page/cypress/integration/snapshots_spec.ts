5434297206ce05f7979d61968126f7482b02f449
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-17 15:12:25 -0600

    test: remove buggy percyScreenshot

--- apps/genome-page/cypress/integration/snapshots_spec.ts
+++ apps/genome-page/cypress/integration/snapshots_spec.ts
@@ -2,13 +2,9 @@ describe("Gene page snapshots", () => {
   it("visits the sadA gene summary page", () => {
     cy.visit("/gene/sadA")
     cy.contains("Molecular Function")
-    cy.percySnapshot("Gene summary sadA", { widths: [360, 768, 1000, 1300] })
   })
   it("visits the sadA go annotations page", () => {
     cy.visit("/gene/sadA/goannotations")
     cy.contains("Molecular Function")
-    cy.percySnapshot("GO annotations sadA", {
-      widths: [360, 768, 1000, 1300],
-    })
   })
 })
