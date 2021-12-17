11e4fb3b334eb74ffcc59a30cf5432fc3a12095a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-17 15:15:30 -0600

    test: add visual testing for phenotypes tab

--- apps/genome-page/cypress/integration/snapshots_spec.ts
+++ apps/genome-page/cypress/integration/snapshots_spec.ts
@@ -3,8 +3,18 @@ describe("Gene page snapshots", () => {
     cy.visit("/gene/sadA")
     cy.contains("Molecular Function")
   })
+  
   it("visits the sadA go annotations page", () => {
     cy.visit("/gene/sadA/goannotations")
     cy.contains("Molecular Function")
+  })
+
+  it("visits the sadA phenotypes page", () => {
+    cy.visit("/gene/sadA/phenotypes")
+    cy.contains("Gene Phenotypes for sadA")
+    cy.contains("Strain")
+    cy.contains("Characteristics")
+    cy.contains("Phenotype")
+    cy.contains("Reference(s)")
   })
 })
