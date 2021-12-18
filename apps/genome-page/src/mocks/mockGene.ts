5fa8b8be564c6eec77e5a9ae1e78dad6750962e2
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-17 19:13:14 -0600

    feat: add empty references mock data

--- apps/genome-page/src/mocks/mockGene.ts
+++ apps/genome-page/src/mocks/mockGene.ts
@@ -1,8 +1,9 @@
 import { Gene } from "dicty-graphql-schema"
 import mockOntologyData from "./mockOntologyData"
 import mockPhenotypesData from "./mockPhenotypesData"
+import mockReferencesData from "./mockReferencesData"
 
-const mockGene: { gene: Gene; allStrains: Gene } = {
+const mockGene: { gene: Gene; allStrains: Gene; allReferences: Gene } = {
   gene: {
     __typename: "Gene",
     id: "DDB_G0288511",
@@ -14,6 +15,12 @@ const mockGene: { gene: Gene; allStrains: Gene } = {
     id: "DDB_G0288511",
     name: "sadA",
     strains: [...mockPhenotypesData.strains],
+  },
+  allReferences: {
+    __typename: "Gene",
+    id: "DDB_G0288511",
+    name: "sadA",
+    strains: [...mockReferencesData],
   },
 }
 export default mockGene
