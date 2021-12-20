0041cc8f11dab8bccd939ef35889e8f2e1e19b1d
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-20 13:06:39 -0600

    refactor: clean up phenotypes components

--- apps/genome-page/src/features/Phenotypes/PhenotypesLoader.tsx
+++ apps/genome-page/src/features/Phenotypes/PhenotypesLoader.tsx
@@ -1,23 +1,15 @@
-import React from "react"
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import Layout from "app/layout/Layout"
-
-type Props = {
-  /** Gene name */
-  gene: string
-}
+import { Skeleton } from "@mui/material"
+import Box from "@material-ui/core/Box"
 
 /**
  * Loading screen for Phenotypes page
  */
-const PhenotypesLoader = ({ gene }: Props) => (
-  <div data-testid="skeleton-loader">
-    <Layout gene={gene}>
-      <SkeletonTheme baseColor="#d1d1d1">
-        <Skeleton count={10} />
-      </SkeletonTheme>
-    </Layout>
-  </div>
+const PhenotypesLoader = () => (
+  <Box mt={"10px"} data-testid="skeleton-loader">
+    {[...Array(10)].map((i) => (
+      <Skeleton key={i} animation="wave" />
+    ))}
+  </Box>
 )
 
 export default PhenotypesLoader
