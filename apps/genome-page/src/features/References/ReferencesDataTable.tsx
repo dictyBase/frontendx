823af7b56101e03896576af009df49e1cdf4b894
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-12-20 15:37:42 -0600

    test: fix phenotypes test cases with new props

---
+++ apps/genome-page/src/features/References/ReferencesDataTable.tsx
@@ -0,0 +1,11 @@
+import { GeneQuery } from "dicty-graphql-schema"
+
+interface Props {
+  data: GeneQuery
+}
+
+const ReferencesDataTable = ({ data }: Props) => {
+  return <p>Data goes here...</p>
+}
+
+export default ReferencesDataTable
