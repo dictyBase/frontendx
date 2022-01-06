277b7538c46b6648cef6e225a6c8c830a8eed20f
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-06 14:24:03 -0600

    refactor: avoid using as for type conversion + move StrainRow into separate file

--- apps/genome-page/src/features/Phenotypes/PhenotypesDataTable.tsx
+++ apps/genome-page/src/features/Phenotypes/PhenotypesDataTable.tsx
@@ -8,44 +8,20 @@ import {
   TableRow,
 } from "@material-ui/core"
 import ShoppingCart from "@material-ui/icons/ShoppingCart"
-import { GeneQuery, Phenotype, Strain } from "dicty-graphql-schema"
-import PhenotypeRow from "./PhenotypeRow"
+import { GeneQuery } from "dicty-graphql-schema"
 import OtherError from "components/errors/OtherError"
-import useStyles from "../../common/styles/dataTableStyles"
+import useStyles from "common/styles/dataTableStyles"
+import RenderPhenotypes from "./RenderPhenotypes"
 
 interface PhenotypesDataTableProps {
   data: GeneQuery
 }
 
-const StrainRow = ({ strain }: { strain: Strain }) => {
-  const phenotypes = strain.phenotypes as Phenotype[]
-  const characteristics = strain.characteristics as string[] | undefined
-  return (
-    <>
-      {phenotypes.map((phenotype, index) =>
-        index === 0 ? (
-          <PhenotypeRow
-            id={strain.id}
-            strain={strain.label}
-            characteristics={characteristics}
-            phenotype={phenotype}
-            in_stock={strain.in_stock}
-            key={index}
-          />
-        ) : (
-          <PhenotypeRow id={strain.id} phenotype={phenotype} key={index} />
-        ),
-      )}
-    </>
-  )
-}
-
 const PhenotypesDataTable = ({ data }: PhenotypesDataTableProps) => {
   const classes = useStyles()
-  const allStrains = data.allStrains
-  if (!allStrains || !allStrains.strains) return <OtherError />
 
-  const strains = allStrains.strains as Strain[]
+  if (!data.allStrains) return <OtherError />
+  const allStrains = data.allStrains
 
   return (
     <TableContainer component={Paper} className={classes.root}>
@@ -62,8 +38,8 @@ const PhenotypesDataTable = ({ data }: PhenotypesDataTableProps) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {strains.map((strain, i) => (
-            <StrainRow strain={strain} key={`strain#${i}`} />
+          {allStrains.strains?.map((strain, i) => (
+            <RenderPhenotypes strain={strain} key={`strain#${i}`} />
           ))}
         </TableBody>
       </Table>
