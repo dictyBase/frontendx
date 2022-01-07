413bb0a4b20e4f7f4ba6c6077913bf223f2864d9
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-07 10:36:30 -0600

    refactor: move src/features/Ontology/utils to common/utils

--- apps/genome-page/src/common/utils/qualifierFormatter.tsx
+++ apps/genome-page/src/common/utils/qualifierFormatter.tsx
@@ -1,21 +1,18 @@
-import React from "react"
-
 /**
  * Helper function to handle GO qualifiers formatting
  */
-
 const qualifierFormatter = (str: string) => {
   // remove pipe after NOT
   let converted = str.replace("NOT|", "NOT ")
 
   if (converted.substring(0, 3) === "NOT") {
     return (
-      <React.Fragment>
-        <strong>NOT </strong> <em>{converted.substring(4)}</em>
-      </React.Fragment>
+      <>
+        <strong>NOT </strong>
+        <em>{converted.substring(4)}</em>
+      </>
     )
   }
-
   return <em>{converted}</em>
 }
 
