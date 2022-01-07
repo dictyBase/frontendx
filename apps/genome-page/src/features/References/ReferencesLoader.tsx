5cfb8bdc3fc738d50cab431e5d361a6b6c7975e7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-07 12:44:10 -0600

    test: add new test cases for References components

--- apps/genome-page/src/features/References/ReferencesLoader.tsx
+++ apps/genome-page/src/features/References/ReferencesLoader.tsx
@@ -2,7 +2,7 @@ import { Box, Skeleton } from "@mui/material"
 
 const ReferencesLoader = () => {
   return (
-    <Box mt="10px">
+    <Box mt="10px" data-testid="skeleton-loader">
       {[...Array(10)].map((i) => (
         <Skeleton animation="wave" key={i} />
       ))}
