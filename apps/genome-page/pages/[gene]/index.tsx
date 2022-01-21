5273c9c32a7428e178154447a99bf49ab028a1a0
Sean Kim <seank4@uic.edu>
2022-01-21 15:23:41 -0600

    refactor: Create Gene Index page and move graphql call to index
    pages/[gene]/index.tsx has been refactored to make the graphql call and then conditionally rendering a component with the data.

---
+++ apps/genome-page/pages/[gene]/index.tsx
@@ -0,0 +1,29 @@
+import SummaryContainer from "components/features/Summary/SummaryContainer"
+import SummaryLoader from "components/features/Summary/SummaryLoader"
+import GraphQLErrorPage from "components/errors/GraphQLErrorPage"
+import { useRouter } from "next/router"
+import { useGeneQuery, Gene } from "dicty-graphql-schema"
+
+/**
+ * Renders the gene page given a gene id
+ */
+const GenomePageWrapper = () => {
+  const { query } = useRouter()
+  const gene = query.id as string
+
+  const { loading, error, data } = useGeneQuery({
+    variables: {
+      gene,
+    },
+    fetchPolicy: "cache-and-network",
+  })
+  return (
+    <>
+      {loading ? <SummaryLoader /> : <></>}
+      {error ? <GraphQLErrorPage error={error} /> : <></>}
+      {data ? <SummaryContainer gene={data.gene as Gene} /> : <></>}
+    </>
+  )
+}
+
+export default GenomePageWrapper
