06f412b39c1c0c9096a84c6e7988348df27be3de
Sean Kim <seank4@uic.edu>
2022-01-21 15:34:37 -0600

    feat: create additional.d.ts

---
+++ apps/genome-page/additional.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="next" />
+import "typescript"
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_BASENAME: string
+      NEXT_PUBLIC_GA_TRACKING_ID: string
+      NEXT_PUBLIC_NAVBAR_JSON: string
+      NEXT_PUBLIC_FOOTER_JSON: string
+      NEXT_PUBLIC_GRAPHQL_SERVER: string
+      // the alternate graphql server used to prevent cross-site cookie issues
+      NEXT_PUBLIC_ALT_GRAPHQL_SERVER: string
+      // Activates mock service worker
+      NEXT_PUBLIC_MOCK_SERVER: "on" | "off"
+      // used to differentiate between deploy environments, specifically for using correct GraphQL URL
+      DEPLOY_ENV: "development" | "staging" | "production"
+    }
+  }
+}
