a2189185d9fa1c0dac0fbf8508a6fc0dd4d9e69a
Sean Kim <seank4@uic.edu>
2022-01-26 10:00:47 -0600

    refactor: Refactor test cases
    TODO: Need to create test cases for pages that make graphql calls

--- apps/genome-page/components/features/Summary/SummaryLoader.test.tsx
+++ apps/genome-page/components/features/Summary/SummaryLoader.test.tsx
@@ -1,18 +1,14 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import SummaryLoader from "./SummaryLoader"
-
+const useRouter = jest.spyOn(require("next/router"), "useRouter")
 const gene = "DDB_G123456"
-jest.mock("react-router-dom", () => {
-  return {
-    useLocation: () => ({
-      pathname: `gene/${gene}`,
-    }),
-  }
-})
 
 describe("components/SummaryLoader", () => {
   it("should render skeleton loader", () => {
+    useRouter.mockImplementation(() => ({
+      query: { id: gene },
+    }))
     render(<SummaryLoader />)
     expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
   })
