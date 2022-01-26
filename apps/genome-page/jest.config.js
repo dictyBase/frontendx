36b7fe2e2f1a6e8cec8df301160276176b6849bc
Sean Kim <seank4@uic.edu>
2022-01-26 11:32:35 -0600

    fix: remove duplicate jest configs

--- apps/genome-page/jest.config.js
+++ apps/genome-page/jest.config.js
@@ -18,6 +18,7 @@ const customJestConfig = {
     "**/components/**/*.{ts,tsx}",
     "**/common/hooks/*.{ts,tsx}",
   ],
+  resetMocks: false,
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
