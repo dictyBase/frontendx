5d2d33256194016ef2f9cc5afbffe017304cc0e0
Sean Kim <seank4@uic.edu>
2022-01-27 11:34:15 -0600

    refactor: convert next.config.js to esm

---
+++ apps/genome-page/next.config.mjs
@@ -0,0 +1,10 @@
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  basePath: base,
+}
+
+export default nextConfig
