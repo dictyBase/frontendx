e90626c2514babb58a6add7f1c0557446b29d30f
Sean Kim <seank4@uic.edu>
2022-01-31 12:16:15 -0600

    feat: add About page

---
+++ apps/genome-page/pages/about.tsx
@@ -0,0 +1,8 @@
+import About from "../components/features/About/About"
+
+/* This page allows us to create custom 404 errors */
+const AboutPage = () => {
+  return <About />
+}
+
+export default AboutPage
