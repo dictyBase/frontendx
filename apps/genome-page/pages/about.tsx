1cf462f8450e324626063aa86939d8969a8e4279
Sean Kim <thesean.kim33@gmail.com>
2022-02-01 13:38:21 -0600

    refactor: remove About page

--- apps/genome-page/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import About from "../components/features/About/About"
-
-/* This page allows us to create custom 404 errors */
-const AboutPage = () => {
-  return <About />
-}
-
-export default AboutPage
