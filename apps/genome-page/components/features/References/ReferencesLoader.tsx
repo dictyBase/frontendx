8e9987afdcf7c7cec3b3e98ee9161582904d7dc0
Sean Kim <thesean.kim33@gmail.com>
2022-02-01 12:56:54 -0600

    fix: add unique key prop to each mapped item

--- apps/genome-page/components/features/References/ReferencesLoader.tsx
+++ apps/genome-page/components/features/References/ReferencesLoader.tsx
@@ -3,8 +3,8 @@ import { Box, Skeleton } from "@mui/material"
 const ReferencesLoader = () => {
   return (
     <Box mt="10px" data-testid="skeleton-loader">
-      {[...Array(10)].map((i) => (
-        <Skeleton animation="wave" key={i} />
+      {[...Array(10)].map((item, key) => (
+        <Skeleton animation="wave" key={key} />
       ))}
     </Box>
   )
