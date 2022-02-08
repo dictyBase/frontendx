b2bcfa5ebb735efef7893c01e4723546e3fd7cfa
Sean Kim <thesean.kim33@gmail.com>
2022-02-08 11:55:17 -0600

    feat: Created first draft of references panel

---
+++ apps/genome-page/components/features/Summary/Panels/ReferencesPanel.tsx
@@ -0,0 +1,71 @@
+import { GeneQuery } from "dicty-graphql-schema"
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@material-ui/core"
+import useStyles from "../../../../styles/dataTableStyles"
+import PublicationRow from "components/features/References/PublicationRow"
+import OtherError from "components/errors/OtherError"
+import { commaSeparateWithAnd } from "common/utils/strings"
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import ArticleIcon from '@mui/icons-material/Article';
+import NoteIcon from '@mui/icons-material/Note';
+
+
+type Props = {
+  /** Array of GO annotations for a particular gene */
+  gene: GeneQuery
+}
+
+/**
+ * Panel to display Gene Ontology Annotations on the Gene Summary page.
+ */
+const ReferencesPanel = ({ gene }: Props) => {
+  const classes = useStyles()
+
+  if (!gene.allPublications) return <OtherError />
+  const publications = gene.allPublications.slice(0,3)
+
+  
+
+  return (
+    <TableContainer component={Paper} className={classes.root}>
+      <Table aria-label="summary-references-table">
+        <TableBody>
+          {publications.map((publication, i) => (
+            <TableRow key={i}>
+              <TableCell>
+                <b>
+                  {commaSeparateWithAnd(publication.authors.map((a) => a.last_name))}
+                </b>
+                &nbsp; &apos;{publication.title}&apos; &nbsp;
+                <i>{publication.journal}</i>
+                &nbsp;
+                {publication.pages}
+              </TableCell>
+
+              <TableCell>
+                <a href="https://www.google.com/?client=safari">
+                  <NewspaperIcon style={{ margin: "0px 5px 5px 0px" }} />
+                </a>
+                <a href="">
+                  <ArticleIcon style={{ margin: "0px 5px 5px 0px" }} />
+                </a>
+                <a href="">
+                  <NoteIcon style={{ margin: "0px 5px 5px 0px" }} />
+                </a>
+              </TableCell>
+          </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default ReferencesPanel
