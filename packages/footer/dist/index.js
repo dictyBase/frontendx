7cacc42e3776d785a153cb77d92ea8150c2ebfa7
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-12 10:07:27 -0600

    feat: export footer data and styles

--- packages/footer/dist/index.js
+++ packages/footer/dist/index.js
@@ -46,7 +46,9 @@ var __toCommonJS = /* @__PURE__ */ ((cache) => {
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
-  Footer: () => Footer
+  Footer: () => Footer,
+  footerData: () => footerData,
+  footerStyles: () => footerStyles
 });
 
 // react-shim.js
@@ -206,8 +208,50 @@ var Footer = ({ theme, links }) => {
     links
   }));
 };
+
+// src/data/footerData.ts
+var footerData = [
+  {
+    url: "/research/techniques",
+    description: "Techniques"
+  },
+  {
+    url: "/research/teach",
+    description: "Teaching Protocols"
+  },
+  {
+    url: "/stockcenter",
+    description: "Dicty Stock Center"
+  },
+  {
+    url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+    description: "Genome Browser"
+  },
+  {
+    url: "/dictyaccess",
+    description: "DictyAccess"
+  },
+  {
+    url: "/community/conference",
+    description: "Conference"
+  },
+  {
+    url: "/community/labs",
+    description: "Labs"
+  },
+  {
+    url: "/about",
+    description: "About"
+  },
+  {
+    url: "/stockcenter/contact",
+    description: "Contact"
+  }
+];
 module.exports = __toCommonJS(src_exports);
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
-  Footer
+  Footer,
+  footerData,
+  footerStyles
 });
--- packages/footer/src/index.tsx
+++ packages/footer/src/index.tsx
@@ -1,2 +1,4 @@
 export * from "footer/src/components/Footer"
 export * from "footer/src/types"
+export * from "footer/src/data/footerData"
+export * from "footer/src/styles/footerStyles"
