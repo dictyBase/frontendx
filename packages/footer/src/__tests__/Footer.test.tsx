e86f5e28fbf3097afd561c47401c179362f2a23a
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-02-12 09:50:55 -0600

    test: add unit tests for footer

--- packages/footer/src/__tests__/Footer.test.tsx
+++ packages/footer/src/__tests__/Footer.test.tsx
@@ -1,3 +1,30 @@
 import { render, screen } from "@testing-library/react"
+import { Footer, FooterItem } from "footer"
 
-describe("Footer", () => {})
+describe("Footer", () => {
+  test("render footer with no links", () => {
+    render(<Footer links={[]} />)
+
+    expect(screen.getByText("Dicty Community Resource")).not.toBeNull()
+    expect(screen.getByText(/Supported by/)).not.toBeNull()
+    expect(screen.getByText("NIH")).not.toBeNull()
+    expect(screen.getByText("NIGMS")).not.toBeNull()
+  })
+
+  test("render footer with links", () => {
+    // Generate links
+    const links: Array<FooterItem> = []
+    for (let i = 0; i < 5; ++i) {
+      links.push({ url: `/link${i + 1}`, description: `Link ${i + 1}` })
+    }
+    render(<Footer links={links} />)
+
+    expect(screen.getByText("Dicty Community Resource")).not.toBeNull()
+    expect(screen.getByText(/Supported by/)).not.toBeNull()
+
+    // Check if generate links are found in the document
+    links.forEach((item) =>
+      expect(screen.getByText(item.description)).not.toBeNull(),
+    )
+  })
+})
