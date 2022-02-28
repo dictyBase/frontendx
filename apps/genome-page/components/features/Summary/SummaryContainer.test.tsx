63689beac2a48e1d030f1a9568874e2040c67256
Sean Kim <thesean.kim33@gmail.com>
2022-02-27 21:43:40 -0600

    feat: update test cases for general information and cypress

--- apps/genome-page/components/features/Summary/SummaryContainer.test.tsx
+++ apps/genome-page/components/features/Summary/SummaryContainer.test.tsx
@@ -20,14 +20,26 @@ describe("features/Summary/SummaryContainer", () => {
     expect(screen.getByText(/Cellular Component/)).toBeInTheDocument()
 
     // Render Latest References
-    expect(screen.getByText(/Kamprad, Witt, Schroder, Kreis, Baumchen, Janshoff & Tarantola/)).toBeInTheDocument()
-    expect(screen.getByText(/Tarantola, Bae, Fuller, Bodenschatz, Rappel & Loomis/)).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        /Kamprad, Witt, Schroder, Kreis, Baumchen, Janshoff & Tarantola/,
+      ),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(/Tarantola, Bae, Fuller, Bodenschatz, Rappel & Loomis/),
+    ).toBeInTheDocument()
     expect(screen.getByText(/Wu & Janetopoulos/)).toBeInTheDocument()
 
     // Render Gene Product Information
-    expect(screen.getByText(/Derived from gene prediction. Supported by mRNA./)).toBeInTheDocument()
+    expect(
+      screen.getByText(/Derived from gene prediction. Supported by mRNA./),
+    ).toBeInTheDocument()
     expect(screen.getByText(/1,148 aa/)).toBeInTheDocument()
     expect(screen.getByText(/129,527.5 Da/)).toBeInTheDocument()
 
+    // Render General Information
+    expect(screen.getByText(/Gene Name/)).toBeInTheDocument()
+    expect(screen.getByText(/Name Description/)).toBeInTheDocument()
+    expect(screen.getByText(/DG1117, amiA, pia, rictor/)).toBeInTheDocument()
   })
 })
