01fab8ac1b81e777c180c8e918118ad2698fe124
Sean Kim <thesean.kim33@gmail.com>
2022-02-27 21:39:55 -0600

    feat: add GeneralInfoPanel

--- apps/genome-page/components/features/Summary/Panels/GeneralInfoPanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/GeneralInfoPanel.tsx
@@ -1,9 +1,10 @@
-import { GeneQuery } from 'dicty-graphql-schema';
+import React from "react"
+import { GeneQuery } from "dicty-graphql-schema"
 import OtherError from "components/errors/OtherError"
 import LeftDisplay from "components/panels/LeftDisplay"
 import ItemDisplay from "components/panels/ItemDisplay"
 import RightDisplay from "components/panels/RightDisplay"
-import React from 'react';
+import { commaSeparate } from "common/utils/strings"
 
 type Props = {
   /** Array of GO annotations for a particular gene */
@@ -14,67 +15,62 @@ type Props = {
  * Panel to display Product Info on the Gene Summary page.
  */
 const GeneralInfoPanel = ({ gene }: Props) => {
-
-  if (!gene.generalInformation) return <OtherError />
+  if (!gene.generalInformation?.general_info) return <OtherError />
+  const gen_info = gene.generalInformation.general_info
 
   return (
     <div>
       <ItemDisplay>
         <LeftDisplay>Gene Name</LeftDisplay>
-        <RightDisplay>
-        {gene.gene?.name}
-        </RightDisplay>
+        <RightDisplay>{gene.gene?.name}</RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>Name Description</LeftDisplay>
         <RightDisplay>
-        {gene.generalInformation.name_description.map((desc, i) => ( 
-          <React.Fragment key={i}>
-            {desc}
-            <br />
-          </React.Fragment>
-        ))}
-        </RightDisplay>
-      </ItemDisplay>
-      <ItemDisplay>
-        <LeftDisplay>Alternative Gene Names</LeftDisplay>
-        <RightDisplay>
-          {gene.generalInformation.alt_gene_name.map((name, i) => ( 
+          {gen_info.name_description?.map((desc, i) => (
             <React.Fragment key={i}>
-              <i>{name}</i>
+              {desc}
               <br />
             </React.Fragment>
           ))}
         </RightDisplay>
       </ItemDisplay>
+      {gen_info.alt_gene_name ? (
+        <ItemDisplay>
+          <LeftDisplay>Alternative Gene Names</LeftDisplay>
+          <RightDisplay>
+                <i>{commaSeparate(gen_info.alt_gene_name)}</i>
+          </RightDisplay>
+        </ItemDisplay>
+      ) : (
+        <></>
+      )}
       <ItemDisplay>
         <LeftDisplay>Gene ID</LeftDisplay>
-        <RightDisplay>
-          {gene.gene?.id}
-        </RightDisplay>
+        <RightDisplay>{gene.gene?.id}</RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>Gene Product</LeftDisplay>
-        <RightDisplay>
-          {gene.generalInformation.gene_product}
-        </RightDisplay>
+        <RightDisplay>{gen_info.gene_product}</RightDisplay>
       </ItemDisplay>
-      <ItemDisplay>
-        <LeftDisplay>Alternative Protein Names</LeftDisplay>
-        <RightDisplay>
-          {gene.generalInformation.alt_protein_names.map((name, i) => ( 
-            <React.Fragment key={i}>
-              {name}
-              <br />
-            </React.Fragment>
-          ))}
-        </RightDisplay>
-      </ItemDisplay>
+      {gen_info.alt_protein_names ? (
+        <ItemDisplay>
+          <LeftDisplay>Alternative Protein Names</LeftDisplay>
+          <RightDisplay>
+            {gen_info.alt_protein_names.map((name, i) => (
+              <React.Fragment key={i}>
+                {name}
+                <br />
+              </React.Fragment>
+            ))}
+          </RightDisplay>
+        </ItemDisplay>
+      ) : (
+        <></>
+      )}
       <ItemDisplay>
         <LeftDisplay>Description</LeftDisplay>
-        <RightDisplay>
-          {gene.generalInformation.description}
-        </RightDisplay>
+        <RightDisplay>{gen_info.description}</RightDisplay>
       </ItemDisplay>
     </div>
   )
