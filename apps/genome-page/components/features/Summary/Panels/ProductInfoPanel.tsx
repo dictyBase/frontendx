259925f4afb0c2be45ed58588d302312bfad0eef
Sean Kim <thesean.kim33@gmail.com>
2022-02-27 21:39:27 -0600

    fix: format ProductInfoPanel

--- apps/genome-page/components/features/Summary/Panels/ProductInfoPanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/ProductInfoPanel.tsx
@@ -1,11 +1,10 @@
-import { GeneQuery } from 'dicty-graphql-schema';
+import { GeneQuery } from "dicty-graphql-schema"
 import OtherError from "components/errors/OtherError"
 import Image from "next/image"
 import LeftDisplay from "components/panels/LeftDisplay"
 import ItemDisplay from "components/panels/ItemDisplay"
 import RightDisplay from "components/panels/RightDisplay"
-import TableDisplay from '../../../panels/GenomicCoordsTable';
-
+import TableDisplay from "../../../panels/GenomicCoordsTable"
 
 type Props = {
   /** Array of GO annotations for a particular gene */
@@ -16,7 +15,6 @@ type Props = {
  * Panel to display Product Info on the Gene Summary page.
  */
 const ProductInfoPanel = ({ gene }: Props) => {
-
   if (!gene.listGeneProductInfo?.product_info) return <OtherError />
   const productInfo = gene.listGeneProductInfo.product_info[0]
 
@@ -25,35 +23,33 @@ const ProductInfoPanel = ({ gene }: Props) => {
       <ItemDisplay>
         <LeftDisplay>Protein Coding Gene</LeftDisplay>
         <RightDisplay>
-        {productInfo.protein_coding_gene}
-        <Image
-                    src="/icon_yes_green.png"
-                    alt="Yes Icon"
-                    width={30}
-                    height={30}
-                  />
-            {"(Curator reviewed)"}
-            <br/>
-            {"Derived from gene prediction. Supported by mRNA."}
+          {productInfo.protein_coding_gene}
+          <Image
+            src="/icon_yes_green.png"
+            alt="Yes Icon"
+            width={30}
+            height={30}
+          />
+          {"(Curator reviewed)"}
+          <br />
+          {"Derived from gene prediction. Supported by mRNA."}
         </RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>Protein Length</LeftDisplay>
-          <RightDisplay>
-            {productInfo.protein_length}
-          </RightDisplay>
+        <RightDisplay>{productInfo.protein_length}</RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>Molecular Weight</LeftDisplay>
-          <RightDisplay>
-            {productInfo.protein_molecular_weight}
-          </RightDisplay>
+        <RightDisplay>{productInfo.protein_molecular_weight}</RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>More Protein Data</LeftDisplay>
-          <RightDisplay>
-            <a href={productInfo.more_protein_data}>Protein sequence, domains and much more...</a>
-          </RightDisplay>
+        <RightDisplay>
+          <a href={productInfo.more_protein_data}>
+            Protein sequence, domains and much more...
+          </a>
+        </RightDisplay>
       </ItemDisplay>
       <ItemDisplay>
         <LeftDisplay>Genomic Coords.</LeftDisplay>
