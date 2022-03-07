403cb99b0a4d497ca9efa07d2a8cabb76b6dbc55
Sean Kim <thesean.kim33@gmail.com>
2022-03-07 01:53:43 -0600

    feat: add PiaA links mock data and update sadA links mock data

--- apps/genome-page/mocks/mockLinksData.ts
+++ apps/genome-page/mocks/mockLinksData.ts
@@ -3,42 +3,30 @@ import { Links, NameWithLink } from "dicty-graphql-schema"
 const mockLinksData: Links = {
   expression: [
     {
-      name: "dictyExpress (microarray)",
-      link: "https://dictyexpress.research.bcm.edu/bcm/#/all",
-    },
-    {
       name: "dictyExpress (RNA-Seq)",
-      link: "https://ailab.si/dictyexpress/run/index.php?gene=DDB_G0277399&db=rnaseq",
+      link: "https://ailab.si/dictyexpress/run/index.php?gene=DDB_G0288511&db=rnaseq",
     },
   ] as NameWithLink[],
   colleagues: {
-    name: "piaA Researchers",
-    link: "http://dictybase.org/db/cgi-bin/dictyBase/colleague/colleagueSearch?locus=179785",
+    name: "sadA Researchers",
+    link: "http://dictybase.org/db/cgi-bin/dictyBase/colleague/colleagueSearch?locus=207301",
   } as NameWithLink,
   ext_resources: [
     {
-      name: "STKE",
-      link: "https://www.science.org/cgi/cm/stkecm;CMC_11286",
-    },
-    {
-      name: "Inparanoid",
-      link: "https://inparanoid.sbc.su.se/cgi-bin/gene_search.cgi?id=DDB_G0277399",
-    },
-    {
       name: "GenBank Protein",
-      link: "https://www.ncbi.nlm.nih.gov/protein/EAL68662.1",
+      link: "http://www.ncbi.nlm.nih.gov/protein/EAL63177.1",
     },
     {
-      name: "UniProtKB: O77203",
-      link: "https://www.uniprot.org/uniprot/O77203",
+      name: "UniProtKB: Q8I7T3",
+      link: "http://www.uniprot.org/entry/?query=Q8I7T3",
     },
     {
       name: "Ensembl",
-      link: "http://protists.ensembl.org/Dictyostelium_discoideum/Gene/Summary?db=core;g=DDB_G0277399;r=2:8027686-8031387;t=EAL68662",
+      link: "http://protists.ensembl.org/Dictyostelium_discoideum/Gene/Summary?db=core;g=DDB_G0288511",
     },
     {
       name: "ENA",
-      link: "https://www.ebi.ac.uk/ena/browser/view/EAL68662.1",
+      link: "http://www.ebi.ac.uk/ena/data/view/EAL63177.1",
     },
   ] as NameWithLink[],
 }
