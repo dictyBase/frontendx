762f060c4048b0d89441e7bd55d45576ff4431dd
Sean Kim <thesean.kim33@gmail.com>
2022-03-07 02:19:38 -0600

    feat: add mockProductInfo for PiaA and sadA

--- apps/genome-page/mocks/mockProductInfo.ts
+++ apps/genome-page/mocks/mockProductInfo.ts
@@ -1,18 +1,33 @@
-import { ProductInformation, GenomicCoordinates } from 'dicty-graphql-schema';
+import { ProductInformation, GenomicCoordinates } from "dicty-graphql-schema"
 
 const mockProductInfo: ProductInformation[] = [
   {
-    protein_coding_gene: "DDB0185055",
-    protein_length: "1,148 aa",
-    protein_molecular_weight: "129,527.5 Da",
-    more_protein_data: "/gene/DDB_G0277399/protein/DDB0185055",
+    protein_coding_gene: {
+      name: "DDB0191090",
+      link: "http://dictybase.org/gene/DDB_G0288511/feature/DDB0191090",
+    },
+    protein_length: "952 aa",
+    protein_molecular_weight: "104,673.8 Da",
+    more_protein_data:
+      "http://dictybase.org/gene/DDB_G0288511/protein/DDB0191090",
     genomic_coords: [
-      {exon: "3", local_coords: "964 - 1203", chrom_coords: "8028649 - 8028888"} as GenomicCoordinates,
-      {exon: "2", local_coords: "388 - 871", chrom_coords: "8028073 - 8028556"} as GenomicCoordinates,
-      {exon: "4", local_coords: "1273 - 3702", chrom_coords: "8028958 - 8031387"} as GenomicCoordinates,
-      {exon: "1", local_coords: "1 - 293", chrom_coords: "8027686 - 8027978"} as GenomicCoordinates,
-    ]
-  }
+      {
+        exon: "1",
+        local_coords: "1 - 626",
+        chrom_coords: "1641837 - 1641212",
+      } as GenomicCoordinates,
+      {
+        exon: "2",
+        local_coords: "705 - 1291",
+        chrom_coords: "1641133 - 1640547",
+      } as GenomicCoordinates,
+      {
+        exon: "3",
+        local_coords: "1393 - 3038",
+        chrom_coords: "1640445 - 1638800",
+      } as GenomicCoordinates,
+    ],
+  },
 ]
 
 export default mockProductInfo
