b69fb45e1d09bb31f2d45fad4232dfc90e3751cc
Sean Kim <thesean.kim33@gmail.com>
2022-03-07 11:12:46 -0600

    fix: update test cases

--- apps/genome-page/components/panels/GenomicCoordsTable.test.tsx
+++ apps/genome-page/components/panels/GenomicCoordsTable.test.tsx
@@ -1,23 +1,20 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import GenomicCoordsTable from "./GenomicCoordsTable"
-import mockProductInfo from '../../mocks/mockProductInfo';
+import mockProductInfo from "../../mocks/mockProductInfo"
 
 describe("components/panels/GenomicCoordsTable", () => {
-
   const product_info = [...mockProductInfo]
 
   it("should render children", () => {
-    render(
-        <GenomicCoordsTable data={product_info[0].genomic_coords}/>
-    )
+    render(<GenomicCoordsTable data={product_info[0].genomic_coords} />)
 
     expect(screen.getByText(/Exon/)).toBeInTheDocument()
     expect(screen.getByText(/Local Coords./)).toBeInTheDocument()
     expect(screen.getByText(/Chrom. Coords./)).toBeInTheDocument()
 
     /* First row data */
-    expect(screen.getByText(/964 - 1203/)).toBeInTheDocument()
-    expect(screen.getByText(/8028649 - 8028888/)).toBeInTheDocument()
+    expect(screen.getByText(/1 - 626/)).toBeInTheDocument()
+    expect(screen.getByText(/1641837 - 1641212/)).toBeInTheDocument()
   })
 })
