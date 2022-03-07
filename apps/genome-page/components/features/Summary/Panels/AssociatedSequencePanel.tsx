daca9179c208078dde2de6d74abe37456f8db09e
Sean Kim <thesean.kim33@gmail.com>
2022-03-07 02:05:39 -0600

    feat: Update AssociatedSequence mock data for PiaA and sadA

--- apps/genome-page/components/features/Summary/Panels/AssociatedSequencePanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/AssociatedSequencePanel.tsx
@@ -54,7 +54,7 @@ const AssociatedSequencePanel = ({ data }: Props) => {
           ) : (
             <></>
           )}
-          <a href="">more..</a>
+          <a href={associated_seq.associated_sequences.more_link}>more..</a>
         </RightDisplay>
       </ItemDisplay>
     </div>
