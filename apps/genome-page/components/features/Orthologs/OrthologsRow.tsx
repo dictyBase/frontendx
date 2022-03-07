cba6bb649dcfd3d4a2fd594ffec8358adbe4a182
Sean Kim <thesean.kim33@gmail.com>
2022-03-07 02:39:51 -0600

    feat: update to take NameWithLink format

--- apps/genome-page/components/features/Orthologs/OrthologsRow.tsx
+++ apps/genome-page/components/features/Orthologs/OrthologsRow.tsx
@@ -1,61 +1,51 @@
-import {
-    TableRow,
-    TableCell,
-    Box,
-  } from "@material-ui/core"
-  import { commaSeparate } from "common/utils/strings"
-  
-  interface OrthologsRowProps {
-    __typename?: "Orthologs"
-    id: string
-    species: string
-    uniprotkb: string
-    gene_product: string
-    source?: Array<string> | null | undefined;
-  }
-  
-  const PhenotypeRow = ({
-    id,
-    species,
-    uniprotkb,
-    gene_product,
-    source,
-  }: OrthologsRowProps) => {
+import { TableRow, TableCell, Box } from "@material-ui/core"
+import { commaSeparate } from "common/utils/strings"
 
-    return (
-      <TableRow>
-        <TableCell>
-          {species ? (
-            <>
-              <Box>
-               {species}
-              </Box>
-            </>
-          ) : (
-            <></>
-          )}
-        </TableCell>
-        <TableCell>
-            {id ? (
-                <>
-                <Box>
-                    <a href={`/gene/${id}`}>{id}</a>
-                </Box>
-                </>
-            ) : (
-                <></>
-            )}
-        </TableCell>
-        <TableCell>
-          {uniprotkb}
-        </TableCell>
-        <TableCell>{gene_product}</TableCell>
-        <TableCell>
-          {source ? commaSeparate(source) : ""}
-        </TableCell>
-      </TableRow>
-    )
-  }
-  
-  export default PhenotypeRow
-  
+interface OrthologsRowProps {
+  __typename?: "Orthologs"
+  id: string
+  species: string
+  uniprotkb: string
+  gene_product: string
+  source?: Array<string> | null | undefined
+}
+
+const PhenotypeRow = ({
+  id,
+  species,
+  uniprotkb,
+  gene_product,
+  source,
+}: OrthologsRowProps) => {
+  return (
+    <TableRow>
+      <TableCell>
+        {species ? (
+          <>
+            <Box>{species}</Box>
+          </>
+        ) : (
+          <></>
+        )}
+      </TableCell>
+      <TableCell>
+        {id ? (
+          <>
+            <Box>
+              <a href={id.link}>{id.name}</a>
+            </Box>
+          </>
+        ) : (
+          <></>
+        )}
+      </TableCell>
+      <TableCell>
+        <a href={uniprotkb.link}>{uniprotkb.name}</a>
+      </TableCell>
+      <TableCell>{gene_product}</TableCell>
+      <TableCell>{source ? commaSeparate(source) : ""}</TableCell>
+    </TableRow>
+  )
+}
+
+export default PhenotypeRow
