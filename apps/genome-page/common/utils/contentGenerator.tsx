c6c61685382a20296e81aba5e6feb76cf7fce424
Sean Kim <thesean.kim33@gmail.com>
2022-03-22 13:15:51 -0500

    feat: Create outline of contentGenerator

---
+++ apps/genome-page/common/utils/contentGenerator.tsx
@@ -0,0 +1,87 @@
+import AssociatedSequencePanel from "components/features/Summary/Panels/AssociatedSequencePanel"
+import GeneralInfoPanel from "components/features/Summary/Panels/GeneralInfoPanel"
+import ProductInfoPanel from "components/features/Summary/Panels/ProductInfoPanel"
+import PanelWrapper from "components/panels/PanelWrapper"
+import { Content, GeneQuery, Gene } from "dicty-graphql-schema"
+import { Component } from "react"
+
+const SummaryContainerContent = {
+  generalInformation: {
+    id: "generalInformation",
+    title: "General Information",
+    route: "/gene/${geneId}/",
+  },
+  gene: {
+    id: "gene",
+    title: "Latest Gene Ontology Annotations",
+    route: "/gene/${geneId}/goannotations",
+  },
+  listGeneProductInfo: {
+    id: "listGeneProductInfo",
+    title: "Gene Product Information",
+    route: "/gene/${geneId}/",
+  },
+  getAssociatedSequnces: {
+    id: "getAssociatedSequnces",
+    title: "Associated Sequences",
+    route: "/gene/${geneId}/",
+  },
+  getLinks: {
+    id: "getLinks",
+    title: "Links",
+    route: "/gene/${geneId}/",
+  },
+  allPublications: {
+    id: "allPublications",
+    title:
+      "Latest References (press references tab to view all ${gene.allPublications.num_pubs} papers)",
+    route: "`/gene/${geneId}/references`",
+  },
+}
+
+const returnComponentByName = (id: string, gene: GeneQuery) => {
+  if (id === "generalInformation") {
+    return <GeneralInfoPanel gene={gene}></GeneralInfoPanel>
+  }
+}
+
+const convertStringToTemplateString = (target: string, gene: GeneQuery) => {
+  /* Not efficient 
+    I tried the regex but I had issue with converting a string to a string literal
+  */
+  if (target === "geneId")
+    return target.replace("$(geneId}", `${gene.gene?.name}`)
+  if (target === "allPublications")
+    return target.replace(
+      "${gene.allPublications.num_pubs",
+      `${gene.allPublications.num_pubs}`,
+    )
+}
+
+/* 
+  This function will take in props and return the container items
+*/
+const contentGenerator = (req: string[], gene: GeneQuery) => {
+  let returnArray: any[] = []
+  console.log(gene)
+  req.forEach((element) => {
+    if (gene[element]) {
+      let entry = {
+        props: SummaryContainerContent[element],
+        component: returnComponentByName(
+          SummaryContainerContent[element].id,
+          gene,
+        ),
+      }
+      returnArray.push(entry)
+    }
+  })
+
+  return returnArray
+}
+
+export {
+  contentGenerator,
+  convertStringToTemplateString,
+  returnComponentByName,
+}
