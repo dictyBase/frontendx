e09c7f53717776407761dd92e148d70032b12485
Sean Kim <thesean.kim33@gmail.com>
2022-05-04 18:15:51 -0500

    feat: add Container to place markdown on

--- apps/genome-page/components/features/CommunityAnnotations/WikiContainer.tsx
+++ apps/genome-page/components/features/CommunityAnnotations/WikiContainer.tsx
@@ -1,7 +1,8 @@
-import Typography from "@material-ui/core/Typography"
 import Layout from "components/layout/Layout"
 import { useRouter } from "next/router"
 import ReactMarkDown from "react-markdown"
+import { Paper, Container, Typography } from "@material-ui/core"
+import useStyles from "../../../styles/dataTableStyles"
 /**
  * Container component that issues a GraphQL query to get gene data for the
  * GO annotations page.
@@ -11,6 +12,8 @@ interface WikiContainerProps {
   markdown?: string
 }
 const WikiContainer = ({ markdown }: WikiContainerProps) => {
+  const classes = useStyles()
+
   const { query } = useRouter()
   const geneId = query.gene as string
 
@@ -20,11 +23,14 @@ const WikiContainer = ({ markdown }: WikiContainerProps) => {
       title={`Community Annotations for ${geneId}`}
       description={`Community Annotations for ${geneId}`}>
       <Typography component="div">
-        {markdown ? (
-          <ReactMarkDown>{markdown}</ReactMarkDown>
-        ) : (
-          `${geneId} doesn't have a community annotation. Be the first to contribute`
-        )}
+        {" "}
+        <Container component={Paper} className={classes.root} maxWidth={false}>
+          {markdown ? (
+            <ReactMarkDown>{markdown}</ReactMarkDown>
+          ) : (
+            `${geneId} doesn't have a community annotation. Be the first to contribute`
+          )}
+        </Container>
       </Typography>
     </Layout>
   )
