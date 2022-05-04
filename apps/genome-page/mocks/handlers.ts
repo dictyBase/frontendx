25cf046ef554c85a67cecd6b9b7daac5ed5265a1
Sean Kim <thesean.kim33@gmail.com>
2022-05-04 17:49:52 -0500

    feat: add mockGeneLimit and add the path to the msw handler

--- apps/genome-page/mocks/handlers.ts
+++ apps/genome-page/mocks/handlers.ts
@@ -3,6 +3,8 @@ import mockGene from "./mockGene"
 import mockGeneLimit from "./mockGeneLimit"
 import mockGenePiaA from "./piaAMocks/mockGenePiaA"
 import mockGenePiaALimit from "./piaAMocks/mockGenePiaALimit"
+import mockGeneAda2 from "./ada2Mocks/mockGeneAda2"
+import mockGeneAda2Limit from "./ada2Mocks/mockGeneAda2Limit"
 
 export const handlers = [
   // Handles the Gene query: https://github.com/dictyBase/dicty-graphql-schema/blob/develop/src/queries/gene.graphql
@@ -22,6 +24,12 @@ export const handlers = [
         return res(ctx.data({ ...mockGenePiaALimit }))
       }
       return res(ctx.data({ ...mockGenePiaA }))
+    }
+    if (gene === "ada2") {
+      if (limit == 3) {
+        return res(ctx.data({ ...mockGeneAda2Limit }))
+      }
+      return res(ctx.data({ ...mockGeneAda2 }))
     }
   }),
 ]
