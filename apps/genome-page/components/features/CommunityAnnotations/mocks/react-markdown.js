bce7c234ff3bd0c315025a9c5695799421ad165c
Sean Kim <thesean.kim33@gmail.com>
2022-05-09 00:30:27 -0500

    feat: Add test case for WikiContainer

---
+++ apps/genome-page/components/features/CommunityAnnotations/mocks/react-markdown.js
@@ -0,0 +1,8 @@
+// https://github.com/remarkjs/react-markdown/issues/635#issuecomment-1080109162
+import React from "react"
+
+function ReactMarkdown({ children }) {
+  return <>{children}</>
+}
+
+export default ReactMarkdown
