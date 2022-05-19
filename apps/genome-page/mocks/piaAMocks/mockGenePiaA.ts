e69789da39c9e137020fa5383a022472634773ad
Sean Kim <thesean.kim33@gmail.com>
2022-05-19 11:01:48 -0500

    feat: add mock ProteinInformation

--- apps/genome-page/mocks/piaAMocks/mockGenePiaA.ts
+++ apps/genome-page/mocks/piaAMocks/mockGenePiaA.ts
@@ -7,6 +7,7 @@ import mockOntologyPiaA from "./mockOntologyPiaA"
 import mockOrthologsPiaA from "./mockOrthologsPiaA"
 import mockReferencesPiaA from "./mockReferencesPiaA"
 import mockPhenotypesPiaA from "./mockPhenotypesPiaA"
+import mockProteinInformation from "mocks/mockProteinInformation"
 
 const mockGenePiaA: GeneQuery = {
   gene: {
@@ -50,6 +51,11 @@ const mockGenePiaA: GeneQuery = {
     id: "DDB_G0277399",
     name: "piaA",
     links: mockLinksPiaA,
+  },
+  getProteinInformation: {
+    id: "DDB_G0277399",
+    name: "piaA",
+    protein_information: mockProteinInformation,
   },
 }
 export default mockGenePiaA
