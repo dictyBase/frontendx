ed8ccc0def8ecef75640f95080dcf3a98a6d7d10
Sean Kim <thesean.kim33@gmail.com>
2022-05-23 03:21:30 -0500

    feat: move Query and Gene to GeneRow along with finishing Gene

--- apps/genome-page/components/features/Blast/BlastContainer.tsx
+++ apps/genome-page/components/features/Blast/BlastContainer.tsx
@@ -2,14 +2,8 @@ import Layout from "components/layout/Layout"
 import { GeneQuery } from "dicty-graphql-schema"
 import { useRouter } from "next/router"
 import useStyles from "../../../styles/blastContainerStyles"
-import {
-  Paper,
-  Container,
-  Typography,
-  Card,
-  TextField,
-  Box,
-} from "@material-ui/core"
+import { Paper, Container } from "@material-ui/core"
+import QueryOrGeneRow from "./Sections/QueryOrGeneRow"
 
 interface BlastContainerProps {
   gene: GeneQuery
@@ -25,31 +19,9 @@ const BlastContainer = ({ gene }: BlastContainerProps) => {
       title={`Phenotypes for ${geneId}`}
       description={`Gene phenotypes for ${geneId}`}>
       <Container component={Paper} className={classes.root} maxWidth={false}>
-        <div className={classes.row}>
-          <Box className={classes.titleBox}>
-            <Typography className={classes.titleBoxText}>
-              Query Sequence
-            </Typography>
-          </Box>
-          <Typography className={classes.titleBoxText}>Or</Typography>
-          <Box className={classes.titleBox}>
-            <Typography className={classes.titleBoxText}>
-              Gene or Gene Model ID
-            </Typography>
-          </Box>
-        </div>
-        <Box sx={{ maxWidth: 500, minHeight: 400 }}>
-          <Card variant="outlined" className={classes.card}>
-            <Typography className={classes.boldText}>
-              Enter query sequence in FASTA format
-            </Typography>
-            <textarea
-              rows={10}
-              placeholder="Type or paste a query or sequence here..."
-              className={classes.textArea}
-            />
-          </Card>
-        </Box>
+        <Container maxWidth="lg">
+          <QueryOrGeneRow />
+        </Container>
       </Container>
     </Layout>
   )
