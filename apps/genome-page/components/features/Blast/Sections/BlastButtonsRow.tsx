24b429fdc040a7cb7d9cd86f00fbf09b4b9b8287
Sean Kim <thesean.kim33@gmail.com>
2022-05-24 01:28:22 -0500

    feat: Add BlastButtonsRow

--- apps/genome-page/components/features/Blast/Sections/BlastButtonsRow.tsx
+++ apps/genome-page/components/features/Blast/Sections/BlastButtonsRow.tsx
@@ -1,37 +1,30 @@
-import useStyles from "../../../../styles/blastContainerStyles"
-import {
-  Typography,
-  Card,
-  Box,
-  TextField,
-  Button,
-  Select,
-  MenuItem,
-  FormControl,
-  InputLabel,
-} from "@material-ui/core"
+import useStyles from "styles/geneOrIDSection"
+import { Card, Button, Grid } from "@material-ui/core"
 
 const BlastButtonsRow = () => {
   const classes = useStyles()
   return (
-    <>
-      {/* Blast Program */}
-      <Box className={classes.contentBox}>
-        <Card variant="outlined" className={classes.card}>
-          <div className={classes.row}>
+    <Grid item xs={12} justifyContent="flex-start">
+      <Card variant="outlined" className={classes.card}>
+        <Grid container>
+          <Grid item xs={1}>
             <Button size="small" variant="contained">
               BLAST
             </Button>
+          </Grid>
+          <Grid item xs={1}>
             <Button size="small" variant="contained">
               Reset
             </Button>
+          </Grid>
+          <Grid item xs={2}>
             <Button size="small" variant="contained">
               BLAST at NCBI
             </Button>
-          </div>
-        </Card>
-      </Box>
-    </>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
   )
 }
 
