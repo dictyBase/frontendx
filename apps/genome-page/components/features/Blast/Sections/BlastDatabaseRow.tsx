a787b4f8f530f4d30d730e494c8df1f9b1ef9731
Sean Kim <thesean.kim33@gmail.com>
2022-05-24 01:28:31 -0500

    feat: Add BlastDatabaseRow

--- apps/genome-page/components/features/Blast/Sections/BlastDatabaseRow.tsx
+++ apps/genome-page/components/features/Blast/Sections/BlastDatabaseRow.tsx
@@ -1,68 +1,79 @@
-import useStyles from "../../../../styles/blastContainerStyles"
+import useStyles from "styles/geneOrIDSection"
 import {
   Typography,
   Card,
   Box,
   TextField,
-  Button,
-  Select,
   MenuItem,
-  FormControl,
-  InputLabel,
+  Grid,
 } from "@material-ui/core"
 
 const BlastDatabaseRow = () => {
   const classes = useStyles()
   return (
-    <>
-      {/* Blast Program */}
-      <div className={classes.col}>
-        <Box className={classes.titleBox}>
-          <Typography className={classes.titleBoxText}>
-            BLAST Database
-          </Typography>
-        </Box>
+    <Grid item xs={12} md={12}>
+      <Box className={classes.titleBox}>
+        <Typography className={classes.titleBoxText}>BLAST Database</Typography>
+      </Box>
 
-        <Box className={classes.contentBox}>
-          <Card variant="outlined" className={classes.card}>
-            <div className={classes.col}>
-              {/* Select Organism */}
-              <div className={classes.row}>
-                <Typography className={classes.boldText}>
-                  Select Organism
-                </Typography>
-                <FormControl fullWidth>
-                  <Select defaultValue={2}>
-                    <MenuItem value={0}>Please Select an Organism</MenuItem>
-                    <MenuItem value={1}>All</MenuItem>
-                    <MenuItem value={2}>Dictyostellum discoldeum</MenuItem>
-                    <MenuItem value={3}>Dictyostellum fasciculatum</MenuItem>
-                    <MenuItem value={4}>Dictyostellum purpureum</MenuItem>
-                    <MenuItem value={5}>Dictyostellum pallidum</MenuItem>
-                  </Select>
-                </FormControl>
-              </div>
-              {/* Select Database */}
-              <div className={classes.row}>
-                <Typography className={classes.boldText}>
-                  Select Database
-                </Typography>
-                <FormControl fullWidth>
-                  <Select defaultValue={2}>
-                    <MenuItem value={0}>Please Select an Organism</MenuItem>
-                    <MenuItem value={1}>All</MenuItem>
-                    <MenuItem value={2}>Dictyostellum discoldeum</MenuItem>
-                    <MenuItem value={3}>Dictyostellum fasciculatum</MenuItem>
-                    <MenuItem value={4}>Dictyostellum purpureum</MenuItem>
-                    <MenuItem value={5}>Dictyostellum pallidum</MenuItem>
-                  </Select>
-                </FormControl>
-              </div>
-            </div>
-          </Card>
-        </Box>
-      </div>
-    </>
+      <Card variant="outlined" className={classes.card}>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid item xs={3} md={3}>
+            <Typography className={classes.boldText}>
+              Select Organism
+            </Typography>
+          </Grid>
+          <Grid item xs={9} md={9}>
+            <TextField
+              size="small"
+              select
+              defaultValue={2}
+              variant="outlined"
+              InputProps={{ style: { fontSize: 12, minWidth: 400 } }}>
+              <MenuItem value={0}>Please Select an Organism</MenuItem>
+              <MenuItem value={1}>All</MenuItem>
+              <MenuItem value={2}>Dictyostellum discoldeum</MenuItem>
+              <MenuItem value={3}>Dictyostellum fasciculatum</MenuItem>
+              <MenuItem value={4}>Dictyostellum purpureum</MenuItem>
+              <MenuItem value={5}>Polysphondylium pallidum</MenuItem>
+            </TextField>
+          </Grid>
+          {/* Select Database */}
+          <Grid item xs={3} md={3}>
+            <Typography className={classes.boldText}>
+              Select Database
+            </Typography>
+          </Grid>
+          <Grid item xs={9} md={9}>
+            <TextField
+              size="small"
+              select
+              defaultValue={0}
+              variant="outlined"
+              InputProps={{ style: { fontSize: 12, minWidth: 400 } }}>
+              <MenuItem value={0}>Please Select a Database</MenuItem>
+              <MenuItem value={1}>
+                D. discoideum Protein sequences - protein
+              </MenuItem>
+              <MenuItem value={2}>
+                D. discoideum Coding sequences - DNA
+              </MenuItem>
+              <MenuItem value={3}>
+                D. discoideum Non-coding sequences - DNA
+              </MenuItem>
+              <MenuItem value={4}>
+                D. discoideum Genomic sequences - DNA
+              </MenuItem>
+              <MenuItem value={5}>D. discoideum EST sequences - DNA</MenuItem>
+              <MenuItem value={6}>
+                D. discoideum Chromosomal DNA: 1,2,3,4,5,6,M and floating
+                contigs - DNA
+              </MenuItem>
+            </TextField>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
   )
 }
 
