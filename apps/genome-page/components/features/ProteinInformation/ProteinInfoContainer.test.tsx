5798042486132a11eb705683c8fdd23800bc8bd8
Sean Kim <thesean.kim33@gmail.com>
2022-05-24 11:38:15 -0500

    feat: update ProteinContainer Test Case

--- apps/genome-page/components/features/ProteinInformation/ProteinInfoContainer.test.tsx
+++ apps/genome-page/components/features/ProteinInformation/ProteinInfoContainer.test.tsx
@@ -13,11 +13,30 @@ describe("features/ProteinInformation/ProteinInfoContainer", () => {
     }))
     render(<ProteinInfoContainer gene={mockGene as GeneQuery} />)
 
-    // wait for data to load...
+    // Tab
+    expect(screen.getByText("Protein Information")).toBeInTheDocument()
+
+    // General information
     const GeneralInfoPanel = screen.getByText(/General Information/)
     expect(GeneralInfoPanel).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        "EGF repeat-containing 9 transmembrane molecule involved in substrate adhesion",
+      ),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        "*This information was obtained from UniProt manually reviewed record",
+      ),
+    ).toBeInTheDocument()
+
+    // Links
     const LinkPanel = screen.getByText(/External Links/)
     expect(LinkPanel).toBeInTheDocument()
+    expect(screen.getByText("UniProtKB: Q8I7T3")).toBeInTheDocument()
+    expect(screen.getByText("GenBank Protein")).toBeInTheDocument()
+
+    // Protein Sequence
     const ProteinSequencePanel = screen.getAllByText(/Protein Sequence/)
     expect(ProteinSequencePanel.length).toBe(2)
   })
