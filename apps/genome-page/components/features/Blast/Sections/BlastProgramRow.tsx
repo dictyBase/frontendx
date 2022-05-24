47e5aa27ca2c34336242d5f576b091995beb587b
Sean Kim <thesean.kim33@gmail.com>
2022-05-24 01:11:44 -0500

    feat: update BlastProgramRow with Grid and Textfield

--- apps/genome-page/components/features/Blast/Sections/BlastProgramRow.tsx
+++ apps/genome-page/components/features/Blast/Sections/BlastProgramRow.tsx
@@ -1,60 +1,52 @@
-import useStyles from "../../../../styles/blastContainerStyles"
+import useStyles from "styles/geneOrIDSection"
 import {
   Typography,
   Card,
   Box,
-  TextField,
-  Button,
-  Select,
+  Grid,
   MenuItem,
-  FormControl,
-  InputLabel,
+  TextField,
 } from "@material-ui/core"
 
 const BlastProgramRow = () => {
   const classes = useStyles()
   return (
-    <>
-      {/* Blast Program */}
-      <div className={classes.col}>
-        <Box className={classes.titleBox}>
-          <Typography className={classes.titleBoxText}>
-            BLAST Program
-          </Typography>
-        </Box>
-
-        <Box className={classes.contentBox}>
-          <Card variant="outlined" className={classes.card}>
-            <div className={classes.row}>
-              <Typography className={classes.boldText}>
-                Select Program
-              </Typography>
-              <FormControl fullWidth>
-                <Select defaultValue={0}>
-                  <MenuItem value={0}>Please Select a Program</MenuItem>
-                  <MenuItem value={1}>
-                    blastn - DNA query to DNA database
-                  </MenuItem>
-                  <MenuItem value={2}>
-                    blastp - Protein query to protein database
-                  </MenuItem>
-                  <MenuItem value={3}>
-                    blastx - Translated (6 frames) DNA query to protein databse
-                  </MenuItem>
-                  <MenuItem value={4}>
-                    tblastx - Translated (6 frames) DNA query to translated (6
-                    frames) DNA database
-                  </MenuItem>
-                  <MenuItem value={5}>
-                    tblastn - Protein query to DNA (6 frames) DNA database
-                  </MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          </Card>
-        </Box>
-      </div>
-    </>
+    <Grid item xs={12} md={12}>
+      <Box className={classes.titleBox}>
+        <Typography className={classes.titleBoxText}>BLAST Program</Typography>
+      </Box>
+      <Card variant="outlined" className={classes.card}>
+        <Grid container spacing={2} justifyContent="center" alignItems="center">
+          <Grid item xs={3} md={3}>
+            <Typography className={classes.boldText}>Select Program</Typography>
+          </Grid>
+          <Grid item xs={9} md={9}>
+            <TextField
+              size="small"
+              select
+              defaultValue={0}
+              variant="outlined"
+              InputProps={{ style: { fontSize: 12, minWidth: 400 } }}>
+              <MenuItem value={0}>Please Select a Program</MenuItem>
+              <MenuItem value={1}>blastn - DNA query to DNA database</MenuItem>
+              <MenuItem value={2}>
+                blastp - Protein query to protein database
+              </MenuItem>
+              <MenuItem value={3}>
+                blastx - Translated (6 frames) DNA query to protein databse
+              </MenuItem>
+              <MenuItem value={4}>
+                tblastx - Translated (6 frames) DNA query to translated (6
+                frames) DNA database
+              </MenuItem>
+              <MenuItem value={5}>
+                tblastn - Protein query to DNA (6 frames) DNA database
+              </MenuItem>
+            </TextField>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
   )
 }
 
