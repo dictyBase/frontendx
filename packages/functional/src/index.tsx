dfb8588ffac1702a364ee7a72ba1e0c0a3164f89
Siddhartha Basu <biosidd@gmail.com>
2023-02-24 18:18:47 -0600

    refactor: use index file to export from functional package

--- packages/functional/src/index.tsx
+++ packages/functional/src/index.tsx
@@ -1,21 +1,2 @@
-import type { Monoid } from "fp-ts/Monoid"
-import { concatAll } from "fp-ts/Monoid"
-import type { ReactNode } from "react"
-
-type A = JSX.Element | ReactNode
-
-const reactMonoid: Monoid<A> = {
-  concat: (x: A, y: A) => (
-    <>
-      {x}
-      {y}
-    </>
-  ),
-  empty: <></>,
-}
-
-const compose = (...v: A[]) => {
-  return { children: concatAll(reactMonoid)(v) }
-}
-
-export default compose
+export { default as compose } from "./reactfp"
+export * from "./select-state"
