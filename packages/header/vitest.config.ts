e5bd016463c8ac572a71a9e5571ed20769aa1a0c
Siddhartha Basu <biosidd@gmail.com>
2023-03-16 08:23:57 -0500

    test: add mock reset configuration

--- packages/header/vitest.config.ts
+++ packages/header/vitest.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   test: {
     "environment": "jsdom",
     "globals": true,
-    "setupFiles": "./src/__tests__/setup.ts"
+    "setupFiles": "./src/__tests__/setup.ts",
+    "mockReset": true
   },
 })
 
