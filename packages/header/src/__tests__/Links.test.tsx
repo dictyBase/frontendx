e535251603c349d2762aa6fa3c3f109d0f037d00
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-03-29 12:06:33 -0500

    test: add tests for links component

--- packages/header/src/__tests__/Links.test.tsx
+++ packages/header/src/__tests__/Links.test.tsx
@@ -1,10 +1,18 @@
-import { render, screen } from "@testing-library/react"
-import { expect, describe } from "vitest"
+import { render } from "@testing-library/react"
+import { expect, describe, test } from "vitest"
 import Links from "../functional/Links"
+import { iconItems } from "../components/LinksContainer"
 
 describe("functional links ", () => {
   test("should generate the links ", () => {
-    render(<Links />)
-    screen.debug()
+    const { getAllByRole } = render(<Links />)
+    expect(getAllByRole("link")).toHaveLength(iconItems.length)
   })
+  test.each(iconItems)(
+    "should have link $href with title $title",
+    ({ href, title }) => {
+      const { getByRole } = render(<Links />)
+      expect(getByRole("link", { name: title })).toHaveAttribute("href", href)
+    },
+  )
 })
