1faa119169842dc103938b6ebab49b20e5974d43
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-03-29 17:23:56 -0500

    docs: add api docs for header component

--- packages/header/src/functional/Header.tsx
+++ packages/header/src/functional/Header.tsx
@@ -12,12 +12,32 @@ const boxWrapper = (children: Comp) => (
   <Box className={headerStyles().header}>{children}</Box>
 )
 
+/**
+ * Renders a header component with a logo, search bar and navigation links.
+ * @returns The rendered header component.
+ * @example
+ * ```
+ * import Header from "@dictybase/header"
+ *
+ * const App = () => (
+ *   <div>
+ *     <Header />
+ *     <p>This is the main content area</p>
+ *   </div>
+ * )
+ * ```
+ */
 const Header = () =>
   pipe(
+    // An array of JSX components
     [<Logo key={uuid4()} />, <Search key={uuid4()} />, <Links key={uuid4()} />],
+    // Converts the array of JSX components into a Option monad
     fromChildren,
+    // Composes the array to a single JSX component
     composeChildren,
+    // Returns Some(boxWrapper(JSX)) or None if JSX is empty
     Omap(boxWrapper),
+    // Extracts the wrapped JSX or returns an empty JSX element
     getOrElse(() => <></>),
   )
 
