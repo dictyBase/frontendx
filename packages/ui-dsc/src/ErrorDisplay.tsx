67f508a83d103511c240e391533faa5f6e230197
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-04 11:08:22 -0500

    fix: fix variable name linting errors

--- packages/ui-dsc/src/ErrorDisplay.tsx
+++ packages/ui-dsc/src/ErrorDisplay.tsx
@@ -3,7 +3,7 @@ import { ApolloError } from "@apollo/client"
 /**
  * The prop type for {@link ErrorDisplay}
  */
-export interface ErrorDisplayProps {
+export interface ErrorDisplayProperties {
   /** The error object of apollo client */
   error: ApolloError | undefined
 }
@@ -11,6 +11,6 @@ export interface ErrorDisplayProps {
 /**
  * Display error(s) coming from apollo client API call
  */
-export function ErrorDisplay({ error }: ErrorDisplayProps): JSX.Element {
+export function ErrorDisplay({ error }: ErrorDisplayProperties): JSX.Element {
   return <h2>Error..... {error?.message}</h2>
 }
