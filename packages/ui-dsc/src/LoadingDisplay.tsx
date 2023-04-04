4cddf272975271a850cbd6c392fe4b8128253f8e
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-04 16:53:58 -0500

    fix: fix the issue with variable name

--- packages/ui-dsc/src/LoadingDisplay.tsx
+++ packages/ui-dsc/src/LoadingDisplay.tsx
@@ -4,10 +4,10 @@ import { Box } from "@material-ui/core"
 /**
  * The prop for {@link LoadingDisplay}
  */
-export interface LoadingDisplayProps {
-  /** Number of rows of loading skeleton*/
+export interface LoadingDisplayProperties {
+  /** Number of rows of loading skeleton */
   rows?: number
-  /** Height of each loading skeleton*/
+  /** Height of each loading skeleton */
   height?: number
 }
 
@@ -17,7 +17,7 @@ export interface LoadingDisplayProps {
 export function LoadingDisplay({
   rows = 6,
   height = 35,
-}: LoadingDisplayProps): JSX.Element {
+}: LoadingDisplayProperties): JSX.Element {
   return (
     <Box mx={8} mt={4}>
       {[...Array(rows)].map((_, i: number) => {
