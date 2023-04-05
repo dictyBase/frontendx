b8c203078574c4f5b79f7bf5eea422f5c2ab5cf8
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-04-05 11:21:41 -0500

    refactor(LinksContainer.tsx): add JSDoc comments and type definitions
    This commit adds JSDoc comments and type definitions to the
    LinksContainer component and its subcomponents. This improves the
    readability and maintainability of the codebase by providing clear
    documentation and enforcing type safety.

--- packages/header/src/components/LinksContainer.tsx
+++ packages/header/src/components/LinksContainer.tsx
@@ -11,6 +11,13 @@ import headerStyles from "../styles/headerStyles"
 
 type IconType = typeof Login
 
+/**
+ * @typedef {Object} IconItemProperty
+ * @property {string} href - Link to the page
+ * @property {string} title - Title of the page
+ * @property {string} key - Unique key for the page
+ * @property {IconType} Icon - Icon for the page
+ */
 type IconItemProperty = {
   href: string
   title: string
@@ -18,14 +25,26 @@ type IconItemProperty = {
   Icon: IconType
 }
 
+/**
+ * @typedef {Object} LinksContainerProperties
+ * @property {Comp} children - Children components
+ */
 interface LinksContainerProperties {
   children: Comp
 }
 
+/**
+ * @typedef {Object} IconButtonProperties
+ * @property {string} href - Link to the page
+ * @property {Comp} children - Children components
+ */
 type IconButtonProperties = LinksContainerProperties & {
   href: string
 }
 
+/**
+ * @type {Array<IconItemProperty>}
+ */
 const iconItems: Array<IconItemProperty> = [
   {
     href: "/community/citation",
@@ -43,20 +62,38 @@ const iconItems: Array<IconItemProperty> = [
   { href: "/login", title: "Login", Icon: Login, key: uuid4() },
 ]
 
+/**
+ * @param {Object} props
+ * @param {string} props.title - Title of the page
+ * @returns {JSX.Element}
+ */
 const Title = ({ title }: { title: string }) => (
   <Typography variant="subtitle2">{title}</Typography>
 )
 
+/**
+ * @param {Object} props
+ * @param {IconType} props.Icon - Icon for the page
+ * @returns {JSX.Element}
+ */
 const LinksIcon = ({ Icon }: { Icon: IconType }) => (
   <Icon className={headerStyles().linksIcon} />
 )
 
+/**
+ * @param {IconButtonProperties} props
+ * @returns {JSX.Element}
+ */
 const LinksIconButton = ({ children, href }: IconButtonProperties) => (
   <IconButton className={headerStyles().linksButton} href={href}>
     {children}
   </IconButton>
 )
 
+/**
+ * @param {LinksContainerProperties} props
+ * @returns {JSX.Element}
+ */
 const LinksContainer = ({ children }: LinksContainerProperties) => (
   <Box className={headerStyles().linksContainer}>{children}</Box>
 )
