b9ac15c277ce281a5f1f9a1e5fc364772a38c76b
Kevin Tun <kevin.tun@northwestern.edu>
2023-04-12 12:52:33 -0500

    refactor(helpers.ts): remove JSDoc type annotation for tableNode parameter The JSDoc type annotation for the tableNode parameter is redundant as the type is already specified in the function signature. Removing the annotation improves code readability and reduces clutter.

--- packages/width-table-plugin/src/helpers.ts
+++ packages/width-table-plugin/src/helpers.ts
@@ -3,7 +3,7 @@ import WidthTableNode from "./WidthTableNode"
 
 /**
  * Selects the first cell of the first row in a given tableNode
- * @param {WidthTableNode} tableNode - The node representing a table within the editor
+ * @param tableNode - The node representing a table within the editor
  */
 export const selectFirstTableCell = (tableNode: WidthTableNode) => {
   const firstRow = tableNode.getFirstChildOrThrow()
