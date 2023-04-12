5ef1170a22b32874103a6895d0701d37505df5b3
Kevin Tun <kevin.tun@northwestern.edu>
2023-04-12 15:10:33 -0500

    refactor(LoadingDisplay.tsx): change prop name from 'icons' to 'className' The prop name 'icons' was misleading and did not accurately describe the purpose of the prop.

--- packages/resizable-image/src/LoadingDisplay.tsx
+++ packages/resizable-image/src/LoadingDisplay.tsx
@@ -1,7 +1,7 @@
 import { Container, CircularProgress } from "@material-ui/core"
 
-const LoadingDisplay = ({ icons }: { icons: string }) => (
-  <Container disableGutters className={icons}>
+const LoadingDisplay = ({ className }: { className: string }) => (
+  <Container disableGutters className={className}>
     <CircularProgress size={56} thickness={6} />
   </Container>
 )
