e757fa48b54ff5a3ddd4b2923de3dbeef36173f3
Kevin Tun <kevin.tun@northwestern.edu>
2023-04-14 02:12:07 -0500

    refactor(main.tsx): use destructuring import for render function and change ReactDOM to render The destructuring import for the render function improves the readability of the code.

--- apps/stock-catalog-list/src/main.tsx
+++ apps/stock-catalog-list/src/main.tsx
@@ -1,10 +1,10 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom"
+import React from "react"
+import { render } from "react-dom"
 import { App } from "./App"
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.querySelector("#root"),
 )
