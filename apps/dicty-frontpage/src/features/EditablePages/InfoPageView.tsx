b3f52c3584d1896cd0680f676e5237cbd88bc9ef
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-02 11:59:53 -0500

    refactor(InfoPageView.tsx): pass props to Editor component
    The Editor component now receives the content and editable props from the parent component.

--- apps/dicty-frontpage/src/features/EditablePages/InfoPageView.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/InfoPageView.tsx
@@ -37,7 +37,7 @@ const InfoPageView = ({ data }: Properties) => {
         />
       )}
       <ThemeProvider theme={appTheme}>
-        <Editor />
+        <Editor content={data?.content} editable={false} />
       </ThemeProvider>
     </Box>
   )
