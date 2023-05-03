7bd1e529f5726697346d7a8f98624698a4ef12db
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-03 09:52:17 -0500

    feat(handlers.ts): add mockLoginMutation and mockGetRefreshTokenQuery
    This commit adds two new mock functions to the handlers file. The mockLoginMutation function simulates a successful login request and returns a mock user object with a token. The mockGetRefreshTokenQuery function simulates a successful request for a refresh token and returns a mock user object with a token. These functions are used for testing purposes.

--- apps/dicty-frontpage/src/mocks/handlers.ts
+++ apps/dicty-frontpage/src/mocks/handlers.ts
@@ -6,12 +6,15 @@ import {
   mockListRecentStrainsQuery,
   mockContentBySlugQuery,
   mockListOrganismsQuery,
+  mockLoginMutation,
+  mockGetRefreshTokenQuery,
 } from "dicty-graphql-schema/introspection/mocks"
 import listRecentPublications from "../common/data/mockPublications"
 import listRecentPlasmids from "../common/data/mockPlasmids"
 import listRecentStrains from "../common/data/mockStrains"
 import listRecentGenes from "../common/data/mockGenes"
 import listOrganisms from "../common/data/mockDownloadsData"
+import MockSuperuser from "../common/data/superuser"
 import contentBySlug from "../common/data/mockContentBySlug"
 
 const handlers = [
@@ -74,11 +77,45 @@ const handlers = [
 
   mockContentBySlugQuery((request, response, context) => {
     const { slug } = request.variables
-    
+
     return response(
       context.data({
         contentBySlug: contentBySlug[slug],
         ...slug,
+      }),
+    )
+  }),
+
+  mockLoginMutation((request, response, context) => {
+    console.log(request.variables)
+
+    return response(
+      context.data({
+        login: {
+          user: MockSuperuser,
+          token:
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
+          identity: {
+            provider: request.variables.input.provider,
+          },
+        },
+      }),
+    )
+  }),
+
+  mockGetRefreshTokenQuery((request, response, context) => {
+    console.log(request.variables)
+
+    return response(
+      context.data({
+        getRefreshToken: {
+          token:
+            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
+          user: MockSuperuser,
+          identity: {
+            provider: "google",
+          },
+        },
       }),
     )
   }),
