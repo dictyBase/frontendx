770891c66bb7a3a719a4b9b7534218c06babf8c1
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-04 14:07:48 -0500

    fix(handlers.ts): add error handling for non-existent page content
    The mock handlers file now includes error handling for when a requested page content does not exist. If the requested page content does not exist, a "Page Content Not Found" error message is returned.

--- apps/dicty-frontpage/src/mocks/handlers.ts
+++ apps/dicty-frontpage/src/mocks/handlers.ts
@@ -77,6 +77,19 @@ const handlers = [
 
   mockContentBySlugQuery((request, response, context) => {
     const { slug } = request.variables
+    const content = contentBySlug[slug]
+
+    if (!content)
+      return response(
+        context.errors([
+          {
+            message: "Page Content Not Found.",
+            extensions: {
+              code: "NotFound",
+            },
+          },
+        ]),
+      )
 
     return response(
       context.data({
@@ -101,20 +114,22 @@ const handlers = [
     ),
   ),
 
-  mockGetRefreshTokenQuery((request, response, context) =>
-    response(
-      context.data({
-        getRefreshToken: {
-          token:
-            "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
-          user: MockSuperuser,
-          identity: {
-            provider: "google",
-          },
-        },
-      }),
-    ),
-  ),
+  // mockGetRefreshTokenQuery((request, response, context) => {
+  //   console.log(request.variables)
+
+  //   return response(
+  //     context.data({
+  //       getRefreshToken: {
+  //         token:
+  //           "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
+  //         user: MockSuperuser,
+  //         identity: {
+  //           provider: "google",
+  //         },
+  //       },
+  //     }),
+  //   )
+  // }),
 ]
 
 export default handlers
