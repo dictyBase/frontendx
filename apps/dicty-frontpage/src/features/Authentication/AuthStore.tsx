fcd5c6b8a5d85b27224f2b29a92a378a75c74229
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-04 21:31:38 -0500

    feat(AuthStore.tsx): add development state for mock environment
    A development auth state is added to the AuthStore component to support the mock environment. The development state is used when the VITE_APP_DEPLOY_ENV environment variable is set to "mock". This allows the developer to interact with components behind private routes without needing to login.

--- apps/dicty-frontpage/src/features/Authentication/AuthStore.tsx
+++ apps/dicty-frontpage/src/features/Authentication/AuthStore.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { User } from "dicty-graphql-schema"
+import MockSuperuser from "../../common/data/superuser"
 
 enum ActionType {
   LOGIN = "LOGIN",
@@ -31,6 +32,15 @@ const initialState = {
   token: "",
   user: {} as User,
   provider: "",
+  error: undefined,
+}
+
+const developmentState = {
+  isAuthenticated: true,
+  token:
+    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
+  user: MockSuperuser,
+  provider: "google",
   error: undefined,
 }
 
@@ -98,7 +108,14 @@ const authReducer = (state: AuthState, action: Action) => {
  * AuthProvider contains global state used for the shopping auth.
  */
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [state, dispatch] = React.useReducer(authReducer, initialState)
+  const [authState, dispatch] = React.useReducer(
+    authReducer,
+    initialState
+    )
+    
+    const state = import.meta.env.VITE_APP_DEPLOY_ENV === "mock"
+      ? developmentState
+      : authState
 
   const contextValue = React.useMemo(
     () => ({ state, dispatch }),
@@ -118,6 +135,7 @@ const useAuthStore = () => {
   if (!context) {
     throw new Error("useAuthStore must be used within a AuthProvider")
   }
+
   return context
 }
 
