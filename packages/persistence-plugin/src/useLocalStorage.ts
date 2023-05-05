5a3481fc6b365badf7f2c16bb6c7ade446ad043d
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-05 10:00:05 -0500

    refactor(useLocalStorage.ts): add storageKey parameter to useLocalStorage hook
    The useLocalStorage hook now accepts a storageKey parameter, which allows the persistence plugin to retrieve different page data from local storage.

--- packages/persistence-plugin/src/useLocalStorage.ts
+++ packages/persistence-plugin/src/useLocalStorage.ts
@@ -1,22 +1,26 @@
 import { useCallback } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 
-const useLocalStorage = () => {
+// if data exists in persistence plugin, get persistence plugin data
+//
+const useLocalStorage = (storageKey: string | undefined) => {
   const [editor] = useLexicalComposerContext()
 
   const saveLocalStorage = useCallback(() => {
+    if (!storageKey) return
     const editorState = editor.getEditorState()
     const editorStateString = JSON.stringify(editorState)
-    localStorage.setItem("lexical-image-demo", editorStateString)
-  }, [editor])
+    localStorage.setItem(`DFP-${storageKey}`, editorStateString)
+  }, [editor, storageKey])
 
   const retrieveLocalStorage = useCallback(() => {
-    const editorString = localStorage.getItem("lexical-image-demo")
+    if (!storageKey) return
+    const editorString = localStorage.getItem(`DFP-${storageKey}`)
     if (editorString) {
       const editorState = editor.parseEditorState(editorString)
       editor.setEditorState(editorState)
     }
-  }, [editor])
+  }, [editor, storageKey])
 
   return { saveLocalStorage, retrieveLocalStorage }
 }
