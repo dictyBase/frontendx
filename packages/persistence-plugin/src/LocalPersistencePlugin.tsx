0a1bbce815aaba9211c0865966bc79d9e574915f
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-08 04:55:36 -0500

    refactor(LocalPersistencePlugin.tsx): change save and retrieve local storage to lexical command listeners
    Saving and retrieving page data to and from local storage is now initiated by dispatching lexical commands SAVE_LOCAL_COMMAND and RETRIEVE_LOCAL_COMMAND. The Save button has also been abstracted from the LocalPersistencePlugin and now can be laid out independently of the plugin.

--- packages/persistence-plugin/src/LocalPersistencePlugin.tsx
+++ packages/persistence-plugin/src/LocalPersistencePlugin.tsx
@@ -1,23 +1,56 @@
 import { useEffect } from "react"
-import SaveButton from "./SaveButton"
-import useLocalStorage from "./useLocalStorage"
+import { createCommand, COMMAND_PRIORITY_EDITOR } from "lexical"
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { saveLocalStorage, retrieveLocalStorage } from "./useLocalStorage"
 
-type LocalPersistencePluginProperties = {
-  storageKey: string | undefined
-  editable: boolean
+type LocalStorageCommandPayload = {
+  storageKey: string
 }
 
+type LocalPersistencePluginProperties = {
+  currentStorageKey: string
+}
+
+export const SAVE_LOCAL_COMMAND = createCommand<LocalStorageCommandPayload>()
+export const RETRIEVE_LOCAL_COMMAND =
+  createCommand<LocalStorageCommandPayload>()
+
 const LocalPersistencePlugin = ({
-  storageKey,
-  editable,
+  currentStorageKey,
 }: LocalPersistencePluginProperties) => {
-  const { retrieveLocalStorage, saveLocalStorage } = useLocalStorage(storageKey)
+  const [editor] = useLexicalComposerContext()
 
   useEffect(() => {
-    retrieveLocalStorage()
-  }, [retrieveLocalStorage])
+    const unregisterSaveLocal = editor.registerCommand(
+      SAVE_LOCAL_COMMAND,
+      ({ storageKey }) => {
+        saveLocalStorage(editor, storageKey)
+        return true
+      },
+      COMMAND_PRIORITY_EDITOR,
+    )
+    const unregisterRetrieveLocal = editor.registerCommand(
+      RETRIEVE_LOCAL_COMMAND,
+      ({ storageKey }) => {
+        retrieveLocalStorage(editor, storageKey)
+        return true
+      },
+      COMMAND_PRIORITY_EDITOR,
+    )
 
-  return editable ? <SaveButton handleSave={saveLocalStorage} /> : <></>
+    return () => {
+      unregisterSaveLocal()
+      unregisterRetrieveLocal()
+    }
+  }, [editor])
+
+  useEffect(() => {
+    editor.dispatchCommand(RETRIEVE_LOCAL_COMMAND, {
+      storageKey: currentStorageKey,
+    })
+  }, [currentStorageKey, editor])
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <></>
 }
 
 export default LocalPersistencePlugin
