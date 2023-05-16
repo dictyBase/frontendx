affd4821c54e7c55173bb412d572755b1aa1049e
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-16 10:13:31 -0500

    style(mockPublications.ts): disable camelcase eslint rule
    The camelcase eslint rule is disabled to allow for the use of snake_case in the publication objects. This improves consistency with the naming conventions used in the data source. The author's properties of each publication was updated to match the graphql schema.

--- apps/dicty-frontpage/src/common/data/mockPublications.ts
+++ apps/dicty-frontpage/src/common/data/mockPublications.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const listRecentPublications = [
   {
     id: "1",
@@ -6,17 +7,15 @@ const listRecentPublications = [
       "Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.",
     abstract: "",
     journal: "Cells 8",
-    // eslint-disable-next-line camelcase
     pub_date: "",
     pages: "",
     issue: "",
     volume: "",
     authors: [
-      {
-        initials: [""],
-        // eslint-disable-next-line camelcase
-        last_name: ["Tanaka", "Jahan", "Kondo", "Nakano & Yumura (2019)"],
-      },
+      { last_name: "Tanaka" },
+      { last_name: "Jahan" },
+      { last_name: "Kondo" },
+      { last_name: "Nakano & Yumura (2019" },
     ],
   },
   {
@@ -26,23 +25,16 @@ const listRecentPublications = [
       "Force balances between interphase centrosomes, as revealed by laser ablation.",
     abstract: "",
     journal: "Mol. Biol. Cell mbcE19010034",
-    // eslint-disable-next-line camelcase
     pub_date: "",
     pages: "",
     issue: "",
     volume: "",
     authors: [
-      {
-        initials: [""],
-        // eslint-disable-next-line camelcase
-        last_name: [
-          "Odell",
-          "Sikirzhytski",
-          "Tikhonenko",
-          "Cobani",
-          "Khodjakov & Koonce (2019)",
-        ],
-      },
+      { last_name: "Odell" },
+      { last_name: "Sikirzhytski" },
+      { last_name: "Tikhonenko" },
+      { last_name: "Cobani" },
+      { last_name: "Khodjakov & Koonce (2019)" },
     ],
   },
   {
@@ -52,27 +44,20 @@ const listRecentPublications = [
       "A terpene synthase-cytochrome P450 cluster in Dictyostelium discoideum produces a novel trisnorsesquiterpene.",
     abstract: "",
     journal: "Elife 8",
-    // eslint-disable-next-line camelcase
     pub_date: "",
     pages: "",
     issue: "",
     volume: "",
     authors: [
-      {
-        initials: [""],
-        // eslint-disable-next-line camelcase
-        last_name: [
-          "Chen",
-          "Luck",
-          "Rabe",
-          "Dinh",
-          "Shaulsky",
-          "Nelson",
-          "Gershenzon",
-          "Dickschat",
-          "Kollner & Chen (2019)",
-        ],
-      },
+      { last_name: "Chen" },
+      { last_name: "Luck" },
+      { last_name: "Rabe" },
+      { last_name: "Dinh" },
+      { last_name: "Shaulsky" },
+      { last_name: "Nelson" },
+      { last_name: "Gershenzon" },
+      { last_name: "Dickschat" },
+      { last_name: "Kollner & Chen (2019)" },
     ],
   },
   {
@@ -82,16 +67,13 @@ const listRecentPublications = [
       "Cyclic AMP induction of Dictyostelium prespore gene expression requires autophagy.",
     abstract: "",
     journal: "Dev. Biol.",
-    // eslint-disable-next-line camelcase
     pub_date: "",
     pages: "",
     issue: "",
     volume: "",
     authors: [
       {
-        initials: [""],
-        // eslint-disable-next-line camelcase
-        last_name: ["Yamada & Schaap (2019)"],
+        last_name: "Yamada & Schaap (2019)",
       },
     ],
   },
