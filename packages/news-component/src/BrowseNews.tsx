5e1b254b60cc4c3a25131ca1df51d56dbca1afbd
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-18 12:19:09 -0500

    feat(news-component): add BrowseNews component
    The BrowseNews component is added to the news-component package. It renders a list of news articles and uses the useListNewsContentQuery hook from the dicty-graphql-schema package to fetch the data. The NewsPreview component is also added to the package, which is used to render each individual news article in the list. The index.ts file is updated to export the BrowseNews component.
    issue #173

---
+++ packages/news-component/src/BrowseNews.tsx
@@ -0,0 +1,29 @@
+import { Grid, Container } from "@material-ui/core"
+import { useListNewsContentQuery } from "dicty-graphql-schema"
+import NewsPreview from "./NewsPreview"
+
+/**
+ * Renders a list of news articles.
+ * @returns
+ */
+const BrowseNews = () => {
+  const { loading, error, data } = useListNewsContentQuery()
+
+  if (error) return <div> Error </div>
+  if (loading) return <div> Loading </div>
+  if (!data) return <div> Fallback </div>
+
+  return (
+    <Container>
+      <Grid container direction="column" spacing={1}>
+        {data.listContent?.map((article) => (
+          <Grid item>
+            <NewsPreview article={article} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
+}
+
+export default BrowseNews
