a0f359101209a80271cd2d059aa03070a91acce4
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-18 12:14:07 -0500

    feat(dicty-frontpage): add mockListNewsContent.ts and mockListNewsContentQuery to handlers.ts
    The mockListNewsContent.ts file contains an array of objects that represent news content. The mockListNewsContentQuery function is added to the handlers.ts file to mock the GraphQL query that retrieves the list of news content. This allows for testing of the front-end components that display the news content.

---
+++ apps/dicty-frontpage/src/common/data/mockListNewsContent.ts
@@ -0,0 +1,70 @@
+const listNewsContent = [
+  {
+    id: "1",
+    name: "Lorem ipsum dolor sit amet",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    updatedAt: "2022-01-01",
+  },
+  {
+    id: "2",
+    name: "Vestibulum lacinia lacus non",
+    content:
+      "Vestibulum lacinia lacus non metus pharetra, et scelerisque dui convallis...",
+    updatedAt: "2022-01-02",
+  },
+  {
+    id: "3",
+    name: "Praesent eu felis bibendum",
+    content:
+      "Praesent eu felis bibendum, ultrices risus eu, hendrerit ipsum...",
+    updatedAt: "2022-01-03",
+  },
+  {
+    id: "4",
+    name: "Aliquam eget bibendum",
+    content:
+      "Aliquam eget bibendum enim. Aenean facilisis dolor nec justo sodales...",
+    updatedAt: "2022-01-04",
+  },
+  {
+    id: "5",
+    name: "Donec at augue vitae ante",
+    content: "Donec at augue vitae ante pharetra elementum eget vel felis...",
+    updatedAt: "2022-01-05",
+  },
+  {
+    id: "6",
+    name: "Phasellus et velit suscipit",
+    content: "Phasellus et velit suscipit, lacinia lacus non, blandit purus...",
+    updatedAt: "2022-01-06",
+  },
+  {
+    id: "7",
+    name: "Nullam porttitor eget turpis",
+    content:
+      "Nullam porttitor eget turpis non dignissim. Sed eleifend fringilla urna...",
+    updatedAt: "2022-01-07",
+  },
+  {
+    id: "8",
+    name: "Curabitur sed lectus ut",
+    content: "Curabitur sed lectus ut lorem mollis congue. In et odio est...",
+    updatedAt: "2022-01-08",
+  },
+  {
+    id: "9",
+    name: "Suspendisse potenti",
+    content:
+      "Suspendisse potenti. Morbi a lorem eget erat blandit elementum a finibus nulla...",
+    updatedAt: "2022-01-09",
+  },
+  {
+    id: "10",
+    name: "Nam semper dignissim dolor",
+    content:
+      "Nam semper dignissim dolor, quis faucibus orci. Phasellus euismod maximus quam sed porttitor...",
+    updatedAt: "2022-01-10",
+  },
+]
+
+export default listNewsContent
