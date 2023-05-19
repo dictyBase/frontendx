7f845509d3785b09aebf757a52414d3f594d0d01
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 16:53:51 -0500

    refactor(DisplayNews.tsx): restructure DisplayNews component to use Grid and NewsHeader
    The DisplayNews component has been restructured to use the Grid component from Material-UI to improve the layout of the component. The NewsHeader component has also been added to the DisplayNews component to display the header of the news article.
    issue #173

--- packages/news-component/src/DisplayNews.tsx
+++ packages/news-component/src/DisplayNews.tsx
@@ -1,8 +1,9 @@
-import { Container, Typography } from "@material-ui/core"
+import { Grid, Container, Typography } from "@material-ui/core"
 import { Editor } from "dicty-editor"
+import NewsHeader from "./NewsHeader"
 
 type DisplayNewsProperties = {
-  data: {
+  content: {
     name: string
     content: string
     updatedAt: string
@@ -10,22 +11,29 @@ type DisplayNewsProperties = {
   }
 }
 
-const DisplayNews = ({ data }: DisplayNewsProperties) => {
-  if (!data) return <div> Fallback </div>
-
-  return (
-    <Container>
-      <Typography variant="h1">{data.name}</Typography>
-      <Typography>{data.updatedAt}</Typography>
-      <Editor
-        editable={false}
-        content={{
-          storageKey: data.slug,
-          editorState: data.content,
-        }}
-      />
-    </Container>
-  )
-}
+const DisplayNews = ({ content }: DisplayNewsProperties) => (
+  <Container>
+    <Grid container direction="column" spacing={1}>
+      <Grid item>
+        <NewsHeader />
+      </Grid>
+      <Grid item>
+        <Typography variant="h1">{content.name}</Typography>
+      </Grid>
+      <Grid item>
+        <Typography>{content.updatedAt}</Typography>
+      </Grid>
+      <Grid item>
+        <Editor
+          editable={false}
+          content={{
+            storageKey: content.slug,
+            editorState: content.content,
+          }}
+        />
+      </Grid>
+    </Grid>
+  </Container>
+)
 
 export default DisplayNews
