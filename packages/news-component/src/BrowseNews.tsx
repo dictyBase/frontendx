1bc957afa45a109e7c9c0ff0726327b833918c33
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 16:52:21 -0500

    refactor(BrowseNews.tsx): replace Typography with NewsHeader component
    The Typography component was replaced with a new NewsHeader component to improve code readability and maintainability. The NewsHeader component is now used to display the title of the news page, the DynamicBreadCrumbs component, and a Create button.
    issue #173

--- packages/news-component/src/BrowseNews.tsx
+++ packages/news-component/src/BrowseNews.tsx
@@ -1,6 +1,7 @@
-import { Grid, Container, Typography } from "@material-ui/core"
+import { Grid, Container } from "@material-ui/core"
 import { useListNewsContentQuery } from "dicty-graphql-schema"
 import NewsPreview from "./NewsPreview"
+import NewsHeader from "./NewsHeader"
 
 /**
  * Renders a list of news articles.
@@ -17,9 +18,7 @@ const BrowseNews = () => {
     <Container>
       <Grid container direction="column" spacing={1}>
         <Grid item>
-          <Typography gutterBottom align="center" variant="h1">
-            Dicty News
-          </Typography>
+          <NewsHeader />
         </Grid>
         {data.listContent?.map((article) => (
           <Grid item>
