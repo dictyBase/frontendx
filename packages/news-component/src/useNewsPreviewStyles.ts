61ca8f81166479ecd1593cd53e3ed5e5df723ef9
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 10:23:47 -0500

    feat(NewsPreview.tsx): add preview text to NewsPreview component
    The NewsPreview component now displays a preview of the article content, which is generated by the parseContentToText function from the dicty-editor package. The preview text is limited to 250 characters and is displayed with an ellipsis if it exceeds that limit. The useNewsPreviewStyles hook provides styles for the NewsPreview component, including padding, height, width, and text overflow.

---
+++ packages/news-component/src/useNewsPreviewStyles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core"
+
+const useNewsPreviewStyles = makeStyles({
+  root: {
+    padding: "1rem",
+    height: "150px",
+    width: "100%",
+    textOverflow: "ellipsis",
+  },
+})
+
+export default useNewsPreviewStyles
