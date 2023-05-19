91ba2f1afb2f0c49213ab2f26b2674243b13cf7a
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 16:59:27 -0500

    refactor(NewsPage.tsx): extract display logic to DisplayNews component
    The display logic for the news page has been extracted to a new component called DisplayNews. This improves the readability and maintainability of the code by separating concerns and reducing the complexity of the NewsPage component.
    issue #173

--- packages/news-component/src/NewsPage.tsx
+++ packages/news-component/src/NewsPage.tsx
@@ -1,7 +1,6 @@
-import { Container, Typography } from "@material-ui/core"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
-import { Editor } from "dicty-editor"
 import { useParams } from "react-router-dom"
+import DisplayNews from "./DisplayNews"
 
 const NewsPage = () => {
   const { id } = useParams()
@@ -15,19 +14,7 @@ const NewsPage = () => {
   if (loading) return <div> Loading </div>
   if (!data?.contentBySlug) return <div> Fallback </div>
 
-  return (
-    <Container>
-      <Typography variant="h1">{data.contentBySlug.name}</Typography>
-      <Typography>{data.contentBySlug.updatedAt}</Typography>
-      <Editor
-        editable={false}
-        content={{
-          storageKey: data.contentBySlug.slug,
-          editorState: data.contentBySlug.content,
-        }}
-      />
-    </Container>
-  )
+  return <DisplayNews content={data.contentBySlug} />
 }
 
 export default NewsPage
