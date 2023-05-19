7415bde8ae780d5be01042d7656fc92f44472015
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 10:10:03 -0500

    refactor(mockListNewsContent.ts): use constant for content string and add namespace property
    The content string is now stored in a constant to improve code readability and reduce duplication. A namespace property has also been added to each object in the listNewsContent array to provide additional context for the data.

--- apps/dicty-frontpage/src/common/data/mockListNewsContent.ts
+++ apps/dicty-frontpage/src/common/data/mockListNewsContent.ts
@@ -1,69 +1,84 @@
+const CONTENT_STRING = `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Annual International Dictyostelium Conference","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"International conferences dedicated to ","type":"text","version":1},{"detail":0,"format":2,"mode":"normal","style":"","text":"Dictyostelium","type":"text","version":1},{"detail":0,"format":0,"mode":"normal","style":"","text":" started in  1977 with the meeting in Sardinia, and continued on a roughly 3-year  cycle into the 1980's. However, as the field became more active, more  local meetings sprang up to fill the gaps in the cycle. Notable amongst  these was an annual series in the UK, which gradually became more  international. By the late 1980's with the successive meetings at  Amsterdam, Oxford, Airlie and Cambridge, the current pattern of annual  meetings was established. Interestingly in the late 1990's as the field  expanded further, local meetings were re-started in several countries.     ","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"flex-layout","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`
 const listNewsContent = [
   {
     id: "1",
+    slug: "1",
     name: "Lorem ipsum dolor sit amet",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-01",
+    namespace: "news",
   },
   {
     id: "2",
+    slug: "2",
     name: "Vestibulum lacinia lacus non",
-    content:
-      "Vestibulum lacinia lacus non metus pharetra, et scelerisque dui convallis...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-02",
+    namespace: "news",
   },
   {
     id: "3",
+    slug: "3",
     name: "Praesent eu felis bibendum",
-    content:
-      "Praesent eu felis bibendum, ultrices risus eu, hendrerit ipsum...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-03",
+    namespace: "news",
   },
   {
     id: "4",
+    slug: "4",
     name: "Aliquam eget bibendum",
-    content:
-      "Aliquam eget bibendum enim. Aenean facilisis dolor nec justo sodales...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-04",
+    namespace: "news",
   },
   {
     id: "5",
+    slug: "5",
     name: "Donec at augue vitae ante",
-    content: "Donec at augue vitae ante pharetra elementum eget vel felis...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-05",
+    namespace: "news",
   },
   {
     id: "6",
+    slug: "6",
     name: "Phasellus et velit suscipit",
-    content: "Phasellus et velit suscipit, lacinia lacus non, blandit purus...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-06",
+    namespace: "news",
   },
   {
     id: "7",
+    slug: "7",
     name: "Nullam porttitor eget turpis",
-    content:
-      "Nullam porttitor eget turpis non dignissim. Sed eleifend fringilla urna...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-07",
+    namespace: "news",
   },
   {
     id: "8",
+    slug: "8",
     name: "Curabitur sed lectus ut",
-    content: "Curabitur sed lectus ut lorem mollis congue. In et odio est...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-08",
+    namespace: "news",
   },
   {
     id: "9",
+    slug: "9",
     name: "Suspendisse potenti",
-    content:
-      "Suspendisse potenti. Morbi a lorem eget erat blandit elementum a finibus nulla...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-09",
+    namespace: "news",
   },
   {
     id: "10",
+    slug: "10",
     name: "Nam semper dignissim dolor",
-    content:
-      "Nam semper dignissim dolor, quis faucibus orci. Phasellus euismod maximus quam sed porttitor...",
+    content: CONTENT_STRING,
     updatedAt: "2022-01-10",
+    namespace: "news",
   },
 ]
 
