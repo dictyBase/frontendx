4441349d8cf69e7407ba7e1f5632ab6931cb9603
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-19 10:19:03 -0500

    refactor(Editor.tsx): move editor configuration to separate file
    The editor configuration has been moved to a separate file, editorConfig.ts, to improve code organization and readability. The configuration is now imported from the new file and merged with the initial configuration object.

---
+++ packages/dicty-editor/src/editorConfig.ts
@@ -0,0 +1,42 @@
+import { ListItemNode, ListNode } from "@lexical/list"
+import { HeadingNode, QuoteNode } from "@lexical/rich-text"
+import { TableCellNode, TableRowNode } from "@lexical/table"
+import { ImageNode } from "image-plugin"
+import { WidthTableNode } from "width-table-plugin"
+import { FlexLayoutNode } from "flex-layout-plugin"
+
+const editorTheme = {
+  paragraph: "editor-paragraph",
+  text: {
+    bold: "editor-text-bold",
+    italic: "editor-text-italic",
+    underline: "editor-text-underline",
+  },
+  table: "editor-table",
+  tableCell: "editor-tablecell",
+  tableCellHeader: "editor-tablecell-head",
+}
+
+const onError = (error: Error) => {
+  // eslint-disable-next-line no-console
+  console.error(error)
+}
+
+const dictyEditorConfig = {
+  namespace: "DictyEditor",
+  theme: { ...editorTheme },
+  nodes: [
+    HeadingNode,
+    QuoteNode,
+    ListItemNode,
+    ListNode,
+    ImageNode,
+    TableCellNode,
+    TableRowNode,
+    WidthTableNode,
+    FlexLayoutNode,
+  ],
+  onError,
+}
+
+export default dictyEditorConfig
