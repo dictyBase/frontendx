b30311e50ace2714f5fbadea9f8240f89e7705f5
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-23 11:16:49 -0500

    feat(WriteNews.tsx): add WriteNews component
    The WriteNews component is added to the news-component package. It allows users to write news articles by providing a form with a title input and a rich text editor. The component also includes a save and cancel button. When the user saves the article, it is sent to the server via a GraphQL mutation. If the save is successful, the user is redirected to the news page. If the user cancels, they are redirected to the news page without saving the article.
    issue #173

---
+++ packages/news-component/src/WriteNews.tsx
@@ -0,0 +1,63 @@
+import { useState, ChangeEvent } from "react"
+import { Grid, Container, TextField } from "@material-ui/core"
+import slugify from "slugify"
+import { useNavigate } from "react-router-dom"
+import { useCreateContentMutation } from "dicty-graphql-schema"
+import { Editor } from "dicty-editor"
+import NewsHeader from "./NewsHeader"
+
+const WriteNews = () => {
+  const [title, setTitle] = useState("")
+  const [createContent] = useCreateContentMutation()
+  const navigate = useNavigate()
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
+
+  const handleSave = async (content: string) => {
+    await createContent({
+      variables: {
+        input: {
+          content,
+          slug: slugify(title),
+          name: title,
+          namespace: "news",
+          createdBy: "george@vandelayindustries.com",
+        },
+      },
+    })
+    navigate(`/news`)
+  }
+
+  const handleCancel = () => {
+    navigate("/news")
+  }
+
+  return (
+    <Container>
+      <Grid container direction="column" spacing={3}>
+        <Grid item>
+          <NewsHeader />
+        </Grid>
+        <Grid item>
+          <TextField
+            label="Article Title"
+            placeholder="Article Title"
+            onChange={onChange}
+            value={title}
+          />
+        </Grid>
+        <Grid item>
+          <Editor
+            editable
+            handleSave={handleSave}
+            handleCancel={handleCancel}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default WriteNews
