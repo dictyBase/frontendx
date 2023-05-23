f9dfcad7a7fd111e5db6d57d2f05489598f4fed6
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-23 17:41:49 -0500

    feat(superuser.ts): add testPermission and superUserRole constants
    The testPermission and superUserRole constants are added to the superuser.ts file. These constants are used to define the properties of a test permission and a superuser role in the mock database respectively.

--- apps/dicty-frontpage/src/common/data/superuser.ts
+++ apps/dicty-frontpage/src/common/data/superuser.ts
@@ -1,30 +1,26 @@
-const MockSuperuser = {
+export const testPermission = {
+  id: "1",
+  level: "test",
+  description: "a test permission",
+  resource: "testresource",
+  createdAt: "123_456",
+  updatedAt: "678_900",
+}
+
+export const superUserRole = {
+  id: "1",
+  name: "superuser",
+  description: "total power!",
+  createdAt: "123_456",
+  updatedAt: "678_900",
+}
+
+export const MockSuperuser = {
   id: "999",
   firstName: "Art",
   lastName: "Vandelay",
   email: "george@vandelayindustries.com",
   isActive: true,
-  createdAt: 123_456,
-  updatedAt: 678_900,
-  roles: [
-    {
-      id: "1",
-      name: "superuser",
-      description: "total power!",
-      createdAt: 123_456,
-      updatedAt: 678_900,
-      permissions: [
-        {
-          id: "1",
-          level: "test",
-          description: "a test permission",
-          resource: "testresource",
-          createdAt: 123_456,
-          updatedAt: 678_900,
-        },
-      ],
-    },
-  ],
+  createdAt: "123_456",
+  updatedAt: "678_900",
 }
-
-export default MockSuperuser
