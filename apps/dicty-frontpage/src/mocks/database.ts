bd4ba15302c3d4f6c71be0e4473b8faf4abf2327
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-23 17:35:47 -0500

    feat(database.ts): add user, role, and permission entities to the database
    The user, role, and permission entities are added to the database. The user entity has fields for id, firstName, lastName, email, isActive, createdAt, updatedAt, and roles. The role entity has fields for id, name, description, createdAt, updatedAt, and permissions. The permission entity has fields for id, level, description, createdAt, updatedAt, and resource. The initializeMockNewsData function is updated to create a superuser with a role that has a permission, and each news item is created with createdBy and updatedBy fields set to the superuser.

--- apps/dicty-frontpage/src/mocks/database.ts
+++ apps/dicty-frontpage/src/mocks/database.ts
@@ -1,19 +1,70 @@
-import { factory, primaryKey } from "@mswjs/data"
+import { factory, primaryKey, oneOf, manyOf, nullable } from "@mswjs/data"
 import listNewsContent from "../common/data/mockListNewsContent"
+import {
+  testPermission,
+  superUserRole,
+  MockSuperuser,
+} from "../common/data/superuser"
 
 const database = factory({
-  news: {
+  user: {
+    id: primaryKey(String),
+    firstName: String,
+    lastName: String,
+    email: String,
+    // groupName: nullable(String),
+    // organization: nullable(String),
+    // firstAddress: nullable(String),
+    // secondAddress: nullable(String),
+    // city: nullable(String),
+    // state: nullable(String),
+    // zipcode: nullable(String),
+    // country: nullable(String),
+    // phone: nullable(String),
+    isActive: Boolean,
+    createdAt: Date,
+    updatedAt: Date,
+    roles: nullable(manyOf("role")),
+  },
+  role: {
+    id: primaryKey(String),
+    name: String,
+    description: String,
+    createdAt: Date,
+    updatedAt: Date,
+    permissions: nullable(manyOf("permission")),
+  },
+  permission: {
+    id: primaryKey(String),
+    level: String,
+    description: String,
+    createdAt: Date,
+    updatedAt: Date,
+    resource: String,
+  },
+  content: {
     id: primaryKey(String),
     slug: String,
     name: String,
     content: String,
-    updatedAt: String,
-    namespace: () => "news",
+    createdBy: oneOf("user"),
+    createdAt: Date,
+    updatedBy: oneOf("user"),
+    updatedAt: Date,
+    namespace: String,
   },
 })
 
 const initializeMockNewsData = () => {
-  listNewsContent.forEach((item) => database.news.create(item))
+  const permissions = database.permission.create(testPermission)
+  const roles = database.role.create({
+    ...superUserRole,
+    permissions: [permissions],
+  })
+  const user = database.user.create({ ...MockSuperuser, roles: [roles] })
+  listNewsContent.forEach((item) =>
+    database.content.create({ ...item, createdBy: user, updatedBy: user }),
+  )
 }
 
 initializeMockNewsData()
