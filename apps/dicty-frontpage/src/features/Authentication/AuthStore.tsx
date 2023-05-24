63250aeb42f0728763c1bd82a4dee3f2ad875f43
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-24 14:09:43 -0500

    refactor(AuthStore.tsx): extract mockUser object and add testPermission
    The mockUser object is now extracted from the MockSuperuser object and includes a new permission, testPermission. This improves the readability of the code and makes it easier to modify the user object in the future. The testPermission is added to the superUserRole object, which is then added to the roles array of the mockUser object.

--- apps/dicty-frontpage/src/features/Authentication/AuthStore.tsx
+++ apps/dicty-frontpage/src/features/Authentication/AuthStore.tsx
@@ -1,6 +1,15 @@
 import React from "react"
 import { User } from "dicty-graphql-schema"
-import { MockSuperuser } from "../../common/data/superuser"
+import {
+  MockSuperuser,
+  superUserRole,
+  testPermission,
+} from "../../common/data/superuser"
+
+const mockUser = {
+  ...MockSuperuser,
+  roles: [{ ...superUserRole, permissions: [testPermission] }],
+}
 
 enum ActionType {
   LOGIN = "LOGIN",
@@ -39,7 +48,7 @@ const developmentAuthState = {
   isAuthenticated: true,
   token:
     "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w",
-  user: MockSuperuser,
+  user: mockUser,
   provider: "google",
   error: undefined,
 }
