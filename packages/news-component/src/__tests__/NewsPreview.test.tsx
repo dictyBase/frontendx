68d5a73c3f85d601de1e94481a6de26df68fdb5c
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-24 10:22:49 -0500

    test(NewsPreview.test.tsx): add testSlug variable and use it in the test
    The test now uses a testSlug variable to improve readability and maintainability. The test now checks if the correct route is found when the NewsPreview component is clicked.

--- packages/news-component/src/__tests__/NewsPreview.test.tsx
+++ packages/news-component/src/__tests__/NewsPreview.test.tsx
@@ -4,10 +4,12 @@ import userEvent from "@testing-library/user-event"
 import { MemoryRouter, Routes, Route } from "react-router-dom"
 import NewsPreview from "../NewsPreview"
 
+const testSlug = "test-slug"
 const contentJsonString = `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Annual International Dictyostelium Conference","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"International conferences dedicated to ","type":"text","version":1},{"detail":0,"format":2,"mode":"normal","style":"","text":"Dictyostelium","type":"text","version":1},{"detail":0,"format":0,"mode":"normal","style":"","text":" started in  1977 with the meeting in Sardinia, and continued on a roughly 3-year  cycle into the 1980's. However, as the field became more active, more  local meetings sprang up to fill the gaps in the cycle. Notable amongst  these was an annual series in the UK, which gradually became more  international. By the late 1980's with the successive meetings at  Amsterdam, Oxford, Airlie and Cambridge, the current pattern of annual  meetings was established. Interestingly in the late 1990's as the field  expanded further, local meetings were re-started in several countries.     ","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"flex-layout","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`
 const parsedContentString = `Annual International Dictyostelium Conference`
 const article = {
   id: "45",
+  slug: testSlug,
   name: "Test News",
   updatedAt: "2021-10-29",
   content: contentJsonString,
@@ -32,7 +34,7 @@ test("When clicked, it navigates to the single news item route", async () => {
     <MemoryRouter initialEntries={["/news"]}>
       <Routes>
         <Route path="news" element={<NewsPreview article={article} />} />
-        <Route path="news/45" element={<h1> Route Found </h1>} />
+        <Route path={`news/${testSlug}`} element={<h1> Route Found </h1>} />
       </Routes>
     </MemoryRouter>,
   )
