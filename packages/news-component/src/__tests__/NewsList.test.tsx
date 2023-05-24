f430635459cf0ffcb09c3a3d78c6c9712d279b49
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-24 10:17:39 -0500

    test(NewsList.test.tsx): change BrowseNews test to NewsList tests
    This news listing functionality has been abstracted from the BrowseNews component into the NewsList component. The component being tested was swapped acoordingly.

--- packages/news-component/src/__tests__/NewsList.test.tsx
+++ packages/news-component/src/__tests__/NewsList.test.tsx
@@ -2,7 +2,7 @@ import { vi, test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import listNewsContent from "../mockNews"
-import BrowseNews from "../BrowseNews"
+import NewsList from "../NewsList"
 
 vi.mock("dicty-graphql-schema", () => ({
   useListNewsContentQuery: () => ({
@@ -15,20 +15,9 @@ vi.mock("dicty-graphql-schema", () => ({
 test("It renders a link for each news item from the query", async () => {
   render(
     <MemoryRouter>
-      <BrowseNews />
+      <NewsList articles={listNewsContent} />
     </MemoryRouter>,
   )
 
   expect(screen.getAllByRole("link")).toHaveLength(listNewsContent.length)
-})
-
-test("It renders a create button", () => {
-  render(
-    <MemoryRouter>
-      <BrowseNews />
-    </MemoryRouter>,
-  )
-
-  expect(screen.getByRole("button")).toBeInTheDocument()
-  expect(screen.getByRole("button")).toHaveTextContent(/Create/)
 })
