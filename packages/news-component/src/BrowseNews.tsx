ae34ef5989590aae8a6872af0ab69c56eb7f628d
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-25 08:35:50 -0500

    feat(BrowseNews.tsx): add DeleteButton to NewsHeader when articles are selected
    The BrowseNews component now includes a DeleteButton in the NewsHeader when one or more articles are selected. This allows the user to delete multiple articles at once, improving the user experience. The useEffect hook has also been added to refetch the list of news articles when the selectedArticlesAtom changes.

--- packages/news-component/src/BrowseNews.tsx
+++ packages/news-component/src/BrowseNews.tsx
@@ -1,17 +1,24 @@
+import { useEffect } from "react"
 import { Grid, Container } from "@material-ui/core"
 import { useListNewsContentQuery } from "dicty-graphql-schema"
+import { useAtomValue } from "jotai"
+import { selectedArticlesAtom } from "./atomConfigs"
 import NewsList from "./NewsList"
 import NewsHeader from "./NewsHeader"
 import CreateButton from "./CreateButton"
-
+import DeleteButton from "./DeleteButton"
 /**
  * Renders a list of news articles.
  * @returns
  */
 const BrowseNews = () => {
-  const { loading, error, data } = useListNewsContentQuery({
+  const selectedArticles = useAtomValue(selectedArticlesAtom)
+  const { loading, error, data, refetch } = useListNewsContentQuery({
     fetchPolicy: "no-cache",
   })
+  useEffect(() => {
+    refetch()
+  }, [refetch, selectedArticles])
 
   if (error) return <div> Error </div>
   if (loading) return <div> Loading </div>
@@ -21,7 +28,22 @@ const BrowseNews = () => {
     <Container>
       <Grid container direction="column" spacing={1}>
         <Grid item>
-          <NewsHeader button={<CreateButton />} />
+          <NewsHeader
+            button={
+              <Grid container spacing={1}>
+                {selectedArticles.length > 0 ? (
+                  <Grid item>
+                    <DeleteButton />
+                  </Grid>
+                ) : (
+                  <></>
+                )}
+                <Grid item>
+                  <CreateButton />
+                </Grid>
+              </Grid>
+            }
+          />
         </Grid>
         <NewsList articles={data?.listContent} />
       </Grid>
