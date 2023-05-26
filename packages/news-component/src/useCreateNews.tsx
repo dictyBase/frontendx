f870f2e74e244ec73c14703dfa553b92b85029b2
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-26 09:27:49 -0500

    feat(news-component): add useCreateNews and useUpdateNews hooks
    Two new hooks, useCreateNews and useUpdateNews, have been added to the news-component package. These hooks are used to create and update news content respectively. They both use the useCreateContentMutation and useUpdateContentMutation hooks from the dicty-graphql-schema package to make the necessary GraphQL mutations. The hooks also use the slugify package to generate a slug from the title of the news content. Finally, the hooks use the useNavigate hook from react-router-dom to navigate to the newly created or updated news content.

---
+++ packages/news-component/src/useCreateNews.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom"
+import { useCreateContentMutation } from "dicty-graphql-schema"
+import slugify from "slugify"
+
+const useCreateNews = (title: string) => {
+  const [createContent] = useCreateContentMutation()
+  const navigate = useNavigate()
+
+  const handleCreate = async (content: string) => {
+    const created = await createContent({
+      variables: {
+        input: {
+          content,
+          slug: slugify(title, { lower: true }),
+          name: title,
+          namespace: "news",
+          createdBy: "george@vandelayindustries.com",
+        },
+      },
+    })
+    navigate(`/news/${created.data?.createContent?.slug}`)
+  }
+
+  return { handleCreate }
+}
+
+export default useCreateNews
