c14cfcb10507496e97e1c7400ef62e60b5e3ff48
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-26 12:57:01 -0500

    feat(news-component): add useCheckboxStyles and useToolbarStyles
    Two new files, useCheckboxStyles.ts and useToolbarStyles.ts, have been added to the news-component package. These files contain the implementation of the makeStyles hook from Material-UI. The useCheckboxStyles hook defines the styles for the checkbox component, while the useToolbarStyles hook defines the styles for the toolbar component. These hooks can be used to apply consistent styles across the application.

---
+++ packages/news-component/src/useCheckboxStyles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core"
+
+const useCheckboxStyles = makeStyles({
+  root: {
+    paddingLeft: "9px",
+    paddingRight: "9px",
+  },
+})
+
+export default useCheckboxStyles
