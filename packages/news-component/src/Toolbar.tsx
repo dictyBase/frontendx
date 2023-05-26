8cf75a6258e18d1dccea92e5c4fae7707ad1dd18
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-26 13:17:48 -0500

    refactor(Toolbar.tsx): remove unused refetch prop from NewsToolbar component
    The refetch prop was not being used in the NewsToolbar component, so it was removed to improve code readability and maintainability.

--- packages/news-component/src/Toolbar.tsx
+++ packages/news-component/src/Toolbar.tsx
@@ -2,18 +2,13 @@ import { Toolbar, Paper, Checkbox } from "@material-ui/core"
 import { useLocation, useParams } from "react-router-dom"
 import { useAtomValue } from "jotai"
 import { selectedArticlesAtom } from "./atomConfigs"
-import { ListNewsContentQueryResult } from "dicty-graphql-schema"
 import CreateButton from "./CreateButton"
 import DeleteButton from "./DeleteButton"
 import EditButton from "./EditButton"
 import useToolbarStyles from "./useToolbarStyles"
 import { getURLPathSegments } from "./utils"
 
-type NewsToolbarProperties = {
-  refetch: ListNewsContentQueryResult["refetch"]
-}
-
-const NewsToolbar = ({ refetch }) => {
+const NewsToolbar = () => {
   const { root } = useToolbarStyles()
   const selectedArticles = useAtomValue(selectedArticlesAtom)
   const location = useLocation()
