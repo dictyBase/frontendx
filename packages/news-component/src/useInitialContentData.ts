86aab3833562c753e306a5d5795c05e0f0f21517
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-26 09:32:36 -0500

    refactor(useInitialContentData.ts): simplify useInitialContentData hook
    The useInitialContentData hook has been simplified to return only the contentBySlug data object. This improves the readability and maintainability of the code.

--- packages/news-component/src/useInitialContentData.ts
+++ packages/news-component/src/useInitialContentData.ts
@@ -3,26 +3,13 @@ import { useParams } from "react-router-dom"
 // import { getURLPathSegments } from "./utils"
 
 const useInitialContentData = () => {
-  let initialTitle = ""
-  let initialId = ""
-  // eslint-disable-next-line unicorn/no-null
-  let initialContent = null
-  let initialSlug = ""
   const { slug } = useParams()
   const { data } = useContentBySlugQuery({
     variables: {
       slug: `${slug}`,
     },
   })
-
-  if (data) {
-    initialSlug = slug || ""
-    initialId = data.contentBySlug?.id || ""
-    initialTitle = data.contentBySlug?.name || ""
-    // eslint-disable-next-line unicorn/no-null
-    initialContent = data.contentBySlug?.content || null
-  }
-
-  return { initialId, initialTitle, initialSlug, initialContent }
+  return data?.contentBySlug
 }
+
 export default useInitialContentData
