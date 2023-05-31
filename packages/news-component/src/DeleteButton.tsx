4b5e26496291044568ef8cc20259987be35a08f9
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-31 17:20:13 -0500

    feat(DeleteButton.tsx): add navigation to news page after deleting articles
    The DeleteButton component now uses the useNavigate hook from react-router-dom to navigate to the news page after deleting articles. This causes a rerender of the BrowseNews component which triggers a refetch of the updated news data.

--- packages/news-component/src/DeleteButton.tsx
+++ packages/news-component/src/DeleteButton.tsx
@@ -1,23 +1,26 @@
 import { IconButton } from "@material-ui/core"
 import DeleteIcon from "@material-ui/icons/Delete"
 import { useDeleteContentBySlugMutation } from "dicty-graphql-schema"
+import { useNavigate } from "react-router-dom"
 import { useAtomValue, useSetAtom } from "jotai"
 import { selectedArticlesAtom, clearSelectedArticles } from "./atomConfigs"
 
 const DeleteButton = () => {
+  const navigate = useNavigate()
   const [deleteContent] = useDeleteContentBySlugMutation()
   const selectedArticles = useAtomValue(selectedArticlesAtom)
   const clearArticles = useSetAtom(clearSelectedArticles)
 
   const onClick = () => {
-    selectedArticles.forEach((articleSlug) => {
-      deleteContent({
+    selectedArticles.forEach(async (articleSlug) => {
+      await deleteContent({
         variables: {
           slug: articleSlug,
         },
       })
     })
     clearArticles()
+    navigate("/news", { replace: true })
   }
 
   return (
