fe0fb1d4525199e26e631c2a5bce563074b0db26
Kevin Tun <kevin.tun@northwestern.edu>
2023-05-31 17:21:12 -0500

    refactor(BrowseNews.tsx): remove unused selectedArticlesAtom
    The selectedArticlesAtom was not being used, so it was removed. The useListNewsContentQuery hook was updated to include variables for limiting the number of articles returned.

--- packages/news-component/src/BrowseNews.tsx
+++ packages/news-component/src/BrowseNews.tsx
@@ -1,8 +1,6 @@
 import { useEffect } from "react"
 import { Grid, Container } from "@material-ui/core"
 import { useListNewsContentQuery } from "dicty-graphql-schema"
-import { useAtomValue } from "jotai"
-import { selectedArticlesAtom } from "./atomConfigs"
 import NewsList from "./NewsList"
 import NewsHeader from "./NewsHeader"
 import NewsToolbar from "./Toolbar"
@@ -11,12 +9,12 @@ import NewsToolbar from "./Toolbar"
  * @returns
  */
 const BrowseNews = () => {
-  const selectedArticles = useAtomValue(selectedArticlesAtom)
-  const { loading, error, data, refetch } = useListNewsContentQuery()
-
+  const { loading, error, data, refetch } = useListNewsContentQuery({
+    variables: { limit: 20 },
+  })
   useEffect(() => {
     refetch()
-  }, [selectedArticles, refetch])
+  })
 
   if (error) return <div> Error </div>
   if (loading) return <div> Loading </div>
@@ -31,7 +29,9 @@ const BrowseNews = () => {
         <Grid item>
           <NewsToolbar />
         </Grid>
-        <NewsList articles={data?.listContent} />
+        {error ? <div> Error </div> : <></>}
+        {loading ? <div> Loading </div> : <></>}
+        {data?.listContent ? <NewsList articles={data?.listContent} /> : <></>}
       </Grid>
     </Container>
   )
