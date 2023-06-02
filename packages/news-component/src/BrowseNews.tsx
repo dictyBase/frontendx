0a28817cde9b26d9d32253c02be475ffcddde522
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-02 16:53:56 -0500

    feat(BrowseNews.tsx): add pagination to news list
    This commit adds pagination to the news list by using the `Pagination` component. The `articlesInRange` atom is used to slice the `listContent` array returned by the `useListNewsContentQuery` hook. The `articlesListTotalAtom` atom is used to set the total number of articles in the list. This allows the `Pagination` component to calculate the number of pages needed to display all the articles.
    issue #231

--- packages/news-component/src/BrowseNews.tsx
+++ packages/news-component/src/BrowseNews.tsx
@@ -1,18 +1,23 @@
 import { useEffect } from "react"
 import { Grid, Container } from "@material-ui/core"
 import { useListNewsContentQuery } from "dicty-graphql-schema"
+import { useAtomValue, useSetAtom } from "jotai"
+import { articlesInRangeAtom, articlesListTotalAtom } from "./atomConfigs"
 import NewsList from "./NewsList"
 import NewsHeader from "./NewsHeader"
 import NewsToolbar from "./Toolbar"
+import Pagination from "./Pagination"
 /**
  * Renders a list of news articles.
  * @returns
  */
 const BrowseNews = () => {
-  const { loading, error, data, refetch } = useListNewsContentQuery({
-    variables: { limit: 20 },
-  })
+  const articlesInRange = useAtomValue(articlesInRangeAtom)
+  const setTotalArticles = useSetAtom(articlesListTotalAtom)
+  const { loading, error, data, refetch } = useListNewsContentQuery()
+
   useEffect(() => {
+    setTotalArticles(data?.listContent?.length || 0)
     refetch()
   })
 
@@ -31,7 +36,23 @@ const BrowseNews = () => {
         </Grid>
         {error ? <div> Error </div> : <></>}
         {loading ? <div> Loading </div> : <></>}
-        {data?.listContent ? <NewsList articles={data?.listContent} /> : <></>}
+        {data?.listContent ? (
+          <NewsList
+            articles={data?.listContent.slice(
+              articlesInRange[0],
+              articlesInRange[1],
+            )}
+          />
+        ) : (
+          <></>
+        )}
+        {data?.listContent ? (
+          <Grid item>
+            <Pagination />
+          </Grid>
+        ) : (
+          <></>
+        )}
       </Grid>
     </Container>
   )
