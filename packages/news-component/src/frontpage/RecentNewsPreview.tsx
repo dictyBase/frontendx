82da8c0dcda689878d2d42e30c8e29ed232891f7
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-02 09:39:40 -0500

    refactor(RecentNewsPreview.tsx): add author name to RecentNewsPreview component
    The RecentNewsPreview component now displays the name of the author who last updated the article. This is achieved by adding the author's first and last name to the component. Additionally, the layout of the component has been improved by using a Grid container to space out the elements.

--- packages/news-component/src/frontpage/RecentNewsPreview.tsx
+++ packages/news-component/src/frontpage/RecentNewsPreview.tsx
@@ -1,17 +1,15 @@
 import { Paper, Typography, Grid } from "@material-ui/core"
 import { Link } from "react-router-dom"
+import { ListNewsContentQuery } from "dicty-graphql-schema"
 import { parseContentToText } from "dicty-editor"
 import useRecentNewsPreviewStyles from "./useRecentNewsPreviewStyles"
 import { formatDateISOString } from "../utils"
 
+type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never
+
 type NewsPreviewProperties = {
-  article: {
-    id: string
-    slug: string
-    name: string
-    updatedAt: string
-    content: string
-  }
+  article: ArrayElement<NonNullable<ListNewsContentQuery["listContent"]>>
 }
 
 const RecentNewsPreview = ({ article }: NewsPreviewProperties) => {
@@ -22,12 +20,19 @@ const RecentNewsPreview = ({ article }: NewsPreviewProperties) => {
     <Paper className={root}>
       <Grid container direction="row" wrap="nowrap">
         <Grid item>
-          <>
-            <Link to={`/news/${article.slug}`}>
-              <Typography variant="h2">{article.name}</Typography>
-            </Link>
-            <Typography>{formatDateISOString(article.updatedAt)}</Typography>
-          </>
+          <Grid container justifyContent="space-between">
+            <Grid item>
+              <Link to={`/news/${article.slug}`}>
+                <Typography variant="h2">{article.name}</Typography>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Typography>{formatDateISOString(article.updatedAt)}</Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Typography>{`by ${article.updatedBy.firstName} ${article.updatedBy.lastName}`}</Typography>
+          </Grid>
           <Typography>{previewText}</Typography>
         </Grid>
       </Grid>
