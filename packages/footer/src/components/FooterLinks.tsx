cc86942a29fd2f2abb02434e61efa69bb05c72ca
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-06 04:18:42 -0500

    refactor(FooterLinks.tsx): simplify FooterLinks component
    The FooterLinks component has been simplified by removing the return statement and curly braces. This improves the readability of the code and reduces the number of lines.
    issue #250

--- packages/footer/src/components/FooterLinks.tsx
+++ packages/footer/src/components/FooterLinks.tsx
@@ -1,10 +1,8 @@
 import { Grid } from "@material-ui/core"
 import { FooterLinksProperties } from "../types"
 
-export const FooterLinks = ({ links }: FooterLinksProperties) => {
-  return (
-    <Grid item container justifyContent="center">
-      {links}
-    </Grid>
-  )
-}
+export const FooterLinks = ({ links }: FooterLinksProperties) => (
+  <Grid item container justifyContent="center">
+    {links}
+  </Grid>
+)
