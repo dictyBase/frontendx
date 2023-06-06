df410509ccb3891dea84c6365b7176faa7de3571
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-06 04:22:52 -0500

    feat(FooterContainer.tsx): add support for children components
    The FooterHead, FooterSponsors, and FooterLinks components have been removed and replaced with support for children components. This change improves the flexibility of the FooterContainer component as it can now accept any child components.
    issue #250

--- packages/footer/src/components/FooterContainer.tsx
+++ packages/footer/src/components/FooterContainer.tsx
@@ -1,23 +1,15 @@
 import { Box, Grid } from "@material-ui/core"
 import { FooterContainerProperties } from "../types"
 import { footerStyles } from "../styles/footerStyles"
-import { FooterHead } from "./FooterHead"
-import { FooterSponsors } from "./FooterSponsors"
-import { FooterLinks } from "./FooterLinks"
 
-export const FooterContainer = ({
-  links,
-  title,
-}: FooterContainerProperties) => {
+export const FooterContainer = ({ children }: FooterContainerProperties) => {
   const classes = footerStyles()
 
   return (
     <footer className={classes.footer}>
       <Box px={2}>
         <Grid container justifyContent="center">
-          <FooterHead title={title} />
-          <FooterLinks links={links} />
-          <FooterSponsors />
+          {children}
         </Grid>
       </Box>
     </footer>
