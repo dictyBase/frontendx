a508d49b6d2c2203d631d1ee3df28b2831964443
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-06 04:17:55 -0500

    refactor(types): rename FooterProperties children prop to links
    The children prop in FooterProperties has been renamed to links to better reflect its purpose. Similarly, the links prop in FooterContainerProperties has been renamed to children to better reflect its purpose.
    issue #250

--- packages/footer/src/types/index.ts
+++ packages/footer/src/types/index.ts
@@ -1,15 +1,15 @@
 import { Theme } from "@material-ui/core"
+import { Comp } from "@dictybase/functional"
 import React from "react"
 
 export interface FooterProperties {
   title: string
-  children: React.ReactNode
+  links: React.ReactNode
   theme?: Partial<Theme>
 }
 
 export interface FooterContainerProperties {
-  title: string
-  links: React.ReactNode
+  children: Comp
 }
 
 export interface FooterLinksProperties {
