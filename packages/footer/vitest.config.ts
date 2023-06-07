cabea71249f9c1d392a34c488fce6186c7d7c9b6
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-07 16:04:30 -0500

    feat(footer): add vitest setup file and vitest configuration
    This commit adds a vitest setup file to the footer package's test directory. The setup file imports and extends the expect method from the vitest library with methods from the react-testing-library. It also runs a cleanup function after each test case to clear the jsdom.
    Additionally, a vitest configuration file is added to the footer package's root directory. The configuration file defines the test environment as jsdom, sets globals to true, specifies the setup file to use, and enables mock reset.

---
+++ packages/footer/vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/__tests__/setup.ts",
+    mockReset: true,
+  },
+})
