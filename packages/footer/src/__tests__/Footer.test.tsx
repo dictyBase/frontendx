7cbb9c0d3d4a6717667d8f8c5844a38b73f7e477
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-07 16:13:55 -0500

    test(footer): refactor Footer test to use functional component and footerData
    The Footer test has been refactored to use the functional component and footerData. This simplifies the test and makes it more readable. The test now checks if each link in the footerData is present in the document and has the correct href attribute.

--- packages/footer/src/__tests__/Footer.test.tsx
+++ packages/footer/src/__tests__/Footer.test.tsx
@@ -1,42 +1,14 @@
+import { test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
-import { Footer, FooterItem, FooterLink } from ".."
+import { Footer } from "../functional/Footer"
+import { footerData } from ".."
 
-describe("Footer", () => {
-  test("render footer with no links", () => {
-    const title = "dictyBase"
-    render(
-      <Footer title={title}>
-        <></>
-      </Footer>,
-    )
+test("renders footer with links of footer data", () => {
+  render(<Footer />)
 
-    expect(screen.getByText(title)).not.toBeNull()
-    expect(screen.getByText(/Supported by/)).not.toBeNull()
-    expect(screen.getByText("NIH")).not.toBeNull()
-    expect(screen.getByText("NIGMS")).not.toBeNull()
-  })
-
-  test("render footer with links", () => {
-    // Generate links
-    const links: Array<FooterItem> = []
-    for (let i = 0; i < 5; ++i) {
-      links.push({ url: `/link${i + 1}`, label: `Link ${i + 1}` })
-    }
-
-    const title = "Dicty Community Resource"
-
-    render(
-      <Footer title={title}>
-        {links.map(({ label, url }, i) => (
-          <FooterLink label={label} url={url} key={i} />
-        ))}
-      </Footer>,
-    )
-
-    expect(screen.getByText(title)).not.toBeNull()
-    expect(screen.getByText(/Supported by/)).not.toBeNull()
-
-    // Check if generate links are found in the document
-    links.forEach((item) => expect(screen.getByText(item.label)).not.toBeNull())
+  footerData.forEach((footerItem) => {
+    const link = screen.getByRole("link", { name: footerItem.label })
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute("href", footerItem.url)
   })
 })
