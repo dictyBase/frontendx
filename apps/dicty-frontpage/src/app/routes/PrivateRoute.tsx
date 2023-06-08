aa737dbe77e203458ccfc1c3a0fe8a1009159ab3
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-08 16:15:36 -0500

    refactor(PrivateRoute.tsx): use react-router-dom's Outlet component
    The PrivateRoute component has been refactored to use the react-router-dom's Outlet component. The Outlet component is used to render nested child routes.

--- apps/dicty-frontpage/src/app/routes/PrivateRoute.tsx
+++ apps/dicty-frontpage/src/app/routes/PrivateRoute.tsx
@@ -1,33 +1,32 @@
-import React from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 import { useAuthStore } from "../../features/Authentication/AuthStore"
 
 /**
  * PrivateRoute redirects user from route if not authenticated.
  * This uses the same API as <Route/>
  */
-const PrivateRoute = ({ children }: any) => {
+
+/*
+  Changes to private routes in react router 6
+  https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f#route-composition-in-react-router-v6
+*/
+const PrivateRoute = () => {
   /* Fix this later in the future */
   const {
     state: { isAuthenticated },
   } = useAuthStore()
 
-  if (isAuthenticated) {
-    return children
+  if (!isAuthenticated) {
+    return (
+      <Navigate
+        to={{
+          pathname: "/login",
+        }}
+        state={{ error: "You must be logged in to view this page!" }}
+      />
+    )
   }
-
-  /*
-    Changes to private routes in react router 6
-    https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f#route-composition-in-react-router-v6
-  */
-  return (
-    <Navigate
-      to={{
-        pathname: "/login",
-      }}
-      state={{ error: "You must be logged in to view this page!" }}
-    />
-  )
+  return <Outlet />
 }
 
 export default PrivateRoute
