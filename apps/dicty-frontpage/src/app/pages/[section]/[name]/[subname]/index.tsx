f9faf9a793d0580a1f3f22a006ea13796ae3bd4f
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-14 16:30:12 -0500

    feat(frontpage): add new pages and set access levels
    This commit adds several new pages to the frontpage application and sets their access levels. The new pages are:
    - callback.tsx
    - edit.tsx
    - index.tsx
    - about.tsx
    - addpage.tsx
    - downloads/index.tsx
    - auth.tsx
    - login.tsx
    - logout.tsx
    - news/[slug]/edit.tsx
    - news/[slug]/index.tsx
    - news/create.tsx
    - news/index.tsx
    - privacy-policy.tsx
    
    The access levels are set using the ACCESS constant from the routes/types file. The access levels are either public, protected, or private.

---
+++ apps/dicty-frontpage/src/app/pages/[section]/[name]/[subname]/index.tsx
@@ -0,0 +1,6 @@
+import InfoPageContainer from "../../../../../features/EditablePages/InfoPageContainer"
+import { ACCESS } from "../../../../routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export default InfoPageContainer
+export const access = ACCESS.public
---
+++ apps/dicty-frontpage/src/app/pages/load/auth.tsx
@@ -0,0 +1,6 @@
+import AuthLoader from "../../../features/Authentication/AuthLoader"
+import { ACCESS } from "../../routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export default AuthLoader
+export const access = ACCESS.public
