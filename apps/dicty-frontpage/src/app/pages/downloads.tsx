2f03192d9c1174d0d4fac672d0ab5688e58602c3
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-14 16:59:15 -0500

    chore(downloads): flatten downloads page directory structure
    Instead of nesting the Downloads component in a downloads folder, the Downloads component is now imported from downloads.tsx in the pages directory.

---
+++ apps/dicty-frontpage/src/app/pages/downloads.tsx
@@ -0,0 +1,6 @@
+import DownloadContainer from "../../features/Downloads/DownloadsContainer"
+import { ACCESS } from "../routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from
+export default DownloadContainer
+export const access = ACCESS.private
