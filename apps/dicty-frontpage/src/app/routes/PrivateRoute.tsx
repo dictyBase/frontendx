a1406e99b0760770241974047f4b0d83f1aed270
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-14 17:20:52 -0500

    fix(PrivateRoute.tsx): return children instead of Outlet
    Returning children from the PrivateRoute components allows us to wrap a component and redirect to the login page if the user is not authenticated when trying to access that page.

--- apps/dicty-frontpage/src/app/routes/PrivateRoute.tsx
+++ apps/dicty-frontpage/src/app/routes/PrivateRoute.tsx
@@ -1,6 +1,9 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 import { useAuthStore } from "../../features/Authentication/AuthStore"
 
+type PrivateRouteProperties = {
+  children: JSX.Element
+}
 /**
  * PrivateRoute redirects user from route if not authenticated.
  * This uses the same API as <Route/>
@@ -10,7 +13,7 @@ import { useAuthStore } from "../../features/Authentication/AuthStore"
   Changes to private routes in react router 6
   https://gist.github.com/mjackson/d54b40a094277b7afdd6b81f51a0393f#route-composition-in-react-router-v6
 */
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }: PrivateRouteProperties) => {
   /* Fix this later in the future */
   const {
     state: { isAuthenticated },
@@ -26,7 +29,7 @@ const PrivateRoute = () => {
       />
     )
   }
-  return <Outlet />
+  return children
 }
 
 export default PrivateRoute
