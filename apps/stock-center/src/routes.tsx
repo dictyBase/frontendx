f9d913d41931c9dcd58a3876e2c5d9a509058f8b
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-19 13:46:49 -0500

    feat(routes.tsx): add dynamic route generation for pages
    This commit adds a new file, `routes.tsx`, which generates routes dynamically for all pages in the `/src/pages` directory. The `parsePath` function is used to convert the file path to a route path, and the `pages` object is generated using `import.meta.glob`. The resulting `routes` array contains objects with `path`, `component`, and `access` properties, which can be used to generate the application's routes.

---
+++ apps/stock-center/src/routes.tsx
@@ -0,0 +1,26 @@
+import { type FunctionComponent } from "react"
+import { ACCESS } from "./types"
+
+type PageImport = {
+  default: FunctionComponent
+  access: ACCESS
+}
+
+const parsePath = (path: string) =>
+  path
+    .replaceAll(/\/src\/pages\/|\/index|\.tsx$/g, "")
+    .replace(/\[\.{3}.+]/, "*")
+    .replaceAll(/\[([^/]+)]/g, ":$1")
+
+const pages: Record<string, PageImport> = import.meta.glob(
+  "/src/pages/**/**/*.tsx",
+  {
+    eager: true,
+  },
+)
+
+export const routes = Object.keys(pages).map((route) => ({
+  path: parsePath(route),
+  component: pages[route].default,
+  access: pages[route].access,
+}))
