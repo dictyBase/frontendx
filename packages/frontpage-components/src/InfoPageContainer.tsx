cd71c32380ed6bbad8598a430d788ac5d38f69b4
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-19 16:08:42 -0500

    feat(frontpage-components): add frontpage-components package
    This commit adds several new components to the frontpage-components package, including BackToHomepageButton, ErrorBoundary, ErrorMessage, ErrorNotification, Fallback, GraphQLErrorPage, InfoPageContainer, and InfoPageView. It also updates the package.json file to include new dependencies and devDependencies, including @apollo/client, @material-ui/core, @material-ui/icons, @material-ui/lab, and eslint-config-custom.

---
+++ packages/frontpage-components/src/InfoPageContainer.tsx
@@ -0,0 +1,79 @@
+import { useParams, useLocation, useNavigate } from "react-router-dom"
+import { Helmet } from "react-helmet"
+import Container from "@material-ui/core/Container"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import Loader from "./Loader"
+import GraphQLErrorPage from "./GraphQLErrorPage"
+import { pageTitleLookup } from "./utils/pageTitleConversions"
+import InfoPageViewToolbar from "./InfoPageViewToolbar"
+import Fallback from "./Fallback"
+import InfoPageView from "./InfoPageView"
+
+// getSlug will use the route's :subname or :name to fetch page content
+// unless the route is for the privacy policy
+const getSlug = (pathname: string, name?: string, subname?: string) => {
+  if (pathname === "/privacy-policy" || pathname === "/privacy-policy/") {
+    return "privacy-policy"
+  }
+
+  if (subname) return subname
+  return name
+}
+
+/**
+ * InfoPageContainer fetches the data for the desired editable page.
+ */
+
+const InfoPageContainer = () => {
+  const { pathname } = useLocation()
+  const { name, subname } = useParams()
+  const navigate = useNavigate()
+  const slug = getSlug(pathname, name, subname)
+
+  const { loading, error, data } = useContentBySlugQuery({
+    variables: {
+      slug: `${slug}`,
+    },
+    fetchPolicy: "cache-and-network",
+  })
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    navigate(`${pathname}/edit`, {
+      state: {
+        data,
+      },
+    })
+  }
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error) {
+    return <GraphQLErrorPage error={error} />
+  }
+
+  if (slug === undefined || !data?.contentBySlug) {
+    return <Fallback />
+  }
+
+  return (
+    <>
+      <Helmet>
+        <title>{pageTitleLookup(slug)} - dictyBase</title>
+      </Helmet>
+      <Container maxWidth="lg">
+        {/* <InfoPageViewToolbar
+          handleClick={handleClick}
+          lastUpdate={data.contentBySlug.updatedAt}
+          user={data.contentBySlug.updatedBy}
+        /> */}
+        <InfoPageView data={data?.contentBySlug} />
+      </Container>
+    </>
+  )
+}
+
+export { getSlug }
+export default InfoPageContainer
