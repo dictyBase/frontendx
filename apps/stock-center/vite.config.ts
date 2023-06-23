39646870e83998afa6189db1a96e85a8cba5f95c
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-23 16:43:18 -0500

    feat(vite.config.ts): add vitest configuration for testing
    This commit adds a vitest configuration for testing. The vitest configuration includes the setup for the testing environment, such as the globals, environment, and setup files. The mergeConfig function is used to merge the viteConfig and vitestConfig objects.

--- apps/stock-center/vite.config.ts
+++ apps/stock-center/vite.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from "vite"
-import { swcReactRefresh } from "vite-plugin-swc-react-refresh"
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-export default defineConfig({
-  plugins: [swcReactRefresh()],
-  esbuild: { jsx: "automatic" },
-  envPrefix: "REACT_APP_",
+import react from "@vitejs/plugin-react"
+import { defineConfig, mergeConfig } from "vite"
+import { defineConfig as defineVitestConfig } from "vitest/config"
+
+// https://vitejs.dev/config/
+const viteConfig = defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/__tests__/setup.ts"],
+  },
+})
+
+export default mergeConfig(viteConfig, vitestConfig)
