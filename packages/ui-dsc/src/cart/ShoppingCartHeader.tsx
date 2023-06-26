6eb938e0766dcd277ed8b54c5ca4ad5df6ce9f2d
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-26 14:37:37 -0500

    feat(ui-dsc): add ShoppingCartHeader component
    The ShoppingCartHeader component is added to the ui-dsc package. It is a simple component that renders a title and a divider for the shopping cart page. It also includes a Helmet component to set the title and meta description of the page.

---
+++ packages/ui-dsc/src/cart/ShoppingCartHeader.tsx
@@ -0,0 +1,33 @@
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import Divider from "@material-ui/core/Divider"
+import { Helmet } from "react-helmet"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    marginTop: theme.spacing(1),
+  },
+}))
+
+const ShoppingCartHeader = () => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <Helmet>
+        <title>Shopping Cart - Dicty Stock Center</title>
+        <meta
+          name="description"
+          content="Shopping cart page for Dicty Stock Center"
+        />
+      </Helmet>
+      <Box marginTop={3} marginBottom={3}>
+        <Typography variant="h1">Your Shopping Cart</Typography>
+        <Divider className={classes.divider} />
+      </Box>
+    </>
+  )
+}
+
+export { ShoppingCartHeader }
