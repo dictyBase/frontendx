8cec0794d9d1198ac96024129f3ff43ac571b697
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-26 14:35:24 -0500

    feat(ui-dsc): add test data for unit tests
    This commit adds test data to be used in unit tests. The test data consists of an array of objects with properties such as id, summary, label, quantity, and fee. This data will be used to test various functions in the ui-dsc package.

---
+++ packages/ui-dsc/src/__tests__/testData.ts
@@ -0,0 +1,25 @@
+const testItems = [
+  {
+    id: "DBS-1",
+    summary: "This is a test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 10,
+  },
+  {
+    id: "DBS-2",
+    summary: "This is another test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 15,
+  },
+  {
+    id: "DBS-3",
+    summary: "This is the last test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 19.79,
+  },
+]
+
+export { testItems }
