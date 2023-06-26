0409389f923de3c5c7a7630393bb6af37a6f36fb
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-26 16:28:00 -0500

    feat(cart.tsx): add deleteItem function to ShoppingCartWithItems component
    The state and atom variables have been renamed to better reflect their purpose. The totalItemsAtom and cartFullAtom have been renamed to strainItemsAtom and isFullAtom respectively. The ShoppingCartWithItems component now has a deleteItem function that allows users to remove items from the cart.

--- apps/stock-center/src/pages/cart.tsx
+++ apps/stock-center/src/pages/cart.tsx
@@ -1,16 +1,22 @@
-import { useAtomValue } from "jotai"
-import { EmptyCart } from "@dictybase/ui-dsc"
-import ShoppingCartWithItems from "../components/ShoppingCartWithItems"
-import { totalItemsAtom, cartFullAtom } from "../state"
+import { useAtomValue, useSetAtom } from "jotai"
+import { EmptyCart } from "../components/EmptyCart"
+import { ShoppingCartWithItems } from "../components/ShoppingCartWithItems"
+import { strainItemsAtom, deleteItemAtom, isFullAtom } from "../cartState"
 /** ShoppingCartPage displays different UIs based on whether
  *  there are currently items in the cart. * */
 const CartHandler = () => {
-  const items = useAtomValue(totalItemsAtom)
-  const isFull = useAtomValue(cartFullAtom)
+  const items = useAtomValue(strainItemsAtom)
+  const isFull = useAtomValue(isFullAtom)
+  const deleteItem = useSetAtom(deleteItemAtom)
+
   return (
     <>
       {items.length > 0 ? (
-        <ShoppingCartWithItems items={items} isFull={isFull} />
+        <ShoppingCartWithItems
+          items={items}
+          isFull={isFull}
+          deleteItem={deleteItem}
+        />
       ) : (
         <EmptyCart />
       )}
