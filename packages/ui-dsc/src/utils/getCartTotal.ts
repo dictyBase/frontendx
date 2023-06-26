3334b26fd1956004808a0bd0b7cae62514e4dd63
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-26 17:18:28 -0500

    refactor(getCartTotal.ts): use StrainItems type
    The Cart type was removed from the codebase as it was no longer being used. Instead, the StrainItems type used in getCartTotal.

--- packages/ui-dsc/src/utils/getCartTotal.ts
+++ packages/ui-dsc/src/utils/getCartTotal.ts
@@ -1,7 +1,7 @@
-import { type Cart } from "../types"
+import { type StrainItems } from "../types"
 import { toCurrencyString } from "./toCurrencyString"
 
-const getCartTotal = (items: Cart["strainItems"]) => {
+const getCartTotal = (items: StrainItems) => {
   const total = items
     .map((item) => Number(item.fee))
     .reduce((accumulator, value) => accumulator + value)
