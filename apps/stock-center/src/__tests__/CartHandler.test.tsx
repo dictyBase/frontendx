5d3c219ecc53d2028227bcf657be30edf3dd4ab2
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-27 08:44:49 -0500

    test(CartHandler.test.tsx): add tests for CartHandler component
    This commit adds two tests for the CartHandler component. The first test checks if the ShoppingCartWithItems component is rendered when there are items in the cart state, and the EmptyShoppingCart component is not rendered. The second test checks if the EmptyShoppingCart component is rendered when there are no items in the cart state, and the ShoppingCartWithItems component is not rendered.

---
+++ apps/stock-center/src/__tests__/CartHandler.test.tsx
@@ -0,0 +1,57 @@
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { Provider } from "jotai"
+import { CartHandler } from "../pages/cart"
+import { strainItemsAtom } from "../cartState"
+
+const testItems = [
+  {
+    id: "DBS-1",
+    summary: "This is a test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 10,
+  },
+  {
+    id: "DBS-2",
+    summary: "This is another test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 15,
+  },
+  {
+    id: "DBS-3",
+    summary: "This is the last test item",
+    label: "testStrain",
+    quantity: 1,
+    fee: 19.79,
+  },
+]
+
+test("Renders ShoppingCartWithItems component if there are items in the cart state. Does not render EmptyShoppingCart", () => {
+  render(
+    <MemoryRouter>
+      <Provider initialValues={[[strainItemsAtom, testItems]]}>
+        <CartHandler />
+      </Provider>
+    </MemoryRouter>,
+  )
+
+  const cartItems = screen.getAllByRole("listitem")
+  expect(cartItems).toHaveLength(3)
+  expect(screen.queryByText(/Your shopping cart is empty./)).toBeNull()
+})
+
+test("Renders EmptyShoppingCart component if there are items in the cart state. Does Not render ShoppingCartWithItems", () => {
+  render(
+    <MemoryRouter>
+      <Provider initialValues={[[strainItemsAtom, []]]}>
+        <CartHandler />
+      </Provider>
+    </MemoryRouter>,
+  )
+
+  expect(screen.getByText(/Your shopping cart is empty./)).toBeInTheDocument()
+  expect(screen.queryAllByRole("listitem")).toHaveLength(0)
+})
