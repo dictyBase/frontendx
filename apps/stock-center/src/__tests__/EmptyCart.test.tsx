5044f6eaa1ee72cca9fd24e859122d71161cf65f
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-27 08:45:19 -0500

    refactor(EmptyCart.test.tsx): remove redundant describe block
    The describe block was redundant as there was only one test suite. Removing it simplifies the code and makes it easier to read.

--- apps/stock-center/src/__tests__/EmptyCart.test.tsx
+++ apps/stock-center/src/__tests__/EmptyCart.test.tsx
@@ -1,32 +1,26 @@
-import { describe, test } from "vitest"
+import { test } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { EmptyCart } from "../components/EmptyCart"
 
-describe("features/ShoppingCart/EmptyCart", () => {
-  describe("initial component render", () => {
-    test("displays empty notification", () => {
-      render(
-        <Router>
-          <EmptyCart />
-        </Router>,
-      )
-      expect(
-        screen.getByText(/Your shopping cart is empty./),
-      ).toBeInTheDocument()
-    })
-    test("displays button links to catalogs", () => {
-      render(
-        <Router>
-          <EmptyCart />
-        </Router>,
-      )
-      expect(
-        screen.getByRole("button", { name: "Strain Catalog" }),
-      ).toBeInTheDocument()
-      expect(
-        screen.getByRole("button", { name: "Plasmid Catalog" }),
-      ).toBeInTheDocument()
-    })
-  })
+test("displays empty notification", () => {
+  render(
+    <Router>
+      <EmptyCart />
+    </Router>,
+  )
+  expect(screen.getByText(/Your shopping cart is empty./)).toBeInTheDocument()
+})
+test("displays button links to catalogs", () => {
+  render(
+    <Router>
+      <EmptyCart />
+    </Router>,
+  )
+  expect(
+    screen.getByRole("button", { name: "Strain Catalog" }),
+  ).toBeInTheDocument()
+  expect(
+    screen.getByRole("button", { name: "Plasmid Catalog" }),
+  ).toBeInTheDocument()
 })
