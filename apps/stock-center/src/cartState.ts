91534edbc5fbc863125f44d80f49af95179ee318
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-27 14:50:56 -0500

    refactor(cartState.ts): remove unused code and refactor strainItemsAtom to use splitAtom
    The maxItemsAtom, isFullAtom, and deleteItemAtom were unused and have been removed. The strainItemsAtom has been refactored to use the splitAtom function from jotai/utils, which allows for more granular control over individual items in the array.

--- apps/stock-center/src/cartState.ts
+++ apps/stock-center/src/cartState.ts
@@ -1,31 +1,19 @@
 import { atom } from "jotai"
+import { splitAtom } from "jotai/utils"
 import { type Strain } from "dicty-graphql-schema"
 
 type PurchaseProperties = { quantity: number; fee: Readonly<number> }
 type StrainItem = Pick<Strain, "id" | "summary" | "label"> & PurchaseProperties
 type StrainItems = Array<StrainItem>
-type CartItemLimit = Readonly<number>
 
 // This is the Cart state
-
 const strainItemsAtom = atom<Array<StrainItem>>([])
-const maxItemsAtom = atom<CartItemLimit>(12)
 
-const isFullAtom = atom(
-  (get) => get(strainItemsAtom).length === get(maxItemsAtom),
-)
-// eslint-disable-next-line unicorn/no-null
-const deleteItemAtom = atom(null, (get, set, deleteId) => {
-  set(
-    strainItemsAtom,
-    get(strainItemsAtom).filter((item) => item.id !== deleteId),
-  )
-})
+const strainItemAtomsAtom = splitAtom(strainItemsAtom)
 
 export {
   type StrainItems,
   type StrainItem,
   strainItemsAtom,
-  isFullAtom,
-  deleteItemAtom,
+  strainItemAtomsAtom,
 }
