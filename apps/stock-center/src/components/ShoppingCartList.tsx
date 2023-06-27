da691abc0aef96fd24be39cfd2a23a8885e81fe9
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-27 14:53:23 -0500

    feat(ShoppingCartList.tsx): rename ShoppingCartWithItems to ShoppingCartList
    It uses the ShoppingCartItem component to display each item in the cart. The component also displays the total cost of the items in the cart and a ContinueShoppingCard component if the cart is not full.

--- apps/stock-center/src/components/ShoppingCartList.tsx
+++ apps/stock-center/src/components/ShoppingCartList.tsx
@@ -1,12 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
-import {
-  ShoppingCartItem,
-  ShoppingCartTotalCard,
-  ContinueShoppingCard,
-} from "@dictybase/ui-dsc"
-import { type StrainItems } from "../cartState"
+import { ShoppingCartTotalCard, ContinueShoppingCard } from "@dictybase/ui-dsc"
+import { useAtom } from "jotai"
+import { ShoppingCartItem } from "./ShoppingCartItem"
+import { type StrainItems, strainItemAtomsAtom } from "../cartState"
+import { isFull } from "../isFull"
 
 const useStyles = makeStyles(() => ({
   list: {
@@ -14,45 +13,36 @@ const useStyles = makeStyles(() => ({
   },
 }))
 
-type ShoppingCartWithItemsProperties = {
+type ShoppingCartListProperties = {
   /** An array of cart items */
   items: StrainItems
-  /** A flag indicating whether the cart is full */
-  isFull: boolean
-  /** A callback that will be run when the user clicks a delete button on a ShoppingCartItem */
-  deleteItem: (id: string) => void
 }
-
 /**
  * ShoppingCartPageWithItems is the display for the cart page when there are
  * items in the cart.
  */
-const ShoppingCartWithItems = ({
-  items,
-  isFull,
-  deleteItem,
-}: ShoppingCartWithItemsProperties) => {
+const ShoppingCartList = ({ items }: ShoppingCartListProperties) => {
+  const [strainItemAtoms, dispatch] = useAtom(strainItemAtomsAtom)
   const classes = useStyles()
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={9}>
         <List className={classes.list}>
-          {items.map((item) => (
+          {strainItemAtoms.map((atom) => (
             <ShoppingCartItem
-              key={item.id}
-              item={item}
-              deleteItem={deleteItem}
+              itemAtom={atom}
+              deleteItem={() => dispatch({ type: "remove", atom })}
             />
           ))}
         </List>
       </Grid>
       <Grid item xs={3}>
         <ShoppingCartTotalCard items={items} />
-        {!isFull && <ContinueShoppingCard />}
+        {!isFull(items) && <ContinueShoppingCard />}
       </Grid>
     </Grid>
   )
 }
 
-export { ShoppingCartWithItems }
+export { ShoppingCartList }
