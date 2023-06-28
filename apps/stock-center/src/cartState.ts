6469f2f120379888f64ee1061bc518cd43d2b059
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-28 16:59:06 -0500

    refactor(cartState.ts): refactor cart state management using atom and splitAtom
    The previous strainItemsAtom has been replaced with a new cartAtom, which now manages the entire cart state. The strainItemsAtom is now derived from the cartAtom using the get function. This refactoring improves the organization and maintainability of the cart state management code.

--- apps/stock-center/src/cartState.ts
+++ apps/stock-center/src/cartState.ts
@@ -6,7 +6,22 @@ type PurchaseProperties = { quantity: number; fee: Readonly<number> }
 type StrainItem = Pick<Strain, "id" | "summary" | "label"> & PurchaseProperties
 type StrainItems = Array<StrainItem>
 
-const strainItemsAtom = atom<Array<StrainItem>>([])
+type Cart = {
+  strainItems: StrainItems
+}
+
+const initialCart = {
+  strainItems: [],
+}
+
+const cartAtom = atom<Cart>(initialCart)
+
+// const strainItemsAtom = atom<Array<StrainItem>>([])
+const strainItemsAtom = atom(
+  (get) => get(cartAtom).strainItems,
+  (get, set, strainItems: StrainItems) =>
+    set(cartAtom, (previous) => ({ ...previous, strainItems })),
+)
 const strainItemAtomsAtom = splitAtom(strainItemsAtom)
 
 // eslint-disable-next-line unicorn/no-null
@@ -20,6 +35,7 @@ const removeItemAtom = atom(null, (get, set, removeId) =>
 export {
   type StrainItems,
   type StrainItem,
+  cartAtom,
   strainItemsAtom,
   strainItemAtomsAtom,
   removeItemAtom,
