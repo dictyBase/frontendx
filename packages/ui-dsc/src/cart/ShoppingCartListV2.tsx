a79aa9e077c3b1d091a518d1c3001dd48f15a3d4
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-28 12:41:23 -0500

    refactor(ShoppingCartListV2.tsx): move render logic from ShoppingCartTotalCard
    This version of the ShoppingCartList component takes the conditional rendering logic from the ShoppingCartTotalCard to render the price totals of strains and plasmids.

--- packages/ui-dsc/src/cart/ShoppingCartListV2.tsx
+++ packages/ui-dsc/src/cart/ShoppingCartListV2.tsx
@@ -1,17 +1,34 @@
+import { Link } from "react-router-dom"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
 import Card from "@material-ui/core/Card"
+import Button from "@material-ui/core/Button"
 import CardActions from "@material-ui/core/CardActions"
 import Divider from "@material-ui/core/Divider"
-import { ShoppingCartTotalCard, ContinueShoppingCard } from "@dictybase/ui-dsc"
+import { ShoppingCartTotalRowV2, ContinueShoppingCard } from "@dictybase/ui-dsc"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ShoppingCartItem } from "./ShoppingCartItem"
 import { type StrainItems } from "../types"
 import { isFull } from "../utils/isFull"
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   list: {
     paddingTop: "0px",
+  },
+  container: {
+    minHeight: "170px",
+    padding: theme.spacing(2),
+  },
+  checkoutBtn: {
+    fontWeight: 1000,
+    minHeight: "50px",
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+  divider: {
+    marginBottom: theme.spacing(2),
   },
 }))
 
@@ -32,6 +49,8 @@ const ShoppingCartList = ({
 }: ShoppingCartListProperties) => {
   const classes = useStyles()
 
+  const strains = items.filter((item) => item.id.slice(0, 3) === "DBS")
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={9}>
@@ -42,7 +61,46 @@ const ShoppingCartList = ({
         </List>
       </Grid>
       <Grid item xs={3}>
-        <Card></Card>
+        <Card className={classes.container}>
+          {strains.length > 0 ? (
+            <ShoppingCartTotalRowV2
+              leftValue="Strains"
+              items={strains}
+              variant="body2"
+            />
+          ) : (
+            <></>
+          )}
+          {/* {plasmid.length > 0 ? (
+            <ShoppingCartTotalRowV2
+              leftValue="Plasmids"
+              items={plasmids}
+              variant="body2"
+            />
+          ) : (
+            <></>
+          )} */}
+          <Divider className={classes.divider} />
+          <ShoppingCartTotalRowV2
+            leftValue="Total"
+            items={items}
+            variant="h3"
+          />
+          <CardActions>
+            {/* separate to checkout button */}
+            <Button
+              component={Link}
+              to="/order/checkout"
+              color="secondary"
+              variant="contained"
+              size="large"
+              fullWidth
+              startIcon={<FontAwesomeIcon icon="shopping-cart" />}
+              className={classes.checkoutBtn}>
+              Proceed to Checkout
+            </Button>
+          </CardActions>
+        </Card>
         {!isFull(items) && <ContinueShoppingCard />}
       </Grid>
     </Grid>
