088686ed7c6ffe198a245c7bb08c4a57239560b1
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-29 00:00:18 -0500

    refactor(ShoppingCartList.tsx): update component to use cartAtom
    - Import `Card` component from `@material-ui/core/Card`
    - Update import statements for `ContinueShoppingCard`, `ShoppingCartItem`, and `ShoppingCartTotalRowV2` components from `@dictybase/ui-dsc`
    - Update type of `items` prop to `cart` in `ShoppingCartListProperties` interface
    - Update variable name from `items` to `cart` in `ShoppingCartList` component
    - Update mapping of `strainItemAtoms` to `cart.strainItems` in `ShoppingCartList` component
    - Update prop name from `items` to `cart` in `ShoppingCartTotalCard` component
    - Update condition for rendering `ContinueShoppingCard` component based on `cart.strainItems` instead of `items`
    
    The changes were made to improve code readability and maintainability. The import statements were updated to reflect the changes in the package structure. The variable

--- apps/stock-center/src/components/ShoppingCartList.tsx
+++ apps/stock-center/src/components/ShoppingCartList.tsx
@@ -1,10 +1,15 @@
+import { match } from "ts-pattern"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
-import { ShoppingCartTotalCard, ContinueShoppingCard } from "@dictybase/ui-dsc"
+import Card from "@material-ui/core/Card"
+import {
+  ContinueShoppingCard,
+  ShoppingCartItem,
+  ShoppingCartTotalRowV2,
+} from "@dictybase/ui-dsc"
 import { useAtom } from "jotai"
-import { ShoppingCartItem } from "./ShoppingCartItem"
-import { type StrainItems, strainItemAtomsAtom } from "../cartState"
+import { Cart, strainItemAtomsAtom } from "../cartState"
 import { isFull } from "../isFull"
 
 const useStyles = makeStyles(() => ({
@@ -15,13 +20,13 @@ const useStyles = makeStyles(() => ({
 
 type ShoppingCartListProperties = {
   /** An array of cart items */
-  items: StrainItems
+  cart: Cart
 }
 /**
  * ShoppingCartPageWithItems is the display for the cart page when there are
  * items in the cart.
  */
-const ShoppingCartList = ({ items }: ShoppingCartListProperties) => {
+const ShoppingCartList = ({ cart }: ShoppingCartListProperties) => {
   const [strainItemAtoms, dispatch] = useAtom(strainItemAtomsAtom)
   const classes = useStyles()
 
@@ -29,17 +34,35 @@ const ShoppingCartList = ({ items }: ShoppingCartListProperties) => {
     <Grid container spacing={2}>
       <Grid item xs={9}>
         <List className={classes.list}>
-          {strainItemAtoms.map((atom) => (
+          {cart.strainItems.map((item, index) => (
             <ShoppingCartItem
-              itemAtom={atom}
-              deleteItem={() => dispatch({ type: "remove", atom })}
+              item={item}
+              key={item.id}
+              deleteItem={() =>
+                dispatch({ type: "remove", atom: strainItemAtoms[index] })
+              }
             />
           ))}
         </List>
       </Grid>
       <Grid item xs={3}>
-        <ShoppingCartTotalCard items={items} />
-        {!isFull(items) && <ContinueShoppingCard />}
+        <Card>
+          {match(cart)
+            .when(
+              ({ strainItems }) => strainItems.length > 0,
+              ({ strainItems }) => (
+                <ShoppingCartTotalRowV2
+                  leftValue="Strains"
+                  items={strainItems}
+                  variant="body2"
+                />
+              ),
+            )
+            .otherwise(() => (
+              <></>
+            ))}
+        </Card>
+        {!isFull(cart.strainItems) && <ContinueShoppingCard />}
       </Grid>
     </Grid>
   )
