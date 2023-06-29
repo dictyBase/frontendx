a1d8e316c938e8cf1adae1ac892091de25721a79
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-29 15:50:44 -0500

    chore(types.ts): add Cart type to export
    The Cart type is added to the export statement in types.ts. This allows the Cart type to be imported and used in other files.

--- packages/ui-dsc/src/types.ts
+++ packages/ui-dsc/src/types.ts
@@ -4,5 +4,8 @@ type PurchaseProperties = { quantity: number; fee: Readonly<number> }
 type StrainItem = Pick<Strain, "id" | "summary" | "label"> & PurchaseProperties
 type StrainItems = Array<StrainItem>
 type CartItemLimit = Readonly<number>
+type Cart = {
+  strainItems: StrainItems
+}
 
-export { type StrainItem, type StrainItems, type CartItemLimit }
+export { type StrainItem, type StrainItems, type CartItemLimit, type Cart }
