9355653d9acfcd8e2606f2217e2256bb86808447
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-29 00:01:10 -0500

    feat(cart.tsx): update CartHandler component to use the new cartAtom
    The strainItemsAtom has been renamed to cartAtom to better reflect its purpose. The CartHandler component has been updated to use the new cartAtom and access the strainItems array from the cart object. This improves code clarity and consistency.

--- apps/stock-center/src/pages/cart.tsx
+++ apps/stock-center/src/pages/cart.tsx
@@ -1,7 +1,7 @@
 import { useAtomValue } from "jotai"
 import { EmptyCart } from "@dictybase/ui-dsc"
 import { ShoppingCartList } from "../components/ShoppingCartList"
-import { strainItemsAtom } from "../cartState"
+import { cartAtom } from "../cartState"
 
 /**
  * Displays different UI components based on whether there are currently items in the cart
@@ -9,10 +9,16 @@ import { strainItemsAtom } from "../cartState"
  * @returns ShoppingCartWithItems or EmptyCart
  */
 const CartHandler = () => {
-  const items = useAtomValue(strainItemsAtom)
+  const cart = useAtomValue(cartAtom)
 
   return (
-    <>{items.length > 0 ? <ShoppingCartList items={items} /> : <EmptyCart />}</>
+    <>
+      {cart.strainItems.length > 0 ? (
+        <ShoppingCartList cart={cart} />
+      ) : (
+        <EmptyCart />
+      )}
+    </>
   )
 }
 
