3d62aa18774182d0f1a1b193d6464e5b64c51cd4
Kevin Tun <kevin.tun@northwestern.edu>
2023-06-30 13:08:00 -0500

    feat(ui-dsc): add functional component for rendering cart totals
    This commit adds a new file `functional.tsx` to the `ui-dsc` package. The file contains a functional component that renders different types of cart totals. The component imports `CartTotalRow` from `@dictybase/ui-dsc`, the `Cart` type from `types.ts`, and the `getCartTotal` function from `getCartTotal.ts`. The component includes functions for rendering strain totals, plasmid totals, strain and plasmid totals together, and the overall cart total. These functions use the imported components and functions to display the appropriate information.

---
+++ packages/ui-dsc/src/functional.tsx
@@ -0,0 +1,57 @@
+import { CartTotalRow } from "@dictybase/ui-dsc"
+import { type Cart } from "./types"
+import { getCartTotal } from "./utils/getCartTotal"
+
+const renderStrainTotal = ({ strainItems }: Cart) => (
+  <CartTotalRow
+    leftValue="Strains"
+    numItems={strainItems.length}
+    total={getCartTotal(strainItems)}
+    variant="body2"
+  />
+)
+
+const renderPlasmidTotal = ({ plasmidItems }: Cart) => (
+  <CartTotalRow
+    leftValue="Plasmids"
+    numItems={plasmidItems.length}
+    total={getCartTotal(plasmidItems)}
+    variant="body2"
+  />
+)
+
+const renderStrainAndPlasmidTotals = ({ strainItems, plasmidItems }: Cart) => (
+  <>
+    <CartTotalRow
+      leftValue="Strains"
+      numItems={strainItems.length}
+      total={getCartTotal(strainItems)}
+      variant="body2"
+    />
+    <CartTotalRow
+      leftValue="Plasmids"
+      numItems={plasmidItems.length}
+      total={getCartTotal(plasmidItems)}
+      variant="body2"
+    />
+  </>
+)
+
+const renderCartTotal = ({ strainItems, plasmidItems }: Cart) => {
+  const cumulative = [...strainItems, plasmidItems]
+  return (
+    <CartTotalRow
+      leftValue="Total"
+      numItems={cumulative.length}
+      total={getCartTotal(cumulative)}
+      variant="body2"
+    />
+  )
+}
+
+export {
+  renderStrainTotal,
+  renderPlasmidTotal,
+  renderStrainAndPlasmidTotals,
+  renderCartTotal,
+}
