72ad306706418a623bf030896200f814905b9016
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-02 23:15:59 -0500

    feat(addressFields.tsx): add addressFields array
    The addressFields array will be mapped over to generate form field components for the ShippingPage.

---
+++ packages/ui-dsc/src/order/addressFields.ts
@@ -0,0 +1,64 @@
+const addressFields = [
+    {
+      name: "firstName",
+      label: "First Name",
+      required: true,
+    },
+    {
+      name: "lastName",
+      label: "Last Name",
+      required: true,
+    },
+    {
+      name: "email",
+      label: "Email",
+      required: true,
+    },
+    {
+      name: "organization",
+      label: "Organization",
+      required: true,
+    },
+    {
+      name: "lab",
+      label: "Lab/Group",
+      required: true,
+    },
+    {
+      name: "phone",
+      label: "Phone Number",
+      required: true,
+    },
+    {
+      name: "address1",
+      label: "Address",
+      required: true,
+    },
+    {
+      name: "address2",
+      label: "Address",
+      required: false,
+    },
+    {
+      name: "city",
+      label: "City",
+      required: true,
+    },
+    {
+      name: "state",
+      label: "State/Province",
+      required: false,
+    },
+    {
+      name: "country",
+      label: "Country",
+      required: true,
+    },
+    {
+      name: "zip",
+      label: "Zip Code",
+      required: true,
+    },
+  ]
+
+export { addressFields }
