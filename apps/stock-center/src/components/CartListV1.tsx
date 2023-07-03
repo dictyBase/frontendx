bf7673239d6173a889978fa694680377fe053cfd
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-02 19:35:44 -0500

    refactor(CartListV1): separate total row render logic to function
    Having the logic for rendering the total rows in the return of CartList was excessively verbose. Moving the conditional rendering logic to a separate function improves the readability of the component.

--- apps/stock-center/src/components/CartListV1.tsx
+++ apps/stock-center/src/components/CartListV1.tsx
@@ -1,6 +1,6 @@
 import { match } from "ts-pattern"
 import { pipe } from "fp-ts/function"
-import {  mapWithIndex } from "fp-ts/Array"
+import { mapWithIndex } from "fp-ts/Array"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
@@ -20,7 +20,6 @@ import { useAtom, PrimitiveAtom } from "jotai"
 import { Cart, strainItemAtomsAtom, StrainItem } from "../cartState"
 import { isFull } from "../isFull"
 
-
 type ShoppingCartListProperties = {
   /** An array of cart items */
   cart: Cart
@@ -37,13 +36,13 @@ type SplitAtomAction<Item> =
       atom: PrimitiveAtom<Item>
       before?: PrimitiveAtom<Item>
     }
-type DispatchProps = (update: SplitAtomAction<StrainItem>) => void
+type DispatchProperties = (update: SplitAtomAction<StrainItem>) => void
 type renderCartItemsPipelineProperties = {
   items: Array<StrainItem>
   fn: (index: number, item: StrainItem) => JSX.Element
 }
 type renderCartItemsProperties = {
-  dispatch: DispatchProps
+  dispatch: DispatchProperties
   strainItemAtoms: Array<PrimitiveAtom<StrainItem>>
 }
 
@@ -56,7 +55,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-
 /**
  * ShoppingCartPageWithItems is the display for the cart page when there are
  * items in the cart.
@@ -64,25 +62,48 @@ const useStyles = makeStyles((theme) => ({
 
 const renderCartItems =
   ({ dispatch, strainItemAtoms }: renderCartItemsProperties) =>
-  (index: number, item: StrainItem) =>
-    (
+  // eslint-disable-next-line react/function-component-definition
+  (index: number, item: StrainItem) => {
+    const { id } = item
+    return (
       <CartItem
         item={item}
-        key={item.id}
+        key={id}
         deleteItem={() =>
           dispatch({ type: "remove", atom: strainItemAtoms[index] })
         }
       />
     )
+  }
 const renderCartItemsPipeline = ({
   items,
   fn,
 }: renderCartItemsPipelineProperties) => pipe(items, mapWithIndex(fn))
 
+const renderTotalRows = (cart: Cart) => (
+  <>
+    {match(cart)
+      // .when(
+      //   ({ strainItems, plasmidItems }) =>
+      //     strainItems.length > 0 && plasmidItems.length > 0,
+      //   renderStrainAndPlasmidTotals,
+      // )
+      .when(({ strainItems }) => strainItems.length > 0, renderStrainTotal)
+      // .when(
+      //   ({ plasmidItems }) => plasmidItems.length > 0,
+      //   renderPlasmidTotal,
+      // )
+      .otherwise(() => (
+        <></>
+      ))}
+    {renderCartTotal(cart)}
+  </>
+)
+
 const CartList = ({ cart }: ShoppingCartListProperties) => {
   const [strainItemAtoms, dispatch] = useAtom(strainItemAtomsAtom)
   const classes = useStyles()
-  const renderCartItemsFn = renderCartItems({ dispatch, strainItemAtoms })
+  const renderCartItemsFunction = renderCartItems({ dispatch, strainItemAtoms })
 
   return (
     <Grid container spacing={2}>
@@ -90,30 +111,13 @@ const CartList = ({ cart }: ShoppingCartListProperties) => {
         <List className={classes.list}>
           {renderCartItemsPipeline({
             items: cart.strainItems,
-            fn: renderCartItemsFn,
+            fn: renderCartItemsFunction,
           })}
         </List>
       </Grid>
       <Grid item xs={3}>
         <Card>
-          {match(cart)
-            // .when(
-            //   ({ strainItems, plasmidItems }) =>
-            //     strainItems.length > 0 && plasmidItems.length > 0,
-            //   renderStrainAndPlasmidTotals,
-            // )
-            .when(
-              ({ strainItems }) => strainItems.length > 0,
-              renderStrainTotal,
-            )
-            // .when(
-            //   ({ plasmidItems }) => plasmidItems.length > 0,
-            //   renderPlasmidTotal,
-            // )
-            .otherwise(() => (
-              <></>
-            ))}
-          {renderCartTotal(cart)}
+          {renderTotalRows(cart)}
           <Divider className={classes.divider} />
           <CardActions>
             <CheckoutButton />
