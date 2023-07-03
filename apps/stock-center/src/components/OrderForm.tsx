fe0cefcec93bb5b31f8882c8e89edac4af9390a9
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-03 16:23:47 -0500

    feat(OrderForm.tsx): add OrderForm component
    The OrderForm component is added to the project. It imports necessary dependencies such as "ts-pattern", "@dictybase/ui-dsc", and "jotai". The component uses the "orderStepAtom" atom from the state file to determine the current step of the order process. Based on the value of "orderStep", the component renders different pages such as ShippingPage, PaymentPage, or SubmitPage. If the value of "orderStep" does not match any of the defined steps, an error message is displayed.

---
+++ apps/stock-center/src/components/OrderForm.tsx
@@ -0,0 +1,22 @@
+import { match } from "ts-pattern"
+import { ShippingPage } from "@dictybase/ui-dsc"
+import { useAtom } from "jotai"
+import { orderStepAtom, OrderSteps } from "../state"
+
+const OrderForm = () => {
+  const [orderStep, setOrderStep] = useAtom(orderStepAtom)
+
+  return (
+    <>
+      {match(orderStep)
+        .with(OrderSteps.SHIPPING, () => <ShippingPage />)
+        .with(OrderSteps.PAYMENT, () => <>PaymentPage not implemented.</>)
+        .with(OrderSteps.SUBMIT, () => <>SubmitPage not implemented.</>)
+        .otherwise(() => (
+          <>Unexpected Error in Order Form. This message should not appear.</>
+        ))}
+    </>
+  )
+}
+
+export { OrderForm }
