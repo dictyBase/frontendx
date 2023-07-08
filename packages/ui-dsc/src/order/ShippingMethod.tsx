6bfb79f5a6ff3f57a83c531c335c800971158f0a
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-07 19:10:02 -0500

    feat(ui-dsc): add ShippingMethod component
    The ShippingMethod component is added to the ui-dsc package. This component renders a panel with the title "Shipping Method" and a placeholder content. It is used to display the available shipping methods in the order process.

---
+++ packages/ui-dsc/src/order/ShippingMethod.tsx
@@ -0,0 +1,9 @@
+import { PanelWrapper } from "./PanelWrapper"
+
+const ShippingMethod = () => (
+  <PanelWrapper title="Shipping Method">
+    <> Shipping Method Here </>
+  </PanelWrapper>
+)
+
+export { ShippingMethod }
