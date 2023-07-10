89fb42bdc1d3ec8e5e69ac30d7a950afe0f05d45
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-10 13:40:25 -0500

    refactor(ShippingMethod.tsx): remove unused imports and commented code, refactor rendering of shipping number or prepaid notice
    The changes in this commit remove unused imports and commented code to improve code cleanliness. The rendering of the shipping number or prepaid notice is refactored to use a pattern matching function for better readability. Additionally, functionality is added to toggle between prepaid and non-prepaid shipping account numbers.

--- packages/ui-dsc/src/order/ShippingMethod.tsx
+++ packages/ui-dsc/src/order/ShippingMethod.tsx
@@ -1,9 +1,39 @@
+import { match } from "ts-pattern"
+import { useState } from "react"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import ShippingMethodPrepaidNotice from "./ShippingMethodPrepaidNotice"
+// import ShippingMethodRadioGroup from "./ShippingMethodRadioGroup"
 import { PanelWrapper } from "./PanelWrapper"
+import { TextField } from "./TextField"
 
-const ShippingMethod = () => (
-  <PanelWrapper title="Shipping Method">
-    <> Shipping Method Here </>
-  </PanelWrapper>
-)
+const renderShippingNumberOrPrepaidNotce = (isPrepaid: boolean) =>
+  match(isPrepaid)
+    .with(true, () => <ShippingMethodPrepaidNotice />)
+    .with(false, () => <TextField name="shippingAccountNumber" />)
+    .exhaustive()
+/**
+ * ShippingMethod contains radio buttons and a text field for listing courier
+ * information.
+ */
+const ShippingMethod = () => {
+  const [isPrepaid, setIsPrepaid] = useState(false)
+
+  return (
+    <Box mt={1} mb={2} p={2}>
+      <Typography variant="h3">Shipping Account:</Typography>
+      <Box mt={1} />
+      {/* <ShippingMethodRadioGroup
+        setShipAccountNum={setShipAccountNum}
+        setPrepaidNotice={setPrepaidNotice}
+      /> */}
+      {renderShippingNumberOrPrepaidNotce(isPrepaid)}
+      <Box mt={1} />
+      <Typography component="p">
+        <em>Note: credit card is not allowed for shipment</em>
+      </Typography>
+    </Box>
+  )
+}
 
 export { ShippingMethod }
