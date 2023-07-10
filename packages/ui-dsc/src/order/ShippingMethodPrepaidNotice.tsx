ad0ed36aecedda3810dc4d613832e109f3120e07
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-10 13:40:15 -0500

    feat(ui-dsc): add ShippingMethodPrepaidNotice component
    The ShippingMethodPrepaidNotice component is added to the ui-dsc package. This component displays a notice for sending a prepaid shipping label and includes a link to the email address where the label should be sent. This component improves the user experience by providing clear instructions for sending a prepaid shipping label.

---
+++ packages/ui-dsc/src/order/ShippingMethodPrepaidNotice.tsx
@@ -0,0 +1,31 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { grey } from "@material-ui/core/colors"
+import Typography from "@material-ui/core/Typography"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  prepaid: {
+    backgroundColor: grey[200],
+    borderRadius: theme.spacing(1),
+    margin: theme.spacing(2),
+    padding: theme.spacing(3),
+  },
+}))
+
+/**
+ * ShippingMethodPrepaidNotice contains the notice for sending a prepaid
+ * shipping label.
+ */
+const ShippingMethodPrepaidNotice = () => {
+  const classes = useStyles()
+
+  return (
+    <Typography className={classes.prepaid}>
+      If using a prepaid shipping label, please send ASAP to{" "}
+      <a href="mailto:dictystocks@northwestern.edu" target="_top">
+        dictystocks@northwestern.edu
+      </a>
+    </Typography>
+  )
+}
+
+export default ShippingMethodPrepaidNotice
