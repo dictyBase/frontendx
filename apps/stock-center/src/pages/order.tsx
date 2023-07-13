cf18f850867a6378e38b71949664e6212e65df73
Kevin Tun <43506054+ktun95@users.noreply.github.com>
2023-07-13 15:56:27 +0000

    feat(order.tsx): remove unnecessary setFormData and nextStep functions from ShippingPage component
    The useAtom hook has been replaced with useAtomValue in order to improve performance and simplify the code. The setFormData and nextStep functions have been removed from the props passed to the ShippingPage component as they were unnecessary.

--- apps/stock-center/src/pages/order.tsx
+++ apps/stock-center/src/pages/order.tsx
@@ -1,20 +1,15 @@
 import { match } from "ts-pattern"
 import { ShippingPage } from "@dictybase/ui-dsc"
-import { useAtom } from "jotai"
+import { useAtomValue } from "jotai"
 import { orderStepAtom, OrderSteps } from "../state"
 
 const OrderForm = () => {
-  const [orderStep, setOrderStep] = useAtom(orderStepAtom)
+  const orderStep = useAtomValue(orderStepAtom)
 
   return (
     <>
       {match(orderStep)
-        .with(OrderSteps.SHIPPING, () => (
-          <ShippingPage
-            setFormData={() => {}}
-            nextStep={() => setOrderStep((previousStep) => previousStep + 1)}
-          />
-        ))
+        .with(OrderSteps.SHIPPING, () => <ShippingPage />)
         .with(OrderSteps.PAYMENT, () => <>PaymentPage not implemented.</>)
         .with(OrderSteps.SUBMIT, () => <>SubmitPage not implemented.</>)
         .otherwise(() => (
