a3b8a56e29356b41a5b2208129ba86f65c39c2e8
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-13 15:49:33 -0500

    feat(ui-dsc): add PaymentMethod component
    The PaymentMethod component is added to the ui-dsc package. This component provides radio buttons and a text field for selecting and displaying the shipping method and account number. It also includes a notice for prepaid shipping and a note about credit card restrictions for shipment.

---
+++ packages/ui-dsc/src/order/PaymentMethod.tsx
@@ -0,0 +1,40 @@
+import { match } from "ts-pattern"
+import { useState } from "react"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import { ShippingMethodPrepaidNotice } from "./ShippingMethodPrepaidNotice"
+import { ShippingMethodRadioGroup } from "./ShippingMethodRadioGroup"
+import { PanelWrapper } from "./PanelWrapper"
+import { TextField } from "./TextField"
+
+const renderShippingNumberOrPrepaidNotice = (isPrepaid: boolean) =>
+  match(isPrepaid)
+    .with(true, () => <ShippingMethodPrepaidNotice />)
+    .with(false, () => (
+      <TextField name="shippingAccountNumber" label="Shipping Account Number" />
+    ))
+    .exhaustive()
+/**
+ * ShippingMethod contains radio buttons and a text field for listing courier
+ * information.
+ */
+const ShippingMethod = () => {
+  const [isPrepaid, setIsPrepaid] = useState(false)
+
+  return (
+    <PanelWrapper title="Payment Method">
+      <Box mt={1} mb={2} p={2}>
+        <Typography variant="h3">Shipping Account:</Typography>
+        <Box mt={1} />
+        <ShippingMethodRadioGroup setIsPrepaid={setIsPrepaid} />
+        {renderShippingNumberOrPrepaidNotice(isPrepaid)}
+        <Box mt={1} />
+        <Typography component="p">
+          <em>Note: credit card is not allowed for shipment</em>
+        </Typography>
+      </Box>
+    </PanelWrapper>
+  )
+}
+
+export { ShippingMethod }
