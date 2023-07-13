76417bc7322fe1bca75d8f66f16bda220cd43abc
Kevin Tun <43506054+ktun95@users.noreply.github.com>
2023-07-13 15:47:54 +0000

    feat(ShippingPage.tsx): add ShippingPage component for entering shipping information
    The ShippingPage component is added to the project. It includes form fields for entering shipping information such as first name, last name, email, organization, lab/group, address, city, zip code, country, phone number, and shipping account number. It also includes components for selecting shipping method and providing additional information. The form data is validated using the yup library. When the form is submitted, the data is stored in the orderFormAtom state and the orderStepAtom state is updated to proceed to the next step.

---
+++ apps/stock-center/src/components/ShippingPage.tsx
@@ -0,0 +1,76 @@
+import { useForm, FormProvider } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import Grid from "@material-ui/core/Grid"
+import { object, string, number, InferType } from "yup"
+import {
+  renderAddressFields,
+  ContinueButton,
+  AdditionalInformation,
+  ShippingMethod,
+} from "@dictybase/ui-dsc"
+import { useSetAtom } from "jotai"
+import { orderFormAtom, orderStepAtom } from "../state"
+
+const validationSchema = object().shape({
+  firstName: string().required("* First name is required"),
+  lastName: string().required("* Last name is required"),
+  email: string().email().required("* Email is required"),
+  organization: string().required("* Organization is required"),
+  lab: string().required("* Lab/Group is required"),
+  address1: string().required("* Address is required"),
+  city: string().required("* City is required"),
+  zip: number().required("* Zip code is required"),
+  country: string().required("* Country is required"),
+  phone: string().required("* Phone number is required"),
+  shippingAccountNumber: string().required(
+    "* Shipping account number is required",
+  ),
+  additionalInformation: string().notRequired(),
+})
+
+type ShippingFormData = InferType<typeof validationSchema>
+
+/**
+ * ShippingPage is the display component for when the user is entering shipping
+ * information.
+ */
+const ShippingPage = () => {
+  const setOrderFormData = useSetAtom(orderFormAtom)
+  const setOrderStep = useSetAtom(orderStepAtom)
+  const methods = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(validationSchema),
+  })
+  const { handleSubmit } = methods
+
+  const onSubmit = (data: ShippingFormData) => {
+    setOrderFormData((previousFormData) => ({ ...previousFormData, ...data }))
+    setOrderStep((previousStep) => previousStep + 1)
+  }
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            {renderAddressFields()}
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <ShippingMethod />
+              </Grid>
+              <Grid item>
+                <AdditionalInformation />
+              </Grid>
+              <Grid item>
+                <ContinueButton />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </FormProvider>
+  )
+}
+
+export { ShippingPage }
