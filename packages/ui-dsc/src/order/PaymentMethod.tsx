f354e469b531b2d4fe9683c1ca1b7a86660c17b2
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-18 04:58:38 -0500

    refactor(PaymentMethod.tsx): add conditional rendering logic for payment methods
    This commit adds a function that conditionally renders different components that correspond to the selected payment method.

--- packages/ui-dsc/src/order/PaymentMethod.tsx
+++ packages/ui-dsc/src/order/PaymentMethod.tsx
@@ -2,32 +2,44 @@ import { match } from "ts-pattern"
 import { useState } from "react"
 import Typography from "@material-ui/core/Typography"
 import Box from "@material-ui/core/Box"
-import { ShippingMethodPrepaidNotice } from "./ShippingMethodPrepaidNotice"
-import { ShippingMethodRadioGroup } from "./ShippingMethodRadioGroup"
 import { PanelWrapper } from "./PanelWrapper"
 import { TextField } from "./TextField"
+import { WaiverRequestInformation } from "./WaiverRequestInformation"
 
-const renderShippingNumberOrPrepaidNotice = (isPrepaid: boolean) =>
-  match(isPrepaid)
-    .with(true, () => <ShippingMethodPrepaidNotice />)
-    .with(false, () => (
-      <TextField name="shippingAccountNumber" label="Shipping Account Number" />
+enum PaymentMethods {
+  PURCHASE_ORDER_NUMBER,
+  WAIVER_REQUESTED,
+  CREDIT,
+  WIRE,
+}
+
+const renderPaymentMethod = (paymentMethod: PaymentMethods) =>
+  match(paymentMethod)
+    .with(PaymentMethods.PURCHASE_ORDER_NUMBER, () => (
+      <TextField
+        name="purchaseOrderNum"
+        label="Purchase Order Number"
+        placeholder="PO Number"
+      />
     ))
-    .exhaustive()
+    .with(PaymentMethods.WAIVER_REQUESTED, () => <WaiverRequestInformation />)
+    .otherwise(() => <></>)
+
 /**
  * ShippingMethod contains radio buttons and a text field for listing courier
  * information.
  */
 const ShippingMethod = () => {
-  const [isPrepaid, setIsPrepaid] = useState(false)
+  const [paymentMethod, setPaymentMethod] = useState(
+    PaymentMethods.PURCHASE_ORDER_NUMBER,
+  )
 
   return (
     <PanelWrapper title="Payment Method">
       <Box mt={1} mb={2} p={2}>
         <Typography variant="h3">Shipping Account:</Typography>
         <Box mt={1} />
-        <ShippingMethodRadioGroup setIsPrepaid={setIsPrepaid} />
-        {renderShippingNumberOrPrepaidNotice(isPrepaid)}
+        {renderPaymentMethod(paymentMethod)}
         <Box mt={1} />
         <Typography component="p">
           <em>Note: credit card is not allowed for shipment</em>
