0953b7b3461c8250fa115d932064ef9d42da9136
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-19 09:12:33 -0500

    refactor(PaymentMethodRadioGroup.tsx): refactor PaymentMethodRadioGroup to use react-hook-form and simplify logic.
    - Change the type imports for ChangeEvent, SetStateAction, and Dispatch from "react" to be imported from "react" individually.
    - Add an enum for PaymentMethods to improve type safety and readability.
    - Update the radioValues array to use the PaymentMethods enum and remove unnecessary comments.
    - Update the PaymentMethodRadioGroupProperties type to use Dispatch<SetStateAction<PaymentMethods>> for setPaymentMethod.
    - Update the handlePaymentChange function to use setPaymentMethod instead of setPurchaseOrderNum and setWaiverRequested.
    - Add the useFormContext hook from react-hook-form and register the "paymentMethod" input.
    - Update the onChange function to use setPaymentMethod with the value as PaymentMethods.
    - Remove the handleChange and values references from the RadioGroup and FormControlLabel components.
    - Export the PaymentMethodRadioGroup component using named exports.
    
    The changes were made to improve the code readability, maintainability,

--- packages/ui-dsc/src/order/PaymentMethodRadioGroup.tsx
+++ packages/ui-dsc/src/order/PaymentMethodRadioGroup.tsx
@@ -1,75 +1,65 @@
-import React from "react"
+import { type ChangeEvent, type SetStateAction, type Dispatch } from "react"
+import { useFormContext } from "react-hook-form"
 import RadioGroup from "@material-ui/core/RadioGroup"
 import Radio from "@material-ui/core/Radio"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 
-const radioValues = [
+enum PaymentMethods {
+  PURCHASE_ORDER_NUMBER = "purchaseOrder",
+  WAIVER_REQUESTED = "waiver",
+  CREDIT = "credit",
+  WIRE = "wire",
+}
+
+const radioValues: Array<{ value: PaymentMethods; label: string }> = [
   {
-    value: "credit",
+    value: PaymentMethods.PURCHASE_ORDER_NUMBER,
+    label: "Purchase Order (PO)",
+  },
+  {
+    value: PaymentMethods.CREDIT,
     label: "Credit Card",
   },
   {
-    value: "wire",
+    value: PaymentMethods.WIRE,
     label: "Wire Transfer",
   },
   {
-    value: "waiver",
+    value: PaymentMethods.WAIVER_REQUESTED,
     label: "Waiver Requested",
-  },
-  {
-    value: "purchaseOrder",
-    label: "Purchase Order (PO)",
   },
 ]
 
 type PaymentMethodRadioGroupProperties = {
-  /** Function to toggle selection of Purchase Order Number radio button */
-  setPurchaseOrderNum: (arg0: boolean) => void
-  /** Function to toggle selection of Waiver Requested radio button */
-  setWaiverRequested: (arg0: boolean) => void
+  /** Function to change the current payment method */
+  setPaymentMethod: Dispatch<SetStateAction<PaymentMethods>>
 }
 
 /**
  * PaymentMethodRadioGroup contains radio buttons for payment methods.
  */
 const PaymentMethodRadioGroup = ({
-  setPurchaseOrderNum,
-  setWaiverRequested,
+  setPaymentMethod,
 }: PaymentMethodRadioGroupProperties) => {
-  const handlePaymentChange = (event: React.ChangeEvent<any>) => {
-    switch (event.target.value) {
-      case "purchaseOrder":
-        setPurchaseOrderNum(true)
-        setWaiverRequested(false)
-        break
-      case "waiver":
-        setPurchaseOrderNum(false)
-        setWaiverRequested(true)
-        break
-      default:
-        setPurchaseOrderNum(false)
-        setWaiverRequested(false)
-    }
+  const { register } = useFormContext()
+  const onChange = ({
+    currentTarget: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setPaymentMethod(value as PaymentMethods)
   }
-
   return (
-    <RadioGroup
-      aria-label="Payment Method"
-      name="paymentMethod"
-      onChange={handleChange}
-      row>
+    <RadioGroup aria-label="Payment Method" onChange={onChange} row>
       {radioValues.map((item) => (
         <FormControlLabel
+          {...register("paymentMethod")}
           key={item.value}
           value={item.value}
           control={<Radio />}
           label={item.label}
-          onChange={handlePaymentChange}
-          checked={values["paymentMethod"] === item.value}
         />
       ))}
     </RadioGroup>
   )
 }
 
-export default PaymentMethodRadioGroup
+export { PaymentMethodRadioGroup }
