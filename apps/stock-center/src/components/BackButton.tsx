0dc2a1fadd15b622d36ed7ec19611751b0acf14d
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-19 09:05:09 -0500

    feat(BackButton.tsx): add BackButton component
    The BackButton component is added to the project. It displays a button with a back arrow icon and the text "Back". When clicked, it decreases the order step by 1 using the `setOrderStep` function from the `orderStepAtom` state. This component is used on each page of the order form to allow users to navigate back to the previous step.

---
+++ apps/stock-center/src/components/BackButton.tsx
@@ -0,0 +1,33 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace"
+import { useSetAtom } from "jotai"
+import { orderStepAtom } from "../state"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  button: {
+    marginRight: theme.spacing(1),
+    minWidth: "200px",
+  },
+}))
+
+/**
+ * BackButton displays the Back button on each page of the order form.
+ */
+const BackButton = () => {
+  const classes = useStyles()
+  const setOrderStep = useSetAtom(orderStepAtom)
+  return (
+    <Button
+      color="default"
+      size="large"
+      variant="contained"
+      className={classes.button}
+      startIcon={<KeyboardBackspaceIcon />}
+      onClick={() => setOrderStep((previousStep) => previousStep - 1)}>
+      Back
+    </Button>
+  )
+}
+
+export { BackButton }
