941a73bdfa6cf5ae8b43c5a177d1a03d55dfb7fc
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-19 13:36:42 -0500

    feat(ui-dsc): add OrderSummary component
    The OrderSummary component is added to the ui-dsc package. This component is responsible for displaying the final page where users can review their order before submitting it. It takes in form data and cart information as props and renders the order summary, including the list of items in the cart, the total price, shipping address, and payment details.

---
+++ packages/ui-dsc/src/order/OrderSummary.tsx
@@ -0,0 +1,121 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import List from "@material-ui/core/List"
+import ListItem from "@material-ui/core/ListItem"
+import ListItemText from "@material-ui/core/ListItemText"
+import Divider from "@material-ui/core/Divider"
+import { getShippingValues, getPaymentValues } from "../utils/getListValues"
+import { type FormData } from "../utils/initialFormValues"
+import { getCartTotal } from "../utils/getCartTotal"
+import { Cart } from "../types"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  details: {
+    marginBottom: theme.spacing(3),
+  },
+  listItem: {
+    padding: theme.spacing(1, 0),
+  },
+  total: {
+    fontWeight: 700,
+  },
+  title: {
+    marginTop: theme.spacing(2),
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+    padding: theme.spacing(1),
+  },
+  subheader: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+    padding: theme.spacing(1),
+  },
+  quantity: {
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    minWidth: "40px",
+  },
+}))
+
+type OrderSummaryProperties = {
+  /** Object containing all entered form data */
+  formData: FormData
+  cart: Cart
+}
+
+/**
+ * OrderSummary is the display component for the final page (Review your order)
+ * before the user submits their order.
+ */
+const OrderSummary = ({ formData, cart }: OrderSummaryProperties) => {
+  const classes = useStyles()
+
+  return (
+    <>
+      <Typography className={classes.subheader} variant="h2" gutterBottom>
+        Order Summary
+      </Typography>
+      <List disablePadding>
+        {cart.strainItems.map((item) => (
+          <ListItem className={classes.listItem} key={item.id}>
+            <ListItemText
+              primary={item.label}
+              secondary={
+                <>
+                  <Typography noWrap>
+                    <em>{item.summary}</em>
+                  </Typography>
+                  <Typography>{item.id}</Typography>
+                </>
+              }
+            />
+            <Typography
+              className={classes.quantity}
+              variant="body2"
+              data-testid="quantity">
+              Qty: {item.quantity}
+            </Typography>
+            <Typography variant="body1">
+              ${Number(item.fee) * item.quantity}.00
+            </Typography>
+          </ListItem>
+        ))}
+        <ListItem className={classes.listItem}>
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography variant="h3" className={classes.total}>
+                Total
+              </Typography>
+            }
+          />
+          <Typography variant="h3" className={classes.total}>
+            {getCartTotal(cart.strainItems)}
+          </Typography>
+        </ListItem>
+      </List>
+      <Divider />
+      <Grid container spacing={2} className={classes.details}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h2" gutterBottom className={classes.title}>
+            Shipping Address
+          </Typography>
+          {getShippingValues(formData).map((item) => (
+            <Typography key={item}>{item}</Typography>
+          ))}
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={6}>
+          <Typography variant="h2" gutterBottom className={classes.title}>
+            Payment Details
+          </Typography>
+          {getPaymentValues(formData).map((item) => (
+            <Typography key={item}>{item}</Typography>
+          ))}
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+export { OrderSummary }
