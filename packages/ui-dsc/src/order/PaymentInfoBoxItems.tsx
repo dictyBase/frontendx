ca0e41c1cad1b8327217d9a6db8cafb3d310e888
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-19 09:03:46 -0500

    feat(ui-dsc): add PaymentInfoBox component and PaymentInfoBoxItems subcomponent
    The PaymentInfoBox component is added to display general information about making a payment. It includes the PaymentInfoBoxItems subcomponent, which displays information about the payment methods listed on the payment page. This new component provides a more organized and visually appealing way to present payment information to users.

---
+++ packages/ui-dsc/src/order/PaymentInfoBoxItems.tsx
@@ -0,0 +1,28 @@
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+
+/**
+ * PaymentInfoBoxItems displays information about the payment methods listed
+ * on the payment page.
+ */
+const PaymentInfoBoxItems = () => (
+  <Box mb={1}>
+    <Typography gutterBottom>
+      <strong>Credit Card: </strong>
+      Secure payment when billed. Incurs a <strong>3.5% service fee.</strong>
+    </Typography>
+    <Typography gutterBottom>
+      <strong>Wire Transfer: </strong>
+      Northwestern bank information will be emailed.
+    </Typography>
+    <Typography gutterBottom>
+      <strong>PO: </strong>
+      Add PO number if available or send ASAP to&nbsp;
+      <a href="mailto:dictystocks@northwestern.edu" target="_top">
+        dictystocks@northwestern.edu
+      </a>
+    </Typography>
+  </Box>
+)
+
+export { PaymentInfoBoxItems }
