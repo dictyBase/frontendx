982b318e7dd9d6160ec7dc603518d53fa6349f7c
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 14:39:04 -0500

    feat(ui-dsc): add unit tests for CountryDropdown, OrderSummary, ShippingMethodPrepaidNotice, ShippingMethodRadioGroup, renderAddressFields, and isCountry functions
    Added unit tests for the following components and functions:
    - CountryDropdown.test.tsx
    - OrderSummary.test.tsx
    - ShippingMethodPrepaidNotice.test.tsx
    - ShippingMethodRadioGroup.test.tsx
    - renderAddressFields.test.tsx
    - isCountry function
    
    These tests ensure that the components and functions are working correctly and provide the expected behavior.

---
+++ packages/ui-dsc/src/__tests__/CountryDropdown.test.tsx
@@ -0,0 +1,12 @@
+import { countryToFlag } from "../order/CountryDropdown"
+
+describe("OrderForm/countryToFlag", () => {
+  it("should return expected string", () => {
+    expect(countryToFlag("IS")).toBe("🇮🇸")
+  })
+  it("should return isoCode if String.fromCodePoint is invalid", () => {
+    // @ts-ignore
+    global.String.fromCodePoint = undefined
+    expect(countryToFlag("IS")).toBe("IS")
+  })
+})
