c692ebdf8a4a52c050ac0905b6e1961da49ff8e9
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 15:23:04 -0500

    feat(ui-dsc): add SubmitError component for displaying form submission errors
    The SubmitError component is added to the ui-dsc package. This component is responsible for displaying an error message when there is an error in form submission. It includes a Material-UI Alert component with an error severity and a Typography component for the error message. The error message provides information about the error and includes a link to contact the support team via email.

---
+++ packages/ui-dsc/src/order/SubmitError.tsx
@@ -0,0 +1,33 @@
+import { makeStyles } from "@material-ui/core/styles"
+import Alert from "@material-ui/lab/Alert"
+import Typography from "@material-ui/core/Typography"
+
+const useStyles = makeStyles((theme) => ({
+  alert: {
+    marginBottom: theme.spacing(2),
+  },
+}))
+
+/**
+ * SubmitError is the display if there is an error in form submission.
+ */
+const SubmitError = () => {
+  const classes = useStyles()
+
+  return (
+    <Alert severity="error" className={classes.alert}>
+      <Typography variant="body1">
+        There was an error submitting your order. This is most likely a problem
+        on our end. If the problem persists, please email us at &nbsp;
+        <a
+          href="mailto:dictystocks@northwestern.edu?Subject=Question"
+          target="_top">
+          dictystocks@northwestern.edu
+        </a>
+        .
+      </Typography>
+    </Alert>
+  )
+}
+
+export { SubmitError }
