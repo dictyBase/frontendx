1a38c5f3b58a52d37efde358507f0f0c3ffa6abd
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 15:27:08 -0500

    refactor(getCartTotal.ts): calculate total based on quantity and fee of each item
    The getCartTotal function has been updated to calculate the total based on the quantity and fee of each item in the items array. This change ensures that the total accurately reflects the cost of all items in the cart.

--- packages/ui-dsc/src/utils/getCartTotal.ts
+++ packages/ui-dsc/src/utils/getCartTotal.ts
@@ -3,7 +3,7 @@ import { toCurrencyString } from "./toCurrencyString"
 
 const getCartTotal = (items: Array<StrainItem>) => {
   const total = items
-    .map((item) => Number(item.fee))
+    .map((item) => item.quantity * Number(item.fee))
     .reduce((accumulator, value) => accumulator + value)
   return toCurrencyString(total)
 }
