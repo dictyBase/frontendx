588085f56b2c512b93d281cc3d995005058c9268
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 15:03:34 -0500

    test(OrderSummary.test.tsx): refactor imports and update test cases
    The imports in the test file have been refactored to match the updated file structure. The test cases have been updated to use the new structure and test the correct items and address formatting.

--- packages/ui-dsc/src/__tests__/OrderSummary.test.tsx
+++ packages/ui-dsc/src/__tests__/OrderSummary.test.tsx
@@ -1,55 +1,42 @@
-import React from "react"
 import { render, screen } from "@testing-library/react"
-import OrderSummary from "./OrderSummary"
-import { MockCartProvider } from "common/utils/testing"
-import mockValues from "../utils/mockValues"
+import { OrderSummary } from "../order/OrderSummary"
+import { mockValues } from "../utils/mockValues"
+import { StrainItem } from "../types"
 
-describe("features/OrderForm/Submit/OrderSummary", () => {
+describe("OrderSummary", () => {
   const items = [
     {
-      fee: "30.00",
+      fee: 30,
+      quantity: 2,
       id: "DBS0351365",
-      name: "HL501/X55",
+      label: "HL501/X55",
       summary: "heterozygote diploid tester strain; Parents: HL501 and X55",
     },
     {
-      fee: "30.00",
-      id: "DBS0351365",
-      name: "HL501/X55",
-      summary: "heterozygote diploid tester strain; Parents: HL501 and X55",
-    },
-    {
-      fee: "15.00",
+      fee: 15,
+      quantity: 1,
       id: "DBP0001068",
-      name: "pSigK/lacZ",
+      label: "pSigK/lacZ",
       summary:
         "The construct was made by sub-cloning the DDB_G0267476 promoter as a 655 bp XbaI/BglII fragment into XbaI/BglII restricted pDdGal-17. This fragment consists of the region -632_+24 bp from the DDB_G0267476 start and restriction sites. This includes the full intergenic region before DDB_G0267476. The construct drives the expression of β-gal when activated; parental vector: pDdGAI-17; vector length: 9137 bp.",
     },
   ]
   it("should display correct items", () => {
-    render(
-      <MockCartProvider mocks={[]} addedItems={items}>
-        <OrderSummary formData={mockValues} />
-      </MockCartProvider>,
-    )
+    render(<OrderSummary formData={mockValues} cart={{ strainItems: items }} />)
     // strain should show price for quantity of 2
     expect(screen.getAllByTestId("quantity")[0]).toHaveTextContent(/Qty: 2/)
     // one plasmid
-    expect(screen.getByText(items[2].id)).toBeInTheDocument()
+    expect(screen.getByText((items[1] as StrainItem).id)).toBeInTheDocument()
     // correct total (30+30+15)
     expect(screen.getByText("$75.00")).toBeInTheDocument()
   })
   it("should display correct address formatting", () => {
-    render(
-      <MockCartProvider mocks={[]} addedItems={items}>
-        <OrderSummary formData={mockValues} />
-      </MockCartProvider>,
-    )
+    render(<OrderSummary formData={mockValues} cart={{ strainItems: items }} />)
     // shipping and payment address are the same
     const addresses = screen.getAllByText(/New York City, NY, USA 10001/)
     expect(addresses).toHaveLength(2)
     // shipping account + number
-    expect(screen.getByText(/FedEx 99999999/)).toBeInTheDocument()
+    expect(screen.getByText(/FedEx 9{8}/)).toBeInTheDocument()
     // payment type
     expect(screen.getByText(/Credit/)).toBeInTheDocument()
   })
