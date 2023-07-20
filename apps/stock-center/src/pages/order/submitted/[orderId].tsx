aebf8d30e12a74cbcdeaac1b08d6aea0a7610c0d
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 14:31:58 -0500

    feat(orderConfirmation): add OrderConfirmation component to display successful order submission notification
    The OrderConfirmation component is added to display a notification to the user that their order was submitted successfully. It includes the OrderHeader, OrderDescription, and OrderSummaryPdf components from the "@dictybase/ui-dsc" library. The component also checks if the order ID matches the one in the URL params and redirects to the home page if they don't match.

---
+++ apps/stock-center/src/pages/order/submitted/[orderId].tsx
@@ -0,0 +1,46 @@
+import { Navigate, useParams } from "react-router-dom"
+import Box from "@material-ui/core/Box"
+import Grid from "@material-ui/core/Grid"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { grey } from "@material-ui/core/colors"
+import {
+  OrderHeader,
+  OrderDescription,
+  OrderSummaryPdf,
+  BackToHomePage,
+} from "@dictybase/ui-dsc"
+import { useAtomValue } from "jotai"
+import { orderAtom } from "../../../state"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: grey[200],
+    margin: theme.spacing(2, 0, 2, 0),
+    minHeight: "320px",
+  },
+}))
+/**
+ * Displays notification that the user's order was submitted successfully.
+ */
+const OrderConfirmation = () => {
+  const classes = useStyles()
+  const order = useAtomValue(orderAtom)
+  const { orderId } = useParams()
+  if (order.orderID !== orderId) return <Navigate to="/" />
+
+  return (
+    <Grid container alignItems="center" className={classes.container}>
+      <Grid item xs={12}>
+        <Box margin={2}>
+          <OrderHeader orderID={order.orderID} />
+          <OrderDescription />
+          <OrderSummaryPdf order={order} />
+        </Box>
+      </Grid>
+      <BackToHomePage />
+    </Grid>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default OrderConfirmation
