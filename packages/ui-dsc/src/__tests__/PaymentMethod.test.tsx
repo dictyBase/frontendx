87aa77fd99eb85bcfc6c9b0f223d172a7ce14760
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 16:00:08 -0500

    test(ui-dsc): add PaymentMethod component tests
    The PaymentMethod component tests have been added to ensure that the component renders correctly and that the radio buttons and text field interact as expected. The tests cover the initial render, the removal of the text field when clicking on certain radio buttons, and the toggling of the text field when clicking on other radio buttons.

---
+++ packages/ui-dsc/src/__tests__/PaymentMethod.test.tsx
@@ -0,0 +1,75 @@
+import { vi } from "vitest"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { PaymentMethod } from "../order/PaymentMethod"
+
+vi.mock("react-hook-form", () => {
+  const originalModule = vi.importActual("react-hook-form")
+
+  return {
+    ...originalModule,
+    useFormContext: () => ({
+      register: () => {},
+      formState: { errors: {} },
+    }),
+  }
+})
+
+describe("OrderForm/Payment/PaymentMethod", () => {
+  describe("initial render", () => {
+    it("renders four radio buttons", () => {
+      render(<PaymentMethod />)
+      const radios = screen.getAllByRole("radio")
+      expect(radios).toHaveLength(4)
+    })
+
+    it("renders one text field", () => {
+      render(<PaymentMethod />)
+      expect(screen.getByRole("textbox")).toBeInTheDocument()
+    })
+  })
+
+  describe("radio button interactions", () => {
+    it("removes textbox when clicking waiver", async () => {
+      render(<PaymentMethod />)
+      const waiver = screen.getByRole("radio", {
+        name: "Waiver Requested",
+      })
+      await userEvent.click(waiver)
+      expect(screen.queryByRole("textbox")).not.toBeInTheDocument()
+    })
+
+    it("removes textbox when clicking credit", async () => {
+      render(<PaymentMethod />)
+      const credit = screen.getByRole("radio", {
+        name: "Credit Card",
+      })
+      await userEvent.click(credit)
+      expect(screen.queryByRole("textbox")).not.toBeInTheDocument()
+    })
+
+    it("removes textbox when clicking wire", async () => {
+      render(<PaymentMethod />)
+      const wire = screen.getByRole("radio", {
+        name: "Wire Transfer",
+      })
+      await userEvent.click(wire)
+      expect(screen.queryByRole("textbox")).not.toBeInTheDocument()
+    })
+
+    it("correctly toggles textbox", async () => {
+      render(<PaymentMethod />)
+      const wire = screen.getByRole("radio", {
+        name: "Wire Transfer",
+      })
+      const po = screen.getByRole("radio", {
+        name: "Purchase Order (PO)",
+      })
+      expect(screen.queryByRole("textbox")).toBeInTheDocument()
+      await userEvent.click(wire)
+      expect(screen.queryByRole("textbox")).not.toBeInTheDocument()
+      await userEvent.click(po)
+      expect(screen.queryByRole("textbox")).toBeInTheDocument()
+    })
+  })
+})
