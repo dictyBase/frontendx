417070075b51af77cf72faef3e9e36f6d466061e
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-20 14:39:04 -0500

    feat(ui-dsc): add unit tests for CountryDropdown, OrderSummary, ShippingMethodPrepaidNotice, ShippingMethodRadioGroup, renderAddressFields, and isCountry functions
    Added unit tests for the following components and functions:
    - CountryDropdown.test.tsx
    - OrderSummary.test.tsx
    - ShippingMethodPrepaidNotice.test.tsx
    - ShippingMethodRadioGroup.test.tsx
    - renderAddressFields.test.tsx
    - isCountry function
    
    These tests ensure that the components and functions are working correctly and provide the expected behavior.

---
+++ packages/ui-dsc/src/__tests__/ShippingMethodRadioGroup.test.tsx
@@ -0,0 +1,81 @@
+import { vi } from "vitest"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { ShippingMethodRadioGroup } from "../order/ShippingMethodRadioGroup"
+
+const mockResetFieldValue = vi.fn()
+const mockSetValue = vi.fn()
+
+vi.mock("react-hook-form", () => {
+  const originalModule = vi.importActual("react-hook-form")
+
+  return {
+    ...originalModule,
+    useFormContext: () => ({
+      resetField: mockResetFieldValue,
+      setValue: mockSetValue,
+    }),
+  }
+})
+
+describe("OrderForm/Shipping/ShippingMethodRadioGroup", () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  const mockSetIsPrepaid = vi.fn()
+
+  const properties = {
+    setIsPrepaid: mockSetIsPrepaid,
+  }
+
+  describe("initial render", () => {
+    it("renders radio buttons", () => {
+      render(<ShippingMethodRadioGroup {...properties} />)
+      const radios = screen.getAllByRole("radio")
+      expect(radios).toHaveLength(4)
+    })
+  })
+
+  describe("radio button interactions", () => {
+    it("sets field value when clicking prepaid shipping label", async () => {
+      render(<ShippingMethodRadioGroup {...properties} />)
+      // click prepaid label button
+      const label = screen.getByRole("radio", {
+        name: "Send prepaid shipping label",
+      })
+      await userEvent.click(label)
+      expect(mockSetValue).toBeCalledTimes(1)
+      expect(mockSetValue).toBeCalledWith(
+        "shippingAccountNumber",
+        "sending prepaid shipping label",
+      )
+      expect(mockSetIsPrepaid).toBeCalledTimes(1)
+      expect(mockSetIsPrepaid).toBeCalledWith(true)
+    })
+    it("does not set field value when clicking others", async () => {
+      render(<ShippingMethodRadioGroup {...properties} />)
+      // click UPS button
+      const ups = screen.getByRole("radio", {
+        name: "UPS",
+      })
+      await userEvent.click(ups)
+      expect(mockResetFieldValue).toBeCalledWith("shippingAccountNumber")
+      expect(mockSetIsPrepaid).toBeCalledWith(false)
+      // click FedEx button
+      const fedex = screen.getByRole("radio", {
+        name: "FedEx",
+      })
+      await userEvent.click(fedex)
+      expect(mockResetFieldValue).toBeCalledWith("shippingAccountNumber")
+      expect(mockSetIsPrepaid).toBeCalledWith(false)
+      // click DHL button
+      const dhl = screen.getByRole("radio", {
+        name: "DHL",
+      })
+      await userEvent.click(dhl)
+      expect(mockResetFieldValue).toBeCalledWith("shippingAccountNumber")
+      expect(mockSetIsPrepaid).toBeCalledWith(false)
+    })
+  })
+})
---
+++ packages/ui-dsc/src/__tests__/renderAddressFields.test.tsx
@@ -0,0 +1,73 @@
+import { vi } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { renderAddressFields, isCountry } from "../functional"
+
+vi.mock("react-hook-form", () => {
+  const originalModule = vi.importActual("react-hook-form")
+
+  return {
+    useFormContext: () => ({ register: vi.fn(), formState: { errors: {} } }),
+    ...originalModule,
+  }
+})
+
+describe("renderAddressFields", () => {
+  describe("initial render with country", () => {
+    const properties = [
+      {
+        name: "firstName",
+        label: "First Name",
+        required: true,
+      },
+      {
+        name: "lastName",
+        label: "Last Name",
+        required: true,
+      },
+      {
+        name: "country",
+        label: "Country",
+        required: true,
+      },
+    ]
+    it("displays all expected text fields", () => {
+      render(<>{renderAddressFields(properties)}</>)
+      properties.forEach((item) => {
+        expect(screen.getAllByText(`${item.label}`)[0]).toBeInTheDocument()
+      })
+      expect(screen.getAllByRole("textbox")).toHaveLength(3)
+    })
+  })
+
+  describe("initial render without country", () => {
+    const properties = [
+      {
+        name: "firstName",
+        label: "First Name",
+        required: true,
+      },
+      {
+        name: "lastName",
+        label: "Last Name",
+        required: true,
+      },
+    ]
+    it("displays all expected text fields", () => {
+      render(<>{renderAddressFields(properties)}</>)
+      properties.forEach((item) => {
+        expect(screen.getAllByText(`${item.label}`)[0]).toBeInTheDocument()
+      })
+      expect(screen.getAllByRole("textbox")).toHaveLength(2)
+    })
+  })
+})
+
+describe("isCountry function", () => {
+  it("should return true for expected country fields", () => {
+    expect(isCountry({ name: "country" })).toBeTruthy()
+    expect(isCountry({ name: "payerCountry" })).toBeTruthy()
+  })
+  it("should return false for wrong field", () => {
+    expect(isCountry({ name: "firstName" })).toBeFalsy()
+  })
+})
