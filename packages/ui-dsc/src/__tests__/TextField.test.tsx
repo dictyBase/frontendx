cc7f369ddbcfa85c8bcfb3f095aab62781f2c1c5
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-21 14:55:28 -0500

    test(TextField.test.tsx): refactor test cases for TextField component
    The test cases for the TextField component have been refactored to improve readability and maintainability. The test cases now use the `test` and `expect` functions from the `vitest` library instead of the `describe` and `it` functions. The test cases have been split into two separate test cases: one for invalid input resulting in an error, and one for valid input not resulting in an error. The test cases now use a `WrappedTextField` component that wraps the `TextField` component in a `FormProvider` and a form with a submit button. The test cases simulate user interaction by clicking the submit button and entering text in the text field using the `userEvent` library. The test cases assert that the field label has the "Mui-error" class and that the error message is displayed for invalid input, and that the field label does not have the "Mui-error" class

--- packages/ui-dsc/src/__tests__/TextField.test.tsx
+++ packages/ui-dsc/src/__tests__/TextField.test.tsx
@@ -1,101 +1,62 @@
-import { ReactNode } from "react"
+import { test, expect } from "vitest"
 import { screen, render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import { useForm, FormProvider } from "react-hook-form"
+import { useForm, FormProvider, type Resolver } from "react-hook-form"
 import { TextField } from "../order/TextField"
 
-type FormProviderWrapperProperties = {
-  children: ReactNode
+type FormValues = {
+  firstName: string
 }
 
-const FormProviderWrapper = ({ children }: FormProviderWrapperProperties) => {
-  const methods = useForm()
-  return <FormProvider {...methods}>{children}</FormProvider>
+const errorMessage = "First name is required"
+
+const resolver: Resolver<FormValues> = async (values) => ({
+  values: values.firstName ? values : {},
+  errors: values.firstName
+    ? {}
+    : {
+        firstName: {
+          type: "required",
+          message: errorMessage,
+        },
+      },
+})
+
+const WrappedTextField = () => {
+  const methods = useForm({ resolver })
+  const { handleSubmit } = methods
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(() => {})}>
+        <TextField name="firstName" label="First Name" />
+        <button type="submit"> Submit </button>
+      </form>
+    </FormProvider>
+  )
 }
 
-describe("TextField", () => {
-  it("renders with default props", () => {
-    render(
-      <FormProviderWrapper>
-        <TextField name="test" label="Test" />
-      </FormProviderWrapper>,
-    )
+test("invalid input results in error", async () => {
+  render(<WrappedTextField />)
 
-    const textField = screen.getByLabelText("Test")
+  await userEvent.click(screen.getByRole("button"))
 
-    expect(textField).toBeInTheDocument()
-    expect(textField).toHaveAttribute("type", "text")
-    expect(textField).toHaveAttribute("margin", "dense")
-    expect(textField).toHaveAttribute("variant", "outlined")
-    expect(textField).toHaveAttribute("fullWidth")
-    expect(textField).not.toHaveClass("Mui-error")
-    expect(textField).not.toHaveAttribute("helperText")
-  })
+  const fieldLabel = screen.getAllByText("First Name")[0]
+  const helperText = screen.getByText(errorMessage)
 
-  it("renders with custom props", () => {
-    render(
-      <FormProviderWrapper>
-        <TextField
-          name="test"
-          label="Test"
-          margin="normal"
-          variant="filled"
-          fullWidth={false}
-          helperText="This is a helper text"
-        />
-      </FormProviderWrapper>,
-    )
+  expect(fieldLabel).toHaveClass("Mui-error")
+  expect(helperText).toBeInTheDocument()
+})
 
-    const textField = screen.getByLabelText("Test")
+test("valid input does not result in error", async () => {
+  render(<WrappedTextField />)
 
-    expect(textField).toBeInTheDocument()
-    expect(textField).toHaveAttribute("type", "text")
-    expect(textField).toHaveAttribute("margin", "normal")
-    expect(textField).toHaveAttribute("variant", "filled")
-    expect(textField).not.toHaveAttribute("fullWidth")
-    expect(textField).not.toHaveClass("Mui-error")
-    expect(textField).toHaveAttribute("helperText", "This is a helper text")
-  })
+  await userEvent.click(screen.getByRole("textbox"))
+  await userEvent.keyboard("Kevin")
+  await userEvent.click(screen.getByRole("button"))
 
-  it("renders with error", () => {
-    const ErrorMessage = () => <div>Required field</div>
+  const fieldLabel = screen.getAllByText("First Name")[0]
+  const helperText = screen.queryByText(errorMessage)
 
-    render(
-      <FormProviderWrapper>
-        <TextField
-          name="test"
-          label="Test"
-          margin="normal"
-          variant="filled"
-          fullWidth={false}
-          error
-          helperText={<ErrorMessage />}
-        />
-      </FormProviderWrapper>,
-    )
-
-    const textField = screen.getByLabelText("Test")
-    const errorMessage = screen.getByText("Required field")
-
-    expect(textField).toBeInTheDocument()
-    expect(textField).toHaveAttribute("type", "text")
-    expect(textField).toHaveAttribute("margin", "normal")
-    expect(textField).toHaveAttribute("variant", "filled")
-    expect(textField).not.toHaveAttribute("fullWidth")
-    expect(textField).toHaveClass("Mui-error")
-    expect(errorMessage).toBeInTheDocument()
-  })
-
-  it("allows user input", () => {
-    render(
-      <FormProviderWrapper>
-        <TextField name="test" label="Test" />
-      </FormProviderWrapper>,
-    )
-
-    const textField = screen.getByLabelText("Test")
-    userEvent.type(textField, "Hello, World!")
-
-    expect(textField).toHaveValue("Hello, World!")
-  })
+  expect(fieldLabel).not.toHaveClass("Mui-error")
+  expect(helperText).toBeNull()
 })
