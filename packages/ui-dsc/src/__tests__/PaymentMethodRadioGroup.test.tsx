3c7799b46c978f79f36c36d8afaa7170c3782c71
Kevin Tun <43506054+ktun95@users.noreply.github.com>
2023-07-24 19:26:42 +0000

    test(ui-dsc): add unit test for PaymentMethodRadioGroup component
    The added unit test verifies that the PaymentMethodRadioGroup component correctly changes the payment method when a radio button is clicked. It uses the react-testing-library and user-event libraries for rendering and interacting with the component. The test covers all available payment methods: Purchase Order (PO), Waiver Requested, Credit Card, and Wire Transfer. It checks that the setPaymentMethod function is called with the correct payment method value and that the selected payment method is displayed on the screen.

---
+++ packages/ui-dsc/src/__tests__/PaymentMethodRadioGroup.test.tsx
@@ -0,0 +1,63 @@
+import { vi, test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { useForm, FormProvider, type Resolver } from "react-hook-form"
+import { PaymentMethodRadioGroup } from "../order/PaymentMethodRadioGroup"
+
+type FormValues = {
+  paymentMethod: "purchaseOrder" | "waiver" | "credit" | "wire"
+}
+
+const mockSetPaymentMethod = vi.fn()
+
+const resolver: Resolver<FormValues> = async (values) => ({
+  values: values.paymentMethod ? values : {},
+  errors: values.paymentMethod
+    ? {}
+    : {
+        paymentMethod: {
+          type: "required",
+          message: "payment method required.",
+        },
+      },
+})
+
+const FormWrapper = () => {
+  const methods = useForm({ resolver })
+  const watchPaymentMethod = methods.watch("paymentMethod")
+  return (
+    <FormProvider {...methods}>
+      <PaymentMethodRadioGroup setPaymentMethod={mockSetPaymentMethod} />
+      <>{watchPaymentMethod}</>
+    </FormProvider>
+  )
+}
+
+test("changes payment method when radio button is clicked", async () => {
+  render(<FormWrapper />)
+
+  await userEvent.click(
+    screen.getByRole("radio", { name: "Purchase Order (PO)" }),
+  )
+  expect(mockSetPaymentMethod).toHaveBeenCalledOnce()
+  expect(mockSetPaymentMethod).toHaveBeenCalledWith("purchaseOrder")
+  expect(screen.getByText("purchaseOrder")).toBeInTheDocument()
+
+  await userEvent.click(screen.getByRole("radio", { name: "Waiver Requested" }))
+
+  expect(mockSetPaymentMethod).toHaveBeenCalledTimes(2)
+  expect(mockSetPaymentMethod).toHaveBeenCalledWith("waiver")
+  expect(screen.getByText("waiver")).toBeInTheDocument()
+
+  await userEvent.click(screen.getByRole("radio", { name: "Credit Card" }))
+
+  expect(mockSetPaymentMethod).toHaveBeenCalledTimes(3)
+  expect(mockSetPaymentMethod).toHaveBeenCalledWith("credit")
+  expect(screen.getByText("credit")).toBeInTheDocument()
+
+  await userEvent.click(screen.getByRole("radio", { name: "Wire Transfer" }))
+
+  expect(mockSetPaymentMethod).toHaveBeenCalledTimes(4)
+  expect(mockSetPaymentMethod).toHaveBeenCalledWith("wire")
+  expect(screen.getByText("wire")).toBeInTheDocument()
+})
