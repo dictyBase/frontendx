44949131d10eb2cb2da25a3bf7808ca9e2a07cb2
Kevin Tun <43506054+ktun95@users.noreply.github.com>
2023-07-24 05:18:32 +0000

    test(CountryDropdown.test.tsx): refactor test suite to use vitest's describe and test functions, and add additional tests for CountryDropdown component
    The test suite for the CountryDropdown component has been refactored to use the describe and test functions provided by the vitest library. Additional tests have been added to validate the input and error handling in the CountryDropdown component. The mapping of the fieldName prop to the form property has also been tested. Finally, a test has been added to ensure that the countryToFlag function returns the expected string.

--- packages/ui-dsc/src/__tests__/CountryDropdown.test.tsx
+++ packages/ui-dsc/src/__tests__/CountryDropdown.test.tsx
@@ -1,7 +1,92 @@
-import { test, expect } from "vitest"
-import { countryToFlag } from "../order/CountryDropdown"
+import { describe, test, expect } from "vitest"
+import { pipe } from "fp-ts/function"
+import { filter, map, reduce } from "fp-ts/Array"
+import { screen, render } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { useForm, FormProvider, type Resolver } from "react-hook-form"
+import { CountryDropdown, countryToFlag } from "../order/CountryDropdown"
 
-describe("OrderForm/countryToFlag", () => {
+type FormValues = {
+  country: string
+  payerCountry: string
+}
+
+const convertToObjectArray = (values: FormValues): Array<[string, string]> => [
+  ["country", values.country],
+  ["payerCountry", values.payerCountry],
+]
+
+const getErrors = (values: FormValues) =>
+  pipe(
+    values,
+    convertToObjectArray,
+    filter(([, value]) => value === ""),
+    map(([fieldName]) => ({
+      [fieldName]: { type: "required", message: `${fieldName} is required` },
+    })),
+    reduce({}, (accumulator, current) => ({ ...accumulator, ...current })),
+  )
+
+const resolver: Resolver<FormValues> = async (values) => ({
+  values: values.country ? values : {},
+  errors: getErrors(values),
+})
+
+const WrappedCountryDropdown = ({ fieldName }: { fieldName: string }) => {
+  const methods = useForm({ resolver })
+  const { handleSubmit } = methods
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(() => {})}>
+        <CountryDropdown fieldName={fieldName} />
+        <button type="submit"> Submit </button>
+      </form>
+    </FormProvider>
+  )
+}
+
+describe("CountryDropdown", () => {
+  test("invalid input results in error", async () => {
+    render(<WrappedCountryDropdown fieldName="country" />)
+
+    await userEvent.click(screen.getByText("Submit"))
+
+    const fieldLabel = screen.getAllByText("Country")[0]
+    const helperText = screen.getByText("country is required")
+
+    expect(fieldLabel).toHaveClass("Mui-error")
+    expect(helperText).toBeInTheDocument()
+  })
+
+  test("valid input does not result in error", async () => {
+    render(<WrappedCountryDropdown fieldName="country" />)
+
+    await userEvent.click(screen.getByTitle("Open"))
+    await userEvent.click(screen.getByText(/Brazil/))
+    await userEvent.click(screen.getByText("Submit"))
+
+    const fieldLabel = screen.getAllByText("Country")[0]
+    const helperText = screen.queryByText("country is required")
+
+    expect(fieldLabel).not.toHaveClass("Mui-error")
+    expect(helperText).toBeNull()
+  })
+  test(`passing "payerCountry" to fieldName prop registers the field to "payerCountry" form property`, async () => {
+    render(<WrappedCountryDropdown fieldName="payerCountry" />)
+
+    await userEvent.click(screen.getByTitle("Open"))
+    await userEvent.click(screen.getByText(/Brazil/))
+    await userEvent.click(screen.getByText("Submit"))
+
+    const fieldLabel = screen.getAllByText("Country")[0]
+    const helperText = screen.queryByText("payerCountry is required")
+
+    expect(fieldLabel).not.toHaveClass("Mui-error")
+    expect(helperText).toBeNull()
+  })
+})
+
+describe("countryToFlag", () => {
   test("should return expected string", () => {
     expect(countryToFlag("IS")).toBe("🇮🇸")
   })
