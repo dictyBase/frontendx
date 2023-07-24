06855cb55a73a85a1afc279c00984e91619c67b7
Kevin Tun <43506054+ktun95@users.noreply.github.com>
2023-07-24 06:29:34 +0000

    test(CartTotalRow.test.tsx): add unit tests for CartTotalRow component
    The first test ensures that the CartTotalRow component renders the leftValue, numItems, and total correctly. It verifies that the expected elements are present in the rendered output.
    
    The second test checks that the Typography components within the CartTotalRow component have the proper variant. It ensures that the variant prop is correctly applied to the Typography components and that they have the expected variant class.

---
+++ packages/ui-dsc/src/__tests__/CartTotalRow.test.tsx
@@ -0,0 +1,42 @@
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { TypographyProps } from "@material-ui/core/Typography"
+import { CartTotalRow } from "../cart/CartTotalRow"
+
+test("should render the leftValue, numItems, and total correctly", () => {
+  const properties = {
+    leftValue: "Strains",
+    numItems: 5,
+    total: "10.99",
+  }
+
+  render(<CartTotalRow {...properties} />)
+
+  const leftValueElement = screen.getByText("Strains")
+  const numberItemsElement = screen.getByText("5 items:")
+  const totalElement = screen.getByText("10.99")
+
+  expect(leftValueElement).toBeInTheDocument()
+  expect(numberItemsElement).toBeInTheDocument()
+  expect(totalElement).toBeInTheDocument()
+})
+
+test("should have the proper variant for Typography components", () => {
+  const properties = {
+    leftValue: "Plasmids",
+    numItems: 0,
+    total: "0.00",
+    variant: "h6" as TypographyProps["variant"],
+  }
+
+  render(<CartTotalRow {...properties} />)
+
+  const leftValueElement = screen.getByText("Plasmids").parentElement
+  const numberItemsElement = screen.getByText("0 items:")
+  const totalElement = screen.getByText("0.00")
+  const variantClassName = "MuiTypography-h6"
+
+  expect(leftValueElement).toHaveClass(variantClassName)
+  expect(numberItemsElement).toHaveClass(variantClassName)
+  expect(totalElement).toHaveClass(variantClassName)
+})
