2e8004011f261eb81a140feb17cfcbab21003776
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-27 16:03:47 -0500

    feat(characterConverter): add characterConverter utility function
    The characterConverter utility function is added to the `utils` directory. This function is used to replace any HTML Symbol Entities with their corresponding characters. It uses a regular expression to convert numeric entities and manually replaces the named entity "&gamma;" with the character "γ". This utility function improves the handling of HTML entities in the application.

---
+++ packages/ui-dsc/src/__tests__/characterConverter.test.ts
@@ -0,0 +1,10 @@
+import { characterConverter } from "../utils/characterConverter"
+
+describe("common/utils/characterConverter", () => {
+  it("should convert numeric HTML entity", () => {
+    expect(characterConverter("&#947;")).toEqual("γ")
+  })
+  it("should convert gamma HTML entity", () => {
+    expect(characterConverter("&gamma;")).toEqual("γ")
+  })
+})
