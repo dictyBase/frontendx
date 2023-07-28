6f758b6bfbb19d12882979f95f592c1af91fc08e
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-28 10:59:28 -0500

    feat(ui-dsc): add DetailsListItem component
    The DetailsListItem component is added to the catalog directory in the ui-dsc package. This component is used to display a single row of data for a stock item. It takes in a title and content as properties, and renders them in a ListItem with Typography components for the title and content. The content can be a string, JSX element, or an array of JSX elements. The component also uses a characterConverter function to convert any HTML entities in the content.

---
+++ packages/ui-dsc/src/catalog/DetailsListItem.tsx
@@ -0,0 +1,46 @@
+import Typography from "@material-ui/core/Typography"
+import Grid from "@material-ui/core/Grid"
+import ListItem from "@material-ui/core/ListItem"
+import { characterConverter } from "../utils/characterConverter"
+import { useStyles } from "./styles"
+
+type DetailsListitemProperties = {
+  /** Type of content to display (i.e. Strain Descriptor, Genotype, etc.) */
+  title: string
+  /** The actual data for that field */
+  content: string | JSX.Element | JSX.Element[] | undefined | null
+}
+
+/**
+ * DetailsHeader used to display a single row of data for a stock item.
+ */
+
+const DetailsListItem = ({ title, content }: DetailsListitemProperties) => {
+  const classes = useStyles()
+
+  let display = content
+  if (typeof display === "string") {
+    // convert any html entities
+    display = characterConverter(display)
+  }
+
+  return (
+    <ListItem className={classes.details} divider>
+      <Grid item xs={12} sm={3} className={classes.listItem}>
+        <Typography
+          component="span"
+          variant="body2"
+          className={classes.detailsItem}>
+          {title}
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={9}>
+        <Typography component="span" variant="body1">
+          {display}
+        </Typography>
+      </Grid>
+    </ListItem>
+  )
+}
+
+export { DetailsListItem }
