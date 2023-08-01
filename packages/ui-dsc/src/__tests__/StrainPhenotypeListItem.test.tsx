e0f18f1f7f687fa62d4ebcca7ca390927a5525a7
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-31 21:26:11 -0500

    feat(ui-dsc): add StrainPhenotypeListItem component and its tests
    The StrainPhenotypeListItem component is added to the ui-dsc package. This component renders a list item for displaying information about a strain's phenotype. It includes the strain's label, associated genes, and a link to a publication if available. The component also has tests to ensure that the expected list items are included and that publications are not displayed when not passed as a prop.

---
+++ packages/ui-dsc/src/__tests__/StrainPhenotypeListItem.test.tsx
@@ -0,0 +1,40 @@
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import { Gene, Publication } from "dicty-graphql-schema"
+import { StrainPhenotypeListItem } from "../catalog/StrainPhenotypeListItem"
+import { availableStrain, strainWithPhenotype } from "../mocks/mockStrain"
+
+test("includes expected list items", () => {
+  render(
+    <BrowserRouter>
+      <StrainPhenotypeListItem strain={availableStrain} />
+    </BrowserRouter>,
+  )
+  // find strain descriptor
+  const label = screen.getByText(availableStrain.label)
+  expect(label).toBeInTheDocument()
+  // find associated genes
+  const genes = availableStrain?.genes as Gene[]
+  test.each(genes)("Gene name is displayed", ({ name }) => {
+    expect(screen.getByText(name)).toBeInTheDocument()
+  })
+  // find pub link
+  const links = screen.getAllByRole("link")
+  const pubLink = links[2]
+  const pub = availableStrain?.publications as Array<Publication>
+  expect(pubLink).toHaveAttribute(
+    "href",
+    `/publication/${(pub[0] as Publication).id}`,
+  )
+})
+
+test("should not include publications when not passed as prop", () => {
+  render(
+    <BrowserRouter>
+      <StrainPhenotypeListItem strain={strainWithPhenotype} />
+    </BrowserRouter>,
+  )
+  const pubDisplay = screen.queryByTestId("publication-display")
+  expect(pubDisplay).not.toBeInTheDocument()
+})
