5e9bf3f0d36182625eb9976df8dde3e3cde6be14
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-01 02:20:24 -0500

    feat(ui-dsc): add DetailsHeader component to catalog module
    The DetailsHeader component is added to the catalog module in the ui-dsc package. This component is used as the header at the top of every stock details page. It displays the name and ID of the stock, and includes a copy icon for the ID.

---
+++ packages/ui-dsc/src/catalog/DetailsHeader.tsx
@@ -0,0 +1,43 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import { DetailsHeaderCopyIcon } from "./DetailsHeaderCopyIcon"
+import { characterConverter } from "../utils/characterConverter"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  name: {
+    marginBottom: theme.spacing(2),
+  },
+  id: {
+    marginBottom: theme.spacing(2),
+  },
+}))
+
+type DetailsHeaderProperties = {
+  /** Stock ID */
+  id: string
+  /** Strain descriptor or plasmid name */
+  name: string
+}
+
+/**
+ * DetailsHeader is the header at the top of every stock details page.
+ */
+
+const DetailsHeader = ({ name, id }: DetailsHeaderProperties) => {
+  const classes = useStyles()
+
+  return (
+    <Box mt={2}>
+      <Typography className={classes.name} variant="h1" noWrap>
+        {characterConverter(name)}
+      </Typography>
+      <Typography className={classes.id} variant="h3" color="textSecondary">
+        <em>{id}</em>
+        <DetailsHeaderCopyIcon id={id} />
+      </Typography>
+    </Box>
+  )
+}
+
+export { DetailsHeader }
