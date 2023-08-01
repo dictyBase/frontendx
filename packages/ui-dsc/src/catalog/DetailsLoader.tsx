068fd52e2306e4e5836246fff2c070a18e594b96
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-01 02:21:08 -0500

    feat(ui-dsc): add DetailsLoader component for displaying loading screen while fetching stock data
    The DetailsLoader component is added to the catalog directory in the ui-dsc package. This component is used to display a loading screen while fetching stock data. It uses the Skeleton component from the react-loading-skeleton library to show placeholders for the content that is being loaded.

---
+++ packages/ui-dsc/src/catalog/DetailsLoader.tsx
@@ -0,0 +1,28 @@
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
+import Grid from "@material-ui/core/Grid"
+
+/**
+ * DetailsLoader is the loading screen displayed while fetching stock data.
+ */
+
+const DetailsLoader = () => (
+  <Grid container spacing={2} data-testid="skeleton-loader">
+    <Grid item xs={12}>
+      <h1>
+        <SkeletonTheme baseColor="#D3D3D3" highlightColor="#DCDCDC">
+          <Skeleton />
+        </SkeletonTheme>
+      </h1>
+    </Grid>
+    <Grid item xs={12}>
+      <SkeletonTheme baseColor="#D3D3D3" highlightColor="#DCDCDC">
+        <Skeleton count={10} />
+        <br />
+        <br />
+        <Skeleton count={10} />
+      </SkeletonTheme>
+    </Grid>
+  </Grid>
+)
+
+export { DetailsLoader }
