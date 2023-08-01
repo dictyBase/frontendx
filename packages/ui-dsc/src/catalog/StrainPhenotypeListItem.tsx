1e1a8b6da062c813fe71e95ad122ee3f5e26edde
Kevin Tun <kevin.tun@northwestern.edu>
2023-07-31 21:26:11 -0500

    feat(ui-dsc): add StrainPhenotypeListItem component and its tests
    The StrainPhenotypeListItem component is added to the ui-dsc package. This component renders a list item for displaying information about a strain's phenotype. It includes the strain's label, associated genes, and a link to a publication if available. The component also has tests to ensure that the expected list items are included and that publications are not displayed when not passed as a prop.

---
+++ packages/ui-dsc/src/catalog/StrainPhenotypeListItem.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import ListItem from "@material-ui/core/ListItem"
+import { makeStyles } from "@material-ui/core/styles"
+import { Strain, Gene, Publication } from "dicty-graphql-schema"
+import { GenesDisplay } from "./GenesDisplay"
+import { PublicationDisplay } from "./PublicationDisplay"
+import { characterConverter } from "../utils/characterConverter"
+
+const useStyles = makeStyles({
+  row: {
+    borderBottom: "1px solid rgba(224, 224, 224, 1)",
+    "&:hover": {
+      backgroundColor: "#eeeeee",
+    },
+  },
+  item: {
+    paddingRight: "10px",
+    fontSize: "0.9rem",
+  },
+})
+
+type StrainPhenotypeListItemProperties = {
+  strain: Strain
+}
+
+const StrainPhenotypeListItem = ({
+  strain,
+}: StrainPhenotypeListItemProperties) => {
+  const classes = useStyles()
+
+  const publications = strain?.publications as Publication[]
+  const genes = strain?.genes as Gene[]
+
+  const pubDisplay =
+    publications && publications.length > 0 ? (
+      <PublicationDisplay publication={publications[0] as Publication} />
+    ) : (
+      <></>
+    )
+
+  return (
+    <ListItem className={classes.row}>
+      <Grid container spacing={0} alignItems="center">
+        <Grid item sm={3} className={classes.item}>
+          <Typography variant="body2" noWrap>
+            <Link to={`/strains/${strain.id}`}>
+              {characterConverter(strain.label)}
+            </Link>
+          </Typography>
+        </Grid>
+        <Grid item sm={3} className={classes.item}>
+          <Typography variant="body2" noWrap>
+            <GenesDisplay genes={genes} />
+          </Typography>
+        </Grid>
+        <Grid item sm={6} className={classes.item}>
+          <Typography component="span" variant="body2">
+            {pubDisplay}
+          </Typography>
+        </Grid>
+      </Grid>
+    </ListItem>
+  )
+}
+
+export { StrainPhenotypeListItem }
