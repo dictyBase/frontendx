5ee581d8d7d1bfb8a0b4f9e6d7c948463f120d29
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-01 02:21:23 -0500

    feat(ui-dsc): add styles for catalog component
    This commit adds a new file `styles.tsx` in the `catalog` directory of the `ui-dsc` package. The file contains styles for the catalog component. The styles include CSS properties for various elements such as card headers, details, list items, IDs, copy icons, and error messages. The `makeStyles` function from `@material-ui/core/styles` is used to define the styles.

---
+++ packages/ui-dsc/src/catalog/styles.tsx
@@ -0,0 +1,46 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+
+// @ts-ignore <-- necessary to include the !important attributes
+const useStyles = makeStyles((theme: Theme) => ({
+  cardHeader: {
+    color: "#002f5e",
+    paddingRight: "8px",
+  },
+  details: {
+    "&:nth-child(odd)": {
+      backgroundColor: "#F4F6F8",
+    },
+    "@media (max-width: 420px)": {
+      display: "block !important",
+    },
+  },
+  listItem: {
+    "@media (max-width: 420px)": {
+      marginBottom: "10px",
+    },
+  },
+  list: {
+    width: "100%",
+    padding: "0px !important",
+  },
+  id: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "5px",
+  },
+  copyIcon: {
+    marginLeft: "5px",
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "rgba(0, 0, 0, 0.87)",
+    },
+  },
+  detailsItem: {
+    fontWeight: 420,
+  },
+  error: {
+    color: theme.palette.error.main,
+  },
+}))
+
+export { useStyles }
