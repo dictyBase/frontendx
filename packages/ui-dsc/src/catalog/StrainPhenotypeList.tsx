b9b68fbfad016cc22b7af68acf6df658d94a1c50
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-01 21:04:15 -0500

    feat(ui-dsc): add StrainPhenotypeList component
    The StrainPhenotypeList component is added to the ui-dsc package. This component provides a list of phenotypes for a given strain. It uses the Material-UI Paper component for styling and renders a header component (StrainPhenotypeListHeader) and a list of phenotype items (StrainPhenotypeListItem). The phenotypes are sorted alphabetically by phenotype name using the fp-ts library.

---
+++ packages/ui-dsc/src/catalog/StrainPhenotypeList.tsx
@@ -0,0 +1,41 @@
+import { Ord as sOrd } from "fp-ts/string"
+import { pipe } from "fp-ts/function"
+import { contramap, Ord } from "fp-ts/lib/Ord"
+import { sort, map } from "fp-ts/Array"
+import Paper from "@material-ui/core/Paper"
+import { Phenotype } from "dicty-graphql-schema"
+import { StrainPhenotypeListHeader } from "./StrainPhenotypeListHeader"
+import { StrainPhenotypeListItem } from "./StrainPhenotypeListItem"
+import { useStyles } from "./phenotypeStyles"
+
+type Properties = {
+  phenotypes: Array<Phenotype>
+}
+
+const byPhenotype: Ord<Phenotype> = pipe(
+  sOrd,
+  contramap((phenotype) => phenotype.phenotype),
+)
+
+/**
+ * PhenotypeList provides a list of phenotypes for a given strain.
+ */
+
+const StrainPhenotypeList = ({ phenotypes }: Properties) => {
+  const classes = useStyles()
+  console.log(phenotypes)
+  return (
+    <Paper className={classes.paper}>
+      <StrainPhenotypeListHeader />
+      {pipe(
+        phenotypes,
+        sort(byPhenotype),
+        map((phenotype) => (
+          <StrainPhenotypeListItem key={phenotype.phenotype} data={phenotype} />
+        )),
+      )}
+    </Paper>
+  )
+}
+
+export { StrainPhenotypeList }
