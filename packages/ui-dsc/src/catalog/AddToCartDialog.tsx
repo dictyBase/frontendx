7ba6aada20ede1de22f5c0306727edcae1ef8515
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-02 09:01:39 -0500

    feat(ui-dsc): add AddToCartDialog component
    The AddToCartDialog component is added to the catalog package in the ui-dsc package. This component is responsible for displaying a dialog box when an item is added to the cart. It receives various properties such as the strain data, functions to handle checked items, show/hide the dialog, and toggle hover state. The dialog box includes a title, content, and actions.

---
+++ packages/ui-dsc/src/catalog/AddToCartDialog.tsx
@@ -0,0 +1,49 @@
+import Dialog from "@material-ui/core/Dialog"
+import { DialogTitleDisplay } from "./DialogTitleDisplay"
+import { AddToCartDialogContent } from "./AddToCartDialogContent"
+import { AddToCartDialogActions } from "./AddToCartDialogActions"
+import { CartItem } from "../types"
+
+type Properties = {
+  /** Strain data */
+  data: Array<CartItem>
+  /** Function to add to checked items array */
+  setCheckedItems?: (argument0: Array<CartItem>) => void
+  /** Function to display dialog box after adding item to cart */
+  setShowDialog: (argument0: boolean) => void
+  /** Function to toggle hover state for given item */
+  setHover?: (argument0: boolean) => void
+}
+
+/**
+ * AddToCartDialog is the dialog box that appears when an item
+ * is added to the cart.
+ */
+
+const AddToCartDialog = ({
+  data,
+  setCheckedItems,
+  setShowDialog,
+  setHover,
+}: Properties) => {
+  const handleClose = () => {
+    setShowDialog(false)
+    setCheckedItems?.([])
+    // set hovering to false if button is in catalog list item
+    setHover?.(false)
+  }
+
+  return (
+    <Dialog
+      onClose={handleClose}
+      fullWidth
+      aria-labelledby="cart-dialog-title"
+      open>
+      <DialogTitleDisplay title="Added to Cart" handleClose={handleClose} />
+      <AddToCartDialogContent data={data} />
+      <AddToCartDialogActions handleClose={handleClose} />
+    </Dialog>
+  )
+}
+
+export { AddToCartDialog }
