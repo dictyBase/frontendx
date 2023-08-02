94b20b9b383a6555223c202846e099b0fb5e5264
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-02 09:14:03 -0500

    feat(mockPhenotype.ts): add mock data for phenotypes
    This commit adds a new file `mockPhenotype.ts` which contains an array of mock data for phenotypes. Each phenotype object includes properties such as phenotype, note, assay, environment, and publication. This mock data will be used for testing and development purposes.

---
+++ packages/ui-dsc/src/mocks/mockPhenotype.ts
@@ -0,0 +1,89 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+const mockPhenotypes = [
+  {
+    phenotype: "aberrant centrosome localization",
+    note: "not localized in a polarized fashion to the stalk side of the tip cells",
+    assay: "confocal microscopy",
+    publication: {
+      id: "11084033",
+      pubDate: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          lastName: "Gliksman",
+        },
+        {
+          lastName: "Santoyo",
+        },
+        {
+          lastName: "Novak",
+        },
+        {
+          lastName: "Titus",
+        },
+      ],
+    },
+  },
+  {
+    phenotype: "decreased gene expression",
+    note: "pspA",
+    assay: "Northern blot",
+    environment: "during development",
+    publication: {
+      id: "11084033",
+      pubDate: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          lastName: "Gliksman",
+        },
+        {
+          lastName: "Santoyo",
+        },
+        {
+          lastName: "Novak",
+        },
+        {
+          lastName: "Titus",
+        },
+      ],
+    },
+  },
+  {
+    phenotype: "decreased sporulation",
+    note: "",
+    publication: {
+      id: "11084033",
+      pubDate: "2000-11-17T00:00:00.000Z",
+      title:
+        "Myosin I phosphorylation is increased by chemotactic stimulation.",
+      journal: "The Journal of biological chemistry",
+      volume: "276",
+      pages: "5235-5239",
+      authors: [
+        {
+          lastName: "Gliksman",
+        },
+        {
+          lastName: "Santoyo",
+        },
+        {
+          lastName: "Novak",
+        },
+        {
+          lastName: "Titus",
+        },
+      ],
+    },
+  },
+]
+
+export { mockPhenotypes }
