3a22c8d6af18b371339c0bb24b897b160c3c0fca
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-02 09:24:43 -0500

    test(ui-dsc): add test for OutlinedDropdown component
    This commit adds a new test file `OutlinedDropdown.test.tsx` to test the functionality of the `OutlinedDropdown` component. The test verifies that the `handleChange` function is called when a menu item is clicked. It also tests that the dropdown renders correctly with the provided label, dropdown values, and initial input value.

---
+++ packages/ui-dsc/src/__tests__/OutlinedDropdown.test.tsx
@@ -0,0 +1,24 @@
+import { vi, test, expect } from "vitest"
+import { render, screen, within } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { OutlinedDropdown } from "../common/OutlinedDropdown"
+
+const handleChangeSpy = vi.fn()
+const values = [1, 2, 3, 4, 5, 6]
+
+test("calls function handleChange when a menu item is clicked", async () => {
+  render(
+    <OutlinedDropdown
+      handleChange={handleChangeSpy}
+      label="Qty"
+      dropdownValues={values}
+      inputValue={4}
+    />,
+  )
+  const dropdown = screen.getByRole("button", { name: "Qty 4" })
+  expect(dropdown).toBeInTheDocument()
+  await userEvent.click(dropdown)
+  const listbox = within(screen.getByRole("listbox"))
+  await userEvent.click(listbox.getByText("2"))
+  expect(handleChangeSpy).toHaveBeenCalledTimes(1)
+})
