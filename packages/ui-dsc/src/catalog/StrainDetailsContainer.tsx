33c3e406cdf97a249b437726569a76f7978f7a8c
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-03 08:17:33 -0500

    feat(ui-dsc): add StrainDetailsContainer component
    The StrainDetailsContainer component is responsible for rendering the details page for an individual strain. It fetches the necessary data using the useStrainQuery hook from the dicty-graphql-schema package. The component handles loading and error states, and renders the appropriate components based on the data received. It also sets the page title and meta description using the Helmet component from react-helmet.

---
+++ packages/ui-dsc/src/catalog/StrainDetailsContainer.tsx
@@ -0,0 +1,51 @@
+import { Helmet } from "react-helmet"
+import { useParams } from "react-router-dom"
+import Box from "@material-ui/core/Box"
+import { useStrainQuery } from "dicty-graphql-schema"
+import { GraphQLErrorPage } from "frontpage-components"
+import { characterConverter } from "../utils/characterConverter"
+import { DetailsHeader } from "./DetailsHeader"
+import { DetailsLoader } from "./DetailsLoader"
+import { StrainDetailsCard } from "./StrainDetailsCard"
+
+/**
+ * StrainDetailsContainer is the main component for an individual strain details page.
+ * It is responsible for fetching the data and passing it down to more specific components.
+ */
+const StrainDetailsContainer = () => {
+  const { id } = useParams()
+  const { loading, error, data } = useStrainQuery({
+    variables: { id: `${id}` },
+    errorPolicy: "ignore",
+    fetchPolicy: "cache-and-network",
+  })
+
+  if (loading) return <DetailsLoader />
+  if (error) return <GraphQLErrorPage error={error} />
+
+  const label = characterConverter(data?.strain?.label as string)
+  let title = `Strain Details for ${label}`
+  if (data?.strain?.phenotypes && data.strain.phenotypes.length > 0) {
+    title = `Phenotype and Strain Details for ${label}`
+  }
+
+  return (
+    <Box textAlign="center">
+      <Helmet>
+        <title>{title} - Dicty Stock Center</title>
+        <meta
+          name="description"
+          content={`Dicty Stock Center strain details page for ${label}`}
+        />
+      </Helmet>
+      {data?.strain && (
+        <>
+          <DetailsHeader id={data.strain.id} name={data.strain.label} />
+          <StrainDetailsCard data={data.strain} />
+        </>
+      )}
+    </Box>
+  )
+}
+
+export { StrainDetailsContainer }
