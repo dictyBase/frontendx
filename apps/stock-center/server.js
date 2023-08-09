987a59ab3a9c88f11da6bd50041429d6514454f9
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-09 09:40:35 -0500

    refactor(server.js): reorganize imports and update code formatting for better readability feat(server.js): add Apollo Server for GraphQL support and define type definitions and resolvers
    The imports in the server.js file have been reorganized to improve readability and maintainability. The code formatting has also been updated for consistency. Additionally, Apollo Server has been added to enable GraphQL support. Type definitions and resolvers have been defined for the GraphQL schema.

--- apps/stock-center/server.js
+++ apps/stock-center/server.js
@@ -1,21 +1,80 @@
-import express from "express"
 import ViteExpress from "vite-express"
-import bodyParser from "body-parser"
-import { generateListStrainDataOfLength } from "./src/mocks/listStrainData"
+import express from "express"
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from '@apollo/server/express4'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { generateListStrainDataOfLength } from "./src/mocks/listStrainData.js"
 
-const listStrainData = generateListStrainDataOfLength(25)
+const expressApp = express()
 
-const app = express()
+const typeDefs = `#graphql
+type Strain {
+  id: ID!
+  label: String!
+  summary: String!
+  inStock: Boolean!
+}
 
-app.use(bodyParser.json())
+type StrainListWithCursor {
+  strains: [Strain!]!
+  nextCursor: Int!
+  previousCursor: Int!
+  limit: Int
+  totalCount: Int!
+}
 
-app.get("graphql", (request, response) => {
-  response.send("test")
-})
+enum StrainType {
+  ALL
+  REGULAR
+  GWDI
+  BACTERIAL
+}
 
-const server = app.listen(3003, "0.0.0.0", () =>
+input StrainListFilter {
+  label: String
+  summary: String
+  id: ID
+  inStock: Boolean
+  strainType: StrainType!
+}
+
+type Query { 
+  listStrains(
+    cursor: Int
+    limit: Int
+    filter: StrainListFilter
+  ): StrainListWithCursor
+}
+`
+const strainList = generateListStrainDataOfLength(25)
+
+const resolvers = {
+  Query: {
+    listStrains: (_, variables) => {
+      const { cursor, limit, filter } = variables
+      const totalCount = strainList.length
+      const nextCursor = cursor + limit < totalCount ? cursor + limit : null
+      const strains = strainList.slice(0, cursor + limit)
+      console.log({ nextCursor, totalCount, strains })
+      return {
+        strains,
+        nextCursor,
+        totalCount
+      }
+    },
+  },
+}
+
+const graphqlServer = new ApolloServer({ typeDefs, resolvers })
+
+await graphqlServer.start()
+
+expressApp.use('/graphql', cors(), bodyParser.json(), expressMiddleware(graphqlServer))
+
+const server = expressApp.listen(3003, "0.0.0.0", () =>
   // eslint-disable-next-line no-console
   console.log("Server is listening..."),
 )
 
-ViteExpress.bind(app, server)
+ViteExpress.bind(expressApp, server)
