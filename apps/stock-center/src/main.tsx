efe3a1b790f05c607eb957835d5a1c043a34b9f1
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-10 08:43:30 -0500

    feat(main.tsx): add support for mock environment in development mode
    The main.tsx file now includes support for a mock environment when running the application in development mode. If the VITE_APP_DEPLOY_ENV environment variable is set to "mock", the application will import and start a mock worker before rendering the main App component. This allows developers to easily test and simulate different scenarios during development.

--- apps/stock-center/src/main.tsx
+++ apps/stock-center/src/main.tsx
@@ -2,9 +2,18 @@ import React from "react"
 import { render } from "react-dom"
 import { App } from "./App"
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.querySelector("#root"),
-)
+const main = async () => {
+  if (import.meta.env.VITE_APP_DEPLOY_ENV === "mock") {
+    const { default: worker } = await import("./mocks/browser")
+    await worker.start()
+  }
+
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.querySelector("#root"),
+  )
+}
+
+main()
