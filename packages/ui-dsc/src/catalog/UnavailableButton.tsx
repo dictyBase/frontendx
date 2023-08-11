f45aa02184872ce5a94e767d6b5d23ef78ccfcb9
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-10 19:21:22 -0500

    feat(UnavailableButton.tsx): add UnavailableButton component to display a disabled button with a tooltip when an item is not in stock
    The UnavailableButton component is added to the catalog directory. This component is used to display a disabled button with a tooltip when an item is not in stock. It uses Material-UI's IconButton and Tooltip components, as well as FontAwesomeIcon for the cart and slash icons. The button is disabled and has a default color. The size of the icon and the title for the tooltip can be customized through props.

---
+++ packages/ui-dsc/src/catalog/UnavailableButton.tsx
@@ -0,0 +1,40 @@
+import { makeStyles } from "@material-ui/core/styles"
+import IconButton from "@material-ui/core/IconButton"
+import Tooltip from "@material-ui/core/Tooltip"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const useStyles = makeStyles(() => ({
+  cartFullSlash: {
+    color: "#8a8a8a",
+  },
+}))
+
+type Props = {
+  /** Title used for button tooltip and aria-label */
+  title: string
+  /** Size of icon */
+  size?: "small" | "medium" | undefined
+}
+/**
+ * UnavailableButton appears on catalog pages when an item
+ * is not in stock.
+ */
+
+export const UnavailableButton = ({ title, size = "medium" }: Props) => {
+  const classes = useStyles()
+
+  return (
+    <Tooltip title={title}>
+      <span>
+        <IconButton disabled size={size} aria-label={title} color="default">
+          <span className="fa-layers fa-fw">
+            <FontAwesomeIcon icon="cart-plus" />
+            <FontAwesomeIcon icon="slash" className={classes.cartFullSlash} />
+          </span>
+        </IconButton>
+      </span>
+    </Tooltip>
+  )
+}
+
+export default UnavailableButton
