e2d66f8cbfa4aa826a42f2b1b7e7a24838072a93
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-14 13:15:11 -0500

    feat(App.tsx): add header and footer components to the app layout
    The styles for the main content and body text have been added to improve the visual appearance of the app. The header and footer components have been included to provide consistent navigation and branding throughout the app.

--- apps/stock-center/src/App.tsx
+++ apps/stock-center/src/App.tsx
@@ -1,12 +1,38 @@
 import { Fragment } from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { CssBaseline } from "@material-ui/core"
+import Container from "@material-ui/core/Container"
 import { useGraphqlClient, useApolloClientCache } from "@dictybase/data-access"
 import { listStrainsPagination } from "@dictybase/hook-dsc"
 import { ApolloProvider } from "@apollo/client"
+import { Header } from "@dictybase/header"
+import { Footer } from "@dictybase/footer"
 import { ThemeProvider } from "./ThemeProvider"
 import { routes } from "./routes"
 
+const useStyles = makeStyles((theme: Theme) => ({
+  main: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+  },
+  body: {
+    fontSize: "16px",
+    color: "#333",
+    backgroundColor: "#fff",
+    "& h1, h2, h3, h4, h5, h6": {
+      fontWeight: 500,
+      lineHeight: 1.1,
+    },
+    "& h4, h5, h6": {
+      marginTop: theme.spacing(1.2),
+      marginBottom: theme.spacing(1.2),
+    },
+  },
+}))
+
 export const App = () => {
+  const classes = useStyles()
   const client = useGraphqlClient({
     uri: import.meta.env.REACT_APP_GRAPHQL_SERVER as string,
     cache: useApolloClientCache({
@@ -23,13 +49,22 @@ export const App = () => {
     <BrowserRouter>
       <ApolloProvider client={client}>
         <ThemeProvider>
-          <Routes>
-            <Route path="/" element={<Navigate to="/strains" />} />
-            {routes.map(({ path, component: Component = Fragment }) => {
-              const element = <Component />
-              return <Route key={path} path={path} element={element} />
-            })}
-          </Routes>
+          <CssBaseline />
+          <div className={classes.body}>
+            <Header />
+            <main className={classes.main}>
+              <Container maxWidth="lg">
+                <Routes>
+                  <Route path="/" element={<Navigate to="/strains" />} />
+                  {routes.map(({ path, component: Component = Fragment }) => {
+                    const element = <Component />
+                    return <Route key={path} path={path} element={element} />
+                  })}
+                </Routes>
+              </Container>
+            </main>
+            <Footer />
+          </div>
         </ThemeProvider>
       </ApolloProvider>
     </BrowserRouter>
