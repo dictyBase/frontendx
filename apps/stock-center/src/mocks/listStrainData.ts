bfe45567e077dd351a30049e4bf95f3c97fad57a
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-14 13:17:03 -0500

    refactor(listStrainData.ts): change array creation syntax
    The code has been refactored to use the `new Array()` syntax instead of the spread operator `...new Array()`. This change improves code readability and consistency.

--- apps/stock-center/src/mocks/listStrainData.ts
+++ apps/stock-center/src/mocks/listStrainData.ts
@@ -11,11 +11,10 @@ const generateRandomData = () => {
     97 + Math.floor(Math.random() * 26),
   ).toLowerCase()}vark) null mutant (β-catenin knock-out)`
   const inStock = Math.random() < 0.5
-  console.log(id, label)
   return { id, label, summary, inStock }
 }
 
 const generateListStrainDataOfLength = (length: number) =>
-  [...new Array(length)].map(() => generateRandomData())
+  new Array(length).map(() => generateRandomData())
 
 export { generateListStrainDataOfLength }
