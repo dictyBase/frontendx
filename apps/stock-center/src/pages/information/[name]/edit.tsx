0e43a1002d6f42759e813c48410c39d91a00e54b
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-14 11:00:20 -0500

    feat(edit.tsx): add new edit page for information
    A new edit page for information is added. This page allows users to edit the information using the Editor component from the "editor" library. The page is wrapped in a Container component from Material-UI and rendered inside a Paper component. The InfoPageContainer component is exported as the default export.

---
+++ apps/stock-center/src/pages/information/[name]/edit.tsx
@@ -0,0 +1,13 @@
+import Container from "@material-ui/core/Container"
+import Paper from "@material-ui/core/Paper"
+import { Editor } from "editor"
+// import { InfoPageContainer } from "frontpage-components"
+
+const InfoPageContainer = () => (
+  <Container>
+    <Editor editable />
+  </Container>
+)
+
+// eslint-disable-next-line import/no-default-export
+export default InfoPageContainer
