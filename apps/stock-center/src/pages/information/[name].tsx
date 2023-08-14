c4b3812bfa2cd938dbef445d7e57fc82f90ae062
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-14 11:00:47 -0500

    chore([name].tsx): reorganize information/:name file structure
    Since an additonal route (information/:name/edit) was added to the project, the file structure for the information pages were changed so the :name route is now represented by information/[name]/index.tsx

--- apps/stock-center/src/pages/information/[name].tsx
+++
@@ -1,6 +0,0 @@
-// import { InfoPageContainer } from "frontpage-components"
-
-const InfoPageContainer = () => <></>
-
-// eslint-disable-next-line import/no-default-export
-export default InfoPageContainer
