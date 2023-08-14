928e0b641ffef14c8b2ce4dd9d9035dfcec16de5
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-14 11:06:09 -0500

    feat(addpage.tsx): add new page for adding information
    A new page called "addpage.tsx" has been added to the "information" directory in the "stock-center" app. This page includes a container component and an editor component from Material-UI. The page is used for adding information and is exported as the default component.

---
+++ apps/stock-center/src/pages/information/addpage.tsx
@@ -0,0 +1,12 @@
+import Container from "@material-ui/core/Container"
+import { Editor } from "editor"
+// import { InfoPageContainer } from "frontpage-components"
+
+const InfoPageContainer = () => (
+  <Container>
+    <Editor editable />
+  </Container>
+)
+
+// eslint-disable-next-line import/no-default-export
+export default InfoPageContainer
