cfaead67f24e071b374fde9e2fedf15daf9fa3f5
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-17 09:14:14 -0500

    fix(AddToCartButtonHandler.tsx): import AddToCartDialog component and add dialog functionality
    The AddToCartButtonHandler component now imports and uses the AddToCartDialog component from the "@dictybase/ui-dsc" library. This allows the user to open a dialog when adding an item to the cart. The dialog functionality is controlled by the useState hook, with the showDialog state variable and setShowDialog function.

--- apps/stock-center/src/components/AddToCartButtonHandler.tsx
+++ apps/stock-center/src/components/AddToCartButtonHandler.tsx
@@ -1,10 +1,11 @@
+import { useState } from "react"
 import { useAtomValue } from "jotai"
 import { findFirst } from "fp-ts/Array"
 import { match as Omatch } from "fp-ts/Option"
 import { match } from "ts-pattern"
 import { pipe } from "fp-ts/function"
 import { Strain } from "dicty-graphql-schema"
-import { UnavailableButton } from "@dictybase/ui-dsc"
+import { UnavailableButton, AddToCartDialog } from "@dictybase/ui-dsc"
 import { AddToCartButton } from "./AddToCartButton"
 import { isFullAtom, strainItemsAtom } from "../state"
 import { RemoveFromCartButton } from "./RemoveFromCartButton"
@@ -32,19 +33,35 @@ const useStockProperties = (
 }
 
 const AddToCartButtonHandler = ({ item }: AddToCartButtonHandlerProperties) => {
+  const [showDialog, setShowDialog] = useState(false)
   const { inStock, isFull, isInCart } = useStockProperties(item)
-  return match({ inStock, isFull, isInCart })
-    .with({ inStock: false }, () => (
-      <UnavailableButton title="Item is currently unavailable" size="medium" />
-    ))
-    .with({ isInCart: true }, () => <RemoveFromCartButton item={item} />)
-    .with({ isFull: true }, () => (
-      <UnavailableButton title="Shopping cart is full" size="medium" />
-    ))
-    .with({ isFull: false, inStock: true }, () => (
-      <AddToCartButton data={[item]} />
-    ))
-    .otherwise(() => <></>)
+  return (
+    <>
+      {match({ inStock, isFull, isInCart })
+        .with({ inStock: false }, () => (
+          <UnavailableButton
+            title="Item is currently unavailable"
+            size="medium"
+          />
+        ))
+        .with({ isInCart: true }, () => <RemoveFromCartButton item={item} />)
+        .with({ isFull: true }, () => (
+          <UnavailableButton title="Shopping cart is full" size="medium" />
+        ))
+        .with({ isFull: false, inStock: true }, () => (
+          <AddToCartButton data={[item]} setShowDialog={setShowDialog} />
+        ))
+        .otherwise(() => (
+          <></>
+        ))}
+      {match(showDialog)
+        .with(true, () => (
+          <AddToCartDialog data={[item]} setShowDialog={setShowDialog} />
+        ))
+        .with(false, () => <></>)
+        .exhaustive()}
+    </>
+  )
 }
 
 export { AddToCartButtonHandler }
