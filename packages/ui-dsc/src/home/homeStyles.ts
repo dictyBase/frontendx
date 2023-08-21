be2860c5d7c2de46b9e930d42c4ff6474e62711d
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-21 12:50:38 -0500

    feat(homeStyles.ts): add styles for the home component
    The `homeStyles.ts` file is added to the `ui-dsc` package. It contains styles for the home component, including styles for the panel, SOP link, header, list, intro, and column. These styles are defined using the `makeStyles` function from `@material-ui/core/styles` and are customizable based on the `panelBackground` property.

---
+++ packages/ui-dsc/src/home/homeStyles.ts
@@ -0,0 +1,50 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { blue, grey } from "@material-ui/core/colors"
+
+type HomeStylesProperties = {
+  /** Background color for panel */
+  panelBackground?: string
+}
+
+const useStyles = makeStyles<Theme, HomeStylesProperties>((theme: Theme) => ({
+  panel: {
+    backgroundColor: ({ panelBackground }) =>
+      panelBackground === "blue" ? blue[50] : grey[100],
+    border: "1px",
+    borderStyle: "solid",
+    borderColor: ({ panelBackground }) =>
+      panelBackground === "blue" ? blue[50] : grey[100],
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    borderRadius: "10px",
+  },
+  sopLink: {
+    color: theme.palette.error.dark,
+    "&:hover": {
+      color: theme.palette.error.main,
+    },
+  },
+  header: {
+    paddingBottom: "0px !important",
+    "& h1": {
+      fontSize: "48px",
+      marginTop: "0px",
+      marginBottom: "25px",
+      borderBottom: "1px solid #eee",
+    },
+  },
+  list: {
+    margin: theme.spacing(0),
+    padding: theme.spacing(0),
+    listStyle: "none",
+  },
+  intro: {
+    paddingTop: "0px !important",
+    paddingBottom: "12px !important",
+  },
+  column: {
+    paddingTop: theme.spacing(1),
+  },
+}))
+
+export { useStyles }
