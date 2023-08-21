0be5c42d985ff93cec90b3f76fddd3eb3c65df33
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-21 18:21:32 -0500

    feat(index.tsx): add homepage component for Dicty Stock Center (DSC)
    The index.tsx file in the pages directory is added to introduce the homepage component for the Dicty Stock Center (DSC) application. This component includes various UI elements such as Grid, Typography, Helmet, and LinkList from the @dictybase/ui-dsc package. It also imports link lists for download, information, materials, and miscellaneous links. The component is wrapped in a Helmet component to set the page title and meta description. The component is exported as the default export.

---
+++ apps/stock-center/src/pages/index.tsx
@@ -0,0 +1,88 @@
+import { Helmet } from "react-helmet"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import {
+  HomepageColumn,
+  LinkList,
+  Slideshow,
+  useStyles,
+} from "@dictybase/ui-dsc"
+// import { Availability } from "./Availability"
+// import OtherMaterials from "./OtherMaterials"
+// import EditablePanel from "./EditablePanel"
+// import StandardOperatingProcedures from "./StandardOperatingProcedures"
+// import { useAuthStore } from "features/Authentication/AuthStore"
+import {
+  downloadLinks,
+  infoLinks,
+  materialsLinks,
+  miscLinks,
+} from "../linkLists"
+// import EditorHolder from "./EditorHolder"
+
+const metaDesc =
+  "The Dicty Stock Center is a rapidly growing central repository for Dictyostelium discoideum strains and those of related species, plasmids, commonly used food bacteria, and other materials such as antibodies."
+
+/**
+ * Homepage is the main homepage component for DSC.
+ */
+
+const Homepage = () => {
+  const classes = useStyles({})
+  // const {
+  //   state: { isAuthenticated, user },
+  // } = useAuthStore()
+  // const fullName = `${user?.first_name} ${user?.last_name}`
+
+  return (
+    <div>
+      <Helmet>
+        <title>Dicty Stock Center</title>
+        <meta name="description" content={metaDesc} />
+      </Helmet>
+      {/* {isAuthenticated && (
+        <span>
+          <h3>Hello, {`${fullName}!`}</h3>
+        </span>
+      )} */}
+      <Grid container justifyContent="space-between" spacing={3}>
+        <Grid item className={classes.header}>
+          <Typography variant="h1">
+            Welcome to Dicty Stock Center (DSC)
+          </Typography>
+        </Grid>
+        {/* <Grid item xs={12} className={classes.intro}>
+          <EditorHolder minHeight="140px">
+            <EditablePanel slug="dsc-intro" skeletonCount={5} />
+          </EditorHolder>
+        </Grid> */}
+        {/* <HomepageColumn
+          components={[
+            <EditorHolder minHeight="300px">
+              <EditablePanel slug="dsc-about" skeletonCount={10} />
+            </EditorHolder>,
+            <LinkList list={miscLinks} bgColor="blue" />,
+          ]}
+        />
+        <HomepageColumn
+          components={[
+            <LinkList list={infoLinks} bgColor="blue" />,
+            <Availability />,
+            <OtherMaterials />,
+            <StandardOperatingProcedures />,
+          ]}
+        /> */}
+        <HomepageColumn
+          components={[
+            <Slideshow />,
+            <LinkList list={materialsLinks} bgColor="gray" />,
+            <LinkList list={downloadLinks} bgColor="gray" />,
+          ]}
+        />
+      </Grid>
+    </div>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Homepage
