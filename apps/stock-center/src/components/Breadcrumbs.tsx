de8dd04caf2b4dc215b0dd6bb250f2d70a9444c3
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 14:14:31 -0500

    feat(Breadcrumbs.tsx): add Breadcrumbs component for displaying navigation breadcrumbs in the DSC app
    The Breadcrumbs component is added to the DSC app to display navigation breadcrumbs. It uses Material-UI components such as Breadcrumbs, Link, and Typography to create the breadcrumb structure. The component also includes a helper function, convertBreadcrumbTitle, to convert specific breadcrumb titles into the desired format.

---
+++ apps/stock-center/src/components/Breadcrumbs.tsx
@@ -0,0 +1,88 @@
+import { makeStyles } from "@material-ui/core/styles"
+import MuiBreadCrumbs from "@material-ui/core/Breadcrumbs"
+import Link from "@material-ui/core/Link"
+import Typography from "@material-ui/core/Typography"
+import { useLocation, Link as RouterLink } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHome } from "@fortawesome/free-solid-svg-icons"
+import { capitalizeEveryWordInString } from "@dictybase/ui-dsc"
+import { BreadcrumbsLink } from "./BreadcrumbsLink"
+
+const useStyles = makeStyles({
+  icon: {
+    marginRight: "5px",
+  },
+})
+
+/**
+ * convertBreadcrumbTitle takes a given breadcrumb and converts it into
+ * the desired format
+ */
+const convertBreadcrumbTitle = (crumb: string) => {
+  let title = crumb
+  switch (crumb) {
+    case "faq":
+      title = "FAQs"
+      break
+    case "mydsc":
+      title = "MyDSC"
+      break
+    case "addpage":
+      title = "Add Page"
+      break
+    default:
+      const cleanString = crumb.replaceAll("+", " ").replaceAll("-", " ")
+      /** don't return uppercase words if crumb is for phenotype
+       * i.e. abolished+protein+phosphorylation
+       */
+      if (crumb.includes("+")) {
+        return cleanString
+      }
+      // for everything else, capitalize
+      return capitalizeEveryWordInString(cleanString)
+  }
+  return title
+}
+
+/**
+ * Breadcrumbs displays navigation breadcrumbs for the DSC app.
+ */
+
+const Breadcrumbs = () => {
+  const classes = useStyles()
+  const location = useLocation()
+  // get list of pathnames, filter out empty strings
+  const pathnames: Array<string> = location.pathname
+    .split("/")
+    .filter((x: string) => x)
+
+  return (
+    <MuiBreadCrumbs aria-label="breadcrumb">
+      {pathnames.length > 0 && (
+        <Link
+          color="inherit"
+          component={RouterLink}
+          to="/"
+          data-testid="breadcrumbs-home">
+          <FontAwesomeIcon icon={faHome} className={classes.icon} />
+          DSC Home
+        </Link>
+      )}
+      {pathnames.map((pathname, index) => {
+        const isLast = index === pathnames.length - 1
+        return isLast ? (
+          <Typography
+            key={pathname}
+            color="textPrimary"
+            data-testid="breadcrumbs-last">
+            {convertBreadcrumbTitle(pathname)}
+          </Typography>
+        ) : (
+          <BreadcrumbsLink key={pathname} pathname={pathname} />
+        )
+      })}
+    </MuiBreadCrumbs>
+  )
+}
+
+export { Breadcrumbs }
