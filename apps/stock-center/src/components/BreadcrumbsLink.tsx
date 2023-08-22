20dec9d51b4d9faa97e7919c593613dd257c6b5a
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 14:05:43 -0500

    feat(BreadcrumbsLink.tsx): add BreadcrumbsLink component
    The BreadcrumbsLink component is added to handle the display of breadcrumbs for routes that are not the final list item. It takes in the pathname from the URL and renders either a clickable link or non-clickable text based on the pathname. The component also includes a mapping of route names for display purposes.

---
+++ apps/stock-center/src/components/BreadcrumbsLink.tsx
@@ -0,0 +1,49 @@
+import Link from "@material-ui/core/Link"
+import Typography from "@material-ui/core/Typography"
+import { Link as RouterLink } from "react-router-dom"
+import { capitalizeFirstCharacter } from "@dictybase/ui-dsc"
+
+const breadcrumbNameMap: { [key: string]: string } = {
+  "/information": "Information",
+  "/order": "Order",
+  "/mydsc": "MyDSC",
+  "/strains": "Strains",
+  "/plasmids": "Plasmids",
+  "/phenotypes": "Phenotypes",
+}
+
+const nonClickableRoutes = ["phenotypes"]
+
+type BreadcrumbsLinkProperties = {
+  /** Pathname from URL (i.e. information) */
+  pathname: string
+}
+
+/**
+ * BreadcrumbsLink handles the display of any breadcrumbs that are not the
+ * final list item.
+ */
+
+const BreadcrumbsLink = ({ pathname }: BreadcrumbsLinkProperties) => {
+  if (nonClickableRoutes.includes(pathname)) {
+    return (
+      <Typography color="textPrimary" data-testid="breadcrumbs-text">
+        {capitalizeFirstCharacter(pathname)}
+      </Typography>
+    )
+  }
+
+  const route = `/${pathname}`
+
+  return (
+    <Link
+      color="inherit"
+      component={RouterLink}
+      to={route}
+      data-testid="breadcrumbs-link">
+      {breadcrumbNameMap[route]}
+    </Link>
+  )
+}
+
+export { BreadcrumbsLink }
