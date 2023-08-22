dc326a7c0f47d7f47927f53dbf5973711947d0e0
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 10:40:25 -0500

    fix(AddToCartButton.tsx): remove unused imports and variables, update property names
    The commit removes the unused import and variable for AddToCartDialog and the setCheckedItems and setHover functions. It also updates the property names in the Properties type to match the changes in the Strain schema, specifically changing "inStock" to "in_stock".

--- apps/stock-center/src/components/AddToCartButton.tsx
+++ apps/stock-center/src/components/AddToCartButton.tsx
@@ -3,11 +3,11 @@ import { makeStyles } from "@material-ui/core/styles"
 import IconButton from "@material-ui/core/IconButton"
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart"
 import { useSetAtom } from "jotai"
-import { AddToCartDialog, fees } from "@dictybase/ui-dsc"
+import { fees } from "@dictybase/ui-dsc"
 import { Strain } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
 import { map } from "fp-ts/Array"
-import { type StrainItem, addItemsAtom } from "../state"
+import { addItemsAtom } from "../state"
 
 const useStyles = makeStyles(({ palette }) => ({
   cartButton: {
@@ -17,19 +17,15 @@ const useStyles = makeStyles(({ palette }) => ({
 
 type Properties = {
   /** Stock data */
-  data: Array<Pick<Strain, "id" | "label" | "summary" | "inStock">>
+  data: Array<Pick<Strain, "id" | "label" | "summary" | "in_stock">>
   /** Function to toggle the AddToCartDialog */
   setShowDialog: React.Dispatch<React.SetStateAction<boolean>>
-  /** Function to add to checked items array */
-  setCheckedItems?: (argument0: Array<StrainItem>) => void
   /** Size of icon */
   size?: "small" | "medium" | undefined
-  /** Function to toggle hover state for given item */
-  setHover?: (argument0: boolean) => void
 }
 
 const appendFee = (
-  a: Pick<Strain, "id" | "label" | "summary" | "inStock">,
+  a: Pick<Strain, "id" | "label" | "summary" | "in_stock">,
 ) => ({ ...a, fee: fees.STRAIN_FEE })
 /**
  * AddToCartButton appears on the catalog page if the stock is available
