b852e936b7fee6fb56ffc277867966d091d97f6e
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 17:55:55 -0500

    chore(browser.ts): remove eslint-disable-next-line import/no-default-export comment
    The eslint-disable-next-line import/no-default-export comment is removed to ensure adherence to the linting rules and maintain code consistency.

--- apps/stock-center/src/mocks/browser.ts
+++ apps/stock-center/src/mocks/browser.ts
@@ -4,4 +4,5 @@ import { handlers } from "./handlers"
 
 // This configures a Service Worker with the given request handlers.
 const worker = setupWorker(...handlers)
+// eslint-disable-next-line import/no-default-export
 export default worker
