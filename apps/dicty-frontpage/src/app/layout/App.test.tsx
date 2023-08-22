1d8f68844f9152f35fcf9c007b74aeb98a442cb2
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 18:15:41 -0500

    chore(App.test.tsx): remove unused test file
    The file App.test.tsx was removed as it was no longer needed.

--- apps/dicty-frontpage/src/app/layout/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { getTokenIntervalDelayInMS } from "./App"
-
-describe("getTokenIntervalDelayInMS function", () => {
-  it("should return 0 if token is empty string", () => {
-    expect(getTokenIntervalDelayInMS("")).toEqual(0)
-  })
-})
