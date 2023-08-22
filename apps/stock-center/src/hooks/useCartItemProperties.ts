3fb1dd79f0a5a7628ff999c541e16b66209bdf5a
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-21 19:33:03 -0500

    feat(useCartItemProperties.ts): add custom hook for retrieving properties of a cart item
    The useCartItemProperties hook is added to the project. This hook takes in a cart item object and returns the following properties: isFull, in_stock, and isInCart. These properties are used to determine the state of the cart item in the application.

---
+++ apps/stock-center/src/hooks/useCartItemProperties.ts
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+import { useAtomValue } from "jotai"
+import { findFirst } from "fp-ts/Array"
+import { match as Omatch } from "fp-ts/Option"
+import { pipe } from "fp-ts/function"
+import { Strain } from "dicty-graphql-schema"
+import { isFullAtom, strainItemsAtom } from "../state"
+
+const useCartItemProperties = (
+  item: Pick<Strain, "id" | "label" | "summary" | "in_stock">,
+) => {
+  const isFull = useAtomValue(isFullAtom)
+  const itemsInCart = useAtomValue(strainItemsAtom)
+  const isInCart = pipe(
+    itemsInCart,
+    findFirst((cartItem) => item.id === cartItem.id),
+    Omatch(
+      () => false,
+      () => true,
+    ),
+  )
+  const { in_stock } = item
+
+  return { isFull, in_stock, isInCart }
+}
+
+export { useCartItemProperties }
