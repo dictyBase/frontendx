a0e0946453317db76b3f1bc4d72d8a39c42ef9ab
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 14:16:20 -0500

    feat(HeaderRow.tsx): add HeaderRow component
    The HeaderRow component is added to the project. It is a cart component that displays between the Navbar and body content on every page. It consists of a Breadcrumbs component and a CartIcon component that shows the current number of added items.

---
+++ apps/stock-center/src/components/HeaderRow.tsx
@@ -0,0 +1,27 @@
+import Grid from "@material-ui/core/Grid"
+import { CartIcon } from "@dictybase/ui-dsc"
+import { useAtomValue } from "jotai"
+import { Breadcrumbs } from "./Breadcrumbs"
+import { isFullAtom, strainItemsAtom } from "../state"
+/**
+ * This is the cart component that displays between the Navbar and body content on every page.
+ * It has a cart icon with the current number of added items next to it.
+ */
+
+const HeaderRow = () => {
+  const isFull = useAtomValue(isFullAtom)
+  const strainItems = useAtomValue(strainItemsAtom)
+
+  return (
+    <Grid container justifyContent="space-between" alignItems="center">
+      <Grid item>
+        <Breadcrumbs />
+      </Grid>
+      <Grid item>
+        <CartIcon isFull={isFull} items={strainItems} />
+      </Grid>
+    </Grid>
+  )
+}
+
+export { HeaderRow }
