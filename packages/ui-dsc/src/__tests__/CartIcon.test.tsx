bf345cd87637a87ced55472afbb86fbab65ff20b
Kevin Tun <kevin.tun@northwestern.edu>
2023-08-22 14:03:02 -0500

    test(ui-dsc): add unit tests for CartIcon component
    Added unit tests for the CartIcon component to ensure that it correctly indicates the number of items in the cart and displays a notice if the cart is full. The tests cover scenarios where there are items in the cart, no items in the cart, and when the cart is full.

---
+++ packages/ui-dsc/src/__tests__/CartIcon.test.tsx
@@ -0,0 +1,42 @@
+import { expect, test } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { MemoryRouter } from "react-router-dom"
+import { CartIcon } from "../cart/CartIcon"
+
+const cartItems = [
+  {
+    id: "DBS123",
+    label: "test1",
+    fee: 30,
+  },
+  {
+    id: "DBP456",
+    label: "test2",
+    fee: 15,
+  },
+]
+test("correctly indicates items in cart", () => {
+  render(
+    <MemoryRouter>
+      <CartIcon items={cartItems} isFull={false} />
+    </MemoryRouter>,
+  )
+  expect(screen.getByText("2")).toBeInTheDocument()
+})
+test("displays notice if cart is full", () => {
+  render(
+    <MemoryRouter>
+      <CartIcon items={[]} isFull />
+    </MemoryRouter>,
+  )
+  expect(screen.getByText("* cart full")).toBeInTheDocument()
+})
+
+test("correctly indicates no items in cart", () => {
+  render(
+    <MemoryRouter>
+      <CartIcon items={[]} isFull={false} />
+    </MemoryRouter>,
+  )
+  expect(screen.getByText("0")).toBeInTheDocument()
+})
