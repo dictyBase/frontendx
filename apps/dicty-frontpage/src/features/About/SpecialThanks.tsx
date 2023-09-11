e7a4900cd40e0612f1fe0c203b071953d3342010
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-11 14:48:49 -0500

    refactor(SpecialThanks.tsx): replace previous editor with new editor.
    - Use the Editor component instead of InlineEditor to display the content retrieved from the query.
    - Refactor the SpecialThanks component to use the match function to handle different scenarios based on the result of the useContentBySlugQuery hook.
    - Export the SpecialThanks component using named exports instead of default export.
    
    The changes were made to improve code organization and readability. The refactored code uses pattern matching to handle different scenarios based on the result of the useContentBySlugQuery hook. The Editor component is now used to display the content, and the component is exported using named exports instead of default export.

--- apps/dicty-frontpage/src/features/About/SpecialThanks.tsx
+++ apps/dicty-frontpage/src/features/About/SpecialThanks.tsx
@@ -1,34 +1,56 @@
 import React from "react"
 import Skeleton from "react-loading-skeleton"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
-import InlineEditor from "../EditablePages/InlineEditor"
+import { match, P } from "ts-pattern"
+import { Editor } from "editor"
 import NAMESPACE from "../../common/constants/namespace"
+
+const dataPattern = {
+  data: {
+    contentBySlug: {
+      content: P.select("content"),
+      slug: P.select("slug"),
+    },
+  },
+}
+const LoadingDisplay = () => (
+  <div>
+    <br />
+    <Skeleton count={2} />
+    <br />
+    <br />
+    <Skeleton count={5} />
+  </div>
+)
 
 /**
  * Displays the Special Thanks section of the About page.
  */
 
 const SpecialThanks = () => {
-  const { loading, error, data } = useContentBySlugQuery({
+  const result = useContentBySlugQuery({
     variables: {
       slug: `${NAMESPACE}-specialthanks`,
     },
   })
-
-  if (loading)
-    return (
-      <div>
-        <br />
-        <Skeleton count={2} />
-        <br />
-        <br />
-        <Skeleton count={5} />
-      </div>
-    )
-
-  if (error) return <div>Error retrieving technical summary information</div>
-
-  return <InlineEditor data={data?.contentBySlug} />
+  return (
+    <>
+      {match(result)
+        .with(dataPattern, ({ content, slug }) => (
+          <Editor
+            content={{
+              editorState: content,
+              storageKey: slug,
+            }}
+            editable={false}
+          />
+        ))
+        .with({ loading: true }, () => <LoadingDisplay />)
+        .with({ error: P.not(undefined) }, () => (
+          <div>Error retrieving technical summary information</div>
+        ))}
+    </>
+  )
 }
 
-export default SpecialThanks
+export { SpecialThanks }
