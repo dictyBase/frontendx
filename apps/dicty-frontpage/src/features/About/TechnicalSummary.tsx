d3b4e579c7cb91033fdbf663b3c736918f74f244
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-11 15:30:36 -0500

    refactor(TechnicalSummary.tsx): replace InlineEditor with current Editor
    - Replace InlineEditor component with Editor component from the "editor" package
    - Refactor the rendering logic using ts-pattern library for better readability and maintainability
    - Export TechnicalSummary component as a named export instead of default export
    
    The changes were made to improve the code by removing unused imports, using a more appropriate component, and refactoring the rendering logic using a pattern matching library. Exporting the component as a named export allows for better module organization and reusability.

--- apps/dicty-frontpage/src/features/About/TechnicalSummary.tsx
+++ apps/dicty-frontpage/src/features/About/TechnicalSummary.tsx
@@ -1,34 +1,58 @@
-import React from "react"
 import Skeleton from "react-loading-skeleton"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
-import InlineEditor from "../EditablePages/InlineEditor"
+import { match, P } from "ts-pattern"
+import { Editor } from "editor"
 import NAMESPACE from "../../common/constants/namespace"
 
+const dataPattern = {
+  data: {
+    contentBySlug: {
+      content: P.select("content"),
+      slug: P.select("slug"),
+    },
+  },
+}
+const LoadingDisplay = () => (
+  <div>
+    <br />
+    <Skeleton count={2} />
+    <br />
+    <br />
+    <Skeleton count={5} />
+  </div>
+)
+
 /**
- * Displays the Technical Summary section of the About page.
+ * Displays the Special Thanks section of the About page.
  */
 
 const TechnicalSummary = () => {
-  const { loading, error, data } = useContentBySlugQuery({
+  const result = useContentBySlugQuery({
     variables: {
       slug: `${NAMESPACE}-technicalsummary`,
     },
   })
-
-  if (loading)
-    return (
-      <div>
-        <br />
-        <Skeleton count={2} />
-        <br />
-        <br />
-        <Skeleton count={5} />
-      </div>
-    )
-
-  if (error) return <div>Error retrieving technical summary information</div>
-
-  return <InlineEditor data={data?.contentBySlug} />
+  return (
+    <>
+      {match(result)
+        .with(dataPattern, ({ content, slug }) => (
+          <Editor
+            content={{
+              editorState: content,
+              storageKey: slug,
+            }}
+            editable={false}
+          />
+        ))
+        .with({ loading: true }, () => <LoadingDisplay />)
+        .with({ error: P.not(undefined) }, () => (
+          <div>Error retrieving technical summary information</div>
+        ))
+        .otherwise(() => (
+          <></>
+        ))}
+    </>
+  )
 }
 
-export default TechnicalSummary
+export { TechnicalSummary }
