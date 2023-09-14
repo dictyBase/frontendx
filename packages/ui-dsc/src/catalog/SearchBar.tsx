2210b8edc79fb8885e900e7e3439c6f445b950fb
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-14 00:56:41 -0500

    refactor(SearchBar.tsx): remove unused setSearchParameters prop and refactor FilterDropdown component usage
    The setSearchParameters prop was not being used in the SearchBar component, so it was removed. The usage of the FilterDropdown component was also refactored to remove the unnecessary searchParamFn prop and directly pass the defaultFilter.param and defaultFilter.value props. This simplifies the code and improves readability.

--- packages/ui-dsc/src/catalog/SearchBar.tsx
+++ packages/ui-dsc/src/catalog/SearchBar.tsx
@@ -1,22 +1,13 @@
 import { Grid } from "@material-ui/core"
 import { defaultFilter, fieldsToVariables } from "@dictybase/hook-dsc"
-import { type SetURLSearchParams } from "react-router-dom"
 import { FilterDropdown } from "./FilterDropdown"
 import { SearchBox } from "./SearchBox"
 import { AppBarHelp } from "./AppBarHelp"
 
-type SearchBarProperties = {
-  setSearchParameters: SetURLSearchParams
-}
-
-const SearchBar = ({ setSearchParameters }: SearchBarProperties) => (
+const SearchBar = () => (
   <Grid container>
     <Grid item>
-      <FilterDropdown
-        searchParamFn={setSearchParameters}
-        param={defaultFilter.param}
-        value={defaultFilter.value}
-      />
+      <FilterDropdown param={defaultFilter.param} value={defaultFilter.value} />
     </Grid>
     <Grid item style={{ flexGrow: 1 }}>
       <SearchBox fields={Object.keys(fieldsToVariables)} />
