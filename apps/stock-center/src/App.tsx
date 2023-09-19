1b0dd03c203a4d26d0e3624bd47ebe4d80f8c957
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-09-19 16:39:19 -0500

    refactor(App.tsx): remove unused HeaderRow component
    The HeaderRow component is no longer used in the App component, so it
    has been removed to improve code cleanliness and remove unnecessary
    code.

--- apps/stock-center/src/App.tsx
+++ apps/stock-center/src/App.tsx
@@ -16,7 +16,6 @@ import { navbarItems, formatNavbarData } from "./navbarItems"
 import { navTheme } from "./themes"
 import { ThemeProvider } from "./ThemeProvider"
 import { dscRouter } from "./routes"
-import { HeaderRow } from "./components/HeaderRow"
 
 const useStyles = makeStyles((theme: Theme) => ({
   main: {
@@ -62,7 +61,6 @@ export const App = () => {
           <Navbar items={formatNavbarData(navbarItems)} theme={navTheme} />
           <main className={classes.main}>
             <Container maxWidth="lg">
-              <HeaderRow />
               <RouterProvider router={dscRouter} />
             </Container>
           </main>
