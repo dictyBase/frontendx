56d2a104d6644b2d77ddebaf884efce81781228e
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-20 14:54:54 -0500

    refactor(ErrorDisplay.tsx): refactor ErrorDisplay to improve visuals
    The component now uses Material-UI components for better styling and layout. The component has been updated to display a sad Dicty image and the error message in a more visually appealing way. The component now also capitalizes the first letter of the error message for better consistency.
    Closes #467

--- packages/ui-dsc/src/ErrorDisplay.tsx
+++ packages/ui-dsc/src/ErrorDisplay.tsx
@@ -1,16 +1,41 @@
 import { ApolloError } from "@apollo/client"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core/styles"
+import sadDicty from "./assets/sad-dicty.png"
 
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+})
 /**
  * The prop type for {@link ErrorDisplay}
  */
 export interface ErrorDisplayProperties {
   /** The error object of apollo client */
-  error: ApolloError | undefined
+  error: ApolloError
 }
 
 /**
  * Display error(s) coming from apollo client API call
  */
-export const ErrorDisplay = ({ error }: ErrorDisplayProperties) => (
-  <h2>Error..... {error?.message}</h2>
-)
+export const ErrorDisplay = ({ error }: ErrorDisplayProperties) => {
+  const { root } = useStyles()
+  const errorMessage = error.graphQLErrors[0]?.message ?? ""
+  return (
+    <Grid container className={root} direction="column">
+      <Grid item>
+        <img src={sadDicty} alt="Sad Dicty -- Page Not Found" />
+      </Grid>
+      <Grid item>
+        <Typography variant="h1">
+          {errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1)}
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
