d17a37322223ad2bfe790afa85ba9c1c6e15bac6
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-20 18:09:38 -0500

    refactor(Navbar.tsx): improve code readability and maintainability by using pattern matching
    The code in Navbar.tsx has been refactored to improve code readability and maintainability by using pattern matching with the ts-pattern library. This allows for more concise and expressive code when rendering the navbar items.

--- packages/navbar/src/components/Navbar.tsx
+++ packages/navbar/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import React, {
   forwardRef,
   ReactNode,
 } from "react"
+import { match, P } from "ts-pattern"
 import { ThemeProvider, styled } from "@material-ui/styles"
 import { Brand } from "./Brand"
 import { Dropdown } from "./Dropdown"
@@ -78,13 +79,15 @@ const Header = styled("li")({
 })
 
 type NavbarProperties = {
-  items: Array<{
-    dropdown: boolean
-    title: string
-    items: Array<{ name: string; href: string }>
-  }>
-  // | { dropdown: boolean; title: string; href: string }
-  // | { element: JSX.Element }
+  items: Array<
+    | {
+        dropdown: boolean
+        title: string
+        items: Array<{ name: string; href: string }>
+      }
+    | { dropdown: boolean; title: string; href: string }
+    | { element: JSX.Element }
+  >
   brand: { title: string; href: string }
   theme: Object
 }
@@ -96,16 +99,18 @@ type NavbarProperties = {
 const Navbar = ({ items, brand, theme }: NavbarProperties) => {
   const [activeIndex, setActiveIndex] = useState(-1)
   const [open, setOpen] = useState(false)
-  const navReference = useRef(null)
+  const navReference = useRef<HTMLElement>(null)
   // const iconReference = useRef(null)
   const containerReference = useRef<HTMLDivElement>(null)
 
   const onClose = () => {
+    if (!containerReference.current) return
     setOpen(false)
     transitionFromAuto(containerReference.current, 50)
   }
 
   const onOpen = () => {
+    if (!containerReference.current) return
     setOpen(true)
     transitionToAuto(containerReference.current)
   }
@@ -117,7 +122,11 @@ const Navbar = ({ items, brand, theme }: NavbarProperties) => {
   }
 
   const handleDocumentClick = (event: MouseEvent) => {
-    if (!wasClicked(event, navReference.current) && open) {
+    if (
+      navReference.current &&
+      !wasClicked(event, navReference.current) &&
+      open
+    ) {
       onClose()
     }
   }
@@ -145,23 +154,38 @@ const Navbar = ({ items, brand, theme }: NavbarProperties) => {
 
   const renderItems = () => (
     <Items>
-      {items.map((item, index) => {
-        if (item.dropdown) {
-          return (
-            <Dropdown
-              key={item.title}
-              index={index}
-              open={activeIndex === index}
-              items={item.items}
-              title={item.title}
-              theme={theme}
-              changeDropdown={changeDropdown}
-              // controlled
-            />
+      {items.map((item, index) =>
+        match(item)
+          .with(
+            {
+              dropdown: true,
+              items: P.select("items_", P.not(undefined)),
+              title: P.select("title", P.string),
+            },
+            ({ items_, title }) => (
+              <Dropdown
+                key={title}
+                index={index}
+                open={activeIndex === index}
+                items={items_}
+                title={title}
+                theme={theme}
+                changeDropdown={changeDropdown}
+                // controlled
+              />
+            ),
           )
-        }
-        return <Link href={item.href as string} title={item.title} />
-      })}
+          .with(
+            {
+              dropdown: false,
+              href: P.select("href", P.string),
+              title: P.select("title", P.string),
+            },
+            ({ href, title }) => <Link href={href as string} title={title} />,
+          )
+          .with({ element: P.select(P.not(undefined)) }, (element) => element)
+          .otherwise(() => <> This message should not appear.</>),
+      )}
     </Items>
   )
 
