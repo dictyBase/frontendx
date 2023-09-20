a33d9c5e1f8e4b3ef20a9a15ee1e1de021cc1dc2
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-20 16:46:46 -0500

    refactor(dom.ts): improve code readability and add type annotations
    The code in dom.ts has been refactored to improve code readability and maintainability. Type annotations have been added to function parameters and variables for better type safety. Additionally, eslint-disable comments have been updated to use the correct syntax.

--- packages/navbar/src/utils/dom.ts
+++ packages/navbar/src/utils/dom.ts
@@ -1,36 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+/* eslint-disable no-param-reassign */
 // @flow
-export const transitionFromAuto = (el: any, endHeight: number) => {
-  el.style.height = getComputedStyle(el).height
+export const transitionFromAuto = (element: any, endHeight: number) => {
+  element.style.height = getComputedStyle(element).height
   // Force repaint
-  //eslint-disable-next-line no-unused-expressions
-  el.offsetHeight
-  el.style.height = endHeight + "px"
+  // eslint-disable-next-line no-unused-expressions
+  element.offsetHeight
+  element.style.height = `${endHeight}px`
   // el.style.overflow = 'hidden'
 }
 
-export const transitionToAuto = (el: any) => {
-  const prevHeight = el.style.height
-  el.style.height = "auto"
-  const endHeight = getComputedStyle(el).height
-  el.style.height = prevHeight
+export const transitionToAuto = (element: HTMLElement) => {
+  const previousHeight = element.style.height
+  element.style.height = "auto"
+  const endHeight = getComputedStyle(element).height
+  element.style.height = previousHeight
   // Force repaint
-  //eslint-disable-next-line no-unused-expressions
-  el.offsetHeight
-  el.style.height = endHeight
+  // eslint-disable-next-line no-unused-expressions
+  element.offsetHeight
+  element.style.height = endHeight
 }
 
-export const wasClicked = (event: MouseEvent, element: any) => {
-  const rect: Object = element.getBoundingClientRect()
+export const wasClicked = (event: MouseEvent, element: HTMLElement) => {
+  const rect = element.getBoundingClientRect()
   const minX: number = rect.left + element.clientLeft
   const x: number = event.clientX
   const minY: number = rect.top + element.clientTop
   const y: number = event.clientY
+
   if (x < minX || x >= minX + element.clientWidth) {
     return false
   }
+  // eslint-disable-next-line sonarjs/prefer-single-boolean-return
   if (y < minY || y >= minY + element.clientHeight) {
     return false
   }
+
   return true
 }
 
@@ -40,7 +45,8 @@ export const calcTextWidth = (
   family: string = "sans-serif",
 ) => {
   const c = document.createElement("canvas")
-  let ctx = c.getContext("2d")
-  ctx.font = `${size} ${family}`
-  return ctx.measureText(text).width
+  const context = c.getContext("2d")
+  if (!context) return 0
+  context.font = `${size} ${family}`
+  return context.measureText(text).width
 }
