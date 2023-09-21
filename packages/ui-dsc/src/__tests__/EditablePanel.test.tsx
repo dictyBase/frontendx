3374034b17317a5641f96e893cc44280c70ad8e7
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-21 16:25:49 -0500

    test(EditablePanel.test.tsx): add unit test for rendering an editor with valid content data
    The new unit test `renders an editor it receives valid content data` checks if the `EditablePanel` component renders an editor and an input field when it receives valid content data. This test ensures that the component behaves correctly when provided with the expected props.

---
+++ packages/ui-dsc/src/__tests__/EditablePanel.test.tsx
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { EditablePanel } from "../home/EditablePanel"
+
+test("renders an editor it receives valid content data", () => {
+  render(<EditablePanel slug="" skeletonCount={10} />)
+
+  expect(screen.getByTestId("editor")).toBeInTheDocument()
+  expect(screen.getByRole("input")).toBeInTheDocument()
+})
