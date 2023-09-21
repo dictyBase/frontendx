5469c7d63e31e43928289f053896d54e8ef31ec2
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-21 11:39:36 -0500

    refactor(EditablePanel.tsx): refactor the rendering logic to use temporary content for development purposes
    The unused imports and code are commented out to improve code cleanliness. The rendering logic is refactored to use temporary content for testing purposes, as the actual data fetching is currently disabled. This allows for easier development and testing of the component.

--- packages/ui-dsc/src/home/EditablePanel.tsx
+++ packages/ui-dsc/src/home/EditablePanel.tsx
@@ -1,7 +1,46 @@
-import { useContentBySlugQuery } from "dicty-graphql-schema"
+// import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { P, match } from "ts-pattern"
 import { Editor } from "editor"
-import { PanelLoader } from "./PanelLoader"
+// import { PanelLoader } from "./PanelLoader"
+
+const temporaryContent = {
+  root: {
+    children: [
+      {
+        children: [
+          {
+            children: [
+              {
+                detail: 0,
+                format: 2,
+                mode: "normal",
+                style: "font-size: 20px;",
+                text: "Content coming soon!",
+                type: "text",
+                version: 1,
+              },
+            ],
+            direction: "ltr",
+            format: "",
+            indent: 0,
+            type: "paragraph",
+            version: 1,
+          },
+        ],
+        direction: "ltr",
+        format: "",
+        indent: 0,
+        type: "flex-layout",
+        version: 1,
+      },
+    ],
+    direction: "ltr",
+    format: "",
+    indent: 0,
+    type: "root",
+    version: 1,
+  },
+}
 
 type EditablePanelProperties = {
   /** The slug name for the data to fetch */
@@ -16,41 +55,51 @@ type EditablePanelProperties = {
  * authorized.
  */
 
-const EditablePanel = ({ slug, skeletonCount }: EditablePanelProperties) => {
-  const { loading, error, data } = useContentBySlugQuery({
-    variables: {
-      slug,
-    },
-    fetchPolicy: "cache-and-network",
-  })
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const EditablePanel = ({ slug, skeletonCount }: EditablePanelProperties) => (
+  // const { loading, error, data } = useContentBySlugQuery({
+  //   variables: {
+  //     slug,
+  //   },
+  //   fetchPolicy: "cache-and-network",
+  // })
 
-  return (
-    <>
-      {match({ loading, error, data })
-        .with(
-          {
-            data: {
-              contentBySlug: {
-                content: P.select("content", P.not(undefined)),
-                slug: P.select("slug_", P.string),
-              },
+  <>
+    {match({
+      loading: false,
+      error: undefined,
+      data: { contentBySlug: { content: temporaryContent, slug: "home" } },
+    })
+      .with(
+        {
+          data: {
+            contentBySlug: {
+              content: P.select("content", P.not(undefined)),
+              slug: P.select("slug_", P.string),
             },
           },
-          ({ content, slug_ }) => (
-            <Editor
-              editable={false}
-              content={{ editorState: content, storageKey: slug_ }}
-            />
-          ),
-        )
-        .with({ loading: true }, () => (
-          <PanelLoader skeletonCount={skeletonCount} />
-        ))
-        .with({ error: P.not(undefined) }, () => (
-          <div>Error fetching data</div>
-        ))}
-    </>
-  )
-}
+        },
+        ({ content, slug_ }) => (
+          <Editor
+            editable={false}
+            content={{
+              editorState: JSON.stringify(content),
+              storageKey: slug_,
+            }}
+          />
+        ),
+      )
+      // .with({ loading: true }, () => (
+      //   <PanelLoader skeletonCount={skeletonCount} />
+      // ))
+      // .with({ error: P.not(undefined) }, () => (
+      //   <div>Error fetching data</div>
+      // ))
+      .otherwise(() => (
+        <> This message should not appear. </>
+      ))}
+  </>
+)
 
 export { EditablePanel }
