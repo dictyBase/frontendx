7cd330830846f2b3d84ff24a69670ede3afd58b3
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-21 16:25:28 -0500

    feat(mockContent.ts): add mockContent object to simulate content data for the EditablePanel component
    The temporaryContent object is replaced with the mockContent object to display mock content in the EditablePanel component. This change allows for easier development and testing of the component without relying on actual data from the server. The mockContent object is added to the project to provide the necessary content structure for the EditablePanel component.

---
+++ packages/ui-dsc/src/mocks/mockContent.ts
@@ -0,0 +1,40 @@
+const mockContent = {
+  root: {
+    children: [
+      {
+        children: [
+          {
+            children: [
+              {
+                detail: 0,
+                format: 2,
+                mode: "normal",
+                style: "font-size: 20px;",
+                text: "Content coming soon!",
+                type: "text",
+                version: 1,
+              },
+            ],
+            direction: "ltr",
+            format: "",
+            indent: 0,
+            type: "paragraph",
+            version: 1,
+          },
+        ],
+        direction: "ltr",
+        format: "",
+        indent: 0,
+        type: "flex-layout",
+        version: 1,
+      },
+    ],
+    direction: "ltr",
+    format: "",
+    indent: 0,
+    type: "root",
+    version: 1,
+  },
+}
+
+export { mockContent }
