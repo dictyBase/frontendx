faf2ef7a66a9ba189a5dc7c2ef1465de89122615
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-21 17:34:38 -0500

    fix(mockContent.ts): change format value from 2 to 0
    The format value for the "detail" property in the mockContent has been changed from italic to normal text.

--- packages/ui-dsc/src/mocks/mockContent.ts
+++ packages/ui-dsc/src/mocks/mockContent.ts
@@ -7,10 +7,10 @@ const mockContent = {
             children: [
               {
                 detail: 0,
-                format: 2,
+                format: 0,
                 mode: "normal",
                 style: "font-size: 20px;",
-                text: "Content coming soon!",
+                text: "To make taffy, the process usually begins by combining sugar, corn syrup, butter, and water in a large saucepan over medium heat. The mixture is stirred until the sugar dissolves completely, and then the heat is increased to bring it to a boil. Once boiling, a candy thermometer is used to monitor the temperature, usually aiming for around 250°F (121°C). At this stage, the saucepan is removed from the heat, and any desired flavorings and food coloring are added. The mixture is then poured onto a greased baking sheet or marble slab. As it cools slightly, the taffy is pulled and folded repeatedly to aerate and stretch it, resulting in a chewy and taffy-like consistency. Once the taffy has cooled enough to handle, it is often cut into small pieces and wrapped individually in wax paper to maintain its freshness. This process requires precision in temperature control and skilled pulling techniques to create the desired texture of the taffy.",
                 type: "text",
                 version: 1,
               },
