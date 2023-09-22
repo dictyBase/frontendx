fe8158bd75b69428ae117803e61e64891dc8aabd
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-09-22 12:45:20 -0500

    refactor(callback.tsx): remove unnecessary return statement in Callback component
    The return statement in the Callback component is unnecessary as it only
    contains a conditional expression. Removing the return statement
    improves the code readability and reduces unnecessary code.

--- apps/stock-center/src/pages/callback.tsx
+++ apps/stock-center/src/pages/callback.tsx
@@ -3,7 +3,7 @@ import { LoadingDisplay } from "@dictybase/ui-dsc"
 
 const Callback = () => {
   const { isLoading } = useHandleSignInCallback(() => {})
-  return isLoading ? <LoadingDisplay rows={5} /> : <></>
+  isLoading ? <LoadingDisplay rows={5} /> : <></>
 }
 
 export default Callback
