44f4231ec5ade7fb5643c5256883bddcb23527f5
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2023-09-22 14:07:10 -0500

    refactor(auth): simplify LoginButton and LogoutButton components
    The LoginButton component has been simplified by removing the
    unnecessary Typography component and wrapping the content directly
    inside the IconButton component. This improves the code readability and
    reduces the number of unnecessary elements in the component.
    The LogoutButton component has also been simplified in a similar way,
    removing the unnecessary Typography component and wrapping the content
    directly inside the IconButton component. This simplification improves
    the code readability and reduces the number of unnecessary elements in
    the component.

--- apps/stock-center/src/components/auth/LoginButton.tsx
+++ apps/stock-center/src/components/auth/LoginButton.tsx
@@ -7,16 +7,16 @@ type LoginButtonProperties = { url: string }
 const LoginButton = ({ url }: LoginButtonProperties) => {
   const { signIn } = useLogto()
   return (
-    <>
-      <Typography variant="subtitle2">Login</Typography>
-      <IconButton
-        className={headerStyles().linksButton}
-        onClick={() => signIn(url)}>
-        <SvgIcon className={headerStyles().linksIcon}>
-          <path d="M14 12L10 8V11H2V13H10V16M22 12A10 10 0 0 1 2.46 15H4.59A8 8 0 1 0 4.59 9H2.46A10 10 0 0 1 22 12Z" />
-        </SvgIcon>
-      </IconButton>
-    </>
+    <IconButton
+      className={headerStyles().linksButton}
+      onClick={() => signIn(url)}>
+      <Typography variant="subtitle2" style={{ display: "flex" }}>
+        Login
+      </Typography>
+      <SvgIcon className={headerStyles().linksIcon}>
+        <path d="M14 12L10 8V11H2V13H10V16M22 12A10 10 0 0 1 2.46 15H4.59A8 8 0 1 0 4.59 9H2.46A10 10 0 0 1 22 12Z" />
+      </SvgIcon>
+    </IconButton>
   )
 }
 
