932c48c0510aa4f7ffff09f49990b75b47173316
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-22 23:51:30 -0500

    test(EditablePanel.test.tsx): update test to use sampleText from mockContent for assertion
    The test has been updated to use the `sampleText` variable from `mockContent` for the assertion instead of the hardcoded string "Content coming soon". This change improves the test's reliability and maintainability by using a consistent source of data for the assertion.

--- packages/ui-dsc/src/__tests__/EditablePanel.test.tsx
+++ packages/ui-dsc/src/__tests__/EditablePanel.test.tsx
@@ -1,9 +1,10 @@
 import { test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { EditablePanel } from "../home/EditablePanel"
+import { sampleText } from "../mocks/mockContent"
 
 test("renders an editor it receives valid content data", () => {
   render(<EditablePanel slug="" skeletonCount={10} />)
 
-  expect(screen.getByText(/Content coming soon/)).toBeInTheDocument()
+  expect(screen.getByText(sampleText)).toBeInTheDocument()
 })
