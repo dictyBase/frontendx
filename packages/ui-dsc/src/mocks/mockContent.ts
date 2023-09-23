6edef54ba318e336c3d431eabfc3b6755ba6518c
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-22 23:48:40 -0500

    chore(mockContent.ts): extract sampleText into a separate variable for reusability
    The sample text used in the mockContent file has been extracted into a separate variable called sampleText. This allows for better code organization and reusability of the sample text in other parts of the codebase.

--- packages/ui-dsc/src/mocks/mockContent.ts
+++ packages/ui-dsc/src/mocks/mockContent.ts
@@ -1,3 +1,6 @@
+const sampleText =
+  "To make taffy, the process usually begins by combining sugar, corn syrup, butter, and water in a large saucepan over medium heat. The mixture is stirred until the sugar dissolves completely, and then the heat is increased to bring it to a boil. Once boiling, a candy thermometer is used to monitor the temperature, usually aiming for around 250°F (121°C). At this stage, the saucepan is removed from the heat, and any desired flavorings and food coloring are added. The mixture is then poured onto a greased baking sheet or marble slab. As it cools slightly, the taffy is pulled and folded repeatedly to aerate and stretch it, resulting in a chewy and taffy-like consistency. Once the taffy has cooled enough to handle, it is often cut into small pieces and wrapped individually in wax paper to maintain its freshness. This process requires precision in temperature control and skilled pulling techniques to create the desired texture of the taffy."
+
 const mockContent = {
   root: {
     children: [
@@ -10,7 +13,7 @@ const mockContent = {
                 format: 0,
                 mode: "normal",
                 style: "font-size: 20px;",
-                text: "To make taffy, the process usually begins by combining sugar, corn syrup, butter, and water in a large saucepan over medium heat. The mixture is stirred until the sugar dissolves completely, and then the heat is increased to bring it to a boil. Once boiling, a candy thermometer is used to monitor the temperature, usually aiming for around 250°F (121°C). At this stage, the saucepan is removed from the heat, and any desired flavorings and food coloring are added. The mixture is then poured onto a greased baking sheet or marble slab. As it cools slightly, the taffy is pulled and folded repeatedly to aerate and stretch it, resulting in a chewy and taffy-like consistency. Once the taffy has cooled enough to handle, it is often cut into small pieces and wrapped individually in wax paper to maintain its freshness. This process requires precision in temperature control and skilled pulling techniques to create the desired texture of the taffy.",
+                text: sampleText,
                 type: "text",
                 version: 1,
               },
@@ -37,4 +40,4 @@ const mockContent = {
   },
 }
 
-export { mockContent }
+export { sampleText, mockContent }
