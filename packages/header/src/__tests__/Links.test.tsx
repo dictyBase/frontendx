3da74c55eeca3c8d641d3664676e62761df1d69e
Siddhartha Basu <biosidd@gmail.com>
2023-09-25 17:15:40 -0500

    test(Links.test.tsx): add test case for LoginButton component in Links component
    The test case has been added to test that the LoginButton component is
    rendered correctly in the Links component.

--- packages/header/src/__tests__/Links.test.tsx
+++ packages/header/src/__tests__/Links.test.tsx
@@ -2,11 +2,25 @@ import { render } from "@testing-library/react"
 import { expect, describe, test } from "vitest"
 import { Links } from "../functional/Links"
 import { iconItems } from "../components/LinksContainer"
+import { IconButton, SvgIcon, Typography } from "@material-ui/core"
+
+const LoginButton = () => {
+  return (
+    <IconButton href="/goofy">
+      <Typography variant="subtitle2" style={{ display: "flex" }}>
+        Login
+      </Typography>
+      <SvgIcon>
+        <path d="M14" />
+      </SvgIcon>
+    </IconButton>
+  )
+}
 
 describe("functional links ", () => {
   test("should generate the links ", () => {
-    const { getAllByRole } = render(<Links />)
-    expect(getAllByRole("link")).toHaveLength(iconItems.length)
+    const { getAllByRole } = render(<Links LoginOut={<LoginButton />} />)
+    expect(getAllByRole("link")).toHaveLength(iconItems.length + 1)
   })
   test.each(iconItems)(
     "should have link $href with title $title",
