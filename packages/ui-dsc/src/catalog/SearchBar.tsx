ca48005acf27dec2c245f130038629187f0edc16
Kevin Tun <kevin.tun@northwestern.edu>
2023-09-26 12:35:31 -0500

    refactor(SearchBar.tsx): add styles to SearchBox component using makeStyles hook
    The SearchBar component now uses the makeStyles hook from Material-UI to define styles for the SearchBox component. This improves the maintainability and reusability of the code by separating the styling concerns from the component logic.
    Closes #485

--- packages/ui-dsc/src/catalog/SearchBar.tsx
+++ packages/ui-dsc/src/catalog/SearchBar.tsx
@@ -1,21 +1,34 @@
 import { Grid } from "@material-ui/core"
 import { defaultFilter, fieldsToVariables } from "@dictybase/hook-dsc"
+import { makeStyles } from "@material-ui/core/styles"
 import { FilterDropdown } from "./FilterDropdown"
 import { SearchBox } from "./SearchBox"
 import { AppBarHelp } from "./AppBarHelp"
 
-const SearchBar = () => (
-  <Grid container>
-    <Grid item>
-      <FilterDropdown param={defaultFilter.param} value={defaultFilter.value} />
+const useStyles = makeStyles({
+  searchBox: {
+    flexBasis: "41.5%",
+  },
+})
+
+const SearchBar = () => {
+  const { searchBox } = useStyles()
+  return (
+    <Grid container>
+      <Grid item>
+        <FilterDropdown
+          param={defaultFilter.param}
+          value={defaultFilter.value}
+        />
+      </Grid>
+      <Grid item className={searchBox}>
+        <SearchBox fields={Object.keys(fieldsToVariables)} />
+      </Grid>
+      <Grid item>
+        <AppBarHelp />
+      </Grid>
     </Grid>
-    <Grid item style={{ flexGrow: 1 }}>
-      <SearchBox fields={Object.keys(fieldsToVariables)} />
-    </Grid>
-    <Grid item>
-      <AppBarHelp />
-    </Grid>
-  </Grid>
-)
+  )
+}
 
 export { SearchBar }
