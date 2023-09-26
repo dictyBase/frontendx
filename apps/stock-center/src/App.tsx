3228765a3fe93a6caa84258642e81431b5ffd23c
Siddhartha Basu <biosidd@gmail.com>
2023-09-26 08:12:39 -0500

    refactor(App.tsx): remove unused imports and simplify component structure
    The unused imports and unnecessary component structure have been removed
    to improve code readability and maintainability. The component now uses
    the simplified DscApp component instead of the previous complex
    structure with HeaderWithAuth, Navbar, Container, and Footer components.

--- apps/stock-center/src/App.tsx
+++ apps/stock-center/src/App.tsx
@@ -1,7 +1,4 @@
-import { RouterProvider } from "react-router-dom"
-import { makeStyles, Theme } from "@material-ui/core/styles"
 import { CssBaseline } from "@material-ui/core"
-import Container from "@material-ui/core/Container"
 import { useGraphqlClient, useApolloClientCache } from "@dictybase/data-access"
 import {
   listStrainsPagination,
@@ -9,44 +6,16 @@ import {
 } from "@dictybase/hook-dsc"
 import { ApolloProvider } from "@apollo/client"
 import "@fontsource/roboto"
-import { Footer } from "@dictybase/footer"
-import { Navbar } from "@dictybase/navbar"
-import { NotFoundError } from "@dictybase/ui-dsc"
-import { navbarItems, formatNavbarData } from "./navbarItems"
-import { navTheme } from "./themes"
 import { ThemeProvider } from "./ThemeProvider"
-import { dscRouter } from "./routes"
 import { LogtoProvider, LogtoConfig } from "@logto/react"
-import { HeaderWithAuth } from "./components/auth/HeaderWithAuth"
-import { LoadingDisplay } from "@dictybase/ui-dsc"
+import { DscApp } from "./components/DscApp"
 
 const logtoConf: LogtoConfig = {
   endpoint: import.meta.env.VITE_LOGTO_ENDPOINT,
   appId: import.meta.env.VITE_LOGTO_APPID,
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  main: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(4),
-  },
-  body: {
-    fontSize: "16px",
-    color: "#333",
-    backgroundColor: "#fff",
-    "& h1, h2, h3, h4, h5, h6": {
-      fontWeight: 500,
-      lineHeight: 1.1,
-    },
-    "& h4, h5, h6": {
-      marginTop: theme.spacing(1.2),
-      marginBottom: theme.spacing(1.2),
-    },
-  },
-}))
-
 export const App = () => {
-  const classes = useStyles()
   const client = useGraphqlClient({
     uri: import.meta.env.VITE_APP_GRAPHQL_SERVER as string,
     cache: useApolloClientCache({
@@ -65,19 +34,7 @@ export const App = () => {
       <LogtoProvider config={logtoConf}>
         <ThemeProvider>
           <CssBaseline />
-          <div className={classes.body}>
-            <HeaderWithAuth />
-            <Navbar items={formatNavbarData(navbarItems)} theme={navTheme} />
-            <main className={classes.main}>
-              <Container maxWidth="lg">
-                <RouterProvider
-                  router={dscRouter}
-                  fallbackElement={<LoadingDisplay rows={6} />}
-                />
-              </Container>
-            </main>
-            <Footer />
-          </div>
+          <DscApp />
         </ThemeProvider>
       </LogtoProvider>
     </ApolloProvider>
