a000fa0248441891edaf1a588d62113a93959eeb
Siddhartha Basu <biosidd@gmail.com>
2023-09-26 17:18:54 -0500

    feat(HeaderWithAuth.tsx): fetch user information and display user name in logout button when authenticated
    The code in HeaderWithAuth.tsx has been refactored to improve code
    readability. The logoutHandler function now accepts an additional user
    parameter to display the user name in the logout button. The
    conditonalHandler function has been updated to check if the user is
    authenticated and display the user name in the logout button if
    available. The useEffect hook has been added to fetch user information
    when the user is authenticated and store it in the state. This allows
    the user name to be displayed in the logout button.

--- apps/stock-center/src/components/auth/HeaderWithAuth.tsx
+++ apps/stock-center/src/components/auth/HeaderWithAuth.tsx
@@ -1,32 +1,50 @@
 import { Header } from "@dictybase/header"
+import { match, P } from "ts-pattern"
+import { useLogto, UserInfoResponse } from "@logto/react"
+import { useState, useEffect } from "react"
 import { LoginButton } from "./LoginButton"
 import { LogoutButton } from "./LogoutButton"
-import { match, P } from "ts-pattern"
-import { useLogto } from "@logto/react"
 import { callbackPath, homePath } from "../../routes"
 
 type logtoHookProperties = {
   error: Error | undefined
   isAuthenticated: boolean
   isLoading: boolean
+  user?: UserInfoResponse
 }
 
 const loginHandler = (url: string) => <LoginButton url={url} />
-const logoutHandler = (url: string) => <LogoutButton url={url} />
-const conditonalHandler = (logtoCase: logtoHookProperties) => {
-  return match(logtoCase)
+const logoutHandler = (url: string, user?: UserInfoResponse) => (
+  <LogoutButton url={url} name={user?.name as string} />
+)
+const conditonalHandler = (logtoCase: logtoHookProperties) =>
+  match(logtoCase)
     .with(
-      P.when(({ isAuthenticated }) => isAuthenticated),
-      () => <Header LoginOut={logoutHandler(homePath)} />,
+      P.when(({ isAuthenticated, user }) =>
+        isAuthenticated && user ? isAuthenticated : false,
+      ),
+      ({ user }) => <Header LoginOut={logoutHandler(homePath, user)} />,
     )
-    .otherwise(() => (
-      <Header LoginOut={loginHandler(callbackPath)} />
-    ))
-}
+    .otherwise(() => <Header LoginOut={loginHandler(callbackPath)} />)
 
 const HeaderWithAuth = () => {
-  const { isAuthenticated, isLoading, error } = useLogto()
-  return conditonalHandler({ isAuthenticated, isLoading, error })
+  const { isAuthenticated, isLoading, error, fetchUserInfo } = useLogto()
+  const [user, setUser] = useState<UserInfoResponse>()
+  useEffect(() => {
+    const handleUserInformation = async () => {
+      if (isAuthenticated) {
+        const authUser = await fetchUserInfo()
+        setUser(authUser)
+      }
+    }
+    handleUserInformation()
+  }, [fetchUserInfo, isAuthenticated])
+  return conditonalHandler({
+    isAuthenticated,
+    isLoading,
+    error,
+    user,
+  })
 }
 
 export { HeaderWithAuth }
