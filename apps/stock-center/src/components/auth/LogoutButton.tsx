332886decd5f321a57766e40c03acfa1da9f6fab
Siddhartha Basu <biosidd@gmail.com>
2023-09-26 20:49:32 -0500

    refactor(LogoutButton.tsx): remove unused imports and improve button styling and functionality
    The unused import of `headerStyles` from `@dictybase/header` has been
    removed. The button component has been updated to use the `Button`
    component from `@material-ui/core` and display the `PersonSharp` icon.
    The button now has a contained variant, primary color, and no elevation.
    The `onClick` event now calls the `signOut` function with the provided
    URL.

--- apps/stock-center/src/components/auth/LogoutButton.tsx
+++ apps/stock-center/src/components/auth/LogoutButton.tsx
@@ -1,5 +1,4 @@
 import { useLogto } from "@logto/react"
-import { headerStyles } from "@dictybase/header"
 import {
   split as splitString,
   slice as sliceString,
@@ -8,12 +7,13 @@ import {
 import { pipe } from "fp-ts/function"
 import { map as Amap } from "fp-ts/Array"
 import { ReadonlyNonEmptyArray, head, last } from "fp-ts/ReadonlyNonEmptyArray"
+import { Button } from "@material-ui/core"
+import { PersonSharp } from "@material-ui/icons"
 
 type LogoutButtonProperties = {
   url: string
   name: string
 }
-
 
 const firstLast = (nameArry: ReadonlyNonEmptyArray<string>) => [
   head(nameArry),
@@ -27,8 +27,17 @@ const nameToUpperInitial = (fullName: string) =>
 
 const LogoutButton = ({ url, name }: LogoutButtonProperties) => {
   const { signOut } = useLogto()
-
   return (
+    <div>
+      <Button
+        disableElevation
+        variant="contained"
+        color="primary"
+        endIcon={<PersonSharp />}
+        onClick={() => signOut(url)}>
+        {nameToUpperInitial(name)}
+      </Button>
+    </div>
   )
 }
 
