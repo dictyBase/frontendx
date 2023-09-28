ac5839e2b299cd04eb3ad618022023e65a0a24b0
Siddhartha Basu <biosidd@gmail.com>
2023-09-28 14:23:08 -0500

    feat(user/edit.tsx): add new page for editing user information
    A new page called "Edit" has been added to the user section of the
    application. This page is responsible for displaying a form to edit user
    information. It receives the user object as a prop from the previous
    page and displays the user's name in a Typography component.

---
+++ apps/stock-center/src/pages/user/edit.tsx
@@ -0,0 +1,11 @@
+import { useLocation } from "react-router-dom"
+import { UserInfoResponse } from "@logto/react"
+import { Typography } from "@material-ui/core"
+
+const Edit = () => {
+  const location = useLocation()
+  const user = location.state as UserInfoResponse
+  return <Typography variant="h1">Edit {user.name}</Typography>
+}
+
+export default Edit
