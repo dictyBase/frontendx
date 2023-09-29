7182051e7dd17c80ac4d53a7d8d8f346c4ad84b6
Siddhartha Basu <biosidd@gmail.com>
2023-09-29 18:33:27 -0500

    feat(Login.tsx): add Login component to handle authentication logic and redirect based on authentication status
    The Login component is added to handle authentication logic. It uses the
    `useLogto` hook from the `@logto/react` package to check if the user is
    authenticated. If the user is authenticated, it redirects to the home
    page ("/"), otherwise it redirects to the callback page ("/callback").

---
+++ packages/auth/src/Login.tsx
@@ -0,0 +1,9 @@
+import { useLogto } from "@logto/react"
+import { Navigate } from "react-router-dom"
+
+const Login = () => {
+  const { isAuthenticated } = useLogto()
+  return isAuthenticated ? <Navigate to="/" /> : <Navigate to="/callback" />
+}
+
+export default Login
