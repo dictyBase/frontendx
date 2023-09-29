19886e840182fdbf131e2f8425c2204255b886f6
Siddhartha Basu <biosidd@gmail.com>
2023-09-29 18:32:38 -0500

    feat(auth): add Callback component to handle sign-in callback
    The Callback component is added to handle the sign-in callback. It uses
    the `useHandleSignInCallback` hook from the `@logto/react` package to
    handle the sign-in callback and redirects the user to the home page
    ("/") using the `useNavigate` hook from the `react-router-dom` package.
    If the sign-in process is still loading, a loading display with 5 rows
    is shown.

---
+++ packages/auth/src/Callback.tsx
@@ -0,0 +1,13 @@
+import { useHandleSignInCallback } from "@logto/react"
+import { LoadingDisplay } from "./LoadingDisplay"
+import { useNavigate } from "react-router-dom"
+
+const Callback = () => {
+  const navigate = useNavigate()
+  const { isLoading } = useHandleSignInCallback(() => {
+    navigate("/")
+  })
+  return isLoading ? <LoadingDisplay rows={5} /> : <></>
+}
+
+export default Callback
