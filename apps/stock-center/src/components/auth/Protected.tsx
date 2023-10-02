630cadb62122d799ee4134e1d7e8a56ee43a9cca
Siddhartha Basu <biosidd@gmail.com>
2023-10-02 13:48:26 -0500

    chore(auth): remove unused authentication components and routes
    The commit removes the following unused authentication components and routes:
    - HeaderWithAuth.tsx
    - LoginButton.tsx
    - LogoutButton.tsx
    - Protected.tsx

--- apps/stock-center/src/components/auth/Protected.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom"
-import { useLogto } from "@logto/react"
-
-const Protected = () => {
-  const { isAuthenticated } = useLogto()
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />
-}
-
-export { Protected }
