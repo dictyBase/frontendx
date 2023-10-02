510f4e0e2052429d099294f6b4c1113a39295c32
Siddhartha Basu <biosidd@gmail.com>
2023-10-02 13:39:02 -0500

    chore(cart.tsx): remove unused CartHandler component
    The CartHandler and SignIn components were no longer being used in the
    application and were therefore removed to improve code cleanliness and
    reduce unnecessary complexity.

--- apps/stock-center/src/pages/login.tsx
+++
@@ -1,9 +0,0 @@
-import { useLogto } from "@logto/react"
-import { Navigate } from "react-router-dom"
-
-const SignIn = () => {
-  const { isAuthenticated } = useLogto()
-  return isAuthenticated ? <Navigate to="/" /> : <Navigate to="/callback" />
-}
-
-export default SignIn
