4c845d1b3d740e24faff21e0930ae12817b61600
Siddhartha Basu <biosidd@gmail.com>
2023-10-04 16:16:11 -0500

    fix(user): update import paths for ACCESS constant in edit.tsx and show.tsx
    The import paths for the ACCESS constant have been updated to reflect
    the correct location in the "auth" module.

--- apps/stock-center/src/pages/user/edit.tsx
+++ apps/stock-center/src/pages/user/edit.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom"
 import { UserInfoResponse } from "@logto/react"
 import { Typography } from "@material-ui/core"
-import { ACCESS } from "../../types"
+import { ACCESS } from "auth"
 
 const Edit = () => {
   const location = useLocation()
