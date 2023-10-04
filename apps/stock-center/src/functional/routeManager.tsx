f0364f1b3d94eeafd3ba6245a22879a0b67d769d
Siddhartha Basu <biosidd@gmail.com>
2023-10-03 21:40:18 -0500

    feat(routeManager.tsx): add routeManager file to handle dynamic routes and build merged routes
    The routeManager.tsx file is added to the functional directory in the
    stock-center app. This file handles the creation of dynamic routes based
    on the provided route configuration. It exports functions to generate
    public and protected routes, as well as a function to build the merged
    routes. The dynamicRoutesProperties type is defined to represent the
    configuration of the dynamic routes. The pathParts function is used to
    transform the route path into a format compatible with react-router-dom.
    The mapToRouteObject function is used to map the route configuration to
    a RouteObject. The publicRoutes function filters the dynamic routes to
    generate an array of public routes. The protectedRoutes function filters
    the dynamic routes to generate an array of protected routes. The
    buildMergedRoutes function combines the public and protected routes and
    adds additional routes for authentication.

---
+++ apps/stock-center/src/functional/routeManager.tsx
@@ -0,0 +1,58 @@
+import { type FunctionComponent } from "react"
+import { ACCESS } from "../types"
+import { RouteObject } from "react-router-dom"
+import { Callback, Login, Protected } from "auth"
+import { collect as Rcollect, filter as Rfilter } from "fp-ts/Record"
+import { Ord } from "fp-ts/string"
+import { append as Arpend } from "fp-ts/Array"
+import { pipe } from "fp-ts/function"
+
+type PageComponentData = {
+  default: FunctionComponent
+  access: ACCESS
+}
+type mergedRoutesProperties = {
+  publicR: Array<RouteObject>
+  protectedR: Array<RouteObject>
+}
+type dynamicRoutesProperties = Record<string, PageComponentData>
+
+const pathParts = (path: string) =>
+  path
+    .replace(/\/src\/pages|index|\.tsx$/g, "")
+    .replace(/\[\.{3}.+]/, "*")
+    .replace(/\[(.+)\]/, ":$1")
+
+const mapToRouteObject = (route: string, value: PageComponentData) => {
+  const PageComponent = value.default
+  return { path: pathParts(route), element: <PageComponent /> }
+}
+
+const publicRoutes = (allRoutes: dynamicRoutesProperties): Array<RouteObject> =>
+  pipe(
+    allRoutes,
+    Rfilter((v) => v.access !== ACCESS.protected),
+    Rcollect(Ord)(mapToRouteObject),
+  )
+const protectedRoutes = (
+  allRoutes: dynamicRoutesProperties,
+): Array<RouteObject> =>
+  pipe(
+    allRoutes,
+    Rfilter((v) => v.access === ACCESS.protected),
+    Rcollect(Ord)(mapToRouteObject),
+  )
+const buildMergedRoutes = ({ publicR, protectedR }: mergedRoutesProperties) =>
+  pipe(
+    publicR,
+    Arpend({ children: protectedR, element: <Protected /> } as RouteObject),
+    Arpend({ path: "/callback", element: <Callback /> } as RouteObject),
+    Arpend({ path: "/login", element: <Login /> } as RouteObject),
+  )
+
+export {
+  publicRoutes,
+  protectedRoutes,
+  buildMergedRoutes,
+  type dynamicRoutesProperties,
+}
