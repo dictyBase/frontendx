9bd11c5e4f1c31c433135c878a5e18948df5c223
Siddhartha Basu <biosidd@gmail.com>
2023-10-03 21:40:34 -0500

    refactor(routes.tsx): remove unused imports and functions, and reorganize code for better readability
    The commit removes unused imports and functions, such as `ACCESS`,
    `Rcollect`, `Rfilter`, `Arpend`, and `pathParts`. It also reorganizes
    the code by moving the `dynamicRoutes` declaration and the
    `buildMergedRoutes` function to the top of the file for better
    readability and separation of concerns.

--- apps/stock-center/src/routes.tsx
+++ apps/stock-center/src/routes.tsx
@@ -1,29 +1,13 @@
-import { type FunctionComponent } from "react"
-import { ACCESS } from "./types"
 import { createBrowserRouter, RouteObject } from "react-router-dom"
 import { HeaderRow } from "./components/HeaderRow"
-import { Callback, Login, Protected } from "auth"
-import { collect as Rcollect, filter as Rfilter } from "fp-ts/Record"
-import { Ord } from "fp-ts/string"
-import { append as Arpend } from "fp-ts/Array"
 import { bind, let as Olet, of, Do, match } from "fp-ts/Option"
 import { pipe } from "fp-ts/function"
-
-type PageComponentData = {
-  default: FunctionComponent
-  access: ACCESS
-}
-type mergedRoutesProperties = {
-  publicR: Array<RouteObject>
-  protectedR: Array<RouteObject>
-}
-type dynamicRoutesProperties = Record<string, PageComponentData>
-
-const pathParts = (path: string) =>
-  path
-    .replace(/\/src\/pages|index|\.tsx$/g, "")
-    .replace(/\[\.{3}.+]/, "*")
-    .replace(/\[(.+)\]/, ":$1")
+import {
+  type dynamicRoutesProperties,
+  publicRoutes,
+  protectedRoutes,
+  buildMergedRoutes,
+} from "./functional/routeManager"
 
 const dynamicRoutes: dynamicRoutesProperties = import.meta.glob(
   "/src/pages/**/**/*.tsx",
@@ -31,33 +15,6 @@ const dynamicRoutes: dynamicRoutesProperties = import.meta.glob(
     eager: true,
   },
 )
-
-const mapToRouteObject = (route: string, value: PageComponentData) => {
-  const PageComponent = value.default
-  return { path: pathParts(route), element: <PageComponent /> }
-}
-
-const publicRoutes = (allRoutes: dynamicRoutesProperties): Array<RouteObject> =>
-  pipe(
-    allRoutes,
-    Rfilter((v) => v.access !== ACCESS.protected),
-    Rcollect(Ord)(mapToRouteObject),
-  )
-const protectedRoutes = (
-  allRoutes: dynamicRoutesProperties,
-): Array<RouteObject> =>
-  pipe(
-    allRoutes,
-    Rfilter((v) => v.access === ACCESS.protected),
-    Rcollect(Ord)(mapToRouteObject),
-  )
-const buildMergedRoutes = ({ publicR, protectedR }: mergedRoutesProperties) =>
-  pipe(
-    publicR,
-    Arpend({ children: protectedR, element: <Protected /> } as RouteObject),
-    Arpend({ path: "/callback", element: <Callback /> } as RouteObject),
-    Arpend({ path: "/login", element: <Login /> } as RouteObject),
-  )
 
 const createRouteDefinition = (allRoutes: dynamicRoutesProperties) =>
   pipe(
