b306550fefb182a11de3849789a62f53fe714fd4
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-11 14:09:25 -0500

    test(functional.test.tsx): add test for renderStrainTotal function
    This commit adds a new test case for the `renderStrainTotal` function in the `functional.test.tsx` file. The test verifies that the function correctly renders the leftValue, numItems, and total based on the provided strainItems array.

---
+++ packages/ui-dsc/src/__tests__/functional.test.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+import { describe, expect, test } from "vitest"
+import { render, screen } from "@testing-library/react"
+import { renderStrainTotal } from "../functional"
+import { StrainCartItem } from "../types"
+
+describe("renderStrainTotal", () => {
+  test("should render the leftValue, numItems, and total correctly", () => {
+    const mockStrainItems: Array<StrainCartItem> = [
+      {
+        id: "DBS0236123",
+        label: "aarA-",
+        summary: "mock strain 1",
+        in_stock: true,
+        fee: 15,
+      },
+      {
+        id: "DBS0236123",
+        label: "aarA",
+        summary: "mock strain 2",
+        in_stock: true,
+        fee: 15,
+      },
+    ]
+
+    render(renderStrainTotal({ strainItems: mockStrainItems }))
+
+    expect(screen.getByText("Strains")).toBeInTheDocument()
+    expect(screen.getByText("2 items:")).toBeInTheDocument()
+    expect(screen.getByText("$30.00")).toBeInTheDocument()
+  })
+})
