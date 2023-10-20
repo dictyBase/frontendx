c1d7cb9fc102c0232e4b0f5a224b8b97f7f0f00c
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-20 09:46:07 -0500

    refactor(publication): update import path for various components
    The import paths for various components have been updated to reflect the correct file locations. This ensures that the components are imported correctly and used in the respective pages.

--- apps/publication/src/pages/logout.tsx
+++ apps/publication/src/pages/logout.tsx
@@ -1,5 +1,6 @@
-import Logout from "components/Logout"
+import { Logout } from "../components/Logout"
 
 const LogoutPage = () => <Logout />
 
+// eslint-disable-next-line import/no-default-export
 export default LogoutPage
