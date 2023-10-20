dea095485e9e5a0fb4acae2441369cf20379503d
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-20 09:47:32 -0500

    feat(publication): add PublicationHeader component
    The PublicationHeader component is added to the publication app. It renders a box with the title "dictyBase Literature" using Material-UI Typography component. This component will be used to display the header of the publication section in the application.

---
+++ apps/publication/src/components/PublicationHeader.tsx
@@ -0,0 +1,10 @@
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+
+const PublicationHeader = () => (
+  <Box mt={2} mb={2}>
+    <Typography> dictyBase Literature</Typography>
+  </Box>
+)
+
+export { PublicationHeader }
