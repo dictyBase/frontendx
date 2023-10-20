db5ee50d52569db1bfc0ac862f3fcac3ab9d79e1
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-20 09:56:30 -0500

    refactor(AuthLoader.tsx): simplify AuthLoader component by removing unnecessary return statement and exporting AuthLoader as named export
    The changes simplify the AuthLoader component by removing the unnecessary return statement and exporting AuthLoader as a named export instead of default export. This improves code readability and maintainability.

--- apps/publication/src/components/auth/AuthLoader.tsx
+++ apps/publication/src/components/auth/AuthLoader.tsx
@@ -5,17 +5,15 @@ import { CircularProgress, Typography } from "@material-ui/core"
 /**
  * Loading screen during the login process
  */
-const AuthLoader = () => {
-  return (
-    <Box display="flex" flexDirection="column" justifyContent="center">
-      <Typography variant="h1" align="center">
-        Logging in...
-      </Typography>
-      <Box textAlign="center" mt={3}>
-        <CircularProgress />
-      </Box>
+const AuthLoader = () => (
+  <Box display="flex" flexDirection="column" justifyContent="center">
+    <Typography variant="h1" align="center">
+      Logging in...
+    </Typography>
+    <Box textAlign="center" mt={3}>
+      <CircularProgress />
     </Box>
-  )
-}
+  </Box>
+)
 
-export default AuthLoader
+export { AuthLoader }
