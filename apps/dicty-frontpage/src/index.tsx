65968e920ac39072cdda32861b9c410589f3fa83
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-25 12:15:42 -0500

    refactor(index.tsx): remove unused imports and unnecessary wrapping components
    The imports for CssBaseline and AuthProvider have been removed as they are no longer used in the code. The wrapping components AppProviders and CssBaseline have also been removed as they are unnecessary. This improves the code by removing unnecessary dependencies and simplifying the rendering of the App component.

--- apps/dicty-frontpage/src/index.tsx
+++ apps/dicty-frontpage/src/index.tsx
@@ -1,11 +1,8 @@
 import ReactDOM from "react-dom"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import { AuthProvider } from "./features/Authentication/AuthStore"
 import "fontsource-roboto"
 import "./common/utils/polyfills"
 import "./common/utils/icons" // fontawesome library
 import { App } from "./app/layout/App"
-import AppProviders from "./app/layout/AppProviders"
 
 const main = async () => {
   // Activate MSW
@@ -14,15 +11,7 @@ const main = async () => {
     await worker.start()
   }
 
-  ReactDOM.render(
-    <AuthProvider>
-      <AppProviders>
-        <CssBaseline />
-        <App />
-      </AppProviders>
-    </AuthProvider>,
-    document.querySelector("#root"),
-  )
+  ReactDOM.render(<App />, document.querySelector("#root"))
 }
 
 main()
