e3e78618c9f8ae66ce631ef683c0f3e073f6b52e
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-25 12:14:22 -0500

    feat(callback.tsx): add callback page component for handling sign-in callback
    The callback.tsx file is a new addition to the project. It introduces a Callback component that handles the sign-in callback functionality. It uses the useHandleSignInCallback hook from the "@logto/react" library to handle the sign-in callback and redirects the user to the home page ("/") using the useNavigate hook from the react-router-dom library. The component also renders a Loader component while the sign-in callback is being processed.

---
+++ apps/dicty-frontpage/src/app/pages/callback.tsx
@@ -0,0 +1,14 @@
+import { useHandleSignInCallback } from "@logto/react"
+import { useNavigate } from "react-router-dom"
+import Loader from "../../common/components/Loader"
+
+const Callback = () => {
+  const navigate = useNavigate()
+  const { isLoading } = useHandleSignInCallback(() => {
+    navigate("/")
+  })
+  return isLoading ? <Loader /> : <></>
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Callback
