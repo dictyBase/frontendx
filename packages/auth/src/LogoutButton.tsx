fe3db7b26b7653550a26cd8df886b855126a8b10
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-27 11:18:35 -0500

    refactor(LogoutButton.tsx): Use specific menu item components for each item
    The changes were made to improve code organization and readability. Unused imports and variables were removed, and the UserMenuItem component was replaced with separate ProfileMenuItem and LogoutMenuItem components for better modularity and reusability.

--- packages/auth/src/LogoutButton.tsx
+++ packages/auth/src/LogoutButton.tsx
@@ -1,6 +1,4 @@
-import { useLogto } from "@logto/react"
 import { Box, Menu } from "@material-ui/core"
-import MeetingRoomIcon from "@material-ui/icons/MeetingRoom"
 import { useState, MouseEvent } from "react"
 import { Router as RemixRouter } from "@remix-run/router"
 import {
@@ -11,10 +9,10 @@ import {
   of as Oof,
 } from "fp-ts/Option"
 import { pipe } from "fp-ts/function"
-import { PersonSharp } from "@material-ui/icons"
 import { type UserWithRoles } from "./const"
 import { UserDisplayButton } from "./UserDisplayButton"
-import { UserMenuItem } from "./UserMenuItem"
+import { ProfileMenuItem } from "./ProfileMenuItem"
+import { LogoutMenuItem } from "./LogoutMenuItem"
 
 type LogoutButtonProperties = {
   url: string
@@ -24,7 +22,6 @@ type LogoutButtonProperties = {
 
 const LogoutButton = ({ url, user, clientRouter }: LogoutButtonProperties) => {
   const [menuElement, setMenuElement] = useState<Option<HTMLElement>>(Onone)
-  const { signOut } = useLogto()
   const getMenuElement = () => pipe(menuElement, toNullable) as HTMLElement
   const handleClose = () => setMenuElement(Onone)
   const handleClick = (event: MouseEvent<HTMLElement>) =>
@@ -46,14 +43,10 @@ const LogoutButton = ({ url, user, clientRouter }: LogoutButtonProperties) => {
         open={isOpen}
         onClose={handleClose}
         keepMounted>
-        <UserMenuItem
-          icon={<PersonSharp />}
-          onClick={() => clientRouter.navigate("/user/show", { state: user })}>
-          Profile
-        </UserMenuItem>
-        <UserMenuItem icon={<MeetingRoomIcon />} onClick={() => signOut(url)}>
-          Logout
-        </UserMenuItem>
+        <ProfileMenuItem
+          onClick={() => clientRouter.navigate("/user/show", { state: user })}
+        />
+        <LogoutMenuItem redirectPath={url} />
       </Menu>
     </Box>
   )
