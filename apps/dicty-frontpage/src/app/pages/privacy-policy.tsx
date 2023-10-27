a35f2f4d72c7327f3728ed0b6f4349ff699fdde9
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-27 18:33:50 -0500

    chore: disable eslint rule for prefer-export-from and import/no-default-export
    The eslint rule for prefer-export-from and import/no-default-export has been disabled in both edit.tsx and privacy-policy.tsx files. This was done to suppress the linter warning and allow the default export to be used in these files.

--- apps/dicty-frontpage/src/app/pages/privacy-policy.tsx
+++ apps/dicty-frontpage/src/app/pages/privacy-policy.tsx
@@ -2,6 +2,6 @@
 import About from "../../features/About/About"
 import { ACCESS } from "../routes/types"
 
-// eslint-disable-next-line unicorn/prefer-export-from
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
 export default About
 export const access = ACCESS.public
