20b8c66fbceee0149ca289efcecac19a09f337b4
Kevin Tun <kevin.tun@northwestern.edu>
2023-10-27 17:25:50 -0500

    fix(pages): add eslint-disable-next-line comments
    The eslint-disable-next-line comments have been added to suppress linting rules. This allows for default exports from the pages directory.

--- apps/dicty-frontpage/src/app/pages/[section]/[name]/edit.tsx
+++ apps/dicty-frontpage/src/app/pages/[section]/[name]/edit.tsx
@@ -1,6 +1,6 @@
 import EditInfoPage from "../../../../features/EditablePages/EditInfoPage"
 import { ACCESS } from "../../../routes/types"
 
-// eslint-disable-next-line unicorn/prefer-export-from
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
 export default EditInfoPage
 export const access = ACCESS.private
--- apps/dicty-frontpage/src/app/pages/login.tsx
+++ apps/dicty-frontpage/src/app/pages/login.tsx
@@ -1,6 +1,6 @@
 import Login from "../../features/Authentication/Login"
 import { ACCESS } from "../routes/types"
 
-// eslint-disable-next-line unicorn/prefer-export-from
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
 export default Login
 export const access = ACCESS.public
