fe8ee2d7fe7173c0d7e60cbb171db391d5432e1a
Siddhartha Basu <biosidd@gmail.com>
2023-12-14 17:56:08 -0600

    docs(HeaderWithAuth.tsx): add detailed comments for better code understanding
    The changes include adding detailed comments to the HeaderWithAuth.tsx
    file. This is done to improve code readability and maintainability. The
    comments provide a clear understanding of the types, functions, and
    components used in the file. They explain the purpose of each code
    block, making it easier for other developers to understand the code.

--- packages/auth/src/HeaderWithAuth.tsx
+++ packages/auth/src/HeaderWithAuth.tsx
@@ -8,10 +8,24 @@ import { LoginButton } from "./LoginButton"
 import { LogoutButton } from "./LogoutButton"
 import { callbackPath, homePath } from "./const"
 
+/**
+ * @description Represents the properties required for the HeaderWithAuth component.
+ * @typedef {object} HeaderWithAuthProperties
+ * @property {RemixRouter} clientRouter - The client router instance used for navigation.
+ */
 type HeaderWithAuthProperties = {
   clientRouter: RemixRouter
 }
 
+/**
+ * Represents the properties for the `logtoHook` function in the `HeaderWithAuth.tsx` file.
+ * @typedef {Object} logtoHookProperties
+ * @property {HeaderWithAuthProperties} - The properties inherited from the `HeaderWithAuthProperties` type.
+ * @property {Error | undefined} [error] - An optional error object.
+ * @property {boolean} isAuthenticated - Indicates whether the user is authenticated.
+ * @property {boolean} isLoading - Indicates whether the page is currently loading.
+ * @property {UserWithRoles | undefined} [user] - An optional object representing a user with roles.
+ */
 type logtoHookProperties = HeaderWithAuthProperties & {
   error: Error | undefined
   isAuthenticated: boolean
@@ -19,6 +33,12 @@ type logtoHookProperties = HeaderWithAuthProperties & {
   user: UserWithRoles | undefined
 }
 
+/**
+ * This function takes in a logtoHookProperties object and returns a JSX element representing a Header component with a login/logout button.
+ * If the user is authenticated and exists, it renders a Header component with a LogoutButton component.
+ * If the user is not authenticated or does not exist, it renders a Header component with a LoginButton component.
+ *
+ */
 const conditonalHandler = (logtoCase: logtoHookProperties) =>
   match(logtoCase)
     .with(
@@ -37,19 +57,32 @@ const conditonalHandler = (logtoCase: logtoHookProperties) =>
     )
     .otherwise(() => <Header LoginOut={<LoginButton url={callbackPath} />} />)
 
+/**
+ * HeaderWithAuth is a React component that renders a header with authentication information.
+ */
 const HeaderWithAuth = ({ clientRouter }: HeaderWithAuthProperties) => {
+  // Destructure properties from the useLogto() hook
   const { isAuthenticated, isLoading, error, fetchUserInfo, getAccessToken } =
     useLogto()
+
+  // Define state for the authenticated user
   const [user, setAuthUser] = useState<UserWithRoles>()
+
+  // Fetch user information upon component mount or when dependencies change
   useEffect(() => {
     const handleUserInformation = async () => {
       if (isAuthenticated) {
+        // Fetch user info and cast result to UserWithRoles type
         const authUser = (await fetchUserInfo()) as UserWithRoles
         setAuthUser(authUser)
       }
     }
+
+    // Call the handleUserInformation function
     handleUserInformation()
   }, [fetchUserInfo, isAuthenticated, getAccessToken])
+
+  // Return the result of the conditonalHandler function
   return conditonalHandler({
     isAuthenticated,
     isLoading,
