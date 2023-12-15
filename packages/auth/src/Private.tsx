49cda66078d24e76f39cd1423fb8071182325927
Siddhartha Basu <biosidd@gmail.com>
2023-12-14 18:05:06 -0600

    docs(auth): add comprehensive comments to components for better understanding
    The changes were made to improve the readability and maintainability of
    the code. Detailed comments were added to the components in the auth
    package to provide a clear understanding of their functionality, props,
    and usage.

--- packages/auth/src/Private.tsx
+++ packages/auth/src/Private.tsx
@@ -14,6 +14,20 @@ type ConditionalRouteProperteies = {
 
 type PrivateProperties = { roles: Array<string> }
 
+/**
+ * This file contains the implementation of the `conditionalRouting` function.
+ * This function takes in a single parameter `props` of type `ConditionalRouteProperteies`.
+ * It uses the `match` function to perform pattern matching on the `props` object.
+ *
+ * The `match` function is a utility function that allows for conditional routing based on the properties of the `props` object.
+ * It takes in the `props` object and uses a series of `.when` methods to define different cases based on the properties of the `props` object.
+ *
+ * The first `.when` case checks if the `isLoading` property of the `props` object is truthy. If it is, it renders a `LoadingDisplay` component with `rows` prop set to 6.
+ *
+ * The second `.when` case checks if the `checkAuthorization` property of the `props` object is truthy. If it is, it renders an `Outlet` component.
+ *
+ * The `.otherwise` method is called when none of the previous cases match. In this case, it renders a `Navigate` component with a `to` prop set to "/".
+ */
 const conditionalRouting = (props: ConditionalRouteProperteies) =>
   match(props)
     .when(
@@ -26,6 +40,27 @@ const conditionalRouting = (props: ConditionalRouteProperteies) =>
     )
     .otherwise(() => <Navigate to="/" />)
 
+/**
+ * The `Private` component is a functional component that renders a private route based on user roles.
+ * It takes an object `roles` as a prop, which is of type `PrivateProperties`.
+ * 
+ * @example
+ * // Single role
+ * <Private roles={['admin']}>
+ *   <AdminDashboard />
+ * </Private>
+ *
+ * // Multiple roles
+ * <Private roles={['admin', 'manager']}>
+ *   <AdminDashboard />
+ * </Private>
+ *
+ * // Usage
+ * const roles: PrivateProperties = { roles: ['admin', 'manager'] };
+ * <Private roles={roles}>
+ *   <AdminDashboard />
+ * </Private>
+ */
 const Private = ({ roles }: PrivateProperties) => {
   const { fetchUserInfo } = useLogto()
   const [checkAuthorization, setAuthorization] = useState<boolean>(false)
