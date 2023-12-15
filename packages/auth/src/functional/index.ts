db1b0191b03423801914ff1d0bf411c4cdb91dd1
Siddhartha Basu <biosidd@gmail.com>
2023-12-15 15:03:52 -0600

    feat(auth): add matchRoles function to compare user roles
    The matchRoles function was added to compare the roles given to a user
    with the expected roles, enhancing the role validation process. Code
    readability was improved by removing unnecessary semicolons and
    adjusting line breaks for better visual separation of code blocks.

--- packages/auth/src/functional/index.ts
+++ packages/auth/src/functional/index.ts
@@ -2,30 +2,46 @@ import {
   split as splitString,
   slice as sliceString,
   toUpperCase,
+  Eq,
 } from "fp-ts/string"
-import { map as Amap } from "fp-ts/Array"
+import { map as Amap, getEq, elem, every } from "fp-ts/Array"
 import { ReadonlyNonEmptyArray, head, last } from "fp-ts/ReadonlyNonEmptyArray"
 import { pipe } from "fp-ts/function"
 
+/**
+ * Matches the given array of roles against the expected array of roles.
+ */
+const matchRoles = (given: Array<string>, expected: Array<string>) => {
+  switch (true) {
+    case expected.length > given.length:
+      return false
+    case expected.length == given.length:
+      return getEq(Eq).equals(expected, given)
+  }
+  return pipe(
+    expected,
+    every((v) => pipe(expected, elem(Eq)(v))),
+  )
+}
 
 const firstLast = (nameArray: ReadonlyNonEmptyArray<string>) => [
   head(nameArray), // Returns the first element of the nameArray
   last(nameArray), // Returns the last element of the nameArray
-];
+]
 
 const upperFirst = (fullname: string) =>
   pipe(
     fullname, // The input fullname string
     sliceString(0, 1), // Takes the first character of the string
-    toUpperCase // Converts the first character to uppercase
-  );
+    toUpperCase, // Converts the first character to uppercase
+  )
 
 const nameToUpperInitial = (fullName: string) =>
   pipe(
     fullName, // The input fullName string
     splitString(" "), // Splits the string into an array of words
     firstLast, // Returns an array containing the first and last words
-    Amap(upperFirst) // Applies the upperFirst function to each word in the array
-  ).join(""); // Joins the array of uppercased initials into a single string
+    Amap(upperFirst), // Applies the upperFirst function to each word in the array
+  ).join("") // Joins the array of uppercased initials into a single string
 
-export { nameToUpperInitial }
+export { nameToUpperInitial, matchRoles }
