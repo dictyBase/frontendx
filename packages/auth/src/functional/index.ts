8f09c4842ffb536868fee2bd8ec27d9fa585673f
Siddhartha Basu <biosidd@gmail.com>
2023-12-14 18:15:47 -0600

    docs(Callback.tsx): add comments to Callback component
    The changes were made to improve code readability and maintainability.
    The comments provide a clear understanding of the purpose and
    functionality of the components, functions, and types. The variable name
    in functional/index.ts was corrected from 'nameArry' to 'nameArray' to
    avoid confusion and improve code clarity.

--- packages/auth/src/functional/index.ts
+++ packages/auth/src/functional/index.ts
@@ -7,14 +7,25 @@ import { map as Amap } from "fp-ts/Array"
 import { ReadonlyNonEmptyArray, head, last } from "fp-ts/ReadonlyNonEmptyArray"
 import { pipe } from "fp-ts/function"
 
-const firstLast = (nameArry: ReadonlyNonEmptyArray<string>) => [
-  head(nameArry),
-  last(nameArry),
-]
+
+const firstLast = (nameArray: ReadonlyNonEmptyArray<string>) => [
+  head(nameArray), // Returns the first element of the nameArray
+  last(nameArray), // Returns the last element of the nameArray
+];
+
 const upperFirst = (fullname: string) =>
-  pipe(fullname, sliceString(0, 1), toUpperCase)
+  pipe(
+    fullname, // The input fullname string
+    sliceString(0, 1), // Takes the first character of the string
+    toUpperCase // Converts the first character to uppercase
+  );
 
 const nameToUpperInitial = (fullName: string) =>
-  pipe(fullName, splitString(" "), firstLast, Amap(upperFirst)).join("")
+  pipe(
+    fullName, // The input fullName string
+    splitString(" "), // Splits the string into an array of words
+    firstLast, // Returns an array containing the first and last words
+    Amap(upperFirst) // Applies the upperFirst function to each word in the array
+  ).join(""); // Joins the array of uppercased initials into a single string
 
 export { nameToUpperInitial }
