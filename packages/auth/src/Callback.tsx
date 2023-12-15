dc8b24e5cc5f9a3909aeffcd3fc694016c93fbdf
Siddhartha Basu <biosidd@gmail.com>
2023-12-14 18:15:47 -0600

    docs(Callback.tsx): add comments to Callback component
    The changes were made to improve code readability and maintainability.
    The comments provide a clear understanding of the purpose and
    functionality of the components, functions, and types. The variable name
    in functional/index.ts was corrected from 'nameArry' to 'nameArray' to
    avoid confusion and improve code clarity.

--- packages/auth/src/Callback.tsx
+++ packages/auth/src/Callback.tsx
@@ -2,6 +2,12 @@ import { useHandleSignInCallback } from "@logto/react"
 import { useNavigate } from "react-router-dom"
 import { LoadingDisplay } from "./LoadingDisplay"
 
+/**
+ * Callback component
+ *
+ * This component is responsible for handling the callback after a successful sign-in.
+ * It renders a loading display until the sign-in process is completed.
+ */
 const Callback = () => {
   const navigate = useNavigate()
   const { isLoading } = useHandleSignInCallback(() => {
--- packages/auth/src/functional/routeManager.tsx
+++ packages/auth/src/functional/routeManager.tsx
@@ -15,42 +15,86 @@ enum ACCESS {
   private,
 }
 
+/**
+ * Represents the data structure for a page component.
+ */
 type PageComponentData = {
+  /**
+   * The default function component.
+   */
   default: FunctionComponent
+  /**
+   * The access level required to access the component.
+   */
   access: ACCESS
+  /**
+   * An optional array of roles that are allowed to access the component.
+   */
   roles?: Array<string>
 }
+
+/**
+ * Represents the properties of merged routes.
+ */
 type mergedRoutesProperties = {
+  /**
+   * An array of public routes.
+   */
   publicR: Array<RouteObject>
+
+  /**
+   * An array of protected routes.
+   */
   protectedR: Array<RouteObject>
+
+  /**
+   * An array of private routes.
+   */
   privateR: Array<RouteObject>
 }
+
 type dynamicRoutesProperties = Record<string, PageComponentData>
 
+/**
+ * This function takes a path and returns the modified path parts.
+ * It replaces specific patterns in the path string with desired values.
+ */
 const pathParts = (path: string) =>
   path
     .replaceAll(/\/src\/pages|index|\.tsx$/g, "")
     .replace(/\[\.{3}.+]/, "*")
     .replace(/\[(.+)]/, ":$1")
 
+/**
+ * Maps a route and its corresponding page component data to a route object.
+ */
 const mapToRouteObject = (route: string, value: PageComponentData) => {
   const PageComponent = value.default
   const { roles } = value
   if (roles) {
+    // If roles exist, return a route object with a private element and a child route.
     return {
-      element: <Private roles={roles} />,
-      children: [{ path: pathParts(route), element: <PageComponent /> }],
+      element: <Private roles={roles} />, // Private component that requires specific roles.
+      children: [{ path: pathParts(route), element: <PageComponent /> }], // Child route with the page component.
     }
   }
+  // If no roles, return a route object with just the page component.
   return { path: pathParts(route), element: <PageComponent /> }
 }
 
+/**
+ * Filters out the routes that are not protected and returns an array of RouteObject.
+ */
 const publicRoutes = (allRoutes: dynamicRoutesProperties): Array<RouteObject> =>
   pipe(
     allRoutes,
     Rfilter((v) => v.access !== ACCESS.protected),
     Rcollect(Ord)(mapToRouteObject),
   )
+
+/**
+ * Filters and collects private routes from the given array of dynamic routes.
+ */
 const privateRoutes = (
   allRoutes: dynamicRoutesProperties,
 ): Array<RouteObject> =>
@@ -59,6 +103,11 @@ const privateRoutes = (
     Rfilter((v) => v.access === ACCESS.private),
     Rcollect(Ord)(mapToRouteObject),
   )
+
+/**
+ * This function takes an array of dynamicRoutesProperties and returns an array of RouteObject.
+ * It filters out the routes with access set to ACCESS.protected and transforms them into RouteObject.
+ */
 const protectedRoutes = (
   allRoutes: dynamicRoutesProperties,
 ): Array<RouteObject> =>
@@ -67,6 +116,10 @@ const protectedRoutes = (
     Rfilter((v) => v.access === ACCESS.protected),
     Rcollect(Ord)(mapToRouteObject),
   )
+
+/**
+ * Builds merged routes by combining public, protected, and private routes.
+ */
 const buildMergedRoutes = ({
   publicR,
   protectedR,
