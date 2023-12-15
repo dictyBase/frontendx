541d85c3a1d1354cf442de86cff1059696c72196
Siddhartha Basu <biosidd@gmail.com>
2023-12-15 14:25:23 -0600

    feat(auth): add UnAuthorized component and sad-dicty asset
    The UnAuthorized component is added to handle unauthorized access
    scenarios. It displays a message to the user indicating they are not
    authorized to access the page. The component also includes a 'Back to
    DSC Homepage' button to redirect users back to the homepage. The
    sad-dicty asset is a visual element to enhance the user experience
    during these scenarios.

---
+++ packages/auth/src/UnAuthorized.tsx
@@ -0,0 +1,62 @@
+import { makeStyles } from "@material-ui/core/styles"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import ErrorIcon from "@mui/icons-material/Error"
+import sadDicty from "./assets/sad-dicty.png";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(2),
+    "&:hover": {
+      color: theme.palette.getContrastText(theme.palette.primary.main),
+    },
+  },
+  container: {
+    backgroundColor: "#eff8fb",
+    paddingTop: theme.spacing(6),
+    paddingBottom: theme.spacing(6),
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    borderRadius: theme.spacing(1),
+  },
+  text: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+}))
+
+/**
+ * UnAuthorized is the display component when there is an error.
+ */
+const UnAuthorized = () => {
+  const classes = useStyles()
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      textAlign="center">
+      <Box className={classes.container}>
+        <img src={sadDicty} alt="Sad Dicty -- HTTP Error" />
+        <Typography className={classes.text} variant="h1">
+          <ErrorIcon /> UnAuthorized
+        </Typography>
+        <Typography variant="body1" className={classes.text}>
+          You are not authorized to access this page
+        </Typography>
+        <Button
+          classes={{ root: classes.root }}
+          href="/"
+          size="medium"
+          variant="contained"
+          color="primary">
+          Back to DSC Homepage
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export { UnAuthorized }
