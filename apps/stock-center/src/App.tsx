f4fd26f15d0448ff590f3d219ec8e665deee8137
Siddhartha Basu <biosidd@gmail.com>
2023-12-16 07:35:03 -0600

    feat(App.tsx): extend LogtoConfig scopes and add resources for enhanced user data access
    The LogtoConfig scopes have been extended to include UserScope.Profile,
    UserScope.Email, UserScope.Phone, UserScope.CustomData,
    UserScope.Identities, and "roles". This allows for more comprehensive
    user data access. Additionally, resources have been added to the
    LogtoConfig, which are fetched from environment variables.

--- apps/stock-center/src/App.tsx
+++ apps/stock-center/src/App.tsx
@@ -6,14 +6,25 @@ import {
 } from "@dictybase/hook-dsc"
 import { ApolloProvider } from "@apollo/client"
 import "@fontsource/roboto"
-import { LogtoProvider, LogtoConfig } from "@logto/react"
+import { LogtoProvider, LogtoConfig, UserScope } from "@logto/react"
 import { ThemeProvider } from "./ThemeProvider"
 import { DscApp } from "./components/DscApp"
 
 const logtoConfig: LogtoConfig = {
   endpoint: import.meta.env.VITE_LOGTO_ENDPOINT,
   appId: import.meta.env.VITE_LOGTO_APPID,
-  scopes: ["openid", "profile", "email", "phone", "roles"],
+  scopes: [
+    UserScope.Profile,
+    UserScope.Email,
+    UserScope.Phone,
+    UserScope.CustomData,
+    UserScope.Identities,
+    "roles",
+  ],
+  resources: [
+    import.meta.env.VITE_APP_LOGTO_API_FIRST_RESOURCE,
+    import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+  ],
 }
 
 export const App = () => {
