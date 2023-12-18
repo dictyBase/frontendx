a5594f4713fec1acd53fbf4bb4a0a351aa546b2b
Siddhartha Basu <biosidd@gmail.com>
2023-12-18 13:22:39 -0600

    feat(information): add new page for adding information
    A new page has been added to the information section of the stock-center
    app. This page includes an editable editor component, allowing users to
    add new information. The page is only accessible to users with private
    access and the roles of 'user-user' and 'content-admin'. This feature
    enhances the app's functionality by providing a dedicated space for
    users to contribute new information.

--- apps/stock-center/src/pages/information/[name]/new.tsx
+++ apps/stock-center/src/pages/information/[name]/new.tsx
@@ -3,13 +3,13 @@ import { Editor } from "editor"
 import { ACCESS } from "auth"
 // import { InfoPageContainer } from "frontpage-components"
 
-const AddPage = () => (
+const Add = () => (
   <Container>
     <Editor editable />
   </Container>
 )
 
 // eslint-disable-next-line import/no-default-export
-export default AddPage
+export default Add
 export const access = ACCESS.private
 export const roles = ["user-user", "content-admin"]
