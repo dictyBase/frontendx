3fa9c2e7f3fc1741e1275dc812a66c2accdc282a
Siddhartha Basu <biosidd@gmail.com>
2023-12-19 15:58:54 -0600

    feat(useAuthorization.ts): add comments to useAuthorization hook for better code readability
    The useAuthorization hook now includes detailed comments explaining each
    step of the authorization process. This will make the code easier to
    understand and maintain, especially for new developers or contributors
    who are not familiar with the codebase.

--- packages/auth/src/useAuthorization.ts
+++ packages/auth/src/useAuthorization.ts
@@ -5,20 +5,35 @@ import { type UserWithRoles } from "./const"
 
 type useAuthorizationProperties = { entries: Array<string> }
 
+/**
+ * A custom hook that checks the authorization of a user based on their roles.
+ *
+ */
 const useAuthorization = ({ entries }: useAuthorizationProperties) => {
+  // Fetch the user information using the useLogto hook
   const { fetchUserInfo } = useLogto()
+
+  // Initialize state variables
   const [isAuthorized, setAuthorization] = useState<boolean>(false)
   const [isLoading, setLoading] = useState<boolean>(true)
+
   useEffect(() => {
+    // Function to check user authorization
     const authCheck = async (entries: Array<string>) => {
+      // Fetch the user information and cast it to UserWithRoles type
       const authUser = (await fetchUserInfo()) as UserWithRoles
       if (authUser) {
+        // Set the authorization based on matching the user's roles with the entries
         setAuthorization(matchEntries(authUser.roles, entries))
       }
+      // Set the loading state to false once the authorization check is completed
       setLoading(false)
     }
+    // Perform the authorization check when the entries array changes
     authCheck(entries)
+    // Only re-run the effect when the entries array changes
   }, [entries])
+  // Return the authorization status and loading state
   return { isAuthorized, isLoading }
 }
 
