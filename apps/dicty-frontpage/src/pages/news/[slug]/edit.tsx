38de5ebd8449044fb08799fbfdecf1fccf180f74
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-19 12:50:05 -0600

    chore(edit.tsx): comment out unused imports and export statement
    Components that use the news component are broken do to missing query hooks in dicty-graphql-schema. This commit temporarily comments out files that use news components

--- apps/dicty-frontpage/src/pages/news/[slug]/edit.tsx
+++ apps/dicty-frontpage/src/pages/news/[slug]/edit.tsx
@@ -1,7 +1,7 @@
-import { UpdateNews } from "news-component"
-import { NewsLayoutWrapper } from "news-component/src/NewsLayout"
-import { ACCESS } from "../../../routes/types"
+// import { UpdateNews } from "news-component"
+// import { NewsLayoutWrapper } from "news-component/src/NewsLayout"
+// import { ACCESS } from "../../../routes/types"
 
-// eslint-disable-next-line import/no-default-export
+// // eslint-disable-next-line import/no-default-export
 // export default NewsLayoutWrapper(UpdateNews)
 // export const access = ACCESS.private
