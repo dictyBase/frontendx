8f520e9c6908c5a64bfbc0046cf5ae3f5e61986a
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-20 14:44:29 -0600

    feat(frontpage): add index.tsx file to handle the frontpage route and export Front component
    The index.tsx file is added to the pages directory in the dicty-frontpage app. This file imports the ACCESS constant from the "auth" module and the Front component from the "../features/Frontpage/Front" module. It exports the Front component as the default export and also exports the ACCESS.public constant. This file is responsible for handling the frontpage route and exporting the necessary components and constants.

---
+++ apps/dicty-frontpage/src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "auth"
+import { Front } from "../features/Frontpage/Front"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default Front
+export const access = ACCESS.public
