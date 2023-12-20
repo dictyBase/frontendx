b4d54c0071d07c4f178d3888cf39532ae7d606b4
Siddhartha Basu <biosidd@gmail.com>
2023-12-20 14:36:02 -0600

    refactor(index.tsx): simplify and modularize Homepage component for better readability and maintainability
    The Homepage component has been refactored to improve readability and
    maintainability. The code has been simplified by breaking down the
    component into smaller, more manageable parts. This includes replacing
    the previous Grid and Typography components with more descriptive
    components such as Title, Heading, IntroEditor, Slide, CatalogLinks, and
    FileLinks. This makes the code easier to understand and modify in the
    future.

--- apps/stock-center/src/pages/index.tsx
+++ apps/stock-center/src/pages/index.tsx
@@ -1,84 +1,34 @@
-import { Helmet } from "react-helmet"
 import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
+import { useStyles } from "@dictybase/ui-dsc"
 import {
-  Slideshow,
-  useStyles,
-  EditablePanel,
-  EditorHolder,
-  HomepageColumn,
+  Title,
+  Heading,
+  IntroEditor,
+  Slide,
+  CatalogLinks,
+  FileLinks,
 } from "@dictybase/ui-dsc"
-// import { Availability } from "./Availability"
-// import OtherMaterials from "./OtherMaterials"
-// import EditablePanel from "./EditablePanel"
-// import StandardOperatingProcedures from "./StandardOperatingProcedures"
-// import { useAuthStore } from "features/Authentication/AuthStore"
-import {
-  downloadLinks,
-  // infoLinks,
-  materialsLinks,
-  // miscLinks,
-} from "../linkLists"
-// import EditorHolder from "./EditorHolder"
-
-const metaDesc =
-  "The Dicty Stock Center is a rapidly growing central repository for Dictyostelium discoideum strains and those of related species, plasmids, commonly used food bacteria, and other materials such as antibodies."
 
 /**
  * Homepage is the main homepage component for DSC.
  */
-
 const Homepage = () => {
   const classes = useStyles({})
   return (
-    <div>
-      <Helmet>
-        <title>Dicty Stock Center</title>
-        <meta name="description" content={metaDesc} />
-      </Helmet>
+    <>
+      <Title />
       <Grid container justifyContent="space-between" spacing={3}>
-        <Grid item className={classes.header}>
-          <Typography variant="h1">
-            Welcome to Dicty Stock Center (DSC)
-          </Typography>
-        </Grid>
+        <Heading />
         <Grid item xs={12} className={classes.intro}>
-          <EditorHolder minHeight="140px">
-            <EditablePanel slug="dsc-intro" skeletonCount={5} />
-          </EditorHolder>
+          <IntroEditor />
         </Grid>
         <Grid container item xs={12} spacing={4} justifyContent="flex-start">
-          <Grid item xs={4}>
-            <Slideshow />
-          </Grid>
-          <Grid container item xs={4} direction="column">
-            <HomepageColumn
-              title="Catalogs/Materials"
-              entries={materialsLinks}
-            />
-          </Grid>
-          <Grid container item xs={4} direction="column">
-            <HomepageColumn title="Downloads" entries={downloadLinks} />
-          </Grid>
+          <Slide />
+          <CatalogLinks />
+          <FileLinks />
         </Grid>
       </Grid>
-    </div>
-    /* <HomepageColumn
-          components={[
-            <EditorHolder minHeight="300px">
-              <EditablePanel slug="dsc-about" skeletonCount={10} />
-            </EditorHolder>,
-            <LinkList list={miscLinks} bgColor="blue" />,
-          ]}
-        />
-        <HomepageColumn
-          components={[
-            <LinkList list={infoLinks} bgColor="blue" />,
-            <Availability />,
-            <OtherMaterials />,
-            <StandardOperatingProcedures />,
-          ]}
-        /> */
+    </>
   )
 }
 
