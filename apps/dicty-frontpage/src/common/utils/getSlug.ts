b2104a4d26917139f2c54eee633e39393381e247
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-21 17:47:06 -0600

    feat(getSlug.ts): add utility function getSlug to generate slugs for content page routes
    The getSlug utility function is added to generate slugs for content page routes. It takes in route parameters and returns a string representing the slug of the URL. This function is intended for use in general pages that deal with Content data under route paths `/:section/:name` or `/:section/:name/:subname`. It uses pattern matching to extract the appropriate values from the route parameters and returns an empty string if no matching values are found.

---
+++ apps/dicty-frontpage/src/common/utils/getSlug.ts
@@ -0,0 +1,20 @@
+import { match, P } from "ts-pattern"
+
+type ContentPageRouteParameters = {
+  name?: string
+  subname?: string
+}
+
+/**
+ * Intended for use in general pages that deal with Content data under route path
+ * `/:section/:name` or `/:section/:name/:subname`
+ *
+ * The returned string represents the slug of the url
+ */
+const getSlug = (routeParameters: ContentPageRouteParameters): string =>
+  match(routeParameters)
+    .with({ subname: P.select(P.string) }, (subname) => subname)
+    .with({ name: P.select(P.string) }, (name) => name)
+    .otherwise(() => "")
+
+export { getSlug }
