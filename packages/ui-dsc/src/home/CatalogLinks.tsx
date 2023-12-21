9544bf6f5a16f922cbc245fee878e12b80d9e529
Siddhartha Basu <biosidd@gmail.com>
2023-12-21 17:50:21 -0600

    feat(CatalogLinks.tsx): add authorization check for displaying additional material links
    The CatalogLinks component now accepts an 'isAuthorized' prop to
    determine which additional material links to display. If the user is
    authorized, additionalMaterialAuth links are displayed, otherwise, only
    additionalMaterial links are shown.

--- packages/ui-dsc/src/home/CatalogLinks.tsx
+++ packages/ui-dsc/src/home/CatalogLinks.tsx
@@ -1,11 +1,30 @@
 import Grid from "@material-ui/core/Grid"
 import { HomepageColumn } from "./HomepageColumn"
-import { materialsLinks } from "../linkLists"
+import {
+  materialsLinks,
+  additionalMaterial,
+  additionalMaterialAuth,
+} from "../linkLists"
+import { fromNullable, match } from "fp-ts/Option"
+import { pipe } from "fp-ts/function"
 
-const CatalogLinks = () => (
-  <Grid container item xs={4} direction="column">
-    <HomepageColumn title="Catalogs/Materials" entries={materialsLinks} />
-  </Grid>
-)
+type CatalogLinksProperties = {
+  isAuthorized?: boolean
+}
+
+const CatalogLinks = ({ isAuthorized }: CatalogLinksProperties) => {
+  const allLinks = pipe(
+    fromNullable(isAuthorized),
+    match(
+      () => Array.of(...materialsLinks, additionalMaterial),
+      () => Array.of(...materialsLinks, additionalMaterialAuth),
+    ),
+  )
+  return (
+    <Grid container item xs={4} direction="column">
+      <HomepageColumn title="Catalogs/Materials" entries={allLinks} />
+    </Grid>
+  )
+}
 
 export { CatalogLinks }
