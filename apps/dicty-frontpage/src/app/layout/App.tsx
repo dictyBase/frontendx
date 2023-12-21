ec271d3aca39364b89441c2581d5fac96a78df30
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-21 17:47:56 -0600

    feat(App.tsx): add additional scopes and resources to LogtoConfig for enhanced user authentication and authorization
    The LogtoConfig in App.tsx has been updated to include additional scopes such as UserScope.Profile, UserScope.Email, UserScope.Phone, UserScope.CustomData, and UserScope.Identities. This allows for more comprehensive user authentication and authorization. Additionally, two new resources have been added to the LogtoConfig to support the VITE_APP_LOGTO_API_FIRST_RESOURCE and VITE_APP_LOGTO_API_SECOND_RESOURCE. These changes enhance the functionality and security of the application.

--- apps/dicty-frontpage/src/app/layout/App.tsx
+++ apps/dicty-frontpage/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { LogtoProvider, LogtoConfig } from "@logto/react"
+import { LogtoProvider, LogtoConfig, UserScope } from "@logto/react"
 import { CssBaseline } from "@material-ui/core"
 import { AppProviders } from "./AppProviders"
 import { FrontPageApp } from "./FrontPageApp"
@@ -6,7 +6,18 @@ import { FrontPageApp } from "./FrontPageApp"
 const logtoConfig: LogtoConfig = {
   endpoint: import.meta.env.VITE_LOGTO_ENDPOINT,
   appId: import.meta.env.VITE_LOGTO_APPID,
-  scopes: ["openid", "profile", "email", "phone", "roles"],
+  scopes: [
+    UserScope.Profile,
+    UserScope.Email,
+    UserScope.Phone,
+    UserScope.CustomData,
+    UserScope.Identities,
+    "roles",
+  ],
+  resources: [
+    import.meta.env.VITE_APP_LOGTO_API_FIRST_RESOURCE,
+    import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+  ],
 }
 
 const App = () => (
