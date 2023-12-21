08b8910498711266203216014c5b05439948c2af
Siddhartha Basu <biosidd@gmail.com>
2023-12-21 17:02:01 -0600

    feat(IntroEditor.tsx): add isEditable prop to IntroEditor component to control editability
    The IntroEditor component now accepts an optional isEditable prop. This
    allows the editability of the Editor component to be controlled
    externally, providing more flexibility in how the component is used. The
    isEditable prop is optional and defaults to false if not provided,
    ensuring backward compatibility.

--- packages/ui-dsc/src/home/IntroEditor.tsx
+++ packages/ui-dsc/src/home/IntroEditor.tsx
@@ -1,5 +1,7 @@
 import Container from "@material-ui/core/Container"
 import { Editor } from "editor"
+import { of, match } from "fp-ts/Option"
+import { pipe } from "fp-ts/function"
 
 const sampleText =
   "To make taffy, the process usually begins by combining sugar, corn syrup, butter, and water in a large saucepan over medium heat. The mixture is stirred until the sugar dissolves completely, and then the heat is increased to bring it to a boil. Once boiling, a candy thermometer is used to monitor the temperature, usually aiming for around 250°F (121°C). At this stage, the saucepan is removed from the heat, and any desired flavorings and food coloring are added. The mixture is then poured onto a greased baking sheet or marble slab. As it cools slightly, the taffy is pulled and folded repeatedly to aerate and stretch it, resulting in a chewy and taffy-like consistency. Once the taffy has cooled enough to handle, it is often cut into small pieces and wrapped individually in wax paper to maintain its freshness. This process requires precision in temperature control and skilled pulling techniques to create the desired texture of the taffy."
@@ -43,16 +45,29 @@ const temporaryContent = {
   },
 }
 
-const IntroEditor = () => (
-  <Container>
-    <Editor
-      content={{
-        storageKey: "",
-        editorState: JSON.stringify(temporaryContent),
-      }}
-      editable={false}
-    />
-  </Container>
-)
+type IntroEditorProperties = {
+  isEditable?: boolean
+}
+
+const IntroEditor = ({ isEditable }: IntroEditorProperties) => {
+  const optIsEditable = pipe(
+    of(isEditable),
+    match(
+      () => false,
+      () => true,
+    ),
+  )
+  return (
+    <Container>
+      <Editor
+        content={{
+          storageKey: "",
+          editorState: JSON.stringify(temporaryContent),
+        }}
+        editable={optIsEditable}
+      />
+    </Container>
+  )
+}
 
 export { IntroEditor }
