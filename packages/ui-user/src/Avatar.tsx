935dc11c24a62d55ff60fc0859bc31c6de32f30b
Siddhartha Basu <biosidd@gmail.com>
2023-12-22 11:40:18 -0600

    feat(ui-user): add Avatar component to ui-user package
    A new Avatar component has been added to the ui-user package. This
    component will be used to display user avatars across the application.
    The Avatar component uses the MoodSharp icon from Material UI and is
    wrapped in an IconButton. The component is also exported in the
    package's index file for easy import elsewhere in the application.

---
+++ packages/ui-user/src/Avatar.tsx
@@ -0,0 +1,10 @@
+import { MoodSharp } from "@material-ui/icons"
+import IconButton from "@material-ui/core/IconButton"
+
+const Avatar = () => (
+  <IconButton size="medium">
+    <MoodSharp style={{ fontSize: 70 }} />
+  </IconButton>
+)
+
+export { Avatar }
