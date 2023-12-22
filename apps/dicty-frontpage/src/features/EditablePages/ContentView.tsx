2c493f1acf0b9780435e4dce52a9f0f42e4a0ac6
Kevin Tun <kevin.tun@northwestern.edu>
2023-12-21 18:28:07 -0600

    refactor(ContentView.tsx): wrap Editor component in a Container component for better styling and layout
    The Editor component is now wrapped in a Container component from the Material-UI library. This change improves the styling and layout of the ContentView component, providing a more consistent and visually appealing user interface.

--- apps/dicty-frontpage/src/features/EditablePages/ContentView.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/ContentView.tsx
@@ -1,15 +1,18 @@
-import { Editor } from "editor"
+import { Container } from "@material-ui/core"
 import { type ContentBySlugQuery } from "dicty-graphql-schema"
+import { Editor } from "editor"
 
 type ContentViewProperties = {
   data: NonNullable<ContentBySlugQuery["contentBySlug"]>
 }
 
 const ContentView = ({ data }: ContentViewProperties) => (
-  <Editor
-    editable={false}
-    content={{ storageKey: data.slug, editorState: data.content }}
-  />
+  <Container>
+    <Editor
+      editable={false}
+      content={{ storageKey: data.slug, editorState: data.content }}
+    />
+  </Container>
 )
 
 export { ContentView }
