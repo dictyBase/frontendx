ddeadd95d37ff7a363a33b6f2eb8eb1eb12cc79e
Siddhartha Basu <biosidd@gmail.com>
2023-12-22 13:50:20 -0600

    refactor(show.tsx): remove unused imports and replace custom components with Avatar and Information components
    The changes were made to simplify the code and improve maintainability.
    By replacing the custom components with the Avatar and Information
    components, we are able to reduce the complexity of the Show component
    and make it easier to understand and maintain.

--- apps/stock-center/src/pages/user/show.tsx
+++ apps/stock-center/src/pages/user/show.tsx
@@ -1,17 +1,8 @@
-import { useLocation, useNavigate } from "react-router-dom"
-import {
-  Box,
-  Grid,
-  Typography,
-  IconButton,
-  Divider,
-  Button,
-} from "@material-ui/core"
-import { MoodSharp } from "@material-ui/icons"
+import { useLocation } from "react-router-dom"
+import { Box, Grid, Typography, Divider } from "@material-ui/core"
 import { makeStyles, Theme } from "@material-ui/core/styles"
 import { ACCESS, UserWithRoles } from "auth"
-import { match } from "fp-ts/Array"
-import { pipe } from "fp-ts/function"
+import { Avatar, Information } from "ui-user"
 
 const useStyles = makeStyles((theme: Theme) => ({
   divider: {
@@ -25,7 +16,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 const Show = () => {
   const classes = useStyles()
-  const navigate = useNavigate()
   const location = useLocation()
   const user = location.state as UserWithRoles
   return (
@@ -38,9 +28,7 @@ const Show = () => {
       <Divider variant="inset" className={classes.divider} />
       <Grid container spacing={3} className={classes.userGrid}>
         <Grid item xs={4} container direction="row" justifyContent="flex-end">
-          <IconButton size="medium">
-            <MoodSharp style={{ fontSize: 70 }} />
-          </IconButton>
+          <Avatar />
         </Grid>
         <Grid
           item
@@ -49,31 +37,7 @@ const Show = () => {
           direction="column"
           justifyContent="center"
           alignItems="baseline">
-          <Typography variant="h2" gutterBottom>
-            {user?.name}
-          </Typography>
-          <Typography variant="h3" gutterBottom>
-            {user?.email}
-          </Typography>
-          <Typography variant="h4" gutterBottom>
-            {user?.phone_number}
-          </Typography>
-          <Button
-            variant="contained"
-            size="small"
-            color="primary"
-            onClick={() => navigate("/user/edit", { state: user })}>
-            Edit
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          {pipe(
-            user.roles,
-            match(
-              () => <h2>No roles for user</h2>,
-              (roles) => <h2>User roles {roles.join(" ")}</h2>,
-            ),
-          )}
+          <Information user={user} />
         </Grid>
       </Grid>
     </Box>
