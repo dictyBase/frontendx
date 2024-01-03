d0a0e8be76da4005522f5dc30078f8a5731ad5b0
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-03 17:00:01 -0600

    feat(frontpage): add new pages and components for editable functionality
    - Added a new file `addpage.tsx` in the directory `apps/dicty-frontpage/src/pages/[section]/[name]/[subname]` to handle the addition of new pages.
    - Added a new file `edit.tsx` in the directory `apps/dicty-frontpage/src/pages/[section]/[name]/[subname]` to handle the editing of existing pages.
    - Added a new file `editable.tsx` in the directory `apps/dicty-frontpage/src/pages/[section]/[name]` to handle the editable functionality.
    - Updated the import statements in `edit.tsx` and `editable.tsx` to use the `EditView` component from the `EditablePages` feature instead of the `EditInfoPage` component.
    - Updated the import statements in `addpage.tsx` and `editable.tsx` to use the `AddPageView` component from the `EditablePages` feature.
    - Added the `ACCESS.private` constant to the exported `access` variable in all the files.
    
    These changes were made to add support for editable functionality in the frontpage application. The new files and components allow users to add and edit pages with private access.

--- apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/editable.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/editable.tsx
@@ -1,6 +1,6 @@
-import { EditView } from "../../../../features/EditablePages/EditView"
+import { Editable } from "../../../../features/EditablePages/Editable"
 import { ACCESS } from "../../../../app/routes/types"
 
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default EditView
+export default Editable
 export const access = ACCESS.private
