2977e5eadc6a92c41e670d73a02c9dcc9d0a8589
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-01-05 11:36:17 -0600

    feat(ui-user): add Paper component and makeStyles for styling in EditablePagesList.tsx and Information.tsx
    The changes were made to improve the visual appearance of the
    EditablePagesList and Information components. The Paper component from
    Material-UI was added to provide a container with a shadow, which makes
    the components stand out more on the page. The makeStyles function was
    used to create a custom style that adds padding to the Paper component,
    improving the overall layout and readability of the components.

--- packages/ui-user/src/EditablePagesList.tsx
+++ packages/ui-user/src/EditablePagesList.tsx
@@ -1,39 +1,50 @@
 import { Link as RouterLink } from "react-router-dom"
+import { makeStyles, Theme } from "@material-ui/core/styles"
 import {
   Typography,
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
+  Paper,
 } from "@material-ui/core"
 import FolderIcon from "@material-ui/icons/Folder"
 
-const EditablePagesList = () => (
-  <>
-    <Typography variant="h2" gutterBottom>
-      Editable Pages
-    </Typography>
-    <Typography variant="h3" gutterBottom>
-      Dicty Stock Center
-    </Typography>
-    <List component="nav">
-      <ListItem button component={RouterLink} to="/home/editable">
-        <ListItemIcon>
-          <FolderIcon />
-        </ListItemIcon>
-        <ListItemText primary="Home" />
-      </ListItem>
-      <ListItem
-        button
-        component={RouterLink}
-        to="/information/additional-materials/editable">
-        <ListItemIcon>
-          <FolderIcon />
-        </ListItemIcon>
-        <ListItemText primary="Additional Materials" />
-      </ListItem>
-    </List>
-  </>
-)
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(3),
+  },
+}))
+
+const EditablePagesList = () => {
+  const classes = useStyles()
+  return (
+    <Paper elevation={3} className={classes.root}>
+      <Typography variant="h2" gutterBottom>
+        Editable Pages
+      </Typography>
+      <Typography variant="h3" gutterBottom>
+        Dicty Stock Center
+      </Typography>
+      <List component="nav">
+        <ListItem button component={RouterLink} to="/home/editable">
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem
+          button
+          component={RouterLink}
+          to="/information/additional-materials/editable">
+          <ListItemIcon>
+            <FolderIcon />
+          </ListItemIcon>
+          <ListItemText primary="Additional Materials" />
+        </ListItem>
+      </List>
+    </Paper>
+  )
+}
 
 export { EditablePagesList }
