594dcd2b17b9a94313551e71dd857dc46aef17dc
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-07 20:16:51 -0600

    feat(FooterWithAuth.tsx): add FooterWithAuth component to display a footer based on user authorization status
    The FooterWithAuth component is added to the app's layout. It imports the Footer component from the "@dictybase/footer" package and other necessary dependencies. The component uses the useAuthorization hook to check if the user is authorized and isLoading to handle loading state. If isLoading is true, a Loader component is displayed. If isLoading is false, the displayOnAuthorzied function is used to conditionally render the Footer component based on the user's authorization status. If the user is authorized, the Footer component is rendered with data from the authFooterData.json file. If the user is unauthorized, a default Footer component is rendered.

---
+++ apps/dicty-frontpage/src/app/layout/FooterWithAuth.tsx
@@ -0,0 +1,24 @@
+import { Footer } from "@dictybase/footer"
+import { match } from "ts-pattern"
+import { displayOnAuthorzied, useAuthorization } from "auth"
+import { Loader } from "../../common/components/Loader"
+import authFooterData from "../../common/data/authFooterData.json"
+
+const FooterWithAuth = () => {
+  const { isAuthorized, isLoading } = useAuthorization({
+    entries: ["content-admin"],
+  })
+
+  return match(isLoading)
+    .with(true, () => <Loader />)
+    .with(false, () =>
+      displayOnAuthorzied({
+        isAuthorized,
+        authorized: <Footer data={authFooterData} />,
+        unauthorized: <Footer />,
+      }),
+    )
+    .exhaustive()
+}
+
+export { FooterWithAuth }
