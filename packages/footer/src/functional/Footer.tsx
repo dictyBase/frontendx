ac8a8e6da8db34944d64e22e6f3800a08ed27d4d
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-07 19:17:10 -0600

    feat(Footer.tsx): add support for custom data in Footer component to allow dynamic rendering of links
    The 'fromNullable' function from the 'fp-ts/Option' library is imported to fix a compilation error. The Footer component now accepts a 'data' prop, which allows for custom data to be passed in for rendering the links dynamically. This provides flexibility in displaying different sets of links based on the specific use case.

--- packages/footer/src/functional/Footer.tsx
+++ packages/footer/src/functional/Footer.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@material-ui/core"
 import { pipe } from "fp-ts/lib/function"
-import { map as Omap, getOrElse } from "fp-ts/Option"
+import { map as Omap, getOrElse, fromNullable } from "fp-ts/Option"
 import { fromChildren, composeChildren, type Comp } from "@dictybase/functional"
 import { v4 as uuid4 } from "uuid"
 import { FooterHead } from "../components/FooterHead"
@@ -8,16 +8,26 @@ import { FooterSponsors } from "../components/FooterSponsors"
 import { FooterContainer } from "../components/FooterContainer"
 import { Links } from "./Links"
 import { footerData } from "../data/footerData"
+import { FooterItem } from "../types"
+
+type FooterProperties = {
+  data?: Array<FooterItem>
+}
 
 const footerContainerWrapper = (children: Comp) => (
   <FooterContainer>{children}</FooterContainer>
 )
 
-export const Footer = () =>
-  pipe(
+export const Footer = ({ data }: FooterProperties) => {
+  const linksData = pipe(
+    data,
+    fromNullable,
+    getOrElse(() => footerData),
+  )
+  return pipe(
     [
       <FooterHead key={uuid4()} title="Dicty Community Resource" />,
-      <Links key={uuid4()} data={footerData} />,
+      <Links key={uuid4()} data={linksData} />,
       <FooterSponsors key={uuid4()} />,
     ],
     fromChildren,
@@ -25,3 +35,4 @@ export const Footer = () =>
     Omap(footerContainerWrapper),
     getOrElse(() => <Box>error in rendering</Box>),
   )
+}
