abf0018eb86375c7f725c24205e81a7d2dee2706
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-09 19:02:37 -0600

    refactor(Links.tsx): remove unused imports and simplify code structure
    The commit removes unused imports and simplifies the code structure by removing unnecessary functions and types. The code is now cleaner and easier to read.

--- packages/header/src/functional/Links.tsx
+++ packages/header/src/functional/Links.tsx
@@ -1,72 +1,18 @@
 import { Box } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
-import { v4 as uuid4 } from "uuid"
-import { map as Amap, let as Alet, bindTo, append as Append } from "fp-ts/Array"
-import { map as Omap, getOrElse, Option } from "fp-ts/Option"
+import { map as Omap, getOrElse } from "fp-ts/Option"
 import { fromChildren, composeChildren, Comp } from "@dictybase/functional"
-import {
-  type IconItemProperty,
-  iconItems,
-  Title,
-  LinksIcon,
-  LinksIconButton,
-  LinksContainer,
-} from "../components/LinksContainer"
+import { LinksContainer } from "../components/LinksContainer"
 
-type composeTitleIconProperties = {
-  items: IconItemProperty
-  titleComp: Comp
-  iconComp: Comp
-}
-
-type linksIconButtonWrapperProperties = composeTitleIconProperties & {
-  children: Option<Comp>
-}
-
-type LinksProperties = { LoginOut: Comp }
+type LinksProperties = { links: Array<Comp> }
 
 const linksContainerWrapper = (children: Comp) => (
   <LinksContainer>{children}</LinksContainer>
 )
 
-const linksIconButtonWrapper = ({
-  children,
-  items: { href },
-}: linksIconButtonWrapperProperties) =>
+const Links = ({ links }: LinksProperties) =>
   pipe(
-    children,
-    Omap((comp) => (
-      <LinksIconButton href={href} key={uuid4()}>
-        {comp}
-      </LinksIconButton>
-    )),
-    getOrElse(() => <Box>Error in rendering</Box>),
-  )
-
-const composeTitleIcon = ({
-  titleComp,
-  iconComp,
-}: composeTitleIconProperties) =>
-  pipe([titleComp, iconComp], fromChildren, composeChildren)
-
-const iconButtonPipe = (items: Array<IconItemProperty>) =>
-  pipe(
-    items,
-    bindTo("items"),
-    Alet("titleComp", ({ items: { title } }) => (
-      <Title title={title} key={uuid4()} />
-    )),
-    Alet("iconComp", ({ items: { Icon } }) => (
-      <LinksIcon Icon={Icon} key={uuid4()} />
-    )),
-    Alet("children", composeTitleIcon),
-    Amap(linksIconButtonWrapper),
-  )
-
-const Links = ({ LoginOut }: LinksProperties) =>
-  pipe(
-    iconButtonPipe(iconItems),
-    Append(LoginOut),
+    links,
     fromChildren,
     composeChildren,
     Omap(linksContainerWrapper),
