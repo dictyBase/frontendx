db4e6c8090682a1a307b55b992ed6e15b7152604
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-10 00:29:30 -0600

    test(Header.test.tsx): update Header component tests to use the new defaultIconItems and iconButtonPipe functions
    The Header component tests have been updated to use the new defaultIconItems and iconButtonPipe functions. This ensures that the tests are using the correct data and functionality for rendering and generating the links in the Header component.

--- packages/header/src/__tests__/Header.test.tsx
+++ packages/header/src/__tests__/Header.test.tsx
@@ -1,13 +1,16 @@
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { expect, describe } from "vitest"
-import { iconItems } from "../components/LinksContainer"
+import { defaultIconItems } from "../defaultIconItems"
+import { iconButtonPipe } from "../functional/iconButtonPipe"
 import "@testing-library/jest-dom"
 import { Header } from "../functional/Header"
 
+const testLinks = iconButtonPipe(defaultIconItems)
+
 describe("header", () => {
   test("should render the logo", () => {
-    const { getByRole } = render(<Header />)
+    const { getByRole } = render(<Header links={testLinks} />)
     expect(getByRole("img")).toHaveAttribute(
       "src",
       expect.stringContaining("logo"),
@@ -17,25 +20,25 @@ describe("header", () => {
     ).toBeInTheDocument()
   })
   test("should render the default search component", () => {
-    const { getByRole } = render(<Header />)
+    const { getByRole } = render(<Header links={testLinks} />)
     expect(getByRole("textbox", { name: /guided search/i })).toBeInTheDocument()
   })
   test("should accept user search input", async () => {
     const user = userEvent.setup()
-    const { getByRole } = render(<Header />)
+    const { getByRole } = render(<Header links={testLinks} />)
     const input = getByRole("textbox", { name: /Guided Search/ })
     expect(input).toBeEmptyDOMElement()
     await user.type(input, "yomar")
     expect(input).toHaveValue("yomar")
   })
   test("should generate the links ", () => {
-    const { getAllByRole } = render(<Header />)
-    expect(getAllByRole("link")).toHaveLength(iconItems.length)
+    const { getAllByRole } = render(<Header links={testLinks} />)
+    expect(getAllByRole("link")).toHaveLength(defaultIconItems.length)
   })
-  test.each(iconItems)(
+  test.each(defaultIconItems)(
     "should have link $href with title $title",
     ({ href, title }) => {
-      const { getByRole } = render(<Header />)
+      const { getByRole } = render(<Header links={testLinks} />)
       expect(getByRole("link", { name: title })).toHaveAttribute("href", href)
     },
   )
