413fb1075222a055c8c4ee51726da006e556a103
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-09 19:02:48 -0600

    refactor(LinksContainer.tsx): remove unused code and exports
    The commit removes unused code related to the iconItems array and the IconItemProperty type. It also removes unnecessary exports that are no longer used in the file. This cleanup improves code readability and reduces unnecessary complexity.

--- packages/header/src/components/LinksContainer.tsx
+++ packages/header/src/components/LinksContainer.tsx
@@ -1,22 +1,9 @@
 import { Box, Typography, IconButton } from "@material-ui/core"
-import {
-  Add,
-  Info,
-  ArrowDownward as Download,
-  ExitToApp as Login,
-} from "@material-ui/icons"
-import { v4 as uuid4 } from "uuid"
+import { ExitToApp as Login } from "@material-ui/icons"
 import type { Comp } from "@dictybase/functional"
 import { headerStyles } from "../styles/headerStyles"
 
 type IconType = typeof Login
-
-type IconItemProperty = {
-  href: string
-  title: string
-  key: string
-  Icon: IconType
-}
 
 interface LinksContainerProperties {
   children: Comp
@@ -25,22 +12,6 @@ interface LinksContainerProperties {
 type IconButtonProperties = LinksContainerProperties & {
   href: string
 }
-
-const iconItems: Array<IconItemProperty> = [
-  {
-    href: "/community/citation",
-    title: "Cite Us",
-    Icon: Add,
-    key: uuid4(),
-  },
-  {
-    href: "/downloads",
-    title: "Downloads",
-    Icon: Download,
-    key: uuid4(),
-  },
-  { href: "/about", title: "About dictybase", Icon: Info, key: uuid4() },
-]
 
 const Title = ({ title }: { title: string }) => (
   <Typography variant="subtitle2">{title}</Typography>
@@ -60,11 +31,4 @@ const LinksContainer = ({ children }: LinksContainerProperties) => (
   <Box className={headerStyles().linksContainer}>{children}</Box>
 )
 
-export {
-  type IconItemProperty,
-  iconItems,
-  Title,
-  LinksIcon,
-  LinksIconButton,
-  LinksContainer,
-}
+export { Title, LinksIcon, LinksIconButton, LinksContainer }
