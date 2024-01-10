d14d4760439875ccc4cfaa2e4f3263d70baac575
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-09 22:18:28 -0600

    feat(headerLinks.ts): add new file headerLinks.ts to handle header links configuration
    The headerLinks.ts file is added to the auth package. It imports necessary dependencies from "@dictybase/header", "@material-ui/icons", and "uuid". It defines two arrays of header links: authorizedIconItems and defaultIconItems. These arrays contain objects with properties such as href, title, Icon, and key. The defaultHeaderLinks and authorizedHeaderLinks functions are defined to handle the configuration of the header links.

---
+++ packages/auth/src/headerLinks.ts
@@ -0,0 +1,29 @@
+import { iconButtonPipe, defaultIconItems } from "@dictybase/header"
+import { Add, Info, ArrowDownward as Download } from "@material-ui/icons"
+import { v4 as uuid4 } from "uuid"
+
+const authorizedIconItems = [
+  {
+    href: "/community/citation/editable",
+    title: "Cite Us",
+    Icon: Add,
+    key: uuid4(),
+  },
+  {
+    href: "/downloads",
+    title: "Downloads",
+    Icon: Download,
+    key: uuid4(),
+  },
+  {
+    href: "/about/editable",
+    title: "About dictybase",
+    Icon: Info,
+    key: uuid4(),
+  },
+]
+
+const defaultHeaderLinks = iconButtonPipe(defaultIconItems)
+const authorizedHeaderLinks = iconButtonPipe(authorizedIconItems)
+
+export { defaultHeaderLinks, authorizedHeaderLinks }
