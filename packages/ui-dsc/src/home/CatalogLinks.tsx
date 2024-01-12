456518b96fbf5fa80ee66ff317effc927b4f06c2
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-11 22:58:48 -0600

    fix(CatalogLinks.tsx): fix the logic of determining the `allLinks` array based on the `isAuthorized` prop
    The `match` function from `fp-ts` has been replaced with the `match` function from `ts-pattern` for better readability and exhaustiveness checking. The logic for determining the `allLinks` array based on the `isAuthorized` prop has been fixed to correctly handle the `isAuthorized` value and return the appropriate array of links.

--- packages/ui-dsc/src/home/CatalogLinks.tsx
+++ packages/ui-dsc/src/home/CatalogLinks.tsx
@@ -1,6 +1,7 @@
 import Grid from "@material-ui/core/Grid"
-import { fromNullable, match } from "fp-ts/Option"
+import { fromNullable, getOrElse } from "fp-ts/Option"
 import { pipe } from "fp-ts/function"
+import { match } from "ts-pattern"
 import { HomepageColumn } from "./HomepageColumn"
 import {
   materialsLinks,
@@ -15,11 +16,14 @@ type CatalogLinksProperties = {
 const CatalogLinks = ({ isAuthorized }: CatalogLinksProperties) => {
   const allLinks = pipe(
     fromNullable(isAuthorized),
-    match(
-      () => Array.of(...materialsLinks, additionalMaterial),
-      () => Array.of(...materialsLinks, additionalMaterialAuth),
-    ),
+    getOrElse(() => false),
+    (b) =>
+      match(b)
+        .with(false, () => Array.of(...materialsLinks, additionalMaterial))
+        .with(true, () => Array.of(...materialsLinks, additionalMaterialAuth))
+        .exhaustive(),
   )
+
   return (
     <Grid container item xs={4} direction="column">
       <HomepageColumn title="Catalogs/Materials" entries={allLinks} />
