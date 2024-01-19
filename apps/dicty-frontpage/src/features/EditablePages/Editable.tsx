403dfd611b57b018cce113900630734a46b5e111
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-19 11:48:27 -0600

    refactor(Editable.tsx): separate reusable code into separate files.
    - Remove unused imports and variables
    - Replace the useParams hook with the useSlug custom hook
    - Move the hasNotFoundError function to a separate file for reusability
    
    The changes were made to improve code readability and maintainability. Unused imports and variables were removed to reduce clutter. The useParams hook was replaced with a custom hook called useSlug to simplify the code. The hasNotFoundError function was moved to a separate file to make it reusable in other parts of the codebase.

--- apps/dicty-frontpage/src/features/EditablePages/Editable.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Editable.tsx
@@ -1,34 +1,15 @@
-import { useParams, Navigate } from "react-router-dom"
-import { type GraphQLErrors, type ApolloError } from "@apollo/client/errors"
+import { Navigate } from "react-router-dom"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
-import {
-  fromArray,
-  filter as readonlyArrayFilter,
-  isNonEmpty,
-} from "fp-ts/lib/ReadonlyArray"
-import { getOrElse, fromNullable } from "fp-ts/Option"
 import { match, P } from "ts-pattern"
 import { GraphQLErrorPage } from "../../common/components/errors/GraphQLErrorPage"
 import { EditableView } from "./EditableView"
 import { Loader } from "../../common/components/Loader"
-import { getSlug } from "../../common/utils/getSlug"
-
-const hasNotFoundError = (apolloError: ApolloError | undefined) =>
-  pipe(
-    apolloError,
-    fromNullable,
-    getOrElse(() => ({
-      graphQLErrors: fromArray([]) as GraphQLErrors,
-    })),
-    ({ graphQLErrors }) => graphQLErrors,
-    readonlyArrayFilter((gqlError) => gqlError.extensions?.code === "NotFound"),
-    isNonEmpty,
-  )
+import { useSlug } from "../../common/utils/useSlug"
+import { hasNotFoundError } from "../../common/utils/hasNotFoundError"
 
 const Editable = () => {
-  const { name, subname } = useParams()
-  const slug = getSlug({ name, subname })
+  const slug = useSlug()
   const { loading, error, data } = useContentBySlugQuery({
     variables: { slug },
   })
