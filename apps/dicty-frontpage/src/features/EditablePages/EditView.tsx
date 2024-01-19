858b38e20b72cbae8904806ff5114ba008c1abf9
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-19 10:49:41 -0600

    feat(EditView.tsx): add functionality to handle save and cancel actions in EditView component
    The EditView component now includes functionality to handle save and cancel actions. The useNavigate hook from react-router-dom is used to navigate to the appropriate route when the save or cancel actions are triggered. The onSave function is called when the save action is triggered, and it currently includes a placeholder comment for the update content mutation. The onCancel function is called when the cancel action is triggered, and it also navigates to the appropriate route.

--- apps/dicty-frontpage/src/features/EditablePages/EditView.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/EditView.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@material-ui/core"
+import { useNavigate } from "react-router-dom"
 import { type ContentBySlugQuery } from "dicty-graphql-schema"
 import { Editor } from "editor"
 
@@ -6,13 +7,26 @@ type ContentViewProperties = {
   data: NonNullable<ContentBySlugQuery["contentBySlug"]>
 }
 
-const EditView = ({ data }: ContentViewProperties) => (
-  <Container>
-    <Editor
-      editable
-      content={{ storageKey: data.slug, editorState: data.content }}
-    />
-  </Container>
-)
+const EditView = ({ data }: ContentViewProperties) => {
+  const navigate = useNavigate()
+  const onSave = async () => {
+    // update content mutation
+    navigate("../editable")
+  }
+  const onCancel = () => {
+    navigate("../editable")
+  }
+
+  return (
+    <Container>
+      <Editor
+        handleSave={onSave}
+        handleCancel={onCancel}
+        editable
+        content={{ storageKey: data.slug, editorState: data.content }}
+      />
+    </Container>
+  )
+}
 
 export { EditView }
