cefa18e1135ad34ec0238869ffd0eb75dd7f19da
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-21 21:46:55 -0600

    refactor(edit.tsx): use the Edit component in the /edit route
    The Edit component is now being used for /edit routes. The component fetches the content edit based on the slug of the URL and renders the EditView component if data is found.

--- apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/edit.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/edit.tsx
@@ -1,6 +1,6 @@
-import { EditView } from "../../../../features/EditablePages/EditView"
+import { Edit } from "../../../../features/EditablePages/Edit"
 import { ACCESS } from "../../../../app/routes/types"
 
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default EditView
+export default Edit
 export const access = ACCESS.private
