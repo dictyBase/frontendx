277401ac40df42045c14b042b8b132c7e6cb813b
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-21 21:49:32 -0600

    feat(frontpage): add show.tsx files for nested routes [section]/[name]/[subname] and [section]/[name]
    The show.tsx files are added to support the routes [section]/[name]/[subname]/show and [section]/[name]/show in the frontpage application. These files import the Show component from the EditablePages feature and define the access level as public using the ACCESS constant from the routes/types module. This allows the pages to be displayed publicly.

--- apps/dicty-frontpage/src/pages/[section]/[name]/show.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/show.tsx
@@ -1,6 +1,6 @@
-import { InfoPageContainer } from "../../../features/EditablePages/InfoPageContainer"
+import { Show } from "../../../features/EditablePages/Show"
 import { ACCESS } from "../../../app/routes/types"
 
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default InfoPageContainer
+export default Show
 export const access = ACCESS.public
