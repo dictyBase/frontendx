74d611ab17cb0d6054ca971dcf4835703ac2777a
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-22 00:04:45 -0600

    s(getLastPathSegment.ts): add documentation to getLastPathSegment

--- apps/dicty-frontpage/src/common/utils/getLastPathSegment.ts
+++ apps/dicty-frontpage/src/common/utils/getLastPathSegment.ts
@@ -5,6 +5,14 @@ import { filter as arrayFilter, difference, last } from "fp-ts/ReadonlyArray"
 
 const viewModes = ["show", "editable", "edit"]
 
+/**
+ * Returns the last segment of the given pathname after splitting it by '/'.
+ * It filters out empty segments, removes any segments that match the viewModes array,
+ * and returns the last remaining segment.
+ *
+ * @param pathname - The pathname to extract the last segment from.
+ * @returns The last segment of the pathname, or an empty string if no segment is found.
+ */
 const getLastPathSegment = (pathname: string) =>
   pipe(
     pathname,
