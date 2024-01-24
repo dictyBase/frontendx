f20b450037d82bbfe4402eb46952bf0dfd6becc0
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-23 23:29:25 -0600

    feat(useFetchPublications.ts): implement useFetchPublication hook to fetch publication data from a given URL using TaskEither monad
    The useFetchPublication hook is implemented to handle fetching publication data from a given URL using the TaskEither monad. The hook sets up state variables for loading and the result of the fetch operation. The useEffect hook is used to perform the fetch operation asynchronously. The createFetchTaskEither function is introduced to encapsulate the logic of fetching the resource using TaskEither.

--- apps/dicty-frontpage/src/common/hooks/useFetchPublications.ts
+++ apps/dicty-frontpage/src/common/hooks/useFetchPublications.ts
@@ -1,15 +1,48 @@
-import { type Either, left, right } from "fp-ts/Either"
+import { useState, useEffect } from "react"
+import { pipe, flow } from "fp-ts/function"
+import {
+  type TaskEither,
+  map as TEmap,
+  match as TEmatch,
+  tryCatch as TEtryCatch,
+} from "fp-ts/TaskEither"
+import { type Either, left as Eleft, right as Eright } from "fp-ts/Either"
 
 const RSS_URL =
   "https://pubmed.ncbi.nlm.nih.gov/rss/search/1xSjLNP-2lGAmjK0hZKzE4pxRxyAAh7BAEFNc5kyVReacTxspv/?limit=15&utm_campaign=pubmed-2&fc=20231211102630"
 
-type Error<E = string> = E
+/**
+ * A function whose return function fetches a resource from a given URL using the TaskEither monad.
+ *
+ * https://gcanti.github.io/fp-ts/modules/TaskEither.ts.html
+ *
+ * @param url - The URL of the resource to fetch.
+ * @returns A TaskEither that represents the asynchronous fetch operation.
+ */
+const createFetchTaskEither = (url: string): TaskEither<string, Response> =>
+  pipe(
+    TEtryCatch(
+      () => fetch(url),
+      (reason) => reason,
+    ),
+    TEmatch(
+      (error) => Eleft(`${error}`),
+      (response) =>
+        response.status === 200
+          ? Eright(response)
+          : Eleft(`HTTP Error ${response.status}: ${response.statusText}`),
+    ),
+  )
 
-const fetchPublicationData = async (): Either<Error, Response> => {
-  try {
-    const data = await fetch(RSS_URL)
-    return right(data)
-  } catch (error) {
-    return left(error.message)
-  }
+const useFetchPublication = (url: string) => {
+  const [loading, setLoading] = useState(false)
+  const [result, setResult] = useState({})
+
+  useEffect(() => {
+    const getPublicationItems = async () => {
+      const dataOrError = pipe(url, createFetchTaskEither)
+    }
+  })
 }
+
+export { createFetchTaskEither as fetchTaskEither }
