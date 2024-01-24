4c7ab24d2339b68af3e1f83f4baf9b58f27a62b7
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-24 14:35:23 -0600

    feat(useFetchPublications.ts): add functionality to fetch and parse RSS feed for publication items
    The code in useFetchPublications.ts has been refactored to improve code readability and maintainability. Changes include renaming functions for better semantics, using more descriptive variable names, and organizing imports. Additionally, the functionality to fetch and parse an RSS feed for publication items has been added. This allows the component to retrieve and display publication data from the specified URL.
    Issue #620

--- apps/dicty-frontpage/src/common/hooks/useFetchPublications.ts
+++ apps/dicty-frontpage/src/common/hooks/useFetchPublications.ts
@@ -1,15 +1,38 @@
+/* eslint-disable unicorn/prefer-query-selector */
 import { useState, useEffect } from "react"
 import { pipe, flow } from "fp-ts/function"
+import { type Task, map as Tmap } from "fp-ts/Task"
+import {
+  fromNullable,
+  getOrElse as OgetOrElse,
+  map as Omap,
+} from "fp-ts/Option"
+import { map as Amap } from "fp-ts/Array"
 import {
   type TaskEither,
+  of as TEof,
   map as TEmap,
   match as TEmatch,
   tryCatch as TEtryCatch,
+  chain as TEchain,
+  getOrElse as TEgetOrElse,
+  tap as TEtap,
+  tapError as TEtapError,
+  left as TEleft,
+  right as TEright,
 } from "fp-ts/TaskEither"
 import { type Either, left as Eleft, right as Eright } from "fp-ts/Either"
 
 const RSS_URL =
   "https://pubmed.ncbi.nlm.nih.gov/rss/search/1xSjLNP-2lGAmjK0hZKzE4pxRxyAAh7BAEFNc5kyVReacTxspv/?limit=15&utm_campaign=pubmed-2&fc=20231211102630"
+
+type PublicationItem = {
+  title: string
+  authors: Array<string>
+  description: string
+  publishDate: string
+  link: string
+}
 
 /**
  * A function whose return function fetches a resource from a given URL using the TaskEither monad.
@@ -19,14 +42,14 @@ const RSS_URL =
  * @param url - The URL of the resource to fetch.
  * @returns A TaskEither that represents the asynchronous fetch operation.
  */
-const createFetchTaskEither = (url: string): TaskEither<string, Response> =>
+const createTaskEitherFetch = (url: string): TaskEither<string, Response> =>
   pipe(
     TEtryCatch(
       () => fetch(url),
-      (reason) => reason,
+      (reason) => `${reason}`,
     ),
     TEmatch(
-      (error) => Eleft(`${error}`),
+      (error) => Eleft(error),
       (response) =>
         response.status === 200
           ? Eright(response)
@@ -34,15 +57,73 @@ const createFetchTaskEither = (url: string): TaskEither<string, Response> =>
     ),
   )
 
-const useFetchPublication = (url: string) => {
+const parseRSSResponse = (response: Response) =>
+  pipe(
+    TEtryCatch(
+      () => response.text(),
+      (reason) => `${reason}`,
+    ),
+  )
+
+const resolveQuerySelector = flow(
+  fromNullable<Element | null>,
+  Omap(({ innerHTML }) => innerHTML),
+  OgetOrElse(() => ""),
+)
+
+const resolveQuerySelectorAll = flow(
+  (a: HTMLCollectionOf<Element>) => [...a],
+  Amap(({ innerHTML }) => innerHTML),
+)
+
+const extractPublicationItems = (xmlString: string): Array<PublicationItem> =>
+  pipe(
+    xmlString,
+    (a) => new window.DOMParser().parseFromString(a, "text/xml"),
+    (b) => b.querySelectorAll("item"),
+    (c) => [...c],
+    Amap((fa) => ({
+      title: pipe(fa.querySelector("title"), resolveQuerySelector),
+      authors: pipe(
+        fa.getElementsByTagName("dc:creator"),
+        resolveQuerySelectorAll,
+      ),
+      description: pipe(fa.querySelector("description"), resolveQuerySelector),
+      publishDate: pipe(fa.querySelector("date"), resolveQuerySelector),
+      link: pipe(fa.querySelector("link"), resolveQuerySelector),
+    })),
+  )
+
+const useFetchPublications = (url: string) => {
   const [loading, setLoading] = useState(false)
-  const [result, setResult] = useState({})
+  const [data, setData] = useState<Array<PublicationItem>>([])
+  const [error, setError] = useState("")
 
   useEffect(() => {
     const getPublicationItems = async () => {
-      const dataOrError = pipe(url, createFetchTaskEither)
+      await pipe(
+        url,
+        createTaskEitherFetch,
+        TEchain(parseRSSResponse),
+        TEmap(extractPublicationItems),
+        TEtapError((e) => {
+          setError(e)
+          return TEleft(e)
+        }),
+        TEtap((a) => {
+          setData(a)
+          return TEright(a)
+        }),
+      )()
     }
   })
 }
 
-export { createFetchTaskEither as fetchTaskEither }
+// pipe(
+//   RSS_URL,
+//   createTaskEitherFetch,
+//   TEchain(parseRSSResponse),
+//   TEmap(extractPublicationItems),
+// )().then((a) => console.log(a))
+
+export { useFetchPublications }
