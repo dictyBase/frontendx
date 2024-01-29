25ea720210a7f2739ae5ab573834c0445a60ce42
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-29 03:24:56 -0600

    refactor(LatestPapersView.tsx): add styling to LatestPapersView
    - Add missing import for Container and makeStyles from @material-ui/core
    - Add missing import for FontAwesomeIcon from @fortawesome/react-fontawesome
    - Remove unused SinglePaper component
    - Add missing styles and classNames for the LatestPapersView component
    - Limit the number of papers displayed to 6
    The changes were made to improve the styling and structure of the LatestPapersView component. The missing imports were added to ensure that the necessary components and styles are available. The unused SinglePaper component was removed to eliminate unnecessary code. The styles and classNames were added to properly style the component and improve its visual appearance. Limiting the number of papers displayed to 6 helps to keep the component more compact and manageable.

--- apps/dicty-frontpage/src/features/Frontpage/LatestPapersView.tsx
+++ apps/dicty-frontpage/src/features/Frontpage/LatestPapersView.tsx
@@ -1,26 +1,101 @@
-import { Grid, Box, Typography } from "@material-ui/core"
+import { Grid, Box, Container, Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/styles"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { LatestPaperItem } from "./LatestPaperItem"
 import { type PublicationItem } from "../../common/hooks/useFetchPublications"
 
 type LatestPapersProperties = {
   data: Array<PublicationItem>
 }
 
-type SinglePaperProperties = {
-  data: PublicationItem
+const useStyles = makeStyles({
+  container: {
+    // zIndex: -4,
+    textAlign: "left",
+    paddingBottom: "10px",
+    backgroundColor: "#eff8fb",
+    borderRadius: "15px",
+    boxSizing: "border-box",
+    marginBottom: "10px",
+    // overflow: "hidden",
+    "@media (max-width: 768px)": {
+      height: "350px",
+    },
+  },
+  title: {
+    paddingLeft: "5px",
+    color: "#086a87",
+    fontSize: "20px",
+    verticalAlign: "top",
+    textAlign: "left",
+  },
+  header: {
+    color: "black",
+    fontSize: "20px",
+    padding: "15px 30px 0px 35px",
+    verticalAlign: "top",
+    textAlign: "right",
+
+    "@media (max-width: 767px)": {
+      fontSize: "24px",
+      textAlign: "right",
+      padding: "20px 5px 20px 15px",
+    },
+  },
+  listBox: {
+    padding: "0px 25px 10px 25px",
+    fontSize: "13px",
+    overflow: "hidden",
+    maxHeight: "440px",
+    marginBottom: "5px",
+    marginTop: "12px",
+    "@media (max-width: 992px) and (min-width: 767px)": {
+      fontSize: "10px",
+    },
+    "@media (max-width: 768px)": {
+      fontSize: "16px",
+    },
+  },
+  link: {
+    textDecoration: "none",
+    color: "#428bca",
+  },
+  bottomLink: {
+    color: "#0b3861",
+    fontSize: "11px",
+    fontStyle: "italic",
+    fontWeight: "normal",
+    textAlign: "center",
+    paddingBottom: "10px",
+
+    "@media (min-width: 1400px)": {
+      paddingTop: "30px",
+      fontSize: "12px",
+    },
+  },
+})
+
+const LatestPapersView = ({ data }: LatestPapersProperties) => {
+  const { container, header, title, listBox } = useStyles()
+  return (
+    <Container className={container}>
+      <Box className={header}>
+        <Grid container>
+          <Typography className={title}>
+            <FontAwesomeIcon icon="paperclip" size="sm" />
+          </Typography>
+          <span className={title}>LATEST PAPERS</span>
+        </Grid>
+      </Box>
+      <Grid container component="ul" className={listBox}>
+        {data.slice(0, 6).map((p) => (
+          <Grid item>
+            <LatestPaperItem data={p} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  )
 }
-
-const SinglePaper = ({ data }: SinglePaperProperties) => (
-  <Box>
-    <Typography>{data.title}</Typography>
-  </Box>
-)
-
-const LatestPapersView = ({ data }: LatestPapersProperties) => (
-  <Grid>
-    {data.map((p) => (
-      <SinglePaper data={p} />
-    ))}
-  </Grid>
-)
 
 export { LatestPapersView }
