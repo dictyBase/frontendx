46bfecab774b89debf5e856bd2c147dc02b7148d
Kevin Tun <kevin.tun@northwestern.edu>
2024-01-29 03:29:21 -0600

    feat(LatestPaperItem.tsx): add LatestPaperItem component to display a single publication item on the frontpage
    The LatestPaperItem component is added to the project. It is responsible for rendering a single publication item on the frontpage. The component receives a data prop which contains the information about the publication item, such as the title and link. The component uses Material-UI for styling and renders the title as the main content and the link as the source content.

---
+++ apps/dicty-frontpage/src/features/Frontpage/LatestPaperItem.tsx
@@ -0,0 +1,53 @@
+import { Typography } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { type PublicationItem } from "../../common/hooks/useFetchPublications"
+
+const useStyles = makeStyles({
+  listItem: {
+    listStyle: "none",
+    marginBottom: "10px",
+    maxHeight: "5rem",
+    // boxSizing: "border-box",
+  },
+  leadText: {
+    color: "#0b3861",
+    paddingRight: "10px",
+  },
+  mainContent: {
+    paddingRight: "10px",
+    fontWeight: "bold",
+    fontStyle: "italic",
+  },
+  sourceContent: {
+    color: "#0b3861",
+  },
+  sourceTitle: {
+    paddingTop: "7px",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  link: {
+    textDecoration: "none",
+    color: "#428bca",
+  },
+})
+
+type LatestPaperItemProperties = {
+  data: PublicationItem
+}
+
+const LatestPaperItem = ({ data }: LatestPaperItemProperties) => {
+  const { mainContent, listItem, sourceContent, sourceTitle, link } =
+    useStyles()
+  return (
+    <li className={listItem}>
+      <Typography className={mainContent}>{data.title}</Typography>
+      <br />
+      <Typography className={sourceContent}>
+        <a href={data.link}>Pubmed</a>
+      </Typography>
+    </li>
+  )
+}
+
+export { LatestPaperItem }
