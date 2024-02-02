acf9ecac343bcdd8f4928753c845ec01e35f109c
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-02 00:19:23 -0600

    feat(publication): add new page for publications
    A new page for publications has been added to the application. This page imports the `Publications` component from the `Publication` feature and exports it as the default export. Additionally, the `access` constant is exported as `ACCESS.public` from the `routes/types` module. This new page allows users to view publications and sets the access level to public.

---
+++ apps/dicty-frontpage/src/pages/publication/index.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "../../app/routes/types"
+import { Publications } from "../../features/Publication/Publications"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default Publications
+export const access = ACCESS.public
