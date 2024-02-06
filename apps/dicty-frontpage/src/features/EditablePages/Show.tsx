f0a32d34a5bb4e09f737198f4da1d07681ee3e0a
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 15:44:09 -0600

    fix(Show.tsx): comment out unused code and update query to use new GraphQL schema and add namespace to slug variable
    The code has been refactored to use the updated GraphQL schema and the `useContentBySlugQuery` hook has been replaced with the `useQuery` hook from `@apollo/client`. The query has been updated to include the `NAMESPACE` constant in the `slug` variable to ensure proper namespace handling. The unused code has been commented out for now.

--- apps/dicty-frontpage/src/features/EditablePages/Show.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Show.tsx
@@ -1,16 +1,34 @@
+import { useParams } from "react-router-dom"
+import { gql, useQuery } from "@apollo/client"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
 import { ContentView } from "./ContentView"
+import { NAMESPACE } from "../../common/constants/namespace"
 import { useSlug } from "../../common/hooks/useSlug"
 import { GraphQLErrorPage } from "../../common/components/errors/GraphQLErrorPage"
 import { Loader } from "../../common/components/Loader"
 
+const QUERY = gql`
+  query contentBySlug($slug: String!) {
+    contentBySlug(slug: $slug) {
+      id
+      content
+      name
+      slug
+      updated_at
+      __typename
+    }
+  }
+`
+
 const Show = () => {
   const slug = useSlug()
-  const { loading, error, data } = useContentBySlugQuery({
-    variables: { slug },
+  // const { loading, error, data } = useContentBySlugQuery({
+  //   variables: { slug },
+  // })
+  const { loading, error, data } = useQuery(QUERY, {
+    variables: { slug: `${NAMESPACE}-${slug}` },
   })
-
   return match({ loading, error, data })
     .with({ loading: true }, () => <Loader />)
     .with({ error: P.select(P.not(undefined)) }, (error_) => (
