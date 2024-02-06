c2afa866a4c7dc4d04925034687698c577416f08
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 12:51:17 -0600

    feat(SinglePublication.tsx): add grid layout and card components to enhance the visual structure of the SinglePublication component
    The SinglePublication component has been refactored to improve readability and maintainability. The changes include:
    - Replacing the getAuthorsString function with getAuthorsCitationString to improve the formatting of the authors' names.
    - Adding a grid layout to organize the different parts of the publication information.
    - Commenting out the hover effect on the mainContent class for now.
    - Adding the abstract and identifiers fields to be displayed in the component.
    These changes improve the overall structure and readability of the SinglePublication component, making it easier to understand and maintain.

--- apps/dicty-frontpage/src/features/Publication/SinglePublication.tsx
+++ apps/dicty-frontpage/src/features/Publication/SinglePublication.tsx
@@ -1,26 +1,30 @@
-import { Box, Typography } from "@material-ui/core"
+import {
+  Box,
+  Grid,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+} from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "react-router-dom"
 import { type PublicationItem } from "../../common/hooks/useFetchPublications"
 import {
-  getAuthorsString,
+  getAuthorsCitationString,
   getPublicationYear,
   formatTitle,
 } from "../../common/utils/citation"
 
 const useStyles = makeStyles({
-  listItem: {
-    listStyle: "none",
-    marginBottom: "10px",
-  },
   leadText: {
     color: "#0b3861",
     paddingRight: "10px",
   },
   mainContent: {
-    "&:hover": {
-      textDecoration: "underline",
-    },
+    // "&:hover": {
+    //   textDecoration: "underline",
+    // },
   },
   sourceContent: {
     color: "#0b3861",
@@ -41,23 +45,31 @@ type SinglePublicationProperties = {
 
 const SinglePublication = ({ data }: SinglePublicationProperties) => {
   const { mainContent, listItem, link } = useStyles()
-  const authors = getAuthorsString(data.authors)
+  const authors = getAuthorsCitationString(data.authors)
   const date = getPublicationYear(data.publishDate)
   const title = formatTitle(data.title).full
-  const { journal, pubmedId } = data
+  const { abstract, journal, identifiers, pubmedId } = data
   return (
     <li className={listItem}>
-      <Box className={mainContent}>
-        <Link
-          to={`/publication/${
-            import.meta.env.VITE_PUBLICATION_URL
-          }/${pubmedId}`}>
-          <Typography className={link} variant="body1">
-            {`${authors}. (${date}). ${title}. `}
-            <em>{journal}</em>
+      <Grid container direction="column" className={mainContent}>
+        <Grid item>
+          <Typography variant="h2" color="primary">
+            {`${title}`}
           </Typography>
-        </Link>
-      </Box>
+        </Grid>
+        <Grid item>
+          <Typography>{authors}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>{journal}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>{identifiers}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>{abstract}</Typography>
+        </Grid>
+      </Grid>
     </li>
   )
 }
