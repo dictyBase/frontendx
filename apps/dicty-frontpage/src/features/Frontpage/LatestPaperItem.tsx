3eb21067c4a2415513b81f01e24b46ab947753b8
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 12:49:52 -0600

    refactor(LatestPaperItem.tsx): rename getAuthorsString to getAuthorsCitationString and update its usage
    The function getAuthorsString has been renamed to getAuthorsCitationString to better reflect its purpose of generating a citation string for authors. The function is now also called with an additional argument { limit: 3 } to limit the number of authors displayed to 3. This improves the readability and conciseness of the LatestPaperItem component.

--- apps/dicty-frontpage/src/features/Frontpage/LatestPaperItem.tsx
+++ apps/dicty-frontpage/src/features/Frontpage/LatestPaperItem.tsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "react-router-dom"
 import { type PublicationItem } from "../../common/hooks/useFetchPublications"
 import {
-  getAuthorsString,
+  getAuthorsCitationString,
   getPublicationYear,
   formatTitle,
 } from "../../common/utils/citation"
@@ -31,7 +31,7 @@ type LatestPaperItemProperties = {
 
 const LatestPaperItem = ({ data }: LatestPaperItemProperties) => {
   const { mainContent, listItem, link } = useStyles()
-  const authors = getAuthorsString(data.authors)
+  const authors = getAuthorsCitationString(data.authors, { limit: 3 })
   const date = getPublicationYear(data.publishDate)
   const title = formatTitle(data.title).withEllipses
   const { journal, pubmedId } = data
