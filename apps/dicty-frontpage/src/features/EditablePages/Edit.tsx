2628d73e2228a7185954d08d58e3dad948f50009
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 15:44:52 -0600

    feat(Edit.tsx): update useQuery hook to temporarily use a custom GraphQL query with a modified slug variable
    The relative path in the Navigate component was updated to "../addpage" to correctly redirect to the addpage route. The useQuery hook was modified to temporarily use a custom GraphQL query (QUERY) instead of the useContentBySlugQuery hook. The slug variable was also modified to include the NAMESPACE constant for proper querying.

--- apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
@@ -1,4 +1,6 @@
 import { Navigate } from "react-router-dom"
+import { gql, useQuery } from "@apollo/client"
+import { NAMESPACE } from "../../common/constants/namespace"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
 import { match, P } from "ts-pattern"
@@ -8,10 +10,26 @@ import { Loader } from "../../common/components/Loader"
 import { useSlug } from "../../common/hooks/useSlug"
 import { hasNotFoundError } from "../../common/utils/hasNotFoundError"
 
+const QUERY = gql`
+  query contentBySlug($slug: String!) {
+    contentBySlug(slug: $slug) {
+      id
+      content
+      name
+      slug
+      updated_at
+      __typename
+    }
+  }
+`
+
 const Edit = () => {
   const slug = useSlug()
-  const { loading, error, data } = useContentBySlugQuery({
-    variables: { slug },
+  // const { loading, error, data } = useContentBySlugQuery({
+  //   variables: { slug },
+  // })
+  const { loading, error, data } = useQuery(QUERY, {
+    variables: { slug: `${NAMESPACE}-${slug}` },
   })
 
   return match({ loading, error, data })
@@ -22,7 +40,7 @@ const Edit = () => {
     )
     .when(
       ({ error: error_ }) => pipe(error_, hasNotFoundError),
-      () => <Navigate to="addpage" replace />,
+      () => <Navigate to="../addpage" replace relative="path" />,
     )
     .with({ error: P.select(P.not(undefined)) }, (error_) => (
       <GraphQLErrorPage error={error_} />
