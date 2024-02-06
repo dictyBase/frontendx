8d5ffa32439aa353952a5e75abf1aec5a63b6486
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 12:48:20 -0600

    refactor(citation.ts): refactor multiple functions, add limit option to getAuthorsCitationString
    - Rename the function `getAuthorsString` to `getAuthorsCitationString` to better reflect its purpose of generating the citation string for authors.
    - Extract the logic for formatting the full author list into a separate function called `formatFullAuthorList` to improve readability and maintainability.
    - Add an optional `limit` parameter to the `getAuthorsCitationString` function to limit the number of authors displayed in the citation.
    - Update the `getAuthorsCitationString` function to handle cases where the number of authors exceeds the specified limit.
    - Update the `createCitation` function to use the renamed `getAuthorsCitationString` function.
    - Remove the unused import statements.
    
    The changes were made to improve the readability and maintainability of the code. The function and variable names were updated to better reflect their purpose, and the logic for formatting the author list was extracted into a separate function for reusability. The addition of the `limit` parameter allows for more flexibility in controlling the number of authors displayed in the citation.

--- apps/dicty-frontpage/src/common/utils/citation.ts
+++ apps/dicty-frontpage/src/common/utils/citation.ts
@@ -1,16 +1,56 @@
 import { pipe } from "fp-ts/function"
-import { map as Amap, last, compact } from "fp-ts/Array"
-import { bindTo, let as Ilet } from "fp-ts/Identity"
+import { getOrElse as OgetOrElse } from "fp-ts/Option"
+import {
+  map as Amap,
+  last,
+  compact as Acompact,
+  intersperse as Aintersperse,
+  foldLeft as AfoldLeft,
+  foldMap as AfoldMap,
+  reduce as Areduce,
+  splitAt as AsplitAt,
+  updateAt as AupdateAt,
+  zero as Azero,
+} from "fp-ts/Array"
+import {
+  type NonEmptyArray,
+  splitAt as NEAsplitAt,
+  last as NEAlast,
+  init as NEAinit,
+  intersperse as NEAintersperse,
+} from "fp-ts/NonEmptyArray"
+import { bindTo as IbindTo, let as Ilet } from "fp-ts/Identity"
+import { Do as ODo, bind as Obind, let as Olet } from "fp-ts/Option"
 import { slice, trimRight } from "fp-ts/string"
 import { match } from "ts-pattern"
 import { type PublicationItem } from "../hooks/useFetchPublications"
 
-const getAuthorsString = (authors: Array<string>) =>
+const formatFullAuthorList = (lastNames: NonEmptyArray<string>) =>
   pipe(
+    lastNames,
+    IbindTo("all"),
+    Ilet("final", ({ all }) => NEAlast(all)),
+    Ilet("rest", ({ all }) => NEAinit(all)),
+    Ilet("withCommas", ({ rest }) =>
+      pipe(
+        rest,
+        Aintersperse(", "),
+        Areduce("", (b, a) => b + a),
+      ),
+    ),
+    ({ withCommas, final }) => `${withCommas} & ${final}`,
+  )
+
+const getAuthorsCitationString = (
+  authors: Array<string>,
+  options?: { limit: number },
+) => {
+  const { limit } = { limit: 100, ...options }
+  return pipe(
     authors,
     Amap((a) => a.split(" ")),
     Amap(last),
-    compact,
+    Acompact,
     (lastNames) =>
       match(lastNames.length)
         .when(
@@ -22,11 +62,16 @@ const getAuthorsString = (authors: Array<string>) =>
           () => `${lastNames[0]} & ${lastNames[1]}`,
         )
         .when(
-          (l) => l >= 3,
+          (l) => l > 2 && l <= limit,
+          () => formatFullAuthorList(lastNames as NonEmptyArray<string>),
+        )
+        .when(
+          (l) => l > limit,
           () => `${lastNames[0]} et al`,
         )
         .otherwise(() => `Unexpected Error`),
   )
+}
 
 const removeTags = (string: string) => string.replaceAll(/<\/?\w+>/g, "")
 
@@ -40,7 +85,7 @@ const limitCharacters = (text: string, limit: number) =>
 const formatTitle = (title: string) =>
   pipe(
     title,
-    bindTo("initial"),
+    IbindTo("initial"),
     Ilet("full", ({ initial }) => removeTags(initial)),
     Ilet("shortened", ({ full }) => pipe(full, slice(0, 90), trimRight)),
     Ilet("withEllipses", ({ shortened, full }) =>
@@ -54,13 +99,13 @@ const createCitation = ({
   title,
   journal,
 }: PublicationItem) =>
-  `${getAuthorsString(authors)}. (${getPublicationYear(
+  `${getAuthorsCitationString(authors)}. (${getPublicationYear(
     publishDate,
   )}). "${limitCharacters(removeTags(title), 90)}." ${journal}`
 
 export {
   createCitation,
-  getAuthorsString,
+  getAuthorsCitationString,
   removeTags,
   getPublicationYear,
   limitCharacters,
