d888d5f55ce046e445433cf1a9270227f5988c18
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-06 12:50:08 -0600

    refactor(LatestPapers.tsx): add error handling to LatestPapers component
    The LatestPapers component now includes error handling to display an error message when there is an error fetching the publications. This improves the user experience by providing feedback in case of any issues with the data retrieval.

--- apps/dicty-frontpage/src/features/Frontpage/LatestPapers.tsx
+++ apps/dicty-frontpage/src/features/Frontpage/LatestPapers.tsx
@@ -1,19 +1,22 @@
 import { match } from "ts-pattern"
 import { useFetchPublications } from "../../common/hooks/useFetchPublications"
-import { LatestPapersView, LatestPapersLoader } from "./LatestPapersView"
+import {
+  LatestPapersView,
+  LatestPapersLoader,
+  LatestPapersError,
+} from "./LatestPapersView"
 
 const LatestPapers = () => {
   const fetchState = useFetchPublications(import.meta.env.VITE_RSS_URL)
-
   return match(fetchState)
     .with({ loading: true }, () => <LatestPapersLoader />)
     .when(
-      ({ error }) => error.length > 0,
-      ({ error }) => <>{error}</>,
-    )
-    .when(
       ({ data }) => data.length > 0,
       ({ data }) => <LatestPapersView data={data} />,
+    )
+    .when(
+      ({ error }) => error.length > 0,
+      ({ error }) => <LatestPapersError error={error} />,
     )
     .otherwise(() => <> This message should not appear. </>)
 }
