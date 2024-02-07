8cdfa68442c57c45047378ba42e16efa3a834952
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-07 15:42:47 -0600

    refactor(NotFoundError.tsx): Clean up and improve readability of NotFoundError component
    The NotFoundError component has been refactored to improve code readability and maintainability. The following changes were made:
    - Remove deprecated code related to authentication and authorization.
    - Removed unused imports and variables
    - Replaced div elements with the Container component from Material-UI
    - Replaced h3 and p elements with Typography components from Material-UI
    - Removed unnecessary className attributes
    - Removed unused code related to editing pages
    These changes make the code cleaner and easier to understand.

--- apps/dicty-frontpage/src/common/components/errors/NotFoundError.tsx
+++ apps/dicty-frontpage/src/common/components/errors/NotFoundError.tsx
@@ -1,73 +1,38 @@
-import { Link, useLocation, useParams } from "react-router-dom"
-import Grid from "@material-ui/core/Grid"
-import Button from "@material-ui/core/Button"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Grid, Typography, Container } from "@material-ui/core"
 import { BackToHomepageButton } from "../BackToHomepageButton"
-import { useAuthorization } from "../../hooks/useAuthorization"
 import sadDicty from "../../assets/sad-dicty.png"
 import { useStyles } from "./errorStyles"
 
-type Properties = {
+type NotFoundErrorProperties = {
   /** Error message to display */
   error: string
 }
-
-type Parameters_ = {
-  /** Name param in URL */
-  name: string
-  /** Subname param in URL */
-  subname: string
-}
-
 /**
  * UI display when an item was not found.
  */
 
 // While not currently used in the component, sometimes NotFoundError is invoked by other components that provide it an error prop, so the parameter will not be removed for now.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const NotFoundError = ({ error }: Properties) => {
-  const { name, subname } = useParams<Parameters_>()
-  const location = useLocation()
-  const { canEditPages, verifiedToken } = useAuthorization()
+const NotFoundError = ({ error }: NotFoundErrorProperties) => {
   const classes = useStyles()
 
   return (
     <Grid container className={classes.mainGrid} justifyContent="center">
       <Grid item xs={10} md={8}>
-        <div className={classes.error400}>
+        <Container className={classes.error400}>
           <img src={sadDicty} alt="Sad Dicty -- Page Not Found" />
-          <h3>Page Not Found</h3>
-          <p className={classes.paragraph}>
+          <Typography variant="h2" gutterBottom>
+            Page Not Found
+          </Typography>
+          <Typography className={classes.paragraph}>
             Sorry! We can&apos;t find that page.
-          </p>
-          <p className={classes.paragraph}>
+          </Typography>
+          <Typography className={classes.paragraph}>
             You can try one of the links in our navbar above, or head back to
             the homepage.
-          </p>
+          </Typography>
           <BackToHomepageButton />
-          {canEditPages && verifiedToken && (
-            <div>
-              <br />
-              <Link
-                className={classes.link}
-                to="/addpage"
-                state={{
-                  name,
-                  subname,
-                  url: location.pathname,
-                }}>
-                <Button
-                  className={classes.addPageButton}
-                  size="small"
-                  variant="contained"
-                  color="primary">
-                  <FontAwesomeIcon icon="plus" />
-                  &nbsp; Add a page to this route
-                </Button>
-              </Link>
-            </div>
-          )}
-        </div>
+        </Container>
       </Grid>
     </Grid>
   )
