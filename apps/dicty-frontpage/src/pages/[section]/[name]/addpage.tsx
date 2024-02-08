e7eb534473a7b0d93e6cc416f3c868d39e0cda84
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-07 18:27:58 -0600

    refactor(addpage.tsx): rename imported component from AddPageView to AddPage for consistency
    The imported component AddPageView has been renamed to AddPage in both files for consistency and clarity. This improves the readability of the code and ensures that the component name accurately reflects its purpose.

--- apps/dicty-frontpage/src/pages/[section]/[name]/addpage.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/addpage.tsx
@@ -1,6 +1,6 @@
-import { AddPageView } from "../../../features/EditablePages/AddPageView"
+import { AddPage } from "../../../features/EditablePages/AddPage"
 import { ACCESS } from "../../../app/routes/types"
 
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default AddPageView
+export default AddPage
 export const access = ACCESS.private
