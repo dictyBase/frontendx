853e7cf21b6b409f5c8b803712978e805d1bbd98
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-08 01:54:17 -0600

    feat(about): add new pages for about section
    Three new files have been added to the about section of the frontpage application:
    - `addpage.tsx`: This file exports the `AddPage` component from the `EditablePages` feature and sets the access level to `private`.
    - `notfound.tsx`: This file exports the `NotFoundError` component from the `common/components/errors` directory and sets the access level to `public`.
    - `notfoundauth.tsx`: This file exports the `NotFoundErrorAuth` component from the `common/components/errors` directory and sets the access level to `public`.
    
    These new pages have been added to enhance the functionality and user experience of the about section in the frontpage application.

---
+++ apps/dicty-frontpage/src/pages/about/addpage.tsx
@@ -0,0 +1,6 @@
+import { AddPage } from "../../features/EditablePages/AddPage"
+import { ACCESS } from "../../app/routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default AddPage
+export const access = ACCESS.private
---
+++ apps/dicty-frontpage/src/pages/about/notfound.tsx
@@ -0,0 +1,6 @@
+import { NotFoundError } from "../../common/components/errors/NotFoundError"
+import { ACCESS } from "../../app/routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NotFoundError
+export const access = ACCESS.public
