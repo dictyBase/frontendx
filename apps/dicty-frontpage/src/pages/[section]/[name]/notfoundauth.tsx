eadd78ed30635e2031a187eff98670cb1b49499f
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-07 18:25:26 -0600

    feat(notfoundauth.tsx): add NotFoundErrorAuth component and access constant to handle missing data in specified private routes
    The NotFoundErrorAuth component is added to handle missing data in specified private routes. The access constant is also added to define the access level for these routes, which is set to "public". This change improves the error handling and authentication flow in the application.

---
+++ apps/dicty-frontpage/src/pages/[section]/[name]/notfoundauth.tsx
@@ -0,0 +1,6 @@
+import { NotFoundErrorAuth } from "../../../common/components/errors/NotFoundErrorAuth"
+import { ACCESS } from "../../../app/routes/types"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NotFoundErrorAuth
+export const access = ACCESS.public
