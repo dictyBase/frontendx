c9dcc55873f314b2096bca514fc6fb74f637c371
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-02-09 15:23:05 -0600

    fix(UnAuthorized.tsx): change import path for ErrorIcon from @mui/icons-material/Error to @material-ui/icons/Error
    The import path for ErrorIcon was incorrect, causing the application to
    fail when trying to render the ErrorIcon. The correct import path from
    the Material-UI library is now used, ensuring the ErrorIcon is properly
    displayed.

--- packages/auth/src/UnAuthorized.tsx
+++ packages/auth/src/UnAuthorized.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
 import Button from "@material-ui/core/Button"
-import ErrorIcon from "@mui/icons-material/Error"
+import ErrorIcon from "@material-ui/icons/Error"
 import sadDicty from "./assets/sad-dicty.png"
 
 const useStyles = makeStyles((theme) => ({
