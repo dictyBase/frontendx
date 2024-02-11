d29dfdbe6c88ec838677d40319050b6b16ac95c9
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-11 03:02:30 -0600

    chore(about.tsx): remove unused About page component and ACCESS constant
    The About page component and the ACCESS constant are no longer used in the application and have been removed to improve code cleanliness and reduce unnecessary code.

--- apps/dicty-frontpage/src/pages/about.tsx
+++
@@ -1,6 +0,0 @@
-import { About } from "../features/About/About"
-import { ACCESS } from "../app/routes/types"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default About
-export const access = ACCESS.public
