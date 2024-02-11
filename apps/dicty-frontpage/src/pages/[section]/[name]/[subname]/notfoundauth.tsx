698b7f2a3fcd1a82b4692d180539a450c242ea6e
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-11 03:03:54 -0600

    feat(frontpage): add "content-admin" role to access control for addpage, edit, editable, and notfoundauth pages
    The "content-admin" role is added to the access control for the following pages: addpage, edit, editable, and notfoundauth. This allows users with the "content-admin" role to access and perform actions on these pages.

--- apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/notfoundauth.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/notfoundauth.tsx
@@ -4,3 +4,4 @@ import { ACCESS } from "../../../../app/routes/types"
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
 export default NotFoundErrorAuth
 export const access = ACCESS.public
+export const roles = ["content-admin"]
--- apps/dicty-frontpage/src/pages/[section]/[name]/addpage.tsx
+++ apps/dicty-frontpage/src/pages/[section]/[name]/addpage.tsx
@@ -4,3 +4,4 @@ import { ACCESS } from "../../../app/routes/types"
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
 export default AddPage
 export const access = ACCESS.private
+export const roles = ["content-admin"]
