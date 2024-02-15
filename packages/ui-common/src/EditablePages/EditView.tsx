aa462597be36d030c87748e5a574da909a6b366e
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 10:32:17 -0600

    feat(EditView.tsx): add functionality to update content using useUpdateContentMutation hook
    The EditView component now uses the useUpdateContentMutation hook from the dicty-graphql-schema package to update the content. The updateContent function is called when the onSave function is triggered, passing the updated content and other necessary variables. This allows the user to save changes made in the editor and update the content in the database.

--- packages/ui-common/src/EditablePages/EditView.tsx
+++ packages/ui-common/src/EditablePages/EditView.tsx
@@ -1,17 +1,42 @@
 import { Container } from "@material-ui/core"
 import { useNavigate } from "react-router-dom"
-import { type ContentBySlugQuery } from "dicty-graphql-schema"
+import {
+  type ContentBySlugQuery,
+  useUpdateContentMutation,
+} from "dicty-graphql-schema"
 import { Editor } from "editor"
 
 type ContentViewProperties = {
   data: NonNullable<ContentBySlugQuery["contentBySlug"]>
+  userId: string
+  token: string
 }
 
-const EditView = ({ data }: ContentViewProperties) => {
+const EditView = ({ data, userId, token }: ContentViewProperties) => {
+  const [updateContent] = useUpdateContentMutation({
+    context: {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  })
   const navigate = useNavigate()
-  const onSave = async () => {
-    // update content mutation
-    navigate("../editable", { relative: "path" })
+  const onSave = async (value: any) => {
+    try {
+      await updateContent({
+        variables: {
+          input: {
+            id: data.id,
+            // eslint-disable-next-line camelcase
+            updated_by: userId,
+            content: value,
+          },
+        },
+      })
+      // navigate("../editable", { relative: "path" })
+    } catch (error) {
+      console.error(error)
+    }
   }
   const onCancel = () => {
     navigate("../editable", { relative: "path" })
