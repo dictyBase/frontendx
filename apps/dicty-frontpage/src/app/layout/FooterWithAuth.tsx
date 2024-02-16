e804ada22e5599d6ef8a03dd89f77d001c03c03f
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 20:39:16 -0600

    refactor(FooterWithAuth.tsx): remove unnecessary isLoading variable and match statement
    The isLoading variable and the match statement are no longer needed as the useAuthorization hook now directly returns the isAuthorized value. This simplifies the code and improves readability.

--- apps/dicty-frontpage/src/app/layout/FooterWithAuth.tsx
+++ apps/dicty-frontpage/src/app/layout/FooterWithAuth.tsx
@@ -1,24 +1,17 @@
 import { Footer } from "@dictybase/footer"
-import { match } from "ts-pattern"
 import { displayOnAuthorized, useAuthorization, authFooterData } from "auth"
-import { Loader } from "../../common/components/Loader"
 
 const authorizedRoles = ["content-admin"]
 
 const FooterWithAuth = () => {
-  const { isAuthorized, isLoading } = useAuthorization({
+  const { isAuthorized } = useAuthorization({
     entries: authorizedRoles,
   })
-  return match(isLoading)
-    .with(true, () => <Loader />)
-    .with(false, () =>
-      displayOnAuthorized({
-        isAuthorized,
-        authorized: <Footer data={authFooterData} />,
-        unauthorized: <Footer />,
-      }),
-    )
-    .exhaustive()
+  return displayOnAuthorized({
+    isAuthorized,
+    authorized: <Footer data={authFooterData} />,
+    unauthorized: <Footer />,
+  })
 }
 
 export { FooterWithAuth }
