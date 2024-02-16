ae63fdf476a540598698bbdee20d96627071966e
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 19:31:18 -0600

    refactor(Editable.tsx): replace Loader component with EditableContentLoadingDisplay component for consistency and better user experience
    The Editable.tsx file has been refactored to import the EditableContentLoadingDisplay component from the "@dictybase/ui-common" library. This component is now used instead of the previously imported Loader component for displaying a loading state. Additionally, some unused imports have been removed to clean up the code. This change improves the consistency and user experience of the Editable feature.

--- apps/dicty-frontpage/src/features/EditablePages/Editable.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Editable.tsx
@@ -2,10 +2,12 @@ import { Navigate } from "react-router-dom"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
 import { match, P } from "ts-pattern"
-import { EditableView } from "@dictybase/ui-common"
+import {
+  EditableView,
+  EditableContentLoadingDisplay,
+} from "@dictybase/ui-common"
 import { NAMESPACE } from "../../common/constants/namespace"
 import { GraphQLErrorPage } from "../../common/components/errors/GraphQLErrorPage"
-import { Loader } from "../../common/components/Loader"
 import { useSlug } from "../../common/hooks/useSlug"
 import { hasNotFoundError } from "../../common/utils/hasNotFoundError"
 
@@ -13,11 +15,10 @@ const Editable = () => {
   const slug = useSlug()
   const result = useContentBySlugQuery({
     variables: { slug: `${NAMESPACE}-${slug}` },
-    fetchPolicy: "cache-and-network",
     errorPolicy: "all",
   })
   return match(result)
-    .with({ loading: true }, () => <Loader />)
+    .with({ loading: true }, () => <EditableContentLoadingDisplay />)
     .with(
       { data: { contentBySlug: P.select({ content: P.string }) } },
       (content) => <EditableView data={content} />,
