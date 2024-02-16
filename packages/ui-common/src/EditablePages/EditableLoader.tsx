17549e699aa0211fa5ff0249bf08f3b203ebce47
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 19:17:46 -0600

    chore(EditableLoader.tsx): add TSDoc comment to describe the purpose of the EditableLoader component
    The EditableLoader component now has a TSDoc comment that provides a brief description of its purpose. This improves code documentation and makes it easier for other developers to understand the intended use of the component.

--- packages/ui-common/src/EditablePages/EditableLoader.tsx
+++ packages/ui-common/src/EditablePages/EditableLoader.tsx
@@ -1,6 +1,9 @@
 import { Container } from "@material-ui/core"
 import { LoadingDisplay } from "../LoadingDisplay"
 
+/**
+ * Loading component intended for use in editable content pages.
+ */
 const EditableLoader = () => (
   <Container>
     <LoadingDisplay rows={1} height={100} />
