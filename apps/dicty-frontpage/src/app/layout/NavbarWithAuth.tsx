616a810300c4697c4d028cc91f8f58db338d86e6
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 20:46:33 -0600

    refactor(NavbarWithAuth.tsx): remove unnecessary isLoading variable and match statement
    The isLoading variable and the match statement are no longer needed as the useAuthorization hook no longer returns an isLoading property. The code has been simplified to directly use the isAuthorized property to determine whether to display the authorized or unauthorized Navbar component.

--- apps/dicty-frontpage/src/app/layout/NavbarWithAuth.tsx
+++ apps/dicty-frontpage/src/app/layout/NavbarWithAuth.tsx
@@ -1,28 +1,21 @@
 import { Navbar, formatNavbarData } from "@dictybase/navbar"
-import { match } from "ts-pattern"
 import { displayOnAuthorized, useAuthorization, authNavbarData } from "auth"
-import { Loader } from "../../common/components/Loader"
 import { navTheme } from "../../common/utils/themes"
 
 const authorizedRoles = ["content-admin"]
 
 const NavbarWithAuth = () => {
-  const { isAuthorized, isLoading } = useAuthorization({
+  const { isAuthorized } = useAuthorization({
     entries: authorizedRoles,
   })
 
-  return match(isLoading)
-    .with(true, () => <Loader />)
-    .with(false, () =>
-      displayOnAuthorized({
-        isAuthorized,
-        authorized: (
-          <Navbar items={formatNavbarData(authNavbarData)} theme={navTheme} />
-        ),
-        unauthorized: <Navbar theme={navTheme} />,
-      }),
-    )
-    .exhaustive()
+  return displayOnAuthorized({
+    isAuthorized,
+    authorized: (
+      <Navbar items={formatNavbarData(authNavbarData)} theme={navTheme} />
+    ),
+    unauthorized: <Navbar theme={navTheme} />,
+  })
 }
 
 export { NavbarWithAuth }
