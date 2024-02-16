6e4ad56910ddff982ffe6c34d742633cabfdfc61
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-15 20:30:53 -0600

    feat(Edit.tsx): replace Loader component with EditableContentLoadingDisplay component from @dictybase/ui-common package
    The code has been refactored to remove unused variables and imports, making the code cleaner and more maintainable. The Loader component has been replaced with the EditableContentLoadingDisplay component from the @dictybase/ui-common package, which provides a more consistent and standardized loading display for editable content.
    Closes #648

--- apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
@@ -2,30 +2,20 @@ import { Navigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { type UserInfoResponse, useLogto } from "@logto/react"
-import { EditView } from "@dictybase/ui-common"
+import { EditView, EditableContentLoadingDisplay } from "@dictybase/ui-common"
 import { match, P } from "ts-pattern"
 import { NAMESPACE } from "../../common/constants/namespace"
 import { GraphQLErrorPage } from "../../common/components/errors/GraphQLErrorPage"
-import { Loader } from "../../common/components/Loader"
 import { useSlug } from "../../common/hooks/useSlug"
 import { hasNotFoundError } from "../../common/utils/hasNotFoundError"
 
 const Edit = () => {
   const slug = useSlug()
-  const {
-    loading: gqlLoading,
-    data,
-    error,
-  } = useContentBySlugQuery({
+  const { loading, data, error } = useContentBySlugQuery({
     variables: { slug: `${NAMESPACE}-${slug}` },
     errorPolicy: "all",
   })
-  const {
-    fetchUserInfo,
-    getAccessToken,
-    isAuthenticated,
-    isLoading: logToLoading,
-  } = useLogto()
+  const { fetchUserInfo, getAccessToken, isAuthenticated } = useLogto()
   const [token, setToken] = useState<string>()
   const [user, setUser] = useState<UserInfoResponse>()
   useEffect(() => {
@@ -47,9 +37,9 @@ const Edit = () => {
     error,
     token,
     user,
-    loading: gqlLoading || logToLoading,
+    loading,
   })
-    .with({ loading: true }, () => <Loader />)
+    .with({ loading: true }, () => <EditableContentLoadingDisplay />)
     .with(
       { data: { contentBySlug: P.select({ content: P.string }) } },
       (content) => (
