98778a86b49037b81cfb9eb06f8e8b038603e490
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-19 02:15:52 -0600

    feat(ui-common): add EditContentToolbar component for displaying edit information
    The new EditContentToolbar component is added to the ui-common package. This component is responsible for displaying information about the last update of content, the user who updated it, and providing an edit button for users to modify the content.

---
+++ packages/ui-common/src/EditablePages/EditContentToolbar.tsx
@@ -0,0 +1,86 @@
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { Box, Tooltip, IconButton, Grid } from "@material-ui/core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { ContentBySlugQuery } from "dicty-graphql-schema"
+import { timeSince } from "../timeSince"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  content: {
+    marginLeft: "auto",
+  },
+  toolbar: {
+    backgroundColor: "#fafafa",
+    borderRadius: "2px",
+    border: "1px solid #ddd",
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(2),
+    width: "100%",
+    alignItems: "center",
+  },
+  label: {
+    marginLeft: "auto",
+    marginRight: theme.spacing(1),
+    padding: theme.spacing(1),
+    fontSize: "0.8rem",
+    fontWeight: "bold",
+    lineHeight: 1,
+    color: "#fff",
+    whiteSpace: "nowrap",
+    borderRadius: "0.25em",
+    backgroundColor: theme.palette.primary.light,
+  },
+  icon: {
+    color: theme.palette.primary.light,
+    fontSize: "1rem",
+    marginRight: theme.spacing(0.5),
+  },
+  text: {
+    color: theme.palette.primary.light,
+  },
+}))
+
+type Properties = {
+  /** Timestamp for when this content was last updated */
+  lastUpdate: string
+  /** User object for who last updated this content */
+  user: NonNullable<ContentBySlugQuery["contentBySlug"]>["updated_by"]
+  /** Function to execute when user clicks edit icon */
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+/** Displays the info page data that was fetched from the InfoPageContainer component */
+
+const EditContentToolbar = ({ handleClick, lastUpdate, user }: Properties) => {
+  const classes = useStyles()
+
+  const fullName = `${user.first_name} ${user.last_name}`
+
+  return (
+    <Box mt={2}>
+      <Grid
+        container
+        justifyContent="space-between"
+        className={classes.toolbar}
+        data-testid="info-page-toolbar">
+        <Grid item>
+          <Box component="span" className={classes.text}>
+            <strong>
+              <FontAwesomeIcon className={classes.icon} icon="user" />{" "}
+              {fullName}
+            </strong>{" "}
+            edited {timeSince(lastUpdate)} ago
+          </Box>
+        </Grid>
+        <Grid item>
+          <Tooltip title="Edit Page" placement="bottom">
+            <IconButton className={classes.icon} onClick={handleClick}>
+              <FontAwesomeIcon icon="pencil-alt" />
+            </IconButton>
+          </Tooltip>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+export { EditContentToolbar }
