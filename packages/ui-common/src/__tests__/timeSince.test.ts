09a6b7899fb45d8aea05ff0561bcb1f19123e7ad
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-19 02:15:22 -0600

    refactor(timeSince.test.ts): update imports in test file for better organization and readability
    The imports in the test file have been updated to include additional functions from the "vitest" library and adjust the path for the "timeSince" import for better organization and readability of the test file.

--- packages/ui-common/src/__tests__/timeSince.test.ts
+++ packages/ui-common/src/__tests__/timeSince.test.ts
@@ -1,5 +1,5 @@
-import { vi } from "vitest"
-import { timeSince } from "./timeSince"
+import { vi, test, expect } from "vitest"
+import { timeSince } from "../timeSince"
 
 describe("common/utils/timeSince", () => {
   const currentDate = new Date("2020-01-02T11:01:58.135Z")
@@ -13,22 +13,22 @@ describe("common/utils/timeSince", () => {
     vi.useRealTimers()
   })
 
-  it("should return years", () => {
+  test("should return years", () => {
     expect(timeSince("2018-01-01")).toEqual("2 years")
   })
-  it("should return months", () => {
+  test("should return months", () => {
     expect(timeSince("2019-11-01")).toEqual("2 months")
   })
-  it("should return days", () => {
+  test("should return days", () => {
     expect(timeSince("2019-12-30")).toEqual("3 days")
   })
-  it("should return hours", () => {
+  test("should return hours", () => {
     expect(timeSince("2020-01-02T09:01:58.135Z")).toEqual("2 hours")
   })
-  it("should return minutes", () => {
+  test("should return minutes", () => {
     expect(timeSince("2020-01-02T10:59:48.135Z")).toEqual("2 minutes")
   })
-  it("should return seconds", () => {
+  test("should return seconds", () => {
     expect(timeSince("2020-01-02T11:01:48.135Z")).toEqual("10 seconds")
   })
 })
