80fda17f9629b082b850a8f988fb26b821330196
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-18 23:00:51 -0600

    chore(frontpage): remove unused callback, login, and logout pages
    The callback.tsx, login.tsx, and logout.tsx files were removed as they were no longer being used in the application. This cleanup helps to declutter the codebase and improve maintainability by removing unnecessary files.

--- apps/dicty-frontpage/src/pages/[provider]/callback.tsx
+++
@@ -1,6 +0,0 @@
-import { OauthCallback } from "../../features/Authentication/OauthCallback"
-import { ACCESS } from "../../app/routes/types"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default OauthCallback
-export const access = ACCESS.public
--- apps/dicty-frontpage/src/pages/login.tsx
+++
@@ -1,6 +0,0 @@
-import { Login } from "../features/Authentication/Login"
-import { ACCESS } from "../app/routes/types"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default Login
-export const access = ACCESS.public
