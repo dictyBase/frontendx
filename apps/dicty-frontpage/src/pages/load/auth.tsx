93dbdde4b0ce06647c25a7fd4c5ec8511aedc898
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-18 23:22:33 -0600

    chore(auth.tsx): remove /load/auth page
    The /load/auth page was not being used in the application, so it was removed to declutter the codebase and improve maintainability.

--- apps/dicty-frontpage/src/pages/load/auth.tsx
+++
@@ -1,6 +0,0 @@
-import { AuthLoader } from "../../features/Authentication/AuthLoader"
-import { ACCESS } from "../../app/routes/types"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default AuthLoader
-export const access = ACCESS.public
