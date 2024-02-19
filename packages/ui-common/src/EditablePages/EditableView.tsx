baa5ce9fc11259cd622f3574476a5fe884462db4
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-19 02:20:29 -0600

    refactor(EditableView.tsx): remove unused imports and handleClick function
    The useNavigate import and handleClick function were removed as they were no longer being used in the component. This cleanup improves code readability and maintainability by eliminating unnecessary code.

--- packages/ui-common/src/EditablePages/EditableView.tsx
+++ packages/ui-common/src/EditablePages/EditableView.tsx
@@ -2,7 +2,6 @@
 import { Container } from "@material-ui/core"
 import { type ContentBySlugQuery } from "dicty-graphql-schema"
 import { Editor } from "editor"
-import { useNavigate } from "react-router-dom"
 import { EditContentToolbar } from "./EditContentToolbar"
 
 type EditableViewProperties = {
@@ -10,18 +9,11 @@ type EditableViewProperties = {
 }
 
 const EditableView = ({ data }: EditableViewProperties) => {
-  const navigate = useNavigate()
   const { updated_at, updated_by, content, slug } = data
-  const handleClick = () => {
-    navigate("../edit", { relative: "path" })
-  }
+
   return (
     <Container>
-      <EditContentToolbar
-        user={updated_by}
-        lastUpdate={updated_at}
-        handleClick={handleClick}
-      />
+      <EditContentToolbar user={updated_by} lastUpdate={updated_at} />
       <Editor
         editable={false}
         content={{ storageKey: slug, editorState: content }}
