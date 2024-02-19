5420551d718dc81c7fa596328a2ec08016ded283
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-19 02:16:08 -0600

    refactor(EditableView.tsx): refactor EditableView component to include a new EditContentToolbar component and use react-router-dom for navigation
    The EditableView component has been refactored to include a new EditContentToolbar component for displaying user and last update information, as well as a navigation functionality using react-router-dom's useNavigate hook. This improves the user experience by providing additional functionality and enhancing the overall design of the EditableView component.

--- packages/ui-common/src/EditablePages/EditableView.tsx
+++ packages/ui-common/src/EditablePages/EditableView.tsx
@@ -1,20 +1,33 @@
+/* eslint-disable camelcase */
 import { Container } from "@material-ui/core"
 import { type ContentBySlugQuery } from "dicty-graphql-schema"
 import { Editor } from "editor"
-import { EditButton } from "./EditButton"
+import { useNavigate } from "react-router-dom"
+import { EditContentToolbar } from "./EditContentToolbar"
 
 type EditableViewProperties = {
   data: NonNullable<ContentBySlugQuery["contentBySlug"]>
 }
 
-const EditableView = ({ data }: EditableViewProperties) => (
-  <Container>
-    <Editor
-      editable={false}
-      content={{ storageKey: data.slug, editorState: data.content }}
-    />
-    <EditButton />
-  </Container>
-)
+const EditableView = ({ data }: EditableViewProperties) => {
+  const navigate = useNavigate()
+  const { updated_at, updated_by, content, slug } = data
+  const handleClick = () => {
+    navigate("../edit", { relative: "path" })
+  }
+  return (
+    <Container>
+      <EditContentToolbar
+        user={updated_by}
+        lastUpdate={updated_at}
+        handleClick={handleClick}
+      />
+      <Editor
+        editable={false}
+        content={{ storageKey: slug, editorState: content }}
+      />
+    </Container>
+  )
+}
 
 export { EditableView }
