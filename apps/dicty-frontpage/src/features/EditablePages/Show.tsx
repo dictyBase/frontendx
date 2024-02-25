656d6df3b9623be879c81ebdb69c4093a06e8d22
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-25 00:17:16 -0600

    refactor(Show.tsx): remove unused import and update error handling logic in Show component
    The import statement for the GraphQLErrorPage component from "@dictybase/ui-common" is removed as it is no longer used in the Show component. The error handling logic is updated to use the contentPageErrorMatcher utility function instead of the hasNotFoundError function. This improves the readability and maintainability of the code.

--- apps/dicty-frontpage/src/features/EditablePages/Show.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Show.tsx
@@ -1,14 +1,10 @@
 import { Navigate } from "react-router-dom"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
-import {
-  ContentView,
-  FullPageLoadingDisplay,
-  GraphQLErrorPage,
-} from "@dictybase/ui-common"
+import { ContentView, FullPageLoadingDisplay } from "@dictybase/ui-common"
 import { NAMESPACE } from "../../common/constants/namespace"
 import { useSlug } from "../../common/hooks/useSlug"
-import { hasNotFoundError } from "../../common/utils/hasNotFoundError"
+import { contentPageErrorMatcher } from "../../common/utils/contentPageErrorMatcher"
 
 const Show = () => {
   const slug = useSlug()
@@ -22,12 +18,10 @@ const Show = () => {
       (content) => <ContentView data={content} />,
     )
     .with({ loading: true }, () => <FullPageLoadingDisplay />)
-    .with({ error: P.select(P.not(undefined)) }, (error) => (
-      <GraphQLErrorPage error={error} />
-    ))
-    .when(
-      ({ error }) => hasNotFoundError(error),
-      () => <Navigate to="../notfound" replace relative="path" />,
+    .with({ error: P.select(P.not(undefined)) }, (error) =>
+      contentPageErrorMatcher(error, () => (
+        <Navigate to="../notfound" replace relative="path" />
+      )),
     )
     .otherwise(() => <> This message should not appear. </>)
 }
