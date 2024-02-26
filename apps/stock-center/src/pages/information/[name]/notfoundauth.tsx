4a647010becb6f33cd1820b2602ebc9bde9b7514
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-26 08:52:04 -0600

    fix(notfoundauth.tsx): change import and export names for NotFoundError to NotFoundErrorAuth
    The import and export names for NotFoundError have been updated to NotFoundErrorAuth to provide more clarity and consistency in the codebase.

--- apps/stock-center/src/pages/information/[name]/notfoundauth.tsx
+++ apps/stock-center/src/pages/information/[name]/notfoundauth.tsx
@@ -1,6 +1,6 @@
-import { NotFoundError } from "@dictybase/ui-common"
+import { NotFoundErrorAuth } from "@dictybase/ui-common"
 import { ACCESS } from "auth"
 // eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default NotFoundError
+export default NotFoundErrorAuth
 export const access = ACCESS.private
 export const roles = ["content-admin"]
