c017d9e1c49f60d25f3e00a3b8ba123a75d2d52b
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-26 08:53:14 -0600

    feat(addpage.tsx): add a new page component for adding content with dynamic routing
    This commit adds a new file `addpage.tsx` which contains a React component for adding content. The component utilizes dynamic routing with the use of React Router's `Navigate` component to redirect to the editable page after successful data retrieval. The component also handles user authentication and access control based on the user's role.

---
+++ apps/stock-center/src/pages/information/[name]/addpage.tsx
@@ -0,0 +1,67 @@
+import { Navigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { type UserInfoResponse, useLogto } from "@logto/react"
+import { match, P } from "ts-pattern"
+import {
+  AddPageView,
+  FullPageLoadingDisplay,
+  contentPageErrorMatcher,
+} from "@dictybase/ui-common"
+import { ACCESS } from "auth"
+import { NAMESPACE } from "../../../namespace"
+import { useSlug } from "../../../hooks/useSlug"
+import { useContentPath } from "../../../hooks/useContentPath"
+
+const AddPage = () => {
+  const slug = useSlug()
+  const contentPath = useContentPath()
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-${slug}` },
+    errorPolicy: "all",
+    fetchPolicy: "network-only",
+  })
+  const { fetchUserInfo, getAccessToken, isAuthenticated } = useLogto()
+  const [token, setToken] = useState<string>()
+  const [user, setUser] = useState<UserInfoResponse>()
+  useEffect(() => {
+    const getUserData = async () => {
+      if (!isAuthenticated) return
+      setToken(
+        await getAccessToken(
+          import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+        ),
+      )
+      setUser(await fetchUserInfo())
+    }
+
+    getUserData()
+  }, [fetchUserInfo, getAccessToken, isAuthenticated])
+
+  return match({
+    token,
+    user,
+    ...result,
+  })
+    .with({ loading: true }, () => <FullPageLoadingDisplay />)
+    .with({ data: { contentBySlug: P.select({ content: P.string }) } }, () => (
+      <Navigate to="../editable" replace relative="path" />
+    ))
+    .with({ error: P.select(P.not(undefined)) }, (error) =>
+      contentPageErrorMatcher(error, () => (
+        <AddPageView
+          token={token as string}
+          userId={user?.email as string}
+          namespace={NAMESPACE}
+          slug={slug}
+          contentPath={contentPath}
+        />
+      )),
+    )
+    .otherwise(() => <> This message should not appear </>)
+}
+
+// eslint-disable-next-line import/no-default-export
+export default AddPage
+export const access = ACCESS.private
+export const roles = ["content-admin"]
