6f9c9b0bf22764cb995bc1642f083b58c42b539a
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-26 08:08:10 -0600

    feat(notfound.tsx, notfoundauth.tsx): add custom 404 error pages for unauthorized and authorized users
    The new files notfound.tsx and notfoundauth.tsx have been added to provide custom 404 error pages for unauthorized and authorized users, respectively. These pages import NotFoundError component from "@dictybase/ui-common" and set the access level and roles accordingly to display the appropriate message to the user.

---
+++ apps/stock-center/src/pages/information/[name]/notfound.tsx
@@ -0,0 +1,5 @@
+import { NotFoundError } from "@dictybase/ui-common"
+import { ACCESS } from "auth"
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NotFoundError
+export const access = ACCESS.public
---
+++ apps/stock-center/src/pages/information/[name]/notfoundauth.tsx
@@ -0,0 +1,6 @@
+import { NotFoundError } from "@dictybase/ui-common"
+import { ACCESS } from "auth"
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NotFoundError
+export const access = ACCESS.private
+export const roles = ["content-admin"]
