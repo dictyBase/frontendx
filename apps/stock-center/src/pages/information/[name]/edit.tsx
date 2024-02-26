30fbb3fe96ffa4f2dd53306bfa5f36a0b5be7ef1
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-26 08:52:41 -0600

    feat(edit.tsx): add support for fetching user info and access token using Logto
    The Edit component has been refactored to improve readability and maintainability by restructuring the code and organizing it into smaller, more manageable parts. Additionally, support has been added for fetching user information and access token using Logto, enhancing the functionality and security of the application.

--- apps/stock-center/src/pages/information/[name]/edit.tsx
+++ apps/stock-center/src/pages/information/[name]/edit.tsx
@@ -1,59 +1,65 @@
-import Container from "@material-ui/core/Container"
-import { Editor } from "editor"
+import { Navigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { type UserInfoResponse, useLogto } from "@logto/react"
+import {
+  EditView,
+  FullPageLoadingDisplay,
+  contentPageErrorMatcher,
+} from "@dictybase/ui-common"
 import { ACCESS } from "auth"
+import { match, P } from "ts-pattern"
+import { NAMESPACE } from "../../../namespace"
+import { useSlug } from "../../../hooks/useSlug"
 
-const temporaryContent = {
-  root: {
-    children: [
+const Edit = () => {
+  const slug = useSlug()
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-${slug}` },
+    errorPolicy: "all",
+  })
+  const { fetchUserInfo, getAccessToken, isAuthenticated } = useLogto()
+  const [token, setToken] = useState<string>()
+  const [user, setUser] = useState<UserInfoResponse>()
+  useEffect(() => {
+    const getUserData = async () => {
+      if (!isAuthenticated) return
+      setToken(
+        await getAccessToken(
+          import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+        ),
+      )
+      setUser(await fetchUserInfo())
+    }
+
+    getUserData()
+  }, [fetchUserInfo, getAccessToken, isAuthenticated])
+
+  return match({
+    token,
+    user,
+    ...result,
+  })
+    .with(
       {
-        children: [
-          {
-            children: [
-              {
-                detail: 0,
-                format: 2,
-                mode: "normal",
-                style: "font-size: 20px;",
-                text: "Content coming soon!",
-                type: "text",
-                version: 1,
-              },
-            ],
-            direction: "ltr",
-            format: "",
-            indent: 0,
-            type: "paragraph",
-            version: 1,
-          },
-        ],
-        direction: "ltr",
-        format: "",
-        indent: 0,
-        type: "flex-layout",
-        version: 1,
+        data: { contentBySlug: P.select("content", { content: P.string }) },
+        user: { email: P.select("userId", P.string) },
+        token: P.select("authToken", P.string),
       },
-    ],
-    direction: "ltr",
-    format: "",
-    indent: 0,
-    type: "root",
-    version: 1,
-  },
+      ({ content, userId, authToken }) => (
+        <EditView data={content} userId={userId} token={authToken} />
+      ),
+    )
+    .with({ loading: true }, () => <FullPageLoadingDisplay />)
+    .with({ error: P.select(P.not(undefined)) }, (error) =>
+      contentPageErrorMatcher(error, () => (
+        <Navigate to="../notfoundauth" replace relative="path" />
+      )),
+    )
+    .otherwise(() => <> This message should not appear </>)
 }
 
-const InfoPageContainer = () => (
-  <Container>
-    <Editor
-      content={{
-        storageKey: "",
-        editorState: JSON.stringify(temporaryContent),
-      }}
-      editable
-    />
-  </Container>
-)
-
 // eslint-disable-next-line import/no-default-export
-export default InfoPageContainer
+export default Edit
 export const access = ACCESS.private
-export const roles = ["user-user", "content-admin"]
+export const roles = ["content-admin"]
