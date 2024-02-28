fcc50991e403c96f8b81388cf8e4f7b7ac7c6d1e
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 16:24:49 -0600

    feat(index.tsx): add ShowHomepage component for the main homepage of DSC
    The ShowHomepage component is added to serve as the main homepage for the DSC (Stock Center) application. It imports various UI components from the "@dictybase/ui-dsc" and "@dictybase/ui-common" packages to build the homepage layout. The component also uses the "useContentBySlugQuery" hook from the "dicty-graphql-schema" package to fetch content data based on a specific slug. The component handles different scenarios such as loading, error, and displaying the content accordingly. The component is exported as the default export and also exports an "access" constant for public access.

---
+++ apps/stock-center/src/pages/index/index.tsx
@@ -0,0 +1,54 @@
+import Grid from "@material-ui/core/Grid"
+import {
+  useStyles,
+  Title,
+  Heading,
+  Slide,
+  CatalogLinks,
+  FileLinks,
+} from "@dictybase/ui-dsc"
+import { ContentView, LoadingDisplay, OtherError } from "@dictybase/ui-common"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { ACCESS } from "auth"
+import { NAMESPACE } from "../../namespace"
+
+/**
+ * Homepage is the main homepage component for DSC.
+ */
+const ShowHomepage = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-intro` },
+  })
+
+  const classes = useStyles({})
+  return (
+    <>
+      <Title />
+      <Grid container justifyContent="space-between" spacing={3}>
+        <Heading />
+        <Grid item xs={12} className={classes.intro}>
+          {match(result)
+            .with(
+              { data: { contentBySlug: P.select({ content: P.string }) } },
+              (content) => <ContentView data={content} />,
+            )
+            .with({ loading: true }, () => <LoadingDisplay rows={4} />)
+            .with({ error: P.not(undefined) }, () => <OtherError />)
+            .otherwise(() => (
+              <> This message should not appear. </>
+            ))}
+        </Grid>
+        <Grid container item xs={12} spacing={4} justifyContent="flex-start">
+          <Slide />
+          <CatalogLinks />
+          <FileLinks />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default ShowHomepage
+export const access = ACCESS.public
