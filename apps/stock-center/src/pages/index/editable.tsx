223c3d57d1be11974bc4d083e3d7c24ad7a8a099
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 14:14:58 -0600

    feat(editable.tsx): add new editable component for the homepage
    The editable.tsx file is a new component that is added to the index page of the stock-center application. This component is responsible for rendering the main homepage content and provides an editable view for content administration. It fetches the content data by slug using the useContentBySlugQuery hook from the dicty-graphql-schema package. The component also includes various UI elements such as Grid, Title, Heading, Slide, CatalogLinks, and FileLinks from the @dictybase/ui-dsc package. The component handles different scenarios based on the result of the query, including rendering the editable view, displaying a loading indicator, showing an error message, or rendering a fallback message. The component is exported as the default export and has private access with the "content-admin" role.

---
+++ apps/stock-center/src/pages/index/editable.tsx
@@ -0,0 +1,56 @@
+import Grid from "@material-ui/core/Grid"
+import {
+  useStyles,
+  Title,
+  Heading,
+  Slide,
+  CatalogLinks,
+  FileLinks,
+} from "@dictybase/ui-dsc"
+import { EditableView, LoadingDisplay } from "@dictybase/ui-common"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { ACCESS } from "auth"
+import { NAMESPACE } from "../../namespace"
+/**
+ * Homepage is the main homepage component for DSC.
+ */
+const Homepage = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-intro` },
+  })
+
+  const classes = useStyles({})
+  return (
+    <>
+      <Title />
+      <Grid container justifyContent="space-between" spacing={3}>
+        <Heading />
+        <Grid item xs={12} className={classes.intro}>
+          {match(result)
+            .with(
+              { data: { contentBySlug: P.select({ content: P.string }) } },
+              (content) => <EditableView data={content} />,
+            )
+            .with({ loading: true }, () => <LoadingDisplay rows={4} />)
+            .with({ error: P.select(P.not(undefined)) }, (error) => (
+              <>error display</>
+            ))
+            .otherwise(() => (
+              <> This message should not appear. </>
+            ))}
+        </Grid>
+        <Grid container item xs={12} spacing={4} justifyContent="flex-start">
+          <Slide />
+          <CatalogLinks />
+          <FileLinks />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Homepage
+export const access = ACCESS.private
+export const roles = ["content-admin"]
