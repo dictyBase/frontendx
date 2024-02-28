f64eb5374d51944e142220296b8256e1f982ef90
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 11:04:34 -0600

    refactor(HeaderRow.tsx): extract container styles into makeStyles hook for reusability
    The container styles in the HeaderRow component have been extracted into a makeStyles hook called "container" to improve code organization and reusability. This allows for easier customization of the container styles and promotes consistency throughout the application.

--- apps/stock-center/src/components/HeaderRow.tsx
+++ apps/stock-center/src/components/HeaderRow.tsx
@@ -1,21 +1,31 @@
-import Grid from "@material-ui/core/Grid"
+import { Grid, makeStyles } from "@material-ui/core"
 import { CartIcon } from "@dictybase/ui-dsc"
 import { useAtomValue } from "jotai"
 import { Outlet } from "react-router-dom"
 import { Breadcrumbs } from "./Breadcrumbs"
 import { isFullAtom, strainItemsAtom } from "../state"
+
+const useStyles = makeStyles({
+  container: {
+    marginBottom: "1rem",
+  },
+})
 /**
  * This is the cart component that displays between the Navbar and body content on every page.
  * It has a cart icon with the current number of added items next to it.
  */
-
 const HeaderRow = () => {
   const isFull = useAtomValue(isFullAtom)
   const strainItems = useAtomValue(strainItemsAtom)
+  const { container } = useStyles()
 
   return (
     <>
-      <Grid container justifyContent="space-between" alignItems="center">
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        className={container}>
         <Grid item>
           <Breadcrumbs />
         </Grid>
