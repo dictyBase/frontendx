6b10bb91cbca87ac0a870068efc38a494c67d653
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 14:15:49 -0600

    feat(edit.tsx): add new EditHomepage component for editing the homepage content
    The EditHomepage component is added to allow editing of the homepage content in the stock-center application. This component fetches the content by slug and displays it in an EditView component. It also includes additional UI components such as Title, Heading, Slide, CatalogLinks, and FileLinks. The component is only accessible to users with the "content-admin" role.

---
+++ apps/stock-center/src/pages/index/edit.tsx
@@ -0,0 +1,64 @@
+import Grid from "@material-ui/core/Grid"
+import {
+  useStyles,
+  Title,
+  Heading,
+  Slide,
+  CatalogLinks,
+  FileLinks,
+} from "@dictybase/ui-dsc"
+import { EditView, LoadingDisplay } from "@dictybase/ui-common"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { ACCESS, useTokenAndUser } from "auth"
+import { NAMESPACE } from "../../namespace"
+/**
+ * Homepage is the main homepage component for DSC.
+ */
+const EditHomepage = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-intro` },
+  })
+  const { token, user } = useTokenAndUser(
+    import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+  )
+  const classes = useStyles({})
+  return (
+    <>
+      <Title />
+      <Grid container justifyContent="space-between" spacing={3}>
+        <Heading />
+        <Grid item xs={12} className={classes.intro}>
+          {match({ ...result })
+            .with(
+              { data: { contentBySlug: P.select({ content: P.string }) } },
+              (content) => (
+                <EditView
+                  data={content}
+                  userId={user?.email as string}
+                  token={token as string}
+                />
+              ),
+            )
+            .with({ loading: true }, () => <LoadingDisplay rows={4} />)
+            .with({ error: P.select(P.not(undefined)) }, (error) => (
+              <>error display</>
+            ))
+            .otherwise(() => (
+              <> This message should not appear. </>
+            ))}
+        </Grid>
+        <Grid container item xs={12} spacing={4} justifyContent="flex-start">
+          <Slide />
+          <CatalogLinks />
+          <FileLinks />
+        </Grid>
+      </Grid>
+    </>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default EditHomepage
+export const access = ACCESS.private
+export const roles = ["content-admin"]
