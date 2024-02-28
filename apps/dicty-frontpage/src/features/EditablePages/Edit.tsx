91c4a068305dfd7d72c11b93d0112b0e00f7b3af
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 14:16:48 -0600

    refactor(EditablePages/Edit.tsx): refactor code to use the useTokenAndUser hook for fetching token and user data
    The code in Edit.tsx has been refactored to use the useTokenAndUser hook instead of the useState and useEffect hooks to fetch the token and user data. This improves code readability and reduces duplication by encapsulating the logic in a custom hook.

--- apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Edit.tsx
@@ -1,13 +1,12 @@
 import { Navigate } from "react-router-dom"
-import { useState, useEffect } from "react"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
-import { type UserInfoResponse, useLogto } from "@logto/react"
 import {
   EditView,
   FullPageLoadingDisplay,
   contentPageErrorMatcher,
 } from "@dictybase/ui-common"
 import { match, P } from "ts-pattern"
+import { useTokenAndUser } from "auth"
 import { NAMESPACE } from "../../common/constants/namespace"
 import { useSlug } from "../../common/hooks/useSlug"
 
@@ -17,22 +16,9 @@ const Edit = () => {
     variables: { slug: `${NAMESPACE}-${slug}` },
     errorPolicy: "all",
   })
-  const { fetchUserInfo, getAccessToken, isAuthenticated } = useLogto()
-  const [token, setToken] = useState<string>()
-  const [user, setUser] = useState<UserInfoResponse>()
-  useEffect(() => {
-    const getUserData = async () => {
-      if (!isAuthenticated) return
-      setToken(
-        await getAccessToken(
-          import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
-        ),
-      )
-      setUser(await fetchUserInfo())
-    }
-
-    getUserData()
-  }, [fetchUserInfo, getAccessToken, isAuthenticated])
+  const { token, user } = useTokenAndUser(
+    import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+  )
 
   return match({
     token,
