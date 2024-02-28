5e92571cd4469be7898fe3b2d813207b2e1340ba
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 13:24:23 -0600

    feat(index.tsx): add dynamic content rendering based on GraphQL query result
    The Homepage component now makes use of the useContentBySlugQuery hook from the dicty-graphql-schema package to fetch content based on a specific slug. The result of the query is then matched using the ts-pattern library to conditionally render different components based on the query result. This allows for dynamic rendering of content on the homepage. Additionally, there was a fix in the import statement for the FileLinks component to ensure proper functionality.

--- apps/stock-center/src/pages/index/index.tsx
+++ apps/stock-center/src/pages/index/index.tsx
@@ -8,11 +8,19 @@ import {
   CatalogLinks,
   FileLinks,
 } from "@dictybase/ui-dsc"
+import { ContentView, LoadingDisplay } from "@dictybase/ui-common"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { NAMESPACE } from "../../namespace"
 
 /**
  * Homepage is the main homepage component for DSC.
  */
 const Homepage = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-intro` },
+  })
+
   const classes = useStyles({})
   return (
     <>
@@ -20,7 +28,18 @@ const Homepage = () => {
       <Grid container justifyContent="space-between" spacing={3}>
         <Heading />
         <Grid item xs={12} className={classes.intro}>
-          <IntroEditor />
+          {match(result)
+            .with(
+              { data: { contentBySlug: P.select({ content: P.string }) } },
+              (content) => <ContentView data={content} />,
+            )
+            .with({ loading: true }, () => <LoadingDisplay rows={4} />)
+            .with({ error: P.select(P.not(undefined)) }, (error) => (
+              <>error display</>
+            ))
+            .otherwise(() => (
+              <> This message should not appear. </>
+            ))}
         </Grid>
         <Grid container item xs={12} spacing={4} justifyContent="flex-start">
           <Slide />
