c7b20649fbef2d75300db7dcfbcc69797645f7fa
Kevin Tun <kevin.tun@northwestern.edu>
2024-02-28 13:19:24 -0600

    refactor(EditableView.tsx): remove unused import and replace Container component with a fragment
    The import statement for the Container component from "@material-ui/core" is removed as it is not being used in the code. The Container component is replaced with a fragment (<>) to avoid unnecessary wrapping of the components.

--- packages/ui-common/src/EditablePages/EditableView.tsx
+++ packages/ui-common/src/EditablePages/EditableView.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import { Container } from "@material-ui/core"
 import { type ContentBySlugQuery } from "dicty-graphql-schema"
 import { Editor } from "editor"
 import { EditableContentToolbar } from "./EditableContentToolbar"
@@ -12,13 +11,13 @@ const EditableView = ({ data }: EditableViewProperties) => {
   const { updated_at, updated_by, content, slug } = data
 
   return (
-    <Container>
+    <>
       <EditableContentToolbar user={updated_by} lastUpdate={updated_at} />
       <Editor
         editable={false}
         content={{ storageKey: slug, editorState: content }}
       />
-    </Container>
+    </>
   )
 }
 
