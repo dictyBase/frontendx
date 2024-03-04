65a9990a953c6f5cf0a8b9ffd278964aa7a308ba
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-04 13:21:53 -0600

    chore(vite.config.ts): change server port from 3003 to 3004 for the dicty-frontpage app
    The server port for the dicty-frontpage app has been changed from 3003 to 3004. This change was made to avoid conflicts with other applications running on the same port and ensure the app runs smoothly.

--- apps/dicty-frontpage/vite.config.ts
+++ apps/dicty-frontpage/vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig as defineVitestConfig } from "vitest/config"
 const viteConfig = defineConfig({
   plugins: [react()],
   server: {
-    port: 3003,
+    port: 3004,
   },
   base: "/",
 })
