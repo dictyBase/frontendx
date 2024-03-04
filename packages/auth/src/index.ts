8082300e290eea32b4287cc58853617e531d49e4
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-04 16:25:01 -0600

    chore(auth): remove unused authNavbarData export
    The authNavbarData export from the auth package is no longer used and has been removed to clean up the codebase.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -12,5 +12,4 @@ export * from "./useRole"
 export * from "./useAuthorization"
 export * from "./useTokenAndUser"
 
-export { default as authNavbarData } from "./data/authNavbarData.json"
 export { default as authFooterData } from "./data/authFooterData.json"
