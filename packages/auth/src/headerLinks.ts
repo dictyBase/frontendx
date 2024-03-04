4e04a4002cb9e5adf457de7897fa73c9f17bf840
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-04 17:20:41 -0600

    fix(headerLinks.ts): update href values to include VITE_APP_FRONTPAGE_URL environment variable
    The href values in the authorizedIconItems array have been updated to include the VITE_APP_FRONTPAGE_URL environment variable. This ensures that the links point to the correct URLs based on the environment configuration.

--- packages/auth/src/headerLinks.ts
+++ packages/auth/src/headerLinks.ts
@@ -1,22 +1,25 @@
+/* eslint-disable dot-notation */
 import { iconButtonPipe, defaultIconItems } from "@dictybase/header"
 import { Add, Info, ArrowDownward as Download } from "@material-ui/icons"
 import { v4 as uuid4 } from "uuid"
 
 const authorizedIconItems = [
   {
-    href: "/community/citation/editable",
+    href: `${
+      import.meta.env["VITE_APP_FRONTPAGE_URL"]
+    }/community/citation/editable`,
     title: "Cite Us",
     Icon: Add,
     key: uuid4(),
   },
   {
-    href: "/downloads",
+    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/downloads`,
     title: "Downloads",
     Icon: Download,
     key: uuid4(),
   },
   {
-    href: "/about/editable",
+    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/about/editable`,
     title: "About dictybase",
     Icon: Info,
     key: uuid4(),
