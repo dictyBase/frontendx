785a21008916644b79a9f57b4f3481e09d432daa
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-04 17:20:33 -0600

    chore(defaultIconItems.tsx): add support for dynamic frontpage URL using import.meta.env
    The defaultIconItems file has been modified to use the import.meta.env object to dynamically generate the URLs for the "Cite Us", "Downloads", and "About dictybase" links. This allows the URLs to be configurable based on the environment in which the application is running.

--- packages/header/src/defaultIconItems.tsx
+++ packages/header/src/defaultIconItems.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import { Add, Info, ArrowDownward as Download } from "@material-ui/icons"
 import { v4 as uuid4 } from "uuid"
 import { IconType } from "./types"
@@ -11,18 +12,25 @@ type IconItemProperty = {
 
 const defaultIconItems: Array<IconItemProperty> = [
   {
-    href: "/community/citation/show",
+    href: `${
+      import.meta.env["VITE_APP_FRONTPAGE_URL"]
+    }/community/citation/show`,
     title: "Cite Us",
     Icon: Add,
     key: uuid4(),
   },
   {
-    href: "/downloads",
+    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/downloads`,
     title: "Downloads",
     Icon: Download,
     key: uuid4(),
   },
-  { href: "/about/show", title: "About dictybase", Icon: Info, key: uuid4() },
+  {
+    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/about/show`,
+    title: "About dictybase",
+    Icon: Info,
+    key: uuid4(),
+  },
 ]
 
 export { defaultIconItems }
