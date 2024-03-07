43357ca9f548f85437705df45ae373788c84e957
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-07 15:51:19 -0600

    fix(handlers.ts): add error response for cases where there is no mock data available for the requested gene
    The 'res' parameter in the handler function for the "Gene" query has been renamed to 'response' to improve clarity and consistency with the naming conventions. Additionally, an error response has been added for cases where there is no mock data available for the requested gene, providing a helpful error message indicating that there is no mock data available for the specified gene.

--- apps/genome-page/mocks/handlers.ts
+++ apps/genome-page/mocks/handlers.ts
@@ -9,27 +9,28 @@ import { mockGeneAda2Limit } from "./ada2Mocks/mockGeneAda2Limit"
 export const handlers = [
   // Handles the Gene query: https://github.com/dictyBase/dicty-graphql-schema/blob/develop/src/queries/gene.graphql
   // Implementation details here: https://github.com/dictyBase/genomepage/pull/825#issuecomment-977246804
-  graphql.query("Gene", (request, res, context) => {
+  graphql.query("Gene", (request, response, context) => {
     const gene = request.body?.variables?.gene
     const limit = request.body?.variables?.limit
 
     if (gene === "sadA") {
       if (limit === 3) {
-        return res(context.data({ ...mockGeneLimit }))
+        return response(context.data({ ...mockGeneLimit }))
       }
-      return res(context.data({ ...mockGene }))
+      return response(context.data({ ...mockGene }))
     }
     if (gene === "piaA") {
       if (limit === 3) {
-        return res(context.data({ ...mockGenePiaALimit }))
+        return response(context.data({ ...mockGenePiaALimit }))
       }
-      return res(context.data({ ...mockGenePiaA }))
+      return response(context.data({ ...mockGenePiaA }))
     }
     if (gene === "ada2") {
       if (limit === 3) {
-        return res(context.data({ ...mockGeneAda2Limit }))
+        return response(context.data({ ...mockGeneAda2Limit }))
       }
-      return res(context.data({ ...mockGeneAda2 }))
+      return response(context.data({ ...mockGeneAda2 }))
     }
+    return response(context.errors([{ message: `No mock for ${gene}` }]))
   }),
 ]
