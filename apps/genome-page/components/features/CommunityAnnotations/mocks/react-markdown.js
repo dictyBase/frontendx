6ce64d335359ef06d7f080144652ca136e124af4
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-07 11:02:33 -0600

    chore(CommunityAnnotations): replace react-markdown mock file with TypeScript version
    The react-markdown mock file "react-markdown.js" has been deleted and replaced with a TypeScript version "react-markdown.tsx". This change was made to improve the codebase by using TypeScript instead of JavaScript for better type safety and development experience.

--- apps/genome-page/components/features/CommunityAnnotations/mocks/react-markdown.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/remarkjs/react-markdown/issues/635#issuecomment-1080109162
-import React from "react"
-
-function ReactMarkdown({ children }) {
-  return <>{children}</>
-}
-
-export { ReactMarkdown } 
---
+++ apps/genome-page/components/features/CommunityAnnotations/mocks/react-markdown.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable unicorn/filename-case */
+// https://github.com/remarkjs/react-markdown/issues/635#issuecomment-1080109162
+import React, { type FunctionComponent } from "react"
+
+const ReactMarkdown: FunctionComponent = ({ children }) => <>{children}</>
+
+// eslint-disable-next-line import/no-default-export
+export default ReactMarkdown
