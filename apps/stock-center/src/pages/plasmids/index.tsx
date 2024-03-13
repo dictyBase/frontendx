1ae1a4ad1d4105d9fcabadd6fe572a43aae0f035
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-12 23:55:13 -0500

    feat(plasmids): add PlasmidCatalog page component
    The PlasmidCatalog page component is added to the stock-center application. This component is responsible for displaying the plasmid catalog, including a search bar, a table of plasmids, and loading and error displays. It also includes functionality for fetching more plasmids when the user scrolls to the bottom of the page.

---
+++ apps/stock-center/src/pages/plasmids/index.tsx
@@ -0,0 +1,74 @@
+import {
+  getStrainListConfiguration,
+  defaultFilter,
+  graphqlQueryVariables,
+} from "@dictybase/hook-dsc"
+import { P, match } from "ts-pattern"
+import {
+  LoadingDisplay,
+  PlasmidCatalogTableDisplay,
+  ErrorDisplay,
+  SearchBar,
+  CatalogListWrapper,
+  CatalogHeader,
+} from "@dictybase/ui-dsc"
+import { useIntersectionObserver } from "@dictybase/hook"
+import { useRef } from "react"
+import { usePlasmidListFilterQuery } from "dicty-graphql-schema"
+
+const PlasmidCatalog = () => {
+  const { loading, error, data, fetchMore } = usePlasmidListFilterQuery({
+    variables: { ...graphqlQueryVariables, filter: "" },
+  })
+  const rootReference = useRef<HTMLDivElement>(null)
+  const targetReference = useRef<HTMLTableRowElement>(null)
+
+  const onIntersection = ([entry]: IntersectionObserverEntry[]) => {
+    const nextCursor = data?.listPlasmids?.nextCursor
+    switch (true) {
+      case !nextCursor:
+        return
+      case loading:
+        return
+      case !entry.isIntersecting:
+        return
+      default:
+        fetchMore({ variables: { cursor: nextCursor } })
+    }
+  }
+  useIntersectionObserver({
+    target: targetReference,
+    onIntersection,
+    option: { root: rootReference.current, threshold: 0.1 },
+  })
+
+  return (
+    <>
+      <CatalogHeader title="Plasmid Catalog" />
+      {/* <SearchBar /> */}
+      <CatalogListWrapper root={rootReference}>
+        {match({ data, loading, error })
+          .with(
+            { data: P.select({ listPlasmids: P.not(undefined) }) },
+            (data_) => (
+              <PlasmidCatalogTableDisplay
+                data={data_}
+                dataField="listPlasmids"
+                target={targetReference}
+              />
+            ),
+          )
+          .with({ loading: true }, () => <LoadingDisplay rows={10} />)
+          .with({ error: P.select({ message: P.string }) }, (error_) => (
+            <ErrorDisplay error={error_} />
+          ))
+          .otherwise(() => (
+            <></>
+          ))}
+      </CatalogListWrapper>
+    </>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default PlasmidCatalog
