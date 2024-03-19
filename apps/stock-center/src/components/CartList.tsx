27722d437054b90673266f28662e091f7414a26d
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-19 16:35:50 -0500

    feat(CartList.tsx): add support for rendering plasmid items in the cart
    Support for rendering and removing plasmid items from the cart has been added. This allows the cart to display and interact with both strain and plasmid items.

--- apps/stock-center/src/components/CartList.tsx
+++ apps/stock-center/src/components/CartList.tsx
@@ -1,6 +1,6 @@
 import { match } from "ts-pattern"
 import { pipe } from "fp-ts/function"
-import { mapWithIndex } from "fp-ts/Array"
+import { map as Amap } from "fp-ts/Array"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import List from "@material-ui/core/List"
@@ -13,12 +13,13 @@ import {
   CartItem,
   CheckoutButton,
   renderStrainTotal,
-  // renderPlasmidTotal,
-  // renderStrainAndPlasmidTotals,
+  renderPlasmidTotal,
+  renderStrainAndPlasmidTotals,
   renderCartTotal,
 } from "@dictybase/ui-dsc"
-import { useAtom, PrimitiveAtom } from "jotai"
-import { Cart, strainItemAtomsAtom, type StrainCartItem } from "../state"
+import { useAtom, useAtomValue, type PrimitiveAtom } from "jotai"
+import { Cart, strainItemAtomsAtom, plasmidItemAtomsAtom } from "../state"
+import type { PlasmidCartItem, StrainCartItem } from "../types"
 import { isFull } from "../isFull"
 
 type ShoppingCartListProperties = {
@@ -37,15 +38,6 @@ type SplitAtomAction<Item> =
       atom: PrimitiveAtom<Item>
       before?: PrimitiveAtom<Item>
     }
-type DispatchProperties = (update: SplitAtomAction<StrainCartItem>) => void
-type renderCartItemsPipelineProperties = {
-  items: Array<StrainCartItem>
-  fn: (index: number, item: StrainCartItem) => JSX.Element
-}
-type renderCartItemsProperties = {
-  dispatch: DispatchProperties
-  strainItemAtoms: Array<PrimitiveAtom<StrainCartItem>>
-}
 
 const useStyles = makeStyles((theme) => ({
   list: {
@@ -61,44 +53,52 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-/**
- * ShoppingCartPageWithItems is the display for the cart page when there are
- * items in the cart.
- */
+type StrainCartItemFromAtomProperties = {
+  strainCartItemAtom: PrimitiveAtom<StrainCartItem>
+  dispatch: (update: SplitAtomAction<StrainCartItem>) => void
+}
 
-const renderCartItems =
-  ({ dispatch, strainItemAtoms }: renderCartItemsProperties) =>
-  // eslint-disable-next-line react/function-component-definition
-  (index: number, item: StrainCartItem) => {
-    const { id } = item
-    return (
-      <CartItem
-        item={item}
-        key={id}
-        deleteItem={() =>
-          dispatch({ type: "remove", atom: strainItemAtoms[index] })
-        }
-      />
-    )
-  }
-const renderCartItemsPipeline = ({
-  items,
-  fn,
-}: renderCartItemsPipelineProperties) => pipe(items, mapWithIndex(fn))
+const StrainCartItemFromAtom = ({
+  strainCartItemAtom,
+  dispatch,
+}: StrainCartItemFromAtomProperties) => {
+  const strainCartItem = useAtomValue(strainCartItemAtom)
+  return (
+    <CartItem
+      item={strainCartItem}
+      deleteItem={() => dispatch({ type: "remove", atom: strainCartItemAtom })}
+    />
+  )
+}
+
+type PlasmidCartItemFromAtomProperties = {
+  plasmidCartItemAtom: PrimitiveAtom<PlasmidCartItem>
+  dispatch: (update: SplitAtomAction<PlasmidCartItem>) => void
+}
+
+const PlasmidCartItemFromAtom = ({
+  plasmidCartItemAtom,
+  dispatch,
+}: PlasmidCartItemFromAtomProperties) => {
+  const strainCartItem = useAtomValue(plasmidCartItemAtom)
+  return (
+    <CartItem
+      item={strainCartItem}
+      deleteItem={() => dispatch({ type: "remove", atom: plasmidCartItemAtom })}
+    />
+  )
+}
 
 const renderTotalRows = (cart: Cart) => (
   <>
     {match(cart)
-      // .when(
-      //   ({ strainItems, plasmidItems }) =>
-      //     strainItems.length > 0 && plasmidItems.length > 0,
-      //   renderStrainAndPlasmidTotals,
-      // )
+      .when(
+        ({ strainItems, plasmidItems }) =>
+          strainItems.length > 0 && plasmidItems.length > 0,
+        renderStrainAndPlasmidTotals,
+      )
       .when(({ strainItems }) => strainItems.length > 0, renderStrainTotal)
-      // .when(
-      //   ({ plasmidItems }) => plasmidItems.length > 0,
-      //   renderPlasmidTotal,
-      // )
+      .when(({ plasmidItems }) => plasmidItems.length > 0, renderPlasmidTotal)
       .otherwise(() => (
         <>No items found. This page should not appear.</>
       ))}
@@ -107,18 +107,34 @@ const renderTotalRows = (cart: Cart) => (
 )
 
 const CartList = ({ cart }: ShoppingCartListProperties) => {
-  const [strainItemAtoms, dispatch] = useAtom(strainItemAtomsAtom)
+  const [strainItemAtoms, dispatchS] = useAtom(strainItemAtomsAtom)
+  const [plasmidItemAtoms, dispatchP] = useAtom(plasmidItemAtomsAtom)
   const classes = useStyles()
-  const renderCartItemsFunction = renderCartItems({ dispatch, strainItemAtoms })
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={9}>
         <List className={classes.list}>
-          {renderCartItemsPipeline({
-            items: cart.strainItems,
-            fn: renderCartItemsFunction,
-          })}
+          {pipe(
+            strainItemAtoms,
+            Amap((strainItemAtom) => (
+              <StrainCartItemFromAtom
+                strainCartItemAtom={strainItemAtom}
+                dispatch={dispatchS}
+              />
+            )),
+          )}
+        </List>
+        <List className={classes.list}>
+          {pipe(
+            plasmidItemAtoms,
+            Amap((plasmidItemAtom) => (
+              <PlasmidCartItemFromAtom
+                plasmidCartItemAtom={plasmidItemAtom}
+                dispatch={dispatchP}
+              />
+            )),
+          )}
         </List>
       </Grid>
       <Grid item xs={3}>
