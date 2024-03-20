4bc92174c890c48c11e23a782390a8077eab03b7
Kevin Tun <kevin.tun@northwestern.edu>
2024-03-20 00:05:07 -0500

    refactor(getCartTotal.ts): change StrainCartItem type to CatalogCartItem type for consistency
    The StrainCartItem type has been renamed to CatalogCartItem for consistency with the naming conventions used in the project. This change ensures uniformity in the naming of types throughout the codebase.

--- packages/ui-dsc/src/utils/getCartTotal.ts
+++ packages/ui-dsc/src/utils/getCartTotal.ts
@@ -1,7 +1,7 @@
-import { type StrainCartItem } from "../types"
+import { type CatalogCartItem } from "../types"
 import { toCurrencyString } from "./toCurrencyString"
 
-const getCartTotal = (items: Array<StrainCartItem>) => {
+const getCartTotal = (items: Array<CatalogCartItem>) => {
   const total = items
     .map((item) => Number(item.fee))
     .reduce((accumulator, value) => accumulator + value, 0)
