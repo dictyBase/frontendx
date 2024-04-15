e5475a07d5b974b57b69f534a61325e2017ab6d5
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-15 13:55:56 -0500

    chore(InsertImageButton.tsx): replace ImageDialogContents with ImageUploadDialog
    The component ImageDialogContents has been replace with ImageUploadDialog which removes a lot of unnecessary code.

--- packages/editor-toolbar/src/components/InsertImageButton.tsx
+++ packages/editor-toolbar/src/components/InsertImageButton.tsx
@@ -2,7 +2,7 @@ import { Button, Dialog } from "@material-ui/core"
 import AddIcon from "@material-ui/icons/Add"
 import { useAtom } from "jotai"
 import { insertImageDialogOpenAtom } from "../context/atomConfigs"
-import { ImageDialogContents } from "./ImageDialogContents"
+import { ImageUploadDialog } from "./ImageUploadDialog"
 
 type InsertImageButtonProperties = {
   handleImageUpload: (file: File) => Promise<string>
@@ -21,9 +21,7 @@ const InsertImageButton = ({
         startIcon={<AddIcon />}>
         Image
       </Button>
-      <Dialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)}>
-        <ImageDialogContents handleImageUpload={handleImageUpload} />
-      </Dialog>
+      <ImageUploadDialog open={isDialogOpen} handleImageUpload={handleImageUpload} />
     </>
   )
 }
