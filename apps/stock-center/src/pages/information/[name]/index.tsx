48f37c33189154112d0c62a73d8fb9292ba9f17d
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-15 16:01:03 -0500

    feat(pages): add RoleRedirect component to handle authorization logic
    Three new files were added, each containing a RoleRedirect component that handles redirect logic based on the user's role and loading state. The access constant was also added to the RoleRedirect component to specify public access.

---
+++ apps/stock-center/src/pages/information/[name]/index.tsx
@@ -0,0 +1,28 @@
+import { Navigate } from "react-router-dom"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
+import { match } from "ts-pattern"
+import { useAuthorization, ACCESS } from "auth"
+
+const RoleRedirect = () => {
+  const { isLoading, isAuthorized } = useAuthorization({
+    entries: ["content-admin"],
+  })
+  return match({ isLoading, isAuthorized })
+    .with({ isAuthorized: true, isLoading: true }, () => (
+      <Navigate to="editable" replace />
+    ))
+    .with({ isAuthorized: true, isLoading: false }, () => (
+      <Navigate to="editable" replace />
+    ))
+    .with({ isAuthorized: false, isLoading: false }, () => (
+      <Navigate to="show" replace />
+    ))
+    .with({ isAuthorized: false, isLoading: true }, () => (
+      <FullPageLoadingDisplay />
+    ))
+    .exhaustive()
+}
+
+// eslint-disable-next-line import/no-default-export
+export default RoleRedirect
+export const access = ACCESS.public
