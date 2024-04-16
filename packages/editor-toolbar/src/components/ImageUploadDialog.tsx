0934cb2780938ff72d929ea0c93122ae9ecc3ae7
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-16 01:45:00 -0500

    feat(ImageUploadDialog.tsx): add file validation and error handling for image uploads
    The changes introduce file validation logic to check if the selected image is too large before uploading. Error handling is improved to display specific error messages for different scenarios, such as when the image size exceeds the limit or when the upload fails. Additionally, the code is refactored to handle errors more effectively and provide a better user experience during the image upload process.

--- packages/editor-toolbar/src/components/ImageUploadDialog.tsx
+++ packages/editor-toolbar/src/components/ImageUploadDialog.tsx
@@ -6,12 +6,43 @@ import {
   DialogActions,
   Button,
   Typography,
-  CircularProgress
+  CircularProgress,
+  Input,
+  makeStyles,
 } from "@material-ui/core"
 import { useState } from "react"
 import { useSetAtom } from "jotai"
+import { pipe } from "fp-ts/function"
+import {
+  Option,
+  some,
+  none,
+  isSome,
+  map as Omap,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { match } from "ts-pattern"
 import { INSERT_IMAGE_COMMAND } from "image-plugin"
 import { insertImageDialogOpenAtom } from "../context/atomConfigs"
+
+type fileError = {
+  errorMessage: string
+}
+
+const useStyles = makeStyles({ input: { display: "flex" } })
+
+const validateFile = (file: File) => {
+  return match(file)
+    .when(
+      ({ size }) => size > 500000,
+      () =>
+        some({
+          errorMessage:
+            "The chosen image is too large. It must be smaller than 0.5MB",
+        }),
+    )
+    .otherwise(() => none)
+}
 
 type ImageUploadDialogProperties = {
   handleImageUpload: (file: File) => Promise<string>
@@ -24,23 +55,39 @@ const ImageUploadDialog = ({
 }: ImageUploadDialogProperties) => {
   const [editor] = useLexicalComposerContext()
   const setDialogDisplay = useSetAtom(insertImageDialogOpenAtom)
-  const [imageSource, setImageSource] = useState<string>()
+  const [imageSource, setImageSource] = useState<string>("")
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [error, setError] = useState<Option<fileError>>(none)
+  const { input } = useStyles()
 
   const onChange: React.ChangeEventHandler<HTMLInputElement> = async ({
     target: { validity, files },
   }) => {
-    if (!validity.valid || !files || !files[0]) return
+    if (!validity.valid || !files || !files[0]) {
+      return
+    }
+    setImageSource("")
+    setError(none)
+    const otherValidationErrors = validateFile(files[0])
+    if (isSome(otherValidationErrors)) {
+      setError(otherValidationErrors)
+      return
+    }
     setLoading(true)
     try {
       const url = await handleImageUpload(files[0])
       setImageSource(url)
-    } catch {
-      setError(true)
+    } catch (error) {
+      setError(some({ errorMessage: "Could not upload image to the server" }))
     } finally {
       setLoading(false)
     }
+  }
+  const handleClose = () => {
+    if (loading) return
+    setImageSource("")
+    setError(none)
+    setDialogDisplay(false)
   }
 
   const onClick = () => {
@@ -50,21 +97,32 @@ const ImageUploadDialog = ({
       width: 300,
       height: 300,
     })
-    setDialogDisplay(false)
+    handleClose()
   }
 
   return (
-    <Dialog
-      open={open}
-      onClose={() => {
-        setDialogDisplay(false)
-      }}>
-      <DialogTitle> Choose an Image to Upload </DialogTitle>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle disableTypography>
+        <Typography variant="h3"> Choose an Image to Upload </Typography>
+      </DialogTitle>
       <DialogContent>
-        <input type="file" id="image-upload" onChange={onChange} />
+        <Input
+          type="file"
+          id="image-upload"
+          onChange={onChange}
+          fullWidth
+          className={input}
+          inputProps={{ accept: "image/*" }}
+        />
         <DialogActions>
+          {pipe(
+            error,
+            Omap(({ errorMessage }) => (
+              <Typography color="error">{errorMessage}</Typography>
+            )),
+            OgetOrElse(() => <></>),
+          )}
           {loading ? <CircularProgress /> : <></>}
-          {console.log(!!imageSource)}
           <Button type="button" disabled={!imageSource} onClick={onClick}>
             Submit
           </Button>
