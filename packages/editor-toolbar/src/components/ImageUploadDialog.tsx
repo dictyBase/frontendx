cced59042b9e780a7e5e8de5c2994a9babfb65e1
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-17 14:32:29 -0500

    feat(ImageUploadDialog.tsx): add validation for file size and display error message if file exceeds 2MB
    The changes were made to improve user experience by providing feedback on file size validation and enhancing the form reset functionality for a smoother user interaction.

--- packages/editor-toolbar/src/components/ImageUploadDialog.tsx
+++ packages/editor-toolbar/src/components/ImageUploadDialog.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import {
   Dialog,
@@ -10,116 +11,107 @@ import {
   Input,
   makeStyles,
 } from "@material-ui/core"
-import { FetchResult } from "@apollo/client"
-import { useUploadFileMutation, UploadFileMutation } from "dicty-graphql-schema"
+import { useUploadFileMutation } from "dicty-graphql-schema"
 import { useLogto } from "@logto/react"
 import { useSetAtom } from "jotai"
 import { pipe } from "fp-ts/function"
 import { head as Ahead } from "fp-ts/Array"
 import {
-  left as Eleft,
-  right as Eright,
+  fromNullable as OfromNullable,
+  bindTo as ObindTo,
+  bind as Obind,
+  let as Olet,
+  none,
+  some,
+  Option,
+  match as Omatch,
+} from "fp-ts/Option"
+import {
   fromOption as EfromOption,
   map as Emap,
   filterOrElse as EfilterOrElse,
+  match as Ematch,
 } from "fp-ts/Either"
-import {
-  left as TEleft,
-  right as TEright,
-  fromOption as TEfromOption,
-  map as TEmap,
-  filterOrElse as TEfilterOrElse,
-  flatMap as TEflatMap,
-  tryCatch as TEtryCatch,
-} from "fp-ts/TaskEither"
 import { match, P } from "ts-pattern"
 import { INSERT_IMAGE_COMMAND } from "image-plugin"
 import { insertImageDialogOpenAtom } from "../context/atomConfigs"
 
 const useStyles = makeStyles({ input: { display: "flex" } })
 
-const validateFile = (file: File) =>
-  match(file)
-    .when(
-      ({ size }) => size > 500_000,
-      () =>
-        Eleft({
-          message:
-            "The chosen image is too large. It must be smaller than 0.5MB",
-        }),
-    )
-    .otherwise((file) => Eright(file))
-
-const fileSizePredicate = (file: File) => file.size > 500_000
-
-const mutationResultToTaskEither = (result: FetchResult<UploadFileMutation>) =>
-  match(result)
-    .with({ data: { uploadFile: { url: P.select(P.string) } } }, (url) =>
-      TEright(url),
-    )
-    .with({ errors: P.not(undefined) }, () => TEleft("Something went wrong"))
-    .otherwise(() => TEleft("Unexpected Error"))
+const fileSizePredicate = (file: File) => file.size < 2_000_000
 
 type ImageUploadDialogProperties = {
   handleImageUpload: (file: File) => Promise<string>
   open: boolean
 }
 
+const getEitherValidFile = (files: FileList | null) =>
+  pipe(
+    files,
+    OfromNullable,
+    ObindTo("files"),
+    Olet("presentFiles", ({ files }) => Array.from(files)),
+    Obind("selectedFile", ({ presentFiles }) => Ahead(presentFiles)),
+    EfromOption(() => "No file selected"),
+    Emap(({ selectedFile }) => selectedFile),
+    EfilterOrElse(
+      fileSizePredicate,
+      () => "The chosen image is too large. It must be smaller than 2MB",
+    ),
+  )
+
+const renderValidationError = (error: Option<string>) =>
+  pipe(
+    error,
+    Omatch(
+      () => <></>,
+      (e) => <Typography color="error">{e}</Typography>,
+    ),
+  )
+
 const ImageUploadDialog = ({ open }: ImageUploadDialogProperties) => {
   const { getAccessToken } = useLogto()
   const [editor] = useLexicalComposerContext()
   const setDialogDisplay = useSetAtom(insertImageDialogOpenAtom)
-  const [uploadImage, { error, loading, data }] = useUploadFileMutation()
+  const [uploadImage, { error, loading, data, reset }] = useUploadFileMutation()
+  const [fileValidationError, setValidationError] =
+    useState<Option<string>>(none)
   const { input } = useStyles()
 
-  const onChange: React.ChangeEventHandler<HTMLInputElement> = async ({
-    target: { validity, files },
-  }) => {
-    if (!validity.valid || !files) {
-      return
-    }
-    const fileUpload = pipe(
-      Array.from(files),
-      Ahead,
-      TEfromOption(() => "No file selected"),
-      TEfilterOrElse(
-        fileSizePredicate,
-        () => "The chosen image is too large. It must be smaller than 0.5MB",
-      ),
-      TEflatMap((file: File) =>
-        TEtryCatch(
-          () => {
-            const token = getAccessToken(
-              import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
-            )
-            const a = uploadImage({
-              variables: { file },
-              context: { headers: { Authorization: `Bearer ${token}` } },
-            })
-            return a
-          },
-          () => "Could not upload image to server",
-        ),
-      ),
-      TEflatMap(mutationResultToTaskEither),
-    )
+  const canInsert = match({ error, loading, data })
+    .with({ data: { uploadFile: { url: P.string } } }, () => true)
+    .otherwise(() => false)
 
-    //    const otherValidationErrors = validateFile(files[0])
-    //    if (isSome(otherValidationErrors)) {
-    //      return
-    //    }
-    //    const token = await getAccessToken(
-    //      import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
-    //    )
-    //    uploadImage({
-    //      variables: { file: files[0] },
-    //      context: { headers: { Authorization: `Bearer ${token}` } },
-    //    })
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = async ({
+    target: { files },
+  }) => {
+    reset()
+    setValidationError(none)
+    pipe(
+      files,
+      getEitherValidFile,
+      Ematch(
+        (error) => {
+          setValidationError(some(error))
+        },
+        async (file) => {
+          const token = await getAccessToken(
+            import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE,
+          )
+          uploadImage({
+            variables: { file },
+            context: { headers: { Authorization: `Bearer ${token}` } },
+          })
+        },
+      ),
+    )
   }
 
   const handleClose = () => {
     if (loading) return
     setDialogDisplay(false)
+    setValidationError(none)
+    reset()
   }
 
   const onClick = () => {
@@ -133,6 +125,7 @@ const ImageUploadDialog = ({ open }: ImageUploadDialogProperties) => {
         handleClose()
       })
       .otherwise(() => {})
+    reset()
   }
 
   return (
@@ -149,19 +142,16 @@ const ImageUploadDialog = ({ open }: ImageUploadDialogProperties) => {
           className={input}
           inputProps={{ accept: "image/*" }}
         />
+        {renderValidationError(fileValidationError)}
         <DialogActions>
-          {/*
-          {pipe(
-            error,
-            Omap(({ errorMessage }) => (
-              <Typography color="error">{errorMessage}</Typography>
-            )),
-            OgetOrElse(() => <></>),
+          {error ? (
+            <Typography color="error">{error.message}</Typography>
+          ) : (
+            <></>
           )}
-          */}
           {loading ? <CircularProgress /> : <></>}
-          <Button type="button" onClick={onClick}>
-            Submit
+          <Button type="button" disabled={!canInsert} onClick={onClick}>
+            Insert Image
           </Button>
         </DialogActions>
       </DialogContent>
