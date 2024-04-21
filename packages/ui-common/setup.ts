76422eb163d7615809e2254471db830db0d4fff9
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-20 20:59:07 -0500

    feat(news-component, ui-common, ui-dsc): add mock for @logto/react in setup files
    The changes introduce a new mock for the "@logto/react" library in the setup files of the news-component, ui-common, and ui-dsc packages. This mock provides a function that returns an object with a method to get an access token. This measure prevents several tests from failing that indirectly rely on @logto/react, which tries to import "react/jsx-runtime" which cannot be resolved in node.

--- packages/ui-common/setup.ts
+++ packages/ui-common/setup.ts
@@ -1,9 +1,18 @@
-import { expect, afterEach } from "vitest"
+import { vi, expect, afterEach } from "vitest"
 import { cleanup } from "@testing-library/react"
 import matchers from "@testing-library/jest-dom/matchers"
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)
+
+// mock @logto
+vi.mock("@logto/react", async () => {
+  return {
+    useLogto: () => ({
+      getAccessToken: () => {}
+    })
+  }
+}) 
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
