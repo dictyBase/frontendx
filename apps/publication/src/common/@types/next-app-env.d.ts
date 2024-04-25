c7b1f4d0c9a5651b087f9507269643ee61a81b8a
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-25 03:10:21 -0500

    feat(next-app-env.d.ts): add type definitions for environment variables used in the Next.js application
    The commit adds type definitions for various environment variables used in the Next.js application. This ensures type safety and provides clarity on the expected types for each environment variable, improving code quality and maintainability.

--- apps/publication/src/common/@types/next-app-env.d.ts
+++ apps/publication/src/common/@types/next-app-env.d.ts
@@ -11,6 +11,8 @@ declare global {
       NEXT_PUBLIC_NAVBAR_JSON: string
       NEXT_PUBLIC_FOOTER_JSON: string
       NEXT_PUBLIC_GRAPHQL_SERVER: string
+      NEXT_PUBLIC_FRONTPAGE_URL: string
+      NEXT_PUBLIC_STOCKCENTER_URL: string
       // the alternate graphql server used to prevent cross-site cookie issues
       NEXT_PUBLIC_ALT_GRAPHQL_SERVER: string
       // used to differentiate between deploy environments, specifically for using correct GraphQL URL
