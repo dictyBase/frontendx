53254470e8aa08a88d7c1c7ebcc2392e7a544718
Kevin Tun <kevin.tun@northwestern.edu>
2024-04-25 04:48:55 -0500

    chore(next.config.js): add dicty-graphql-schema to transpile modules in next.config.js
    The dicty-graphql-schema package has been added to the list of transpile modules in the next.config.js file. This change ensures that the dicty-graphql-schema package is transpiled along with the existing @dictybase/navbar package, allowing both packages to be used seamlessly in the Next.js application.

--- apps/publication/next.config.js
+++ apps/publication/next.config.js
@@ -1,7 +1,10 @@
 const base = "/publication"
 
 // eslint-disable-next-line import/no-commonjs, unicorn/prefer-module
-const withTM = require("next-transpile-modules")(["@dictybase/navbar"])
+const withTM = require("next-transpile-modules")([
+  "@dictybase/navbar",
+  "dicty-graphql-schema",
+])
 // eslint-disable-next-line import/no-commonjs, unicorn/prefer-module
 module.exports = withTM({
   basePath: base,
