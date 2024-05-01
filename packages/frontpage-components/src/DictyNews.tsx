f2696508e679db2809187b46f5153798eeae929e
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-01 11:07:37 -0500

    style(DictyNews.tsx): add Grid component from Material-UI to improve layout structure
    feat(DictyNews.tsx): utilize fp-ts functions makeBy and map to dynamically render news items
    The changes were made to improve the layout structure of the DictyNews component by adding the Grid component from Material-UI. Additionally, the fp-ts functions makeBy and map were implemented to dynamically render news items, enhancing the flexibility and scalability of the component.

--- packages/frontpage-components/src/DictyNews.tsx
+++ packages/frontpage-components/src/DictyNews.tsx
@@ -1,8 +1,27 @@
-import { Box, Typography } from "@material-ui/core"
+import { Box, Typography, Grid } from "@material-ui/core"
+import { pipe } from "fp-ts/function"
+import { makeBy as AmakeBy, map as Amap } from "fp-ts/Array"
 
 const DictyNews = () => (
   <Box>
-    <Typography>Dicty News</Typography>
+    <Grid direction="column" spacing={1} container>
+      <Grid item>
+        <Typography variant="h1">Dicty News</Typography>
+      </Grid>
+      <Grid item>
+        {pipe(
+          AmakeBy(6, (i) => i),
+          Amap(() => (
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Typography>
+          )),
+        )}
+      </Grid>
+    </Grid>
   </Box>
 )
 
