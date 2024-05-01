481e868c808e32ab5930eb4c03825b8b7d17c362
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-01 14:57:01 -0500

    chore(publication): remove unnecessary callback and index page components
    The callback.tsx and index.tsx files were deleted as they contained unnecessary code that was no longer needed in the project. The PublicationPageWrapper component was refactored to handle rendering the publication page based on the publication id, improving code organization and readability.

--- apps/publication/src/pages/[id]/callback.tsx
+++
@@ -1,6 +0,0 @@
-import { OauthCallback } from "../../components/auth/OauthCallback"
-
-const CallbackPage = () => <OauthCallback />
-
-// eslint-disable-next-line import/no-default-export
-export default CallbackPage
--- apps/publication/src/pages/index.tsx
+++ apps/publication/src/pages/index.tsx
@@ -1,15 +1,39 @@
 import React from "react"
 import { useRouter } from "next/router"
+import { Publication, usePublicationQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
 import { PublicationLoader } from "../components/PublicationLoader"
+import { ErrorPage } from "../components/errors/ErrorPage"
+import { NoPublicationIdErrorPage } from "../components/errors/NoPublicationIdErrorPage"
+import { PublicationPage } from "../components/PublicationPage"
 
-const Home = () => {
-  const router = useRouter()
-  React.useEffect(() => {
-    router.push("/26088819")
-  }, [router])
-
-  return <PublicationLoader />
+/**
+ * Renders the publication page given a publication id
+ */
+const PublicationPageWrapper = () => {
+  const { asPath } = useRouter()
+  const id = asPath.slice(1)
+  const { data, loading, error } = usePublicationQuery({
+    variables: { id },
+  })
+  return (
+    <>
+      {match({ data, loading, error, id })
+        .with({ id: "" }, () => <NoPublicationIdErrorPage />)
+        .with(
+          { data: { publication: P.select(P.not(undefined)) } },
+          (publication) => (
+            <PublicationPage publication={publication as Publication} />
+          ),
+        )
+        .with({ loading: true }, () => <PublicationLoader />)
+        .with({ error: P.not(undefined) }, () => <ErrorPage />)
+        .otherwise(() => (
+          <> This message should not appear </>
+        ))}
+    </>
+  )
 }
 
 // eslint-disable-next-line import/no-default-export
-export default Home
+export default PublicationPageWrapper
