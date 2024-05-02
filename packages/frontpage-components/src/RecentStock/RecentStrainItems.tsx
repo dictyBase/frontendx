671994f7f1323de5d4493556e199aafd5786d950
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-02 18:08:36 -0500

    style(RecentStrainItems.tsx): add Typography component for better semantic structure and readability
    The changes were made to improve the semantic structure and readability of the RecentStrainItems component. By adding the Typography component with the "Strains" heading, the component now has a clearer hierarchy and is easier to understand for developers working on the codebase.

--- packages/frontpage-components/src/RecentStock/RecentStrainItems.tsx
+++ packages/frontpage-components/src/RecentStock/RecentStrainItems.tsx
@@ -1,16 +1,21 @@
-import { Grid, makeStyles } from "@material-ui/core"
+import { Grid, Typography, makeStyles } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
 import { map as Amap } from "fp-ts/Array"
 import { RecentStockItem } from "./RecentStockItem"
 import { recentStrainItems } from "./recentStockItemsData"
 
 const useRecentStrainItemStyles = makeStyles({
-  root: {},
+  root: {
+    rowGap: "0.25rem"
+  },
 })
 const RecentStrainItems = () => {
   const { root } = useRecentStrainItemStyles()
   return (
-    <Grid container className={root}>
+    <Grid container direction="column" alignItems="center" className={root}>
+      <Grid item>
+        <Typography variant="h3"> Strains </Typography>
+      </Grid>
       {pipe(
         recentStrainItems,
         Amap(({ id, name, dateAdded }) => (
