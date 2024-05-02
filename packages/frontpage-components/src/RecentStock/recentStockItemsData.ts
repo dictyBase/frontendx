bdb0ad6d08665b52cdd3d5cdb689bde5f6a60144
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-02 02:28:47 -0500

    feat(recentStockItemsData.ts): add recentPlasmidItems array to store plasmid data alongside strain data
    The recentPlasmidItems array is added to store plasmid data alongside the existing strain data in the recentStrainItems array. This change allows for better organization and separation of different types of items within the recent stock items data file.

--- packages/frontpage-components/src/RecentStock/recentStockItemsData.ts
+++ packages/frontpage-components/src/RecentStock/recentStockItemsData.ts
@@ -1,17 +1,47 @@
 const recentStrainItems = [
-  { name: "HAD213", dateAdded: "yesterday" },
   {
+    id: "DBS0236123",
+    name: "HAD213",
+    dateAdded: "yesterday",
+  },
+  {
+    id: "DBS0236123",
     name: "RAD301",
     dateAdded: "3 days ago",
   },
   {
+    id: "DBS0236123",
     name: "TUN410",
     dateAdded: "last week",
   },
   {
+    id: "DBS0236123",
     name: "DTP5012",
     dateAdded: "2 weeks ago",
   },
 ]
 
-export { recentStrainItems }
+const recentPlasmidItems = [
+  {
+    id: "DBP0236123",
+    name: "pCpnA-GFP",
+    dateAdded: "yesterday",
+  },
+  {
+    id: "DBP0236123",
+    name: "pCreCpnAKO",
+    dateAdded: "3 days ago",
+  },
+  {
+    id: "DBP0236123",
+    name: "pGFP-CpnA",
+    dateAdded: "last week",
+  },
+  {
+    id: "DBP0236123",
+    name: "pLoxNeoIII",
+    dateAdded: "2 weeks ago",
+  },
+]
+
+export { recentStrainItems, recentPlasmidItems }
