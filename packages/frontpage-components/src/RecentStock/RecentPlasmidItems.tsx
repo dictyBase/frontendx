c642bd70c8bb41988473172843df252a0128bc0d
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-02 18:08:53 -0500

    style(RecentPlasmidItems.tsx): add Typography component and adjust Grid layout for better readability
    The changes were made to improve the styling and layout of the RecentPlasmidItems component. By adding the Typography component and adjusting the Grid layout, the readability and visual presentation of the component are enhanced.

--- packages/frontpage-components/src/RecentStock/RecentPlasmidItems.tsx
+++ packages/frontpage-components/src/RecentStock/RecentPlasmidItems.tsx
@@ -1,17 +1,22 @@
-import { Grid, makeStyles } from "@material-ui/core"
+import { Grid, Typography, makeStyles } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
 import { map as Amap } from "fp-ts/Array"
 import { RecentStockItem } from "./RecentStockItem"
 import { recentPlasmidItems } from "./recentStockItemsData"
 
 const useRecentPlasmidItemStyles = makeStyles({
-  root: {},
+  root: {
+    rowGap: "0.25rem"
+  },
 })
 
 const RecentPlasmidItems = () => {
   const { root } = useRecentPlasmidItemStyles()
   return (
-    <Grid container className={root}>
+    <Grid container direction="column" alignItems="center" className={root}>
+      <Grid item>
+        <Typography variant="h3"> Plasmids </Typography>
+      </Grid>
       {pipe(
         recentPlasmidItems,
         Amap(({ id, name, dateAdded }) => (
