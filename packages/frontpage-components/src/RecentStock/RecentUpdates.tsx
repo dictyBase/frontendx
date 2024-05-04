baee286c54211fe3fc9f42a4571e21e50afd301a
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-04 02:15:22 -0500

    style(RecentUpdates.tsx): add boxShadow to main container for better visual appeal
    The changes were made to improve the visual appeal and layout structure of the RecentUpdates component in the frontpage-components package. The addition of boxShadow to the main container enhances the visual aesthetics, while adding the spacing prop to the Grid component improves the overall layout structure for better readability and user experience.

--- packages/frontpage-components/src/RecentStock/RecentUpdates.tsx
+++ packages/frontpage-components/src/RecentStock/RecentUpdates.tsx
@@ -8,6 +8,7 @@ const useRecentUpdateStyles = makeStyles({
   main: {
     borderRadius: "10px",
     backgroundColor: "#eff8fb",
+    boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)"
   },
   inner: {
     paddingTop: "10px",
@@ -22,7 +23,7 @@ const RecentUpdates = () => {
     <Container disableGutters className={main}>
       <TitleBox content="New Stock Center Items" icon={<NewReleasesIcon />} />
       <Container className={inner}>
-        <Grid direction="column" container>
+        <Grid spacing={3} direction="column" container>
           <Grid item>
             <RecentStrainItems />
           </Grid>
