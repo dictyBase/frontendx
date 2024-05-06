e0cf2fe3d1ef070021a9182e2a2bd672160194d4
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-06 10:36:01 -0500

    style(Featured.tsx): fix formatting of boxShadow property for better readability
    The boxShadow property in the Featured component's styles has been reformatted to improve readability and maintain consistency in the codebase.

--- packages/frontpage-components/src/Featured.tsx
+++ packages/frontpage-components/src/Featured.tsx
@@ -20,7 +20,8 @@ const useFeaturedStyles = makeStyles({
     borderRadius: "10px",
     backgroundColor: "#eff8fb",
     color: "#04313f",
-    boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)"
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
   },
   link: {
     textDecoration: "underline",
