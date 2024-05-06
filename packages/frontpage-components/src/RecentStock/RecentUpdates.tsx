9691f67e003ccb5ca93785dea210fd4e6dcfbd4e
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-06 10:32:43 -0500

    style(RecentUpdates.tsx): fix import statement for NewReleasesIcon
    The commit fixes the import statement for the NewReleasesIcon component to be consistent with the other imports. Additionally, the boxShadow property is now formatted on multiple lines for better readability and maintainability.

--- packages/frontpage-components/src/RecentStock/RecentUpdates.tsx
+++ packages/frontpage-components/src/RecentStock/RecentUpdates.tsx
@@ -1,14 +1,15 @@
 import { Container, Grid, makeStyles } from "@material-ui/core"
+import NewReleasesIcon from "@material-ui/icons/NewReleases"
 import { TitleBox } from "./TitleBox"
 import { RecentStrainItems } from "./RecentStrainItems"
 import { RecentPlasmidItems } from "./RecentPlasmidItems"
-import NewReleasesIcon from '@material-ui/icons/NewReleases'
 
 const useRecentUpdateStyles = makeStyles({
   main: {
     borderRadius: "10px",
     backgroundColor: "#eff8fb",
-    boxShadow: "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)"
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
   },
   inner: {
     paddingTop: "10px",
