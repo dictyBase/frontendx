d57dc47ed165920611e3b447476c4e5f3bf4d8ca
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-06 21:42:31 -0500

    style(DictyNews.tsx): restructure DictyNews component to improve readability and maintainability
    The DictyNews component has been refactored to improve code organization and readability. The component has been split into smaller, more focused components such as DictyNewsTitle, NewsList, and MoreNewsLink. This restructuring enhances the maintainability of the codebase and makes it easier to understand and modify individual parts of the component.

--- packages/frontpage-components/src/DictyNews.tsx
+++ packages/frontpage-components/src/DictyNews.tsx
@@ -1,28 +1,121 @@
-import { Box, Typography, Grid } from "@material-ui/core"
+import {
+  Container,
+  Typography,
+  Grid,
+  Button,
+  makeStyles,
+} from "@material-ui/core"
+import AnnouncementIcon from "@material-ui/icons/Announcement"
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow"
+import { Link } from "react-router-dom"
 import { pipe } from "fp-ts/function"
 import { makeBy as AmakeBy, map as Amap } from "fp-ts/Array"
 
-const DictyNews = () => (
-  <Box>
-    <Grid direction="column" spacing={1} container>
+const useDictyNewsStyles = makeStyles((theme) => ({
+  root: {
+  },
+  main: {
+    height: "440px",
+  },
+  top: {
+    borderTopLeftRadius: "10px",
+    borderTopRightRadius: "10px",
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
+  },
+  middle: {
+    boxShadow: "inset 0px -2px",
+  },
+  bottom: {
+    borderBottomLeftRadius: "10px",
+    borderBottomRightRadius: "10px",
+    boxShadow:
+      "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
+  },
+  newsListItem: {
+    overflow: "auto",
+  },
+  link: {
+    color: theme.palette.primary.main,
+    "&:hover": {
+      backgroundColor: "inherit",
+      color: "red",
+    },
+  },
+  newsIcon: {
+    display: "block",
+  },
+}))
+
+const DictyNewsTitle = () => {
+  const { newsIcon } = useDictyNewsStyles()
+  return (
+    <Grid container spacing={1} alignItems="center">
       <Grid item>
         <Typography variant="h1">Dicty News</Typography>
       </Grid>
-      {pipe(
-        AmakeBy(5, (index) => index),
-        Amap((key) => (
-          <Grid key={key} item>
-            <Typography>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </Typography>
-          </Grid>
-        )),
-      )}
+      <Grid item>
+        <AnnouncementIcon className={newsIcon} />
+      </Grid>
     </Grid>
-  </Box>
+  )
+}
+
+const NewsList = () => (
+  <Grid container spacing={1} direction="column">
+    {pipe(
+      AmakeBy(7, (index) => index),
+      Amap((key) => (
+        <Grid key={key} item>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Typography>
+        </Grid>
+      )),
+    )}
+  </Grid>
 )
+
+const MoreNewsLink = () => {
+  const { link } = useDictyNewsStyles()
+  return (
+    <Grid justifyContent="flex-end" container>
+      <Grid item>
+        <Link to="/#">
+          <Button className={link} endIcon={<DoubleArrowIcon />}>
+            <Typography variant="h2"> More News </Typography>
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
+  )
+}
+
+const DictyNews = () => {
+  const { root, main, newsListItem } = useDictyNewsStyles()
+  return (
+    <Container className={root}>
+      <Grid
+        direction="column"
+        spacing={1}
+        container
+        wrap="nowrap"
+        className={main}>
+        <Grid item>
+          <DictyNewsTitle />
+        </Grid>
+        <Grid item className={newsListItem}>
+          <NewsList />
+        </Grid>
+        <Grid item>
+          <MoreNewsLink />
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
 
 export { DictyNews }
