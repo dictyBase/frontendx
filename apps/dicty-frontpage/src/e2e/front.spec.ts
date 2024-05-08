a60317e7bd0781bab64baef7580f2a73d402fda9
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-08 18:59:43 -0500

    feat(e2e): add e2e tests for content pages and front page of the application
    The e2e tests for content pages and the front page of the application have been added to ensure that the pages are properly rendered and have the correct title. This helps in verifying the functionality and appearance of the pages, improving the overall quality and reliability of the application.

--- apps/dicty-frontpage/src/e2e/front.spec.ts
+++ apps/dicty-frontpage/src/e2e/front.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from "@playwright/test"
 
+const frontpageUrl = "http://localhost:3004" 
+
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3004")
+  await page.goto(frontpageUrl)
 
   await expect(page).toHaveTitle(/dictyBase/)
 })
 
 test(" title", async ({ page }) => {
-  await page.goto("http://localhost:3004")
+  await page.goto(frontpageUrl)
 
   await expect(page).toHaveTitle(/dictyBase/)
 })
