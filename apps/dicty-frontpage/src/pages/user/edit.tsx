850cf0e4d8ebe3b315f38e0640fc9ae8f9e25c9e
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-09 13:59:40 -0500

    feat(user): add new user edit and show pages
    The commit adds two new files, `edit.tsx` and `show.tsx`, under the `user` directory in the `dicty-frontpage` app. The `edit.tsx` file contains a functional component `Edit` that retrieves user information from the location state and displays it in a Typography component. The `show.tsx` file contains a functional component `Show` that displays user information, including avatar, user details, and a list of editable pages based on user roles. These new pages enhance the user experience by providing functionality to edit and view user information.

---
+++ apps/dicty-frontpage/src/pages/user/edit.tsx
@@ -0,0 +1,14 @@
+import { useLocation } from "react-router-dom"
+import { UserInfoResponse } from "@logto/react"
+import { Typography } from "@material-ui/core"
+import { ACCESS } from "auth"
+
+const Edit = () => {
+  const location = useLocation()
+  const user = location.state as UserInfoResponse
+  return <Typography variant="h1">Edit {user.name}</Typography>
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Edit
+export const access = ACCESS.protected
