73c1ce22d5703025da6527076ee3e1fdd78d178c
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-08 20:16:16 -0500

    feat(playwright.config.ts): use environment variable for baseURL and webServer URL to make them configurable
    The changes allow for the baseURL and webServer URL to be set using the process.env.BASE_URL environment variable, providing flexibility in configuring the URLs for different environments. This enhancement enables easier deployment and testing in various setups without the need to hardcode URLs in the configuration file.

--- apps/dicty-frontpage/playwright.config.ts
+++ apps/dicty-frontpage/playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    baseURL: process.env.BASE_URL,
+ 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
@@ -72,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "yarn dev",
-    url: "http://localhost:3004",
+    url: process.env.BASE_URL,
     reuseExistingServer: !process.env.CI,
   },
 })
