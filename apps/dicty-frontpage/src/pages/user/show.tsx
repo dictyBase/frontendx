216c8891e295208e79cad5f6ff774022c13c1179
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-09 13:59:40 -0500

    feat(user): add new user edit and show pages
    The commit adds two new files, `edit.tsx` and `show.tsx`, under the `user` directory in the `dicty-frontpage` app. The `edit.tsx` file contains a functional component `Edit` that retrieves user information from the location state and displays it in a Typography component. The `show.tsx` file contains a functional component `Show` that displays user information, including avatar, user details, and a list of editable pages based on user roles. These new pages enhance the user experience by providing functionality to edit and view user information.

---
+++ apps/dicty-frontpage/src/pages/user/show.tsx
@@ -0,0 +1,55 @@
+import { useLocation } from "react-router-dom"
+import { Box, Grid, Divider } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import { ACCESS, UserWithRoles, displayOnAuthorized, matchEntries } from "auth"
+import { Avatar, Information, EditablePagesList, Title, NoPages } from "ui-user"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  divider: {
+    border: "1px",
+    height: "3px",
+  },
+  userGrid: {
+    marginTop: theme.spacing(2),
+  },
+}))
+
+const Show = () => {
+  const location = useLocation()
+  const user = location.state as UserWithRoles
+  const classes = useStyles()
+  return (
+    <Box mt={4}>
+      <Grid container direction="row" justifyContent="center">
+        <Grid item>
+          <Title />
+        </Grid>
+      </Grid>
+      <Divider variant="inset" className={classes.divider} />
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        spacing={1}
+        className={classes.userGrid}>
+        <Grid item xs={4} container direction="column" alignItems="center">
+          <Avatar />
+        </Grid>
+        <Grid item xs={4} container direction="column" alignItems="flex-start">
+          <Information user={user} />
+        </Grid>
+        <Grid item xs={4} container direction="column" alignItems="flex-start">
+          {displayOnAuthorized({
+            isAuthorized: matchEntries(user.roles, ["content-admin"]),
+            authorized: <EditablePagesList />,
+            unauthorized: <NoPages />,
+          })}
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Show
+export const access = ACCESS.protected
