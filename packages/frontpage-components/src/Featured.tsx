e8181dc9ace004abb420b45d3d8d537c64ee71c9
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-09 11:32:19 -0500

    feat(Featured.tsx): add support for disabled state in FeaturedLink component
    The Featured component now supports a disabled state for each FeaturedLink item. This allows for better user experience by visually indicating when certain links are disabled and not clickable.

--- packages/frontpage-components/src/Featured.tsx
+++ packages/frontpage-components/src/Featured.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react"
 import { Link } from "react-router-dom"
 import {
   Container,
@@ -5,6 +6,7 @@ import {
   Typography,
   Button,
   makeStyles,
+  Tooltip,
 } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
 import { map as Amap } from "fp-ts/Array"
@@ -26,33 +28,49 @@ const useFeaturedStyles = makeStyles({
   link: {
     textDecoration: "underline",
   },
+  disabledLink: {
+    pointerEvents: "none",
+  },
+  toolTip: {
+  }
 })
 
-const featuredLinks = [
+type FeaturedLink = {
+  icon: ReactNode
+  name: string
+  to: string
+  description: string
+  disabled: boolean
+}
+
+const featuredLinks: Array<FeaturedLink> = [
   {
     icon: <ShoppingCartIcon />,
     name: "Dicty Stock Center",
     to: `${import.meta.env.VITE_APP_STOCKCENTER_URL}`,
     description: "Central repository for Dictyostelium discoideum strains.",
+    disabled: false,
   },
   {
     icon: <SearchIcon />,
     name: "Genome Browser",
     to: "/#",
     description:
-      "Search using a sequence name, gene name, locus, or other landmark.",
+      "Search using a sequence name, gene name, locus, or other landmark. (Coming Soon)",
+    disabled: true,
   },
   {
     icon: <TableChartIcon />,
     name: "Gene Page",
     to: "/#",
     description:
-      "Provides extensive information about a given gene, including links to all its associated sequences, such as Curated Models, Gene Predictions, GenBank Sequences, and ESTs.",
+      "Provides extensive information about a given gene, including links to all its associated sequences, such as Curated Models, Gene Predictions, GenBank Sequences, and ESTs. (Coming Soon)",
+    disabled: true,
   },
 ]
 
 const Featured = () => {
-  const { main, link } = useFeaturedStyles()
+  const { main, link, disabledLink} = useFeaturedStyles()
   return (
     <Container className={main}>
       <Grid spacing={1} direction="column" container>
@@ -63,15 +81,15 @@ const Featured = () => {
         </Grid>
         {pipe(
           featuredLinks,
-          Amap(({ icon, name, to, description }) => (
+          Amap(({ icon, name, to, description, disabled }) => (
             <Grid item>
-              <Link to={to}>
-                <Button color="primary" startIcon={icon}>
-                  <Typography className={link} variant="h2">
-                    {name}
-                  </Typography>
-                </Button>
-              </Link>
+                <Link to={to} className={disabled ? disabledLink : ""}>
+                  <Button disabled={disabled} color="primary" startIcon={icon}>
+                    <Typography className={link} variant="h2">
+                      {name}
+                    </Typography>
+                  </Button>
+                </Link>
               <Typography>{description}</Typography>
             </Grid>
           )),
