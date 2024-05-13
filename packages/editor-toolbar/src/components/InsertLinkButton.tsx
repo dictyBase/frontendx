e19d35fccf8e51a9762991cce5cfd2be0ada265c
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-12 22:32:10 -0500

    feat(InsertLinkButton.tsx): add functionality to insert a link in the editor when the InsertLinkButton is clicked
    The InsertLinkButton component now includes functionality to insert a link in the editor when the button is clicked. This is achieved by updating the editor state on click to toggle a link and insert the specified link text. This enhancement improves the user experience by providing a convenient way to add links within the editor.

--- packages/editor-toolbar/src/components/InsertLinkButton.tsx
+++ packages/editor-toolbar/src/components/InsertLinkButton.tsx
@@ -1,8 +1,25 @@
 import { IconButton } from "@material-ui/core"
 import InsertLinkIcon from '@material-ui/icons/InsertLink'
+import { $createLinkNode, toggleLink, TOGGLE_LINK_COMMAND } from "@lexical/link"
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { $getSelection, $isTextNode } from "lexical"
 
 const InsertLinkButton = () => {
-  return <IconButton><InsertLinkIcon /></IconButton>
+  const [editor] = useLexicalComposerContext()
+
+  const onClick = () => {
+    editor.update(() => {
+      const selection = $getSelection()
+      const selectionNodes = selection.getNodes()
+      editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)
+      toggleLink("test")
+      console.log(selection)
+      console.log(selectionNodes)
+    }) 
+
+  }
+
+  return <IconButton onClick={onClick}><InsertLinkIcon /></IconButton>
 }
 
 export { InsertLinkButton }
