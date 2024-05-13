ab1df63a17c0ea260ee7fd5716adb998e6039550
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-12 19:20:22 -0500

    feat(editor-toolbar): add InsertLinkButton component to the project
    The InsertLinkButton component has been added to the project to provide a button with an insert link icon for use in the editor toolbar. This new component enhances the functionality of the editor toolbar by allowing users to easily insert links into the editor content.

---
+++ packages/editor-toolbar/src/components/InsertLinkButton.tsx
@@ -0,0 +1,8 @@
+import { IconButton } from "@material-ui/core"
+import InsertLinkIcon from '@material-ui/icons/InsertLink'
+
+const InsertLinkButton = () => {
+  return <IconButton><InsertLinkButton /></IconButton>
+}
+
+export { InsertLinkButton }
