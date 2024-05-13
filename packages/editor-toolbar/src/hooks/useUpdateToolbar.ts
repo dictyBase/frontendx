e52aa8af7386400a51a6b9b28d41b9890294b1a3
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-13 16:31:03 -0500

    feat(useUpdateToolbar): add support for updating link properties in the toolbar
    The useUpdateToolbar hook now includes a new function call to updateLinkProperties, which allows for updating link properties in the toolbar. This enhancement provides additional functionality to the toolbar by enabling users to modify link properties seamlessly.

--- packages/editor-toolbar/src/hooks/useUpdateToolbar.ts
+++ packages/editor-toolbar/src/hooks/useUpdateToolbar.ts
@@ -2,20 +2,24 @@ import { useCallback } from "react"
 import { useFontProperties } from "./useFontProperties"
 import { useFontColorProperties } from "./useFontColorProperties"
 import { useBlockTypeProperties } from "./useBlockTypeProperties"
+import { useLinkProperties } from "./useLinkProperties"
 
 const useUpdateToolbar = () => {
   const updateFontProperties = useFontProperties()
   const updateTextColorProperties = useFontColorProperties()
   const updateBlockTypeProperties = useBlockTypeProperties()
+  const updateLinkProperties = useLinkProperties() 
 
   return useCallback(() => {
     updateFontProperties()
     updateTextColorProperties()
     updateBlockTypeProperties()
+    updateLinkProperties()
   }, [
     updateBlockTypeProperties,
     updateFontProperties,
     updateTextColorProperties,
+    updateLinkProperties,
   ])
 }
 
