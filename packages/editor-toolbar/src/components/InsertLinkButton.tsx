4a8a67f4956ec914f5d96bf3806d552f8f749ac9
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-13 16:32:05 -0500

    feat(InsertLinkButton.tsx): add functionality to toggle link creation based on isLink state
    The code now includes functionality to toggle link creation based on the isLink state. When the isLink state is true, the editor will dispatch the TOGGLE_LINK_COMMAND with a null value. When the isLink state is false, the editor will dispatch the TOGGLE_LINK_COMMAND with an empty string. This change enhances the InsertLinkButton component's behavior by dynamically handling link creation based on the isLink state.

--- packages/editor-toolbar/src/components/InsertLinkButton.tsx
+++ packages/editor-toolbar/src/components/InsertLinkButton.tsx
@@ -3,23 +3,31 @@ import InsertLinkIcon from '@material-ui/icons/InsertLink'
 import { $createLinkNode, toggleLink, TOGGLE_LINK_COMMAND } from "@lexical/link"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { $getSelection, $isTextNode } from "lexical"
+import { useAtom } from "jotai"
+import { match } from "ts-pattern"
+import { isLinkAtom } from "../context/atomConfigs"
+import { useActiveClass } from "../hooks/useActiveClass"
 
 const InsertLinkButton = () => {
   const [editor] = useLexicalComposerContext()
+  const [isLink, setIsLink] = useAtom(isLinkAtom)
+  const isActive = useActiveClass(isLinkAtom)
 
   const onClick = () => {
     editor.update(() => {
-      const selection = $getSelection()
-      const selectionNodes = selection.getNodes()
-      editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)
-      toggleLink("test")
-      console.log(selection)
-      console.log(selectionNodes)
+      match(isLink)
+        .with(true, () => {
+          editor.dispatchCommand(TOGGLE_LINK_COMMAND, null)
+        })
+        .with(false, () => {
+          editor.dispatchCommand(TOGGLE_LINK_COMMAND, "")
+        })
+        .exhaustive()
     }) 
 
   }
 
-  return <IconButton onClick={onClick}><InsertLinkIcon /></IconButton>
+  return <IconButton className={isActive} onClick={onClick}><InsertLinkIcon /></IconButton>
 }
 
 export { InsertLinkButton }
