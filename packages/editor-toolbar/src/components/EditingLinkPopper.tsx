826720d5580fccc5d563dc6afe536a6ce51c7571
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-13 21:46:07 -0500

    feat(EditingLinkPopper.tsx): add a new component EditingLinkPopper to handle link editing functionality
    The EditingLinkPopper component is added to the project to handle link editing functionality within the editor toolbar. It utilizes various libraries and functions from fp-ts and lexical to manage the anchor element for the popper, determine if a link is present, and control its appearance based on certain conditions. The component listens for editor state updates and dynamically sets the anchor element for the popper to display a TextField for editing links.

---
+++ packages/editor-toolbar/src/components/EditingLinkPopper.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react"
+import { Popper, TextField } from "@material-ui/core"
+import { $getSelection } from "lexical"
+import { useAtomValue } from "jotai"
+import { $isLinkNode, LinkNode } from "@lexical/link"
+import {
+  TextNode,
+  $isRangeSelection,
+  $isTextNode,
+  LexicalEditor,
+} from "lexical"
+import {
+  head as Ahead,
+  reduce as Areduce,
+  map as Amap,
+  filter as Afilter,
+} from "fp-ts/Array"
+import {
+  fromNullable as OfromNullable,
+  toNullable as OtoNullable,
+  getOrElse as OgetOrElse,
+  bindTo as ObindTo,
+  bind as Obind,
+  let as Olet,
+  Do as ODo,
+  map as Omap,
+  none,
+} from "fp-ts/Option"
+import { MonoidAll } from "fp-ts/boolean"
+import { pipe } from "fp-ts/function"
+import { isLinkAtom } from "../context/atomConfigs"
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+
+const getAnchorElement = (editor: LexicalEditor) => {
+  const selection = $getSelection()
+  if (!$isRangeSelection(selection)) return none
+  return pipe(
+    ODo,
+    Obind("textNode", () =>
+      pipe(selection.getNodes(), Afilter($isTextNode), Ahead),
+    ),
+    Obind("linkNode", ({ textNode }) =>
+      pipe(textNode.getParents(), Afilter($isLinkNode), Ahead),
+    ),
+    Obind("anchorElement", ({ linkNode }) =>
+      OfromNullable(linkNode.exportDOM(editor).element),
+    ),
+    Omap(({ anchorElement }) => anchorElement),
+  )
+}
+
+const EditingLinkPopper = () => {
+  const [editor] = useLexicalComposerContext()
+  const [anchorElement, setAnchorElement] = useState<HTMLElement | null>(null)
+  const isLink = useAtomValue(isLinkAtom)
+  const shouldAppear = MonoidAll.concat(isLink as boolean, editor.isEditable())
+
+  useEffect(() => {
+    editor.registerUpdateListener(({ editorState }) => {
+      editorState.read(() => {
+        setAnchorElement(pipe(editor, getAnchorElement, OtoNullable))
+        console.log("popper")
+        console.log(shouldAppear)
+      })
+    })
+  }, [editor, setAnchorElement])
+
+  console.log(anchorElement)
+  return (
+    <Popper anchorEl={anchorElement} open={shouldAppear}>
+      <TextField />
+    </Popper>
+  )
+}
+
+export { EditingLinkPopper }
