73934b98f7f2b3fff35c2507beb802b0f7183854
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-13 21:45:47 -0500

    feat(InsertLinkButton.tsx): add EditingLinkPopper component to InsertLinkButton for editing link functionality
    The changes in this commit focus on improving the code style by organizing imports and formatting the code for better readability. Additionally, a new EditingLinkPopper component is added to the InsertLinkButton component to provide functionality for editing links within the editor toolbar.

--- packages/editor-toolbar/src/components/InsertLinkButton.tsx
+++ packages/editor-toolbar/src/components/InsertLinkButton.tsx
@@ -1,10 +1,10 @@
 import { IconButton } from "@material-ui/core"
-import InsertLinkIcon from '@material-ui/icons/InsertLink'
-import { $createLinkNode, toggleLink, TOGGLE_LINK_COMMAND } from "@lexical/link"
+import InsertLinkIcon from "@material-ui/icons/InsertLink"
+import { TOGGLE_LINK_COMMAND } from "@lexical/link"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import { $getSelection, $isTextNode } from "lexical"
 import { useAtom } from "jotai"
 import { match } from "ts-pattern"
+import { EditingLinkPopper } from "./EditingLinkPopper"
 import { isLinkAtom } from "../context/atomConfigs"
 import { useActiveClass } from "../hooks/useActiveClass"
 
@@ -23,11 +23,17 @@ const InsertLinkButton = () => {
           editor.dispatchCommand(TOGGLE_LINK_COMMAND, "")
         })
         .exhaustive()
-    }) 
-
+    })
   }
 
-  return <IconButton className={isActive} onClick={onClick}><InsertLinkIcon /></IconButton>
+  return (
+    <>
+      <IconButton className={isActive} onClick={onClick}>
+        <InsertLinkIcon />
+      </IconButton>
+      <EditingLinkPopper />
+    </>
+  )
 }
 
 export { InsertLinkButton }
