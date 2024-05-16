1c1c00e51f11f26048865fc32f597589c445870e
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-16 10:23:44 -0500

    refactor(handlers.ts): refactor Gene query handler to use ts-pattern library for better readability and maintainability
    The Gene query handler in the handlers.ts file has been refactored to use the ts-pattern library, which simplifies the conditional logic and improves the readability of the code. This refactor enhances maintainability and makes it easier to add new cases for different genes in the future.

--- apps/genome-page/mocks/handlers.ts
+++ apps/genome-page/mocks/handlers.ts
@@ -1,37 +1,18 @@
-import { graphql } from "msw"
 import { mockGeneOntologyAnnotationQuery } from "dicty-graphql-schema"
 import { mockGene } from "./mockGene"
-import { mockGeneLimit } from "./mockGeneLimit"
 import { mockGenePiaA } from "./piaAMocks/mockGenePiaA"
-import { mockGenePiaALimit } from "./piaAMocks/mockGenePiaALimit"
 import { mockGeneAda2 } from "./ada2Mocks/mockGeneAda2"
-import { mockGeneAda2Limit } from "./ada2Mocks/mockGeneAda2Limit"
+import { match } from "ts-pattern"
 
 export const handlers = [
   // Handles the Gene query: https://github.com/dictyBase/dicty-graphql-schema/blob/develop/src/queries/gene.graphql
   // Implementation details here: https://github.com/dictyBase/genomepage/pull/825#issuecomment-977246804
-  graphql.query("Gene", (request, response, context) => {
-    const gene = request.body?.variables?.gene
-    const limit = request.body?.variables?.limit
-
-    if (gene === "sadA") {
-      if (limit === 3) {
-        return response(context.data({ ...mockGeneLimit }))
-      }
-      return response(context.data({ ...mockGene }))
-    }
-    if (gene === "piaA") {
-      if (limit === 3) {
-        return response(context.data({ ...mockGenePiaALimit }))
-      }
-      return response(context.data({ ...mockGenePiaA }))
-    }
-    if (gene === "ada2") {
-      if (limit === 3) {
-        return response(context.data({ ...mockGeneAda2Limit }))
-      }
-      return response(context.data({ ...mockGeneAda2 }))
-    }
-    return response(context.errors([{ message: `No mock for ${gene}` }]))
+  mockGeneOntologyAnnotationQuery((request, response, context) => {
+    const { gene } = request.variables
+    return match(gene)
+      .with("sadA", () => response(context.data({ geneOntologyAnnotation: mockGene.gene.goas })))
+      .with("piaA", () => response(context.data({ geneOntologyAnnotation: mockGenePiaA.gene.goas })))
+      .with("ada2", () => response(context.data({ geneOntologyAnnotation: mockGeneAda2.gene.goas })))
+      .otherwise((unmockedGene) => response(context.errors([{ message: `No mock for ${unmockedGene}` }])))
   }),
 ]
