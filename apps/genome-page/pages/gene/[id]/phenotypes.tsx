6119e2835e0b6af4c054fd25950f573931c8a8c4
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-17 13:19:43 -0500

    refactor(phenotypes.tsx): refactor PhenotypesPageWrapper to use ts-pattern for better readability and maintainability
    The code in PhenotypesPageWrapper has been refactored to use ts-pattern for pattern matching, which improves the readability and maintainability of the code. This change makes it easier to handle different states such as loading, errors, and data, resulting in more concise and structured code.

--- apps/genome-page/pages/gene/[id]/phenotypes.tsx
+++ apps/genome-page/pages/gene/[id]/phenotypes.tsx
@@ -1,15 +1,16 @@
 import { PhenotypesContainer } from "components/features/Phenotypes/PhenotypesContainer"
 import { GraphQLErrorPage } from "components/errors/GraphQLErrorPage"
 import { PhenotypesLoader } from "components/features/Phenotypes/PhenotypesLoader"
-import { useGeneQuery, GeneQuery } from "dicty-graphql-schema"
+import { useListStrainsWithGeneQuery } from "dicty-graphql-schema"
 import { useRouter } from "next/router"
+import { match, P } from "ts-pattern"
 /*
     Renders the Phenotypes page given a gene id
 */
 const PhenotypesPageWrapper = () => {
   const { query } = useRouter()
   const gene = query.id as string
-  const { loading, error, data } = useGeneQuery({
+  const result = useListStrainsWithGeneQuery({
     variables: {
       gene,
     },
@@ -17,9 +18,22 @@ const PhenotypesPageWrapper = () => {
 
   return (
     <>
-      {loading ? <PhenotypesLoader /> : <></>}
-      {error ? <GraphQLErrorPage error={error} /> : <></>}
-      {data ? <PhenotypesContainer gene={data as GeneQuery} /> : <></>}
+      {match(result)
+        .with({ loading: true }, () => <PhenotypesLoader />)
+        .with({ error: P.select(P.not(undefined)) }, (error) => (
+          <GraphQLErrorPage error={error} />
+        ))
+        .with(
+          {
+            data: {
+              listStrainsWithGene: P.select(P.array({ id: P.string })),
+            },
+          },
+          (strains) => <PhenotypesContainer strains={strains} />,
+        )
+        .otherwise(() => (
+          <> This message should not appear. </>
+        ))}
     </>
   )
 }
