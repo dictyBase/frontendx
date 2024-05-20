1ae698ff3b8890440e68a622abfd52c7588e8b0f
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-20 01:12:29 -0500

    style(InsertImageHelpers.ts): format import statements for better readability
    The import statements have been reformatted to align with the project's coding style guidelines, improving the overall readability and consistency of the codebase.

--- packages/image-plugin/src/InsertImageHelpers.ts
+++ packages/image-plugin/src/InsertImageHelpers.ts
@@ -17,7 +17,10 @@ import {
   bindTo as ObindTo,
   bind as Obind,
 } from "fp-ts/Option"
-import { $isFlexLayoutNode, FlexLayoutNode } from "@dictybase/flex-layout-plugin"
+import {
+  $isFlexLayoutNode,
+  FlexLayoutNode,
+} from "@dictybase/flex-layout-plugin"
 
 const getDifference = (first: number, second: number) =>
   Math.abs(first - second)
