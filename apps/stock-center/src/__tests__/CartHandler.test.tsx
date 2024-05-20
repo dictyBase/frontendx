8635e3d33eb731ea6b2cd913a72167067ab974c0
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-20 16:02:51 -0500

    feat(CartHandler.test.tsx): add support for creating a Jotai store in tests to manage state
    The changes introduce the creation of a Jotai store in the test file CartHandler.test.tsx to manage the state of the cartAtom. This allows for better control and manipulation of the state during testing scenarios, improving the overall testability and reliability of the CartHandler component.

--- apps/stock-center/src/__tests__/CartHandler.test.tsx
+++ apps/stock-center/src/__tests__/CartHandler.test.tsx
@@ -1,15 +1,18 @@
 import { test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
-import { Provider } from "jotai"
+import { Provider, createStore } from "jotai"
 import CartHandler from "../pages/cart"
 import { cartAtom } from "../cartState"
 import { testItems } from "../mocks/cartData"
 
+const store = createStore()
+
 test("Renders ShoppingCartWithItems component if there are items in the cart state. Does not render EmptyShoppingCart", () => {
+  store.set(cartAtom, { strainItems: testItems, plasmidItems: [], maxItems: 12 })
   render(
     <MemoryRouter>
-      <Provider initialValues={[[cartAtom, { strainItems: testItems }]]}>
+      <Provider store={store}>
         <CartHandler />
       </Provider>
     </MemoryRouter>,
@@ -21,9 +24,10 @@ test("Renders ShoppingCartWithItems component if there are items in the cart sta
 })
 
 test("Renders EmptyShoppingCart component if there are items in the cart state. Does Not render ShoppingCartWithItems", () => {
+  store.set(cartAtom, { strainItems: [], plasmidItems: [], maxItems: 12 })
   render(
     <MemoryRouter>
-      <Provider initialValues={[[cartAtom, { strainItems: [] }]]}>
+      <Provider store={store}>
         <CartHandler />
       </Provider>
     </MemoryRouter>,
