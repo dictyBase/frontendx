33681982aba9e07a194004bf2b914d8f3b62d629
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-21 23:52:27 -0500

    feat(SummaryContainer.tsx): update import statement for GeneSummaryQuery to reflect changes in the dicty-graphql-schema
    The import statement for GeneQuery has been updated to GeneSummaryQuery to accurately reflect the changes made in the dicty-graphql-schema. Unused imports and variables have been removed, and the code structure has been simplified for better readability and maintainability.

--- apps/genome-page/components/features/Summary/SummaryContainer.tsx
+++ apps/genome-page/components/features/Summary/SummaryContainer.tsx
@@ -1,56 +1,29 @@
 import Typography from "@material-ui/core/Typography"
 import { PanelWrapper } from "components/panels/PanelWrapper"
 import { Layout } from "components/layout/Layout"
-import { GeneQuery } from "dicty-graphql-schema"
+import { GeneSummaryQuery } from "dicty-graphql-schema"
 import { useRouter } from "next/router"
-import {
-  containerGenerator,
-  createRouteFromString,
-} from "../../../common/utils/containerGenerator"
+import { GeneralInfoPanel } from "components/features/Summary/Panels/GeneralInfoPanel"
 
 interface SummaryContainerProperties {
-  gene: GeneQuery
+  geneSummary: GeneSummaryQuery
 }
 
-interface ChildContent {
-  panelProps: {
-    id: string
-    title: string
-    route: string
-  }
-  child: JSX.Element | undefined
-}
 
-const SummaryContainer = ({ gene }: SummaryContainerProperties) => {
+const SummaryContainer = ({ geneSummary }: SummaryContainerProperties) => {
   const { query } = useRouter()
+  const { geneGeneralInformation } = geneSummary
   const geneId = query.id as string
-
   return (
     <Layout
       gene={geneId}
       title={`Gene Summary for ${geneId}`}
-      description={`Gene information for ${geneId}`}>
+      description={`Gene information for ${geneId}`}
+    >
       <Typography component="div">
-        {(
-          containerGenerator(
-            [
-              "generalInformation",
-              "gene",
-              "listGeneProductInfo",
-              "getAssociatedSequnces",
-              "getLinks",
-              "allPublications",
-            ],
-            gene,
-          ) as ChildContent[]
-        ).map((item) => (
-          <PanelWrapper
-            key={item.panelProps.id}
-            title={createRouteFromString(item!.panelProps.title, gene)}
-            route={createRouteFromString(item!.panelProps.route, gene)}>
-            {item!.child}
-          </PanelWrapper>
-        ))}
+        <PanelWrapper title="General Information" route="test">
+          <GeneralInfoPanel generalInformation={geneGeneralInformation} />
+        </PanelWrapper>
       </Typography>
     </Layout>
   )
