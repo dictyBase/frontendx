2e95d7d68f0e41a41bbd476a3b0840901e1fa40a
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-21 23:17:43 -0500

    feat(genome-page): update mock handlers to use mockGeneSummaryQuery instead of mockGeneGeneralInformationQuery
    The mock handlers in genome-page now use mockGeneSummaryQuery instead of mockGeneGeneralInformationQuery for better clarity and consistency. The mockGeneralInfoData and mockGeneralInfoPiaA files have been updated to include gene IDs and use empty arrays for alt_gene_name and alt_protein_names to align with the expected data structure.

--- apps/genome-page/mocks/mockGeneralInfoData.ts
+++ apps/genome-page/mocks/mockGeneralInfoData.ts
@@ -4,10 +4,11 @@ import { GeneGeneralInfo } from "dicty-graphql-schema"
  * Reference: http://dictybase.org/gene/piaA
  */
 const mockGeneralInfoData: GeneGeneralInfo = {
+  id: "DDB_G0288511",
   name_description: ["sadA = Substrate ADhesion"],
-  alt_gene_name: undefined,
+  alt_gene_name: [],
   gene_product: "substrate adhesion molecule",
-  alt_protein_names: undefined,
+  alt_protein_names: [],
   description:
     "EGF repeat-containing 9 transmembrane molecule involved in substrate adhesion",
 }
