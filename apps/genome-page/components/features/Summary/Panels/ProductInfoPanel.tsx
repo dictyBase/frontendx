fa45fd0281405ec9a11bf33a0bce58fa978dd61a
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-24 00:40:39 -0500

    refactor(ProductInfoPanel.tsx): refactor ProductInfoPanel component to use fp-ts Option monad for better error handling and readability
    The ProductInfoPanel component has been refactored to use the fp-ts Option monad for better error handling and improved readability. This change simplifies the code by using functional programming concepts to handle optional values and streamline the logic flow within the component.

--- apps/genome-page/components/features/Summary/Panels/ProductInfoPanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/ProductInfoPanel.tsx
@@ -1,7 +1,15 @@
 import { GeneSummaryQuery } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
 import { map as Amap, head as Ahead } from "fp-ts/Array"
-import { map as Omap } from "fp-ts/Option"
+import {
+  Do as ODo,
+  bind as Obind,
+  bindTo as ObindTo,
+  let as Olet,
+  map as Omap,
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
 import { OtherError } from "components/errors/OtherError"
 import { LeftDisplay } from "components/panels/LeftDisplay"
 import { ItemDisplay } from "components/panels/ItemDisplay"
@@ -16,26 +24,42 @@ type Properties = {
 /**
  * Panel to display Product Info on the Gene Summary page.
  */
-const ProductInfoPanel = ({ geneProductInformation }: Properties) => {
-  if (!geneProductInformation) return <OtherError />
-  const productInformation = geneProductInformation[0]
-  return pipe(
-    productInformation,
-    (productInfo): Array<{ id: ContentId; value: any }> => [
-      { id: "Protein Coding Gene", value: productInfo.protein_coding_gene },
-      { id: "Protein Length", value: productInfo.protein_length },
-      { id: "Molecular Weight", value: productInfo.protein_molecular_weight },
-      { id: "More Protein Data", value: productInfo.more_protein_data },
-      { id: "Genomic Coords.", value: productInfo.genomic_coords },
-    ],
-    Amap(({ id, value }) => (
-      <ItemDisplay key={id}>
-        <LeftDisplay>{id}</LeftDisplay>
-        <RightDisplay>{returnPanelContentById(id, value)}</RightDisplay>
-      </ItemDisplay>
-    )),
-    (children) => <div>{children}</div>,
+const ProductInfoPanel = ({ geneProductInformation }: Properties) =>
+  pipe(
+    geneProductInformation,
+    OfromNullable,
+    ObindTo("geneProduct"),
+    Obind("firstGeneProduct", ({ geneProduct }) => Ahead(geneProduct)),
+    Olet(
+      "displayItems",
+      ({ firstGeneProduct }): Array<{ id: ContentId; value: any }> => [
+        {
+          id: "Protein Coding Gene",
+          value: firstGeneProduct.protein_coding_gene,
+        },
+        { id: "Protein Length", value: firstGeneProduct.protein_length },
+        {
+          id: "Molecular Weight",
+          value: firstGeneProduct.protein_molecular_weight,
+        },
+        { id: "More Protein Data", value: firstGeneProduct.more_protein_data },
+        { id: "Genomic Coords.", value: firstGeneProduct.genomic_coords },
+      ],
+    ),
+    Olet("element", ({ displayItems }) =>
+      pipe(
+        displayItems,
+        Amap(({ id, value }) => (
+          <ItemDisplay key={id}>
+            <LeftDisplay>{id}</LeftDisplay>
+            <RightDisplay>{returnPanelContentById(id, value)}</RightDisplay>
+          </ItemDisplay>
+        )),
+        (children) => <div>{children}</div>,
+      ),
+    ),
+    Omap(({ element }) => element),
+    OgetOrElse(() => <OtherError />),
   )
-}
 
 export { ProductInfoPanel }
