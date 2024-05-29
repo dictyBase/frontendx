62c0a34eb7a4f9c5d987fbd69caebb5bde9314c6
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-28 19:03:10 -0500

    feat(AssociatedSequencePanel.tsx): refactor AssociatedSequencePanel component to use fp-ts Option monad for safer handling of data
    The AssociatedSequencePanel component has been refactored to use the fp-ts Option monad for safer handling of data. This change improves the robustness of the component by providing a more functional approach to dealing with potentially nullable values. The use of fp-ts functions like OfromNullable, ObindTo, Olet, Omap, and OgetOrElse ensures that the component handles data in a more concise and reliable manner, reducing the risk of runtime errors related to null or undefined values.

--- apps/genome-page/components/features/Summary/Panels/AssociatedSequencePanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/AssociatedSequencePanel.tsx
@@ -1,48 +1,67 @@
 import React from "react"
+import { pipe } from "fp-ts/function"
+import {
+  bindTo as ObindTo,
+  fromNullable as OfromNullable,
+  let as Olet,
+  map as Omap,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { map as Amap } from "fp-ts/Array"
 import { ItemDisplay } from "components/panels/ItemDisplay"
 import { LeftDisplay } from "components/panels/LeftDisplay"
 import { RightDisplay } from "components/panels/RightDisplay"
-import { GeneQuery } from "dicty-graphql-schema"
+import { GeneSummaryQuery } from "dicty-graphql-schema"
 import { OtherError } from "components/errors/OtherError"
-import { panelGenerator } from "common/utils/panelGenerator"
+import { ContentId, returnPanelContentById } from "common/utils/panelGenerator"
 
 type Properties = {
   /** Array of GO annotations for a particular gene */
-  data: GeneQuery
+  associatedSequences: GeneSummaryQuery["associatedSequences"]
 }
 
 /**
  * Panel to display Associated Sequence Panel on the Gene Summary page.
  */
-const AssociatedSequencePanel = ({ data }: Properties) => {
-  if (!data.getAssociatedSequnces) return <OtherError />
-  const associated_seq = data.getAssociatedSequnces
-
-  const output = panelGenerator(
-    [
+const AssociatedSequencePanel = ({ associatedSequences }: Properties) => {
+  console.log(associatedSequences)
+  return pipe(
+    associatedSequences,
+    OfromNullable,
+    ObindTo("assocSeq"),
+    Olet("displayItems", ({ assocSeq }) => [
       {
-        id: "GenBank Genomic Fragment",
-        value: associated_seq.associated_sequences.genbank_genomic_fragment,
+        id: "GenBank Genomic Fragment" as ContentId,
+        value: pipe(
+          assocSeq.genbank_genomic_fragment,
+          OfromNullable,
+          OgetOrElse(() => ({ name: "", link: "" })),
+        ),
       },
       {
-        id: "GenBank mRNA",
-        value: associated_seq.associated_sequences.genbank_mrna,
+        id: "GenBank mRNA" as ContentId,
+        value: pipe(
+          assocSeq.genbank_mrna,
+          OfromNullable,
+          OgetOrElse(() => ({ name: "", link: "" })),
+        ),
       },
-      { id: "ESTs", value: associated_seq.associated_sequences },
-    ],
-    "getAssociatedSequnces",
-    data,
-  )
-
-  return (
-    <div>
-      {output?.map((item) => (
-        <ItemDisplay key={item.leftDisplay}>
-          <LeftDisplay>{item.leftDisplay}</LeftDisplay>
-          <RightDisplay>{item.rightDisplay}</RightDisplay>
-        </ItemDisplay>
-      ))}
-    </div>
+      { id: "ESTs" as ContentId, value: assocSeq.ests },
+    ]),
+    Olet("element", ({ displayItems }) =>
+      pipe(
+        displayItems,
+        Amap(({ id, value }) => (
+          <ItemDisplay key={id}>
+            <LeftDisplay>{id}</LeftDisplay>
+            <RightDisplay>{returnPanelContentById(id, value)}</RightDisplay>
+          </ItemDisplay>
+        )),
+        (children) => <div>{children}</div>,
+      ),
+    ),
+    Omap(({ element }) => element),
+    OgetOrElse(() => <OtherError />),
   )
 }
 
