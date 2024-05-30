759ee6006240532f48771ef6845697ff4d5fc2aa
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-30 18:50:20 -0500

    refactor(handlers.ts): remove unused mockProductInfo, mockProductInfoPiaA, and mockAssociatedData imports
    The changes were made to clean up the codebase by removing unused imports and updating field names in mock data to align with changes in the schema. This ensures consistency and improves code readability.

--- apps/genome-page/mocks/piaAMocks/mockGeneralInfoPiaA.ts
+++ apps/genome-page/mocks/piaAMocks/mockGeneralInfoPiaA.ts
@@ -9,9 +9,9 @@ const mockGeneralInfoPiaA: GeneGeneralInfo = {
     "pia = PIAnissimo",
     "rictor = Rapamycin-Insensitive Companion of mTOR",
   ],
-  alt_gene_name: ["DG1117", "amiA", "pia", "rictor"],
+  // alt_gene_name: ["DG1117", "amiA", "pia", "rictor"],
   gene_product: "cytosolic regulator of adenylyl cyclase PiaA",
-  alt_protein_names: ["Pianissimo, cytosolic regulator of adenylate cyclase"],
+  synonyms: ["Pianissimo, cytosolic regulator of adenylate cyclase"],
   description:
     "required for receptor-mediated activation of adenylyl cyclase; component of the TORC2 (Tor complex 2) with Tor, Lst8, and Rip3 that plays a role in regulation of adenylate cyclase (ACA) and protein kinase B (PKB) activation during aggregation",
 }
