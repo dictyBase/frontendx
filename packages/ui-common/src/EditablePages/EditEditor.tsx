b023ea2befe66458f82f50d9ad144ba70bebd7c7
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-30 12:28:19 -0500

    feat(EditEditor.tsx): refactor onSave function to use fp-ts TaskEither for better error handling and readability
    The onSave function in EditEditor.tsx has been refactored to use fp-ts TaskEither for improved error handling and readability. This change allows for a more structured approach to handling asynchronous tasks and errors, making the code easier to maintain and understand.

--- packages/ui-common/src/EditablePages/EditEditor.tsx
+++ packages/ui-common/src/EditablePages/EditEditor.tsx
@@ -3,40 +3,73 @@ import {
   type ContentBySlugQuery,
   useUpdateContentMutation,
 } from "dicty-graphql-schema"
+import { pipe } from "fp-ts/function"
+import {
+  Do as TEDo,
+  let as TElet,
+  bind as TEbind,
+  tryCatch as TEtryCatch,
+} from "fp-ts/TaskEither"
 import { Editor } from "@dictybase/editor"
 import { createToolbarWrapper } from "./createToolbarWrapper"
 
 type EditEditorProperties = {
   data: NonNullable<ContentBySlugQuery["contentBySlug"]>
   userId: string
-  token: string
+  getAccessToken: (resource?: string) => Promise<string | undefined>
 }
 
-const EditEditor = ({ data, userId, token }: EditEditorProperties) => {
+enum ErrorType {
+  ACCESS_TOKEN_ERROR,
+  UPDATE_FAILURE,
+}
+
+const accessTokenError = {
+  errorType: ErrorType.ACCESS_TOKEN_ERROR,
+  message: "Could not get access token",
+}
+
+const updateFailureError = {
+  errorType: ErrorType.UPDATE_FAILURE,
+  message: "Could not update content",
+}
+
+const EditEditor = ({ data, userId, getAccessToken }: EditEditorProperties) => {
   const { id, updated_by, updated_at, slug, content } = data
   const [updateContent] = useUpdateContentMutation({
     context: {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getAccessToken}`,
       },
     },
   })
   const navigate = useNavigate()
   const onSave = async (contentValue: string) => {
-    try {
-      await updateContent({
-        variables: {
-          input: {
-            id,
-            updated_by: userId,
-            content: contentValue,
-          },
-        },
-      })
-      navigate("../editable", { relative: "path" })
-    } catch {
-      // Toggle some error notification
-    }
+    const updateTask = pipe(
+      TEDo,
+      TElet("updatedContent", () => contentValue),
+      TEbind("token", () =>
+        TEtryCatch(
+          () =>
+            getAccessToken(import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE),
+          () => accessTokenError,
+        ),
+      ),
+      TEbind("update", ({ updatedContent, token }) =>
+        TEtryCatch(
+          () =>
+            updateContent({
+              variables: {
+                input: { id, updated_by: userId, content: updatedContent },
+              },
+              context: { headers: { Authorization: `Bearer ${token}` } },
+            }),
+          () => updateFailureError,
+        ),
+      ),
+    )
+    await updateTask()
+    navigate("../editable", { relative: "path" })
   }
   const onCancel = () => {
     navigate("../editable", { relative: "path" })
