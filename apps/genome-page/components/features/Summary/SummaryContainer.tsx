1bdbe508dd7b8363b97be86bfb41a42c3db9afe9
Kevin Tun <kevin.tun@northwestern.edu>
2024-05-31 14:17:05 -0500

    feat(SummaryContainer.tsx): replace ProductInfoPanel and AssociatedSequencePanel with ReferencesPanel for displaying gene publications
    The changes were made to improve the SummaryContainer component by replacing the ProductInfoPanel and AssociatedSequencePanel with the ReferencesPanel to display gene publications. Additionally, logic was added to limit the number of displayed publications to 5 for better user experience and performance optimization.

--- apps/genome-page/components/features/Summary/SummaryContainer.tsx
+++ apps/genome-page/components/features/Summary/SummaryContainer.tsx
@@ -5,8 +5,7 @@ import { GeneSummaryQuery } from "dicty-graphql-schema"
 import { useRouter } from "next/router"
 import { GeneralInfoPanel } from "components/features/Summary/Panels/GeneralInfoPanel"
 import { GoaPanel } from "components/features/Summary/Panels/GoaPanel"
-import { ProductInfoPanel } from "components/features/Summary/Panels/ProductInfoPanel"
-import { AssociatedSequencePanel } from "components/features/Summary/Panels/AssociatedSequencePanel"
+import { ReferencesPanel } from "components/features/Summary/Panels/ReferencesPanel"
 
 interface SummaryContainerProperties {
   geneSummary: GeneSummaryQuery
@@ -17,19 +16,35 @@ const SummaryContainer = ({ geneSummary }: SummaryContainerProperties) => {
   const {
     geneGeneralInformation,
     geneOntologyAnnotation,
+    listPublicationsWithGene,
   } = geneSummary
+  const publicationLimit = 5
+  const partialPublicationsList = listPublicationsWithGene.slice(
+    0,
+    publicationLimit,
+  )
   const geneId = query.id as string
   return (
     <Layout
       gene={geneId}
       title={`Gene Summary for ${geneId}`}
-      description={`Gene information for ${geneId}`}>
+      description={`Gene information for ${geneId}`}
+    >
       <Typography component="div">
         <PanelWrapper title="General Information">
           <GeneralInfoPanel generalInformation={geneGeneralInformation} />
         </PanelWrapper>
-        <PanelWrapper title="Gene Ontology Annotations">
+        <PanelWrapper
+          route={`${geneId}/goannotations`}
+          title="Gene Ontology Annotations"
+        >
           <GoaPanel goas={geneOntologyAnnotation} />
+        </PanelWrapper>
+        <PanelWrapper
+          route={`${geneId}/references`}
+          title={`Publications (${publicationLimit} of ${listPublicationsWithGene.length}) `}
+        >
+          <ReferencesPanel publications={partialPublicationsList} />
         </PanelWrapper>
       </Typography>
     </Layout>
