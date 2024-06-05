a869a8e42a9d7f32125a1360f4928574eb8359c5
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-05 10:20:31 -0500

    chore: remove unused files

--- apps/genome-page/pages/gene/[id]/callback.tsx
+++
@@ -1,6 +0,0 @@
-import { OauthCallback } from "components/features/Authentication/OauthCallback"
-
-const OauthCallbackPage = () => <OauthCallback />
-
-// eslint-disable-next-line import/no-default-export
-export default OauthCallbackPage
