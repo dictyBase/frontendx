a3cde3f7fe9dbdbb8bafd1543fae2b9548b6d7ac
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-06 10:06:17 -0500

    refactor(headerLinks.ts): refactor functions to create icon items and header icons for better code organization and readability
    The functions in headerLinks.ts have been refactored to improve code organization and readability. The functions now explicitly create icon items and header icons, making it easier to understand their purpose and usage. This refactoring enhances maintainability and makes the code more modular.

--- packages/auth/src/headerLinks.ts
+++ packages/auth/src/headerLinks.ts
@@ -1,32 +1,32 @@
-/* eslint-disable dot-notation */
-import { iconButtonPipe, defaultIconItems } from "@dictybase/header"
+import { iconButtonPipe, createDefaultIconItems } from "@dictybase/header"
+import { pipe } from "fp-ts/function"
 import { Add, Info, ArrowDownward as Download } from "@material-ui/icons"
 import { v4 as uuid4 } from "uuid"
 
-const authorizedIconItems = [
+const createAuthorizedIconItems = (frontPageUrl: string) => [
   {
-    href: `${
-      import.meta.env["VITE_APP_FRONTPAGE_URL"]
-    }/community/citation/editable`,
+    href: `${frontPageUrl}/community/citation/editable`,
     title: "Cite Us",
     Icon: Add,
     key: uuid4(),
   },
   {
-    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/downloads`,
+    href: `${frontPageUrl}/downloads`,
     title: "Downloads",
     Icon: Download,
     key: uuid4(),
   },
   {
-    href: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/about/editable`,
+    href: `${frontPageUrl}/about/editable`,
     title: "About dictybase",
     Icon: Info,
     key: uuid4(),
   },
 ]
+const createDefaultHeaderIcons = (frontPageUrl: string) =>
+  pipe(frontPageUrl, createDefaultIconItems, iconButtonPipe)
 
-const defaultHeaderLinks = iconButtonPipe(defaultIconItems)
-const authorizedHeaderLinks = iconButtonPipe(authorizedIconItems)
+const createAuthorizedHeaderIcons = (frontPageUrl: string) =>
+  pipe(frontPageUrl, createAuthorizedIconItems, iconButtonPipe)
 
-export { defaultHeaderLinks, authorizedHeaderLinks }
+export { createDefaultHeaderIcons, createAuthorizedHeaderIcons }
