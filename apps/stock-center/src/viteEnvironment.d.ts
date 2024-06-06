d78907de0280aa8914737db93e45ac9861244430
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-06 10:13:51 -0500

    chore(viteEnvironment.d.ts): add typings for Vite environment variables to improve type safety
    By adding typings for Vite environment variables, we improve the type safety of the application. This helps in catching potential errors during development related to accessing these environment variables.

--- apps/stock-center/src/viteEnvironment.d.ts
+++ apps/stock-center/src/viteEnvironment.d.ts
@@ -1,1 +1,13 @@
+/* eslint-disable unicorn/prevent-abbreviations */
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_LOGTO_APPID: string
+  VITE_APP_BASENAME: string
+  VITE_APP_FRONTPAGE_URL: string
+  VITE_APP_STOCKCENTER_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
