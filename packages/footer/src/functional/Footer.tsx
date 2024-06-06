d8a6ec6aceda8ac64b8b59801c63fd4a352304a3
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-06 10:01:44 -0500

    refactor(Footer.tsx): remove unnecessary data manipulation and simplify code structure
    The changes made in this commit involve simplifying the code structure in the Footer component by removing unnecessary data manipulation. The data prop is now directly passed to the Links component without going through additional processing, making the code more straightforward and easier to understand.

--- packages/footer/src/functional/Footer.tsx
+++ packages/footer/src/functional/Footer.tsx
@@ -1,33 +1,27 @@
 import { Box } from "@material-ui/core"
 import { pipe } from "fp-ts/lib/function"
-import { map as Omap, getOrElse, fromNullable } from "fp-ts/Option"
+import { map as Omap, getOrElse } from "fp-ts/Option"
 import { fromChildren, composeChildren, type Comp } from "@dictybase/functional"
 import { v4 as uuid4 } from "uuid"
 import { FooterHead } from "../components/FooterHead"
 import { FooterSponsors } from "../components/FooterSponsors"
 import { FooterContainer } from "../components/FooterContainer"
 import { Links } from "./Links"
-import { footerData } from "../data/footerData"
 import { FooterItem } from "../types"
 
 type FooterProperties = {
-  data?: Array<FooterItem>
+  data: Array<FooterItem>
 }
 
 const footerContainerWrapper = (children: Comp) => (
   <FooterContainer>{children}</FooterContainer>
 )
 
-export const Footer = ({ data }: FooterProperties) => {
-  const linksData = pipe(
-    data,
-    fromNullable,
-    getOrElse(() => footerData),
-  )
-  return pipe(
+export const Footer = ({ data }: FooterProperties) =>
+  pipe(
     [
       <FooterHead key={uuid4()} title="Dicty Community Resource" />,
-      <Links key={uuid4()} data={linksData} />,
+      <Links key={uuid4()} data={data} />,
       <FooterSponsors key={uuid4()} />,
     ],
     fromChildren,
@@ -35,4 +29,3 @@ export const Footer = ({ data }: FooterProperties) => {
     Omap(footerContainerWrapper),
     getOrElse(() => <Box>error in rendering</Box>),
   )
-}
