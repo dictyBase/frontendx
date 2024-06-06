16b5d1409d65d71752823ed01bf5aab69f9131e4
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-06 10:02:47 -0500

    feat(authFooterData.ts): add support for creating footer items with custom URLs for front page and stock center
    The variables and functions in the file have been renamed to improve clarity and consistency. Two new functions, createAuthFooterItems and createFooterItems, have been added to allow the creation of footer items with custom URLs for the front page and stock center. This change enhances the flexibility of the codebase by enabling the customization of footer URLs based on specific requirements.

--- packages/auth/src/data/authFooterData.ts
+++ packages/auth/src/data/authFooterData.ts
@@ -1,17 +1,21 @@
-/* eslint-disable dot-notation */
-const authFooterItems = [
+type CreateFooterItemsProperties = {
+  frontPageUrl: string
+  stockCenterUrl: string
+}
+const createAuthFooterItems = ({
+  frontPageUrl,
+  stockCenterUrl,
+}: CreateFooterItemsProperties) => [
   {
-    url: `${
-      import.meta.env["VITE_APP_FRONTPAGE_URL"]
-    }/research/techniques/editable`,
+    url: `${frontPageUrl}/research/techniques/editable`,
     label: "Techniques",
   },
   {
-    url: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/research/teach/editable`,
+    url: `${frontPageUrl}/research/teach/editable`,
     label: "Teaching Protocols",
   },
   {
-    url: `${import.meta.env["VITE_APP_STOCKCENTER_URL"]}`,
+    url: `${stockCenterUrl}`,
     label: "Dicty Stock Center",
   },
   {
@@ -23,23 +27,63 @@ const authFooterItems = [
     label: "DictyAccess",
   },
   {
-    url: `${
-      import.meta.env["VITE_APP_FRONTPAGE_URL"]
-    }/community/conference/editable`,
+    url: `${frontPageUrl}/community/conference/editable`,
     label: "Conference",
   },
   {
-    url: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/community/labs/editable`,
+    url: `${frontPageUrl}/community/labs/editable`,
     label: "Labs",
   },
   {
-    url: `${import.meta.env["VITE_APP_FRONTPAGE_URL"]}/about/editable`,
+    url: `${frontPageUrl}/about/editable`,
     label: "About",
   },
   {
-    url: `${import.meta.env["VITE_APP_STOCKCENTER_URL"]}/contact/editable`,
+    url: `${stockCenterUrl}/contact/editable`,
     label: "Contact",
   },
 ]
 
-export { authFooterItems }
+const createFooterItems = ({
+  frontPageUrl,
+  stockCenterUrl,
+}: CreateFooterItemsProperties) => [
+  {
+    url: `${frontPageUrl}/research/techniques/show`,
+    label: "Techniques",
+  },
+  {
+    url: `${frontPageUrl}/research/teach/show`,
+    label: "Teaching Protocols",
+  },
+  {
+    url: `${stockCenterUrl}`,
+    label: "Dicty Stock Center",
+  },
+  {
+    url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+    label: "Genome Browser",
+  },
+  {
+    url: "/dictyaccess",
+    label: "DictyAccess",
+  },
+  {
+    url: `${frontPageUrl}/community/conference/show`,
+    label: "Conference",
+  },
+  {
+    url: `${frontPageUrl}/community/labs/show`,
+    label: "Labs",
+  },
+  {
+    url: `${frontPageUrl}/about/show`,
+    label: "About",
+  },
+  {
+    url: `${stockCenterUrl}/contact/show`,
+    label: "Contact",
+  },
+]
+
+export { createAuthFooterItems, createFooterItems }
