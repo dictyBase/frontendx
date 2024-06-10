4260aa165aff466fbc13e52e2faedaeac5b3a3d4
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-10 12:41:32 -0500

    test(Footer.test.tsx): update test data in Footer component test to use a local testData variable
    The test data in the Footer component test has been updated to use a local testData variable instead of importing it from an external file. This change improves the test's readability and maintainability by making the test data more explicit and self-contained within the test file.

--- packages/footer/src/__tests__/Footer.test.tsx
+++ packages/footer/src/__tests__/Footer.test.tsx
@@ -1,12 +1,30 @@
 import { test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
 import { Footer } from "../functional/Footer"
-import { footerData } from ".."
 
+const testData = [
+  {
+    url: `/research/techniques/show`,
+    label: "Techniques",
+  },
+  {
+    url: `/research/teach/show`,
+    label: "Teaching Protocols",
+  },
+  {
+    url: `/stockcenter`,
+    label: "Dicty Stock Center",
+  },
+  {
+    url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+    label: "Genome Browser",
+  },
+]
+  
 test("renders footer with links of footer data", () => {
-  render(<Footer />)
+  render(<Footer data={testData} />)
 
-  footerData.forEach((footerItem) => {
+  testData.forEach((footerItem) => {
     const link = screen.getByRole("link", { name: footerItem.label })
     expect(link).toBeInTheDocument()
     expect(link).toHaveAttribute("href", footerItem.url)
