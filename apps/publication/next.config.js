de77aace83007c0ba9e6f3b00b0b037bf5f98c4d
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-20 23:20:13 -0500

    chore(next.config.js): add transpile modules for new packages and update existing ones
    The next.config.js file now includes transpile modules for new packages such as "@dictybase/header", "@dictybase/footer", "@dictybase/auth", "@dictybase/dicty-image", "@dictybase/functional", "@dictybase/ui-common", "@dictybase/image-plugin", and "@logto/react". This update ensures that these packages are transpiled correctly by Next.js.

--- apps/publication/next.config.js
+++ apps/publication/next.config.js
@@ -3,7 +3,15 @@ const base = "/publication"
 // eslint-disable-next-line import/no-commonjs, unicorn/prefer-module
 const withTM = require("next-transpile-modules")([
   "@dictybase/navbar",
+  "@dictybase/header",
+  "@dictybase/footer",
+  "@dictybase/auth",
+  "@dictybase/dicty-image",
+  "@dictybase/functional",
+  "@dictybase/ui-common",
+  "@dictybase/image-plugin",
   "dicty-graphql-schema",
+  "@logto/react",
 ])
 // eslint-disable-next-line import/no-commonjs, unicorn/prefer-module
 module.exports = withTM({
