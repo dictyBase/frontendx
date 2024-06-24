8837c426c3e6b268074e935eb9d00107604b5d79
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-24 15:36:29 -0500

    refactor(handlers.ts): refactor handlers array to include a new mockListContentByNamespaceQuery function
    The handlers array in the file has been refactored to include a new function mockListContentByNamespaceQuery. This function generates mock news data and returns it as a response for the listContentByNamespace query. This change improves the modularity and readability of the code by separating the logic for generating mock news into its own function.

--- apps/dicty-frontpage/src/mocks/handlers.ts
+++ apps/dicty-frontpage/src/mocks/handlers.ts
@@ -1,6 +1,11 @@
-import { publicHandlers } from "./publicHandlers"
-import { privateHandlers } from "./privateHandlers"
+import { mockListContentByNamespaceQuery } from "dicty-graphql-schema/types/mocks"
+import { generateMockNews } from "./generateMockNews"
 
-const handlers = [...publicHandlers, ...privateHandlers]
+const handlers = [
+  mockListContentByNamespaceQuery((_, response, context) => {
+    const mockNews = generateMockNews(15)
+    return response(context.data({ listContentByNamespace: mockNews }))
+  }),
+]
 
 export { handlers }
