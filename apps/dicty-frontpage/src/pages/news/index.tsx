8e725d577b60d477accbf4c9207d8798321bcd27
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-24 12:39:58 -0500

    fix(news): import P from ts-pattern to use pattern matching in the code
    The code now imports P from ts-pattern to enable pattern matching in the code. Function and variable names have been updated to be more descriptive and consistent, improving code readability and maintainability.

--- apps/dicty-frontpage/src/pages/news/index.tsx
+++ apps/dicty-frontpage/src/pages/news/index.tsx
@@ -1,26 +1,31 @@
-import { match } from "ts-pattern"
+import { match, P } from "ts-pattern"
 import { FullPageLoadingDisplay } from "@dictybase/ui-common"
-import { PublicationsView } from "./PublicationsView"
-import { useListContentByNamespace, ListContentByNamespace } from "dicty-graphql-schema"
+import {
+  useListContentByNamespaceQuery,
+  ListContentByNamespaceQuery,
+} from "dicty-graphql-schema"
 
 type NewsViewProperties = {
-  data: 
+  contentList: ListContentByNamespaceQuery["listContentByNamespace"]
 }
 
-const NewsView = () => <></>
+const NewsView = ({ contentList }: NewsViewProperties) => <></>
 
 const News = () => {
-  const fetchState = useListNewsContentByNamespace({ variables: { namspace: "news" })
+  const fetchState = useListContentByNamespaceQuery({
+    variables: { namespace: "news" },
+  })
 
   return match(fetchState)
     .with({ loading: true }, () => <FullPageLoadingDisplay />)
-    .when(
-      ({ error }) => error.length > 0,
-      ({ error }) => <>{error}</>,
-    )
-    .when(
-      ({ data }) => data.length > 0,
-      ({ data }) => <NewsView data={data} />,
+    .with({ error: P.select(P.not(undefined)) }, (error) => <>{error}</>)
+    .with(
+      {
+        data: {
+          listContentByNamespace: P.select(P.array({ content: P.string })),
+        },
+      },
+      (contentList) => <NewsView contentList={contentList} />,
     )
     .otherwise(() => <> This message should not appear. </>)
 }
