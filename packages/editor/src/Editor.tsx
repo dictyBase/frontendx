5d65735a895c3f4bb5177d17204fc92799533d44
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-26 12:40:28 -0500

    style(Editor.tsx): remove unused imports and clean up code formatting
    The commit removes unused imports such as Container, makeStyles, Theme, and useEditorInputStyles to improve code cleanliness and readability. It also cleans up code formatting by removing unnecessary comments and eslint-disable directives.

--- packages/editor/src/Editor.tsx
+++ packages/editor/src/Editor.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import { FunctionComponent } from "react"
 import {
   InitialEditorStateType,
@@ -9,7 +10,7 @@ import { ListPlugin } from "@lexical/react/LexicalListPlugin"
 import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin"
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary"
-import { Grid, Container, Button, makeStyles, Theme } from "@material-ui/core"
+import { Grid, Button } from "@material-ui/core"
 import { ImagePlugin } from "@dictybase/image-plugin"
 import { SaveButton } from "@dictybase/persistence-plugin"
 import { WidthTablePlugin } from "@dictybase/width-table-plugin"
@@ -18,7 +19,7 @@ import { TableActionPlugin } from "@dictybase/table-action-plugin"
 import { DictybaseToolbar } from "@dictybase/editor-toolbar"
 import { dictyEditorConfig } from "./editorConfig"
 import {
-  useEditorInputStyles,
+  useEditorAreaStyles,
   useEditorPlaceholderStyles,
 } from "./useEditorStyles"
 import { initialStateString } from "./initialState"
@@ -46,16 +47,6 @@ type ReadProperties = {
   handleSave?: never
 }
 
-const useEditorAreaStyles = makeStyles<Theme, { editable: boolean }>({
-  container: {
-    overflowY: ({ editable }) => (editable ? "scroll" : "initial"),
-    maxHeight: ({ editable }) => (editable ? "70vh" : "auto"),
-  },
-  root: {
-    position: "relative",
-  },
-})
-
 const Editor = ({
   content,
   editable = false,
@@ -68,7 +59,6 @@ const Editor = ({
   | (EditorProperties & EditProperties)) => {
   // eslint-disable-next-line unicorn/no-null
   const initialEditorState = content?.editorState || initialStateString || null
-  const inputClasses = useEditorInputStyles()
   const placeholderClasses = useEditorPlaceholderStyles()
   // const persistencePluginStyles = usePersistencePluginStyles()
   const editorAreaClasses = useEditorAreaStyles({ editable })
@@ -114,22 +104,18 @@ const Editor = ({
           <></>
         )}
         <Grid item>
-          {/* eslint-disable-next-line dot-notation */}
-          <div className={editorAreaClasses["container"]}>
-            {/* eslint-disable-next-line dot-notation */}
-            <Container disableGutters className={editorAreaClasses["root"]!}>
-              <RichTextPlugin
-                ErrorBoundary={LexicalErrorBoundary}
-                contentEditable={
-                  <ContentEditable className={inputClasses.root} />
-                }
-                placeholder={
-                  <div className={placeholderClasses.root}>
-                    Enter some text...
-                  </div>
-                }
-              />
-            </Container>
+          <div>
+            <RichTextPlugin
+              ErrorBoundary={LexicalErrorBoundary}
+              contentEditable={
+                <ContentEditable className={editorAreaClasses["container"]} />
+              }
+              placeholder={
+                <div className={placeholderClasses.root}>
+                  Enter some text...
+                </div>
+              }
+            />
           </div>
         </Grid>
       </Grid>
