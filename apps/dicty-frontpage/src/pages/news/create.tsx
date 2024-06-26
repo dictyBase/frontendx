25c59207a08ec8292ec543a3c90baf559f3f1812
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-26 14:23:14 -0500

    feat(news): add new EditableNews component to display and edit news items
    The useAuthorizedCreateContent import was mistakenly placed in the wrong location in create.tsx and has been moved to the correct location. The new EditableNews component has been added to display and edit news items, providing a more interactive and user-friendly experience for managing news content.

--- apps/dicty-frontpage/src/pages/news/create.tsx
+++ apps/dicty-frontpage/src/pages/news/create.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom"
-import { useAuthorizedCreateContent } from "../../common/utils/useAuthorizedCreateContent"
 import { Container, Grid, Box, Theme, Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { FunctionComponent } from "react"
 import { ACCESS } from "@dictybase/auth"
 import { Editor } from "@dictybase/editor"
+import { useAuthorizedCreateContent } from "../../common/utils/useAuthorizedCreateContent"
 
 const useStyles = makeStyles((theme: Theme) => ({
   content: {
@@ -73,7 +73,10 @@ const CreateNewsToolbar: FunctionComponent<{
 const CreateNews = () => {
   const navigate = useNavigate()
   const newsContentName = crypto.randomUUID()
-  const authorizedCreateContent = useAuthorizedCreateContent("news", newsContentName)
+  const authorizedCreateContent = useAuthorizedCreateContent(
+    "news",
+    newsContentName,
+  )
 
   const handleSave = async (contentValue: string) => {
     // handle error / success state
---
+++ apps/dicty-frontpage/src/pages/news/editable.tsx
@@ -0,0 +1,113 @@
+import { Container, Box, Typography, Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import { Link } from "react-router-dom"
+import { pipe } from "fp-ts/function"
+import { map as Amap } from "fp-ts/Array"
+import { match, P } from "ts-pattern"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
+import { Editor } from "@dictybase/editor"
+import {
+  useListContentByNamespaceQuery,
+  ListContentByNamespaceQuery,
+} from "dicty-graphql-schema"
+import { parseISO, format } from "date-fns/fp"
+import { ACCESS } from "@dictybase/auth"
+
+const useStyles = makeStyles({
+  container: {
+    textAlign: "left",
+    padding: "0px 6rem 1rem 6rem",
+    borderRadius: "15px",
+    boxSizing: "border-box",
+    marginBottom: "10px",
+    "@media (max-width: 768px)": {
+      padding: "0 0 0 0",
+    },
+  },
+
+  header: {
+    color: "black",
+    fontSize: "20px",
+    padding: "15px 35px 15px 35px",
+
+    "@media (max-width: 767px)": {
+      fontSize: "24px",
+      textAlign: "right",
+      padding: "20px 5px 20px 15px",
+    },
+  },
+})
+
+type NewsItemProperties = {
+  id: string
+  content: string
+  updated_at: string
+}
+
+const EditableNewsItem = ({ id, content, updated_at }: NewsItemProperties) => (
+  <Box>
+    <Typography>{pipe(updated_at, parseISO, format("PPPP"))}</Typography>
+    <Editor
+      content={{ storageKey: "test", editorState: content }}
+      editable={false}
+    />
+    <Link to={id}> Read more </Link>
+  </Box>
+)
+
+type NewsViewProperties = {
+  contentList: ListContentByNamespaceQuery["listContentByNamespace"]
+}
+
+const NewsView = ({ contentList }: NewsViewProperties) => {
+  const { container, header } = useStyles()
+  return pipe(
+    contentList,
+    Amap(({ id, content, updated_at }) => (
+      <EditableNewsItem
+        key={id}
+        id={id}
+        updated_at={updated_at}
+        content={content}
+      />
+    )),
+    Amap((item) => <Grid item>{item}</Grid>),
+    (items) => (
+      <Container className={container}>
+        <Box className={header}>
+          <Typography variant="h1" align="center">
+            News
+          </Typography>
+        </Box>
+        <Grid container spacing={4}>
+          {items}
+        </Grid>
+      </Container>
+    ),
+  )
+}
+
+const EditableNews = () => {
+  const fetchState = useListContentByNamespaceQuery({
+    variables: { namespace: "news" },
+  })
+  return match(fetchState)
+    .with({ loading: true }, () => <FullPageLoadingDisplay />)
+    .with({ error: P.select(P.not(undefined)) }, (error) => (
+      <>{error.message}</>
+    ))
+    .with(
+      {
+        data: {
+          listContentByNamespace: P.select(P.array({ content: P.string })),
+        },
+      },
+      (contentList) => <NewsView contentList={contentList} />,
+    )
+    .otherwise(() => <> This message should not appear. </>)
+}
+
+// eslint-disable-next-line import/no-default-export
+export default EditableNews
+export const roles = ["content-admin"]
+export const access = ACCESS.private
