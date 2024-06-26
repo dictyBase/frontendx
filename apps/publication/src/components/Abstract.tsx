c553e4355b01a39917cc8a93236946ef51b761cc
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-26 13:10:51 -0500

    refactor(Abstract.tsx): simplify abstract rendering logic using fp-ts library functions
    The code has been refactored to use functions from the fp-ts library to simplify the logic for rendering the abstract content. By utilizing functions like pipe, SisEmpty, and Bmatch, the code is more concise and easier to understand. This refactor improves the readability and maintainability of the Abstract component.

--- apps/publication/src/components/Abstract.tsx
+++ apps/publication/src/components/Abstract.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
 import Divider from "@material-ui/core/Divider"
+import { Do as IDo, bind as Ibind } from "fp-ts/Identity"
+import { pipe } from "fp-ts/function"
+import { isEmpty as SisEmpty } from "fp-ts/string"
+import { match as Bmatch } from "fp-ts/boolean"
 
 type Properties = {
   abstract: string
@@ -10,25 +14,28 @@ type Properties = {
 /**
  * Displays the abstract of the publication.
  */
-const Abstract = ({ abstract }: Properties) => {
-  const body =
-    abstract === "" ? (
-      <></>
-    ) : (
-      <>
-        <Typography variant="h2">Abstract</Typography>
-        <Divider />
-        <Box pt={2} pb={2}>
-          {abstract}
-        </Box>
-      </>
-    )
-
-  return (
-    <Box pt={2} pb={1}>
-      {body}
-    </Box>
+const Abstract = ({ abstract }: Properties) =>
+  pipe(
+    abstract,
+    SisEmpty,
+    Bmatch(
+      () =>
+        pipe({ __html: abstract }, (abstractContent) => (
+          <>
+            <Typography variant="h2">Abstract</Typography>
+            <Divider />
+            <Box pt={2} pb={2}>
+              <div dangerouslySetInnerHTML={abstractContent} />
+            </Box>
+          </>
+        )),
+      () => <></>,
+    ),
+    (inner) => (
+      <Box pt={2} pb={1}>
+        {inner}
+      </Box>
+    ),
   )
-}
 
 export { Abstract }
