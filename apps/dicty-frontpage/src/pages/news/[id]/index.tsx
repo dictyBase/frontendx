7ba2ad56767a982a7864f8211817d59d18c39a94
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-27 16:19:38 -0500

    refactor(news page): remove unused imports and comments, refactor NewsShow component for better readability and maintainability
    The changes involve removing unused imports and commented-out code to clean up the file. The NewsShow component has been refactored to improve readability and maintainability by restructuring the code and adding more descriptive variable names.

--- apps/dicty-frontpage/src/pages/news/[id]/index.tsx
+++ apps/dicty-frontpage/src/pages/news/[id]/index.tsx
@@ -1,7 +1,38 @@
-// import { NewsLayoutWrapper } from "news-component/src/NewsLayout"
-// import { NewsPage } from "news-component"
-// import { ACCESS } from "../../../routes/types"
+import { Navigate } from "react-router-dom"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import {
+  NotFoundError,
+  FullPageLoadingDisplay,
+  contentPageErrorMatcher,
+} from "@dictybase/ui-common"
+import { ContentView } from "@dictybase/editor"
+import { ACCESS } from "@dictybase/auth"
+// import { NAMESPACE } from "../../../common/constants/namespace"
+import { useSlug } from "../../../common/hooks/useSlug"
 
-// // eslint-disable-next-line import/no-default-export
-// export default NewsLayoutWrapper(NewsPage)
-// export const access = ACCESS.protected
+const NAMESPACE = "news"
+
+const NewsShow = () => {
+  const slug = useSlug()
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-${slug}` },
+    errorPolicy: "none",
+  })
+  return match(result)
+    .with(
+      { data: { contentBySlug: P.select({ content: P.string }) } },
+      (content) => <ContentView data={content} />,
+    )
+    .with({ loading: true }, () => <FullPageLoadingDisplay />)
+    .with({ error: P.select(P.not(undefined)) }, (error) =>
+      contentPageErrorMatcher(error, () => (
+        <NotFoundError />
+      )),
+    )
+    .otherwise(() => <> This message should not appear. </>)
+}
+
+// eslint-disable-next-line import/no-default-export
+export default NewsShow
+export const access = ACCESS.public
