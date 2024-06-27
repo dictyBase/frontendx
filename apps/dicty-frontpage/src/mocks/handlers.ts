528fc5cb441fe195fa150247f50881b83c207bd8
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-27 16:21:59 -0500

    feat(handlers.ts): add mock handlers for createContentMutation, contentBySlugQuery, and updateContentMutation
    The mock handlers for createContentMutation, contentBySlugQuery, and updateContentMutation have been added to the handlers array. These handlers simulate the behavior of the corresponding GraphQL mutations and queries for creating, retrieving, and updating content in the database. This allows for testing the application's GraphQL functionality without making actual database calls.

--- apps/dicty-frontpage/src/mocks/handlers.ts
+++ apps/dicty-frontpage/src/mocks/handlers.ts
@@ -1,7 +1,71 @@
-import { mockListContentByNamespaceQuery } from "dicty-graphql-schema/types/mocks"
+import { BrowserLevel } from "browser-level"
+import {
+  mockListContentByNamespaceQuery,
+  mockCreateContentMutation,
+  mockContentBySlugQuery,
+  mockUpdateContentMutation,
+  Content,
+} from "dicty-graphql-schema/types/mocks"
+import { formatISO } from "date-fns"
+import { NAMESPACE } from "../common/constants/namespace"
+import { superuserProperties } from "../common/data/superuser"
 import { generateMockNews } from "./generateMockNews"
 
+const database = new BrowserLevel<string, Content>(NAMESPACE, {
+  valueEncoding: "json",
+})
+
 const handlers = [
+  mockCreateContentMutation(async (request, response, context) => {
+    const { name, content, namespace, created_by } = request.variables.input
+    const contentAdmin = { ...superuserProperties, email: created_by }
+    const now = formatISO(new Date())
+    try {
+      await database.put(name, {
+        __typename: "Content",
+        id: name,
+        name,
+        namespace,
+        slug: `${namespace}-${name}`,
+        content,
+        created_by: contentAdmin,
+        updated_by: contentAdmin,
+        created_at: now,
+        updated_at: now,
+      })
+      const created = await database.get(name)
+      return response(context.data({ createContent: created }))
+    } catch {
+      return response(context.status(500))
+    }
+  }),
+  mockContentBySlugQuery(async (request, response, context) => {
+    const { slug } = request.variables
+    try {
+      const allNews = await database.values().all()
+      const contentBySlug = allNews.find((content) => content.slug === slug)
+      if (!contentBySlug) return response(context.status(404))
+      return response(context.data({ contentBySlug }))
+    } catch {
+      return response(context.status(500))
+    }
+  }),
+  mockUpdateContentMutation(async (request, response, context) => {
+    const { id, content, updated_by } = request.variables.input
+    try {
+      const existingContent = await database.get(id)
+      await database.put(id, {
+        ...existingContent,
+        content,
+        updated_by: { ...superuserProperties, email: updated_by },
+        updated_at: formatISO(new Date()),
+      })
+      const updateContent = await database.get(id)
+      return response(context.data({ updateContent }))
+    } catch {
+      return response(context.status(500))
+    }
+  }),
   mockListContentByNamespaceQuery((_, response, context) => {
     const mockNews = generateMockNews(15)
     return response(context.data({ listContentByNamespace: mockNews }))
