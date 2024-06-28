ea95f22d6e428ea70706b320ab1adfb9f30ce6dd
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-28 12:35:21 -0500

    feat(utils): add ordByDate utility function for sorting dates in fp-ts library
    The ordByDate utility function is added to the project to provide a way to compare dates using the fp-ts library. This function uses date-fns for date comparison and fp-ts for defining the ordering logic. It enhances the project by enabling sorting functionality based on dates in a functional programming paradigm.

---
+++ apps/dicty-frontpage/src/common/utils/ordByDate.ts
@@ -0,0 +1,24 @@
+import { fromCompare } from "fp-ts/Ord"
+import { Ordering } from "fp-ts/Ordering"
+import { isAfter, isBefore, isEqual } from "date-fns"
+import { match } from "ts-pattern"
+
+const dateCompare = (first: Date, second: Date): Ordering =>
+  match<{ first: Date; second: Date }, Ordering>({ first, second })
+    .when(
+      ({ first: f, second: s }) => isAfter(f, s),
+      () => 1,
+    )
+    .when(
+      ({ first: f, second: s }) => isEqual(f, s),
+      () => 0,
+    )
+    .when(
+      ({ first: f, second: s }) => isBefore(f, s),
+      () => -1,
+    )
+    .otherwise(() => 0)
+
+const ordByDate = fromCompare(dateCompare)
+
+export { ordByDate }
