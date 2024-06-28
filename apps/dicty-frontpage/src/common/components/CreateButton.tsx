6c04085245213fc285750c66273cdeffd4e89954
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-28 00:15:49 -0500

    feat(CreateButton.tsx): add CreateButton component for creating and saving content
    The CreateButton component is added to the project to provide a button that allows users to create and save content. This component includes functionality to handle updating content, saving it, and navigating to the appropriate page after the save operation is completed.

---
+++ apps/dicty-frontpage/src/common/components/CreateButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@material-ui/core"
+import { useNavigate } from "react-router-dom"
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { useAuthorizedCreateContent } from "../utils/useAuthorizedCreateContent"
+
+type CreateButtonProperties = {
+  namespace: string
+  name: string
+}
+
+const CreateButton = ({ namespace, name }: CreateButtonProperties) => {
+  const [editor] = useLexicalComposerContext()
+  const navigate = useNavigate()
+  const authorizedCreateContent = useAuthorizedCreateContent(namespace, name)
+
+  const handleUpdate = async () => {
+    // handle error / success state
+    const contentValue = JSON.stringify(editor.getEditorState().toJSON())
+    await authorizedCreateContent(contentValue)
+    navigate("../editable", { relative: "path" })
+  }
+
+  return <Button onClick={handleUpdate}> Save </Button>
+}
+
+export { CreateButton }
