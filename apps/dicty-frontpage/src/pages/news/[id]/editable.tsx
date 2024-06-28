aac493bed7491572aa220bca670bae82a2d20112
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-28 00:19:39 -0500

    feat(news): add new ActionBar component to EditableView for editing news content
    The changes introduce a new ActionBar component to the EditableView, allowing users to edit news content more easily. Additionally, the EditableNews component is renamed to Editable to improve clarity and maintain consistency with naming conventions.

--- apps/dicty-frontpage/src/pages/news/[id]/editable.tsx
+++ apps/dicty-frontpage/src/pages/news/[id]/editable.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom"
+import { Container, Typography, Button } from "@material-ui/core"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
 import {
@@ -5,12 +7,36 @@ import {
   FullPageLoadingDisplay,
   contentPageErrorMatcher,
 } from "@dictybase/ui-common"
-import { EditableView } from "@dictybase/editor"
+import { Editor } from "@dictybase/editor"
 import { ACCESS } from "@dictybase/auth"
 import { NEWS_NAMESPACE } from "../../../common/constants/namespace"
 import { useSlug } from "../../../common/hooks/useSlug"
+import { ActionBar } from "../../../common/components/ActionBar"
 
-const EditableNews = () => {
+type EditableViewProperties = {
+  content: string
+}
+const EditableView = ({ content }: EditableViewProperties) => {
+  const navigate = useNavigate()
+  const handleEdit = async () => {
+    navigate("../edit", { relative: "path" })
+  }
+
+  const toolbar = (
+    <ActionBar descriptionElement={<Typography>Write News</Typography>}>
+      <Button onClick={handleEdit}> Edit </Button>
+    </ActionBar>
+  )
+  return (
+    <Container>
+      <Editor
+        content={{ storageKey: undefined, editorState: content }}
+        toolbar={toolbar}
+      />
+    </Container>
+  )
+}
+const Editable = () => {
   const slug = useSlug()
   const result = useContentBySlugQuery({
     variables: { slug: `${NEWS_NAMESPACE}-${slug}` },
@@ -19,8 +45,8 @@ const EditableNews = () => {
 
   return match(result)
     .with(
-      { data: { contentBySlug: P.select({ content: P.string }) } },
-      (content) => <EditableView data={content} />,
+      { data: { contentBySlug: ({ content: P.select(P.string) }) } },
+      (content) => <EditableView content={content} />,
     )
     .with({ loading: true }, () => <FullPageLoadingDisplay />)
     .with({ error: P.select(P.not(undefined)) }, (error) =>
@@ -32,6 +58,6 @@ const EditableNews = () => {
 }
 
 // eslint-disable-next-line import/no-default-export
-export default EditableNews
+export default Editable
 export const roles = ["content-admin"]
 export const access = ACCESS.private
