e377dca2e49b8d6b7602d1b0799acf29f85a915f
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-28 00:19:12 -0500

    refactor(news edit page): remove unused imports and styles, refactor toolbar component into ActionBar component
    The changes involve removing unused imports and styles, refactoring the toolbar component into the ActionBar component for better organization and readability. Additionally, the UpdateButton and ActionBar components are added to enhance the functionality and user experience of the news edit page.

--- apps/dicty-frontpage/src/pages/news/[id]/edit.tsx
+++ apps/dicty-frontpage/src/pages/news/[id]/edit.tsx
@@ -1,118 +1,48 @@
 import { useNavigate } from "react-router-dom"
-import { Container, Grid, Box, Theme, Typography } from "@material-ui/core"
+import { Container, Typography, Button } from "@material-ui/core"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
-import { useSlug } from "../../../common/hooks/useSlug"
 import {
   NotFoundError,
   FullPageLoadingDisplay,
   contentPageErrorMatcher,
 } from "@dictybase/ui-common"
-import { makeStyles } from "@material-ui/core/styles"
-import { FunctionComponent } from "react"
 import { ACCESS } from "@dictybase/auth"
 import { Editor } from "@dictybase/editor"
+import { useSlug } from "../../../common/hooks/useSlug"
 import { NEWS_NAMESPACE } from "../../../common/constants/namespace"
-import { useAuthorizedUpdateContent } from "../../../common/utils/useAuthorizedUpdateContent"
+import { UpdateButton } from "../../../common/components/UpdateButton"
+import { ActionBar } from "../../../common/components/ActionBar"
 
-const useStyles = makeStyles((theme: Theme) => ({
-  content: {
-    marginLeft: "auto",
-  },
-  toolbar: {
-    backgroundColor: "#fafafa",
-    borderRadius: "2px",
-    border: "1px solid #ddd",
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-    width: "100%",
-    alignItems: "center",
-  },
-  label: {
-    marginLeft: "auto",
-    marginRight: theme.spacing(1),
-    padding: theme.spacing(1),
-    fontSize: "0.8rem",
-    fontWeight: "bold",
-    lineHeight: 1,
-    color: "#fff",
-    whiteSpace: "nowrap",
-    borderRadius: "0.25em",
-    backgroundColor: theme.palette.primary.light,
-  },
-  icon: {
-    color: theme.palette.primary.light,
-    fontSize: "1rem",
-    marginRight: theme.spacing(0.5),
-  },
-  text: {
-    color: theme.palette.primary.light,
-  },
-}))
-
-const CreateNewsToolbar: FunctionComponent<{
-  children: Array<JSX.Element>
-}> = ({ children }) => {
-  const { toolbar, text } = useStyles()
-  return (
-    <Grid
-      container
-      justifyContent="space-between"
-      className={toolbar}
-      data-testid="info-page-toolbar">
-      <Grid item>
-        <Box component="span" className={text}>
-          <Typography>Write News</Typography>
-        </Box>
-      </Grid>
-      <Grid item>
-        <Grid container spacing={1}>
-          {children.map((child, index) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <Grid key={index} item>
-              {child}
-            </Grid>
-          ))}
-        </Grid>
-      </Grid>
-    </Grid>
-  )
-}
-type UpdateNewsProperties = {
+type EditViewProperties = {
   contentId: string
   content: string
 }
 
-const UpdateNews = ({ contentId, content }: UpdateNewsProperties) => {
+const EditView = ({ contentId, content }: EditViewProperties) => {
   const navigate = useNavigate()
-  const authorizedUpdateContent = useAuthorizedUpdateContent(
-   contentId,
-  )
-
-  const handleSave = async (contentValue: string) => {
-    // handle error / success state
-    await authorizedUpdateContent(contentValue)
-    navigate("../editable", { relative: "path" })
-  }
-
   const handleCancel = async () => {
     navigate("../editable", { relative: "path" })
   }
 
+  const toolbar = (
+    <ActionBar descriptionElement={<Typography>Write News</Typography>}>
+      <UpdateButton contentId={contentId} />
+      <Button onClick={handleCancel}> Cancel </Button>
+    </ActionBar>
+  )
   return (
     <Container>
       <Editor
         content={{ storageKey: undefined, editorState: content }}
         editable
-        toolbar={CreateNewsToolbar}
-        handleCancel={handleCancel}
-        handleSave={handleSave}
+        toolbar={toolbar}
       />
     </Container>
   )
 }
 
-const UpdatePage = () => {
+const Edit = () => {
   const slug = useSlug()
   const result = useContentBySlugQuery({
     variables: { slug: `${NEWS_NAMESPACE}-${slug}` },
@@ -121,17 +51,15 @@ const UpdatePage = () => {
   return match(result)
     .with(
       { data: { contentBySlug: P.select({ content: P.string }) } },
-      ({id, content}) => <UpdateNews contentId={id} content={content} />,
+      ({ id, content }) => <EditView contentId={id} content={content} />,
     )
     .with({ loading: true }, () => <FullPageLoadingDisplay />)
     .with({ error: P.select(P.not(undefined)) }, (error) =>
-      contentPageErrorMatcher(error, () => (
-        <NotFoundError />
-      )),
+      contentPageErrorMatcher(error, () => <NotFoundError />),
     )
     .otherwise(() => <> This message should not appear. </>)
 }
 // eslint-disable-next-line import/no-default-export
-export default UpdatePage
+export default Edit
 export const roles = ["content-admin"]
 export const access = ACCESS.private
