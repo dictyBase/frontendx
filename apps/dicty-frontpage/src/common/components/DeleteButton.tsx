197bfc772d99531b92703f0400557a9ea7bee63e
Kevin Tun <kevin.tun@northwestern.edu>
2024-06-28 10:27:49 -0500

    feat(DeleteButton.tsx): add DeleteButton component to allow users to delete content
    The DeleteButton component has been added to the project, providing users with the ability to delete content. This component includes functionality to handle the deletion process, including error and success states, and redirects users to the "/news/editable" page after deletion.

---
+++ apps/dicty-frontpage/src/common/components/DeleteButton.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@material-ui/core"
+import { useNavigate } from "react-router-dom"
+import { useAuthorizedDeleteContent } from "../utils/useAuthorizedDeleteContent"
+
+type DeleteButtonProperties = {
+  id: string
+}
+
+const DeleteButton = ({ id }: DeleteButtonProperties) => {
+  const navigate = useNavigate()
+  const authorizedDeleteContent = useAuthorizedDeleteContent(id)
+
+  const handleDelete = async () => {
+    // handle error / success state
+    await authorizedDeleteContent()
+    navigate("/news/editable", { relative: "path" })
+  }
+
+  return <Button onClick={handleDelete}> Delete </Button>
+}
+
+export { DeleteButton }
