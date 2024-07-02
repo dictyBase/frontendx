198bc0fa9e6b2090287bb8d41c64588b89ccbf01
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-02 17:51:57 -0500

    test(setupTests.js): add mock for @logto/react to provide a dummy implementation of useLogto
    The mock for @logto/react is added to provide a dummy implementation of the useLogto hook, which includes a getAccessToken function that returns an empty object. This allows for testing components that rely on the useLogto hook without needing to implement the actual functionality.

--- apps/dicty-frontpage/src/setupTests.js
+++ apps/dicty-frontpage/src/setupTests.js
@@ -5,5 +5,11 @@ import { vi } from "vitest"
 
 const fetchMock = createFetchMock(vi)
 
+vi.mock("@logto/react", async () => ({
+  useLogto: () => ({
+    getAccessToken: () => {},
+  }),
+}))
+
 // sets globalThis.fetch and globalThis.fetchMock to our mocked version
 fetchMock.enableMocks()
