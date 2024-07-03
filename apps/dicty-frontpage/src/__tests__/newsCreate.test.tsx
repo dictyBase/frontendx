8fe233b4a3556519fff1f65c848196a2c282f9d2
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-02 23:55:46 -0500

    test(newsCreate.test.tsx): refactor routing setup to use createMemoryRouter for better test isolation
    The changes were made to improve the readability of the test file by reorganizing import statements and refactoring the routing setup using createMemoryRouter for better test isolation. Additionally, the tests were updated to ensure proper navigation behavior when interacting with the Cancel and Save buttons.

--- apps/dicty-frontpage/src/__tests__/newsCreate.test.tsx
+++ apps/dicty-frontpage/src/__tests__/newsCreate.test.tsx
@@ -1,20 +1,39 @@
-import { expect, describe, test, vi } from "vitest"
+import { vi, expect, describe, test } from "vitest"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { MockedProvider } from "@apollo/client/testing"
+import { RouterProvider, createMemoryRouter } from "react-router-dom"
 import Create from "../pages/news/create"
 
-const mockNavigate = vi.hoisted(() => vi.fn())
+const createRoute = "/news/create"
 
-vi.mock("react-router-dom", () => ({
-  useNavigate: () => mockNavigate,
+const routeConfiguration = [
+  {
+    path: createRoute,
+    element: <Create />,
+  },
+  {
+    path: "/news/editable",
+    element: <> Editable News List Route </>,
+  },
+]
+
+const { mockAuthorizedCreateContent } = vi.hoisted(() => ({
+  mockAuthorizedCreateContent: vi.fn(),
+}))
+
+vi.mock("../common/hooks/useAuthorizedCreateContent", () => ({
+  useAuthorizedCreateContent: () => mockAuthorizedCreateContent,
 }))
 
 describe("Create Component", () => {
   test('renders an element with the "textbox" role', () => {
+    const router = createMemoryRouter(routeConfiguration, {
+      initialEntries: [createRoute],
+    })
     render(
       <MockedProvider>
-        <Create />
+        <RouterProvider router={router} />
       </MockedProvider>,
     )
     const textbox = screen.getByRole("textbox")
@@ -23,24 +42,35 @@ describe("Create Component", () => {
 
   test('renders a button with the text "Cancel" that navigates to `/news/editable` when clicked', async () => {
     const user = userEvent.setup()
+    const router = createMemoryRouter(routeConfiguration, {
+      initialEntries: [createRoute],
+    })
     render(
       <MockedProvider>
-        <Create />
+        <RouterProvider router={router} />
       </MockedProvider>,
     )
     const cancelButton = screen.getByText("Cancel")
     expect(cancelButton).toBeInTheDocument()
+
     await user.click(cancelButton)
-    expect(mockNavigate).toHaveBeenCalledWith("/news/editable")
+    expect(screen.getByText("Editable News List Route")).toBeInTheDocument()
   })
 
-  test('renders a button with the text "Save"', () => {
+  test('renders a button with the text "Save" that navigates to calls createContent mutation and navigates to `/news/editable` when clicked', async () => {
+    const user = userEvent.setup()
+    const router = createMemoryRouter(routeConfiguration, {
+      initialEntries: [createRoute],
+    })
     render(
       <MockedProvider>
-        <Create />
+        <RouterProvider router={router} />
       </MockedProvider>,
     )
     const saveButton = screen.getByText("Save")
     expect(saveButton).toBeInTheDocument()
+    await user.click(saveButton)
+    expect(mockAuthorizedCreateContent).toHaveBeenCalled()
+    expect(screen.getByText("Editable News List Route")).toBeInTheDocument()
   })
 })
