7f1d3377c0d96f23a7d43fc2d1ecd4375124479b
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-03 16:06:53 -0500

    feat(tests): add unit tests for EditView, NewsItem, and NewsView components
    The commit adds unit tests for the EditView, NewsItem, and NewsView components in the dicty-frontpage application. These tests ensure that the components render correctly and behave as expected, improving the overall reliability and maintainability of the codebase.

---
+++ apps/dicty-frontpage/src/__tests__/NewsItem.test.tsx
@@ -0,0 +1,32 @@
+import { describe, test, expect } from "vitest"
+import { RouterProvider, createMemoryRouter } from "react-router-dom"
+import { render, screen } from "@testing-library/react"
+import { NewsItem } from "../pages/news/show"
+
+const CONTENT_STRING = `{"root":{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"Annual International Dictyostelium Conference","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"heading","version":1,"tag":"h1"},{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"International conferences dedicated to ","type":"text","version":1},{"detail":0,"format":2,"mode":"normal","style":"","text":"Dictyostelium","type":"text","version":1},{"detail":0,"format":0,"mode":"normal","style":"","text":" started in  1977 with the meeting in Sardinia, and continued on a roughly 3-year  cycle into the 1980's. However, as the field became more active, more  local meetings sprang up to fill the gaps in the cycle. Notable amongst  these was an annual series in the UK, which gradually became more  international. By the late 1980's with the successive meetings at  Amsterdam, Oxford, Airlie and Cambridge, the current pattern of annual  meetings was established. Interestingly in the late 1990's as the field  expanded further, local meetings were re-started in several countries.     ","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":"ltr","format":"","indent":0,"type":"flex-layout","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`
+
+const newsItemProps = {
+    name: 'test-news',
+    content: CONTENT_STRING,
+    updated_at: '2022-01-01T00:00:00.000Z'
+}
+
+describe('NewsItem', () => {
+  const renderNewsItem = () => {
+    const router = createMemoryRouter([
+      {
+        index: true,
+        element: <NewsItem {...newsItemProps} />
+      }
+    ])
+    render(
+        <RouterProvider router={router} />          
+    )
+  }
+
+  test('renders a link to `/news/${name}/show`', () => {
+    renderNewsItem()
+    expect(screen.getByRole('link', { name: /read more/i })).toHaveAttribute('href', `/news/${newsItemProps.name}/show`)
+    expect(screen.getByText(/Annual International Dictyostelium Conference/)).toBeInTheDocument()
+  })
+})
---
+++ apps/dicty-frontpage/src/__tests__/NewsView.test.tsx
@@ -0,0 +1,36 @@
+import { describe, test, expect } from "vitest"
+import { RouterProvider, createMemoryRouter } from "react-router-dom"
+import { render, screen } from "@testing-library/react"
+import { makeBy as AmakeBy } from "fp-ts/Array"
+import { NewsView } from "../pages/news/show"
+import { superuserProperties } from "../common/data/superuser"
+
+const testText = "test content"
+const contentString = `{"root":{"children":[{"children":[{"children":[{"detail":0,"format":0,"mode":"normal","style":"","text":"${testText}","type":"text","version":1}],"direction":"ltr","format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"flex-layout","version":1}],"direction":"ltr","format":"","indent":0,"type":"root","version":1}}`
+const listLength = 5
+const contentList = AmakeBy(listLength, (index) => ({
+  id: `${index}`,
+  name: `${index}`,
+  slug: `news-${index}`,
+  content: contentString,
+  created_at: `2022-01-0${index + 1}T00:00:00.000Z`,
+  updated_at: `2022-01-0${index + 1}T00:00:00.000Z`,
+  created_by: superuserProperties,
+  updated_by: superuserProperties,
+}))
+
+describe("NewsView", () => {
+  test("renders a link to `/news/${name}/show`", () => {
+    const router = createMemoryRouter([
+      {
+        index: true,
+        element: <NewsView contentList={contentList} />,
+      },
+    ])
+    render(<RouterProvider router={router} />)
+    expect(screen.getAllByRole("link", { name: /read more/i })).toHaveLength(
+      listLength,
+    )
+    expect(screen.getAllByText(new RegExp(testText))).toHaveLength(listLength)
+  })
+})
