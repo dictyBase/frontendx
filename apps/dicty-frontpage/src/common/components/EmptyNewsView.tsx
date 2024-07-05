1fe109baa208ac46d7efc0bb784ae128b8ef28c9
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-05 14:20:30 -0500

    feat(EmptyNewsView.tsx): add EmptyNewsView component to display a message when there are no news items
    The EmptyNewsView component is added to the project to provide a visually appealing message when there are no news items to display. This component includes styling for the header and the message itself, making it easy for users to understand the absence of news content.

---
+++ apps/dicty-frontpage/src/common/components/EmptyNewsView.tsx
@@ -0,0 +1,37 @@
+import { Container, Typography, Grid, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles({
+  header: {
+    color: "black",
+    fontSize: "20px",
+    padding: "15px 35px 15px 35px",
+
+    "@media (max-width: 767px)": {
+      fontSize: "24px",
+      textAlign: "right",
+      padding: "20px 5px 20px 15px",
+    },
+  },
+})
+
+const EmptyNewsView = () => {
+  const { header } = useStyles()
+  return (
+    <Container>
+      <Grid className={header}>
+        <Typography variant="h1" align="center">
+          News
+        </Typography>
+      </Grid>
+      <Grid justifyContent="center" container>
+        <Grid item>
+          <Typography variant="h2">
+            There are currently no news items.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export { EmptyNewsView }
