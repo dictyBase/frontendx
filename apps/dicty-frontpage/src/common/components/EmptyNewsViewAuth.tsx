ecab09796cad7a6ce6aa5db462325d2139df342c
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-05 16:49:50 -0500

    feat(EmptyNewsViewAuth.tsx): add EmptyNewsViewAuth component to display a message when there are no news items
    The EmptyNewsViewAuth component is added to the project to provide a visually appealing message when there are no news items to display. This component includes a header, a message indicating the absence of news items, and a button to navigate to the news creation page.

---
+++ apps/dicty-frontpage/src/common/components/EmptyNewsViewAuth.tsx
@@ -0,0 +1,59 @@
+import {
+  Container,
+  Typography,
+  Grid,
+  Button,
+  makeStyles,
+} from "@material-ui/core"
+import { useNavigate } from "react-router-dom"
+import CreateIcon from "@material-ui/icons/Create"
+
+const useStyles = makeStyles({
+  header: {
+    color: "black",
+    fontSize: "20px",
+    padding: "15px 35px 15px 35px",
+
+    "@media (max-width: 767px)": {
+      fontSize: "24px",
+      textAlign: "right",
+      padding: "20px 5px 20px 15px",
+    },
+  },
+})
+
+const EmptyNewsViewAuth = () => {
+  const navigate = useNavigate()
+  const { header } = useStyles()
+  const onClick = () => {
+    navigate("/news/create")
+  }
+  return (
+    <Container>
+      <Grid container direction="column" alignItems="center" spacing={5}>
+        <Grid item className={header}>
+          <Typography variant="h1" align="center">
+            News
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h2">
+            There are currently no news items.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            startIcon={<CreateIcon />}
+            variant="outlined"
+            color="primary"
+            size="large"
+            onClick={onClick}>
+            Write News Article
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export { EmptyNewsViewAuth }
