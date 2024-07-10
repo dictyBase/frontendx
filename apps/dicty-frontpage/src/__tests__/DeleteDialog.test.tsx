810f31f9bbc78248737963fc8de9d81e6bed5ec9
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-10 12:17:16 -0500

    feat(DeleteDialog.test.tsx): add support for routing in DeleteDialog tests using createMemoryRouter
    The changes in DeleteDialog.test.tsx introduce support for routing in the tests by using createMemoryRouter to create a mock router. Additionally, the tests now utilize MockedProvider and RouterProvider to integrate Apollo Client and React Router, respectively, enhancing the testing environment and ensuring proper functionality of the DeleteDialog component.

--- apps/dicty-frontpage/src/__tests__/DeleteDialog.test.tsx
+++ apps/dicty-frontpage/src/__tests__/DeleteDialog.test.tsx
@@ -1,27 +1,69 @@
 import { test, expect } from "vitest"
 import { render, screen } from "@testing-library/react"
+import { MockedProvider } from "@apollo/client/testing"
+import { RouterProvider, createMemoryRouter } from "react-router-dom"
 import { DeleteDialog } from "../common/components/DeleteDialog"
 
 test("When open = false, the dialog should be closed", async () => {
-  render(<DeleteDialog open={false} onClose={() => {}} />)
+  const router = createMemoryRouter([
+    {
+      index: true,
+      element: <DeleteDialog open={false} onClose={() => {}} />,
+    },
+  ])
+  render(
+    <MockedProvider>
+      <RouterProvider router={router} />
+    </MockedProvider>,
+  )
 
   expect(screen.queryByText(/are you sure/i)).toBeNull()
 })
 
 test("When open = true, the dialog should be closed", async () => {
-  render(<DeleteDialog open={true} onClose={() => {}} />)
+  const router = createMemoryRouter([
+    {
+      index: true,
+      element: <DeleteDialog open={true} onClose={() => {}} />,
+    },
+  ])
+  render(
+    <MockedProvider>
+      <RouterProvider router={router} />
+    </MockedProvider>,
+  )
 
   expect(screen.queryByText(/are you sure/i)).toBeInTheDocument()
 })
 
 test("Shows a delete button when open", async () => {
-  render(<DeleteDialog open={true} onClose={() => {}} />)
+  const router = createMemoryRouter([
+    {
+      index: true,
+      element: <DeleteDialog open={true} onClose={() => {}} />,
+    },
+  ])
+  render(
+    <MockedProvider>
+      <RouterProvider router={router} />
+    </MockedProvider>,
+  )
 
   expect(screen.getByRole("button", { name: "Delete" })).toBeInTheDocument()
 })
 
 test("Shows a cancel button when open", async () => {
-  render(<DeleteDialog open={true} onClose={() => {}} />)
- 
+  const router = createMemoryRouter([
+    {
+      index: true,
+      element: <DeleteDialog open={true} onClose={() => {}} />,
+    },
+  ])
+  render(
+    <MockedProvider>
+      <RouterProvider router={router} />
+    </MockedProvider>,
+  )
+
   expect(screen.getByRole("button", { name: "Cancel" })).toBeInTheDocument()
 })
