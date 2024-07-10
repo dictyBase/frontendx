98998eafd502a9fec089415dcd1a777b71474a3d
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-10 11:48:47 -0500

    refactor(useAuthorizedDeleteContent.tsx): modify useAuthorizedDeleteContent function to accept an Option type for contentId parameter
    The useAuthorizedDeleteContent function has been refactored to accept an Option type for the contentId parameter, allowing for better error handling in case the contentId is missing. Error handling has been added for cases where the contentId is missing or when the access token cannot be retrieved. This ensures that the function can handle these scenarios gracefully and provide appropriate error messages.

--- apps/dicty-frontpage/src/common/hooks/useAuthorizedDeleteContent.tsx
+++ apps/dicty-frontpage/src/common/hooks/useAuthorizedDeleteContent.tsx
@@ -1,20 +1,28 @@
 import { pipe } from "fp-ts/function"
 import { useLogto } from "@logto/react"
 import { useDeleteContentMutation } from "dicty-graphql-schema"
+import { Option } from "fp-ts/Option"
 import {
   Do as TEDo,
   bind as TEbind,
   tryCatch as TEtryCatch,
   map as TEmap,
+  fromOption as TEfromOption
 } from "fp-ts/TaskEither"
 
 enum ErrorType {
-  ACCESS_TOKEN_ERROR,
+  MISSING_CONTENT_ID,
+  ACCESS_TOKEN,
   DELETE_FAILURE,
 }
 
+const missingContentIdError = {
+  errorType: ErrorType.MISSING_CONTENT_ID,
+  message: "Could not get access token",
+}
+
 const accessTokenError = {
-  errorType: ErrorType.ACCESS_TOKEN_ERROR,
+  errorType: ErrorType.ACCESS_TOKEN,
   message: "Could not get access token",
 }
 
@@ -23,13 +31,14 @@ const deleteFailureError = {
   message: "Could not delete content",
 }
 
-const useAuthorizedDeleteContent = (contentId: string) => {
+const useAuthorizedDeleteContent = (contentId: Option<string>) => {
   const { getAccessToken } = useLogto()
   const [deleteContent] = useDeleteContentMutation()
 
   return () => {
     const task = pipe(
       TEDo,
+      TEbind("id", () => pipe(contentId, TEfromOption(() => missingContentIdError))),
       TEbind("token", () =>
         TEtryCatch(
           () =>
@@ -37,12 +46,12 @@ const useAuthorizedDeleteContent = (contentId: string) => {
           () => accessTokenError,
         ),
       ),
-      TEbind("deleted", ({ token }) =>
+      TEbind("deleted", ({ id, token }) =>
         TEtryCatch(
           () =>
             deleteContent({
               variables: {
-                id: contentId,
+                id
               },
               context: { headers: { Authorization: `Bearer ${token}` } },
             }),
