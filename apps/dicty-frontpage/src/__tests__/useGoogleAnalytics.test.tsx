34ff1c15f9950a7931faf4955b582d39e21495f4
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-20 13:36:44 -0500

    feat(useGoogleAnalytics.test.tsx): add unit tests for useGoogleAnalytics hook to ensure proper functionality
    The new test file useGoogleAnalytics.test.tsx contains unit tests for the useGoogleAnalytics hook. These tests cover scenarios where Google Analytics should not load in a non-production environment, should load in a production environment with a tracking ID, and should not load if there is no tracking ID specified. The tests help ensure that the useGoogleAnalytics hook behaves as expected in different environments and configurations.

---
+++ apps/dicty-frontpage/src/__tests__/useGoogleAnalytics.test.tsx
@@ -0,0 +1,44 @@
+import { vi, test, expect, beforeEach, afterEach } from "vitest"
+import { render } from "@testing-library/react"
+import { useGoogleAnalytics } from "../common/hooks/useGoogleAnalytics"
+
+const Wrapper = () => {
+  useGoogleAnalytics()
+  return <></>
+}
+
+beforeEach(() => {
+  vi.stubGlobal("gtag", undefined)
+})
+
+afterEach(() => {
+  vi.unstubAllGlobals()
+})
+
+test("Does not load Google Analytics in a non-production environment", () => {
+  import.meta.env.VITE_DEPLOY_ENV = "testing"
+  import.meta.env.VITE_GA_TRACKING_ID = "ABC"
+  render(<Wrapper />)
+
+  expect(window.gtag).toBeUndefined()
+})
+
+test("Loads Google Analytics in production environment", () => {
+  import.meta.env.VITE_DEPLOY_ENV = "production"
+  import.meta.env.VITE_GA_TRACKING_ID = "ABC"
+  render(<Wrapper />)
+
+  expect(window.gtag).toBeDefined()
+})
+
+test("Does not load Google Analytics if there is no tracking ID", () => {
+  const mockConsoleError = vi.fn()
+  vi.stubGlobal("console", { error: mockConsoleError })
+  import.meta.env.VITE_DEPLOY_ENV = "production"
+  import.meta.env.VITE_GA_TRACKING_ID = ""
+  render(<Wrapper />)
+
+  expect(window.gtag).toBeUndefined()
+  expect(mockConsoleError).toHaveBeenCalled()
+})
+
