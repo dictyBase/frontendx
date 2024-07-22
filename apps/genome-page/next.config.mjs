c805ec1f826688da1cef873f6c2246c826d8b30d
Kevin Tun <kevin.tun@northwestern.edu>
2024-07-22 00:12:58 -0500

    chore(next.config.mjs): reorganize the transpileModules array for better readability and maintainability
    The transpileModules array in next.config.mjs has been reorganized to group related modules together, making it easier to read and maintain. This change does not affect the functionality of the code but improves the organization of the dependencies.

--- apps/genome-page/next.config.mjs
+++ apps/genome-page/next.config.mjs
@@ -23,15 +23,16 @@ const nextConfig = {
 }
 
 const withTM = transpileModules([
-  "@dictybase/navbar",
-  "@dictybase/header",
-  "@dictybase/footer",
   "@dictybase/auth",
   "@dictybase/dicty-image",
+  "@dictybase/footer",
   "@dictybase/functional",
+  "@dictybase/google-analytics",
+  "@dictybase/header",
+  "@dictybase/navbar",
   "@dictybase/ui-common",
-  "dicty-graphql-schema",
   "@logto/react",
+  "dicty-graphql-schema",
 ])
 
 // eslint-disable-next-line import/no-default-export
