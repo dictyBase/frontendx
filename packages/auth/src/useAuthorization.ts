dc600db7f9e6a37a5bfa82fcbc6fa04876abe481
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-21 15:51:40 -0500

    fix(useAuthorization.ts): use self defined loading state
    Using a self-defined loading state instead of Logto's loading state to
    indicate whether the user's role check has been completed. Previously
    relying on Logto's loading state to determine whether the user's role
    has been checked was incorrect.
    
    Fixes #817

--- packages/auth/src/useAuthorization.ts
+++ packages/auth/src/useAuthorization.ts
@@ -11,16 +11,17 @@ type useAuthorizationProperties = { entries: Array<string> }
  */
 const useAuthorization = ({ entries }: useAuthorizationProperties) => {
   // Fetch the user information using the useLogto hook
-  const { fetchUserInfo, isAuthenticated, isLoading } = useLogto()
+  const { fetchUserInfo, isAuthenticated } = useLogto()
 
   // Initialize state variables
   const [isAuthorized, setAuthorization] = useState<boolean>(false)
-  // const [isLoading, setLoading] = useState<boolean>(true)
+  const [isLoading, setLoading] = useState<boolean>(true)
   const [user, setUser] = useState<UserWithRoles>()
   useEffect(() => {
     // Function to check user authorization
     const authCheck = async (records: Array<string>) => {
       if (!isAuthenticated) {
+        setLoading(false)
         return
       }
       // Fetch the user information and cast it to UserWithRoles type
@@ -30,6 +31,7 @@ const useAuthorization = ({ entries }: useAuthorizationProperties) => {
         setAuthorization(matchEntries(records, authUser.roles))
         setUser(authUser)
       }
+      setLoading(false)
     }
     // Perform the authorization check when the entries array changes
     authCheck(entries)
