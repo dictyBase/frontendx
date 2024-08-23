4979c443c3faa76da82dea8a9952f7025c90b30a
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-23 17:31:24 -0500

    feat(ui-frontpage): add components for displaying authorized news list and more news link
    The changes introduce new components for displaying authorized news list and a link to view more news. The AuthorizedDictyNews component is added to render the authorized news list with styling and logic to handle different states of the fetched news content. The AuthorizedMoreNewsLink component is included to provide a link for users to access more news content. These additions enhance the user experience by allowing users to easily navigate and view news content on the front page of the UI.

---
+++ packages/ui-frontpage/src/news/AuthorizedMoreNewsLink.tsx
@@ -0,0 +1,31 @@
+import { Grid, Button, Typography } from "@material-ui/core"
+import { Link } from "react-router-dom"
+import DoubleArrowIcon from "@material-ui/icons/DoubleArrow"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useDictyNewsStyles = makeStyles((theme) => ({
+  link: {
+    color: theme.palette.primary.main,
+    "&:hover": {
+      backgroundColor: "inherit",
+      color: "red",
+    },
+  },
+}))
+
+const AuthorizedMoreNewsLink = () => {
+  const { link } = useDictyNewsStyles()
+  return (
+    <Grid justifyContent="flex-end" container>
+      <Grid item>
+        <Link to="/news/editable">
+          <Button className={link} endIcon={<DoubleArrowIcon />}>
+            <Typography variant="h2"> More News </Typography>
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
+  )
+}
+
+export { AuthorizedMoreNewsLink }
