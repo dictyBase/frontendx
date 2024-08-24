a138010395777480816512a100796e52d3ec7cf0
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-23 19:05:52 -0500

    feat(AuthorizedNewsList.test.tsx): add test case to render news content with dynamic text
    The changes introduce a new test case in the AuthorizedNewsList component test file. The test now includes rendering news content with dynamic text "Rice & Beans" extracted from the mockContent object. This enhancement ensures that the AuthorizedNewsList component can render news content with varying text content dynamically.

--- packages/ui-frontpage/src/__tests__/AuthorizedNewsList.test.tsx
+++ packages/ui-frontpage/src/__tests__/AuthorizedNewsList.test.tsx
@@ -1,19 +1,63 @@
 import { render, screen } from "@testing-library/react"
 import { describe, it, expect } from "vitest"
-import { AuthorizedNewsList } from "../src/news/AuthorizedNewsList"
 import { BrowserRouter } from "react-router-dom"
+import { AuthorizedNewsList } from "../news/AuthorizedNewsList"
+
+const expectedText = "Rice & Beans"
+const mockContent = {
+  root: {
+    children: [
+      {
+        children: [
+          {
+            children: [
+              {
+                detail: 0,
+                format: 0,
+                mode: "normal",
+                style: "font-size: 20px;",
+                text: expectedText,
+                type: "text",
+                version: 1,
+              },
+            ],
+            direction: "ltr",
+            format: "",
+            indent: 0,
+            type: "paragraph",
+            version: 1,
+          },
+        ],
+        direction: "ltr",
+        format: "",
+        indent: 0,
+        type: "flex-layout",
+        version: 1,
+      },
+    ],
+    direction: "ltr",
+    format: "",
+    indent: 0,
+    type: "root",
+    version: 1,
+  },
+}
 
 describe("AuthorizedNewsList", () => {
   it("renders authorized news items", () => {
     const contentList = [
-      { name: "news1", content: "Content 1", updated_at: "2024-08-23T00:00:00Z" },
+      {
+        name: "news1",
+        content: JSON.stringify(mockContent),
+        updated_at: "2024-08-23T00:00:00Z",
+      },
     ]
     render(
       <BrowserRouter>
         <AuthorizedNewsList contentList={contentList} />
-      </BrowserRouter>
+      </BrowserRouter>,
     )
-    expect(screen.getByText("August 23, 2024")).toBeInTheDocument()
-    expect(screen.getByText("Content 1")).toBeInTheDocument()
+    expect(screen.getByText("August 23rd, 2024")).toBeInTheDocument()
+    expect(screen.getByText(expectedText)).toBeInTheDocument()
   })
 })
