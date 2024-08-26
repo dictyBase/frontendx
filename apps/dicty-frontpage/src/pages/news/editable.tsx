8e38a9462b86b9ba1d67ead7572f6928be3e8def
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-26 15:59:51 -0500

    chore(news/editable.tsx): add OtherError component import and replace error message display with EmptyNewsViewAuth component
    The import of the OtherError component from "@dictybase/ui-common" was added to the file. The error message display was replaced with the EmptyNewsViewAuth component to improve the user experience and provide a more consistent look and feel across the application.

--- apps/dicty-frontpage/src/pages/news/editable.tsx
+++ apps/dicty-frontpage/src/pages/news/editable.tsx
@@ -5,7 +5,7 @@ import { pipe } from "fp-ts/function"
 import { map as Amap, sort as Asort } from "fp-ts/Array"
 import { Ord, contramap } from "fp-ts/Ord"
 import { match, P } from "ts-pattern"
-import { FullPageLoadingDisplay } from "@dictybase/ui-common"
+import { FullPageLoadingDisplay, OtherError } from "@dictybase/ui-common"
 import {
   useListContentByNamespaceQuery,
   ListContentByNamespaceQuery,
@@ -133,8 +133,8 @@ const EditableNews = () => {
       (contentList) => <NewsView contentList={contentList} />,
     )
     .with({ loading: true }, () => <FullPageLoadingDisplay />)
-    .with({ error: P.select(P.not(undefined)) }, (error) => (
-      <>{error.message}</>
+    .with({ error: P.select(P.not(undefined)) }, () => (
+      <EmptyNewsViewAuth />
     ))
     .otherwise(() => <> This message should not appear. </>)
 }
