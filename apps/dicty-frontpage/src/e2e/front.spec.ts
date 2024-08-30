ac0bbc1ba9431b6132226a0c17a57f4b41fdcb1e
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-30 14:54:21 -0500

    test(front.spec.ts): remove test for checking if all links are working as it is not reliable
    The test for checking if all links are working has been removed as it is not a reliable way to ensure the functionality of the application. The test relied on checking the status of responses for each link, which may not accurately reflect the actual behavior of the links.

--- apps/dicty-frontpage/src/e2e/front.spec.ts
+++ apps/dicty-frontpage/src/e2e/front.spec.ts
@@ -7,15 +7,3 @@ test("has title", async ({ page }) => {
 
   await expect(page).toHaveTitle(/dictyBase/)
 })
-
-test("All Links are working", async ({ page }) => {
-  await page.goto(BASE_URL)
-  const links = await page.getByRole("link").all()
-  for (const link of links) {
-    const [response] = await Promise.all([
-      page.waitForResponse((response) => response.status() === 200),
-      link.click(),
-    ])
-    expect(response.status()).toBe(200)
-  }
-})
