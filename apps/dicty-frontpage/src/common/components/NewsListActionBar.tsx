12fccad92caa2e9eb6f7af4d3fb7368cfc5b2602
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-30 11:02:09 -0500

    style(NewsListActionBar.tsx): remove unused imports, clean up styles, and simplify button component usage
    The changes were made to improve code readability and maintainability by removing unused imports, cleaning up unnecessary styles, and simplifying the usage of the button component for writing news articles.

--- apps/dicty-frontpage/src/common/components/NewsListActionBar.tsx
+++ apps/dicty-frontpage/src/common/components/NewsListActionBar.tsx
@@ -1,55 +1,21 @@
-import { useNavigate } from "react-router-dom"
 import { Grid, Theme, makeStyles, Button } from "@material-ui/core"
-import CreateIcon from "@material-ui/icons/Create"
+import { WriteNewsButton } from "@dictybase/ui-frontpage"
 
 const useStyles = makeStyles((theme: Theme) => ({
-  content: {
-    marginLeft: "auto",
-  },
   toolbar: {
     padding: theme.spacing(1),
     marginBottom: theme.spacing(2),
-    width: "100%",
-    alignItems: "center",
-  },
-  label: {
-    marginLeft: "auto",
-    marginRight: theme.spacing(1),
-    padding: theme.spacing(1),
-    fontSize: "0.8rem",
-    fontWeight: "bold",
-    lineHeight: 1,
-    color: "#fff",
-    whiteSpace: "nowrap",
-    borderRadius: "0.25em",
-    backgroundColor: theme.palette.primary.light,
-  },
-  icon: {
-    color: theme.palette.primary.light,
-    fontSize: "1rem",
-    marginRight: theme.spacing(0.5),
-  },
-  text: {
-    color: theme.palette.primary.light,
+    top: 20,
+    position: "sticky",
   },
 }))
 
 const NewsListActionBar = () => {
   const { toolbar } = useStyles()
-  const navigate = useNavigate()
-  const handleClick = () => {
-    navigate("/news/create")
-  }
   return (
-    <Grid container className={toolbar} data-testid="info-page-toolbar">
+    <Grid container direction="column" className={toolbar} data-testid="info-page-toolbar">
       <Grid item>
-        <Button
-          startIcon={<CreateIcon />}
-          color="primary"
-          variant="outlined"
-          onClick={handleClick}>
-          Write News Article
-        </Button>
+        <WriteNewsButton />
       </Grid>
     </Grid>
   )
