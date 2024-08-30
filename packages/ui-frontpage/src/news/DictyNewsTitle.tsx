506df01ecf736958558897222799c6cb5dfb0456
Kevin Tun <kevin.tun@northwestern.edu>
2024-08-30 15:56:58 -0500

    style(news): update news titles to use "DCR News" instead of "Dicty News" and "Dicty Community Resource News"
    The news titles have been updated to use "DCR News" for consistency and to reduce the size of the title News title on the landing page. This change improves the overall style and branding of the news section in the UI.

--- packages/ui-frontpage/src/news/DictyNewsTitle.tsx
+++ packages/ui-frontpage/src/news/DictyNewsTitle.tsx
@@ -13,7 +13,7 @@ const DictyNewsTitle = () => {
   return (
     <Grid container spacing={1} alignItems="center">
       <Grid item>
-        <Typography variant="h1">Dicty Community Resource News</Typography>
+        <Typography variant="h1">DCR News</Typography>
       </Grid>
       <Grid item>
         <AnnouncementIcon className={newsIcon} />
