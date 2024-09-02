7d1eba64a72130530b78f641d8835dfc40397c54
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-02 10:21:12 -0500

    feat(state.ts): add selectedNewsArticlesAtom atom to manage selected news articles in the frontpage app
    The unused file newsState.ts has been removed to declutter the project structure. The selectedNewsArticlesAtom atom has been added to manage the state of selected news articles in the frontpage app, improving the state management and organization of the application.

--- apps/dicty-frontpage/src/newsState.ts
+++
@@ -1,8 +0,0 @@
-import { PrimitiveAtom, atom } from "jotai"
-import {
-  ListContentByNamespaceQuery,
-} from "dicty-graphql-schema"
-
-const selectedNewsArticlesAtom = atom<ListContentByNamespaceQuery["listContentByNamespace"]>([])
-
-export { selectedNewsArticlesAtom }
--- apps/dicty-frontpage/src/state.ts
+++ apps/dicty-frontpage/src/state.ts
@@ -1,6 +1,12 @@
 import { atom } from "jotai"
 import { Option, none } from "fp-ts/Option"
+import { ListContentByNamespaceQuery } from "dicty-graphql-schema"
 
 const contentIdAtom = atom<Option<string>>(none)
 
-export { contentIdAtom }
+
+const selectedNewsArticlesAtom = atom<
+  ListContentByNamespaceQuery["listContentByNamespace"]
+>([])
+
+export { contentIdAtom, selectedNewsArticlesAtom }
