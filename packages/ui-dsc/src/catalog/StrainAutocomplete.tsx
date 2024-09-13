0be4aaa70b6116ec0f8cff12edeb68df2bf8f6fd
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-13 03:09:26 -0500

    feat(ui-dsc): add functionality to handle autocomplete change events in StrainAutocomplete component
    The changes introduce a new function, handleAutocompleteChange, to handle autocomplete change events in the StrainAutocomplete component. This function allows for different actions based on the reason for the change event. Additionally, the unused useState and useEffect for searchLabel have been removed to clean up the code and improve maintainability.

--- packages/ui-dsc/src/catalog/StrainAutocomplete.tsx
+++ packages/ui-dsc/src/catalog/StrainAutocomplete.tsx
@@ -1,33 +1,50 @@
-import { useState, useEffect, ChangeEvent } from "react"
+import { ChangeEvent } from "react"
 import { Autocomplete } from "@material-ui/lab"
 import { TextField, CircularProgress } from "@material-ui/core"
+import { pipe } from "fp-ts/function"
+import { none, fromNullable, map as Omap } from "fp-ts/Option"
 import { match, P } from "ts-pattern"
+import { useSetAtom } from "jotai"
 import { useStrainListLazyQuery, StrainType } from "dicty-graphql-schema"
+import { selectedStrainIdAtom } from "../addPhenotypeState"
 
 const StrainAutocomplete = () => {
-  const [searchLabel, setSearchLabel] = useState("")
   const [getStrains, { data, loading, error }] = useStrainListLazyQuery()
+  const setSelectedStrainId = useSetAtom(selectedStrainIdAtom)
 
-  useEffect(() => {
-    const fetchStrains = async () => {
-      getStrains({
-        variables: {
-          cursor: 10,
-          limit: 10,
-          filter: {
-            strain_type: StrainType.All,
-            label: searchLabel,
-          },
-        },
+  const handleAutocompleteChange = (
+    _: ChangeEvent<{}>,
+    value: { id: string } | null,
+    reason: "select-option" | "clear" | "create-option" | "remove-option",
+  ) => {
+    match(reason)
+      .with("clear", () => {
+        setSelectedStrainId(none)
       })
-    }
-    fetchStrains()
-  }, [getStrains, searchLabel])
+      .with("select-option", () => {
+        pipe(
+          value,
+          fromNullable,
+          Omap(({ id }) => id),
+          setSelectedStrainId,
+        )
+      })
+      .otherwise(() => {})
+  }
 
   const handleTextFieldChange = ({
     target: { value },
   }: ChangeEvent<HTMLInputElement>) => {
-    setSearchLabel(value)
+    getStrains({
+      variables: {
+        cursor: 10,
+        limit: 10,
+        filter: {
+          strain_type: StrainType.All,
+          label: value,
+        },
+      },
+    })
   }
 
   const options = match(data)
@@ -40,7 +57,7 @@ const StrainAutocomplete = () => {
     .otherwise(() => [])
 
   const endAdornment = match(loading)
-    .with(true, () => <CircularProgress />)
+    .with(true, () => <CircularProgress size="1rem" />)
     .with(false, () => <></>)
     .exhaustive()
 
@@ -48,6 +65,7 @@ const StrainAutocomplete = () => {
     <Autocomplete
       options={options}
       getOptionLabel={(option) => option.label}
+      onChange={handleAutocompleteChange}
       renderInput={(parameters) => (
         <TextField
           {...parameters}
