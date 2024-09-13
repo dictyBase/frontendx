f0962847cd100398406dcad1f0b6e4de020a525e
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-13 01:31:46 -0500

    feat(phenotypes/add.tsx): add PhenotypesLoader component to display phenotypes for a specific strain
    The PhenotypesLoader component is added to the AddPhenotypePage to display phenotypes for a specific strain. This enhances the functionality of the page by providing a way to view and manage phenotypes associated with the selected strain.

--- apps/stock-center/src/pages/phenotypes/add.tsx
+++ apps/stock-center/src/pages/phenotypes/add.tsx
@@ -1,7 +1,16 @@
-import { PhenotypeInputPanel } from "@dictybase/ui-dsc"
+import { PhenotypeInputPanel, PhenotypesLoader } from "@dictybase/ui-dsc"
 import { Grid } from "@material-ui/core"
 
-const AddPhenotypePage = () => <Grid container />
+const AddPhenotypePage = () => (
+  <Grid spacing={3} container>
+    <Grid sm={3} xl={3} item>
+      <PhenotypeInputPanel />
+    </Grid>
+    <Grid sm={9} xl={9} item>
+      <PhenotypesLoader strainId="test" />
+    </Grid>
+  </Grid>
+)
 
-// eslint-disable-next-line import/no-default-export, unicorn/prefer-export-from
-export default PhenotypeInputPanel
+// eslint-disable-next-line import/no-default-export
+export default AddPhenotypePage
