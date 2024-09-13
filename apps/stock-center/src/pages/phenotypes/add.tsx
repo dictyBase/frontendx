43e79967e8759b3daeff9ebb54e61b998f0e3db9
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-13 03:20:41 -0500

    feat(phenotypes/add.tsx): add dynamic rendering based on selected strain ID
    The code now dynamically renders either a NoStrainSelected component or a PhenotypesLoader component based on the selectedStrain ID. This change improves the user experience by showing relevant information based on the user's selection.

--- apps/stock-center/src/pages/phenotypes/add.tsx
+++ apps/stock-center/src/pages/phenotypes/add.tsx
@@ -1,16 +1,33 @@
-import { PhenotypeInputPanel, PhenotypesLoader } from "@dictybase/ui-dsc"
+import {
+  PhenotypeInputPanel,
+  PhenotypesLoader,
+  NoStrainSelected,
+  selectedStrainIdAtom,
+} from "@dictybase/ui-dsc"
+import { pipe } from "fp-ts/function"
+import { match as Omatch } from "fp-ts/Option"
+import { useAtomValue } from "jotai"
 import { Grid } from "@material-ui/core"
 
-const AddPhenotypePage = () => (
-  <Grid spacing={3} container>
-    <Grid sm={3} xl={3} item>
-      <PhenotypeInputPanel />
+const AddPhenotypePage = () => {
+  const selectedStrain = useAtomValue(selectedStrainIdAtom)
+  return (
+    <Grid spacing={3} container>
+      <Grid sm={3} xl={3} item>
+        <PhenotypeInputPanel />
+      </Grid>
+      <Grid sm={9} xl={9} item>
+        {pipe(
+          selectedStrain,
+          Omatch(
+            () => <NoStrainSelected />,
+            (id) => <PhenotypesLoader strainId={id} />,
+          ),
+        )}
+      </Grid>
     </Grid>
-    <Grid sm={9} xl={9} item>
-      <PhenotypesLoader strainId="test" />
-    </Grid>
-  </Grid>
-)
+  )
+}
 
 // eslint-disable-next-line import/no-default-export
 export default AddPhenotypePage
