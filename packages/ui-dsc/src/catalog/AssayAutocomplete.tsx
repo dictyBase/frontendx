42eeb32585ee01ce92aec4454c70d4d1436952e9
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-13 10:51:18 -0500

    feat(ui-dsc): add AssayAutocomplete component for strain search functionality
    The new AssayAutocomplete component is added to provide a user interface for searching strains. It includes functionality to fetch strains based on user input, display loading indicator, and render the autocomplete dropdown with strain options. This component enhances the user experience by enabling easy strain selection within the application.

---
+++ packages/ui-dsc/src/catalog/AssayAutocomplete.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect, ChangeEvent } from "react"
+import { Autocomplete } from "@material-ui/lab"
+import { TextField, CircularProgress } from "@material-ui/core"
+import { match, P } from "ts-pattern"
+import { useStrainListLazyQuery, StrainType } from "dicty-graphql-schema"
+
+const AssayAutocomplete = () => {
+  const [searchLabel, setSearchLabel] = useState("")
+  const [getStrains, { data, loading, error }] = useStrainListLazyQuery()
+
+  useEffect(() => {
+    const fetchStrains = async () => {
+      getStrains({
+        variables: {
+          cursor: 10,
+          limit: 10,
+          filter: {
+            strain_type: StrainType.All,
+            label: searchLabel,
+          },
+        },
+      })
+    }
+    fetchStrains()
+  }, [getStrains, searchLabel])
+
+  const handleTextFieldChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setSearchLabel(value)
+  }
+
+  const options = match(data)
+    .with(
+      {
+        listStrains: { strains: P.select(P.array({ label: P.string })) },
+      },
+      (strains) => strains,
+    )
+    .otherwise(() => [])
+
+  const endAdornment = match(loading)
+    .with(true, () => <CircularProgress />)
+    .with(false, () => <></>)
+    .exhaustive()
+
+  return (
+    <Autocomplete
+      options={options}
+      getOptionLabel={(option) => option.label}
+      renderInput={(parameters) => (
+        <TextField
+          {...parameters}
+          size="small"
+          label="Assay"
+          variant="outlined"
+          onChange={handleTextFieldChange}
+          InputProps={{
+            ...parameters.InputProps,
+            endAdornment: (
+              <>
+                {endAdornment}
+                {parameters.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  )
+}
+
+export { AssayAutocomplete }
