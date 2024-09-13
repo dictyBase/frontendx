4b82613c5968019a9ff7a9dc5174bc4ff3081712
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-13 01:32:14 -0500

    feat(ui-dsc): add PhenotypesLoader component to dynamically display existing phenotypes for a given strain
    The PhenotypesLoader component is added to the project to dynamically display existing phenotypes for a specific strain. This component fetches data using the useStrainQuery hook and then uses pattern matching to render different UI components based on the loading state, error state, or successful data retrieval. This enhances the user experience by providing real-time information on the phenotypes associated with a particular strain.

---
+++ packages/ui-dsc/src/catalog/PhenotypesLoader.tsx
@@ -0,0 +1,27 @@
+import { Container } from "@material-ui/core"
+import { useStrainQuery, Strain } from "dicty-graphql-schema"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
+import { match, P } from "ts-pattern"
+import { ExistingPhenotypeList } from "./ExistingPhenotypesList"
+
+type PhenotypesLoaderProperties = {
+  strainId: Strain["id"]
+}
+
+const PhenotypesLoader = ({ strainId }: PhenotypesLoaderProperties) => {
+  const result = useStrainQuery({ variables: { id: strainId } })
+  return match(result)
+    .with({ loading: true }, () => <FullPageLoadingDisplay />)
+    .with({ error: P.not(undefined) }, () => <> error </>)
+    .with(
+      {
+        data: {
+          strain: { phenotypes: P.select(P.array({ phenotype: P.string })) },
+        },
+      },
+      (phenotypes) => <ExistingPhenotypeList phenotypes={phenotypes} />,
+    )
+    .otherwise(() => <> This message should not appear </>)
+}
+
+export { PhenotypesLoader }
