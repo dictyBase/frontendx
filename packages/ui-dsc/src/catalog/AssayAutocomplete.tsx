ecd40fa21fbbec96978bf668335db16276aa00b8
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-16 08:57:55 -0500

    style(ui-dsc): improve code readability by restructuring and simplifying the AssayAutocomplete component
    The code has been refactored to improve readability and maintainability. The component now uses the react-hook-form library for form control, making it easier to manage form state and validation. The use of the useListPhenotypeAssaysLazyQuery hook has been added to fetch phenotype assays. The handleAutocompleteChange function has been updated to handle different reasons for change. Additionally, the Autocomplete component now directly uses the value from the form controller, enhancing consistency in data flow.

--- packages/ui-dsc/src/catalog/AssayAutocomplete.tsx
+++ packages/ui-dsc/src/catalog/AssayAutocomplete.tsx
@@ -1,59 +1,78 @@
-import { useState, useEffect, ChangeEvent } from "react"
+/* eslint-disable dot-notation */
+import { ChangeEvent } from "react"
+import { useController } from "react-hook-form"
 import { Autocomplete } from "@material-ui/lab"
 import { TextField, CircularProgress } from "@material-ui/core"
 import { match, P } from "ts-pattern"
-import { useStrainListLazyQuery, StrainType } from "dicty-graphql-schema"
+import { useListPhenotypeAssaysLazyQuery } from "dicty-graphql-schema"
 
 const AssayAutocomplete = () => {
-  const [searchLabel, setSearchLabel] = useState("")
-  const [getStrains, { data, loading, error }] = useStrainListLazyQuery()
+  const {
+    field: { value, onChange, onBlur },
+    formState: { errors },
+  } = useController({ name: "assay" })
+  const [getAssays, { data, loading, error }] =
+    useListPhenotypeAssaysLazyQuery()
 
-  useEffect(() => {
-    const fetchStrains = async () => {
-      getStrains({
-        variables: {
-          cursor: 10,
-          limit: 10,
-          filter: {
-            strain_type: StrainType.All,
-            label: searchLabel,
-          },
-        },
+  const handleAutocompleteChange = (
+    _: ChangeEvent<{}>,
+    changeValue: string,
+    reason:
+      | "select-option"
+      | "clear"
+      | "create-option"
+      | "remove-option"
+      | "blur",
+  ) => {
+    match(reason)
+      .with("select-option", () => {
+        onChange(changeValue)
       })
-    }
-    fetchStrains()
-  }, [getStrains, searchLabel])
-
+      .with("clear", () => {
+        onChange(changeValue)
+      })
+      .otherwise(() => {})
+  }
   const handleTextFieldChange = ({
-    target: { value },
+    target: { value: textFieldValue },
   }: ChangeEvent<HTMLInputElement>) => {
-    setSearchLabel(value)
+    getAssays({
+      variables: {
+        search: textFieldValue,
+      },
+    })
   }
 
   const options = match(data)
     .with(
       {
-        listStrains: { strains: P.select(P.array({ label: P.string })) },
+        listPhenotypeAssays: P.select(P.array(P.string)),
       },
-      (strains) => strains,
+      (assys) => assys,
     )
     .otherwise(() => [])
 
   const endAdornment = match(loading)
-    .with(true, () => <CircularProgress />)
+    .with(true, () => <CircularProgress size="1rem" />)
     .with(false, () => <></>)
     .exhaustive()
 
   return (
     <Autocomplete
+      value={value}
       options={options}
-      getOptionLabel={(option) => option.label}
+      onBlur={onBlur}
+      onChange={handleAutocompleteChange}
+      getOptionLabel={(option) => option}
       renderInput={(parameters) => (
         <TextField
           {...parameters}
-          size="small"
+          name="assay"
           label="Assay"
+          size="small"
           variant="outlined"
+          error={!!errors["assay"]}
+          helperText={errors["assay"]?.message || ""}
           onChange={handleTextFieldChange}
           InputProps={{
             ...parameters.InputProps,
