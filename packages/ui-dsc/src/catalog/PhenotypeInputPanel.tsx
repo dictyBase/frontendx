41456325e5bff3bbeceb09be48d2518d736f6ccd
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-16 08:58:31 -0500

    refactor(PhenotypeInputPanel.tsx): restructure form layout and add validation schema for form fields
    The changes introduce new autocompletion fields for environment and assay in the PhenotypeInputPanel component. Additionally, the form layout has been restructured for better organization, and a validation schema has been implemented for the form fields to ensure required fields are filled before submission.

--- packages/ui-dsc/src/catalog/PhenotypeInputPanel.tsx
+++ packages/ui-dsc/src/catalog/PhenotypeInputPanel.tsx
@@ -1,6 +1,18 @@
-import { Paper, Container, Grid, Button, makeStyles } from "@material-ui/core"
+import {
+  Paper,
+  Container,
+  Grid,
+  Button,
+  Typography,
+  makeStyles,
+} from "@material-ui/core"
+import { useForm, FormProvider, SubmitHandler } from "react-hook-form"
+import { object, string, InferType } from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { StrainAutocomplete } from "./StrainAutocomplete"
 import { PhenotypeAutocomplete } from "./PhenotypeAutocomplete"
+import { EnvironmentAutocomplete } from "./EnvironmentAutocomplete"
+import { AssayAutocomplete } from "./AssayAutocomplete"
 import { PhenotypeNotesField } from "./PhenotypeNotesField"
 
 const useStyles = makeStyles({
@@ -9,38 +21,67 @@ const useStyles = makeStyles({
     paddingBottom: "1rem",
   },
 })
+
+const schemaValidation = object().shape({
+  phenotype: string().required("* Phenotype is required"),
+  environment: string(),
+  assay: string(),
+  publication: string().required("* Publication is required"),
+  notes: string(),
+})
+
 const PhenotypeInputPanel = () => {
+  const methods = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(schemaValidation),
+    defaultValues: {
+      phenotype: "",
+      environment: "",
+      assay: "",
+    },
+  })
   const { root } = useStyles()
+  const onSubmit: SubmitHandler<InferType<typeof schemaValidation>> = (
+    data,
+  ) => {
+    console.log("submit")
+    console.log(data)
+  }
+  const onClick = () => {
+    console.log(methods.getValues())
+  }
   return (
-    <Paper elevation={3} className={root}>
-      <Container>
-        <Grid container direction="column" spacing={2}>
-          <Grid item>
-            <StrainAutocomplete />
+    <FormProvider {...methods}>
+      <Paper elevation={3} className={root}>
+        <Container>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <Typography variant="h3"> Add Phenotype </Typography>
+            </Grid>
+            <Grid item>
+              <StrainAutocomplete />
+            </Grid>
+            <Grid item>
+              <PhenotypeAutocomplete />
+            </Grid>
+            <Grid item>
+              <EnvironmentAutocomplete />
+            </Grid>
+            <Grid item>
+              <AssayAutocomplete />
+            </Grid>
+            <Grid item>
+              <PhenotypeNotesField />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" onClick={onClick}>
+                Add
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item>
-            <PhenotypeAutocomplete />
-          </Grid>
-          <Grid item>
-            <PhenotypeAutocomplete />
-          </Grid>
-          <Grid item>
-            <PhenotypeAutocomplete />
-          </Grid>
-          <Grid item>
-            <PhenotypeAutocomplete />
-          </Grid>
-          <Grid item>
-            <PhenotypeNotesField />
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary">
-              Add
-            </Button>
-          </Grid>
-        </Grid>
-      </Container>
-    </Paper>
+        </Container>
+      </Paper>
+    </FormProvider>
   )
 }
 
