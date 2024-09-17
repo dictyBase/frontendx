105225ed6b766339442b279de923396f187b79f9
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-17 11:11:48 -0500

    feat(strains): add new editable and index files for strain details page
    The new editable.tsx and index.tsx files have been added to the strains directory in order to provide components for displaying and editing strain details on the respective pages. This enhances the functionality of the application by allowing users to view and modify strain information efficiently.

---
+++ apps/stock-center/src/pages/strains/[id]/editable.tsx
@@ -0,0 +1,4 @@
+import { EditableStrainDetailsContainer } from "@dictybase/ui-dsc"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default EditableStrainDetailsContainer
