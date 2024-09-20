d08103e318fc4f745747a2d502d0458f6535f0c6
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-20 12:38:08 -0500

    style(PhenotypeReferenceField.tsx): remove unused IconButton and SearchIcon imports, simplify component structure
    The changes were made to improve code readability and remove unnecessary imports and elements from the PhenotypeReferenceField component. This simplification enhances the maintainability of the codebase.

--- packages/ui-dsc/src/catalog/PhenotypeReferenceField.tsx
+++ packages/ui-dsc/src/catalog/PhenotypeReferenceField.tsx
@@ -1,33 +1,23 @@
 /* eslint-disable dot-notation */
-import { TextField, IconButton } from "@material-ui/core"
-import SearchIcon from "@material-ui/icons/Search"
+import { TextField } from "@material-ui/core"
 import { useFormContext } from "react-hook-form"
 
-type PhenotypeReferenceFieldProperties = {
-  onClick: () => void
-}
-const PhenotypeReferenceField = ({
-  onClick,
-}: PhenotypeReferenceFieldProperties) => {
+const PhenotypeReferenceField = () => {
   const {
     register,
     formState: { errors },
   } = useFormContext()
 
   return (
-    <>
-      <TextField
-        {...register("publication")}
-        label="Reference"
-        size="small"
-        variant="outlined"
-        error={!!errors["publication"]}
-        helperText={errors["publication"]?.message || ""}
-      />
-      <IconButton onClick={onClick}>
-        <SearchIcon />
-      </IconButton>
-    </>
+    <TextField
+      {...register("publication")}
+      fullWidth
+      label="* Reference"
+      size="small"
+      variant="outlined"
+      error={!!errors["publication"]}
+      helperText={errors["publication"]?.message || ""}
+    />
   )
 }
 
