80007a1ee7738af93679e8200c9e206ee2c62e32
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-20 12:38:25 -0500

    refactor(PhenotypeReferencePanel.tsx): simplify PhenotypeReferencePanel component structure
    The PhenotypeReferencePanel component structure has been simplified by removing unnecessary code related to form context and lazy query. This refactor improves code readability and reduces complexity by focusing only on rendering the PhenotypeReferenceField and PhenotypeReferenceDetails components within a Grid container.

--- packages/ui-dsc/src/catalog/PhenotypeReferencePanel.tsx
+++ packages/ui-dsc/src/catalog/PhenotypeReferencePanel.tsx
@@ -1,26 +1,16 @@
 import { Grid } from "@material-ui/core"
-import { usePublicationLazyQuery } from "dicty-graphql-schema"
-import { useFormContext } from "react-hook-form"
 import { PhenotypeReferenceField } from "./PhenotypeReferenceField"
 import { PhenotypeReferenceDetails } from "./PhenotypeReferenceDetails"
 
-const PhenotypeReferencePanel = () => {
-  const { getValues, setValue } = useFormContext()
-  const [getPublication, result] = usePublicationLazyQuery()
-
-  const onClick = () => {
-    getPublication(getValues("publication"))
-  }
-  return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item>
-        <PhenotypeReferenceField onClick={onClick} />
-      </Grid>
-      <Grid item>
-        <PhenotypeReferenceDetails result={result} />
-      </Grid>
+const PhenotypeReferencePanel = () => (
+  <Grid container direction="column" spacing={2}>
+    <Grid item>
+      <PhenotypeReferenceField />
     </Grid>
-  )
-}
+    <Grid item>
+      <PhenotypeReferenceDetails />
+    </Grid>
+  </Grid>
+)
 
 export { PhenotypeReferencePanel }
