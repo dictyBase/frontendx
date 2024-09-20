77d9c5b18d072055bc3a769822734d2ee782b4aa
Kevin Tun <kevin.tun@northwestern.edu>
2024-09-20 12:37:18 -0500

    feat(ui-dsc): add usePhenotypeValidation hook for form validation in the catalog component
    The usePhenotypeValidation hook is added to provide form validation for the catalog component. It utilizes the react-hook-form library along with Yup schema validation to ensure that the phenotype and reference publication fields are required. Additionally, it includes a test to check if the publication exists by making a query to the GraphQL schema. This hook enhances the user experience by validating form inputs before submission.

---
+++ packages/ui-dsc/src/catalog/usePhenotypeValidation.ts
@@ -0,0 +1,50 @@
+import { useMemo } from "react"
+import { pipe } from "fp-ts/function"
+import { fromNullable as OfromNullable, matchW as OmatchW } from "fp-ts/Option"
+import { useForm } from "react-hook-form"
+import { object, string } from "yup"
+import { usePublicationLazyQuery } from "dicty-graphql-schema"
+import { yupResolver } from "@hookform/resolvers/yup"
+
+const usePhenotypeValidation = () => {
+  const [getPublication] = usePublicationLazyQuery()
+
+  const schemaValidation = useMemo(() =>
+    object().shape({
+      phenotype: string().required("* Phenotype is required"),
+      environment: string(),
+      assay: string(),
+      publication: string()
+        .required("* Reference Publication is required")
+        .test(async (value, context) => {
+          if (value.length === 0) return false
+          const { data } = await getPublication({ variables: { id: value } })
+          return pipe(
+            data,
+            OfromNullable,
+            OmatchW(
+              () => context.createError({ message: "* Publication not found"}),
+              () => true,
+            ),
+          )
+        }),
+      note: string(),
+    }),
+    [getPublication]
+  )
+
+  const methods = useForm({
+    mode: "onTouched",
+    resolver: yupResolver(schemaValidation),
+    defaultValues: {
+      phenotype: "",
+      environment: "",
+      assay: "",
+      publication: "",
+      note: "",
+    },
+  })
+  return { methods, schemaValidation }
+}
+
+export { usePhenotypeValidation }
