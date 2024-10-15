a9f081382bbf59ba6d2a01ea374dfd64eae379e0
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-14 19:49:45 -0500

    chore(header): update module declarations for image file types to include support for webp format
    The module declarations for image file types have been updated to include support for the webp format. This change ensures that the header package can now handle webp images in addition to png, jpg, and avif formats.

--- packages/header/index.d.ts
+++ packages/header/index.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="vite/client" />
 
-declare module "*.png"
+declare module "*.png" {
+  const image: string | { src: string }
+  export = image
+}
+declare module "*.webp" {
+  const image: string | { src: string }
+  export = image
+}
 declare module "*.jpg"
 declare module "*.avif"
-declare module "*.webp"
