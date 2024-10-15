93a2cc94681f4a6791c3652aa91fb6ed11774186
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-14 20:46:07 -0500

    refactor(resolveImage.ts): simplify resolveImage function by removing unnecessary return statement and curly braces
    The resolveImage function has been refactored to simplify the code by removing the unnecessary return statement and curly braces, making the code cleaner and more concise.

--- packages/header/src/helper/resolveImage.ts
+++ packages/header/src/helper/resolveImage.ts
@@ -3,13 +3,12 @@ import { match, P } from "ts-pattern"
 /**
  * Resolves differences between image imports by Vite's image loader and Nextjs's image loader.
  */
-const resolveImage = (image: string | { src: string }): string => {
-  return match(image)
+const resolveImage = (image: string | { src: string }): string =>
+  match(image)
     // Vite
     .with(P.select(P.string), (pngUrl) => pngUrl)
     // Nextjs
     .with({ src: P.select(P.string) }, (webpUrl) => webpUrl)
     .exhaustive()
-}
 
 export { resolveImage }
