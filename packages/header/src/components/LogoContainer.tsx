673899391ded3dfcf2d5187c306751ea245fe326
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-14 20:40:23 -0500

    feat(LogoContainer.tsx): add resolveImage helper function to dynamically resolve image paths
    The resolveImage helper function is added to dynamically resolve image paths for LogoPng and LogoWebp. This change improves code readability and maintainability by centralizing the logic for resolving image paths in one place.

--- packages/header/src/components/LogoContainer.tsx
+++ packages/header/src/components/LogoContainer.tsx
@@ -2,6 +2,7 @@ import Typography from "@material-ui/core/Typography"
 import type { ReactNode } from "react"
 import { Image } from "@dictybase/dicty-image"
 import { headerStyles } from "../styles/headerStyles"
+import { resolveImage } from "../helper/resolveImage"
 import LogoPng from "../images/logo.png"
 import LogoWebp from "../images/logo.webp"
 
@@ -10,15 +11,19 @@ interface ReactChildrenProperty {
   frontPageUrl: string
 }
 
-const ImgContainer = () => (
-  <Image
-    src={LogoPng}
-    webpSrc={LogoWebp}
-    height="10vh"
-    width="90%"
-    fit="fill"
-  />
-)
+const ImgContainer = () => {
+  const png = resolveImage(LogoPng)
+  const webp = resolveImage(LogoWebp)
+  return (
+    <Image
+      src={png}
+      webpSrc={webp}
+      height="10vh"
+      width="90%"
+      fit="fill"
+    />
+  )
+}
 
 const TitleContainer = ({ title }: { title: string }) => (
   <Typography variant="subtitle1" className={headerStyles().logoTitle}>
