abf114309b16e1a57137b85d20df339ef6e86793
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-17 10:06:40 -0500

    style(LogoContainer.tsx): reformat indentation for better code readability
    The indentation of the Image component in the ImgContainer function has been adjusted for better code readability and consistency. This change improves the overall structure of the codebase.

--- packages/header/src/components/LogoContainer.tsx
+++ packages/header/src/components/LogoContainer.tsx
@@ -9,13 +9,13 @@ interface ReactChildrenProperty {
 }
 
 const ImgContainer = () => (
-    <Image
-        src="https://storage.dictybase.dev/editor/assets/2024-10-17/b5d1517b-6740-4e07-8d8b-761a6005a647"
-        webpSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/2382123c-8448-4f47-b991-06c1384d2c82"
-        avifSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/04cea154-6be4-4c78-9d29-0b328cd67b0e"
-        height="10vh"
-        width="90%"
-        fit="fill" />
+  <Image
+    src="https://storage.dictybase.dev/editor/assets/2024-10-17/b5d1517b-6740-4e07-8d8b-761a6005a647"
+    webpSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/2382123c-8448-4f47-b991-06c1384d2c82"
+    avifSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/04cea154-6be4-4c78-9d29-0b328cd67b0e"
+    height="10vh"
+    width="90%"
+    fit="fill" />
 )
 
 const TitleContainer = ({ title }: { title: string }) => (
