cd4cd2c9ecda4a3761c2922bf98eddc41ee01359
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-17 09:56:21 -0500

    style(LogoContainer.tsx): refactor ImgContainer component to use inline Image component with direct image URLs
    The ImgContainer component has been refactored to use the Image component directly with inline image URLs instead of resolving images from local files. This change improves readability and simplifies the component structure.

--- packages/header/src/components/LogoContainer.tsx
+++ packages/header/src/components/LogoContainer.tsx
@@ -2,20 +2,21 @@ import Typography from "@material-ui/core/Typography"
 import type { ReactNode } from "react"
 import { Image } from "@dictybase/dicty-image"
 import { headerStyles } from "../styles/headerStyles"
-import { resolveImage } from "../helper/resolveImage"
-import LogoPng from "../images/logo.png"
-import LogoWebp from "../images/logo.webp"
 
 interface ReactChildrenProperty {
   children: ReactNode
   frontPageUrl: string
 }
 
-const ImgContainer = () => {
-  const png = resolveImage(LogoPng)
-  const webp = resolveImage(LogoWebp)
-  return <Image src={png} webpSrc={webp} height="10vh" width="90%" fit="fill" />
-}
+const ImgContainer = () => (
+    <Image
+        src="https://storage.dictybase.dev/editor/assets/2024-10-17/b5d1517b-6740-4e07-8d8b-761a6005a647"
+        webpSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/2382123c-8448-4f47-b991-06c1384d2c82"
+        avifSrc="https://storage.dictybase.dev/editor/assets/2024-10-17/04cea154-6be4-4c78-9d29-0b328cd67b0e"
+        height="10vh"
+        width="90%"
+        fit="fill" />
+)
 
 const TitleContainer = ({ title }: { title: string }) => (
   <Typography variant="subtitle1" className={headerStyles().logoTitle}>
