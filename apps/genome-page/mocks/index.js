54ff837165ce00b72928702f7513e35792ecd795
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-18 10:40:47 -0500

    fix(mocks): update import path for setupWorker to use msw/browser instead of msw
    The import path for setupWorker has been updated to use "msw/browser" instead of "msw" to ensure the correct module is being imported. This change helps in maintaining consistency across different parts of the application where setupWorker is used with msw.

--- apps/genome-page/mocks/index.js
+++ apps/genome-page/mocks/index.js
@@ -1,7 +1,7 @@
 const enableMock = async () => {
   if (typeof window === "undefined") {
-    const { server } = await import("./server")
-    server.listen()
+    //    const { server } = await import("./server")
+    //    server.listen()
   } else {
     const { worker } = await import("./browser")
     await worker.start()
