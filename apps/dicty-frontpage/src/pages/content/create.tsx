26c274d6320b866375e224608b0d95c86f9e4893
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-25 01:18:42 -0500

    feat(content/create.tsx): refactor toolbar in AddPageView component to use CreateContentForm component for consistency and reusability
    The toolbar in the AddPageView component has been refactored to use the CreateContentForm component instead of the custom Controls component. This change improves consistency across components and promotes reusability of the CreateContentForm component.

--- apps/dicty-frontpage/src/pages/content/create.tsx
+++ apps/dicty-frontpage/src/pages/content/create.tsx
@@ -1,20 +1,12 @@
-import { useState, ChangeEvent, Dispatch, SetStateAction } from "react"
 import { useNavigate } from "react-router-dom"
 import {
   Button,
   Typography,
-  TextField,
   Container,
-  Paper,
-  InputLabel,
-  Select,
-  Grid,
-  MenuItem,
-  FormControl,
   makeStyles,
 } from "@material-ui/core"
+import { CreateContentForm, ActionBar } from "@dictybase/ui-common"
 import { Editor } from "@dictybase/editor"
-import { ActionBar } from "@dictybase/ui-common"
 import { ACCESS } from "@dictybase/auth"
 import { CreateButton } from "../../common/components/CreateButton"
 
@@ -22,74 +14,6 @@ type AddPageViewProperties = {
   namespace: string
   name: string
   contentPath: string
-}
-
-const useStyles = makeStyles({
-  root: {
-    padding: "0.5rem",
-  },
-  grid: {
-    alignContent: "baseline",
-  },
-})
-
-enum Section {
-  EMPTY = "",
-  EXPLORE = "Explore",
-  RESEARCH = "Research",
-  COMMUNITY = "Community",
-  INFORMATION = "DSC Information",
-}
-
-type SectionSelectProperties = {
-  section: Section
-  setSection: Dispatch<SetStateAction<Section>>
-}
-
-const SectionSelect = ({ section, setSection }: SectionSelectProperties) => {
-  const onChange = ({
-    target: { value },
-  }: ChangeEvent<{ name?: string; value: unknown }>) => {
-    setSection(value as Section)
-  }
-  return (
-    <>
-      <InputLabel id="section-select-label"> Section </InputLabel>
-      <Select
-        id="section-select"
-        labelId="section-select-label"
-        autoWidth
-        variant="outlined"
-        onChange={onChange}>
-        <MenuItem value={Section.EXPLORE}>{Section.EXPLORE}</MenuItem>
-        <MenuItem value={Section.RESEARCH}>{Section.RESEARCH}</MenuItem>
-        <MenuItem value={Section.COMMUNITY}>{Section.COMMUNITY}</MenuItem>
-        <MenuItem value={Section.INFORMATION}>{Section.INFORMATION}</MenuItem>
-      </Select>
-    </>
-  )
-}
-
-const Controls = () => {
-  const { root } = useStyles()
-  const [section, setSection] = useState<Section>(Section.EMPTY)
-  return (
-    <Container>
-      <Paper className={root}>
-        <Grid container spacing={1}>
-          <Grid item>
-            <SectionSelect section={section} setSection={setSection} />
-          </Grid>
-          <Grid item>
-            <TextField label="Name" name="name" variant="outlined" />
-          </Grid>
-          <Grid item>
-            <TextField label="Subname" name="subname" variant="outlined" />
-          </Grid>
-        </Grid>
-      </Paper>
-    </Container>
-  )
 }
 
 const AddPageView = ({
@@ -114,7 +38,7 @@ const AddPageView = ({
   )
   return (
     <Container>
-      <Editor editable toolbar={<Controls />} />
+      <Editor editable toolbar={<CreateContentForm />} />
     </Container>
   )
 }
