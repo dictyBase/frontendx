fe50fc1fbc4675006ac29f6db03e198127d73a38
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-26 05:46:12 -0500

    refactor(useCreateContentFromEditor.tsx): remove unnecessary parameters from useCreateContentFromEditor hook
    The unnecessary parameters 'namespace' and 'name' have been removed from the useCreateContentFromEditor hook to simplify its usage. The hook now only requires the 'editor' context and calls the 'authorizedCreateContent' function with the appropriate parameters directly.

--- apps/dicty-frontpage/src/common/hooks/useCreateContentFromEditor.tsx
+++ apps/dicty-frontpage/src/common/hooks/useCreateContentFromEditor.tsx
@@ -1,21 +1,13 @@
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { useAuthorizedCreateContent } from "./useAuthorizedCreateContent"
 
-type CreateButtonProperties = {
-  namespace: string
-  name: string
-}
-
-const useCreateContentFromEditor = ({
-  namespace,
-  name,
-}: CreateButtonProperties) => {
+const useCreateContentFromEditor = () => {
   const [editor] = useLexicalComposerContext()
-  const authorizedCreateContent = useAuthorizedCreateContent(namespace, name)
+  const authorizedCreateContent = useAuthorizedCreateContent()
 
-  return async () => {
+  return async (namespace: string, slug: string) => {
     const contentValue = JSON.stringify(editor.getEditorState().toJSON())
-    return authorizedCreateContent(contentValue)
+    return authorizedCreateContent(contentValue, namespace, slug)
   }
 }
 
