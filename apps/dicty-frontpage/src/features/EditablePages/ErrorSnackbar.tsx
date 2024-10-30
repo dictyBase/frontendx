38956f80938244debbe9572168d6136721761b6f
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-30 09:59:43 -0500

    feat(ErrorSnackbar.tsx): add ErrorSnackbar component for displaying error messages in a Snackbar
    The ErrorSnackbar component is added to the project to provide a user-friendly way of displaying error messages in a Snackbar format. This component includes the necessary structure and styling to show error messages with a close button for user interaction.

---
+++ apps/dicty-frontpage/src/features/EditablePages/ErrorSnackbar.tsx
@@ -0,0 +1,44 @@
+import { Snackbar, IconButton, makeStyles, Typography } from "@material-ui/core"
+import { Alert, AlertTitle } from "@material-ui/lab"
+import CloseIcon from "@material-ui/icons/Close"
+
+type ErrorSnackbarProperties = {
+  open: boolean
+  message: string
+  handleClose: () => void
+}
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: "20rem",
+  },
+})
+
+const ErrorSnackbar = ({
+  open,
+  message,
+  handleClose,
+}: ErrorSnackbarProperties) => {
+  const { root } = useStyles()
+  return (
+    <Snackbar
+      open={open}
+      onClose={handleClose}
+      autoHideDuration={5000}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      action={
+        <IconButton size="small" color="inherit" onClick={handleClose}>
+          <CloseIcon fontSize="small" />
+        </IconButton>
+      }>
+      <Alert severity="error" onClose={handleClose} className={root}>
+        <AlertTitle>
+          <Typography variant="h2"> Error </Typography>
+        </AlertTitle>
+        <Typography variant="h3">{message}</Typography>
+      </Alert>
+    </Snackbar>
+  )
+}
+
+export { ErrorSnackbar }
