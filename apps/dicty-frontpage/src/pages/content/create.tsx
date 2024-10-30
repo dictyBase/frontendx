4fa86e0e81c369cb5d0afaff51155909049be211
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-30 10:01:39 -0500

    refactor(create.tsx): remove unused useNavigate import and simplify CreateContentView component
    The useNavigate import was not being used in the CreateContentView component, so it was removed to clean up the code. The CreateContentView component was also simplified by removing unnecessary curly braces and return statement, making the code more concise and easier to read.

--- apps/dicty-frontpage/src/pages/content/create.tsx
+++ apps/dicty-frontpage/src/pages/content/create.tsx
@@ -1,18 +1,13 @@
-import { useNavigate } from "react-router-dom"
 import { Container } from "@material-ui/core"
 import { Editor } from "@dictybase/editor"
 import { ACCESS } from "@dictybase/auth"
 import { CreateContentForm } from "../../features/EditablePages/CreateContentForm"
 
-const CreateContentView = () => {
-  const navigate = useNavigate()
-
-  return (
-    <Container>
-      <Editor editable toolbar={<CreateContentForm />} />
-    </Container>
-  )
-}
+const CreateContentView = () => (
+  <Container>
+    <Editor editable toolbar={<CreateContentForm />} />
+  </Container>
+)
 
 // eslint-disable-next-line import/no-default-export
 export default CreateContentView
