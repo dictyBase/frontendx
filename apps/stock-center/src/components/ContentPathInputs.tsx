a114f568edaeee1c70158ce8241a8a2763a1a4f4
Kevin Tun <kevin.tun@northwestern.edu>
2024-10-30 10:28:16 -0500

    feat(components): add new components ContentPathInputs, CreateContentForm, CreateContentFormButtons, SectionSelect
    The new components ContentPathInputs, CreateContentForm, CreateContentFormButtons, and SectionSelect have been added to the project. ContentPathInputs provides input fields for content paths, CreateContentForm handles the creation of content, CreateContentFormButtons contains buttons for CreateContentForm, and SectionSelect allows users to select a section for the content. These components enhance the functionality and user experience of the application by providing structured and interactive elements for creating and managing content.

---
+++ apps/stock-center/src/components/ContentPathInputs.tsx
@@ -0,0 +1,48 @@
+import { TextField, Grid } from "@material-ui/core"
+import { useFormContext } from "react-hook-form"
+import { isEmpty as SisEmpty } from "fp-ts/string"
+import { MonoidAny } from "fp-ts/boolean"
+import { SectionSelect } from "./SectionSelect"
+
+const ContentPathInputs = () => {
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext()
+  const sectionValue = watch("section")
+  const nameValue = watch("name")
+  const subnameDisabled = MonoidAny.concat(
+    SisEmpty(sectionValue),
+    SisEmpty(nameValue),
+  )
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <SectionSelect />
+      </Grid>
+      <Grid item>
+        <TextField
+          {...register("name")}
+          error={!!errors.name}
+          helperText={errors.name?.message}
+          label="* Name"
+          name="name"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          {...register("subname")}
+          error={!!errors.subname}
+          label="Subname"
+          name="subname"
+          variant="outlined"
+          disabled={subnameDisabled}
+        />
+      </Grid>
+    </Grid>
+  )
+}
+
+export { ContentPathInputs }
