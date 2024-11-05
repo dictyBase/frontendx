f556757e5e62e5bfdfa002c0a15184f7ae83ac36
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-05 16:55:05 -0600

    feat: add mock data for plasmids
    Mock Plasmid data has been added to the @dictybase/ui-dsc package to
    enable mocking with msw. This allows for development of frontend
    features of the plasmid catalog while the backend is still under
    development.

---
+++ packages/ui-dsc/src/mocks/mockDepositor.ts
@@ -0,0 +1,11 @@
+const mockDepositor = {
+  id: "878898",
+  first_name: "Adrian",
+  last_name: "Harwood",
+  email: "test@testemail.com",
+  created_at: 123_456,
+  updated_at: 987_654,
+  is_active: true,
+}
+
+export { mockDepositor }
