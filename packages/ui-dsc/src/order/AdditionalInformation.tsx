c49c646272dc468d7079ab2a351145eb33fec126
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-12 10:39:21 -0600

    refactor(AdditionalInformation.tsx): refactor AdditionalInformation component to handle error state and display helper text
    The AdditionalInformation component has been refactored to handle the error state and display a helper text when there is an error. This change improves the user experience by providing more informative feedback when there are validation errors in the form.

--- packages/ui-dsc/src/order/AdditionalInformation.tsx
+++ packages/ui-dsc/src/order/AdditionalInformation.tsx
@@ -1,12 +1,24 @@
 import TextField from "@material-ui/core/TextField"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
+import { pipe } from "fp-ts/function"
+import {
+  flatMap as OflatMap,
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
 import { useFormContext } from "react-hook-form"
 import { PanelWrapper } from "./PanelWrapper"
 
 const AdditionalInformation = () => {
-  const { register } = useFormContext()
-
+  const { register, getFieldState } = useFormContext()
+  const { error, invalid } = getFieldState("additionalInformation")
+  const helperText = pipe(
+    error,
+    OfromNullable,
+    OflatMap(({ message }) => OfromNullable(message)),
+    OgetOrElse(() => ""),
+  )
   return (
     <PanelWrapper title="Additional Information">
       <Box mt={1} mb={2} p={2}>
@@ -18,6 +30,8 @@ const AdditionalInformation = () => {
           variant="outlined"
           minRows={5}
           maxRows={5}
+          error={invalid}
+          helperText={helperText}
           {...register("additionalInformation")}
         />
       </Box>
