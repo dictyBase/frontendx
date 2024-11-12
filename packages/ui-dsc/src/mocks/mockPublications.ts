5709e58ebde4c2fec229bceb5ec47b61929c3775
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-11 23:30:48 -0600

    feat(mockPublications): add mock data for publications in the UI to simulate API responses
    The mockPublications array is added to provide sample data for publications in the UI. This data will be used to simulate API responses during development and testing, ensuring that the UI components can display and interact with publication data effectively.

---
+++ packages/ui-dsc/src/mocks/mockPublications.ts
@@ -0,0 +1,82 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+const mockPublications = [
+  {
+    id: "1",
+    doi: "/publication/31108912",
+    title:
+      "Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.",
+    abstract: "",
+    journal: "Cells 8",
+    pub_date: "2011-10-10T14:48:00",
+    pages: "",
+    issue: "",
+    volume: "",
+    authors: [
+      { last_name: "Tanaka" },
+      { last_name: "Jahan" },
+      { last_name: "Kondo" },
+      { last_name: "Nakano & Yumura" },
+    ],
+  },
+  {
+    id: "2",
+    doi: "/publication/31067156",
+    title:
+      "Force balances between interphase centrosomes, as revealed by laser ablation.",
+    abstract: "",
+    journal: "Mol. Biol. Cell mbcE19010034",
+    pub_date: "2011-10-10T14:48:00",
+    pages: "",
+    issue: "",
+    volume: "",
+    authors: [
+      { last_name: "Odell" },
+      { last_name: "Sikirzhytski" },
+      { last_name: "Tikhonenko" },
+      { last_name: "Cobani" },
+      { last_name: "Khodjakov & Koonce" },
+    ],
+  },
+  {
+    id: "3",
+    doi: "/publication/31063135",
+    title:
+      "A terpene synthase-cytochrome P450 cluster in Dictyostelium discoideum produces a novel trisnorsesquiterpene.",
+    abstract: "",
+    journal: "Elife 8",
+    pub_date: "2011-10-10T14:48:00",
+    pages: "",
+    issue: "",
+    volume: "",
+    authors: [
+      { last_name: "Chen" },
+      { last_name: "Luck" },
+      { last_name: "Rabe" },
+      { last_name: "Dinh" },
+      { last_name: "Shaulsky" },
+      { last_name: "Nelson" },
+      { last_name: "Gershenzon" },
+      { last_name: "Dickschat" },
+      { last_name: "Kollner & Chen" },
+    ],
+  },
+  {
+    id: "4",
+    doi: "/publication/31051160",
+    title:
+      "Cyclic AMP induction of Dictyostelium prespore gene expression requires autophagy.",
+    abstract: "",
+    journal: "Dev. Biol.",
+    pub_date: "2011-10-10T14:48:00",
+    pages: "",
+    issue: "",
+    volume: "",
+    authors: [
+      {
+        last_name: "Yamada & Schaap",
+      },
+    ],
+  },
+]
+
+export { mockPublications }
