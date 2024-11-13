55ccdd175a4cb0c19d6db9d62217ae989de8a542
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-13 13:59:52 -0600

    refactor: Improve parseFormattedStringToDomElements utility function

--- packages/ui-dsc/src/utils/parseFormattedStringToDomElements.ts
+++ packages/ui-dsc/src/utils/parseFormattedStringToDomElements.ts
@@ -1,37 +1,71 @@
-import {
-  createElement,
-  DOMElement,
-  DOMAttributes,
-  DetailedReactHTMLElement,
-  HTMLAttributes,
-} from "react"
+import { createElement, DOMElement, DOMAttributes } from "react"
 import { pipe } from "fp-ts/function"
 import { split as Ssplit, Monoid as SMonoid } from "fp-ts/string"
 import {
+  makeBy as AmakeBy,
   map as Amap,
   compact as Acompact,
   intercalate as Aintercalate,
 } from "fp-ts/Array"
 import { fromNullable as OfromNullable } from "fp-ts/Option"
 import { map as RNEAmap } from "fp-ts/ReadonlyNonEmptyArray"
+import { match } from "ts-pattern"
 
 const supportedTags = ["i", "b", "sup", "sub", "h1", "h2", "h3", "h4"]
+
+const isEven = (n: number) => n % 2 === 0
+const isOdd = (n: number) => n % 2 !== 0
 
 const parseIrregularTags = (s: string) =>
   pipe(s.replaceAll("&lt;", "<"), (next) => next.replaceAll("&gt;", ">"))
 
+// Assumes that both trailing and leading arrays do not contain any nullish values
+const interleaf = <A, B>(leading: readonly A[], trailing: readonly B[]) => {
+  const totalLength = leading.length + trailing.length
+  let cursorL = 0
+  let cursorT = 0
+
+  return AmakeBy<A | B | undefined>(totalLength, (index) =>
+    match(index)
+      .when(isEven, () => {
+        if (leading[cursorL]) {
+          const next = leading[cursorL]
+          cursorL += 1
+          return next
+        }
+        const next = trailing[cursorT]
+        cursorT += 1
+        return next
+      })
+      .when(isOdd, () => {
+        if (trailing[cursorT]) {
+          const next = trailing[cursorT]
+          cursorT += 1
+          return next
+        }
+        const next = leading[cursorL]
+        cursorL += 1
+        return next
+      })
+      .otherwise(() => undefined),
+  )
+}
+
+/**
+ * Parses a string containing formatting tags and returns an array of formatted and unformatted DOM elements.
+ */
 const parseFormattedStringToDomElements = (
   s: string,
 ): DOMElement<DOMAttributes<Element>, Element>[] => {
   const normalizedString = parseIrregularTags(s)
-  // Regular Expression used to capture the text content in format tags.
+  // Regular Expression used to capture the format tag names and the text content inside those format tags.
   const formatTagRegex = pipe(
     supportedTags,
     Amap((tag) => `<(${tag})>(.+?)<\\/${tag}>`),
     Aintercalate(SMonoid)("|"),
     (exp) => new RegExp(exp, "g"),
   )
-  // Regular Expression used to capture the text content in format tags.
+  // Regular Expression used to capture all unformatted text.
   const splitRegex = pipe(
     supportedTags,
     Amap((tag) => `<${tag}>.+?<\\/${tag}>`),
@@ -57,24 +91,11 @@ const parseFormattedStringToDomElements = (
     // eslint-disable-next-line unicorn/no-null
     RNEAmap((text) => createElement("span", null, text)),
   )
-  const final = []
-  // eslint-disable-next-line unicorn/no-for-loop
-  for (let index = 0; index < unformattedTextElements.length; index += 1) {
-    final.push(
-      unformattedTextElements[index] as DetailedReactHTMLElement<
-        HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >,
-    )
-    if (formattedTextElements[index])
-      final.push(
-        formattedTextElements[index] as DOMElement<
-          DOMAttributes<Element>,
-          Element
-        >,
-      )
-  }
-  return final
+  return pipe(
+    interleaf(unformattedTextElements, formattedTextElements),
+    Amap(OfromNullable),
+    Acompact,
+  )
 }
 
 export { parseFormattedStringToDomElements }
