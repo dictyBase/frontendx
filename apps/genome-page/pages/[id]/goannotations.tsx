55b5c3eb44b614806d057fc473591664e58aa407
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-14 12:08:02 -0600

    chore: remove unused GraphQLErrorPage test and component files
    The removed files were no longer needed and were causing clutter in the project directory. This cleanup improves code maintainability and reduces unnecessary files in the repository.

--- apps/genome-page/pages/[id]/goannotations.tsx
+++ apps/genome-page/pages/[id]/goannotations.tsx
@@ -1,5 +1,5 @@
 import { OntologyContainer } from "components/features/Ontology/OntologyContainer"
-import { GraphQLErrorPage } from "components/errors/GraphQLErrorPage"
+import { GraphQLErrorPage } from "@dictybase/ui-common"
 import { OntologyLoader } from "components/features/Ontology/OntologyLoader"
 import { useRouter } from "next/router"
 import { useGeneOntologyAnnotationQuery } from "dicty-graphql-schema"
