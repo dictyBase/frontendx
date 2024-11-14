7e0ba1f41b57069d21e3e1e82be5ec03b0938e7f
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-14 00:54:38 -0600

    style(vitest.config.ts): add testTimeout option to defineConfig for better test control
    The testTimeout option is added to the defineConfig function to set a timeout of 30 seconds for tests. This change improves test control and ensures that tests do not run indefinitely, helping to manage test execution time effectively.

--- packages/ui-dsc/vitest.config.ts
+++ packages/ui-dsc/vitest.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./setup.ts",
     mockReset: true,
+    testTimeout: 30_000,
   },
 })
