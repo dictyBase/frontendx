46ed7d78d9fe5e83d8e79e5d988a96c1d16d758c
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-13 23:19:13 -0600

    feat(ui-common): add GraphQLErrorPage component to handle and display GraphQL errors
    The GraphQLErrorPage component is added to handle and display errors that occur when issuing a GraphQL query or mutation. It provides a way to differentiate between different types of errors such as network errors, GraphQL errors, and other errors, and display the appropriate error component based on the error code. This enhances the user experience by providing more specific error messages for different scenarios.

---
+++ packages/ui-common/src/Error/GraphQLErrorPage.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable unicorn/filename-case */
+import { ApolloError } from "@apollo/client"
+import { pipe } from "fp-ts/function"
+import { head as RAhead } from "fp-ts/ReadonlyArray"
+import {
+  map as Omap,
+  flatMap as OflatMap,
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { match, P } from "ts-pattern"
+import { ServerError } from "./ServerError"
+import { NotFoundError } from "./NotFoundError"
+import { OtherError } from "./OtherError"
+
+type GraphQlErrorPageProperties = {
+  /** GraphQL error object */
+  error: ApolloError
+}
+
+/**
+ * Displays any errors found when issuing a GraphQL query or mutation.
+ * Returns one of the other error components based on the error code.
+ */
+
+const GraphQLErrorPage = ({ error }: GraphQlErrorPageProperties) =>
+  match(error)
+    .with({ networkError: P.not(P.nullish) }, () => <ServerError />)
+    .with({ graphQLErrors: P.select(P.not(P.nullish)) }, (errors) => {
+      const primaryError = pipe(
+        errors,
+        RAhead,
+        OflatMap(({ extensions }) =>
+          pipe(
+            extensions,
+            OfromNullable,
+            // eslint-disable-next-line dot-notation
+            Omap((extension) => extension["code"] as string),
+          ),
+        ),
+        OgetOrElse(() => ""),
+      )
+      return match(primaryError)
+        .with("Unavailable", () => <ServerError />)
+        .with("NotFound", () => <NotFoundError />)
+        .otherwise(() => <OtherError />)
+    })
+    .otherwise(() => <OtherError />)
+
+export { GraphQLErrorPage }
