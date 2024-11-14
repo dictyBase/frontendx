0625fcb8ac7c8545dd1c1837ca0425f7c7f012da
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-13 22:59:13 -0600

    refactor(AddPage.tsx): remove unused code related to AddPage component
    The AddPage.tsx file and its related imports and exports have been removed as they are no longer needed in the project. This cleanup helps to reduce clutter and improve code maintainability.

--- apps/dicty-frontpage/src/pages/about/addpage.tsx
+++
@@ -1,7 +0,0 @@
-import { ACCESS } from "@dictybase/auth"
-import { AddPage } from "../../features/EditablePages/AddPage"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default AddPage
-export const access = ACCESS.private
-export const roles = ["content-admin"]
