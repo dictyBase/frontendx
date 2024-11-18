734e55243b6acd41229be34131a49146c75ba94e
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-18 07:54:40 -0600

    feat(genome-page): implement pattern matching for handling different states in GenomePageWrapper component
    The code now uses pattern matching from the ts-pattern library to handle different states (loading, error, data) returned by the useGeneSummaryQuery hook. This improves readability and maintainability of the component by clearly defining the behavior for each state.

--- apps/genome-page/pages/[id]/index.tsx
+++ apps/genome-page/pages/[id]/index.tsx
@@ -3,6 +3,7 @@ import { Loader } from "components/Loader"
 import { GraphQLErrorPage } from "@dictybase/ui-common"
 import { useRouter } from "next/router"
 import { useGeneSummaryQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
 
 /**
  * Renders the gene page given a gene id
@@ -10,20 +11,24 @@ import { useGeneSummaryQuery } from "dicty-graphql-schema"
 const GenomePageWrapper = () => {
   const { query } = useRouter()
   const gene = query.id as string
-
-  const { loading, error, data } = useGeneSummaryQuery({
+  const result = useGeneSummaryQuery({
     variables: {
       gene,
     },
     fetchPolicy: "cache-and-network",
   })
-  return (
-    <>
-      {loading ? <Loader /> : <></>}
-      {error ? <GraphQLErrorPage error={error} /> : <></>}
-      {data ? <SummaryContainer geneSummary={data} /> : <></>}
-    </>
-  )
+  return match(result)
+    .with({ loading: true }, () => <Loader />)
+    .with({ error: P.select(P.not(undefined)) }, (error) => (
+      <GraphQLErrorPage error={error} />
+    ))
+    .with(
+      {
+        data: P.select(P.not(P.nullish)),
+      },
+      (data) => <SummaryContainer geneSummary={data} />,
+    )
+    .otherwise(() => <> This message should not appear. </>)
 }
 
 // eslint-disable-next-line import/no-default-export
