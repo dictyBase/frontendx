55721e7dc150c145770706d52f44268a3ffcfa95
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-19 00:27:11 -0600

    feat(StatusReport.tsx): add support for displaying different colors based on status in the statusIndicator style
    The changes introduce the ability to display different colors in the statusIndicator style based on the status value provided. This enhancement improves the visual representation of the status of the component, making it more informative for users.

--- packages/ui-frontpage/src/StatusReport.tsx
+++ packages/ui-frontpage/src/StatusReport.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom"
+import { match } from "ts-pattern"
 import { makeStyles, Paper, Grid, Typography, Divider } from "@material-ui/core"
 import RadioButtonCheckedIcon from "@material-ui/icons/RadioButtonChecked"
-import { UptimeProperties } from "./types"
+import { UptimeProperties, Status } from "./types"
 
 const useStyles = makeStyles({
   root: {
@@ -12,26 +14,35 @@ const useStyles = makeStyles({
   },
   statusIndicator: {
     // filter: "blur(1px)",
-    color: "green",
+    color: ({ status }: { status: Status }) =>
+      match(status)
+        .with(Status.UP, () => "green")
+        .with(Status.DOWN, () => "orange")
+        .exhaustive(),
+  },
+  text: {
+    fontFamily: "'Nimbus Mono PS', 'Courier New', monospace",
   },
 })
 
 const StatusReport = ({ name, url, status }: UptimeProperties) => {
-  const { root, statusGrid, statusIndicator } = useStyles()
+  const { text, root, statusGrid, statusIndicator } = useStyles({ status })
   return (
-    <Paper className={root}>
-      <Grid container spacing={1} alignContent="center">
-        <Grid item className={statusGrid}>
-          <RadioButtonCheckedIcon className={statusIndicator} />
+    <Link to={url}>
+      <Paper className={root}>
+        <Grid container spacing={1} alignContent="center">
+          <Grid item className={statusGrid}>
+            <RadioButtonCheckedIcon className={statusIndicator} />
+          </Grid>
+          <Grid item>
+            <Divider orientation="vertical" />
+          </Grid>
+          <Grid item>
+            <Typography className={text}>{name}</Typography>
+          </Grid>
         </Grid>
-        <Grid item >
-          <Divider orientation="vertical"/>
-        </Grid>
-        <Grid item>
-          <Typography>{name}</Typography>
-        </Grid>
-      </Grid>
-    </Paper>
+      </Paper>
+    </Link>
   )
 }
 
