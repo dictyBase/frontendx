1d558b1b749ea8291c9396c59e9ed4dbc7b66913
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-19 00:27:24 -0600

    refactor(StatusReportDisplay.tsx): add makeStyles hook for styling and improve readability of the component
    The changes introduce the makeStyles hook from Material-UI to handle component styling. This improves the readability of the code and separates the styling concerns from the component logic. The refactored component now has a more structured layout and uses the resolved site names for display.

--- packages/ui-frontpage/src/StatusReportDisplay.tsx
+++ packages/ui-frontpage/src/StatusReportDisplay.tsx
@@ -1,36 +1,62 @@
-import { Grid } from "@material-ui/core"
+import { makeStyles, Typography, Grid } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
+import {
+  replace as Sreplace,
+  Monoid as SMonoid,
+  toLowerCase as StoLowerCase,
+} from "fp-ts/string"
 import { map as Amap } from "fp-ts/Array"
-import { toArray as RtoArray } from "fp-ts/Record"
-import { Status, UptimeProperties } from "./types"
+import { getOrElse as OgetOrElse } from "fp-ts/Option"
+import { lookup as Rlookup } from "fp-ts/Record"
+import { UptimeProperties } from "./types"
 import { StatusReport } from "./StatusReport"
 
 const SiteNames = {
   DCR: "Dicty CR",
   "DCR dev": "Dev Site",
-  "DCR news pages": "Dev Site",
+  "DCR news pages": "DCR News",
   "DSC dev": "Stock Center",
 }
 
 type StatusReportDisplayProperties = {
   summaries: Array<UptimeProperties>
 }
-/**
- * DATA UNDEFINED -> UNAVAILABLE, GREY
- * DATA, STATUS UP -> GREEN
- * DATA, STATUS DOWN -> ORANGE
- */
-const StatusReportDisplay = ({ summaries }: StatusReportDisplayProperties) => (
-  <Grid container spacing={2}>
-    {pipe(
-      summaries,
-      Amap(({ name, url, status }) => (
-        <Grid item>
-          <StatusReport name={name} url={url} status={status} />
-        </Grid>
-      )),
-    )}
-  </Grid>
-)
+
+const useStyles = makeStyles({
+  text: {
+    fontFamily: "'Nimbus Mono PS', 'Courier New', monospace",
+  },
+})
+
+const StatusReportDisplay = ({ summaries }: StatusReportDisplayProperties) => {
+  const { text } = useStyles()
+  return (
+    <Grid container spacing={2}>
+      <Grid item>
+        <Typography className={text}>Live Site Status</Typography>
+      </Grid>
+      {pipe(
+        summaries,
+        Amap(({ name, status }) => {
+          const resolvedName = pipe(
+            SiteNames,
+            Rlookup(name),
+            OgetOrElse(() => name),
+          )
+          const nameSegment = pipe(name, StoLowerCase, Sreplace(/\s/g, "-"))
+          const url = SMonoid.concat(
+            "https://status.dictybase.dev/history/",
+            nameSegment,
+          )
+          return (
+            <Grid item>
+              <StatusReport name={resolvedName} url={url} status={status} />
+            </Grid>
+          )
+        }),
+      )}
+    </Grid>
+  )
+}
 
 export { StatusReportDisplay }
