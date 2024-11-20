fac1950295505883152cba4b32e40658fc7b842a
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-20 10:20:41 -0600

    feat(StatusReportContainer.tsx): add SiteStatusUnavailable component for displaying site status when data is unavailable
    The SiteStatusUnavailable component is added to the StatusReportContainer to provide a visual representation when the data for the site status is unavailable. This improves the user experience by clearly indicating the status in such cases.

--- packages/ui-frontpage/src/StatusReportContainer.tsx
+++ packages/ui-frontpage/src/StatusReportContainer.tsx
@@ -2,13 +2,14 @@ import { pipe } from "fp-ts/function"
 import { match as Omatch } from "fp-ts/Option"
 import { useDictyStatus } from "./hooks/useDictyStatus"
 import { StatusPopover } from "./StatusPopover"
+import { SiteStatusUnavailable } from "./SiteStatusUnavailable"
 
 const StatusReportContainer = () => {
   const data = useDictyStatus()
   return pipe(
     data,
     Omatch(
-      () => <></>,
+      () => <SiteStatusUnavailable />,
       (summaries) => <StatusPopover summaries={summaries} />,
     ),
   )
