29521c900d9714f04c969a79e3d8c572653c3d03
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-22 11:23:33 -0600

    feat(publication): add pattern matching with ts-pattern library for better handling of usePublicationQuery result
    The code now uses the ts-pattern library to perform pattern matching on the result of the usePublicationQuery hook. This change improves the readability and maintainability of the code by providing a more structured way to handle different states (loading, error, data) returned by the query.

--- apps/publication/src/pages/[id]/index.tsx
+++ apps/publication/src/pages/[id]/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useRouter } from "next/router"
 import { Publication, usePublicationQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
 import { PublicationLoader } from "../../components/PublicationLoader"
 import { ErrorPage } from "../../components/errors/ErrorPage"
 import { PublicationPage } from "../../components/PublicationPage"
@@ -12,19 +13,22 @@ const PublicationPageWrapper = () => {
   const { query } = useRouter()
   const id = query.id as string
 
-  const { loading, error, data } = usePublicationQuery({
+  const result = usePublicationQuery({
     variables: { id },
   })
 
-  return (
-    <>
-      {loading && <PublicationLoader />}
-      {error && <ErrorPage />}
-      {data && (
+  return match(result)
+    .with(
+      {
+        data: P.select(P.not(P.nullish)),
+      },
+      (data) => (
         <PublicationPage publication={data.publication as Publication} />
-      )}
-    </>
-  )
+      ),
+    )
+    .with({ loading: true }, () => <PublicationLoader />)
+    .with({ error: P.not(P.nullish) }, () => <ErrorPage />)
+    .otherwise(() => <> This message should not appear. </>)
 }
 
 // eslint-disable-next-line import/no-default-export
