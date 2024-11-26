6e33bcd402267ef615968f0621b5110b5dfeefe1
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-26 16:34:39 -0600

    refactor(JournalData.tsx): clean up code by removing unused imports and variables, and improve code readability by adding a function to format the issue string
    The changes involve removing unused imports and variables, as well as adding a function to format the issue string in the JournalData component. This refactoring enhances code readability and maintainability.

--- apps/publication/src/components/version_a/JournalData.tsx
+++ apps/publication/src/components/version_a/JournalData.tsx
@@ -1,20 +1,38 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
 import Typography from "@material-ui/core/Typography"
+import { pipe } from "fp-ts/function"
+import { isEmpty as SisEmpty } from "fp-ts/string"
+import {
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+  Option,
+} from "fp-ts/Option"
+import { match as Bmatch } from "fp-ts/boolean"
 import { makeStyles, Theme } from "@material-ui/core/styles"
 import { addDays, format, parseISO } from "date-fns"
 import { Publication } from "dicty-graphql-schema"
 import { JournalDataItem } from "./JournalDataItem"
 
+const formatIssue = (issue: string | null | undefined) =>
+  pipe(
+    issue,
+    OfromNullable,
+    OgetOrElse(() => ""),
+    SisEmpty,
+    Bmatch(
+      () => `(${issue})`,
+      () => "",
+    ),
+  )
+
 const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    fontStyle: "italic",
-  },
   text: {
     fontFamily: "'Inter Tight Variable', sans-serif",
   },
   journal: {
     fontWeight: 600,
+    fontStyle: "italic",
     paddingRight: theme.spacing(0.5),
     fontFamily: "'Inter Tight Variable', sans-serif",
   },
@@ -30,8 +48,9 @@ interface JournalDataProperties {
 
 const JournalData = ({ data }: JournalDataProperties) => {
   const { id, doi, pub_date, journal, pages, issue, volume } = data
-
   const classes = useStyles()
+  const formattedIssue = formatIssue(issue)
+  console.log(issue, formattedIssue)
   const pubmedURL = `https://pubmed.gov/${id}`
   const doiURL = `https://doi.org/${doi}`
   // convert ISO 8601 string to Date format
@@ -41,7 +60,7 @@ const JournalData = ({ data }: JournalDataProperties) => {
   const date = format(day, "PPP")
   return (
     <Box mt={2}>
-      <Box pb={1} className={classes.root}>
+      <Box pb={1}>
         <Typography variant="h3" component="span" className={classes.text}>
           {`Published on `}
         </Typography>
@@ -52,7 +71,8 @@ const JournalData = ({ data }: JournalDataProperties) => {
           {journal},
         </Typography>
         <Typography variant="h3" component="span" className={classes.text}>
-          {volume}({issue}):{pages}
+          <i>{volume}</i>
+          {`${formattedIssue}, ${pages}`}
         </Typography>
       </Box>
       <Box>
