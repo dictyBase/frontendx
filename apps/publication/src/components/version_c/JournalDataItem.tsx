bfb71d1c4429d14d80846f6147c4cc13c29b7671
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-27 13:20:09 -0600

    feat(publication): add new components Abstract, Authors, Interactables, JournalData,
    These new components are added to enhance the display and functionality of the
    publication page. The components handle rendering the abstract, authors, interactables,
    journal data, journal data items, publication body, publication loader, publication page,
    and publication sidebar. They improve the overall user experience and provide a more
    comprehensive view of publication details.

---
+++ apps/publication/src/components/version_c/JournalDataItem.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import { Maybe } from "dicty-graphql-schema"
+
+type Properties = {
+  title: string
+  url: string
+  content?: Maybe<string>
+}
+
+/**
+ * JournalDataItem displays an item of journal data (i.e. PMID: 123456)
+ */
+
+const JournalDataItem = ({ title, url, content }: Properties) => {
+  if (!content) return <></>
+
+  return (
+    <Box component="span" pr={3}>
+      <Typography component="span">{title}: </Typography>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {content}
+      </a>
+    </Box>
+  )
+}
+
+export { JournalDataItem }
---
+++ apps/publication/src/components/version_c/PublicationPage.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { Box, Grid, Container } from "@material-ui/core"
+import { Publication } from "dicty-graphql-schema"
+import Head from "next/head"
+import { PublicationSidebar } from "./PublicationSidebar"
+import { PublicationBody } from "./PublicationBody"
+import { useStyles } from "./useStyles"
+
+interface PublicationPageProperties {
+  publication: Publication
+}
+
+const PublicationPage = ({ publication }: PublicationPageProperties) => {
+  const classes = useStyles()
+  const { title } = publication
+  return (
+    <Box className={classes.background}>
+      <Container className={classes.foreground}>
+        <Head>
+          <title>dictyBase Literature - {title}</title>
+          <meta
+            name="description"
+            content={`dictyBase literature page for title ${title}`}
+          />
+        </Head>
+        <PublicationBody publication={publication} />
+      </Container>
+    </Box>
+  )
+}
+
+export { PublicationPage }
