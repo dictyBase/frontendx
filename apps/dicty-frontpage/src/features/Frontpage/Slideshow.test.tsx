2b196f91d88c63b18404c6ea17d3dcaa350bae94
Kevin Tun <kevin.tun@northwestern.edu>
2024-11-27 18:54:45 -0600

    style(Slideshow.test.tsx): fix typo in alt text for alpha-tubulin in Slideshow component test
    The alt text for the alpha-tubulin image in the Slideshow component test has been corrected from "apha-tubulin" to "alpha-tubulin" for accuracy and consistency.

--- apps/dicty-frontpage/src/features/Frontpage/Slideshow.test.tsx
+++ apps/dicty-frontpage/src/features/Frontpage/Slideshow.test.tsx
@@ -9,7 +9,7 @@ describe("feature/Frontpage/Slideshow", () => {
     expect(screen.getAllByAltText("D. discoideum slug")[0]).toBeInTheDocument()
     expect(
       screen.getAllByAltText(
-        "Triple stained and fixed Dictyostelium cells: apha-tubulin (green), centrosome (red, appears yellow due to colocalization with tubulin), nuclei (blue)",
+        "Triple stained and fixed Dictyostelium cells: alpha-tubulin (green), centrosome (red, appears yellow due to colocalization with tubulin), nuclei (blue)",
       )[0],
     ).toBeInTheDocument()
   })
