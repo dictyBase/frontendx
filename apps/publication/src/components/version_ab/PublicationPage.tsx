f9400e52f05d99c2507be962a542085c99095dbf
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-03 11:35:55 -0600

    feat(publication): add new components for displaying publication information
    The changes introduce new components for displaying publication information in a more structured and organized manner. These components include Abstract, Authors, JournalData, JournalDataItem, PublicationBody, PublicationHeader, PublicationLoader, PublicationPage, and PublicationSidebar. These components enhance the readability and user experience of the publication pages by providing clear and concise information about each publication.

---
+++ apps/publication/src/components/version_ab/PublicationPage.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { Grid, Container, Box } from "@material-ui/core"
+import { Publication } from "dicty-graphql-schema"
+import Head from "next/head"
+import { PublicationSidebar } from "./PublicationSidebar"
+import { PublicationBody } from "./PublicationBody"
+import { makeStyles } from "@material-ui/core/styles"
+import { blueGrey, grey, teal, cyan } from "@material-ui/core/colors"
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundColor: grey[100],
+  },
+  foreground: {
+    backgroundColor: "white",
+    boxShadow: `2px 2px 7px ${grey[300]}, -2px 2px 7px ${grey[300]}`,
+  },
+  sidebar: {
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center",
+    },
+  },
+}))
+
+export { useStyles }
+
+interface PublicationPageProperties {
+  publication: Publication
+}
+
+const PublicationPage = ({ publication }: PublicationPageProperties) => {
+  const classes = useStyles()
+  const { title, doi } = publication
+  return (
+    <Box className={classes.background}>
+      <Container disableGutters className={classes.foreground}>
+        <Grid container>
+          <Head>
+            <title>dictyBase Literature - {title}</title>
+            <meta
+              name="description"
+              content={`dictyBase literature page for title ${title}`}
+            />
+          </Head>
+
+          <Grid item xs={12} sm={12} md={1} className={classes.sidebar}>
+            <PublicationSidebar doi={doi} title={title} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={10}>
+            <PublicationBody publication={publication} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  )
+}
+
+export { PublicationPage }
---
+++ apps/publication/src/components/version_ab/PublicationSidebar.tsx
@@ -0,0 +1,88 @@
+import React from "react"
+import { makeStyles, Theme, Grid } from "@material-ui/core"
+import { blue, grey, orange } from "@material-ui/core/colors"
+import CallMadeIcon from "@material-ui/icons/CallMade"
+import EmailIcon from "@material-ui/icons/Email"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  link: {
+    textDecoration: "none",
+    color: blue[900],
+    "&:hover": {
+      color: orange[900]
+    },
+  },
+  section: {
+    height: "100%",
+    backgroundColor: blue[50],
+    position: "sticky",
+    top: 40,
+    paddingTop: theme.spacing(3),
+    boxShadow: `${grey[500]} 1px 0px 1px`,
+    rowGap: theme.spacing(3),
+    flexDirection: "column",
+    [theme.breakpoints.down("sm")]: {
+      boxShadow: `${grey[500]} 0px 1px 1px`,
+      alignItems: "baseline",
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(1),
+      flexDirection: "row",
+      columnGap: theme.spacing(3),
+    },
+  },
+  icon: {
+    marginLeft: theme.spacing(1),
+  },
+}))
+
+type Properties = {
+  doi: string | undefined | null
+  title: string
+}
+
+/**
+ * LeftSidebar is the main component for the left sidebar on an individual publication page.
+ */
+
+const PublicationSidebar = ({ doi, title }: Properties) => {
+  const classes = useStyles()
+  const doiURL = `https://doi.org/${doi}`
+  const url = window.location.href
+
+  return (
+    <Grid container direction="column" className={classes.section}>
+      <Grid item>
+        <a
+          href={doiURL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.link}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <CallMadeIcon />
+            </Grid>
+            <Grid item>Full Text</Grid>
+          </Grid>
+        </a>
+      </Grid>
+      <Grid item>
+        <a
+          href={`mailto:?subject=${title}&body=I thought you might find this article interesting: ${url}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.link}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <EmailIcon />
+            </Grid>
+            <Grid item>Email to</Grid>
+          </Grid>
+        </a>
+      </Grid>
+    </Grid>
+  )
+}
+
+export { PublicationSidebar }
