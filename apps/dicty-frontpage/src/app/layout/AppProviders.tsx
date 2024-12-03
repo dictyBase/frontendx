347199367f67f116673ed38f8c2fdefaf3d1d002
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-03 10:45:27 -0600

    feat(ui): add support for custom Dicty theme in the UI components
    The changes introduce a custom Dicty theme for the UI components, enhancing the visual consistency and branding across the application. Additionally, new fonts (@fontsource/inter and @fontsource/poppins) have been added to the UI common package to provide more font options for styling.

--- apps/dicty-frontpage/src/app/layout/AppProviders.tsx
+++ apps/dicty-frontpage/src/app/layout/AppProviders.tsx
@@ -7,75 +7,8 @@ import {
   useApolloClientCache,
   storageType,
 } from "@dictybase/data-access"
+import { dictyTheme } from "@dictybase/ui-common"
 
-const appTheme = createTheme({
-  // use color tool for palette -- https://material.io/resources/color/
-  palette: {
-    primary: {
-      main: "#004080",
-      light: "#476ab0",
-      dark: "#001b53",
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#008080",
-      light: "#4cb0af",
-      dark: "#005354",
-    },
-    error: {
-      main: "#b2102f",
-      light: "#ea4f58",
-      dark: "#7b0008",
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-    h1: {
-      fontSize: "2.00em",
-    },
-    h2: {
-      fontSize: "1.50em",
-    },
-    h3: {
-      fontSize: "1.17em",
-    },
-    h4: {
-      fontSize: "1.00em",
-    },
-    h5: {
-      fontSize: "0.83em",
-    },
-    h6: {
-      fontSize: "0.67em",
-    },
-  },
-  overrides: {
-    MuiTab: {
-      root: {
-        textTransform: "none",
-      },
-    },
-    MuiTabs: {
-      root: {
-        backgroundColor: "#cce6ff",
-        color: "#000",
-      },
-      indicator: {
-        backgroundColor: "#858780",
-      },
-    },
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          textDecoration: "none",
-          color: "#004080",
-        },
-      },
-    },
-  },
-})
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
   const { cache, isInitializing } = useApolloClientCache({
     storage: storageType.INDEX,
@@ -90,9 +23,9 @@ const AppProviders = ({ children }: { children: React.ReactNode }) => {
   }
   return (
     <ApolloProvider client={client}>
-      <MuiThemeProvider theme={appTheme}>{children}</MuiThemeProvider>
+      <MuiThemeProvider theme={dictyTheme}>{children}</MuiThemeProvider>
     </ApolloProvider>
   )
 }
 
-export { appTheme, AppProviders }
+export { dictyTheme as appTheme, AppProviders }
---
+++ packages/ui-common/src/dictyTheme.ts
@@ -0,0 +1,80 @@
+import { createTheme } from "@material-ui/core"
+import "@fontsource/poppins"
+import "@fontsource/inter"
+
+const dictyTheme = createTheme({
+  // use color tool for palette -- https://material.io/resources/color/
+  palette: {
+    primary: {
+      main: "#004080",
+      light: "#476ab0",
+      dark: "#001b53",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#008080",
+      light: "#4cb0af",
+      dark: "#005354",
+    },
+    error: {
+      main: "#b2102f",
+      light: "#ea4f58",
+      dark: "#7b0008",
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    body1: {
+      fontFamily: "'Inter Variable', sans-serif"
+    },
+    h1: {
+      fontSize: "2.00em",
+      fontFamily: "'Poppins', sans-serif"
+    },
+    h2: {
+      fontSize: "1.50em",
+      fontFamily: "'Poppins', sans-serif"
+    },
+    h3: {
+      fontSize: "1.17em",
+      fontFamily: "'Poppins', sans-serif"
+    },
+    h4: {
+      fontSize: "1.00em",
+    },
+    h5: {
+      fontSize: "0.83em",
+    },
+    h6: {
+      fontSize: "0.67em",
+    },
+  },
+  overrides: {
+    MuiTab: {
+      root: {
+        textTransform: "none",
+      },
+    },
+    MuiTabs: {
+      root: {
+        backgroundColor: "#cce6ff",
+        color: "#000",
+      },
+      indicator: {
+        backgroundColor: "#858780",
+      },
+    },
+    MuiCssBaseline: {
+      "@global": {
+        a: {
+          textDecoration: "none",
+          color: "#004080",
+        },
+      },
+    },
+  },
+})
+
+export { dictyTheme }
