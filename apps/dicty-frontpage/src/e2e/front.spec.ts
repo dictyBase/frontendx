f501c08247e6c1da28f43e4a808a6cf45a57c9fa
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-06 11:26:17 -0600

    refactor(e2e tests): remove redundant BASE_URL constant and replace with relative path for page navigation
    The BASE_URL constant was redundant as it was not being used consistently across the e2e tests. By replacing it with a relative path for page navigation ("/"), the code becomes more concise and easier to maintain.

--- apps/dicty-frontpage/src/e2e/front.spec.ts
+++ apps/dicty-frontpage/src/e2e/front.spec.ts
@@ -1,9 +1,7 @@
-import { test, expect } from "@playwright/test"
-
-const BASE_URL = process.env.FRONTPAGE_URL || "http://localhost:3004"
+import { test, expect } from "../../testCoverage"
 
 test("has title", async ({ page }) => {
-  await page.goto(BASE_URL)
+  await page.goto("/")
 
   await expect(page).toHaveTitle(/dictyBase/)
 })
