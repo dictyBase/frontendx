73bf29780b829f04d1486bdebc2616ae588ab567
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-09 21:55:32 -0600

    build(vite.config.ts): add istanbul plugin to generate code coverage reports
    The istanbul plugin is added to the Vite configuration to generate code coverage reports for the project. This will help in analyzing the codebase's test coverage and identifying areas that may need additional testing.

--- apps/dicty-frontpage/vite.config.ts
+++ apps/dicty-frontpage/vite.config.ts
@@ -1,11 +1,21 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 import react from "@vitejs/plugin-react"
+import istanbul from "vite-plugin-istanbul"
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 const viteConfig = defineConfig({
-  plugins: [react()],
+  plugins: [
+    // @ts-ignore
+    ...react(),
+    // @ts-ignore
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "__test__/"],
+      extension: [".ts", ".tsx"],
+    }),
+  ],
   server: {
     port: 3004,
   },
