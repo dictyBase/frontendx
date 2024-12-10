7af2bad62d4dd49dba24004aa89d1c031b5ea64c
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-10 14:55:58 -0600

    chore(frontpage): remove unused notfound and notfoundauth components
    The notfound.tsx and notfoundauth.tsx components were deleted as they were no longer being used in the frontpage application. This cleanup helps to reduce clutter and improve code maintainability.

--- apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/notfound.tsx
+++
@@ -1,6 +0,0 @@
-import { ACCESS } from "@dictybase/auth"
-import { NotFoundError } from "../../../../common/components/errors/NotFoundError"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default NotFoundError
-export const access = ACCESS.public
--- apps/dicty-frontpage/src/pages/[section]/[name]/[subname]/notfoundauth.tsx
+++
@@ -1,7 +0,0 @@
-import { ACCESS } from "@dictybase/auth"
-import { NotFoundErrorAuth } from "../../../../common/components/errors/NotFoundErrorAuth"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default NotFoundErrorAuth
-export const access = ACCESS.public
-export const roles = ["content-admin"]
