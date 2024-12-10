4a301c35c53e436f52bdace11e386048f8314933
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-10 10:17:55 -0600

    test(e2e): add tests
    The changes include adding tests to ensure that unauthorized users are redirected to the correct page, checking the display of components in the front page, and verifying the rendering of the Latest Publications heading in the papers page.

--- apps/dicty-frontpage/src/e2e/front.spec.ts
+++ apps/dicty-frontpage/src/e2e/front.spec.ts
@@ -2,6 +2,24 @@ import { test, expect } from "./baseFixtures"
 
 test("has title", async ({ page }) => {
   await page.goto("/")
-
   await expect(page).toHaveTitle(/dictyBase/)
+})
+
+test("Dicty News component is displayed", async ({ page }) => {
+  await page.goto("/")
+  const dictyNewsTitle = page.getByText("DCR News")
+  await expect(page).toHaveTitle(/dictyBase/)
+  await expect(dictyNewsTitle).toBeInViewport()
+})
+
+test("Dicty News renders link to /news", async ({ page }) => {
+  await page.goto("/")
+  await page.getByRole("link", { name: "More News"}).click()
+  await expect(page).toHaveURL(/^.*\/news(\/(show|editable))?$/)
+})
+
+test("Latest Papers renders link to /papers", async ({ page }) => {
+  await page.goto("/")
+  await page.getByRole("link", { name: "More Papers"}).click()
+  await expect(page).toHaveURL(/^.*\/papers$/)
 })
