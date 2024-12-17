91349d32c0b8c8dbb8ab5fca50186647fda6f0dc
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-17 16:08:51 -0600

    feat(editor-toolbar): add AttachFileButton component for inserting images in the editor toolbar
    The new AttachFileButton component allows users to easily insert images into the editor toolbar. It includes a button with an "Image" label and an icon that opens an ImageUploadDialog when clicked. This feature enhances the functionality of the editor toolbar by providing a convenient way to add images to the content.

---
+++ packages/editor-toolbar/src/components/AttachFileButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@material-ui/core"
+import AddIcon from "@material-ui/icons/Add"
+import { useAtom } from "jotai"
+import { insertImageDialogOpenAtom } from "../context/atomConfigs"
+import { ImageUploadDialog } from "./ImageUploadDialog"
+
+const AttachFileButton = () => {
+  const [isDialogOpen, setIsDialogOpen] = useAtom(insertImageDialogOpenAtom)
+
+  return (
+    <>
+      <Button
+        variant="text"
+        onClick={() => setIsDialogOpen(true)}
+        startIcon={<AddIcon />}>
+        Image
+      </Button>
+      <ImageUploadDialog open={isDialogOpen} />
+    </>
+  )
+}
+
+export { AttachFileButton }
