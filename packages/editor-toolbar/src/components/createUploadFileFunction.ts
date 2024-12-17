17790a7f7c19d40f965f4a4df6c3528a9a0d83a3
Kevin Tun <kevin.tun@northwestern.edu>
2024-12-17 11:47:22 -0600

    feat(editor-toolbar): add createUploadFileFunction to handle file upload process
    The createUploadFileFunction is added to handle the file upload process in the editor toolbar component. It includes steps for validating the file size, obtaining an access token, uploading the file, and returning the uploaded file URL through a GraphQL mutation. This function enhances the functionality of the editor toolbar by providing a structured approach to handling file uploads.

---
+++ packages/editor-toolbar/src/components/createUploadFileFunction.ts
@@ -0,0 +1,82 @@
+import { pipe } from "fp-ts/function"
+import {
+  Do as TEDo,
+  let as TElet,
+  bind as TEbind,
+  tryCatch as TEtryCatch,
+  of as TEof,
+  filterOrElse as TEfilterOrElse,
+  fromOption as EfromOption,
+} from "fp-ts/TaskEither"
+import { fromNullable as OfromNullable, map as Omap } from "fp-ts/Option"
+import { UploadFileMutationHookResult } from "dicty-graphql-schema"
+
+enum ErrorType {
+  VALIDITY_ERROR,
+  ACCESS_TOKEN_ERROR,
+  UPLOAD_FAILURE,
+}
+
+const overFileSizeLimitError = {
+  errorType: ErrorType.VALIDITY_ERROR,
+  message: "Chosen file size is too large. It must be smaller than 1MB.",
+}
+
+const accessTokenError = {
+  errorType: ErrorType.ACCESS_TOKEN_ERROR,
+  message: "Could not get access token",
+}
+
+const uploadFailureError = {
+  errorType: ErrorType.UPLOAD_FAILURE,
+  message: "Could not upload image to server",
+}
+
+const FILE_SIZE_LIMIT = 10_000_000
+
+const isValidFile = (file: File) => file.size < FILE_SIZE_LIMIT
+
+/**
+ * 1. User inputs file
+ * 2. File is validated (size, type?)
+ * 3. File is uploaded
+ * 4. GraphQL mutation returns uploaded file url to user
+ */
+const createUploadFileFunction = (
+  file: File,
+  uploadMutation: UploadFileMutationHookResult[0],
+  getAccessToken: (
+    resource?: string | undefined,
+  ) => Promise<string | undefined>,
+) =>
+  pipe(
+    TEDo,
+    TElet("selectedFile", () => file),
+    TEbind("token", () =>
+      TEtryCatch(
+        () =>
+          getAccessToken(import.meta.env.VITE_APP_LOGTO_API_SECOND_RESOURCE),
+        () => accessTokenError,
+      ),
+    ),
+    TEbind("uploadResult", ({ selectedFile, token }) =>
+      TEtryCatch(
+        () =>
+          uploadMutation({
+            variables: { file: selectedFile },
+            context: { headers: { Authorization: `Bearer ${token}` } },
+          }),
+        () => uploadFailureError,
+      ),
+    ),
+    TElet("url", ({ uploadResult }) =>
+      pipe(
+        uploadResult.data,
+        OfromNullable,
+        Omap(({ uploadFile: { url } }) => url),
+        EfromOption(() => uploadFailureError),
+      ),
+    ),
+  )
+
+export { createUploadFileFunction }
