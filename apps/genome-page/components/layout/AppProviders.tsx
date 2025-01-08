27986bfa08db5748009f1837e56bc28e82557278
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-08 09:54:44 -0600

    refactor(AppProviders.tsx): remove unused MuiTheme creation and switch to dictyTheme
    The commit removes the unused MuiTheme creation block and replaces it with the dictyTheme from the "@dictybase/ui-common" package. This change improves code cleanliness and ensures consistency with the theme used across the application.

--- apps/genome-page/components/layout/AppProviders.tsx
+++ apps/genome-page/components/layout/AppProviders.tsx
@@ -1,84 +1,14 @@
 import React from "react"
 import { ApolloProvider } from "@apollo/client"
 import { LogtoProvider, LogtoConfig, UserScope } from "@logto/react"
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles"
+import { MuiThemeProvider } from "@material-ui/core/styles"
 import {
   useGraphqlClient,
   useApolloClientCache,
   storageType,
 } from "@dictybase/data-access"
 import CircularProgress from "@material-ui/core/CircularProgress"
-
-const muiTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#004080",
-      light: "#476ab0",
-      dark: "#001b53",
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#008080",
-      light: "#4cb0af",
-      dark: "#005354",
-    },
-    error: {
-      main: "#b2102f",
-      light: "#ea4f58",
-      dark: "#7b0008",
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-    h1: {
-      fontSize: "2.00em",
-    },
-    h2: {
-      fontSize: "1.50em",
-    },
-    h3: {
-      fontSize: "1.17em",
-    },
-    h4: {
-      fontSize: "1.00em",
-    },
-    h5: {
-      fontSize: "0.83em",
-    },
-    h6: {
-      fontSize: "0.67em",
-    },
-  },
-  overrides: {
-    MuiTab: {
-      root: {
-        textTransform: "none",
-      },
-    },
-    MuiTabs: {
-      root: {
-        backgroundColor: "#cce6ff",
-        color: "#000",
-      },
-      indicator: {
-        backgroundColor: "#858780",
-      },
-    },
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          textDecoration: "none",
-          color: "#004080",
-          "&:hover": {
-            color: "#001b53",
-          },
-        },
-      },
-    },
-  },
-})
+import { dictyTheme } from "@dictybase/ui-common"
 
 const logtoConfig: LogtoConfig = {
   endpoint: process.env.NEXT_PUBLIC_LOGTO_ENDPOINT,
@@ -119,7 +49,7 @@ const AppProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <LogtoProvider config={logtoConfig}>
       <ApolloProvider client={client}>
-        <MuiThemeProvider theme={muiTheme}>{children}</MuiThemeProvider>
+        <MuiThemeProvider theme={dictyTheme}>{children}</MuiThemeProvider>
       </ApolloProvider>
     </LogtoProvider>
   )
