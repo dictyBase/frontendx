4baa9d2dbd32e8180dd9ad213adc2a39b8552057
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-08 10:37:44 -0600

    style(AppProviders.tsx): remove unused MUI theme and import custom dictyTheme for consistency
    The commit removes the unused MUI theme and imports the custom dictyTheme from the @dictybase/ui-common package for consistency in styling across the application.

--- apps/publication/src/components/layout/AppProviders.tsx
+++ apps/publication/src/components/layout/AppProviders.tsx
@@ -8,63 +8,7 @@ import {
   useApolloClientCache,
   storageType,
 } from "@dictybase/data-access"
-
-const muiTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#004080",
-      light: "#476ab0",
-      dark: "#001b53",
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#008080",
-      light: "#4cb0af",
-      dark: "#005354",
-    },
-    error: {
-      main: "#b2102f",
-      light: "#ea4f58",
-      dark: "#7b0008",
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-    h1: {
-      fontSize: "2.00em",
-    },
-    h2: {
-      fontSize: "1.50em",
-    },
-    h3: {
-      fontSize: "1.17em",
-    },
-    h4: {
-      fontSize: "1.00em",
-    },
-    h5: {
-      fontSize: "0.83em",
-    },
-    h6: {
-      fontSize: "0.67em",
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          textDecoration: "none",
-          color: "#004080",
-          "&:hover": {
-            color: "#001b53",
-          },
-        },
-      },
-    },
-  },
-})
+import { dictyTheme } from "@dictybase/ui-common"
 
 const logtoConfig: LogtoConfig = {
   endpoint: process.env.NEXT_PUBLIC_LOGTO_ENDPOINT,
@@ -105,7 +49,7 @@ const AppProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <LogtoProvider config={logtoConfig}>
       <ApolloProvider client={client}>
-        <MuiThemeProvider theme={muiTheme}>{children}</MuiThemeProvider>
+        <MuiThemeProvider theme={dictyTheme}>{children}</MuiThemeProvider>
       </ApolloProvider>
     </LogtoProvider>
   )
