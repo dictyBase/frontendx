bc56dd15f38cdcf3fa20a76e323ea34739b5e6dc
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-08 11:19:40 -0600

    refactor(ThemeProvider.tsx): remove custom theme creation and import theme from external library for consistency and maintainability
    The custom theme creation code has been removed from the file, and instead, the theme is now imported from an external library (@dictybase/ui-common). This change improves consistency across the codebase and makes it easier to maintain the theme configurations by centralizing them in a shared library.

--- apps/stock-center/src/ThemeProvider.tsx
+++ apps/stock-center/src/ThemeProvider.tsx
@@ -1,63 +1,9 @@
 import React from "react"
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles"
-
-const theme = createTheme({
-  // use color tool for palette -- https://material.io/resources/color/
-  palette: {
-    primary: {
-      main: "#004080",
-      light: "#476ab0",
-      dark: "#001b53",
-      contrastText: "#fff",
-    },
-    secondary: {
-      main: "#008080",
-      light: "#4cb0af",
-      dark: "#005354",
-    },
-    error: {
-      main: "#b2102f",
-      light: "#ea4f58",
-      dark: "#7b0008",
-    },
-  },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-    h1: {
-      fontSize: "2.00em",
-    },
-    h2: {
-      fontSize: "1.50em",
-    },
-    h3: {
-      fontSize: "1.17em",
-    },
-    h4: {
-      fontSize: "1.00em",
-    },
-    h5: {
-      fontSize: "0.83em",
-    },
-    h6: {
-      fontSize: "0.67em",
-    },
-  },
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          textDecoration: "none",
-          color: "#004080",
-        },
-      },
-    },
-  },
-})
+import { MuiThemeProvider } from "@material-ui/core/styles"
+import { dictyTheme } from "@dictybase/ui-common"
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => (
-  <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+  <MuiThemeProvider theme={dictyTheme}>{children}</MuiThemeProvider>
 )
 
-export { theme, ThemeProvider }
+export { ThemeProvider }
