571be956b20b9206c89aa5cf13bda9d5c867b4bc
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-28 13:15:38 -0600

    refactor(types.ts): extend IconType definition to include muiName property for better type checking
    The IconType definition in types.ts has been extended to include a muiName property. This change improves type checking and provides additional information about the IconType object.
    issue #909

--- packages/header-mui5/src/types.ts
+++ packages/header-mui5/src/types.ts
@@ -1,7 +1,9 @@
 import { type SvgIconTypeMap } from "@mui/material"
 import { type OverridableComponent } from "@mui/material/OverridableComponent"
 
-type IconType = OverridableComponent<SvgIconTypeMap<{}, "svg">>
+type IconType = OverridableComponent<SvgIconTypeMap<{}, "svg">> & {
+  muiName: string
+}
 
 type IconItemProperty = {
   href: string
