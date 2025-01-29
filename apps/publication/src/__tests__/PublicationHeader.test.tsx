06cbf2e700fd9e1f6fefe98f4c218a937a068303
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-29 11:42:50 -0600

    chore(tests): remove unused test files related to PublicationHeader, JournalDataItem, PublicationLoader, and PublicationPage components
    The test files were no longer needed as the components they were testing have been removed or refactored. Removing these unused test files helps to keep the codebase clean and maintainable.

--- apps/publication/src/__tests__/PublicationHeader.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import { PublicationHeader } from "../components/PublicationHeader"
-
-describe("componets/PublicationHeader", () => {
-  it("should show header", () => {
-    render(<PublicationHeader />)
-    expect(screen.getByText("dictyBase Literature")).toBeInTheDocument()
-  })
-})
