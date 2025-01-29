7f8b83e2a7c0ce145247fc35917e105ba233d1a2
Kevin Tun <kevin.tun@northwestern.edu>
2025-01-29 07:57:31 -0600

    refactor(pages): update import path for PublicationLoader component
    The import path for the PublicationLoader component has been updated to reflect the correct location within the project structure. This refactor ensures that the component is imported from the correct directory.

--- apps/publication/src/pages/index.tsx
+++ apps/publication/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useRouter } from "next/router"
 import { match } from "ts-pattern"
-import { PublicationLoader } from "../components/version_ab/PublicationLoader"
+import { PublicationLoader } from "../components/PublicationLoader"
 
 const Home = () => {
   const router = useRouter()
