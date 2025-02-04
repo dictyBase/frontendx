31a93c9ab1af0fe2d22761bdc840d89b1053d383
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 21:42:59 -0600

    feat(genome-page): add Layout component to GenomePageWrapper for consistent page layout
    The Layout component is added to the GenomePageWrapper to provide a consistent layout structure for the page. This change ensures that all pages within the genome-page app have a unified look and feel, enhancing the overall user experience.
    
    issue #965

--- apps/genome-page/pages/[id]/index.tsx
+++ apps/genome-page/pages/[id]/index.tsx
@@ -1,6 +1,7 @@
 import { SummaryContainer } from "components/features/Summary/SummaryContainer"
 import { SummaryLoader } from "components/features/Summary/SummaryLoader"
 import { GraphQLErrorPage } from "components/errors/GraphQLErrorPage"
+import { Layout } from "components/layout/Layout"
 import { useRouter } from "next/router"
 import { useGeneSummaryQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
@@ -18,18 +19,27 @@ const GenomePageWrapper = () => {
     fetchPolicy: "cache-and-network",
     nextFetchPolicy: "cache-only",
   })
-  return match(result)
-    .with(
-      {
-        data: P.select(P.not(P.nullish)),
-      },
-      (data) => <SummaryContainer geneSummary={data} />,
-    )
-    .with({ loading: true }, () => <SummaryLoader />)
-    .with({ error: P.select(P.not(undefined)) }, (error) => (
-      <GraphQLErrorPage error={error} />
-    ))
-    .otherwise(() => <> This message should not appear. </>)
+  return (
+    <Layout
+      gene={gene}
+      title={`Gene Summary for ${gene}`}
+      description={`Gene information for ${gene}`}
+    >
+        .with(
+          {
+            data: P.select(P.not(P.nullish)),
+          },
+          (data) => <SummaryContainer geneSummary={data} />,
+        )
+        .with({ loading: true }, () => <SummaryLoader />)
+        .with({ error: P.select(P.not(undefined)) }, (error) => (
+          <GraphQLErrorPage error={error} />
+        ))
+        .otherwise(() => (
+          <> This message should not appear. </>
+        ))}
+    </Layout>
+  )
 }
 
 // eslint-disable-next-line import/no-default-export
