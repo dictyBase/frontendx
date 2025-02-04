7968530eabeedaef353d3e36c39c58ad764db746
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 19:14:08 -0600

    refactor(PhenotypesContainer.tsx): remove unnecessary Layout component and useRouter hook
    The Layout component and the useRouter hook were removed from the PhenotypesContainer component as they were not being used. This cleanup simplifies the code and improves readability by eliminating unnecessary code.
    
    issue #965

--- apps/genome-page/components/features/Phenotypes/PhenotypesContainer.tsx
+++ apps/genome-page/components/features/Phenotypes/PhenotypesContainer.tsx
@@ -1,26 +1,14 @@
 import Typography from "@material-ui/core/Typography"
-import { Layout } from "components/layout/Layout"
 import { ListStrainsWithGeneQuery } from "dicty-graphql-schema"
-import { useRouter } from "next/router"
 import { PhenotypesDataTable } from "./PhenotypesDataTable"
 
 interface PhenotypesContainerProperties {
   strains: NonNullable<ListStrainsWithGeneQuery["listStrainsWithGene"]>
 }
-const PhenotypesContainer = ({ strains }: PhenotypesContainerProperties) => {
-  const { query } = useRouter()
-  const geneId = query.id as string
-
-  return (
-    <Layout
-      gene={geneId}
-      title={`Phenotypes for ${geneId}`}
-      description={`Gene phenotypes for ${geneId}`}>
-      <Typography component="div">
-        <PhenotypesDataTable strains={strains} />
-      </Typography>
-    </Layout>
-  )
-}
+const PhenotypesContainer = ({ strains }: PhenotypesContainerProperties) => (
+  <Typography component="div">
+    <PhenotypesDataTable strains={strains} />
+  </Typography>
+)
 
 export { PhenotypesContainer }
