e0e2224936d157654d913425fd8a8d26d224934e
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 21:27:23 -0600

    refactor(ReferencesLoader.tsx): remove unused useRouter and Layout imports, simplify ReferencesLoader component
    The changes were made to clean up the code by removing unused imports and simplifying the ReferencesLoader component to make it more concise and easier to read.
    
    issue #965

--- apps/genome-page/components/features/References/ReferencesLoader.tsx
+++ apps/genome-page/components/features/References/ReferencesLoader.tsx
@@ -1,21 +1,8 @@
-import { useRouter } from "next/router"
-import { Layout } from "components/layout/Layout"
 import { Loader } from "components/Loader"
 
 /**
  * Loading screen for Summary page
  */
-const ReferencesLoader = () => {
-  const { query } = useRouter()
-  const geneId = query.id as string
-  return (
-    <Layout
-      gene={geneId}
-      title={`References for ${geneId}`}
-      description={`Gene references for ${geneId}`}>
-      <Loader />
-    </Layout>
-  )
-}
+const ReferencesLoader = () => <Loader />
 
 export { ReferencesLoader }
