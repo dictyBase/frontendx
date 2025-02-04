035549907e772dff77da1b6bb314b5d3a78e8499
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 20:17:59 -0600

    style(OtherError.tsx): replace FontAwesomeIcon with Typography for consistent styling
    The FontAwesomeIcon component is replaced with Typography to maintain consistent styling across the application. This change ensures a unified design language and improves the overall visual coherence of the error component.
    
    issue #965

--- apps/genome-page/components/errors/OtherError.tsx
+++ apps/genome-page/components/errors/OtherError.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Typography from "@material-ui/core/Typography"
 import Image from "next/image"
 import { ErrorMessage } from "./ErrorMessage"
 import { useStyles } from "../../styles/errorStyles"
@@ -13,18 +13,16 @@ const OtherError = () => {
   const classes = useStyles()
 
   return (
-    <Grid container className={classes.mainGrid} justifyContent="center">
+    <Grid container justifyContent="center">
       <Grid item xs={10} md={8}>
         <div className={classes.error400}>
           <Image
-            src="/sad-dicty.png"
+            src="https://storage.dictybase.dev/editor/assets/2024-11-04/0627257c-9ce3-4f02-b000-9e16ef5b1062"
             alt="Sad Dicty Logo"
             width="350px"
             height="200%"
           />
-          <h1>
-            <FontAwesomeIcon icon="exclamation-circle" /> Error
-          </h1>
+          <Typography variant="h2"> Sorry, something went wrong. </Typography>
           <ErrorMessage />
         </div>
       </Grid>
