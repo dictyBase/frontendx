2e522f26fcb518cf96ca9ddb76757d9613c0d89e
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 21:48:58 -0600

    refactor: remove Layout component and its props as they are no longer needed
    The Layout component and its associated props were removed from both SummaryContainer.tsx and SummaryLoader.tsx files as they were no longer necessary for the functionality of the components. This cleanup simplifies the code and improves readability.
    
    issue #965

--- apps/genome-page/components/features/Summary/SummaryLoader.tsx
+++ apps/genome-page/components/features/Summary/SummaryLoader.tsx
@@ -1,20 +1,11 @@
-import { useRouter } from "next/router"
-import { Layout } from "components/layout/Layout"
 import { Loader } from "components/Loader"
 
 /**
  * Loading screen for Summary page
  */
 const SummaryLoader = () => {
-  const { query } = useRouter()
-  const geneId = query.id as string
   return (
-    <Layout
-      gene={geneId}
-      title={`GO Annotations for ${geneId}`}
-      description={`Gene Ontology Annotations for ${geneId}`}>
       <Loader />
-    </Layout>
   )
 }
 
