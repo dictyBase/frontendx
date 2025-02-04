357f6fed3cd834a82c07ebfbacd3a192e291e716
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 22:10:51 -0600

    style(Loader.tsx): refactor Loader component to use Paper and makeStyles for improved styling
    The Loader component has been refactored to use the Paper component from Material-UI for a more visually appealing loading skeleton. Additionally, the component now utilizes the makeStyles hook to define styling for the container, enhancing the overall design and structure of the component.
    
    issue #965

--- apps/genome-page/components/Loader.tsx
+++ apps/genome-page/components/Loader.tsx
@@ -1,16 +1,31 @@
 import { makeBy as AmakeBy } from "fp-ts/Array"
 import { Skeleton } from "@material-ui/lab"
 import Box from "@material-ui/core/Box"
+import Paper from "@material-ui/core/Paper"
+import { makeStyles } from "@material-ui/core/styles"
+import { blue } from "@material-ui/core/colors"
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: blue[50],
+    padding: "1rem",
+  },
+})
 
 /**
  * Loader is the default loading skeleton component.
  */
-const Loader = () => (
-  <Box mt="10px" data-testid="skeleton-loader">
-    {AmakeBy(12, (key) => (
-      <Skeleton height={50} key={key} animation="wave" />
-    ))}
-  </Box>
-)
+const Loader = () => {
+  const classes = useStyles()
+  return (
+    <Paper className={classes.container}>
+      <Box data-testid="skeleton-loader">
+        {AmakeBy(12, (key) => (
+          <Skeleton height={50} key={key} animation="wave" />
+        ))}
+      </Box>
+    </Paper>
+  )
+}
 
 export { Loader }
