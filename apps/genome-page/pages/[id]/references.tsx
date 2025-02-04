1182f58b7b993a7ff7434d8611923de91a57e6bb
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 19:15:32 -0600

    feat(references.tsx): add Layout and NoDataDisplay components for improved UI
    The changes introduce the Layout and NoDataDisplay components to enhance the user interface of the ReferencesPageWrapper. The Layout component provides a structured layout for displaying gene references, while the NoDataDisplay component handles cases where no data is available for the specified gene.
    
    issue #965

--- apps/genome-page/pages/[id]/references.tsx
+++ apps/genome-page/pages/[id]/references.tsx
@@ -1,5 +1,7 @@
 import { ReferencesContainer } from "components/features/References/ReferencesContainer"
 import { GraphQLErrorPage } from "@dictybase/ui-common"
+import { Layout } from "components/layout/Layout"
+import { NoDataDisplay } from "components/NoDataDisplay"
 import { ReferencesLoader } from "components/features/References/ReferencesLoader"
 import { useRouter } from "next/router"
 import { useListPublicationsWithGeneQuery } from "dicty-graphql-schema"
@@ -10,28 +12,45 @@ import { match, P } from "ts-pattern"
 */
 const ReferencesPageWrapper = () => {
   const { query } = useRouter()
-  const gene = query.id as string
+  const geneId = query.id as string
   const result = useListPublicationsWithGeneQuery({
     variables: {
-      gene,
+      gene: geneId,
     },
     fetchPolicy: "cache-and-network",
     nextFetchPolicy: "cache-only",
   })
-  return match(result)
-    .with(
-      {
-        data: {
-          listPublicationsWithGene: P.select(P.array({ id: P.string })),
-        },
-      },
-      (publications) => <ReferencesContainer publications={publications} />,
-    )
-    .with({ loading: true }, () => <ReferencesLoader />)
-    .with({ error: P.select(P.not(undefined)) }, (error) => (
-      <GraphQLErrorPage error={error} />
-    ))
-    .otherwise(() => <> This message should not appear. </>)
+  return (
+    <Layout
+      gene={geneId}
+      title={`References for ${geneId}`}
+      description={`Gene references for ${geneId}`}>
+      {match(result)
+        .with(
+          {
+            data: {
+              listPublicationsWithGene: [],
+            },
+          },
+          () => <NoDataDisplay query="References" geneId={geneId} />,
+        )
+        .with(
+          {
+            data: {
+              listPublicationsWithGene: P.select(P.array({ id: P.string })),
+            },
+          },
+          (publications) => <ReferencesContainer publications={publications} />,
+        )
+        .with({ loading: true }, () => <ReferencesLoader />)
+        .with({ error: P.select(P.not(undefined)) }, (error) => (
+          <GraphQLErrorPage error={error} />
+        ))
+        .otherwise(() => (
+          <> This message should not appear. </>
+        ))}
+    </Layout>
+  )
 }
 
 // eslint-disable-next-line import/no-default-export
