7389ac82759d4a63a2b9de4fa2aa6c71dfd71be3
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 21:27:49 -0600

    refactor(OntologyContainer.tsx): remove unused imports and simplify component structure
    The commit simplifies the OntologyContainer component by removing unused imports (Layout and useRouter) and simplifying the component structure to improve readability and maintainability.
    
    issue #965

--- apps/genome-page/components/features/Ontology/OntologyContainer.tsx
+++ apps/genome-page/components/features/Ontology/OntologyContainer.tsx
@@ -1,7 +1,5 @@
 import Typography from "@material-ui/core/Typography"
-import { Layout } from "components/layout/Layout"
 import { GeneOntologyAnnotationQuery } from "dicty-graphql-schema"
-import { useRouter } from "next/router"
 import { OntologyTabLayout } from "./OntologyTabLayout"
 /**
  * Container component that issues a GraphQL query to get gene data for the
@@ -11,20 +9,10 @@ import { OntologyTabLayout } from "./OntologyTabLayout"
 interface OntologyContainerProperties {
   goas: NonNullable<GeneOntologyAnnotationQuery["geneOntologyAnnotation"]>
 }
-const OntologyContainer = ({ goas }: OntologyContainerProperties) => {
-  const { query } = useRouter()
-  const geneId = query.id as string
-
-  return (
-    <Layout
-      gene={geneId}
-      title={`GO Annotations for ${geneId}`}
-      description={`Gene Ontology Annotations for ${geneId}`}>
-      <Typography component="div">
-        <OntologyTabLayout goas={goas} />{" "}
-      </Typography>
-    </Layout>
-  )
-}
+const OntologyContainer = ({ goas }: OntologyContainerProperties) => (
+  <Typography component="div">
+      <OntologyTabLayout goas={goas} />{" "}
+  </Typography>
+)
 
 export { OntologyContainer }
