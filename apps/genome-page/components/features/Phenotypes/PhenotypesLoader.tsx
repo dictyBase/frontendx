37f7a91866bf39bd64185eb793f3cf9c10668a9b
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-03 21:27:08 -0600

    refactor(PhenotypesLoader.tsx): simplify PhenotypesLoader component by removing unused code related to next/router and Layout component
    The code has been refactored to simplify the PhenotypesLoader component by removing unused code related to next/router and the Layout component. This improves code readability and reduces unnecessary complexity in the component.
    
    issue #965

--- apps/genome-page/components/features/Phenotypes/PhenotypesLoader.tsx
+++ apps/genome-page/components/features/Phenotypes/PhenotypesLoader.tsx
@@ -1,21 +1,8 @@
-import { useRouter } from "next/router"
-import { Layout } from "components/layout/Layout"
 import { Loader } from "components/Loader"
 
 /**
  * Loading screen for Summary page
  */
-const PhenotypesLoader = () => {
-  const { query } = useRouter()
-  const geneId = query.id as string
-  return (
-    <Layout
-      gene={geneId}
-      title={`Phenotypes for ${geneId}`}
-      description={`Gene phenotypes for ${geneId}`}>
-      <Loader />
-    </Layout>
-  )
-}
+const PhenotypesLoader = () => <Loader />
 
 export { PhenotypesLoader }
