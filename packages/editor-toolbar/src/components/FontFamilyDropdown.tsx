e482db10cda5b8f855107d6d8122d6a2ea1343c5
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-12 12:24:39 -0600

    refactor(editor-toolbar): use fontFamilyOptions atom for dropdown
    This commit refactors the FontFamilyDropdown component to use the
    fontFamilyOptions atom for rendering the dropdown options. It removes
    the defaultFontFamilyOptions and fontOptions prop, and instead maps
    over the fontFamilyOptions atom to generate the MenuItem components.
    The value prop of the Select component is now bound to the value
    property of the fontFamily atom.

--- packages/editor-toolbar/src/components/FontFamilyDropdown.tsx
+++ packages/editor-toolbar/src/components/FontFamilyDropdown.tsx
@@ -2,51 +2,37 @@ import React from "react"
 import { Select, MenuItem } from "@material-ui/core"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { useAtom } from "jotai"
-import { fontFamilyAtom } from "../context/atomConfigs"
+import { fontFamilyAtom, fontFamilyOptions } from "../context/atomConfigs"
 import { applyTextStyles } from "../utils/textStyles"
 import { useToolbarItemStyles } from "../hooks/useToolbarItemStyles"
-
-const defaultFontFamilyOptions = [
-  "Arial",
-  "Courier New",
-  "Georgia",
-  "Times New Roman",
-  "Trebuchet MS",
-  "Verdana",
-]
 
 type FontFamilySelectProperties = React.ChangeEvent<{
   name?: string | undefined
   value: unknown
 }>
 
-type FontFamilyDropdownProperties = {
-  fontOptions?: typeof defaultFontFamilyOptions
-}
-
 const title = "Font Family"
 
-const FontFamilyDropdown = ({
-  fontOptions = defaultFontFamilyOptions,
-}: FontFamilyDropdownProperties) => {
+const FontFamilyDropdown = () => {
   const [fontFamily] = useAtom(fontFamilyAtom)
   const [editor] = useLexicalComposerContext()
   const classes = useToolbarItemStyles()
   const joinedClasses = `${classes.root} ${classes.spaced}`
 
   const onFontFamilySelect = (event: FontFamilySelectProperties) => {
+    console.log(event.target.value)
     applyTextStyles(editor, { "font-family": event.target.value as string })
   }
-
+  console.log(fontFamily)
   return (
     <Select
       title={title}
       className={joinedClasses}
       onChange={onFontFamilySelect}
-      value={fontFamily}>
-      {fontOptions.map((option) => (
-        <MenuItem key={option} value={option}>
-          {option}
+      value={fontFamily.value}>
+      {fontFamilyOptions.map((option) => (
+        <MenuItem key={option.name} value={option.value}>
+          {option.name}
         </MenuItem>
       ))}
     </Select>
