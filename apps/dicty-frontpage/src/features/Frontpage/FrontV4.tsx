3de48c9b0e8b7adfb14abc96f6eb1bfdcbadfa20
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-13 11:32:50 -0600

    feat: implement new frontpage version 4
    This commit introduces a new version of the frontpage with the following features:
    - A slideshow component with curated Dictyostelium images and Dicty info.
    - A new DictyInfo component that fetches content from GraphQL.
    - Reorganized layout with Material-UI Grid.
    - Addition of a v4 page to route to the new frontpage.
    - Addition of react-responsive-carousel library for the slideshow.
    
    The new frontpage provides a more modern and engaging user experience.

---
+++ apps/dicty-frontpage/src/features/Frontpage/FrontV4.tsx
@@ -0,0 +1,76 @@
+import { Helmet } from "react-helmet"
+import { Grid, Container } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import {
+  RecentUpdates,
+  ComingSoon,
+  Featured,
+  StatusReportContainer,
+} from "@dictybase/ui-frontpage"
+import { Slideshow } from "./SlideshowV4"
+import { LatestPapers } from "./LatestPapers"
+import { DictyNewsWithAuth } from "./DictyNewsWithAuth"
+
+const useStyles = makeStyles((theme: Theme) => ({
+  topItem: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+  },
+  bottomItem: {
+    padding: "1px 2px 1px 2px",
+  },
+  main: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+    [theme.breakpoints.up("xl")]: {
+      maxWidth: "90%",
+    },
+  },
+}))
+
+/** This is the frontpage component that appears when the user hits the "/" route. */
+
+const Front = () => {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="xl" className={classes.main}>
+      <Helmet>
+        <title>
+          dictyBase - your central resource for Dictyostelid genomics
+        </title>
+        <meta
+          name="description"
+          content="dictyBase is a central resource for Dictyostelid genomics"
+        />
+      </Helmet>
+      <Grid container justifyContent="center">
+        <Grid item className={classes.topItem} sm={12} md={12} xl={12}>
+          <StatusReportContainer />
+        </Grid>
+        <Grid item className={classes.topItem} sm={12} md={6} xl={6}>
+          <Slideshow />
+        </Grid>
+        <Grid item className={classes.topItem} sm={12} md={6} xl={6}>
+          <DictyNewsWithAuth />
+        </Grid>
+        <Grid item className={classes.topItem} xs={12} xl={12}>
+          <LatestPapers />
+        </Grid>
+        <Grid item className={classes.topItem} xs={12} sm={4} lg={4} xl={4}>
+          <Featured />
+        </Grid>
+        <Grid item className={classes.topItem} xs={12} sm={4} lg={4} xl={4}>
+          <RecentUpdates />
+        </Grid>
+        <Grid item className={classes.topItem} xs={12} sm={4} lg={4} xl={4}>
+          <ComingSoon text="Recent Annotations coming soon!" height="390px" />
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export { Front }
