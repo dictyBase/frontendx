9db40c0e12a1f6741de3cdecd2ef0b7ef7981be4
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-13 11:32:41 -0600

    feat: integrate new frontpage components and layout
    This commit introduces a new version of the frontpage with updated components and a new layout. It includes the following changes:
    
    - Added `DictyInfoV3.tsx`: This component fetches and displays content from a CMS based on a slug.
    - Added `FrontV3.tsx`: This component is the main frontpage layout, incorporating various components such as `StatusReportContainer`, `Slideshow`, `DictyNewsWithAuth`, `DictyInfo`, `LatestPapers`, `Featured`, `RecentUpdates`, and `ComingSoon`.
    - Added `v3.tsx`: This page component exports the new `FrontV3` component and sets the access level to public.
    
    The new frontpage provides a more modern and informative experience for users.

---
+++ apps/dicty-frontpage/src/features/Frontpage/DictyInfoV3.tsx
@@ -0,0 +1,37 @@
+import { Box, makeStyles } from "@material-ui/core"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { Editor } from "@dictybase/editor"
+import { purple } from "@material-ui/core/colors"
+import { NAMESPACE } from "../../common/constants/namespace"
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: purple[50],
+    color: "#04313f",
+    padding: "1rem",
+    borderRadius: "16px",
+  },
+})
+
+const DictyInfo = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-info` },
+  })
+  const classes = useStyles()
+  return match(result)
+    .with(
+      { data: { contentBySlug: P.select({ content: P.string }) } },
+      ({ content, slug }) => (
+        <Box className={classes.root}>
+          <Editor content={{ editorState: content, storageKey: slug }} />
+        </Box>
+      ),
+    )
+    .with({ data: { contentBySlug: P.nullish } }, () => <></>)
+    .with({ loading: true }, () => <></>)
+    .with({ error: P.select(P.not(undefined)) }, () => <></>)
+    .otherwise(() => <> This message should not appear. </>)
+}
+
+export { DictyInfo }
