fa56e41979970388c562875f4dddfd34cd9e7d23
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-13 11:32:14 -0600

    feat: implement new frontpage layout
    This commit introduces a new frontpage layout with the following features:
    
    - A new `FrontV2` component that serves as the main frontpage.
    - A `Showcase` component that displays a slideshow and dictyBase information.
    - A `DictyInfo` component that fetches and renders dictyBase information from a GraphQL API.
    - The new frontpage is accessible at the `/v2` route.
    - The new frontpage is public.
    
    The new layout is more modern and visually appealing, and it provides a better user experience.

---
+++ apps/dicty-frontpage/src/pages/v2.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "@dictybase/auth"
+import { Front } from "../features/Frontpage/FrontV2"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default Front
+export const access = ACCESS.public
