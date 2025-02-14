a37cff81d50c8912e8755b73a790ffc712c6c39d
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-14 11:29:37 -0600

    feat: create new frontpage component and page
    This commit introduces a new frontpage component (FrontV3b.tsx) and a corresponding page (v3b.tsx). The new frontpage includes various sections such as a status report, dicty info, slideshow, dicty news, latest papers, featured content, recent updates, and a coming soon section. The page is publicly accessible. This change allows for a redesigned frontpage with updated content and layout.

---
+++ apps/dicty-frontpage/src/pages/v3b.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "@dictybase/auth"
+import { Front } from "../features/Frontpage/FrontV3b"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default Front
+export const access = ACCESS.public
