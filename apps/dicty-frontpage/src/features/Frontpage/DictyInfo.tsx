e94b2b18e4676db1928e68eb8ffaab178801b8c8
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-17 15:10:40 -0600

    feat(DictyInfo.tsx): display dicty info with loading and styling
    This commit adds a loading state and styling to the DictyInfo component.
    The component now displays a loading indicator while fetching data.
    It also includes styling to improve the visual appearance of the component.
    The styling includes a background color, text color, padding, and border radius.
    A heading was added to improve the semantics of the component.

--- apps/dicty-frontpage/src/features/Frontpage/DictyInfo.tsx
+++ apps/dicty-frontpage/src/features/Frontpage/DictyInfo.tsx
@@ -1,24 +1,44 @@
-import { Box } from "@material-ui/core"
+import { Typography, Box, makeStyles } from "@material-ui/core"
+import { LoadingDisplay } from "@dictybase/ui-common"
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
 import { Editor } from "@dictybase/editor"
+import { teal } from "@material-ui/core/colors"
 import { NAMESPACE } from "../../common/constants/namespace"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: teal[50],
+    color: "#04313f",
+    padding: theme.spacing(3),
+    borderRadius: theme.spacing(2),
+  },
+}))
 
 const DictyInfo = () => {
   const result = useContentBySlugQuery({
     variables: { slug: `${NAMESPACE}-info` },
   })
+  const classes = useStyles()
   return match(result)
     .with(
       { data: { contentBySlug: P.select({ content: P.string }) } },
       ({ content, slug }) => (
-        <Box>
+        <Box className={classes.root}>
+          <Typography color="secondary" variant="h2">
+            {" "}
+            Dictyostelium discoideum
+          </Typography>
           <Editor content={{ editorState: content, storageKey: slug }} />
         </Box>
       ),
     )
     .with({ data: { contentBySlug: P.nullish } }, () => <></>)
-    .with({ loading: true }, () => <></>)
+    .with({ loading: true }, () => (
+      <Box className={classes.root}>
+        <LoadingDisplay rows={5} />
+      </Box>
+    ))
     .with({ error: P.select(P.not(undefined)) }, () => <></>)
     .otherwise(() => <> This message should not appear. </>)
 }
