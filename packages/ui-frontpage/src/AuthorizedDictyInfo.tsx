1117c63e483fffa7863e9f27c28c091317892688
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-17 15:41:34 -0600

    feat(ui-frontpage): create AuthorizedDictyInfo component
    This component fetches content from a GraphQL API using a slug, displays it using the Editor component, and provides a loading state. It also includes styling with Material-UI.

---
+++ packages/ui-frontpage/src/AuthorizedDictyInfo.tsx
@@ -0,0 +1,49 @@
+import { Typography, Box, IconButton, makeStyles } from "@material-ui/core"
+import EditIcon from "@material-ui/icons/Edit"
+import { LoadingDisplay } from "@dictybase/ui-common"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { Editor } from "@dictybase/editor"
+import { teal } from "@material-ui/core/colors"
+import { NAMESPACE } from "./namespace"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: teal[50],
+    color: "#04313f",
+    padding: theme.spacing(3),
+    borderRadius: theme.spacing(2),
+  },
+}))
+
+const AuthorizedDictyInfo = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-info` },
+  })
+  const classes = useStyles()
+  return match(result)
+    .with(
+      { data: { contentBySlug: P.select({ content: P.string }) } },
+      ({ content, slug }) => (
+        <Box className={classes.root}>
+          <Typography color="secondary" variant="h2">
+            Dictyostelium discoideum
+            <IconButton color="secondary" size="small">
+              <EditIcon />
+            </IconButton>
+          </Typography>
+          <Editor content={{ editorState: content, storageKey: slug }} />
+        </Box>
+      ),
+    )
+    .with({ data: { contentBySlug: P.nullish } }, () => <></>)
+    .with({ loading: true }, () => (
+      <Box className={classes.root}>
+        <LoadingDisplay rows={5} />
+      </Box>
+    ))
+    .with({ error: P.select(P.not(undefined)) }, () => <></>)
+    .otherwise(() => <> This message should not appear. </>)
+}
+
+export { AuthorizedDictyInfo }
