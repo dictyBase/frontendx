4bb635a2a4665f1240f426c4111d6c06e6f31af7
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-17 15:59:25 -0600

    feat(DictyInfoWithAuth.tsx): create DictyInfoWithAuth component
    This component conditionally renders either the DictyInfo or
    AuthorizedDictyInfo component based on user authorization. It uses the
    useAuthorization hook to check if the user has the "content-admin" role.
    If the user is authorized, it renders the AuthorizedDictyInfo component,
    otherwise it renders the DictyInfo component. Both components receive
    the result of the useContentBySlugQuery hook, which fetches content
    data.

---
+++ apps/dicty-frontpage/src/features/Frontpage/DictyInfoWithAuth.tsx
@@ -0,0 +1,20 @@
+import { DictyInfo, AuthorizedDictyInfo } from "@dictybase/ui-frontpage"
+import { useAuthorization } from "@dictybase/auth"
+import { match } from "ts-pattern"
+import { useContentBySlugQuery } from "dicty-graphql-schema"
+import { NAMESPACE } from "../../common/constants/namespace"
+
+const authorizedRoles = ["content-admin"]
+
+const DictyInfoWithAuth = () => {
+  const result = useContentBySlugQuery({
+    variables: { slug: `${NAMESPACE}-info` },
+  })
+  const { isAuthorized } = useAuthorization({ entries: authorizedRoles })
+  return match(isAuthorized)
+    .with(true, () => <AuthorizedDictyInfo result={result}/>)
+    .with(false, () => <DictyInfo result={result} />)
+    .exhaustive()
+}
+
+export { DictyInfoWithAuth }
