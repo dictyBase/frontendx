d66909a72b718fe1e1ebb289a83d449d73b475ca
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-17 15:10:09 -0600

    feat: remove unused frontpage versions
    Removes v2, v3, v3b, and v4 frontpage versions. These versions
    are no longer in use, so they have been removed to reduce
    codebase size and complexity.

--- apps/dicty-frontpage/src/pages/v2.tsx
+++
@@ -1,6 +0,0 @@
-import { ACCESS } from "@dictybase/auth"
-import { Front } from "../features/Frontpage/FrontV2"
-
-// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
-export default Front
-export const access = ACCESS.public
