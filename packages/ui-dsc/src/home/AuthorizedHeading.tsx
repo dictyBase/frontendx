0abf4379697975c089ca12a6a42dadb5730ae9f9
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-18 11:11:51 -0600

    feat(AuthorizedHeading.tsx): create AuthorizedHeading component
    This commit introduces the AuthorizedHeading component, which displays a welcome message and an edit icon button. Clicking the edit icon navigates the user to the "/information/intro/edit" route. This component is intended for authorized users who have permission to edit the welcome message.

---
+++ packages/ui-dsc/src/home/AuthorizedHeading.tsx
@@ -0,0 +1,26 @@
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import EditIcon from "@material-ui/icons/Edit"
+import { useNavigate } from "react-router-dom"
+import { useStyles } from "./homeStyles"
+
+const AuthorizedHeading = () => {
+  const navigate = useNavigate()
+  const onClick = () => {
+    navigate("/information/intro/edit")
+  }
+  const classes = useStyles({})
+  return (
+    <Grid item className={classes.header}>
+      <Typography variant="h1">
+        Welcome to Dicty Stock Center (DSC)
+        <IconButton size="medium" onClick={onClick}>
+          <EditIcon className={classes.icon} />
+        </IconButton>
+      </Typography>
+    </Grid>
+  )
+}
+
+export { AuthorizedHeading }
