b3d5f21e40c88f46f0222b839f93709124118e60
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-25 17:05:31 -0600

    feat(ui-frontpage): create DictyNewsDisplay component to display news
    This component fetches and displays news articles from a GraphQL API.
    It handles loading, empty states, errors, and displays a list of the
    three most recently updated news articles. It uses `ts-pattern` for
    pattern matching on the query result and `fp-ts` for array manipulation.

---
+++ packages/ui-frontpage/src/news/DictyNewsDisplay.tsx
@@ -0,0 +1,52 @@
+import { match, P } from "ts-pattern"
+import { pipe } from "fp-ts/function"
+import {
+  takeLeft as AtakeLeft,
+  sort as Asort,
+  reverse as Areverse,
+} from "fp-ts/Array"
+import {
+  ListContentByNamespaceQueryHookResult,
+  ListContentByNamespaceQuery,
+} from "dicty-graphql-schema"
+import { NewsList } from "./NewsList"
+import { EmptyNewsList } from "./EmptyNewsList"
+import { NewsLoader } from "./NewsLoader"
+import { ordByUpdatedAt } from "../utils/ordByUpdatedAt"
+
+type AuthorizedDictyNewsDisplayProperties = {
+  queryResult: ListContentByNamespaceQueryHookResult
+}
+
+const renderNewsList = (
+  newsList: ListContentByNamespaceQuery["listContentByNamespace"],
+) =>
+  pipe(newsList, Asort(ordByUpdatedAt), Areverse, AtakeLeft(3), (list) => (
+    <NewsList contentList={list} />
+  ))
+
+const DictyNewsDisplay = ({
+  queryResult,
+}: AuthorizedDictyNewsDisplayProperties) =>
+  match(queryResult)
+    .with(
+      {
+        data: {
+          listContentByNamespace: [],
+        },
+      },
+      () => <EmptyNewsList />,
+    )
+    .with(
+      {
+        data: {
+          listContentByNamespace: P.select(P.array({ content: P.string })),
+        },
+      },
+      renderNewsList,
+    )
+    .with({ loading: true }, () => <NewsLoader />)
+    .with({ error: P.select(P.not(undefined)) }, () => <EmptyNewsList />)
+    .otherwise(() => <> This message should not appear. </>)
+
+export { DictyNewsDisplay }
