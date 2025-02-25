7b6c7305393f4b47f047f53695b9bd5704cf9ee3
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-25 17:02:47 -0600

    feat(ui-frontpage): create AuthorizedDictyNewsDisplay component
    This component displays a list of authorized news items.
    It uses a GraphQL query to fetch the news items and displays them in a list.
    It also handles the loading and error states of the query.
    The component uses pattern matching to determine which UI to render based on the query result.
    It displays a loader while the data is loading, an empty list if there is no data, and an error message if there is an error.
    The component takes the top 3 most recent news items and displays them.

---
+++ packages/ui-frontpage/src/news/AuthorizedDictyNewsDisplay.tsx
@@ -0,0 +1,52 @@
+import { match, P } from "ts-pattern"
+import { pipe } from "fp-ts/function"
+import {
+  takeLeft as AtakeLeft,
+  sort as Asort,
+  reverse as Areverse,
+} from "fp-ts/Array"
+import {
+  ListContentByNamespaceQueryHookResult,
+  ListContentByNamespaceQuery,
+} from "dicty-graphql-schema"
+import { AuthorizedNewsList } from "./AuthorizedNewsList"
+import { EmptyNewsList } from "./EmptyNewsList"
+import { NewsLoader } from "./NewsLoader"
+import { ordByUpdatedAt } from "../utils/ordByUpdatedAt"
+
+type AuthorizedDictyNewsDisplayProperties = {
+  queryResult: ListContentByNamespaceQueryHookResult
+}
+
+const renderAuthorizedNewsList = (
+  newsList: ListContentByNamespaceQuery["listContentByNamespace"],
+) =>
+  pipe(newsList, Asort(ordByUpdatedAt), Areverse, AtakeLeft(3), (list) => (
+    <AuthorizedNewsList contentList={list} />
+  ))
+
+const AuthorizedDictyNewsDisplay = ({
+  queryResult,
+}: AuthorizedDictyNewsDisplayProperties) =>
+  match(queryResult)
+    .with(
+      {
+        data: {
+          listContentByNamespace: [],
+        },
+      },
+      () => <EmptyNewsList />,
+    )
+    .with(
+      {
+        data: {
+          listContentByNamespace: P.select(P.array({ content: P.string })),
+        },
+      },
+      renderAuthorizedNewsList,
+    )
+    .with({ loading: true }, () => <NewsLoader />)
+    .with({ error: P.select(P.not(undefined)) }, () => <EmptyNewsList />)
+    .otherwise(() => <> This message should not appear. </>)
+
+export { AuthorizedDictyNewsDisplay }
