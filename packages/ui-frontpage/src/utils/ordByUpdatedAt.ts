635b0c852c62cff0a8a27be504d6f639b6de8947
Kevin Tun <kevin.tun@northwestern.edu>
2025-02-25 13:50:25 -0600

    feat(ordByUpdatedAt.ts): add ordByUpdatedAt function to order content by updated_at field
    This commit introduces a new function, ordByUpdatedAt, that allows ordering of content items based on their updated_at field. It uses fp-ts library for functional programming and date-fns for date parsing and comparison. The function takes Content type as input and returns an Ord type that can be used to compare two Content items based on their updated_at field.

---
+++ packages/ui-frontpage/src/utils/ordByUpdatedAt.ts
@@ -0,0 +1,12 @@
+import { pipe } from "fp-ts/function"
+import { Ord as NOrd } from "fp-ts/number"
+import { Ord, contramap } from "fp-ts/Ord"
+import { Content } from "dicty-graphql-schema"
+import { parseISO, getTime } from "date-fns/fp"
+
+const ordByUpdatedAt: Ord<Content> = pipe(
+  NOrd,
+  contramap(({ updated_at }) => pipe(updated_at, parseISO, getTime)),
+)
+
+export { ordByUpdatedAt }
