0fef26637c72128495eb1754103f63f44fdb20ed
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-04 10:38:39 -0600

    feat(BreadcrumbsLink.tsx): simplify BreadcrumbsLink component
    This commit simplifies the BreadcrumbsLink component by removing the
    breadcrumbNameMap and nonClickableRoutes variables. The component now
    accepts a name and path prop, which are used to render the link. This
    makes the component more flexible and easier to use.

--- apps/stock-center/src/components/BreadcrumbsLink.tsx
+++ apps/stock-center/src/components/BreadcrumbsLink.tsx
@@ -1,22 +1,11 @@
 import Link from "@material-ui/core/Link"
-import Typography from "@material-ui/core/Typography"
 import { Link as RouterLink } from "react-router-dom"
 import { capitalizeFirstCharacter } from "@dictybase/ui-dsc"
 
-const breadcrumbNameMap: { [key: string]: string } = {
-  "/information": "Information",
-  "/order": "Order",
-  "/mydsc": "MyDSC",
-  "/strains": "Strains",
-  "/plasmids": "Plasmids",
-  "/phenotypes": "Phenotypes",
-}
-
-const nonClickableRoutes = new Set(["phenotypes"])
-
 type BreadcrumbsLinkProperties = {
   /** Pathname from URL (i.e. information) */
-  pathname: string
+  name: string
+  path: string
 }
 
 /**
@@ -24,26 +13,14 @@ type BreadcrumbsLinkProperties = {
  * final list item.
  */
 
-const BreadcrumbsLink = ({ pathname }: BreadcrumbsLinkProperties) => {
-  if (nonClickableRoutes.has(pathname)) {
-    return (
-      <Typography color="textPrimary" data-testid="breadcrumbs-text">
-        {capitalizeFirstCharacter(pathname)}
-      </Typography>
-    )
-  }
-
-  const route = `/${pathname}`
-
-  return (
-    <Link
-      color="inherit"
-      component={RouterLink}
-      to={route}
-      data-testid="breadcrumbs-link">
-      {breadcrumbNameMap[route]}
-    </Link>
-  )
-}
+const BreadcrumbsLink = ({ name, path }: BreadcrumbsLinkProperties) => (
+  <Link
+    color="inherit"
+    component={RouterLink}
+    to={path}
+    data-testid="breadcrumbs-link">
+    {capitalizeFirstCharacter(name)}
+  </Link>
+)
 
 export { BreadcrumbsLink }
