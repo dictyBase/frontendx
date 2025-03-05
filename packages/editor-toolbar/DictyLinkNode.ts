6cb4993ac1b25ba5dcab6e59a019f7e01b47466c
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-05 11:16:22 -0600

    feat(editor-toolbar): add download attribute to DictyLinkNode
    This commit introduces the ability to add a download attribute to
    DictyLinkNode. This allows users to specify a filename for the linked
    resource when it is downloaded.
    
    The following changes were made:
    
    - Added a `__download` property to the `DictyLinkNode` class to store
      the download filename.
    - Added `setDownload` and `getDownload` methods to the
      `DictyLinkNode` class to set and retrieve the download filename.
    - Updated the `clone` method to copy the `__download` property when
      cloning a `DictyLinkNode`.
    - Updated the `createDOM` method to set the `download` attribute of
      the anchor element when the `__download` property is set.
    - Updated the `updateDOM` method to update the `download` attribute of
      the anchor element when the `__download` property changes.
    
    These changes allow users to create links that automatically trigger a
    download with a specified filename when clicked.

--- packages/editor-toolbar/DictyLinkNode.ts
+++ packages/editor-toolbar/DictyLinkNode.ts
@@ -1,6 +1,7 @@
-import { NodeKey } from "lexical"
+import { EditorConfig, NodeKey } from "lexical"
 import { LinkNode, LinkAttributes } from "@lexical/link"
 import { pipe } from "fp-ts/function"
+import { match as Bmatch } from "fp-ts/boolean"
 import {
   fromNullable as OfromNullable,
   flatMap as OflatMap,
@@ -12,7 +13,7 @@ type DictyLinkAttributes = LinkAttributes & {
 }
 
 class DictyLinkNode extends LinkNode {
-  __download: string
+  __download: null | string
 
   constructor(url: string, attributes?: DictyLinkAttributes, key?: NodeKey) {
     super(url, attributes, key)
@@ -20,9 +21,76 @@ class DictyLinkNode extends LinkNode {
       attributes,
       OfromNullable,
       OflatMap(({ download }) => OfromNullable(download)),
-      Omatch(() =>  "", (f) => f)
+      Omatch(
+        () => null,
+        (f) => f,
+      ),
     )
     this.__download = filename
+  }
+
+  setDownload(filename: string) {
+    const self = this.getWritable()
+    self.__download = filename
+  }
+
+  getDownload() {
+    const self = this.getLatest()
+    return self.__download
+  }
+
+  static override clone(node: DictyLinkNode) {
+    const cloneAttributes = {
+      rel: node.__rel,
+      target: node.__target,
+      title: node.__title,
+      download: node.__download,
+    }
+    return new DictyLinkNode(node.__url, cloneAttributes, node.__key)
+  }
+
+  override createDOM(config: EditorConfig) {
+    const element = super.createDOM(config)
+    pipe(
+      this.__download,
+      OfromNullable,
+      Omatch(
+        () => {},
+        (download) => (element.download = download),
+      ),
+    )
+    return element
+  }
+  override updateDOM(
+    previousNode: DictyLinkNode,
+    anchor: HTMLAnchorElement,
+    config: EditorConfig,
+  ) {
+    const isUpdated = super.updateDOM(previousNode, anchor, config)
+    // If new value and previous value of download are the same, do nothing.
+    // If the new value is null, remove the attribute.
+    // Otherwise, the new value is non-null and different, so set the anchor's download attribute to the new value.
+    pipe(
+      this.__download !== previousNode.__download,
+      Bmatch(
+        () => {},
+        () => {
+          pipe(
+            this.__download,
+            OfromNullable,
+            Omatch(
+              () => {
+                anchor.removeAttribute("download")
+              },
+              (nextDownloadValue) => {
+                anchor.download === nextDownloadValue
+              },
+            ),
+          )
+        },
+      ),
+    )
+    return isUpdated
   }
 }
 
