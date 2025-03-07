5130cf10643332e01c3e395200ea92ba4c348243
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-07 09:21:37 -0600

    feat(editor-toolbar): create SelectedFile component and integrate it into Upload component
    The SelectedFile component is created to display the name of the selected file in the Upload component.
    The Upload component is updated to use the SelectedFile component to display the name of the selected file.
    This improves the user interface by providing a dedicated component for displaying the selected file name.

---
+++ packages/editor-toolbar/src/components/SelectedFile.tsx
@@ -0,0 +1,36 @@
+import { Box, Grid, Typography, makeStyles } from "@material-ui/core"
+import InsertDriveFileIconOutlined from "@material-ui/icons/InsertDriveFileOutlined"
+import { grey } from "@material-ui/core/colors"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    backgroundColor: grey[200],
+    borderRadius: "0.3125rem",
+  },
+}))
+
+type SelectedFileProperties = {
+  filename: string
+}
+
+/* Used for displaying the name of the selected file in the `Upload` component. */
+const SelectedFile = ({ filename }: SelectedFileProperties) => {
+  const classes = useStyles()
+  return (
+    <Grid item>
+      <Box className={classes.root}>
+        <Grid container spacing={1} alignItems="center">
+          <Grid item>
+            <InsertDriveFileIconOutlined />
+          </Grid>
+          <Grid item>
+            <Typography>{filename}</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Grid>
+  )
+}
+
+export { SelectedFile }
