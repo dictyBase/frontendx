7e3bad931d41666700d8f13dcc9b98634fa59884
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-10 00:04:53 -0500

    feat(UploadAsField): display validation errors in the helper text
    The TextField now displays validation errors, if any, in the helper text.
    The label has been changed to 'Upload Name' for clarity.
    This commit improves the user experience by providing feedback on validation errors.

--- packages/editor-toolbar/src/components/UploadAsField.tsx
+++ packages/editor-toolbar/src/components/UploadAsField.tsx
@@ -1,18 +1,36 @@
+import { pipe } from "fp-ts/function"
+import {
+  fromNullable as OfromNullable,
+  flatMap as OflatMap,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
 import { TextField } from "@material-ui/core"
-import { UseFormRegister } from "react-hook-form"
+import { UseFormRegister, FieldErrors } from "react-hook-form"
 
 type UploadAsFieldProperties = {
   register: UseFormRegister<{ uploadName: string }>
+  errors: FieldErrors<{ uploadName: string }>
 }
 
-const UploadAsField = ({ register }: UploadAsFieldProperties) => (
-  <TextField
-    {...register("uploadName")}
-    label="File Name"
-    fullWidth
-    variant="outlined"
-    helperText="The name of the file that will be downloaded"
-  />
-)
+const initialHelpText = "Specify the name you want to upload the file as"
+
+const UploadAsField = ({ register, errors }: UploadAsFieldProperties) => {
+  const helperText = pipe(
+    errors.uploadName,
+    OfromNullable,
+    OflatMap(({ message }) => OfromNullable(message)),
+    OgetOrElse(() => initialHelpText),
+  )
+  return (
+    <TextField
+      {...register("uploadName")}
+      error={!!errors.uploadName}
+      helperText={helperText}
+      label="Upload Name"
+      fullWidth
+      variant="outlined"
+    />
+  )
+}
 
 export { UploadAsField }
