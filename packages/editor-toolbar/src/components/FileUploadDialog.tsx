d5ef3136bd838b06774b311d4a89895b63e45516
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-10 00:05:28 -0500

    refactor(FileUploadDialog): simplify component and remove unused logic
    This commit refactors the FileUploadDialog component to simplify its logic and remove unused code. The changes include:
    
    - Removal of state variables for selected file, file error, and upload as file name.
    - Removal of the onFileChange and onSubmit functions.
    - Removal of the useLogto hook.
    - Moving the validation logic to a separate hook.
    - Passing the mutation function and loading state to the Upload component.
    
    These changes make the FileUploadDialog component more focused on its primary responsibility of displaying a dialog for file uploads. The upload logic is now handled by the Upload component, which makes the code more modular and easier to maintain.

--- packages/editor-toolbar/src/components/FileUploadDialog.tsx
+++ packages/editor-toolbar/src/components/FileUploadDialog.tsx
@@ -1,75 +1,20 @@
-import { useState } from "react"
 import { Dialog } from "@material-ui/core"
 import { useUploadFileMutation } from "dicty-graphql-schema"
-import { useLogto } from "@logto/react"
 import { useSetAtom } from "jotai"
 import { match, P } from "ts-pattern"
-import { pipe } from "fp-ts/function"
-import { head as Ahead } from "fp-ts/Array"
-import { mapLeft as EmapLeft } from "fp-ts/Either"
-import {
-  some,
-  none,
-  Option,
-  fromNullable as OfromNullable,
-  map as Omap,
-  flatMap as OflatMap,
-  getOrElse as OgetOrElse,
-} from "fp-ts/Option"
 import { uploadFileDialogOpenAtom } from "../context/atomConfigs"
-import {
-  getFileValidationError,
-  ErrorState,
-  isValidFile,
-} from "./helpers/fileUploadHelpers"
 import { InsertUrl } from "./InsertUrl"
 import { Upload } from "./Upload"
-import { createFileUploadFunction } from "./helpers/createUploadFileFunction"
+import { useValidateUploadName } from "./fileUploadHelpers"
 
 type FileUploadDialogProperties = {
   open: boolean
 }
 
 const FileUploadDialog = ({ open }: FileUploadDialogProperties) => {
-  const [selectedFile, setSelectedFile] = useState<Option<File>>(none)
-  const [fileError, setFileError] = useState<Option<ErrorState>>(none)
-  const fileName = pipe(
-    selectedFile,
-    Omap(({ name }) => name),
-  )
-  const [uploadAsFileName, setUploadAsFileName] = useState(
-    pipe(
-      fileName,
-      OgetOrElse(() => ""),
-    ),
-  )
-  const canSubmit = pipe(
-    selectedFile,
-    Omap(isValidFile),
-    OgetOrElse(() => false),
-  )
-
-  const { getAccessToken } = useLogto()
-  const [uploadFile, { data, loading, reset }] = useUploadFileMutation()
+  const [uploadFileMutation, { data, loading, reset }] = useUploadFileMutation()
 
   const setDialogDisplay = useSetAtom(uploadFileDialogOpenAtom)
-
-  const onFileChange: React.ChangeEventHandler<HTMLInputElement> = async ({
-    target: { files },
-  }) => {
-    reset()
-    // Get the file selected by the user.
-    const selected = pipe(
-      files,
-      OfromNullable,
-      Omap((someFiles) => [...someFiles]),
-      OflatMap(Ahead),
-    )
-    // Set the error state of the file.
-    pipe(selected, OflatMap(getFileValidationError), setFileError)
-    // Set the file state.
-    setSelectedFile(selected)
-  }
 
   const handleClose = () => {
     if (loading) return
@@ -77,24 +22,7 @@ const FileUploadDialog = ({ open }: FileUploadDialogProperties) => {
   }
 
   const handleClearForm = () => {
-    setSelectedFile(none)
-    setFileError(none)
     reset()
-  }
-
-  const onSubmit = async () => {
-    const uploadFunction = createFileUploadFunction(
-      selectedFile,
-      uploadAsFileName,
-      uploadFile,
-      getAccessToken,
-    )
-    pipe(
-      await uploadFunction(),
-      EmapLeft((error) => {
-        setFileError(some(error))
-      }),
-    )
   }
 
   return (
@@ -108,16 +36,7 @@ const FileUploadDialog = ({ open }: FileUploadDialogProperties) => {
           />
         ))
         .otherwise(() => (
-          <Upload
-            fileName={fileName}
-            fileError={fileError}
-            uploadAsName={uploadAsFileName}
-            setUploadAsName={setUploadAsFileName}
-            loading={loading}
-            canSubmit={canSubmit}
-            onFileChange={onFileChange}
-            onSubmit={onSubmit}
-          />
+          <Upload mutationFunction={uploadFileMutation} loading={loading} />
         ))}
     </Dialog>
   )
