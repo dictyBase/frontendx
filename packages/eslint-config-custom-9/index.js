6e21cc531094d9f2df2e98a5abaa52db2787bd88
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-14 10:09:46 -0500

    feat(eslint-config): add sonarjs, unicorn and typescript support to eslint
    This commit introduces several enhancements to the ESLint configuration:
    
    -   Integrates `eslint-plugin-sonarjs` to enforce SonarJS coding standards,
        improving code quality and maintainability.
    -   Adds `eslint-plugin-unicorn` with its recommended rules to promote
        consistent and modern JavaScript/TypeScript code.
    -   Configures ESLint to properly lint TypeScript files (`.ts`, `.tsx`) by
        specifying the appropriate parser and settings. This includes setting
        the import parser and resolver to handle TypeScript-specific syntax and
        module resolution.

--- packages/eslint-config-custom-9/index.js
+++ packages/eslint-config-custom-9/index.js
@@ -1,11 +1,30 @@
 // @ts-check
 import turbo from "eslint-config-turbo/flat"
 import github from "eslint-plugin-github"
+import unicorn from "eslint-plugin-unicorn"
+import sonarjs from "eslint-plugin-sonarjs"
+import typescriptParser from "@typescript-eslint/parser"
 
 export default [
   ...turbo,
   github.getFlatConfigs().recommended,
+  sonarjs.configs.recommended,
+  unicorn.configs.recommended,
   {
     files: ["src/**/*.ts", "src/**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
   },
 ]
