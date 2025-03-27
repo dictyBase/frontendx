8581646d827d789d6fd963e08b7f2ffeba20fd37
Kevin Tun <kevin.tun@northwestern.edu>
2025-03-27 10:50:05 -0500

    feat(playwright): add screenshot comparison threshold
    Adds `toHaveScreenshot` configuration to allow a 5% pixel difference.
    
    This change introduces a threshold for screenshot comparisons in
    Playwright tests. The `maxDiffPixelRatio` is set to 0.05, allowing
    for a 5% difference in pixels between the expected and actual
    screenshots. This helps to reduce false positives due to minor
    rendering differences across different environments.

--- apps/dicty-frontpage/playwright.config.ts
+++ apps/dicty-frontpage/playwright.config.ts
@@ -23,22 +23,23 @@ export default defineConfig({
   reporter: process.env.CI ? "github" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect: {
-    timeout: 60_000
+    timeout: 60_000,
+    toHaveScreenshot: { maxDiffPixelRatio: 0.05 },
   },
   /* Configure projects for major browsers */
   projects: [
-    //{
+    // {
     //  name: "chromium",
     //  use: { ...devices["Desktop Chrome"] },
-    //},
+    // },
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-    //{
+    // {
     //  name: "webkit",
     //  use: { ...devices["Desktop Safari"] },
-    //},
+    // },
 
     /* Test against mobile viewports. */
     // {
@@ -52,12 +53,12 @@ export default defineConfig({
 
     /* Test against branded browsers. */
     {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" },
     },
     {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
     },
   ],
 
@@ -70,7 +71,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL,
- 
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
