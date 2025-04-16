38e9f4e495389c66aea149b2b7758421e0161a4f
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-16 06:46:48 -0500

    feat(table-action-plugin): conditionally render table action menu based on editing state and selected cell
    The table action menu is now only rendered when the table is not in editing mode and a cell is selected. This prevents the menu from interfering with the editing process and improves the user experience. The logic was changed to use fp-ts library to improve readability.

--- packages/table-action-plugin/src/TableActionPlugin.tsx
+++ packages/table-action-plugin/src/TableActionPlugin.tsx
@@ -1,34 +1,59 @@
 import { createPortal } from "react-dom"
 import { useAtomValue } from "jotai"
+import { pipe } from "fp-ts/function"
+import { MonoidAll as BMonoidAll, match as Bmatch } from "fp-ts/boolean"
+import {
+  fromNullable as OfromNullable,
+  isSome as OisSome,
+  match as Omatch,
+} from "fp-ts/Option"
 import { selectedTableCellNode, tableActionMenuOpenAtom } from "./atomConfigs"
 import { usePositionMenuButton } from "./usePositionMenuButton"
 import { TableActionMenuButton } from "./TableActionMenuButton"
 import { TableActionMenu } from "./TableActionMenu"
 import { useSelectCurrentCell } from "./useSelectCurrentCell"
 
-const TableActionPlugin = () => {
+type TableActionPluginProperties = {
+  isEditing: boolean
+}
+
+/*
+ * This plugin decides whether to render a menu button in a table cell. It is also reponsible for rendering the Table Action Menu.
+ */
+const TableActionPlugin = ({ isEditing }: TableActionPluginProperties) => {
   const currentTableCellNode = useAtomValue(selectedTableCellNode)
   const isMenuOpen = useAtomValue(tableActionMenuOpenAtom)
   const menuButtonReference = usePositionMenuButton()
   useSelectCurrentCell()
 
-  if (currentTableCellNode)
-    return (
-      <>
-        {createPortal(
-          <TableActionMenuButton menuButtonReference={menuButtonReference} />,
-          document.body,
-        )}
-        {menuButtonReference.current ? (
-          <TableActionMenu
-            anchorElement={menuButtonReference.current}
-            isMenuOpen={isMenuOpen}
-          />
-        ) : undefined}
-      </>
-    )
-
-  return <></>
+  const isCellSelected = pipe(currentTableCellNode, OfromNullable, OisSome)
+  return pipe(
+    BMonoidAll.concat(isEditing, isCellSelected),
+    Bmatch(
+      () => <></>,
+      () => (
+        <>
+          {createPortal(
+            <TableActionMenuButton menuButtonReference={menuButtonReference} />,
+            document.body,
+          )}
+          {pipe(
+            menuButtonReference.current,
+            OfromNullable,
+            Omatch(
+              () => <></>,
+              (anchorElement) => (
+                <TableActionMenu
+                  anchorElement={anchorElement}
+                  isMenuOpen={isMenuOpen}
+                />
+              ),
+            ),
+          )}
+        </>
+      ),
+    ),
+  )
 }
 
 export { TableActionPlugin }
