81074873ec258537c9427b0e2134e0b3ab5da82f
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-16 12:17:56 -0500

    docs(useConfirmNavigation.ts): add documentation to the hook
    Adds documentation to the `useConfirmNavigation` hook to explain its purpose and functionality.

--- packages/hook/src/useConfirmNavigation.ts
+++ packages/hook/src/useConfirmNavigation.ts
@@ -1,5 +1,12 @@
 import { useEffect } from "react"
 
+/*
+ * Adds a `beforeunload` listener that is triggered when a user attempts to navigate away from the page.
+ * When the event fires, the handler causes a dialog to appear that confirms the user is about to leave the
+ * current page and lose any unsaved information.
+ *
+ * note: This does not include navigation that is handled by `react-router-dom`
+ */
 const useConfirmNavigation = () => {
   useEffect(() => {
     const handler = (event: BeforeUnloadEvent) => {
