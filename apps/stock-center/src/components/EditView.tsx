9ea4e9b011f5e62a1a7c3decf2259f657437e757
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-21 07:42:53 -0500

    feat(EditView.tsx): add autosave functionality to the editor
    This commit introduces autosave functionality to the editor in the EditView component. The autosave feature automatically saves the editor's content periodically, preventing data loss in case of unexpected events. A snackbar displays success or error messages to the user, providing feedback on the autosave process. The useAutoSave hook handles the autosave logic, and error messages are displayed using the Alert component from @material-ui/lab.

--- apps/stock-center/src/components/EditView.tsx
+++ apps/stock-center/src/components/EditView.tsx
@@ -1,13 +1,17 @@
-import { useNavigate } from "react-router-dom"
-import { Button } from "@material-ui/core"
+import { Snackbar } from "@material-ui/core"
 import PersonIcon from "@material-ui/icons/Person"
 import { ActionBar } from "@dictybase/ui-common"
 import { Editor } from "@dictybase/editor"
 import { useConfirmNavigation } from "@dictybase/hook"
-import { type ContentBySlugQuery } from "dicty-graphql-schema"
-import { UpdateButton } from "./UpdateButton"
-import { timeSince } from "../timeSince"
+import { ContentBySlugQuery } from "dicty-graphql-schema"
+import { useState } from "react"
+import { Alert } from "@material-ui/lab"
+import { match, P } from "ts-pattern"
+import { Option, some, none } from "fp-ts/Option"
 import { truncateEmail } from "../truncateEmail"
+import { timeSince } from "../timeSince"
+import { UpdateButton } from "./UpdateButton"
+import { useAutoSave } from "../hooks/useAutoSave"
 
 type EditActionBarProperties = {
   editedBy: string
@@ -21,10 +25,26 @@ const EditActionBar = ({
   updatedAt,
 }: EditActionBarProperties) => {
   useConfirmNavigation()
-  const navigate = useNavigate()
-  const handleCancel = async () => {
-    navigate("../editable", { relative: "path" })
+  const [isOpen, setIsOpen] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<Option<string>>(none)
+
+  const handleClose = () => {
+    setIsOpen(false)
+    setErrorMessage(none)
   }
+
+  useAutoSave({
+    contentId,
+    onError: (error) => {
+      setErrorMessage(some(error.message))
+      setIsOpen(true)
+    },
+    onSuccess: () => {
+      setErrorMessage(none)
+      setIsOpen(true)
+    },
+  })
+
   return (
     <ActionBar
       descriptionElement={
@@ -36,7 +56,15 @@ const EditActionBar = ({
         </>
       }>
       <UpdateButton contentId={contentId} />
-      <Button onClick={handleCancel}> Cancel </Button>
+      <Snackbar open={isOpen} onClose={handleClose} autoHideDuration={3000}>
+        {match(errorMessage)
+          .with(P.string, () => (
+            <Alert severity="error"> Could not autosave progress. </Alert>
+          ))
+          .otherwise(() => (
+            <Alert severity="success"> Work Saved. </Alert>
+          ))}
+      </Snackbar>
     </ActionBar>
   )
 }
