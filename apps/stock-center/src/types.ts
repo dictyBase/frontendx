cab1f16abc036195f0f00366027887fd8cadc51d
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-29 07:34:55 -0500

    feat(stock-center): add error handling types and constants
    Adds types and constants for handling errors in the stock center application.
    This includes an enum for different error types, a type for content errors,
    and constants for specific error messages. This will allow for more
    consistent and easier error handling throughout the application.

--- apps/stock-center/src/types.ts
+++ apps/stock-center/src/types.ts
@@ -62,7 +62,65 @@ type OrderState = {
   cartTotal: string
 }
 
+enum ErrorType {
+  MISSING_CONTENT_ID,
+  ACCESS_TOKEN_ERROR,
+  USER_INFO_ERROR,
+  CREATE_FAILURE,
+  FETCH_FAILURE,
+  UPDATE_FAILURE,
+  DELETE_FAILURE,
+}
+
+type ContentError = {
+  errorType: ErrorType
+  message: string
+}
+
+const missingContentIdError = {
+  errorType: ErrorType.MISSING_CONTENT_ID,
+  message: "Content ID missing",
+}
+
+const userInfoError = {
+  errorType: ErrorType.USER_INFO_ERROR,
+  message: "Could not get user info",
+}
+
+const accessTokenError = {
+  errorType: ErrorType.ACCESS_TOKEN_ERROR,
+  message: "Could not get access token",
+}
+
+const createFailureError = {
+  errorType: ErrorType.CREATE_FAILURE,
+  message: "Could not create content",
+}
+
+const fetchContentError = {
+  errorType: ErrorType.FETCH_FAILURE,
+  message: "Could not fetch content",
+}
+
+const updateFailureError = {
+  errorType: ErrorType.UPDATE_FAILURE,
+  message: "Could not update content",
+}
+
+const deleteFailureError = {
+  errorType: ErrorType.DELETE_FAILURE,
+  message: "Could not delete content",
+}
+
 export {
+  missingContentIdError,
+  userInfoError,
+  accessTokenError,
+  createFailureError,
+  updateFailureError,
+  fetchContentError,
+  deleteFailureError,
+  type ContentError,
   type StrainItem,
   type PlasmidItem,
   type CatalogItem,
