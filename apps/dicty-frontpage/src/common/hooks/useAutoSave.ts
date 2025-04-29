ac28025797dd3600b8473f6a1be28bfada91305e
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-28 23:32:03 -0500

    feat(useAutoSave.ts): add autosave functionality to editor
    This commit adds autosave functionality to the editor. The editor
    content is saved automatically every `saveInterval` milliseconds.
    The autosave function checks if the content has changed before saving.
    If the content has changed, it calls the `authorizedUpdateContent`
    mutation to save the content. If the save is successful, the
    `onSuccess` callback is called. If the save fails, the `onError`
    callback is called.
    The autosave interval is configurable via the `saveInterval` prop.
    The default save interval is 5 minutes.
    The `contentSlug` prop is used to fetch the content from the server.
    The `useContentBySlugLazyQuery` hook is used to fetch the content.
    The `isContentChanged` variable is used to check if the content has
    changed.
    The `OfromNullable`, `Omap`, `OflatMap`, and `OgetOrElse` functions
    from the `fp-ts/Option` module are used to safely access the content
    from the server.
    The `ContentError` type is imported from the `../constants/types` file.

--- apps/dicty-frontpage/src/common/hooks/useAutoSave.ts
+++ apps/dicty-frontpage/src/common/hooks/useAutoSave.ts
@@ -1,51 +1,77 @@
 import { useEffect } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { pipe } from "fp-ts/function"
-import { match as Ematch } from "fp-ts/Either"
 import {
-  useAuthorizedUpdateContent,
-  UpdateContentError,
-} from "./useAuthorizedUpdateContent"
+  fromNullable as OfromNullable,
+  map as Omap,
+  flatMap as OflatMap,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { match as Ematch } from "fp-ts/Either"
+import { useContentBySlugLazyQuery } from "dicty-graphql-schema"
+import { useAuthorizedUpdateContent } from "./useAuthorizedUpdateContent"
+import { ContentError } from "../constants/types"
 
 // 5 minutes in milliseconds
 const DEFAULT_SAVE_INTERVAL = 1000 * 60 * 5
 
 type useAutoSaveProperties = {
   contentId: string
-  onError: (error: UpdateContentError) => void
+  contentSlug: string
+  onError: (error: ContentError) => void
   onSuccess: () => void
   saveInterval?: number
 }
 const useAutoSave = ({
   contentId,
+  contentSlug,
   onError,
   onSuccess,
   saveInterval = DEFAULT_SAVE_INTERVAL,
 }: useAutoSaveProperties) => {
   const [editor] = useLexicalComposerContext()
   const authorizedUpdateContent = useAuthorizedUpdateContent(contentId)
+  const [getContentBySlug] = useContentBySlugLazyQuery({
+    variables: { slug: contentSlug },
+  })
 
   useEffect(() => {
     const saveTimeout = setInterval(async () => {
-      const contentValue = JSON.stringify(editor.getEditorState().toJSON())
-      const result = await authorizedUpdateContent(contentValue)
-      pipe(
-        result,
-        Ematch(
-          (error) => {
-            onError(error)
-          },
-          () => {
-            onSuccess()
-          },
-        ),
+      const editorContent = JSON.stringify(editor.getEditorState().toJSON())
+      const { data } = await getContentBySlug()
+      const isContentChanged = pipe(
+        data,
+        OfromNullable,
+        OflatMap(({ contentBySlug }) => OfromNullable(contentBySlug)),
+        Omap(({ content }) => content !== editorContent),
+        OgetOrElse(() => false),
       )
+      if (isContentChanged) {
+        pipe(
+          await authorizedUpdateContent(editorContent),
+          Ematch(
+            (error) => {
+              onError(error)
+            },
+            () => {
+              onSuccess()
+            },
+          ),
+        )
+      }
     }, saveInterval)
 
     return () => {
       clearInterval(saveTimeout)
     }
-  }, [authorizedUpdateContent, editor, onError, onSuccess, saveInterval])
+  }, [
+    authorizedUpdateContent,
+    editor,
+    getContentBySlug,
+    onError,
+    onSuccess,
+    saveInterval,
+  ])
 }
 
 export { useAutoSave }
