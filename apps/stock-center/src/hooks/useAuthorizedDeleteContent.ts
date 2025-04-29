fc150df9595adb347c2f804162d3102226f89e03
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-29 07:35:16 -0500

    feat(stock-center): move error types to separate file

--- apps/stock-center/src/hooks/useAuthorizedDeleteContent.ts
+++ apps/stock-center/src/hooks/useAuthorizedDeleteContent.ts
@@ -1,35 +1,33 @@
 import { pipe } from "fp-ts/function"
 import { useLogto } from "@logto/react"
 import { useDeleteContentMutation } from "dicty-graphql-schema"
+import { Option } from "fp-ts/Option"
 import {
   Do as TEDo,
   bind as TEbind,
   tryCatch as TEtryCatch,
   map as TEmap,
+  fromOption as TEfromOption,
 } from "fp-ts/TaskEither"
+import {
+  missingContentIdError,
+  accessTokenError,
+  deleteFailureError,
+} from "../types"
 
-enum ErrorType {
-  ACCESS_TOKEN_ERROR,
-  DELETE_FAILURE,
-}
-
-const accessTokenError = {
-  errorType: ErrorType.ACCESS_TOKEN_ERROR,
-  message: "Could not get access token",
-}
-
-const deleteFailureError = {
-  errorType: ErrorType.DELETE_FAILURE,
-  message: "Could not delete content",
-}
-
-const useAuthorizedDeleteContent = (contentId: string) => {
+const useAuthorizedDeleteContent = (contentId: Option<string>) => {
   const { getAccessToken } = useLogto()
   const [deleteContent] = useDeleteContentMutation()
 
   return () => {
     const task = pipe(
       TEDo,
+      TEbind("id", () =>
+        pipe(
+          contentId,
+          TEfromOption(() => missingContentIdError),
+        ),
+      ),
       TEbind("token", () =>
         TEtryCatch(
           () =>
@@ -37,12 +35,12 @@ const useAuthorizedDeleteContent = (contentId: string) => {
           () => accessTokenError,
         ),
       ),
-      TEbind("deleted", ({ token }) =>
+      TEbind("deleted", ({ id, token }) =>
         TEtryCatch(
           () =>
             deleteContent({
               variables: {
-                id: contentId,
+                id,
               },
               context: { headers: { Authorization: `Bearer ${token}` } },
             }),
