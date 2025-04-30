4746c4d311e3408958a98e8a873df2597b69d698
Kevin Tun <kevin.tun@northwestern.edu>
2025-04-30 12:36:27 -0500

    refactor(useAutoSave.ts): remove autosave interval and use editor update listener to trigger save
    The autosave functionality is refactored to use the editor's update listener instead of a fixed interval. This change improves the efficiency of the autosave feature by triggering a save only when the content has changed, rather than at fixed intervals. It removes the need to fetch the content by slug and compare it with the editor content, which reduces unnecessary API calls. The save is triggered only when the editor state changes.

--- apps/dicty-frontpage/src/common/hooks/useAutoSave.ts
+++ apps/dicty-frontpage/src/common/hooks/useAutoSave.ts
@@ -1,77 +1,46 @@
 import { useEffect } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { pipe } from "fp-ts/function"
-import {
-  fromNullable as OfromNullable,
-  map as Omap,
-  flatMap as OflatMap,
-  getOrElse as OgetOrElse,
-} from "fp-ts/Option"
 import { match as Ematch } from "fp-ts/Either"
-import { useContentBySlugLazyQuery } from "dicty-graphql-schema"
 import { useAuthorizedUpdateContent } from "./useAuthorizedUpdateContent"
 import { ContentError } from "../constants/types"
-
-// 5 minutes in milliseconds
-const DEFAULT_SAVE_INTERVAL = 1000 * 60 * 5
 
 type useAutoSaveProperties = {
   contentId: string
   contentSlug: string
   onError: (error: ContentError) => void
   onSuccess: () => void
-  saveInterval?: number
 }
+
 const useAutoSave = ({
   contentId,
-  contentSlug,
   onError,
   onSuccess,
-  saveInterval = DEFAULT_SAVE_INTERVAL,
 }: useAutoSaveProperties) => {
   const [editor] = useLexicalComposerContext()
   const authorizedUpdateContent = useAuthorizedUpdateContent(contentId)
-  const [getContentBySlug] = useContentBySlugLazyQuery({
-    variables: { slug: contentSlug },
-  })
-
-  useEffect(() => {
-    const saveTimeout = setInterval(async () => {
-      const editorContent = JSON.stringify(editor.getEditorState().toJSON())
-      const { data } = await getContentBySlug()
-      const isContentChanged = pipe(
-        data,
-        OfromNullable,
-        OflatMap(({ contentBySlug }) => OfromNullable(contentBySlug)),
-        Omap(({ content }) => content !== editorContent),
-        OgetOrElse(() => false),
-      )
-      if (isContentChanged) {
-        pipe(
-          await authorizedUpdateContent(editorContent),
-          Ematch(
-            (error) => {
-              onError(error)
-            },
-            () => {
-              onSuccess()
-            },
-          ),
-        )
-      }
-    }, saveInterval)
-
-    return () => {
-      clearInterval(saveTimeout)
-    }
-  }, [
-    authorizedUpdateContent,
-    editor,
-    getContentBySlug,
-    onError,
-    onSuccess,
-    saveInterval,
-  ])
+  useEffect(
+    () =>
+      editor.registerUpdateListener(
+        async ({ editorState, prevEditorState }) => {
+          const previousEditorContent = JSON.stringify(prevEditorState.toJSON())
+          const editorContent = JSON.stringify(editorState.toJSON())
+          if (previousEditorContent === editorContent) return
+          pipe(
+            await authorizedUpdateContent(editorContent),
+            Ematch(
+              (error) => {
+                onError(error)
+              },
+              () => {
+                onSuccess()
+              },
+            ),
+          )
+        },
+      ),
+    [authorizedUpdateContent, editor, onError, onSuccess],
+  )
 }
 
 export { useAutoSave }
