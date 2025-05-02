394841724cf4f90278dbbbccfd3753b8285503ae
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-02 11:23:13 -0500

    feat(ui-common): implement ProgressSaved component with fade-out animation
    This commit introduces a new `ProgressSaved` component that displays
    "Saved" text alongside a cloud done icon. The component includes a
    fade-out animation to provide visual feedback to the user. The
    component uses Material-UI's `Grid`, `Typography`, and `makeStyles`
    for styling and layout. A fadeOut animation is added to improve UX.

--- packages/ui-common/src/ProgressSaved.tsx
+++ packages/ui-common/src/ProgressSaved.tsx
@@ -1,5 +1,33 @@
 import CloudDoneIcon from "@material-ui/icons/CloudDone"
+import { Grid, Typography, makeStyles } from "@material-ui/core"
 
-const ProgressSaved = () => <CloudDoneIcon />
+const useStyles = makeStyles({
+  fadeOut: {
+    animation: "$fadeAway 0.25s forwards",
+    animationDelay: "0.3s",
+  },
+  "@keyframes fadeAway": {
+    "0%": {
+      opacity: 1,
+    },
+    "100%": {
+      opacity: 0,
+    },
+  },
+})
+
+const ProgressSaved = () => {
+  const { fadeOut } = useStyles()
+  return (
+    <Grid alignItems="center" container spacing={1} wrap="nowrap">
+      <Grid item>
+        <Typography className={fadeOut}> Saved </Typography>
+      </Grid>
+      <Grid item>
+        <CloudDoneIcon />
+      </Grid>
+    </Grid>
+  )
+}
 
 export { ProgressSaved }
