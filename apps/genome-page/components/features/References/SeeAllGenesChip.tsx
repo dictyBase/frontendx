a539f2d9db651c3fde3856ebc517cdd275959ca2
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-05 11:05:06 -0500

    feat(genome-page): create SeeAllGenesChip component
    This commit introduces a new `SeeAllGenesChip` component.
    The component is a clickable chip that displays the label "See all".
    It is used to navigate to a page containing all genes.
    The chip uses the secondary color and has a small size.
    It also has a margin of 0px 5px 5px 0px.

---
+++ apps/genome-page/components/features/References/SeeAllGenesChip.tsx
@@ -0,0 +1,14 @@
+import { Chip } from "@material-ui/core"
+
+const SeeAllGenesChip = () => (
+  <Chip
+    clickable
+    key="see-all"
+    label="See all"
+    size="small"
+    color="secondary"
+    style={{ margin: "0px 5px 5px 0px" }}
+  />
+)
+
+export { SeeAllGenesChip }
