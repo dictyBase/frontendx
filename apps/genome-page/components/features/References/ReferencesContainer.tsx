2c19bccad0b79bcc4092f216d67934779b5a384e
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-05 07:59:59 -0500

    refactor(ReferencesContainer.tsx): remove Layout component
    The Layout component was removed from the ReferencesContainer component to simplify the component and improve reusability. The Layout component is not needed in this component, as the layout is already handled by the parent component.

--- apps/genome-page/components/features/References/ReferencesContainer.tsx
+++ apps/genome-page/components/features/References/ReferencesContainer.tsx
@@ -1,7 +1,5 @@
 import Typography from "@material-ui/core/Typography"
-import { Layout } from "components/layout/Layout"
 import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
-import { useRouter } from "next/router"
 import { ReferencesDataTable } from "./ReferencesDataTable"
 
 interface ReferencesContainerProperties {
@@ -12,20 +10,10 @@ interface ReferencesContainerProperties {
 
 const ReferencesContainer = ({
   publications,
-}: ReferencesContainerProperties) => {
-  const { query } = useRouter()
-  const geneId = query.id as string
-
-  return (
-    <Layout
-      gene={geneId}
-      title={`References for ${geneId}`}
-      description={`Gene references for ${geneId}`}>
-      <Typography component="div">
-        <ReferencesDataTable publications={publications} />
-      </Typography>
-    </Layout>
-  )
-}
+}: ReferencesContainerProperties) => (
+  <Typography component="div">
+    <ReferencesDataTable publications={publications} />
+  </Typography>
+)
 
 export { ReferencesContainer }
