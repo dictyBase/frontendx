a1e98e2856152da4a0f16f6c208fb7b5aa1fd9e4
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-06 10:54:06 -0500

    feat(genome-page): add MentionedGenes component and integrate with publication selection
    The component displays a list of genes mentioned in a selected publication. The component is displayed when a publication is selected, otherwise the ReferencesDataTable is displayed.

--- apps/genome-page/components/features/References/ReferencesContainer.tsx
+++ apps/genome-page/components/features/References/ReferencesContainer.tsx
@@ -1,6 +1,11 @@
 import Typography from "@material-ui/core/Typography"
 import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
+import { pipe } from "fp-ts/function"
+import { match as Omatch } from "fp-ts/Option"
+import { useAtomValue } from "jotai"
 import { ReferencesDataTable } from "./ReferencesDataTable"
+import { MentionedGenes } from "./MentionedGenes"
+import { selectedPublication } from "./state"
 
 interface ReferencesContainerProperties {
   publications: NonNullable<
@@ -10,10 +15,19 @@ interface ReferencesContainerProperties {
 
 const ReferencesContainer = ({
   publications,
-}: ReferencesContainerProperties) => (
-  <Typography component="div">
-    <ReferencesDataTable publications={publications} />
-  </Typography>
-)
+}: ReferencesContainerProperties) => {
+  const publication = useAtomValue(selectedPublication)
+  return pipe(
+    publication,
+    Omatch(
+      () => (
+        <Typography component="div">
+          <ReferencesDataTable publications={publications} />
+        </Typography>
+      ),
+      (p) => <MentionedGenes publication={p} />,
+    ),
+  )
+}
 
 export { ReferencesContainer }
