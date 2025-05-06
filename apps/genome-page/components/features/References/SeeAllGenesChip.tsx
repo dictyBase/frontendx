e1690ad49ce845b540f300de5d7133bd45aaf471
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-06 10:53:44 -0500

    feat(SeeAllGenesChip.tsx): make chip clickable and set publication atom on click
    This change makes the "See all" chip clickable and updates the
    selectedPublication atom with the corresponding publication data when
    clicked. This allows users to view all genes associated with a
    particular publication.

--- apps/genome-page/components/features/References/SeeAllGenesChip.tsx
+++ apps/genome-page/components/features/References/SeeAllGenesChip.tsx
@@ -1,14 +1,29 @@
 import { Chip } from "@material-ui/core"
+import { useSetAtom } from "jotai"
+import { some } from "fp-ts/Option"
+import { PublicationWithGene } from "dicty-graphql-schema"
+import { selectedPublication } from "./state"
 
-const SeeAllGenesChip = () => (
-  <Chip
-    clickable
-    key="see-all"
-    label="See all"
-    size="small"
-    color="secondary"
-    style={{ margin: "0px 5px 5px 0px" }}
-  />
-)
+type SeeAllGenesChipProperties = {
+  publication: Pick<PublicationWithGene, "id" | "related_genes">
+}
+
+const SeeAllGenesChip = ({ publication }: SeeAllGenesChipProperties) => {
+  const setPublication = useSetAtom(selectedPublication)
+  const handleClick = () => {
+    setPublication(some(publication))
+  }
+  return (
+    <Chip
+      clickable
+      key="see-all"
+      label="See all"
+      size="small"
+      color="secondary"
+      onClick={handleClick}
+      style={{ margin: "0px 5px 5px 0px" }}
+    />
+  )
+}
 
 export { SeeAllGenesChip }
