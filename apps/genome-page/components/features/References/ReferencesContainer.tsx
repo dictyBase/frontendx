0d061d44cfbb9e8b08a5c2c804e5c8174df422e8
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-07 11:30:18 -0500

    feat(ReferencesContainer.tsx): remove MentionedGenes component and selectedPublication state
    This commit removes the MentionedGenes component and the selectedPublication state. The ReferencesContainer component now always renders the ReferencesDataTable component. This simplifies the component and removes unused functionality.

--- apps/genome-page/components/features/References/ReferencesContainer.tsx
+++ apps/genome-page/components/features/References/ReferencesContainer.tsx
@@ -1,11 +1,6 @@
 import Typography from "@material-ui/core/Typography"
 import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
-import { pipe } from "fp-ts/function"
-import { match as Omatch } from "fp-ts/Option"
-import { useAtomValue } from "jotai"
 import { ReferencesDataTable } from "./ReferencesDataTable"
-import { MentionedGenes } from "./MentionedGenes"
-import { selectedPublication } from "./state"
 
 interface ReferencesContainerProperties {
   publications: NonNullable<
@@ -16,17 +11,10 @@ interface ReferencesContainerProperties {
 const ReferencesContainer = ({
   publications,
 }: ReferencesContainerProperties) => {
-  const publication = useAtomValue(selectedPublication)
-  return pipe(
-    publication,
-    Omatch(
-      () => (
-        <Typography component="div">
-          <ReferencesDataTable publications={publications} />
-        </Typography>
-      ),
-      (p) => <MentionedGenes publication={p} />,
-    ),
+  return (
+    <Typography component="div">
+      <ReferencesDataTable publications={publications} />
+    </Typography>
   )
 }
 
