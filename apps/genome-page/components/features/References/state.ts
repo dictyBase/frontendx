66511b75526fcb1e2115ba7041e8910a0ae05ad9
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-07 11:25:03 -0500

    feat(genome-page): remove unused state file for references
    The state file for references was not being used, so it has been removed to reduce the bundle size and improve the overall performance of the application.

--- apps/genome-page/components/features/References/state.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai"
-import { PublicationWithGene } from "dicty-graphql-schema"
-import { Option, none } from "fp-ts/Option"
-
-type SelectedPublication = Pick<PublicationWithGene, "id" | "title" | "related_genes">
-const selectedPublication = atom<Option<SelectedPublication>>(none)
-
-export { type SelectedPublication, selectedPublication }
