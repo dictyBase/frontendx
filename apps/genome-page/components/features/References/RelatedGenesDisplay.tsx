e44084e1e03e0193c23d74edc762be5ffa22504a
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-08 10:58:33 -0500

    feat(genome-page): display related genes as clickable chips
    This commit introduces a new component that displays related genes as clickable chips.
    The chips are displayed in a table, with each row containing a maximum of 7 chips.
    Clicking on a chip navigates the user to the corresponding gene page.
    The component uses Material-UI for styling and fp-ts for functional programming.
    The component is scrollable if the number of related genes exceeds the available space.
    The component is styled to be visually appealing and user-friendly.

--- apps/genome-page/components/features/References/RelatedGenesDisplay.tsx
+++ apps/genome-page/components/features/References/RelatedGenesDisplay.tsx
@@ -1,11 +1,13 @@
+import { useRouter } from "next/router"
 import {
   makeStyles,
-  Grid,
+  Chip,
   TableContainer,
   TableBody,
   TableRow,
   TableCell,
 } from "@material-ui/core"
+import { blueGrey, lightBlue } from "@material-ui/core/colors"
 import { pipe } from "fp-ts/function"
 import {
   map as Amap,
@@ -14,42 +16,63 @@ import {
 } from "fp-ts/Array"
 import { Gene } from "dicty-graphql-schema"
 
-type MentionedGenesProperties = { genes: Array<Gene> }
+type RelatedGenesProperties = { genes: Array<Gene> }
 
-const useStyles = makeStyles({
-  body: {
-    boxShadow: "inset -5px -5px black",
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "fit-content",
+    maxHeight: "30rem",
     borderWidth: "1px",
     borderStyle: "solid",
-    borderColor: "red",
-    borderCollapse: "collapse",
+    borderColor: "rgba(50, 50, 50, 0.5)",
+    boxShadow: `inset 2px 2px 2px ${blueGrey[300]}`,
+    borderRadius: "0.5rem",
+    backgroundColor: lightBlue[50],
+    overflowY: "scroll",
   },
-})
+  cell: {
+    borderBottom: "unset",
+    padding: "0.75rem",
+  },
+  chip: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: `2px 2px ${blueGrey[300]}`,
+    fontSize: "1rem",
+    width: "100%",
+  },
+}))
 
-const RelatedGenesDisplay = ({ genes }: MentionedGenesProperties) => {
+const RelatedGenesDisplay = ({ genes }: RelatedGenesProperties) => {
+  const router = useRouter()
   const classes = useStyles()
   return (
-    <TableContainer>
-      <Grid container justifyContent="center">
-        <Grid item>
-          <TableBody className={classes.body}>
-            {pipe(
-              genes,
-              AchunksOf(12),
-              AmapWithIndex((index, genesChunk) => (
-                <TableRow key={index}>
-                  {pipe(
-                    genesChunk,
-                    Amap((gene) => (
-                      <TableCell key={gene.id}>{gene.name}</TableCell>
-                    )),
-                  )}
-                </TableRow>
-              )),
-            )}
-          </TableBody>
-        </Grid>
-      </Grid>
+    <TableContainer className={classes.container}>
+      <TableBody>
+        {pipe(
+          genes,
+          AchunksOf(7),
+          AmapWithIndex((index, genesChunk) => (
+            <TableRow key={index}>
+              {pipe(
+                genesChunk,
+                Amap((gene) => (
+                  <TableCell key={gene.id} className={classes.cell}>
+                    <Chip
+                      clickable
+                      onClick={() => router.push(`/${gene.name}`)}
+                      key={gene.id}
+                      label={gene.name}
+                      size="medium"
+                      variant="outlined"
+                      className={classes.chip}
+                    />
+                  </TableCell>
+                )),
+              )}
+            </TableRow>
+          )),
+        )}
+      </TableBody>
     </TableContainer>
   )
 }
