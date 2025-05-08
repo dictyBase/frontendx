74ca8b025a38871f43e1f86cfc5cd919ef7350ae
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-08 10:58:38 -0500

    feat(genome-page): create RelatedGenesHeader component to display publication information
    This commit introduces the `RelatedGenesHeader` component, which is responsible for displaying information about a selected publication, including its title, journal, pages, and authors. This component enhances the user interface by providing context for the related genes being displayed. It uses Material-UI components for styling and layout, and `fp-ts` for functional data manipulation. The component receives a `publication` object as a prop, extracts the relevant information, and renders it in a structured format.

---
+++ apps/genome-page/components/features/References/RelatedGenesHeader.tsx
@@ -0,0 +1,51 @@
+import { Grid, Typography, makeStyles } from "@material-ui/core"
+import { pipe } from "fp-ts/function"
+import { map as Amap } from "fp-ts/Array"
+import { grey } from "@material-ui/core/colors"
+import { commaSeparateWithAnd } from "common/utils/strings"
+import { SelectedPublication } from "common/@types"
+
+type RelatedGenesHeaderProperties = {
+  publication: SelectedPublication
+}
+
+const useStyles = makeStyles({
+  text: {
+    color: grey[800],
+  },
+  main: {
+    marginBottom: "0.25rem",
+  },
+})
+
+const RelatedGenesHeader = ({
+  publication: { title, authors, journal, pages },
+}: RelatedGenesHeaderProperties) => {
+  const classes = useStyles()
+  return (
+    <Grid container direction="column" justifyContent="center">
+      <Grid item>
+        <Typography variant="h4" className={classes.text}>
+          Genes mentioned in &nbsp;
+        </Typography>
+        <Grid item />
+        <Grid item className={classes.main}>
+          <Typography variant="h2">
+            {title} <i>{journal}</i>, {pages}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1">
+            {pipe(
+              authors,
+              Amap((author) => author.last_name),
+              commaSeparateWithAnd,
+            )}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export { RelatedGenesHeader }
