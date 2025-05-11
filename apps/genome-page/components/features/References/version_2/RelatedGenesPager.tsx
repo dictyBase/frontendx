4a6ce8b41ba9ad2cbc3dc5df1f70b230daf12cfd
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-11 13:50:26 -0500

    feat(RelatedGenesPager.tsx): display none when no genes are found
    The component now displays "none" when no genes are found for the current page.
    The component was refactored to use Option.match to handle the case where no genes are found for the current page.
    This improves the user experience by providing feedback when no genes are found.
    A container class was added to the component to set the width to 100%.

--- apps/genome-page/components/features/References/version_2/RelatedGenesPager.tsx
+++ apps/genome-page/components/features/References/version_2/RelatedGenesPager.tsx
@@ -1,14 +1,27 @@
 import { ChangeEvent, ChangeEventHandler, useState } from "react"
 import { pipe } from "fp-ts/function"
-import { startsWith as SstartsWith, includes as Sincludes } from "fp-ts/string"
-import { chunksOf as AchunksOf, filter as Afilter } from "fp-ts/Array"
-import { Grid } from "@material-ui/core"
+import { match } from "ts-pattern"
+import { includes as Sincludes } from "fp-ts/string"
+import {
+  chunksOf as AchunksOf,
+  filter as Afilter,
+  isEmpty as AisEmpty,
+  lookup as Alookup,
+} from "fp-ts/Array"
+import { match as Omatch } from "fp-ts/Option"
+import { Grid, makeStyles } from "@material-ui/core"
 import Pagination from "@material-ui/lab/Pagination"
 import { Gene } from "dicty-graphql-schema"
 import { RelatedGenesDisplay } from "./RelatedGenesDisplay"
 import { RelatedGenesControls } from "./RelatedGenesControls"
 
 const GENES_PER_PAGE = 16
+
+const useStyles = makeStyles({
+  container: {
+    width: "100%",
+  },
+})
 
 type Properties = { genes: Array<Gene> }
 /**
@@ -18,26 +31,43 @@ type Properties = { genes: Array<Gene> }
 const RelatedGenesPager = ({ genes }: Properties) => {
   const [page, setPage] = useState(1)
   const [filter, setFilter] = useState("")
-  const filteredGenes = pipe(genes, Afilter(({ name }) => Sincludes(filter)(name)))
+  const filteredGenes = pipe(
+    genes,
+    Afilter(({ name }) => Sincludes(filter)(name)),
+  )
   const geneChunks = pipe(filteredGenes, AchunksOf(GENES_PER_PAGE))
   const pageCount = geneChunks.length
-  const currentChunk = geneChunks[page - 1]
 
   const handlePageChange = (_: ChangeEvent<unknown>, pageNumber: number) => {
     setPage(pageNumber)
   }
 
-  const handleFilterChange: ChangeEventHandler<HTMLInputElement> = ({ currentTarget: { value }}) => {
+  const handleFilterChange: ChangeEventHandler<HTMLInputElement> = ({
+    currentTarget: { value },
+  }) => {
     setFilter(value)
   }
 
+  const classes = useStyles()
   return (
-    <Grid container direction="column" spacing={2}>
+    <Grid
+      container
+      direction="column"
+      spacing={2}
+      className={classes.container}
+    >
       <Grid item>
-        <RelatedGenesControls filter={filter} onChange={handleFilterChange}/>
+        <RelatedGenesControls filter={filter} onChange={handleFilterChange} />
       </Grid>
       <Grid item>
-        <RelatedGenesDisplay genes={currentChunk} />
+        {pipe(
+          geneChunks,
+          Alookup(page - 1),
+          Omatch(
+            () => <>none</>,
+            (genes) => <RelatedGenesDisplay genes={genes} />,
+          ),
+        )}
       </Grid>
       <Grid item>
         <Pagination count={pageCount} page={page} onChange={handlePageChange} />
