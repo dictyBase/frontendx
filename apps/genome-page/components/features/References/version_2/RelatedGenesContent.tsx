244e525ab7aacab0e02e2fc13249167130ec17b5
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-11 13:50:19 -0500

    refactor(RelatedGenesContent.tsx): remove makeStyles and unused CSS class
    The makeStyles and the genes CSS class are not needed anymore, so they
    are removed. The Grid component now uses the alignItems prop to
    achieve the same layout.

--- apps/genome-page/components/features/References/version_2/RelatedGenesContent.tsx
+++ apps/genome-page/components/features/References/version_2/RelatedGenesContent.tsx
@@ -1,4 +1,4 @@
-import { Grid, makeStyles } from "@material-ui/core"
+import { Grid } from "@material-ui/core"
 import { SelectedPublication } from "common/@types"
 import { RelatedGenesHeader } from "./RelatedGenesHeader"
 import { RelatedGenesPager } from "./RelatedGenesPager"
@@ -7,22 +7,15 @@ type RelatedGenesContentProperties = {
   publication: SelectedPublication
 }
 
-const useStyles = makeStyles({
-  genes: {
-    alignSelf: "center",
-  },
-})
-
 const RelatedGenesContent = ({
   publication,
 }: RelatedGenesContentProperties) => {
-  const classes = useStyles()
   return (
-    <Grid container direction="column" spacing={3}>
+    <Grid container direction="column" alignItems="stretch" spacing={3}>
       <Grid item>
         <RelatedGenesHeader publication={publication} />
       </Grid>
-      <Grid item className={classes.genes}>
+      <Grid item>
         <RelatedGenesPager genes={publication.related_genes} />
       </Grid>
     </Grid>
