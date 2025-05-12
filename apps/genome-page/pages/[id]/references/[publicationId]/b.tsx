1fbf239426c926b856b2ebb38afbc4098fab63c3
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-12 07:08:56 -0500

    feat(genome-page): create related genes page
    This commit introduces a new page that displays the related genes of a publication associated with a given gene id.
    The page fetches data using the useListPublicationsWithGeneQuery hook from the dicty-graphql-schema package.
    It uses fp-ts for functional programming constructs such as pipe, findFirst, and match.
    The page handles different states such as loading, no data, and errors using the ts-pattern library.
    The page is wrapped in a Layout component that provides the basic layout and metadata for the page.

---
+++ apps/genome-page/pages/[id]/references/[publicationId]/b.tsx
@@ -0,0 +1,70 @@
+import { pipe } from "fp-ts/function"
+import { findFirst as AfindFirst } from "fp-ts/Array"
+import { match as Omatch } from "fp-ts/Option"
+import { RelatedGenesContainer } from "components/features/References/version_2/RelatedGenesContainer"
+import { GraphQLErrorPage } from "@dictybase/ui-common"
+import { Layout, TabValues } from "components/layout/Layout"
+import { NoDataDisplay } from "components/NoDataDisplay"
+import { Loader } from "components/Loader"
+import { useRouter } from "next/router"
+import { useListPublicationsWithGeneQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+
+/*
+    Renders the related genes of a publication associated with a given gene id
+*/
+const RelatedGenesWrapper = () => {
+  const { query } = useRouter()
+  const geneId = query.id as string
+  const publicationId = query.publicationId as string
+  const result = useListPublicationsWithGeneQuery({
+    variables: {
+      gene: geneId,
+    },
+    fetchPolicy: "cache-and-network",
+    nextFetchPolicy: "cache-only",
+  })
+  return (
+    <Layout
+      tabValue={TabValues.REFERENCES}
+      gene={geneId}
+      title={`References for ${geneId}`}
+      description={`Gene references for ${geneId}`}>
+      {match(result)
+        .with(
+          {
+            data: {
+              listPublicationsWithGene: P.select(P.array({ id: P.string })),
+            },
+          },
+          (publications) =>
+            pipe(
+              publications,
+              AfindFirst((publication) => publication.id === publicationId),
+              Omatch(
+                () => <></>,
+                (p) => <RelatedGenesContainer publication={p} />,
+              ),
+            ),
+        )
+        .with({ loading: true }, () => <Loader />)
+        .with(
+          {
+            data: {
+              listPublicationsWithGene: [],
+            },
+          },
+          () => <NoDataDisplay query="References" geneId={geneId} />,
+        )
+        .with({ error: P.select(P.not(undefined)) }, (error) => (
+          <GraphQLErrorPage error={error} />
+        ))
+        .otherwise(() => (
+          <> This message should not appear. </>
+        ))}
+    </Layout>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default RelatedGenesWrapper
