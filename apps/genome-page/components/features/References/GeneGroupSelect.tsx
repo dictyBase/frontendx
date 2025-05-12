9e500b7c2a780b7ed77cc224611ef690aadc7af8
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-12 13:36:59 -0500

    feat(genome-page): create GeneGroupSelect component
    This commit introduces a new component, GeneGroupSelect, which allows users to select a gene group from a dropdown menu. The component includes options for "All Gene Types", "Named Genes", and "Unnamed Genes". It uses Material-UI components for styling and functionality. This component will be used to filter genes based on their group in the references section of the genome page.

---
+++ apps/genome-page/components/features/References/GeneGroupSelect.tsx
@@ -0,0 +1,31 @@
+import { ChangeEventHandler } from "react"
+import { Select, MenuItem, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+  select: {
+    backgroundColor: theme.palette.background.paper,
+  },
+}))
+
+enum GeneGroups {
+  ALL,
+  NAMED,
+  UNNAMED,
+}
+
+type GeneGroupSelectProperties = {
+  value: GeneGroups
+  onChange: ChangeEventHandler<{ value: unknown; }>
+}
+
+const GeneGroupSelect = ({ value, onChange }: GeneGroupSelectProperties) => {
+  const classes = useStyles()
+  return (
+    <Select value={value} onChange={onChange} variant="outlined" className={classes.select}>
+      <MenuItem value={GeneGroups.ALL}> All Gene Types </MenuItem>
+      <MenuItem value={GeneGroups.NAMED}> Named Genes </MenuItem>
+      <MenuItem value={GeneGroups.UNNAMED}> Unnamed Genes </MenuItem>
+    </Select>
+  )
+}
+export { GeneGroupSelect, GeneGroups }
