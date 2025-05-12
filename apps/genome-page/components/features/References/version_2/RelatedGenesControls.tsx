f4740b960a902f219229a2e2cc42766a3d15ba11
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-12 07:40:13 -0500

    feat(genome-page): improve RelatedGenesControls component UI
    The UI of the RelatedGenesControls component is improved by adding a
    background color, a border radius, and a "Showing 72 Genes" text. The
    component is also refactored to use Grid for layout.

--- apps/genome-page/components/features/References/version_2/RelatedGenesControls.tsx
+++ apps/genome-page/components/features/References/version_2/RelatedGenesControls.tsx
@@ -1,49 +1,64 @@
 import { ChangeEventHandler } from "react"
 import {
-  Box,
+  Grid,
   Paper,
   TextField,
+  Typography,
   IconButton,
   makeStyles,
 } from "@material-ui/core"
 import { teal } from "@material-ui/core/colors"
-import FilterListIcon from '@material-ui/icons/FilterList';
+import FilterListIcon from "@material-ui/icons/FilterList"
 
 const useStyles = makeStyles((theme) => ({
   container: {
     padding: "1rem",
+  },
+  surface: {
     backgroundColor: teal[50],
+    borderRadius: "1rem",
   },
   textField: {
-    backgroundColor: theme.palette.background.paper
+    backgroundColor: theme.palette.background.paper,
+  },
+  text: {
+    color: teal[900]
   }
 }))
 
 type RelatedGenesControlsProperties = {
- filter: string
- onChange: ChangeEventHandler<HTMLInputElement>
+  filter: string
+  onChange: ChangeEventHandler<HTMLInputElement>
 }
 
-const RelatedGenesControls = ({ filter, onChange }: RelatedGenesControlsProperties) => {
+const RelatedGenesControls = ({
+  filter,
+  onChange,
+}: RelatedGenesControlsProperties) => {
   const classes = useStyles()
   return (
-    <Paper>
-      <Box className={classes.container}>
-        <TextField
-          value={filter}
-          placeholder="Filter Genes"
-          variant="outlined"
-          InputProps={{
-            startAdornment: (
-              <IconButton disabled>
-                <FilterListIcon />
-              </IconButton>
-            ),
-          }}
-          onChange={onChange}
-          className={classes.textField}
-        />
-      </Box>
+    <Paper className={classes.surface}>
+      <Grid container alignItems="center" spacing={2} className={classes.container}>
+        <Grid item>
+          <TextField
+            value={filter}
+            placeholder="Filter Genes"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <IconButton disabled>
+                  <FilterListIcon />
+                </IconButton>
+              ),
+            }}
+            onChange={onChange}
+            className={classes.textField}
+          />
+        </Grid>
+        <Grid item>
+          <Typography className={classes.text}> Showing 72 Genes </Typography>
+        </Grid>
+      </Grid>
     </Paper>
   )
 }
