99dae5a04a699aeb94b28ff3395cfadb730e509c
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-14 10:06:54 -0500

    feat(GeneralInfoPanel.tsx): remove unnecessary Option usage and enforce non-nullable type for generalInformation prop
    The code was refactored to remove unnecessary usage of the Option type from fp-ts, simplifying the data processing pipeline. The generalInformation prop is now enforced as non-nullable, ensuring that the component always receives valid data. This change improves code readability and reduces the risk of runtime errors due to unexpected null values.

--- apps/genome-page/components/features/Summary/Panels/GeneralInfoPanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/GeneralInfoPanel.tsx
@@ -3,7 +3,6 @@ import { GeneGeneralInformationSummaryQuery } from "dicty-graphql-schema"
 import { pipe } from "fp-ts/function"
 import { map as Amap } from "fp-ts/Array"
 import {
-  map as Omap,
   fromNullable as OfromNullable,
   getOrElse as OgetOrElse,
 } from "fp-ts/Option"
@@ -13,7 +12,9 @@ import { ItemDisplay } from "components/panels/ItemDisplay"
 import { RightDisplay } from "components/panels/RightDisplay"
 
 type Properties = {
-  generalInformation: GeneGeneralInformationSummaryQuery["geneGeneralInformation"]
+  generalInformation: NonNullable<
+    GeneGeneralInformationSummaryQuery["geneGeneralInformation"]
+  >
 }
 
 type PanelRowData = { id: ContentId; value: string[] | string }
@@ -24,55 +25,49 @@ type PanelRowData = { id: ContentId; value: string[] | string }
 const GeneralInfoPanel = ({ generalInformation }: Properties) =>
   pipe(
     generalInformation,
-    OfromNullable,
-    Omap(
-      (info) =>
-        [
-          {
-            id: "Name Description",
-            value: pipe(
-              info.name_description,
-              Amap(OfromNullable),
-              Amap(OgetOrElse(() => "")),
-            ),
-          },
-          { id: "dictyBase ID", value: info.id },
-          {
-            id: "Gene Product",
-            value: pipe(
-              info.gene_product,
-              OfromNullable,
-              OgetOrElse(() => ""),
-            ),
-          },
-          {
-            id: "Alternative Protein Names",
-            value: pipe(
-              info.synonyms,
-              Amap(OfromNullable),
-              Amap(OgetOrElse(() => "")),
-            ),
-          },
-          {
-            id: "Description",
-            value: pipe(
-              info.description,
-              OfromNullable,
-              OgetOrElse(() => ""),
-            ),
-          },
-        ] as Array<PanelRowData>,
-    ),
-    Omap(
-      Amap(({ id, value }) => (
-        <ItemDisplay key={id}>
-          <LeftDisplay>{id}</LeftDisplay>
-          <RightDisplay>{returnPanelContentById(id, value)}</RightDisplay>
-        </ItemDisplay>
-      )),
-    ),
-    Omap((children) => <div>{children}</div>),
-    OgetOrElse(() => <></>),
+    (info) =>
+      [
+        {
+          id: "Name Description",
+          value: pipe(
+            info.name_description,
+            Amap(OfromNullable),
+            Amap(OgetOrElse(() => "")),
+          ),
+        },
+        { id: "dictyBase ID", value: info.id },
+        {
+          id: "Gene Product",
+          value: pipe(
+            info.gene_product,
+            OfromNullable,
+            OgetOrElse(() => ""),
+          ),
+        },
+        {
+          id: "Alternative Protein Names",
+          value: pipe(
+            info.synonyms,
+            Amap(OfromNullable),
+            Amap(OgetOrElse(() => "")),
+          ),
+        },
+        {
+          id: "Description",
+          value: pipe(
+            info.description,
+            OfromNullable,
+            OgetOrElse(() => ""),
+          ),
+        },
+      ] as Array<PanelRowData>,
+    Amap(({ id, value }) => (
+      <ItemDisplay key={id}>
+        <LeftDisplay>{id}</LeftDisplay>
+        <RightDisplay>{returnPanelContentById(id, value)}</RightDisplay>
+      </ItemDisplay>
+    )),
+    (children) => <div>{children}</div>,
   )
 
 export { GeneralInfoPanel }
