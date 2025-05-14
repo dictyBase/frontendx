af6a57f35964775d09930f616b750733e1ac2014
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-14 10:07:40 -0500

    feat(genome-page): create GoaQuery component to fetch and display gene ontology annotations
    This commit introduces a new component, GoaQuery, which fetches and displays gene ontology annotations for a given gene. It uses the `useGeneOntologyAnnotationSummaryQuery` hook from `dicty-graphql-schema` to query the data and renders different components based on the query result:
    
    - `Loader`: While the data is loading.
    - `GoaPanel`: If the data is successfully fetched.
    - `GraphQLErrorPage`: If there is an error during the query.
    - `NoDataDisplay`: If no data is found for the gene.
    
    The component also uses `PanelWrapper` to provide a consistent look and feel. The gene ID is obtained from the Next.js router query parameters.

---
+++ apps/genome-page/components/features/Summary/Panels/GoaQuery.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "next/router"
+import { useGeneOntologyAnnotationSummaryQuery } from "dicty-graphql-schema"
+import { match, P } from "ts-pattern"
+import { NoDataDisplay } from "components/NoDataDisplay"
+import { Loader } from "components/Loader"
+import { GraphQLErrorPage } from "components/errors/GraphQLErrorPage"
+import { PanelWrapper } from "components/panels/PanelWrapper"
+import { GoaPanel } from "./GoaPanel"
+
+const GoaQuery = () => {
+  const { query } = useRouter()
+  const gene = query.id as string
+  const result = useGeneOntologyAnnotationSummaryQuery({
+    variables: {
+      gene,
+    },
+    fetchPolicy: "cache-and-network",
+    nextFetchPolicy: "cache-only",
+  })
+  return (
+    <PanelWrapper
+      route={`${gene}/goannotations`}
+      title="Gene Ontology Annotations">
+      {match(result)
+        .with({ loading: true }, () => <Loader />)
+        .with(
+          {
+            data: { geneOntologyAnnotation: P.select(P.not(P.nullish)) },
+          },
+          (goas) => <GoaPanel goas={goas} />,
+        )
+        .with({ error: P.select(P.not(P.nullish)) }, (error) => (
+          <GraphQLErrorPage error={error} />
+        ))
+        .with(
+          {
+            data: P.nullish,
+          },
+          () => <NoDataDisplay query="GO Annotations" geneId={gene} />,
+        )
+        .otherwise(() => (
+          <> This message should not appear. </>
+        ))}
+    </PanelWrapper>
+  )
+}
+
+export { GoaQuery }
