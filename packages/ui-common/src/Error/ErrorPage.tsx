bc2a1d2363b973fa7e336cf6e27049a8070e5b30
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-19 02:13:03 -0500

    refactor(genome-page): replace GraphQLErrorPage with ErrorPage in ErrorPageWrapper
    The ErrorPageWrapper component in the genome-page app now uses the ErrorPage component from the ui-common package instead of GraphQLErrorPage.
    A new ErrorPage component was created in the ui-common package to handle errors and provide navigation and reload functionality.
    The GraphQLErrorPage component in the ui-common package was refactored to handle different error types based on the error code. It now returns one of the other error components based on the error code.

---
+++ packages/ui-common/src/Error/ErrorPage.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable unicorn/filename-case */
+import { ApolloError } from "@apollo/client"
+import Paper from "@material-ui/core/Paper"
+import { makeStyles } from "@material-ui/core/styles"
+import { blue, grey } from "@material-ui/core/colors"
+import { ErrorDisplay } from "./ErrorDisplay"
+import { getErrorMessage } from "./getErrorMessage"
+
+const useStyles = makeStyles({
+  container: {
+    backgroundColor: blue[50],
+    color: grey[600],
+  },
+  grid: {
+    minHeight: "10rem",
+  },
+  icon: {
+    fontSize: "4rem",
+  },
+})
+
+type GraphQlErrorPageProperties = {
+  /** GraphQL error object */
+  error: ApolloError
+  handleNavigateHome: () => void
+  handleReload: () => void
+}
+
+/**
+ * Displays any errors found when issuing a GraphQL query or mutation.
+ */
+const ErrorPage = ({
+  error,
+  handleNavigateHome,
+  handleReload,
+}: GraphQlErrorPageProperties) => {
+  const classes = useStyles()
+
+  const { message } = getErrorMessage(error)
+  return (
+    <Paper className={classes.container}>
+      <ErrorDisplay
+        details={message}
+        handleNavigateHome={handleNavigateHome}
+        handleReload={handleReload}
+      />
+    </Paper>
+  )
+}
+
+export { ErrorPage }
--- packages/ui-common/src/Error/GraphQLErrorPage.tsx
+++ packages/ui-common/src/Error/GraphQLErrorPage.tsx
@@ -1,51 +1,55 @@
 /* eslint-disable unicorn/filename-case */
 import { ApolloError } from "@apollo/client"
-import Paper from "@material-ui/core/Paper"
-import { makeStyles } from "@material-ui/core/styles"
-import { blue, grey } from "@material-ui/core/colors"
-import { ErrorDisplay } from "./ErrorDisplay"
-import { getErrorMessage } from "./getErrorMessage"
-
-const useStyles = makeStyles({
-  container: {
-    backgroundColor: blue[50],
-    color: grey[600],
-  },
-  grid: {
-    minHeight: "10rem",
-  },
-  icon: {
-    fontSize: "4rem",
-  },
-})
+import { pipe } from "fp-ts/function"
+import { head as RAhead } from "fp-ts/ReadonlyArray"
+import {
+  map as Omap,
+  flatMap as OflatMap,
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { match, P } from "ts-pattern"
+import { ServerError } from "./ServerError"
+import { NotFoundError } from "./NotFoundError"
+import { OtherError } from "./OtherError"
 
 type GraphQlErrorPageProperties = {
   /** GraphQL error object */
   error: ApolloError
-  handleNavigateHome: () => void
-  handleReload: () => void
 }
 
 /**
  * Displays any errors found when issuing a GraphQL query or mutation.
+ * Returns one of the other error components based on the error code.
  */
-const GraphQLErrorPage = ({
-  error,
-  handleNavigateHome,
-  handleReload,
-}: GraphQlErrorPageProperties) => {
-  const classes = useStyles()
 
-  const { message } = getErrorMessage(error)
-  return (
-    <Paper className={classes.container}>
-      <ErrorDisplay
-        details={message}
-        handleNavigateHome={handleNavigateHome}
-        handleReload={handleReload}
-      />
-    </Paper>
-  )
-}
+const GraphQLErrorPage = ({ error }: GraphQlErrorPageProperties) =>
+  match(error)
+    .with({ networkError: P.not(P.nullish) }, () => <ServerError />)
+    .with({ graphQLErrors: P.select(P.not(P.nullish)) }, (errors) => {
+      const primaryError = pipe(errors, RAhead)
+      const primaryErrorCode = pipe(
+        primaryError,
+        OflatMap(({ extensions }) =>
+          pipe(
+            extensions,
+            OfromNullable,
+            // eslint-disable-next-line dot-notation
+            Omap((extension) => extension["code"] as string),
+          ),
+        ),
+        OgetOrElse(() => ""),
+      )
+      const primaryErrorMessage = pipe(
+        primaryError,
+        Omap(({ message }) => message),
+        OgetOrElse(() => ""),
+      )
+      return match(primaryErrorCode)
+        .with("Unavailable", () => <ServerError />)
+        .with("NotFound", () => <NotFoundError />)
+        .otherwise(() => <OtherError message={primaryErrorMessage} />)
+    })
+    .otherwise(() => <OtherError />)
 
 export { GraphQLErrorPage }
