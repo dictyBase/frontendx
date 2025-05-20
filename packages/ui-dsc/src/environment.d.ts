4fba1199a9a38862485a583eec99c9ef21fd8226
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-20 11:02:49 -0500

    feat: add ErrorPageWrapper component and VITE_APP_FRONTPAGE_URL env variable
    Adds a new ErrorPageWrapper component to handle error display and
    introduces the VITE_APP_FRONTPAGE_URL environment variable. This
    allows redirecting users to a specified front page in case of errors.

--- packages/ui-dsc/src/environment.d.ts
+++ packages/ui-dsc/src/environment.d.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line unicorn/prevent-abbreviations
 interface ImportMetaEnv {
   VITE_APP_STOCKCENTER_URL: string
+  VITE_APP_FRONTPAGE_URL: string
 }
 
 interface ImportMeta {
