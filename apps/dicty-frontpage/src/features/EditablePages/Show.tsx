56748cf39144bccd8b444fc009feee94f24e8e4f
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-20 11:14:37 -0500

    feat(Show.tsx): remove GraphQLErrorPage component
    The GraphQLErrorPage component was removed as it was not providing
    any additional value. The component was not handling any errors
    and was just rendering a generic error message.

--- apps/dicty-frontpage/src/features/EditablePages/Show.tsx
+++ apps/dicty-frontpage/src/features/EditablePages/Show.tsx
@@ -1,6 +1,6 @@
 import { useContentBySlugQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
-import { FullPageLoadingDisplay, GraphQLErrorPage } from "@dictybase/ui-common"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
 import { ShowView } from "./ShowView"
 import { NAMESPACE } from "../../common/constants/namespace"
 import { useSlug } from "../../common/hooks/useSlug"
