575be3309699427de8d8bec60c42792bdfa4d5ea
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-21 10:48:40 -0500

    feat(genome-page): remove related genes from PublicationRow component
    The related genes were removed from the PublicationRow component.
    This is because the related genes are not needed in the PublicationRow component.
    The SinglePublication component already displays the related genes.
    This change simplifies the PublicationRow component and makes it easier to maintain.

--- apps/genome-page/components/features/References/PublicationRow.tsx
+++ apps/genome-page/components/features/References/PublicationRow.tsx
@@ -1,13 +1,6 @@
-import { useRouter } from "next/router"
-import { pipe } from "fp-ts/function"
-import { match as Bmatch } from "fp-ts/boolean"
-import { map as Amap, takeLeft as AtakeLeft } from "fp-ts/Array"
-import { Chip, TableCell, TableRow, makeStyles } from "@material-ui/core"
+import { TableCell, TableRow, makeStyles } from "@material-ui/core"
 import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
-import { parseFormattedStringToDomElements } from "@dictybase/ui-common"
-import { commaSeparateWithAnd } from "common/utils/strings"
 import { SinglePublication } from "./SinglePublication"
-import { SeeAllGenesChip } from "./SeeAllGenesChip"
 
 const useStyles = makeStyles({
   cell: { padding: 0 },
@@ -16,47 +9,13 @@ interface PublicationRowProperties {
   publication: ListPublicationsWithGeneQuery["listPublicationsWithGene"][0]
 }
 
-const GENES_LIMIT = 5
-
 const PublicationRow = ({ publication }: PublicationRowProperties) => {
-  const router = useRouter()
   const classes = useStyles()
   return (
     <TableRow>
       <TableCell className={classes.cell}>
         <SinglePublication publication={publication} />
       </TableCell>
-      {/*
-      <TableCell>
-        {pipe(
-          publication.related_genes,
-          AtakeLeft(GENES_LIMIT),
-          Amap((gene) => (
-            <Chip
-              clickable
-              onClick={() => router.push(`/${gene.name}`)}
-              key={gene.id}
-              label={gene.name}
-              size="small"
-              style={{ margin: "0px 5px 5px 0px" }}
-              variant="outlined"
-            />
-          )),
-        )}
-        {pipe(
-          publication.related_genes.length > GENES_LIMIT,
-          Bmatch(
-            () => <></>,
-            () => (
-              <SeeAllGenesChip
-                publicationId={publication.id}
-                geneCount={publication.related_genes.length}
-              />
-            ),
-          ),
-        )}
-      </TableCell>
-        */}
     </TableRow>
   )
 }
