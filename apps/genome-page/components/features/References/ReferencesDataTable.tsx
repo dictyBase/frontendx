8d7ea3aa874075bd603cb1197ef7ed8dbcd8e21b
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-22 10:52:51 -0500

    feat(ReferencesDataTable.tsx): add table header to display number of references
    This change adds a table header to the ReferencesDataTable component that displays the number of references. This provides users with a clear indication of the amount of data presented in the table.

--- apps/genome-page/components/features/References/ReferencesDataTable.tsx
+++ apps/genome-page/components/features/References/ReferencesDataTable.tsx
@@ -23,6 +23,13 @@ const ReferencesDataTable = ({ publications }: Properties) => {
   return (
     <TableContainer component={Paper} className={classes.root}>
       <Table aria-label="phenotypes-table">
+        <TableHead className={classes.head}>
+          <TableRow className={classes.headRow}>
+            <TableCell
+              className={classes.referenceColumn}
+            >{`${publications.length} Reference(s)`}</TableCell>
+          </TableRow>
+        </TableHead>
         <TableBody>
           {publications.map((publication) => (
             <PublicationRow publication={publication} key={publication.id} />
