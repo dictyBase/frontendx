19cafd44d26643444ac0b3c4b2b81fb42eba6854
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-22 12:08:56 -0500

    feat(genome-page): add ReferencesDataTable test to check pluralization of the word Reference
    This commit adds a test suite for the ReferencesDataTable component. The tests verify that the component correctly displays "Reference" when there is only one publication and "References" when there are multiple publications. This ensures correct pluralization of the word "Reference" in the UI.

---
+++ apps/genome-page/components/features/References/ReferencesDataTable.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react"
+import { mockReferencesData } from "mocks/mockReferencesData"
+import { ReferencesDataTable } from "./ReferencesDataTable"
+
+// eslint-disable-next-line import/no-commonjs, unicorn/prefer-module -- ESM not supported by default as of Jest 29
+const useRouter = jest.spyOn(require("next/router"), "useRouter")
+
+const gene = "sadA"
+const pathname = `gene/${gene}/references`
+
+describe("features/References/ReferencesDataTable", () => {
+  test("Displays text 'Reference' when there is only a single publication item", () => {
+    useRouter.mockImplementation(() => ({
+      query: { id: gene },
+      pathname,
+    }))
+    render(<ReferencesDataTable publications={[...mockReferencesData.slice(0, 1)]} />)
+
+    // Renders skeleton loading
+    expect(screen.getByText("1 Reference")).toBeInTheDocument()
+  })
+  test("Displays text 'References' when there are mulitple publication items", () => {
+    useRouter.mockImplementation(() => ({
+      query: { id: gene },
+      pathname,
+    }))
+    render(<ReferencesDataTable publications={mockReferencesData} />)
+
+    // Renders skeleton loading
+    expect(screen.getByText("14 References")).toBeInTheDocument()
+  })
+})
