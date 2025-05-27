84e50d49ee40c5845f2e456d63ca278595343516
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-27 13:32:29 -0500

    refactor(references): move order functions to separate module
    This commit refactors the `ReferencesToolbar` component by moving the
    order functions and related types to a separate module called
    `referenceOrderHelpers.ts`. This improves the separation of concerns
    and makes the code more modular and testable.
    
    The following changes were made:
    
    - Created a new file `referenceOrderHelpers.ts` to contain the order
      functions and related types.
    - Moved the `orderFunctions` object, `OrderFunctionKeys` type, and
      `PublicationWithGene` type from `ReferencesToolbar.tsx` to
      `referenceOrderHelpers.ts`.
    - Updated the import statements in `ReferencesDataTable.tsx` and
      `ReferencesToolbar.tsx` to import the order functions and types from
      the new module.
    - Updated "Title (A - Z)" to "Title (A to Z)" in referenceOrderHelpers.ts to improve semantics.

--- apps/genome-page/components/features/References/ReferencesToolbar.tsx
+++ apps/genome-page/components/features/References/ReferencesToolbar.tsx
@@ -2,49 +2,9 @@ import { Dispatch, SetStateAction } from "react"
 import { Grid, Toolbar, Typography, Select, MenuItem } from "@material-ui/core"
 import { pipe } from "fp-ts/function"
 import { match as Bmatch } from "fp-ts/boolean"
-import { Ord as SOrd } from "fp-ts/string"
-import { Ord as DOrd } from "fp-ts/Date"
 import { map as Amap } from "fp-ts/Array"
 import { keys as Rkeys } from "fp-ts/Record"
-import { Ord, contramap, reverse as ORDreverse } from "fp-ts/Ord"
-import {
-  fromNullable as OfromNullable,
-  getOrElse as OgetOrElse,
-} from "fp-ts/Option"
-import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
-import { parseISO } from "date-fns/fp"
-
-type PublicationWithGene =
-  ListPublicationsWithGeneQuery["listPublicationsWithGene"][0]
-
-const ordByOldest: Ord<PublicationWithGene> = pipe(
-  DOrd,
-  contramap(({ pub_date }) =>
-    pipe(
-      pub_date,
-      OfromNullable,
-      OgetOrElse(() => ""),
-      parseISO,
-    ),
-  ),
-)
-
-const ordByNewest: Ord<PublicationWithGene> = pipe(ordByOldest, ORDreverse)
-
-const ordByTitle: Ord<PublicationWithGene> = pipe(
-  SOrd,
-  contramap(({ title }) => title),
-)
-const ordByTitleReverse: Ord<PublicationWithGene> = pipe(ordByTitle, ORDreverse)
-
-const orderFunctions = {
-  "Newest First": ordByNewest,
-  "Oldest First": ordByOldest,
-  "Title (A - Z)": ordByTitle,
-  "Title (Z - A)": ordByTitleReverse,
-}
-
-type OrderFunctionKeys = keyof typeof orderFunctions
+import { orderFunctions, type OrderFunctionKeys } from "./referenceOrderHelpers"
 
 type ReferencesToolbarProperties = {
   publicationCount: number
@@ -91,4 +51,6 @@ const ReferencesToolbar = ({
   </Toolbar>
 )
 
-export { ReferencesToolbar, orderFunctions, type OrderFunctionKeys }
+export { ReferencesToolbar }
+
+export { orderFunctions, type OrderFunctionKeys } from "./referenceOrderHelpers"
---
+++ apps/genome-page/components/features/References/referenceOrderHelpers.ts
@@ -0,0 +1,44 @@
+import { pipe } from "fp-ts/function"
+import { Ord as SOrd } from "fp-ts/string"
+import { Ord as DOrd } from "fp-ts/Date"
+import { Ord, contramap, reverse as ORDreverse } from "fp-ts/Ord"
+import {
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+} from "fp-ts/Option"
+import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
+import { parseISO } from "date-fns/fp"
+
+type PublicationWithGene =
+  ListPublicationsWithGeneQuery["listPublicationsWithGene"][0]
+
+const ordByOldest: Ord<PublicationWithGene> = pipe(
+  DOrd,
+  contramap(({ pub_date }) =>
+    pipe(
+      pub_date,
+      OfromNullable,
+      OgetOrElse(() => ""),
+      parseISO,
+    ),
+  ),
+)
+
+const ordByNewest: Ord<PublicationWithGene> = pipe(ordByOldest, ORDreverse)
+
+const ordByTitle: Ord<PublicationWithGene> = pipe(
+  SOrd,
+  contramap(({ title }) => title),
+)
+const ordByTitleReverse: Ord<PublicationWithGene> = pipe(ordByTitle, ORDreverse)
+
+const orderFunctions = {
+  "Newest First": ordByNewest,
+  "Oldest First": ordByOldest,
+  "Title (A to Z)": ordByTitle,
+  "Title (Z to A)": ordByTitleReverse,
+}
+
+type OrderFunctionKeys = keyof typeof orderFunctions
+
+export { orderFunctions, type PublicationWithGene, type OrderFunctionKeys }
