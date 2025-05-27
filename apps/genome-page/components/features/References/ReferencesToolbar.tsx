654a5e2fcc4d44ae75d0601dd61dd4d2400cf495
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-27 13:35:40 -0500

    feat(genome-page): style select component to match background color
    The background color of the select component was not matching the
    background color of the page. This commit adds a style to the select
    component to match the background color of the page.

--- apps/genome-page/components/features/References/ReferencesToolbar.tsx
+++ apps/genome-page/components/features/References/ReferencesToolbar.tsx
@@ -1,10 +1,23 @@
 import { Dispatch, SetStateAction } from "react"
-import { Grid, Toolbar, Typography, Select, MenuItem } from "@material-ui/core"
+import {
+  Grid,
+  Toolbar,
+  Typography,
+  Select,
+  MenuItem,
+  makeStyles,
+} from "@material-ui/core"
 import { pipe } from "fp-ts/function"
 import { match as Bmatch } from "fp-ts/boolean"
 import { map as Amap } from "fp-ts/Array"
 import { keys as Rkeys } from "fp-ts/Record"
 import { orderFunctions, type OrderFunctionKeys } from "./referenceOrderHelpers"
+
+const useStyles = makeStyles((theme) => ({
+  select: {
+    backgroundColor: theme.palette.background.default,
+  },
+}))
 
 type ReferencesToolbarProperties = {
   publicationCount: number
@@ -16,40 +29,44 @@ const ReferencesToolbar = ({
   publicationCount,
   order,
   setOrder,
-}: ReferencesToolbarProperties) => (
-  <Toolbar variant="dense">
-    <Grid container spacing={2} alignItems="center">
-      <Grid item>
-        <Typography>
-          {publicationCount}{" "}
-          {pipe(
-            publicationCount > 1,
-            Bmatch(
-              () => "Reference",
-              () => "References",
-            ),
-          )}
-        </Typography>
+}: ReferencesToolbarProperties) => {
+  const classes = useStyles()
+  return (
+    <Toolbar variant="dense">
+      <Grid container spacing={2} alignItems="center">
+        <Grid item>
+          <Typography>
+            {publicationCount}{" "}
+            {pipe(
+              publicationCount > 1,
+              Bmatch(
+                () => "Reference",
+                () => "References",
+              ),
+            )}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Select
+            labelId="reference-sort-select"
+            id="reference-sort-select"
+            variant="outlined"
+            value={order}
+            onChange={({ target: { value } }) => {
+              setOrder(value as OrderFunctionKeys)
+            }}
+            className={classes.select}>
+            {pipe(
+              orderFunctions,
+              Rkeys,
+              Amap((label) => <MenuItem value={label}>{label}</MenuItem>),
+            )}
+          </Select>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Select
-          labelId="reference-sort-select"
-          id="reference-sort-select"
-          variant="outlined"
-          value={order}
-          onChange={({ target: { value } }) => {
-            setOrder(value as OrderFunctionKeys)
-          }}>
-          {pipe(
-            orderFunctions,
-            Rkeys,
-            Amap((label) => <MenuItem value={label}>{label}</MenuItem>),
-          )}
-        </Select>
-      </Grid>
-    </Grid>
-  </Toolbar>
-)
+    </Toolbar>
+  )
+}
 
 export { ReferencesToolbar }
 
