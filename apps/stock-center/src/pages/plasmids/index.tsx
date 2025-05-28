89a113c9ca563f37be8717d91d4be992fe67791d
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-28 08:05:42 -0500

    fix(plasmids): rename ErrorPageWrapper to ErrorDisplay
    The ErrorPageWrapper component was renamed to ErrorDisplay for
    better clarity and consistency in naming.

--- apps/stock-center/src/pages/plasmids/index.tsx
+++ apps/stock-center/src/pages/plasmids/index.tsx
@@ -3,7 +3,7 @@ import { P, match } from "ts-pattern"
 import {
   LoadingDisplay,
   PlasmidCatalogTableDisplay,
-  ErrorPageWrapper,
+  ErrorDisplay,
   CatalogListWrapper,
   CatalogHeader,
 } from "@dictybase/ui-dsc"
@@ -57,7 +57,7 @@ const PlasmidCatalog = () => {
           )
           .with({ loading: true }, () => <LoadingDisplay rows={10} />)
           .with({ error: P.select({ message: P.string }) }, (error_) => (
-            <ErrorPageWrapper error={error_} />
+            <ErrorDisplay error={error_} />
           ))
           .otherwise(() => (
             <></>
