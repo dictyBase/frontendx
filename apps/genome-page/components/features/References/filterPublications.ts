53c6d88dc34c248cf2c8356aa40f8f43a0dcb1f6
Kevin Tun <kevin.tun@northwestern.edu>
2025-05-30 09:13:33 -0500

    refactor(filterPublications.ts): improve filter logic using Option
    Refactors the filter logic to use `fp-ts/Option` for handling
    search parameters. This change avoids passing empty strings to
    filter functions when a parameter is not provided, leading to
    more concise and readable code. The `match` function from
    `fp-ts/Option` is used to conditionally apply filter functions
    only when a search parameter is present.

--- apps/genome-page/components/features/References/filterPublications.ts
+++ apps/genome-page/components/features/References/filterPublications.ts
@@ -2,6 +2,7 @@ import { pipe } from "fp-ts/function"
 import { match as Bmatch } from "fp-ts/boolean"
 import { includes as Sincludes, isString, toLowerCase } from "fp-ts/string"
 import { filter as Afilter, exists as Aexists } from "fp-ts/Array"
+import { some, none, fromNullable as OfromNullable, match as Omatch } from "fp-ts/Option"
 import { ListPublicationsWithGeneQuery } from "dicty-graphql-schema"
 
 type Publications = NonNullable<
@@ -42,6 +43,8 @@ const filterByGene = (searchTerm: string) => (publications: Publications) =>
     ),
   )
 
+const pass = (publications: Publications) => publications
+
 const filterPublications = (
   publications: Publications,
   searchParameters: Record<string, NonNullable<string | string[] | undefined>>,
@@ -50,32 +53,32 @@ const filterPublications = (
     searchParameters.title,
     isString,
     Bmatch(
-      () => "",
-      () => searchParameters.title as string,
+      () => none,
+      () => some(searchParameters.title as string),
     ),
   )
   const authorParameter = pipe(
     searchParameters.author,
     isString,
     Bmatch(
-      () => "",
-      () => searchParameters.author as string,
+      () => none,
+      () => some(searchParameters.author as string),
     ),
   )
   const geneParameter = pipe(
     searchParameters.gene,
     isString,
     Bmatch(
-      () => "",
-      () => searchParameters.gene as string,
+      () => none,
+      () => some(searchParameters.gene as string),
     ),
   )
 
   return pipe(
     publications,
-    filterByTitle(titleParameter),
-    filterByAuthor(authorParameter),
-    filterByGene(geneParameter),
+    pipe(titleParameter, Omatch(() => pass, (t) => filterByTitle(t))),
+    pipe(authorParameter, Omatch(() => pass, (a) => filterByAuthor(a))),
+    pipe(geneParameter, Omatch(() => pass, (g) => filterByGene(g))),
   )
 }
 
