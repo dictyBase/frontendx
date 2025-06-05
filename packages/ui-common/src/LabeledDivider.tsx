447d60e0d063f19a55aa970af61d9aa20f01eac0
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-05 13:53:01 -0500

    feat(ui-common): add LabeledDivider and groupByDate components
    This commit introduces two new components to the ui-common package:
    
    - LabeledDivider: A component that renders a divider with a label.
    - groupByDate: A utility function that groups a list of content items by date.
    
    These components are added to improve the reusability of UI elements and
    provide a convenient way to group content by date.

---
+++ packages/ui-common/src/LabeledDivider.tsx
@@ -0,0 +1,34 @@
+import { Grid, Typography, Divider, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  labelText: {
+    color: theme.palette.secondary.dark
+  },
+  divider: {
+    backgroundColor: theme.palette.secondary.dark,
+    opacity: 0.3,
+    height: "2px"
+  }
+}))
+
+const LabeledDivider = ({ label }: { label: string }) => {
+  const { root, labelText, divider } = useStyles()
+  return (
+    <Grid container spacing={2} alignItems="center" className={root}>
+      <Grid item>
+        <Typography variant="h2" variantMapping={{ h2: "subtitle1" }} className={labelText}>
+          {label}
+        </Typography>
+      </Grid>
+      <Grid item style={{ flexGrow: 1 }}>
+        <Divider className={divider} />
+      </Grid>
+    </Grid>
+  )
+}
+
+export { LabeledDivider }
