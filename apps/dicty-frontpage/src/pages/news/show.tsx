2df407f7e9bf8f98734314e5e6cd6bd33da897c8
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-06 12:14:48 -0500

    refactor(news): simplify NewsView component
    The news items are now rendered inside the NewsListWrapper component.
    The worker code was removed as it was not being used.
    The useStyles hook was removed from the NewsView component.
    The NewsView component was simplified by removing the useStyles hook and the worker code.

--- apps/dicty-frontpage/src/pages/news/show.tsx
+++ apps/dicty-frontpage/src/pages/news/show.tsx
@@ -8,7 +8,12 @@ import {
   makeStyles,
 } from "@material-ui/core"
 import { pipe, flow } from "fp-ts/function"
-import { map as Amap, sort as Asort, filter as Afilter, isNonEmpty } from "fp-ts/Array"
+import {
+  map as Amap,
+  sort as Asort,
+  filter as Afilter,
+  isNonEmpty,
+} from "fp-ts/Array"
 import { fst, snd, mapFst, mapSnd } from "fp-ts/Tuple"
 import { Ord, contramap } from "fp-ts/Ord"
 import { toEntries as RtoEntries } from "fp-ts/Record"
@@ -19,7 +24,7 @@ import {
   LabeledDivider,
   groupByDate,
 } from "@dictybase/ui-common"
-import { NewsItem } from "@dictybase/ui-frontpage"
+import { NewsListWrapper, NewsItem } from "@dictybase/ui-frontpage"
 import {
   useListContentByNamespaceQuery,
   ListContentByNamespaceQuery,
@@ -30,38 +35,11 @@ import { parseISO } from "date-fns/fp"
 import { NEWS_NAMESPACE } from "../../common/constants/namespace"
 import { ordByDate } from "../../common/utils/ordByDate"
 import { EmptyNewsView } from "../../common/components/EmptyNewsView"
-import Worker from "../../worker?worker"
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(4),
-    textAlign: "left",
-    padding: "0px 6rem 1rem 6rem",
-    borderRadius: "15px",
-    boxSizing: "border-box",
-    "@media (max-width: 768px)": {
-      padding: "0 0 0 0",
-    },
-  },
-  headerContainer: {
-    background: `linear-gradient(to right, ${theme.palette.primary.main}, ${theme.palette.primary.light})`,
-    borderRadius: theme.shape.borderRadius,
-    boxShadow: theme.shadows[3],
-    padding: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    color: "#ffffff",
-  },
-  heading: {
-    marginBottom: theme.spacing(1),
-  },
-  subheading: {
-    fontStyle: "italic",
-  },
-}))
+type NewsList = ListContentByNamespaceQuery["listContentByNamespace"]
 
 type NewsViewProperties = {
-  contentList: ListContentByNamespaceQuery["listContentByNamespace"]
+  contentList: NewsList
 }
 
 const OrdByNewest: Ord<
@@ -71,74 +49,42 @@ const OrdByNewest: Ord<
   contramap((content) => pipe(content.created_at, parseISO)),
 )
 
+const renderNewsItem = flow(
+  (item: ListContentByNamespaceQuery["listContentByNamespace"][0]) => ({
+    ...item,
+    content: parseContentToText(item.content),
+  }),
+  ({ id, name, content, created_at }) => (
+    <Grid item key={id}>
+      <NewsItem name={name} createdAt={created_at} content={content} />
+    </Grid>
+  ),
+)
+
+const processGroupedContentList = flow(Asort(OrdByNewest), Amap(renderNewsItem))
+
 const NewsView = ({ contentList }: NewsViewProperties) => {
-  const { container, headerContainer, heading, subheading } = useStyles()
-  const worker = useRef<null | Worker>(null)
-
-  // We use the `useEffect` hook to setup the worker as soon as the `App` component is mounted.
-  useEffect(() => {
-    // Create the worker if it does not yet exist.
-    console.log(import.meta.url)
-    worker.current ??= new Worker()
-
-    // Create a callback function for messages from the worker thread.
-    const onMessageReceived = (e) => {
-      // TODO: Will fill in later
-    }
-
-    // Attach the callback function as an event listener.
-    worker.current.addEventListener("message", onMessageReceived)
-
-    // Define a cleanup function for when the component is unmounted.
-    return () =>
-      worker.current.removeEventListener("message", onMessageReceived)
-  })
-  console.log(groupByDate(contentList))
-  // ListContentByNamespaceQuery["listContentByNamespace"]
-  const renderNewsItem = flow(
-    (item: ListContentByNamespaceQuery["listContentByNamespace"][0]) => ({
-      ...item,
-      content: parseContentToText(item.content),
-    }),
-    ({ id, name, content, created_at }) => (
-      <NewsItem key={id} name={name} createdAt={created_at} content={content} />
-    ),
-    (item) => <Grid item>{item}</Grid>,
-  )
-
-  const processGroupedContentList = flow(
-    Asort(OrdByNewest),
-    Amap(renderNewsItem),
-  )
 
   return pipe(
-    contentList, //separate into groups
+    contentList,
+    // groupByDate turns Array<NewsList> -> Array<[string, NewsList]>,
+    // where the string is the label of a date grouping, such as "Last Month".
     groupByDate,
+    // If any date groups have empty lists, remove them.
     Afilter(flow(snd, isNonEmpty)),
+    // Map each group's corresponding news list into a news item UI element.
     Amap(mapSnd(processGroupedContentList)),
+    // Render each date group with a label.
     Amap((entry) => (
-      <>
+      <Box key={fst(entry)}>
         <LabeledDivider label={fst(entry)} />
         <Grid container direction="column" spacing={3}>
           {snd(entry)}
         </Grid>
-      </>
+      </Box>
     )),
-    // do these to each group
-    // wrap in final container
-    (groups) => (
-      <Container className={container}>
-        <Box className={headerContainer}>
-          <Typography variant="h1" align="center" className={heading}>
-            Dicty Community Resource News
-          </Typography>
-          <Typography align="center" className={subheading}>
-            Latest updates from the Dictyostelium research community
-          </Typography>
-        </Box>
-        {groups}
-      </Container>
-    ),
+    // Wrap in a news list container.
+    (groups) => <NewsListWrapper>{groups}</NewsListWrapper>,
   )
 }
 
