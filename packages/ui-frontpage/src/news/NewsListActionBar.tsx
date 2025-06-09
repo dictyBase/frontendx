00709fb2234c62800d83e4dae479e01df518190d
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-08 22:58:10 -0500

    feat(NewsListActionBar): enhance NewsListActionBar component with styling and Paper component :wq The NewsListActionBar component is now wrapped in a Paper component for better visual appearance. The component now uses the lightBlue color from material-ui and has a sticky position.

--- packages/ui-frontpage/src/news/NewsListActionBar.tsx
+++ packages/ui-frontpage/src/news/NewsListActionBar.tsx
@@ -1,28 +1,36 @@
-import { Grid, Theme, makeStyles } from "@material-ui/core"
+import { Paper, Grid, Theme, makeStyles } from "@material-ui/core"
+import { blue, lightBlue } from "@material-ui/core/colors"
 import { WriteNewsButton } from "./WriteNewsButton"
 
 const useStyles = makeStyles((theme: Theme) => ({
   toolbar: {
+    height: "100%",
+    background: lightBlue[50],
+  },
+  grid: {
     padding: theme.spacing(1),
     marginBottom: theme.spacing(2),
-    top: 20,
+    top: 10,
     position: "sticky",
   },
 }))
 
 const NewsListActionBar = () => {
-  const { toolbar } = useStyles()
+  const { toolbar, grid } = useStyles()
   return (
-    <Grid
-      container
-      spacing={2}
-      direction="column"
-      className={toolbar}
-      data-testid="info-page-toolbar">
-      <Grid item>
-        <WriteNewsButton />
+    <Paper elevation={3} className={toolbar}>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+        alignItems="center"
+        className={grid}
+        data-testid="info-page-toolbar">
+        <Grid item>
+          <WriteNewsButton />
+        </Grid>
       </Grid>
-    </Grid>
+    </Paper>
   )
 }
 
