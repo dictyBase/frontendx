58a1b19ecdf712445d29a07a54afe65d27f0ccef
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-08 22:58:18 -0500

    feat(WriteNewsButton.tsx): add styling to WriteNewsButton
    The WriteNewsButton component now has a custom style using
    Material-UI's makeStyles hook. The button has a gradient background
    from dark blue to the theme's primary light color, and the text
    color is set to the theme's primary contrast text color. This
    enhances the visual appearance of the button.

--- packages/ui-frontpage/src/news/WriteNewsButton.tsx
+++ packages/ui-frontpage/src/news/WriteNewsButton.tsx
@@ -1,15 +1,24 @@
 import { useNavigate } from "react-router-dom"
-import { Fab, Tooltip } from "@material-ui/core"
+import { Fab, Tooltip, makeStyles } from "@material-ui/core"
+import { blue } from "@material-ui/core/colors"
 import CreateIcon from "@material-ui/icons/Create"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: `linear-gradient(to top, ${"#000080"}, ${theme.palette.primary.light})`,
+    color: theme.palette.primary.contrastText,
+  },
+}))
+
 const WriteNewsButton = () => {
+  const { root } = useStyles()
   const navigate = useNavigate()
   const handleClick = () => {
     navigate("/news/create")
   }
   return (
     <Tooltip title="Write News Article" aria-label="write-news-article">
-      <Fab color="primary" onClick={handleClick}>
+      <Fab onClick={handleClick} className={root}>
         <CreateIcon />
       </Fab>
     </Tooltip>
