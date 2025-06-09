347ccdf463a2087654430c190eae90de9c31cd29
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-08 22:57:59 -0500

    feat(ui-common): add LabeledDivider component to improve UI readability
    This commit introduces a new `LabeledDivider` component to the `ui-common` package. The component consists of a divider with a label, enhancing the visual structure and readability of the UI by clearly separating sections with descriptive titles. The styling has been adjusted to improve visual appeal and consistency with the overall theme, including increased top margin and label color.

--- packages/ui-common/src/LabeledDivider.tsx
+++ packages/ui-common/src/LabeledDivider.tsx
@@ -2,17 +2,17 @@ import { Grid, Typography, Divider, makeStyles } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(2),
     marginBottom: theme.spacing(1),
   },
   labelText: {
-    color: theme.palette.secondary.dark
+    color: theme.palette.secondary.dark,
   },
   divider: {
     backgroundColor: theme.palette.secondary.dark,
     opacity: 0.3,
-    height: "2px"
-  }
+    height: "2px",
+  },
 }))
 
 const LabeledDivider = ({ label }: { label: string }) => {
@@ -20,7 +20,10 @@ const LabeledDivider = ({ label }: { label: string }) => {
   return (
     <Grid container spacing={2} alignItems="center" className={root}>
       <Grid item>
-        <Typography variant="h2" variantMapping={{ h2: "subtitle1" }} className={labelText}>
+        <Typography
+          variant="h2"
+          variantMapping={{ h2: "subtitle1" }}
+          className={labelText}>
           {label}
         </Typography>
       </Grid>
