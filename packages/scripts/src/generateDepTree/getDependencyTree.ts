3552302dd8d74530a79601a902c24beb2718e9a3
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-13 08:29:40 -0500

    feat(getDependencyTree.ts): implement function to generate dependency tree
    This commit introduces the `getDependencyTree` function, which constructs a dependency tree based on a project manifest and a collection of local packages. It recursively resolves dependencies, creating a nested structure that represents the project's dependency graph. The function utilizes `fp-ts` for functional data manipulation, enhancing code clarity and maintainability.

---
+++ packages/scripts/src/generateDepTree/getDependencyTree.ts
@@ -0,0 +1,37 @@
+import { pipe } from "fp-ts/function"
+import { Ord as SOrd } from "fp-ts/string"
+import {
+  collect as Rcollect,
+  has as Rhas,
+  mapWithIndex as RmapWithIndex,
+} from "fp-ts/Record"
+import { ProjectManifest, DependencyNode } from "./types"
+
+const getDependencyTree = (
+  manifest: ProjectManifest,
+  localPackages: Record<string, ProjectManifest>,
+): DependencyNode => {
+  let dependencies = pipe(
+    manifest.dependencies,
+    RmapWithIndex((name, version) => {
+      if (Rhas(name, localPackages)) {
+        return getDependencyTree(localPackages[name], localPackages)
+      } else {
+        return {
+          name,
+          version,
+          dependencies: [],
+        }
+      }
+    }),
+    Rcollect(SOrd)((_, nodes) => nodes)
+  )
+
+  return {
+    name: manifest.name,
+    version: manifest.version,
+    dependencies,
+  }
+}
+
+export { getDependencyTree }
