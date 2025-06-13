ea872367bc84f245463347fc83fe4e14de834fcd
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-13 11:24:49 -0500

    feat(generateDepTree): output dependency graph to JSON file
    This change introduces the functionality to output the dependency graph to a JSON file. The dependency graph is generated using the `getDependencyTree` function, and then written to a file named `dependency-graph.json` in the root directory. This allows for easier analysis and visualization of the project's dependencies.

--- packages/scripts/src/generateDepTree/index.ts
+++ packages/scripts/src/generateDepTree/index.ts
@@ -1,14 +1,18 @@
+import { writeFileSync } from "node:fs"
 import { join } from "node:path"
 import { getManifests } from "./getLocalPackages"
 import { getDependencyTree } from "./getDependencyTree"
 
-const PACKAGES_DIRECTORY = join(process.cwd(), "packages")
-const APPS_DIRECTORY = join(process.cwd(), "apps")
+const ROOT_DIR = process.cwd()
+const PACKAGES_DIRECTORY = join(ROOT_DIR, "packages")
+const APPS_DIRECTORY = join(ROOT_DIR, "apps")
 
 const main = () => {
   const apps = getManifests(APPS_DIRECTORY)
   const packages = getManifests(PACKAGES_DIRECTORY)
-  console.log(JSON.stringify(getDependencyTree(apps["dicty-frontpage"], packages)))
+  const jsonOutput = join(ROOT_DIR, 'dependency-graph.json')
+  const graph = getDependencyTree(apps["dicty-frontpage"], packages)
+  writeFileSync(jsonOutput, JSON.stringify(graph, null, 2))
 }
 
 main()
