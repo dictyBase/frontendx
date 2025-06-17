5f9b9595165bb8d94a91a81ccbbeb87eb3eab764
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-16 19:24:13 -0500

    feat(GoaPanel.tsx): refactor GoaPanel to improve code readability and remove empty panels
    The dataFilter function is curried to improve readability and reusability.
    fp-ts library is used to conditionally render ItemDisplay components, which removes empty panels.
    The code is more declarative and easier to understand.

--- apps/genome-page/components/features/Summary/Panels/GoaPanel.tsx
+++ apps/genome-page/components/features/Summary/Panels/GoaPanel.tsx
@@ -1,3 +1,5 @@
+import { pipe } from "fp-ts/function"
+import { map as Amap, match as Amatch } from "fp-ts/Array"
 import { ItemDisplay } from "components/panels/ItemDisplay"
 import { LeftDisplay } from "components/panels/LeftDisplay"
 import { RightDisplay } from "components/panels/RightDisplay"
@@ -6,52 +8,53 @@ import { GoaPanelContent } from "./GoaPanelContent"
 
 type GOType = "molecular_function" | "biological_process" | "cellular_component"
 
-const dataFilter = (
-  array: NonNullable<
-    GeneOntologyAnnotationSummaryQuery["geneOntologyAnnotation"]
-  >,
-  type: GOType,
-) => {
-  // get the attributes from specified type
-  const attribute = array.filter((item) => item.type === type)
+const dataFilter =
+  (type: GOType) =>
+  (
+    array: NonNullable<
+      GeneOntologyAnnotationSummaryQuery["geneOntologyAnnotation"]
+    >,
+  ) => {
+    // get the attributes from specified type
+    const attribute = array.filter((item) => item.type === type)
 
-  // get the five most recent EXP annotations
-  const expChecker = attribute
-    .filter(
-      (item) =>
-        item.evidence_code === "IMP" ||
-        item.evidence_code === "IGI" ||
-        item.evidence_code === "IDA" ||
-        item.evidence_code === "IPI" ||
-        item.evidence_code === "IEP" ||
-        item.evidence_code === "EXP",
-    )
-    .sort((a, b) => b.date.localeCompare(a.date))
-    .slice(0, 5)
+    // get the five most recent EXP annotations
+    const expChecker = attribute
+      .filter(
+        (item) =>
+          item.evidence_code === "IMP" ||
+          item.evidence_code === "IGI" ||
+          item.evidence_code === "IDA" ||
+          item.evidence_code === "IPI" ||
+          item.evidence_code === "IEP" ||
+          item.evidence_code === "EXP",
+      )
+      .sort((a, b) => b.date.localeCompare(a.date))
+      .slice(0, 5)
 
-  // get five most recent manual
-  const manualChecker = attribute
-    .filter((item) => item.evidence_code !== "IEA")
-    .sort((a, b) => b.date.localeCompare(a.date))
-    .slice(0, 5)
+    // get five most recent manual
+    const manualChecker = attribute
+      .filter((item) => item.evidence_code !== "IEA")
+      .sort((a, b) => b.date.localeCompare(a.date))
+      .slice(0, 5)
 
-  // get five most recent electronic
-  const electronicChecker = attribute
-    .filter((item) => item.evidence_code === "IEA")
-    .sort((a, b) => b.date.localeCompare(a.date))
-    .slice(0, 5)
+    // get five most recent electronic
+    const electronicChecker = attribute
+      .filter((item) => item.evidence_code === "IEA")
+      .sort((a, b) => b.date.localeCompare(a.date))
+      .slice(0, 5)
 
-  // check if EXP array is empty
-  // if it is, return manual
-  // if manual is empty, return electronic
-  if (!Array.isArray(expChecker) || expChecker.length === 0) {
-    if (!Array.isArray(manualChecker) || manualChecker.length === 0) {
-      return electronicChecker
+    // check if EXP array is empty
+    // if it is, return manual
+    // if manual is empty, return electronic
+    if (!Array.isArray(expChecker) || expChecker.length === 0) {
+      if (!Array.isArray(manualChecker) || manualChecker.length === 0) {
+        return electronicChecker
+      }
+      return manualChecker
     }
-    return manualChecker
+    return expChecker
   }
-  return expChecker
-}
 
 type Properties = {
   /** Array of GO annotations for a particular gene */
@@ -65,30 +68,48 @@ type Properties = {
  */
 const GoaPanel = ({ goas }: Properties) => (
   <div>
-    <ItemDisplay>
-      <LeftDisplay>Molecular Function</LeftDisplay>
-      <RightDisplay>
-        {dataFilter(goas, "molecular_function").map((item) => (
-          <GoaPanelContent key={item.id} goa={item} />
-        ))}
-      </RightDisplay>
-    </ItemDisplay>
-    <ItemDisplay>
-      <LeftDisplay>Biological Process</LeftDisplay>
-      <RightDisplay>
-        {dataFilter(goas, "biological_process").map((item) => (
-          <GoaPanelContent key={item.id} goa={item} />
-        ))}
-      </RightDisplay>
-    </ItemDisplay>
-    <ItemDisplay>
-      <LeftDisplay>Cellular Component</LeftDisplay>
-      <RightDisplay>
-        {dataFilter(goas, "cellular_component").map((item) => (
-          <GoaPanelContent key={item.id} goa={item} />
-        ))}
-      </RightDisplay>
-    </ItemDisplay>
+    {pipe(
+      goas,
+      dataFilter("molecular_function"),
+      Amap((item) => <GoaPanelContent key={item.id} goa={item} />),
+      Amatch(
+        () => <></>,
+        (children) => (
+          <ItemDisplay>
+            <LeftDisplay>Molecular Function</LeftDisplay>
+            <RightDisplay>{children}</RightDisplay>
+          </ItemDisplay>
+        ),
+      ),
+    )}
+    {pipe(
+      goas,
+      dataFilter("biological_process"),
+      Amap((item) => <GoaPanelContent key={item.id} goa={item} />),
+      Amatch(
+        () => <></>,
+        (children) => (
+          <ItemDisplay>
+            <LeftDisplay>Biological Process</LeftDisplay>
+            <RightDisplay>{children}</RightDisplay>
+          </ItemDisplay>
+        ),
+      ),
+    )}
+    {pipe(
+      goas,
+      dataFilter("cellular_component"),
+      Amap((item) => <GoaPanelContent key={item.id} goa={item} />),
+      Amatch(
+        () => <></>,
+        (children) => (
+          <ItemDisplay>
+            <LeftDisplay>Cellular Component</LeftDisplay>
+            <RightDisplay>{children}</RightDisplay>
+          </ItemDisplay>
+        ),
+      ),
+    )}
   </div>
 )
 
