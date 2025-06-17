bcfa18e7d655e570541b03ec6d93854859edfea7
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-17 10:04:21 -0500

    refactor(TreeNode.ts): remove PackageNode class and update TreeNode class to implement ITreeNode interface
    The PackageNode class was removed as it was not necessary and the TreeNode class was updated to implement the ITreeNode interface. This simplifies the code and makes it more maintainable. The parent type was changed to TreeNode and children type to ITreeNode to improve type safety and allow for more flexibility in the future.

--- packages/generate-dependency-graph/src/TreeNode.ts
+++ packages/generate-dependency-graph/src/TreeNode.ts
@@ -1,6 +1,6 @@
 interface ITreeNode {
   parent: ITreeNode | null
-  children: Array<TreeNode>
+  children: Array<ITreeNode>
 
   getParent(): ITreeNode | null
   addChild(child: ITreeNode): void
@@ -11,7 +11,8 @@ interface TreeNodeConstructorParameters {
 }
 
 class TreeNode implements ITreeNode {
-  parent: ITreeNode | null
+  parent: TreeNode | null
+
   children: Array<TreeNode>
 
   constructor({ parent }: TreeNodeConstructorParameters) {
@@ -19,40 +20,13 @@ class TreeNode implements ITreeNode {
     this.children = []
   }
 
-  getParent(): ITreeNode | null {
+  getParent(): TreeNode | null {
     return this.parent
   }
 
-  addChild(child: ITreeNode) {
+  addChild(child: TreeNode) {
     this.children.push(child)
   }
 }
 
-class PackageNode extends TreeNode {
-  parent: PackageNode | null
-  name: string
-  version: string
-
-  constructor({
-    name,
-    version,
-    parent = null,
-  }: {
-    name: string
-    version: string
-    parent?: PackageNode["parent"]
-  }) {
-    super({ parent })
-    this.parent = parent
-    this.name = name
-    this.version = version
-  }
-
-  getParent(): PackageNode | null {
-    return this.parent
-  }
-
-  addChild(child: PackageNode) {
-    this.children.push(child)
-  }
-}
+export { TreeNode }
