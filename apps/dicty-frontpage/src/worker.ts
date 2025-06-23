5039ed72373d6b0c513ec05af5e91fd82f74250a
Kevin Tun <kevin.tun@northwestern.edu>
2025-06-23 11:38:40 -0500

    feat: add worker.ts for handling WebWorkerMLCEngineHandler
    This commit introduces a new file, worker.ts, which is responsible
    for handling the WebWorkerMLCEngineHandler. The handler resides in
    the worker thread and listens for messages using self.onmessage.
    This setup enables offloading potentially heavy tasks to a separate
    thread, improving the main thread's performance and responsiveness.

--- apps/dicty-frontpage/src/worker.ts
+++ apps/dicty-frontpage/src/worker.ts
@@ -3,7 +3,7 @@ import { WebWorkerMLCEngineHandler } from "@mlc-ai/web-llm";
 
 // A handler that resides in the worker thread
 const handler = new WebWorkerMLCEngineHandler();
-console.log("loaded")
+
 self.onmessage = (msg: MessageEvent) => {
   handler.onmessage(msg);
 };
