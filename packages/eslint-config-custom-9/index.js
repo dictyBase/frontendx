dd8084d1148e1da5fd334f19abd78ccb7138218c
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-08 10:31:27 -0500

    feat(eslint-config): integrate typescript config and reorder eslint configs
    This commit integrates the typescript eslint configuration and reorders the eslint configurations to ensure proper linting and formatting. The typescript config is now included for better typescript support. The order of configurations has been adjusted to ensure that the most important rules are applied first, and prettier is applied last to avoid conflicts.

--- packages/eslint-config-custom-9/index.js
+++ packages/eslint-config-custom-9/index.js
@@ -7,16 +7,18 @@ import unicorn from "eslint-plugin-unicorn"
 import sonarjs from "eslint-plugin-sonarjs"
 import prettier from "eslint-plugin-prettier/recommended"
 import typescriptParser from "@typescript-eslint/parser"
+import tseslintconfig from "./typescript.js"
 import reactApp from "./reactAppClone.js"
 
 export default defineConfig([
   ...turbo,
-  prettier,
+  reactApp,
+  tseslintconfig,
   github.getFlatConfigs().recommended,
   sonarjs.configs.recommended,
   unicorn.configs.recommended,
-  reactApp,
   compat.configs["flat/recommended"],
+  prettier,
   {
     files: ["src/**/*.ts", "src/**/*.tsx"],
     languageOptions: {
