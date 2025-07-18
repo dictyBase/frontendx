e005a32f4e358942b760b417e257f03da18c2846
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-18 15:11:16 -0500

    fix(editor-toolbar): fix formatting issues
    These changes improve code readability, formatting, and consistency across various files. The README.md file now has a newline after the heading, improving its visual structure. The test description in Footer.test.tsx is corrected for clarity. A newline is added in graphql_cache.ts for better readability. The NavbarProperties type definition in Navbar.tsx is formatted for consistency. The LoadingDisplay component in ui-dsc is formatted for readability. Finally, the variable declaration in $createWidthTable.test.tsx is formatted for consistency.

--- packages/footer/src/__tests__/Footer.test.tsx
+++ packages/footer/src/__tests__/Footer.test.tsx
@@ -38,12 +38,13 @@ test.each([
     url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
     label: "Genome Browser",
   },
-])("renders footer with links of footer data with $label and $url",
+])(
+  "renders footer with links of footer data with $label and $url",
   ({ label, url }) => {
     render(<Footer data={testData} />)
 
     const link = screen.getByRole("link", { name: label })
     expect(link).toBeInTheDocument()
     expect(link).toHaveAttribute("href", url)
-  }
+  },
 )
