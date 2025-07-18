9841bcddbea26cac4226c1de823733b550e1ba83
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-18 11:23:19 -0500

    test(footer-mui5): use test.each to check all links in footer
    The test now uses test.each to iterate through the testData array and check if each link is rendered correctly. This makes the test more concise and easier to read. It also ensures that all links are tested, even if the testData array is updated.

--- packages/footer-mui5/src/__tests__/Footer.test.tsx
+++ packages/footer-mui5/src/__tests__/Footer.test.tsx
@@ -21,12 +21,29 @@ const testData = [
   },
 ]
 
-test("renders footer with links of footer data", () => {
-  render(<Footer data={testData} />)
+test.each([
+  {
+    url: `/research/techniques/show`,
+    label: "Techniques",
+  },
+  {
+    url: `/research/teach/show`,
+    label: "Teaching Protocols",
+  },
+  {
+    url: `/stockcenter`,
+    label: "Dicty Stock Center",
+  },
+  {
+    url: "http://dictybase.org/tools/jbrowse/?data=data%2Fjbrowse%2Fdiscoideum&loc=6%3A1..50022&tracks=reference%2Cgene%2Ctranscript&highlight=",
+    label: "Genome Browser",
+  },
+])("renders footer with links of footer data with $label and $url",
+  ({ label, url }) => {
+    render(<Footer data={testData} />)
 
-  testData.forEach((footerItem) => {
-    const link = screen.getByRole("link", { name: footerItem.label })
+    const link = screen.getByRole("link", { name: label })
     expect(link).toBeInTheDocument()
-    expect(link).toHaveAttribute("href", footerItem.url)
-  })
-})
+    expect(link).toHaveAttribute("href", url)
+  }
+)
