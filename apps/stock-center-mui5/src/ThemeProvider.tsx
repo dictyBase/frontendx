43d9ac83cbca64f7a9ed736c913a88f6d1b051e7
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-22 10:04:21 -0500

    feat(ThemeProvider.tsx): add MUI5 theme provider and remove deprecated material-ui/core
    This commit adds the MUI5 theme provider and removes the deprecated material-ui/core. It also adds a StylesProvider to generate class names with a prefix to avoid conflicts with other libraries.

--- apps/stock-center-mui5/src/ThemeProvider.tsx
+++ apps/stock-center-mui5/src/ThemeProvider.tsx
@@ -1,9 +1,25 @@
-import React from "react"
-import { MuiThemeProvider } from "@material-ui/core/styles"
-import { dictyTheme } from "@dictybase/ui-common"
+import {
+  MuiThemeProvider as MuiThemeProviderMUI4,
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles"
+import { ThemeProvider as MUI5ThemeProvider } from "@mui/material/styles"
+import { dictyTheme as dictyThemeMUI4 } from "@dictybase/ui-common"
+import { dictyTheme as dictyThemeMUI5 } from "@dictybase/ui-common-mui5"
+
+const generateClassName = createGenerateClassName({
+  disableGlobal: true,
+  seed: "dicty-mui-jss",
+})
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => (
-  <MuiThemeProvider theme={dictyTheme}>{children}</MuiThemeProvider>
+  <StylesProvider generateClassName={generateClassName}>
+    <MUI5ThemeProvider theme={dictyThemeMUI5}>
+      <MuiThemeProviderMUI4 theme={dictyThemeMUI4}>
+        {children}
+      </MuiThemeProviderMUI4>
+    </MUI5ThemeProvider>
+  </StylesProvider>
 )
 
 export { ThemeProvider }
