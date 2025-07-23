57c5ce7d87f2b3562e58993c7d99e8ab676e23bb
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-23 18:52:02 -0500

    fix(FrontPageApp.tsx): align imports to be multiline for readability
    The imports are now multiline, which improves readability.

--- apps/dicty-frontpage-mui5/src/app/layout/FrontPageApp.tsx
+++ apps/dicty-frontpage-mui5/src/app/layout/FrontPageApp.tsx
@@ -1,7 +1,11 @@
 import { RouterProvider } from "react-router-dom"
 import { Box } from "@material-ui/core"
 import { makeStyles, Theme } from "@material-ui/core/styles"
-import { HeaderWithAuth, NavbarWithAuth, FooterWithAuth } from "@dictybase/auth-mui5"
+import {
+  HeaderWithAuth,
+  NavbarWithAuth,
+  FooterWithAuth,
+} from "@dictybase/auth-mui5"
 import { useGoogleAnalytics } from "../../common/hooks/useGoogleAnalytics"
 import { ErrorBoundary } from "../../common/components/errors/ErrorBoundary"
 import { frontpageRouter } from "../../routes"
