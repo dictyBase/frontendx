f9bfc8c2b7e6f5037f874070695a68b7c6035f15
Kevin Tun <kevin.tun@northwestern.edu>
2025-07-30 17:42:18 -0500

    feat(genome-page): add unit tests for App, AppProviders, and ThemeProvider components
    This commit adds unit tests for the App, AppProviders, and
    ThemeProvider components in the genome-page application. These tests
    ensure that the components render correctly and that they provide the
    expected functionality.
    - App.test.tsx: Tests that the App component renders its children.
    - AppProviders.test.tsx: Tests that the AppProviders component renders
      a loader when the ApolloClientCache is initializing and renders its
      children when initialization is complete.
    - ThemeProvider.test.tsx: Tests that the ThemeProvider component
      provides theme styles to both MUI4 and MUI5 components.

---
+++ apps/genome-page/components/layout/ThemeProvider.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from "@testing-library/react"
+import { makeStyles as makeStylesMUI5 } from "tss-react/mui"
+import { Box as BoxMUI4, makeStyles as makeStylesMUI4 } from "@material-ui/core"
+import { Box as BoxMUI5 } from "@mui/material"
+import { ThemeProvider } from "./ThemeProvider"
+
+const useMUI4ThemeStyles = makeStylesMUI4((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}))
+
+const useMUI5ThemeStyles = makeStylesMUI5()((theme) => ({
+  root: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+}))
+
+const TestComponentMUI4 = () => {
+  const classes = useMUI4ThemeStyles()
+  return (
+    <BoxMUI4 data-testid="mui4-box" className={classes.root}>
+      test
+    </BoxMUI4>
+  )
+}
+
+const TestComponentMUI5 = () => {
+  const { classes } = useMUI5ThemeStyles()
+  return (
+    <BoxMUI5 data-testid="mui5-box" className={classes.root}>
+      test
+    </BoxMUI5>
+  )
+}
+test("It provides theme styles to MUI4 components", () => {
+  render(
+    <ThemeProvider>
+      <TestComponentMUI4 />
+    </ThemeProvider>,
+  )
+  expect(screen.getByTestId("mui4-box")).toHaveStyle({
+    backgroundColor: "#004080",
+  })
+  expect(screen.getByTestId("mui4-box")).not.toHaveStyle({
+    backgroundColor: "#008080",
+  })
+})
+
+test("It provides theme styles to MUI5 components", () => {
+  render(
+    <ThemeProvider>
+      <TestComponentMUI5 />
+    </ThemeProvider>,
+  )
+  expect(screen.getByTestId("mui5-box")).toHaveStyle({
+    backgroundColor: "#008080",
+  })
+  expect(screen.getByTestId("mui5-box")).not.toHaveStyle({
+    backgroundColor: "#004080",
+  })
+})
