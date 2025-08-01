b31f849d1edacedfdd399f2fae6e6daf365ed867
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-01 01:51:32 -0500

    feat(genome-page): create mockSinglePhenotype.ts to mock single phenotype data
    This commit introduces a new mock file for single phenotype data. This will be used for testing and development purposes in the genome-page application. The mock data includes information about a phenotype, its associated publication, authors, title, journal, and other relevant details.

---
+++ apps/genome-page/mocks/mockSinglePhenotype.ts
@@ -0,0 +1,40 @@
+const singlePhenotype = {
+  phenotype: "aberrant actin filament organization",
+  publication: {
+    id: "12499361",
+    authors: [
+      {
+        last_name: "Fey",
+        first_name: "",
+        initials: "",
+        rank: "",
+      },
+      {
+        last_name: "Stephens",
+        first_name: "",
+        initials: "",
+        rank: "",
+      },
+      {
+        last_name: "Titus",
+        first_name: "",
+        initials: "",
+        rank: "",
+      },
+      {
+        last_name: "Chisholm",
+        first_name: "",
+        initials: "",
+        rank: "",
+      },
+    ],
+    title: "SadA, a novel adhesion receptor in Dictyostelium.",
+    journal: "J Cell Biol",
+    pages: "159:1109-19",
+    volume: "",
+    pub_date: "",
+  },
+}
+
+export { singlePhenotype }
+
