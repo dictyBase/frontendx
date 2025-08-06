8cc82b2a8c3ab9f10a714ec3f41ee960da7b14aa
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-06 09:17:03 -0500

    fix(summaryPage.spec.ts): fix summary page e2e tests
    The summary page e2e tests were failing due to changes in the data returned by the GraphQL API. This commit updates the tests to use a different gene (DDB_G0269114) and updates the expected values to match the data returned by the API for that gene. Also, the tests were updated to check for the correct number of publications.

--- apps/genome-page/e2e/summaryPage.spec.ts
+++ apps/genome-page/e2e/summaryPage.spec.ts
@@ -1,46 +1,74 @@
+/* eslint-disable unicorn/no-null */
 import { test, expect } from "@playwright/test"
-import { GeneOntologyAnnotationQueryResult } from "dicty-graphql-schema"
+import {
+  GeneOntologyAnnotationQueryResult,
+  ListPublicationsWithGeneSummaryQueryResult,
+} from "dicty-graphql-schema"
 import {
   geneGeneralInformationSummaryQueryData,
   geneOntologyAnnotationSummaryQueryData,
+  listPublicationsWithGeneSummaryQueryData,
 } from "./utils/gqlRequestData"
 
-const TEST_GENE = "DDB_G0267382"
+const GRAPHQL_ENDPOINT = "https://graphql.dictybase.dev/graphql"
+
+const TEST_GENE = "DDB_G0269114"
 
 const EXPECTED_GENERAL_INFO = {
-  id: "DDB_G0267382",
-  gene_product: "coronin",
-  name_description: ["corA = CORonin"],
+  id: "DDB_G0269114",
+  name_description: ["num = NUcleoMorphin"],
+  gene_product: "nucleomorphin\ncalmodulin-binding protein",
   synonyms: [],
   description:
-    "actin binding protein regulating actin nucleation, involved in cytokinesis and cell motility",
+    "there are three reported numA isoforms, the largest 97 kDa numA2, the 66 kDa numA1, and the smallest 43 kDA numA3; all isoforms contain a nuclear localization signal, a CaM binding domain, and a DEED (D/E repeat) domain; the larger numA2 contains an additional BRCT (Breast Cancer C_Terminal) domain",
 }
+
 const EXPECTED_GOA = {
-  id: "UniProtKB:P27133!615349167",
+  id: "UniProtKB:Q9U7C9!801562706",
   type: "biological_process",
-  date: "20190926",
-  go_term: "mitotic cytokinesis",
-  evidence_code: "IGI",
-  with: [{ id: "P08799", db: "UniProtKB", name: "" }],
-  // eslint-disable-next-line unicorn/no-null
+  date: "20121023",
+  go_term: "regulation of mitotic nuclear division",
+  evidence_code: "IMP",
+  with: null,
   extensions: null,
 }
 
-test.beforeAll("Renders Gene General Information", async ({ playwright }) => {
+const EXPECTED_REFERENCE = {
+  id: "30781559",
+  title:
+    "Proteins of the Nucleolus of \u003Ci\u003EDictyostelium discoideum\u003C/i\u003E: Nucleolar Compartmentalization, Targeting Sequences, Protein Translocations and Binding Partners.",
+  journal: "Cells",
+  pages: "E167",
+  issue: "2",
+  authors: [{ last_name: "O'Day" }],
+}
+
+test.beforeAll("Test Summary Page API", async ({ playwright }) => {
   const apiContext = await playwright.request.newContext()
 
   const geneGeneralInformation = await apiContext.post(
-    "https://graphql.dictybase.dev/graphql",
+    GRAPHQL_ENDPOINT,
     geneGeneralInformationSummaryQueryData(TEST_GENE),
   )
   const goaSummary = await apiContext.post(
-    "https://graphql.dictybase.dev/graphql",
+    GRAPHQL_ENDPOINT,
     geneOntologyAnnotationSummaryQueryData(TEST_GENE),
   )
 
+  const references = await apiContext.post(
+    GRAPHQL_ENDPOINT,
+    listPublicationsWithGeneSummaryQueryData(TEST_GENE),
+  )
+
+  const { data: goaData }: GeneOntologyAnnotationQueryResult =
+    await goaSummary.json()
+
+  const { data: referencesData }: ListPublicationsWithGeneSummaryQueryResult =
+    await references.json()
+
   expect(goaSummary.ok()).toBeTruthy()
   expect(geneGeneralInformation.ok()).toBeTruthy()
-
+  expect(references.ok()).toBeTruthy()
   expect(await geneGeneralInformation.json()).toEqual(
     expect.objectContaining({
       data: {
@@ -48,12 +76,11 @@ test.beforeAll("Renders Gene General Information", async ({ playwright }) => {
       },
     }),
   )
-
-  const { data: goaData }: GeneOntologyAnnotationQueryResult =
-    await goaSummary.json()
-
   expect(goaData?.geneOntologyAnnotation).toContainEqual(
     expect.objectContaining(EXPECTED_GOA),
+  )
+  expect(referencesData?.listPublicationsWithGene).toContainEqual(
+    expect.objectContaining(EXPECTED_REFERENCE),
   )
 })
 
@@ -64,9 +91,7 @@ test.beforeEach(async ({ page }) => {
 test.describe(() => {
   test.describe.configure({ retries: 3 })
   test("Renders General information panel", async ({ page }) => {
-    // General Information Panel
     await expect(page.getByText("General Information")).toBeVisible()
-
     await expect(
       page.getByText(new RegExp(`^${EXPECTED_GENERAL_INFO.id}$`)),
     ).toBeVisible()
@@ -82,19 +107,21 @@ test.describe(() => {
   })
 
   test("Renders Gene Ontology Annotations Panel ", async ({ page }) => {
-    // General Information Panel
     await expect(page.getByText("Gene Ontology Annotations")).toBeVisible()
     await expect(
-      page.getByText("protein bindingwith UniProtKB:Q54K81 (IPI)"),
+      page.getByText("protein bindingwith UniProtKB:Q54RF4 (IPI)"),
     ).toBeVisible()
-    await expect(page.getByText("phagocytosis (IMP)")).toBeVisible()
     await expect(
-      page.getByText("pathogen-containing vacuole (IDA)"),
+      page.getByText("regulation of mitotic nuclear division (IMP)"),
+    ).toBeVisible()
+    await expect(
+      page.getByText(
+        "nuclear envelopeexistence_starts_and_ends_during GO:0000089  (IDA)",
+      ),
     ).toBeVisible()
   })
 
   test("Renders Publication Panel", async ({ page }) => {
-    // General Information Panel
-    await expect(page.getByText("Publications (5 of 90)")).toBeVisible()
+    await expect(page.getByText("Publications (5 of 17)")).toBeVisible()
   })
 })
