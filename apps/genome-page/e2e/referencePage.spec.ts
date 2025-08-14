d0592f3333b27e5f7114edd8e020e6d1510c308a
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-13 22:47:45 -0500

    feat(genome-page): add e2e tests for reference page
    This commit adds comprehensive end-to-end tests for the reference
    page in the genome-page application. The tests cover various
    aspects of the page, including:
    
    - Displaying references from the API, including the correct number
      of rows, full title, publication date, journal, pages, other genes
      mentioned, and authors' last name.
    - Navigating to a gene's summary page when clicking on a mentioned
      gene.
    - Navigating to the related genes page when clicking on the "See All"
      button.
    - Sorting by newest first (default), oldest first, title (A to Z),
      and title (Z to A).
    
    These tests ensure the reference page functions correctly and
    provides a reliable user experience.

--- apps/genome-page/e2e/referencePage.spec.ts
+++ apps/genome-page/e2e/referencePage.spec.ts
@@ -52,10 +52,90 @@ test.beforeEach(async ({ page }) => {
 })
 
 test("Displays References from API", async ({ page }) => {
-  const referenceItem = page
-    .getByLabel("phenotypes-table")
-    .locator("div")
-    .filter({ hasText: "Detection of calmodulin-" })
+  const references = page.locator("tbody").getByRole("row")
 
-  await expect(page.getByText(EXPECTED_REFERENCE.title)).toBeVisible()
+  const referenceItem = references.filter({
+    hasText: "Detection of calmodulin-",
+  })
+
+  // Displays correct number of rows
+  expect(page.getByText("17 References")).toBeVisible()
+  expect(references).toHaveCount(17)
+  // Displays full title
+  await expect(referenceItem.getByText(EXPECTED_REFERENCE.title)).toBeVisible()
+  // Displays publication date
+  await expect(referenceItem.getByText("July 31st, 2001")).toBeVisible()
+  // Displays journal
+  await expect(
+    referenceItem.getByText(EXPECTED_REFERENCE.journal),
+  ).toBeVisible()
+  // Displays pages
+  await expect(referenceItem.getByText(EXPECTED_REFERENCE.pages)).toBeVisible()
+  // Displays other genes mentioned
+  await expect(referenceItem.getByText("calA")).toBeVisible()
+  await expect(referenceItem.getByText("cmbB")).toBeVisible()
+  await expect(referenceItem.getByText("cmbC")).toBeVisible()
+  // Displays authors' last name
+  await expect(
+    referenceItem.getByText(EXPECTED_REFERENCE.authors[0].last_name),
+  ).toBeVisible()
+  await expect(
+    referenceItem.getByText(EXPECTED_REFERENCE.authors[1].last_name),
+  ).toBeVisible()
+})
+
+test("Clicking on mentioned gene navigates to that gene's summary page", async ({
+  page,
+}) => {
+  await page.getByText("calA").first().click()
+  await expect(
+    page.getByRole("heading", { name: "Gene Summary for calA" }),
+  ).toBeVisible()
+})
+
+test("Clicking on `See all` button navigates to the related genes page", async ({
+  page,
+}) => {
+  await page.getByText("See All").first().click()
+  await expect(
+    page.getByRole("heading", { name: /Genes mentioned in/ }),
+  ).toBeVisible()
+})
+
+test("Sorts by newest first by default", async ({ page }) => {
+  const references = page.locator("tbody").getByRole("row")
+
+  await expect(references.first()).toHaveText(/November 25th, 2019/)
+  await expect(references.last()).toHaveText(/July 31st, 2001/)
+})
+
+test("Sort by oldest first", async ({ page }) => {
+  const references = page.locator("tbody").getByRole("row")
+
+  // eslint-disable-next-line sonarjs/no-duplicate-string
+  await page.getByLabel("Newest First").click()
+  await page.getByRole("option", { name: "Oldest First" }).click()
+
+  await expect(references.first()).toHaveText(/July 31st, 2001/)
+  await expect(references.last()).toHaveText(/November 25th, 2019/)
+})
+
+test("Sort by title (A to Z) ", async ({ page }) => {
+  const references = page.locator("tbody").getByRole("row")
+
+  await page.getByLabel("Newest First").click()
+  await page.getByRole("option", { name: "Title (A to Z)" }).click()
+
+  await expect(references.first()).toHaveText(/An N-terminal nuclear/)
+  await expect(references.last()).toHaveText(/Signalling and sex in the/)
+})
+
+test("Sort by title (Z to A) ", async ({ page }) => {
+  const references = page.locator("tbody").getByRole("row")
+
+  await page.getByLabel("Newest First").click()
+  await page.getByRole("option", { name: "Title (Z to A)" }).click()
+
+  await expect(references.first()).toHaveText(/Signalling and sex in the/)
+  await expect(references.last()).toHaveText(/An N-terminal nuclear/)
 })
