fd2830f7b1da9ee76518c3101e5b78f5bd4dda1a
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-19 11:56:28 -0500

    feat(strainPage.spec.ts): add e2e tests for strain details page
    Adds e2e tests to verify that the strain details page displays
    the correct information for a given strain, including strain
    descriptor, names, characteristics, genetic modification,
    mutagenesis method, and genotypes.
    The tests use Playwright to navigate to the strain details page
    and assert that the expected text is visible on the page.
    The tests use data from the EXPECTED_STRAIN constant to verify
    the displayed information.
    The tests use fp-ts to iterate over the characteristics and
    genotypes arrays and assert that each element is visible on the
    page.
    The empty test was removed.

--- apps/stock-center/src/e2e/strainPage.spec.ts
+++ apps/stock-center/src/e2e/strainPage.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect } from "@playwright/test"
+import { pipe } from "fp-ts/lib/function.js"
+import { map as Amap } from "fp-ts/lib/Array.js"
 import { strainQueryData } from "./utils/gqlRequestData"
 import { EXPECTED_STRAIN } from "./utils/expectedData"
 
@@ -17,4 +19,42 @@ test.beforeAll("Test Reference Page API", async ({ playwright }) => {
   })
 })
 
-test("", () => {})
+test.beforeEach(async ({ page }) => {
+  await page.goto(`/stockcenter/strains/${TEST_STRAIN_ID}`)
+})
+
+test("Displays Strain Details", async ({ page }) => {
+  const details = page.locator("main").getByRole("list")
+  // Strain Descriptor
+  await expect(details.getByText(/piaA\(G917D\)\/hephA-/)).toBeVisible()
+  // Strain Names
+  await expect(details.getByText(/HSB1HectPH1-Ko/)).toBeVisible()
+  await expect(
+    details.getByText(
+      /suppressor mutant; deletion of hephA suppresses the agg- phenotype/,
+    ),
+  ).toBeVisible()
+  await expect(details.getByText(EXPECTED_STRAIN.id)).toBeVisible()
+  await Promise.all(
+    pipe(
+      EXPECTED_STRAIN.characteristics,
+      Amap((characteristic) =>
+        expect(page.getByText(characteristic).first()).toBeVisible(),
+      ),
+    ),
+  )
+  await expect(
+    details.getByText(EXPECTED_STRAIN.genetic_modification),
+  ).toBeVisible()
+  await expect(
+    details.getByText(EXPECTED_STRAIN.mutagenesis_method),
+  ).toBeVisible()
+  await Promise.all(
+    pipe(
+      EXPECTED_STRAIN.genotypes,
+      Amap((genotype) =>
+        expect(page.getByText(genotype).first()).toBeVisible(),
+      ),
+    ),
+  )
+})
