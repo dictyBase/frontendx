88a0a90faf8944fbd907bad253e9526d0bf9b786
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-18 22:51:28 -0500

    refactor(snapshots.spec.ts): move beforeEach hook to avoid code duplication
    The beforeEach hook is now used to navigate to the page and wait for all images to load. This avoids code duplication in each test and makes the tests more readable.

--- apps/stock-center/src/e2e/snapshots.spec.ts
+++ apps/stock-center/src/e2e/snapshots.spec.ts
@@ -1,38 +1,29 @@
 import { test, expect } from "@playwright/test"
 import { waitForImageLoad } from "./utils/waitForImageLoad"
 
-test("Header snapshot", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/")
   await page.waitForLoadState("networkidle")
   // Wait for all images to load
   await page.waitForFunction(waitForImageLoad)
+})
+
+test("Header snapshot", async ({ page }) => {
   const header = page.locator("header")
   await expect(header).toHaveScreenshot()
 })
 
 test("Footer snapshot", async ({ page }) => {
-  await page.goto("/")
-  await page.waitForLoadState("networkidle")
-  // Wait for all images to load
-  await page.waitForFunction(waitForImageLoad)
   const footer = page.locator("footer")
   await expect(footer).toHaveScreenshot()
 })
 
 test("Navbar snapshot", async ({ page }) => {
-  await page.goto("/")
-  await page.waitForLoadState("networkidle")
-  // Wait for all images to load
-  await page.waitForFunction(waitForImageLoad)
   const navbar = page.locator("nav")
   await expect(navbar).toHaveScreenshot()
 })
 
 test("Navbar Item Menu snapshot", async ({ page }) => {
-  await page.goto("/")
-  await page.waitForLoadState("networkidle")
-  // Wait for all images to load
-  await page.waitForFunction(waitForImageLoad)
   const genomesDropdown = page.getByText("Genomes")
   await genomesDropdown.click()
   const genomesList = page.getByTestId("navbar-dropdown-Genomes")
