4dbcd27d71fdb944a687d367a93e43b369142df2
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-18 23:35:07 -0500

    fix(playwright.config.ts): use process.loadEnvFile to load env vars
    The playwright config was not loading the environment variables correctly.
    This change fixes that by using the process.loadEnvFile function.

--- apps/stock-center/playwright.config.ts
+++ apps/stock-center/playwright.config.ts
@@ -2,9 +2,8 @@ import { defineConfig, devices } from "@playwright/test"
 
 /**
  * Read environment variables from file.
- * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+process.loadEnvFile(".env.development")
 
 /**
  * See https://playwright.dev/docs/test-configuration.
