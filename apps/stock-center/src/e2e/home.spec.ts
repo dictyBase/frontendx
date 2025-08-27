cbe2c8feb8d4b503cd8872e931d9404c67f5f21f
Kevin Tun <kevin.tun@northwestern.edu>
2025-08-27 10:11:07 -0500

    feat(e2e): add end-to-end tests for the stock center homepage
    This commit introduces end-to-end tests for the stock center
    homepage using Playwright. The tests verify the presence of the
    welcome message, navigation to the cart, visibility of catalog and
    download links, and loading of all carousel images. These tests
    ensure the basic functionality and UI elements of the homepage are
    working as expected.

---
+++ apps/stock-center/src/e2e/home.spec.ts
@@ -0,0 +1,82 @@
+import { test, expect } from "@playwright/test"
+import { pipe } from "fp-ts/lib/function.js"
+import { map as Amap } from "fp-ts/lib/Array.js"
+
+const CAROUSEL_IMAGE_ALT = 'img[alt*="mutant"]'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:3003/stockcenter")
+})
+
+test("Displays welcome message", async ({ page }) => {
+  const heading = page.getByRole("heading", {
+    name: "Welcome to Dicty Stock Center (DSC)",
+  })
+  await expect(heading).toBeVisible()
+})
+
+test("The cart button navigates the user to their cart", async ({ page }) => {
+  const cartLink = page.getByRole("link", { name: "shopping cart" })
+  await expect(cartLink).toHaveAttribute("href", "/stockcenter/cart")
+
+  await cartLink.click()
+  await expect(page).toHaveURL(/\/stockcenter\/cart/)
+})
+
+test("Displays `catalog` links", async ({ page }) => {
+  const main = page.locator("main")
+  await expect(main.getByRole("link", { name: "Strain Catalog" })).toBeVisible()
+  await expect(
+    main.getByRole("link", { name: "Plasmid Catalog" }),
+  ).toBeVisible()
+  await expect(
+    main.getByRole("link", { name: "Bacterial Strains" }),
+  ).toBeVisible()
+  await expect(main.getByRole("link", { name: "GWDI Catalog" })).toBeVisible()
+  await expect(
+    main.getByRole("link", { name: "GoldenBraid List" }),
+  ).toBeVisible()
+  await expect(
+    main.getByRole("link", { name: "Additional Materials" }),
+  ).toBeVisible()
+})
+
+test("Displays `downloads` links", async ({ page }) => {
+  await expect(
+    page.getByRole("link", { name: "Phenotype Ontology" }),
+  ).toBeVisible()
+  await expect(
+    page.getByRole("link", { name: "Strain Characteristics" }),
+  ).toBeVisible()
+  await expect(
+    page.getByRole("link", { name: "Mutagenesis Methods" }),
+  ).toBeVisible()
+  await expect(
+    page.getByRole("link", { name: "Plasmid Keywords" }),
+  ).toBeVisible()
+})
+
+test("All Carousel images are loaded", async ({ page }) => {
+  const images = page.locator(CAROUSEL_IMAGE_ALT)
+
+  await expect(images).toHaveCount(6)
+
+  await Promise.all(
+    pipe(
+      await images.all(),
+      Amap((image) => expect(image).toBeVisible()),
+    ),
+  )
+
+  await Promise.all(
+    pipe(
+      await images.all(),
+      Amap((image) =>
+        image.evaluate(({ naturalWidth }: HTMLImageElement) => naturalWidth),
+      ),
+      Amap(async (naturalWidth) =>
+        expect(await naturalWidth).toBeGreaterThan(0),
+      ),
+    ),
+  )
+})
