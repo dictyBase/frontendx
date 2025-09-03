f2ac1d4f32955e5c7ccdbba6a4e5e74765a89a07
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-03 09:11:09 -0500

    feat(CountryDropdown.tsx): move countryToFlag function to utils
    The countryToFlag function was moved to a separate utils file to promote code reuse and maintainability. This allows other components to use the function without having to duplicate the code.

--- packages/ui-dsc/src/order/CountryDropdown.tsx
+++ packages/ui-dsc/src/order/CountryDropdown.tsx
@@ -3,17 +3,7 @@ import Autocomplete from "@material-ui/lab/Autocomplete"
 import TextField from "@material-ui/core/TextField"
 import { useController } from "react-hook-form"
 import { countryList, CountryOption } from "../utils/countryList"
-
-// ISO 3166-1 alpha-2 https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-const countryToFlag = (isoCode: string) => {
-  // verify fromCodePoint is a valid method for browser
-  if (String.fromCodePoint !== undefined) {
-    return isoCode.replaceAll(/./g, (char: string) =>
-      String.fromCodePoint((char.codePointAt(0) as number) + 127_397),
-    )
-  }
-  return isoCode
-}
+import { countryToFlag } from "../utils/countryToFlag"
 
 type CountryDropdownProperties = {
   fieldName: string
