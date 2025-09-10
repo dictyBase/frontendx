2b2bfcef351eb7b423ec6dac766ddee87aa7674c
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-10 00:06:20 -0500

    feat(ui-dsc): add phenotype tab test to EditableStrainDetailsCard
    Added a test to ensure the Phenotypes tab renders correctly when a strain has phenotypes in EditableStrainDetailsCard. The existing StrainDetailsCard test was updated to include tabValue and setTabValue props.

--- packages/ui-dsc/src/__tests__/EditableStrainDetailsCard.test.tsx
+++ packages/ui-dsc/src/__tests__/EditableStrainDetailsCard.test.tsx
@@ -1,13 +1,13 @@
 import { test, expect } from "vitest"
-import { render, screen } from "@testing-library/react"
+import { screen, render } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
-import { StrainDetailsCard } from "../catalog/StrainDetailsCard"
+import { EditableStrainDetailsCard } from "../catalog/EditableStrainDetailsCard"
 import { strainWithPhenotype } from "../mocks/mockStrain"
 
 test("if the strain has 1 or more phenotypes, render a `Phenotypes` tab", () => {
   render(
     <MemoryRouter>
-      <StrainDetailsCard
+      <EditableStrainDetailsCard
         data={strainWithPhenotype}
         tabValue={1}
         setTabValue={() => {}}
