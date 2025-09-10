94711c3b48b10c442005471f0eb24d4e6e6156f6
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-10 00:21:57 -0500

    feat(ui-dsc): add tests for strain details and phenotype tab rendering
    Added tests to verify the rendering of strain details and the conditional rendering of the Phenotypes tab based on the presence of phenotypes. The tests ensure that strain details are displayed correctly and that the Phenotypes tab is only rendered when the strain has one or more phenotypes.

--- packages/ui-dsc/src/__tests__/EditableStrainDetailsCard.test.tsx
+++ packages/ui-dsc/src/__tests__/EditableStrainDetailsCard.test.tsx
@@ -2,9 +2,49 @@ import { test, expect } from "vitest"
 import { screen, render } from "@testing-library/react"
 import { MemoryRouter } from "react-router-dom"
 import { EditableStrainDetailsCard } from "../catalog/EditableStrainDetailsCard"
-import { strainWithPhenotype } from "../mocks/mockStrain"
+import { strainWithPhenotype, availableStrain } from "../mocks/mockStrain"
 
-test("if the strain has 1 or more phenotypes, render a `Phenotypes` tab", () => {
+test("Displays strain details", () => {
+  render(
+    <MemoryRouter>
+      <EditableStrainDetailsCard
+        data={strainWithPhenotype}
+        tabValue={0}
+        setTabValue={() => {}}
+      />
+    </MemoryRouter>,
+  )
+
+  expect(screen.getByText("Strain Descriptor")).toBeVisible()
+  expect(screen.getByText("Strain Names")).toBeVisible()
+  expect(screen.getByText("Strain Summary")).toBeVisible()
+  expect(screen.getByText("Systematic Name")).toBeVisible()
+  expect(screen.getByText("Strain Characteristics")).toBeVisible()
+  expect(screen.getByText("Genetic Modification")).toBeVisible()
+  expect(screen.getByText("Mutagenesis Method")).toBeVisible()
+  expect(screen.getByText("Parental Strain")).toBeVisible()
+  expect(screen.getByText("Plasmid")).toBeVisible()
+  expect(screen.getByText("Associated Gene(s)")).toBeVisible()
+  expect(screen.getByText("Genotype")).toBeVisible()
+  expect(screen.getByText("Depositor")).toBeVisible()
+  expect(screen.getByText("Reference(s)")).toBeVisible()
+})
+
+test("If the strain has 0 phenotypes, does not render a `Phenotypes` tab", () => {
+  render(
+    <MemoryRouter>
+      <EditableStrainDetailsCard
+        data={availableStrain}
+        tabValue={1}
+        setTabValue={() => {}}
+      />
+    </MemoryRouter>,
+  )
+
+  expect(screen.queryByText(/Phenotypes/)).toBeNull()
+})
+
+test("If the strain has 1 or more phenotypes, render a `Phenotypes` tab", () => {
   render(
     <MemoryRouter>
       <EditableStrainDetailsCard
