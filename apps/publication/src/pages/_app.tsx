606704b8495477a75c2705fa84c6020f750ce6b8
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:49:40 -0500

    feat(publication): remove authentication context and related components
    The authentication context and related components like AuthLoader, OauthCallback, AuthStore, and MockAuthProvider have been removed. The _app.tsx file was updated to remove the AuthProvider. This change simplifies the application by removing authentication logic.

--- apps/publication/src/pages/_app.tsx
+++ apps/publication/src/pages/_app.tsx
@@ -3,30 +3,27 @@ import CssBaseline from "@material-ui/core/CssBaseline"
 import Head from "next/head"
 import { AppProviders } from "../components/layout/AppProviders"
 import { App } from "../components/layout/App"
-import { AuthProvider } from "../components/auth/AuthStore"
 
 const PublicationApp = ({ Component, pageProps }: AppProps) => (
-  <AuthProvider>
-    <AppProviders>
-      <CssBaseline />
-      <App>
-        <Head>
-          <link rel="shortcut icon" href="favicon.ico" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <meta name="theme-color" content="#000000" />
+  <AppProviders>
+    <CssBaseline />
+    <App>
+      <Head>
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content="#000000" />
 
-          <link rel="manifest" href="manifest.json" />
+        <link rel="manifest" href="manifest.json" />
 
-          <title>dictyBase Literature</title>
-        </Head>
+        <title>dictyBase Literature</title>
+      </Head>
 
-        <Component {...pageProps} />
-      </App>
-    </AppProviders>
-  </AuthProvider>
+      <Component {...pageProps} />
+    </App>
+  </AppProviders>
 )
 
 // eslint-disable-next-line import/no-default-export
