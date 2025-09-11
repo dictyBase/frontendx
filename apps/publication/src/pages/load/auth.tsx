7892c2ec5a98c4b59008c4a9eba8121503574c3c
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:49:40 -0500

    feat(publication): remove authentication context and related components
    The authentication context and related components like AuthLoader, OauthCallback, AuthStore, and MockAuthProvider have been removed. The _app.tsx file was updated to remove the AuthProvider. This change simplifies the application by removing authentication logic.

--- apps/publication/src/pages/load/auth.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { AuthLoader } from "../../components/auth/AuthLoader"
-
-const AuthPage = () => <AuthLoader />
-
-// eslint-disable-next-line import/no-default-export
-export default AuthPage
