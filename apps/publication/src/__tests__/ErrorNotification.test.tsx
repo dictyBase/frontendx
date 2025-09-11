06232a1dd94c312e60c455a3da86c7a9cb1e8e51
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:37:51 -0500

    test(publication): replace describe blocks with test blocks in test files
    The describe blocks in the test files were replaced with test blocks to simplify the test structure and align with best practices for writing unit tests. Additionally, the mock for next/router was updated to use vi instead of jest.

--- apps/publication/src/__tests__/ErrorNotification.test.tsx
+++ apps/publication/src/__tests__/ErrorNotification.test.tsx
@@ -2,9 +2,7 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import { ErrorNotification } from "../components/errors/ErrorNotification"
 
-describe("components/errors/ErrorNotification", () => {
-  it("should display provided error message", () => {
-    render(<ErrorNotification error="not a marine biologist" />)
-    expect(screen.getByText(/not a marine biologist/)).toBeInTheDocument()
-  })
+test("should display provided error message", () => {
+  render(<ErrorNotification error="not a marine biologist" />)
+  expect(screen.getByText(/not a marine biologist/)).toBeInTheDocument()
 })
