83e08595ea4229e9e5d154858396afa70a8c94c1
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:38:21 -0500

    feat(publication): configure vitest for testing React components
    Configured Vitest with jsdom environment, setup files, coverage and globals.
    Also configured tsconfig to include vitest types and jsx to react-jsx.
    This setup enables testing of React components with Vitest.

---
+++ apps/publication/vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/__tests__/mocks/**",
+    ],
+    coverage: {
+      include: ["**/components/**/*.{ts,tsx}", "**/common/hooks/*.{ts,tsx}"],
+      exclude: ["**/__tests__/mocks/**", "**/common/hooks/"],
+      reporter: ["text", "json", "html"],
+    },
+    globals: true,
+  },
+})
+
