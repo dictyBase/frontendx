6f96f339eed45689c9f89cd4ecf4193fb1f7f8c8
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:50:35 -0500

    feat: remove auth related tests
    The AuthLoader.test.tsx and AuthStore.test.tsx tests were removed.
    These tests are no longer needed because the related components
    have been removed.

--- apps/publication/src/__tests__/AuthLoader.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { render, screen } from "@testing-library/react"
-import { AuthLoader } from "../components/auth/AuthLoader"
-
-test("displays expected text", () => {
-  render(<AuthLoader />)
-  expect(screen.getByText(/Logging in/)).toBeInTheDocument()
-})
