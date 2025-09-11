ef87479cea85be66c9d0b93aae132d93d422a0fe
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 10:31:38 -0500

    test(AuthLoader.test.tsx): remove describe block from AuthLoader test
    The describe block was removed to reduce nesting and improve readability of the test file.

--- apps/publication/src/__tests__/AuthLoader.test.tsx
+++ apps/publication/src/__tests__/AuthLoader.test.tsx
@@ -2,9 +2,7 @@ import React from "react"
 import { render, screen } from "@testing-library/react"
 import { AuthLoader } from "../components/auth/AuthLoader"
 
-describe("features/Authentication/AuthLoader", () => {
-  it("displays expected text", () => {
-    render(<AuthLoader />)
-    expect(screen.getByText(/Logging in/)).toBeInTheDocument()
-  })
+test("displays expected text", () => {
+  render(<AuthLoader />)
+  expect(screen.getByText(/Logging in/)).toBeInTheDocument()
 })
