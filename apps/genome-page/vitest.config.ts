1e6eb47b1d3265547257f45c0a2f57b2666dd3e9
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 16:51:19 -0500

    feat(genome-page): add vite-tsconfig-paths plugin to resolve imports
    The vite-tsconfig-paths plugin is added to resolve imports using the paths defined in the tsconfig.json file. This simplifies import statements and improves code readability.

--- apps/genome-page/vitest.config.ts
+++ apps/genome-page/vitest.config.ts
@@ -1,7 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts", "./vitest.setupEnv.ts"],
