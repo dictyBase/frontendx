b7f775122b2baf8e1e961056334521fa92c0c88e
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 22:28:36 -0500

    feat(genome-page): add alias configuration for imports in vitest
    The alias configuration simplifies imports in vitest tests by creating shortcuts for common directories. This improves readability and maintainability of test files.

--- apps/genome-page/vitest.config.ts
+++ apps/genome-page/vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   test: {
     environment: "jsdom",
@@ -17,5 +18,13 @@ export default defineConfig({
       reporter: ["text", "json", "html"],
     },
     globals: true,
+  },
+  resolve: {
+    alias: {
+      common: new URL("common/", import.meta.url).pathname,
+      components: new URL("components/", import.meta.url).pathname,
+      styles: new URL("styles/", import.meta.url).pathname,
+      mocks: new URL("mocks/", import.meta.url).pathname,
+    },
   },
 })
