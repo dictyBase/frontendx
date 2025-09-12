6ce8f14166c588ac5e3906fda0be9d6dfa440790
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-11 22:28:15 -0500

    refactor: remove vite-tsconfig-paths plugin and config from genome-page
    The vite-tsconfig-paths plugin was removed because it is no longer needed.

--- apps/genome-page/vitest.config.ts
+++ apps/genome-page/vitest.config.ts
@@ -1,9 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts", "./vitest.setupEnv.ts"],
