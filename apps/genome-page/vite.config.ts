61d0988fb195521c9ba792e558f847d993f744e9
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-16 04:06:00 -0500

    feat(genome-page): add vite config for genome-page application
    The vite configuration file was added to configure the genome-page application. It includes plugins, server settings, base URL, resolve aliases, and test configurations. This setup standardizes the build process, simplifies module resolution, and configures testing environments for the genome-page application.

---
+++ apps/genome-page/vite.config.ts
@@ -0,0 +1,40 @@
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig, coverageConfigDefaults } from "vitest/config"
+
+// https://vitejs.dev/config/
+const viteConfig = defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3002,
+  },
+  base: "/gene",
+  resolve: {
+    alias: {
+      common: new URL("common/", import.meta.url).pathname,
+      components: new URL("components/", import.meta.url).pathname,
+      styles: new URL("styles/", import.meta.url).pathname,
+      mocks: new URL("mocks/", import.meta.url).pathname,
+    },
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts", "./vitest.setupEnvironment.ts"],
+    testTimeout: 10_000,
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/e2e/**",
+      "**/__tests__/mocks/**",
+    ],
+    coverage: {
+      include: ["**/components/**/*.{ts,tsx}", "**/common/hooks/*.{ts,tsx}"],
+      exclude: ["**/__tests__/mocks/**", "**/common/hooks/", ...coverageConfigDefaults.exclude],
+      reporter: ["text", "json", "html"],
+      all: false,
+    },
+  },
+})
+
+// eslint-disable-next-line import/no-default-export
+export default viteConfig
