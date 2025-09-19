7436d746f3b2bbc202d5fc47b6312c1214bd5f20
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-19 04:40:49 -0500

    feat(playwright.config.ts): remove conditional github reporter
    The github reporter was removed to simplify the configuration and ensure consistent reporting across all environments.

--- apps/genome-page/playwright.config.ts
+++ apps/genome-page/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "list",
+  reporter: "list",
   timeout: 60_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect: {
