ad300abd65907828e600e3fbfae6821f9d6f9fc0
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-19 01:07:45 -0500

    feat(genome-page): add OtherError component and tests for error handling
    The new OtherError component displays a generic error message. The ErrorPageWrapper component was updated to handle more specific error scenarios, and new tests were added to ensure the correct error messages are displayed for different error types.

--- apps/genome-page/src/components/errors/OtherError.tsx
+++ apps/genome-page/src/components/errors/OtherError.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { Box, Grid, Typography } from "@material-ui/core"
 import { Image } from "@dictybase/dicty-image-mui5"
 import { ErrorMessage } from "./ErrorMessage"
