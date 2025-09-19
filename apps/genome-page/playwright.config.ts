70092aa01eb98083a72aeb40fde42be6d7ad96e8
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-19 04:05:14 -0500

    feat(playwright.config.ts): use BASE_URL env variable for playwright
    The playwright configuration now uses the BASE_URL environment variable for the webServer URL, allowing the tests to be run against different environments.

--- apps/genome-page/playwright.config.ts
+++ apps/genome-page/playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "yarn dev",
-    port: 3000,
+    url: process.env.BASE_URL,
     reuseExistingServer: !process.env.CI,
   },
   use: {
