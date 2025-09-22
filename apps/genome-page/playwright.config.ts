376e2cc17c9206fd15cedead3347e139c4430808
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-22 00:40:31 -0500

    fix(playwright.config.ts): add /gene to baseURL for playwright tests
    The baseURL in the playwright configuration was updated to include "/gene". This ensures that the tests navigate to the correct base URL for the genome page, which is located under the /gene path.

--- apps/genome-page/playwright.config.ts
+++ apps/genome-page/playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
   },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `${process.env.BASE_URL}`,
+    baseURL: `${process.env.BASE_URL}/gene`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
