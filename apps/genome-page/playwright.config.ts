0480655722d90dab439948a2d7d156c33307fbee
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-21 23:59:29 -0500

    feat(playwright.config.ts): configure playwright for CI environment
    The playwright configuration is updated to optimize test execution in CI environments. Parallel execution is enabled by default, but limited to a single worker in CI to prevent resource contention. The number of retries is set to 2 in CI to handle flaky tests.

--- apps/genome-page/playwright.config.ts
+++ apps/genome-page/playwright.config.ts
@@ -17,13 +17,13 @@ try {
 export default defineConfig({
   testDir: "./src/e2e",
   /* Run tests in files in parallel */
-  fullyParallel: !process.env.CI,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? "github" : "list",
   timeout: 60_000,
