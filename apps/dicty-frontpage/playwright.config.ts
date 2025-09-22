93869d6d672eea55e0f78c6ff71c2cdf27389e09
Kevin Tun <kevin.tun@northwestern.edu>
2025-09-21 23:14:51 -0500

    chore(playwright.config.ts): disable eslint rule for default export
    The eslint rule `import/no-default-export` is disabled to allow the playwright configuration to use default export.

--- apps/dicty-frontpage/playwright.config.ts
+++ apps/dicty-frontpage/playwright.config.ts
@@ -9,6 +9,7 @@ import { defineConfig, devices } from "@playwright/test"
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
   testDir: "./src/e2e",
   /* Run tests in files in parallel */
