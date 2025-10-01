d2f7c11b9ef6fcba72cfb6926a8b9ee53e1e4c3d
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-01 13:20:33 -0500

    feat(Heading.tsx): apply direct styling to Heading component for customization
    The Heading component now uses direct styling instead of useStyles to customize the padding, font size, margin, and border bottom.

--- packages/ui-dsc/src/home/Heading.tsx
+++ packages/ui-dsc/src/home/Heading.tsx
@@ -1,14 +1,20 @@
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
-import { useStyles } from "./homeStyles"
 
-const Heading = () => {
-  const classes = useStyles({})
-  return (
-    <Grid item className={classes.header}>
-      <Typography variant="h1">Welcome to Dicty Stock Center (DSC)</Typography>
-    </Grid>
-  )
-}
+const Heading = () => (
+  <Grid
+    item
+    sx={{
+      paddingBottom: "0px !important",
+      "& h1": {
+        fontSize: "48px",
+        marginTop: "0px",
+        marginBottom: "25px",
+        borderBottom: "1px solid #eee",
+      },
+    }}>
+    <Typography variant="h1">Welcome to Dicty Stock Center (DSC)</Typography>
+  </Grid>
+)
 
 export { Heading }
