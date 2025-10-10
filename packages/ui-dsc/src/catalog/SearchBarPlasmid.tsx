ce6c52cc20e097dc8e989989fde8ada7505150bc
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-10 11:14:25 -0500

    feat(SearchBarPlasmid): create SearchBarPlasmid component for catalog
    The new SearchBarPlasmid component was created, which includes a FilterDropdown, SearchBox, and AppBarHelp. This component will be used in the catalog page to search for plasmids.

---
+++ packages/ui-dsc/src/catalog/SearchBarPlasmid.tsx
@@ -0,0 +1,34 @@
+import { Grid } from "@material-ui/core"
+import { defaultFilter, fieldsToVariables } from "@dictybase/hook-dsc"
+import { makeStyles } from "@material-ui/core/styles"
+import { FilterDropdown } from "./FilterDropdown"
+import { SearchBox } from "./SearchBox"
+import { AppBarHelp } from "./AppBarHelp"
+
+const useStyles = makeStyles({
+  searchBox: {
+    flexBasis: "41.5%",
+  },
+})
+
+const SearchBarPlasmid = () => {
+  const { searchBox } = useStyles()
+  return (
+    <Grid container>
+      <Grid item>
+        <FilterDropdown
+          param={defaultFilter.param}
+          value={defaultFilter.value}
+        />
+      </Grid>
+      <Grid item className={searchBox}>
+        <SearchBox fields={Object.keys(fieldsToVariables)} />
+      </Grid>
+      <Grid item>
+        <AppBarHelp />
+      </Grid>
+    </Grid>
+  )
+}
+
+export { SearchBarPlasmid }
