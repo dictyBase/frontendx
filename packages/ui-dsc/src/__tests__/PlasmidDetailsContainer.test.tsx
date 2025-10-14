d04b29b6bdb5b67ed468c671c1f1b6d4dc1b1750
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-14 11:04:47 -0500

    feat(PlasmidDetailsContainer.test.tsx): add tests for container component
    Added tests for the PlasmidDetailsContainer component to ensure it correctly renders plasmid details when data is present. This includes setting up a mock router and Apollo provider to simulate the component's environment.

--- packages/ui-dsc/src/__tests__/PlasmidDetailsContainer.test.tsx
+++ packages/ui-dsc/src/__tests__/PlasmidDetailsContainer.test.tsx
@@ -1,7 +1,49 @@
 import { describe, test, expect } from "vitest"
+import { createMemoryRouter, RouterProvider } from "react-router-dom"
+import { MockedProvider } from "@apollo/client/testing"
 import { render, screen } from "@testing-library/react"
-import { PlasmidDetails } from "../catalog/PlasmidDetailsContainer"
+import { PlasmidDocument } from "dicty-graphql-schema"
+import {
+  PlasmidDetails,
+  PlasmidDetailsContainer,
+} from "../catalog/PlasmidDetailsContainer"
 import { mockPlasmid } from "../mocks/mockPlasmid"
+
+const routes = [
+  {
+    path: "/plasmids/:id",
+    element: <PlasmidDetailsContainer />,
+  },
+]
+
+const router = createMemoryRouter(routes, {
+  initialEntries: [`/plasmids/${mockPlasmid.id}`],
+})
+
+const mocks = [
+  {
+    request: {
+      query: PlasmidDocument,
+      variables: { id: mockPlasmid.id },
+    },
+    result: {
+      data: {
+        plasmid: mockPlasmid,
+      },
+    },
+  },
+]
+
+describe("PlasmidDetailsContainer", () => {
+  test("Renders plasmid details if data is present", async () => {
+    render(
+      <MockedProvider mocks={mocks}>
+        <RouterProvider router={router} />
+      </MockedProvider>,
+    )
+    expect(await screen.findByText(mockPlasmid.summary)).toBeVisible()
+  })
+})
 
 describe("PlasmidDetails", () => {
   test("Renders heading with name of plasmid", () => {
