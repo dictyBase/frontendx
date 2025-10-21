f3bcf796ca636ca774b91608285c763bcb930fc0
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-21 09:49:28 -0500

    feat(EditingContent): implement save and cancel functionalities
    The save and cancel functionalities are implemented. The cancel button now sets the isEditing state to false, and the save button is prepared for future implementation. The buttons are styled with Material UI for better visual appearance. The type of setIsEditing is changed to Dispatch<SetStateAction<boolean>>. A spacing of 1 is added to the Stack component.

--- apps/genome-page/src/components/EditingContent.tsx
+++ apps/genome-page/src/components/EditingContent.tsx
@@ -1,23 +1,33 @@
-import { FunctionComponent, SetStateAction } from "react"
+import { FunctionComponent, Dispatch, SetStateAction } from "react"
 import Stack from "@mui/material/Stack"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
 
 const EditingContent: FunctionComponent<{
   content: string
-  setIsEditing: SetStateAction<boolean>
-}> = ({ content, setIsEditing }) => (
-  <Stack direction="row">
-    <TextField
-      multiline
-      fullWidth
-      maxRows={4}
-      variant="outlined"
-      value={content}
-    />
-    <Button sx={{ alignSelf: "flex-start" }}> Save </Button>
-    <Button sx={{ alignSelf: "flex-start" }}> Cancel </Button>
-  </Stack>
-)
+  setIsEditing: Dispatch<SetStateAction<boolean>>
+}> = ({ content, setIsEditing }) => {
+  const handleSave = () => {}
+  const handleCancel = () => {
+    setIsEditing(false)
+  }
+  return (
+    <Stack direction="row" spacing={1}>
+      <TextField
+        multiline
+        fullWidth
+        maxRows={4}
+        variant="outlined"
+        value={content}
+      />
+      <Button variant="contained" color="primary" onClick={handleSave}>
+        Save
+      </Button>
+      <Button variant="contained" color="inherit" onClick={handleCancel}>
+        Cancel
+      </Button>
+    </Stack>
+  )
+}
 
 export { EditingContent }
