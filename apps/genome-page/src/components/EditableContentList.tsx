6b687c8d6f2992e72c19b414332dbb4b00c907fe
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-22 11:21:40 -0500

    feat(genome-page): create EditableContentList component with delete and create functionality
    The new EditableContentList component allows displaying a list of strings as chips, with the ability to delete existing items and create new ones. It uses dialogs for confirmation before deletion and for creating new items, enhancing the user interface for managing content lists.

---
+++ apps/genome-page/src/components/EditableContentList.tsx
@@ -0,0 +1,68 @@
+import { useState, FunctionComponent, Dispatch, SetStateAction } from "react"
+import { pipe } from "fp-ts/function"
+import { map as Amap } from "fp-ts/Array"
+import { Option, some, none } from "fp-ts/Option"
+import AddIcon from "@mui/icons-material/Add"
+import Stack from "@mui/material/Stack"
+import Chip from "@mui/material/Chip"
+import IconButton from "@mui/material/IconButton"
+import { ConfirmDeleteDialog } from "./ConfirmDeleteDialog"
+import { CreateItemDialog } from "./CreateItemDialog"
+
+const EditableContentList: FunctionComponent<{
+  contentList: Array<string>
+}> = ({ contentList }) => {
+  const [deleteIsOpen, setDeleteIsOpen] = useState(false)
+  const [createIsOpen, setCreateIsOpen] = useState(false)
+  const [selectedForDeletion, setSelectedForDeletion] =
+    useState<Option<string>>(none)
+
+  const handleDelete = (selected: string) => () => {
+    setSelectedForDeletion(some(selected))
+    setDeleteIsOpen(true)
+  }
+
+  const handleCloseDelete = () => {
+    setDeleteIsOpen(false)
+  }
+
+  const handleCloseCreate = () => {
+    setCreateIsOpen(false)
+  }
+
+  const handleCreate = () => {
+    setCreateIsOpen(true)
+  }
+  return (
+    <>
+      <Stack
+        direction="row"
+        alignItems="center"
+        flexWrap="wrap"
+        spacing={1}
+        rowGap={1}>
+        {pipe(
+          contentList,
+          Amap((s) => <Chip key={s} label={s} onDelete={handleDelete(s)} />),
+        )}
+        <IconButton
+          size="small"
+          onClick={handleCreate}
+          sx={{
+            backgroundColor: "primary.main",
+            color: "primary.contrastText",
+          }}>
+          <AddIcon />
+        </IconButton>
+      </Stack>
+      <ConfirmDeleteDialog
+        open={deleteIsOpen}
+        onClose={handleCloseDelete}
+        selectedValue={selectedForDeletion}
+      />
+      <CreateItemDialog open={createIsOpen} onClose={handleCloseCreate} />
+    </>
+  )
+}
+
+export { EditableContentList }
