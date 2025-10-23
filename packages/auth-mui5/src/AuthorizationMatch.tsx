a0d61e157942afe5e58707f89d91fb4dee868f92
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-23 14:09:54 -0500

    feat(auth-mui5): add AuthorizationMatch component for role-based access control
    The new AuthorizationMatch component allows conditional rendering based on user roles, enhancing access control. The component is exported to be used in other modules.

---
+++ packages/auth-mui5/src/AuthorizationMatch.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, ReactElement } from "react"
+import { match } from "ts-pattern"
+import { useAuthorization } from "./useAuthorization"
+import { Roles } from "./const"
+
+const AuthorizationMatch: FunctionComponent<{
+  allowedRoles: Set<Roles>
+  authorized: ReactElement
+  unauthorized: ReactElement
+  loading: ReactElement
+}> = ({ allowedRoles, authorized, unauthorized, loading }) => {
+  const authorizationResult = useAuthorization({
+    entries: allowedRoles,
+  })
+  return match(authorizationResult)
+    .with({ isLoading: true }, () => loading)
+    .with({ isAuthorized: true }, () => authorized)
+    .with({ isAuthorized: false }, () => unauthorized)
+    .otherwise(() => unauthorized)
+}
+
+export { AuthorizationMatch }
