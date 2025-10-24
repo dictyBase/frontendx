50bee40b2ec9c97b58b74c82ad689fce81387aac
Kevin Tun <kevin.tun@northwestern.edu>
2025-10-24 11:19:25 -0500

    feat(CreateItemDialog.tsx): implement create item functionality
    The create item dialog now allows users to input a new item, save it, and update the gene general information using the `useAuthorizedUpdateGeneGeneralInfo` hook. The input is validated to prevent saving empty strings.

--- apps/genome-page/src/components/CreateItemDialog.tsx
+++ apps/genome-page/src/components/CreateItemDialog.tsx
@@ -1,4 +1,6 @@
-import { FunctionComponent } from "react"
+import { FunctionComponent, ChangeEventHandler, useState } from "react"
+import { UpdateGeneGeneralInfoInput } from "dicty-graphql-schema"
+import { isEmpty as SisEmpty } from "fp-ts/string"
 import Dialog from "@mui/material/Dialog"
 import DialogTitle from "@mui/material/DialogTitle"
 import DialogActions from "@mui/material/DialogActions"
@@ -6,26 +8,55 @@ import DialogContent from "@mui/material/DialogContent"
 import Typography from "@mui/material/Typography"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
+import { useAuthorizedUpdateGeneGeneralInfo } from "common/hooks/useAuthorizedUpdateGeneGeneralInfo"
 
 const CreateItemDialog: FunctionComponent<{
+  id: string
+  label: string
+  field: keyof Omit<UpdateGeneGeneralInfoInput, "user">
+  infoList: Array<string>
   open: boolean
   onClose: () => void
-  label: string
-}> = ({ label, open, onClose }) => (
-  <Dialog open={open} onClose={onClose}>
-    <DialogTitle>
-      <Typography variant="h2">{`Create ${label}`}</Typography>
-    </DialogTitle>
-    <DialogContent>
-      <TextField id="create" label={label} sx={{ marginTop: "1rem" }} />
-    </DialogContent>
-    <DialogActions>
-      <Button color="success"> Create </Button>
-      <Button color="inherit" onClick={onClose}>
-        Cancel
-      </Button>
-    </DialogActions>
-  </Dialog>
-)
+}> = ({ id, field, infoList, label, open, onClose }) => {
+  const [newItem, setNewItem] = useState("")
+  const update = useAuthorizedUpdateGeneGeneralInfo()
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = ({
+    currentTarget: { value },
+  }) => {
+    setNewItem(value)
+  }
+
+  const handleSave = () => {
+    if (SisEmpty(newItem)) return
+    console.log(`saving ${field}`)
+    update(id, { [field]: [...infoList, newItem] })
+  }
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>
+        <Typography variant="h2">{`Create ${label}`}</Typography>
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          id="create"
+          label={label}
+          sx={{ marginTop: "1rem" }}
+          value={newItem}
+          onChange={handleChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button color="success" onClick={handleSave}>
+          Create
+        </Button>
+        <Button color="inherit" onClick={onClose}>
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
 
 export { CreateItemDialog }
