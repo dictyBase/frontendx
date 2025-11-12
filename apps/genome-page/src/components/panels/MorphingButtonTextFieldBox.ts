c5d86581521ea9d5dd1dcc77b25281088b5f739f
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-12 09:37:35 -0600

    feat(MorphingButtonTextFieldBox): rename interface for clarity
    The interface name is changed from `MorphingButtonBoxProperties` to `MorphingButtonTextFieldBoxProperties` to provide a clearer and more descriptive name that accurately reflects the component's purpose.

--- apps/genome-page/src/components/panels/MorphingButtonTextFieldBox.ts
+++ apps/genome-page/src/components/panels/MorphingButtonTextFieldBox.ts
@@ -1,18 +1,18 @@
 import { styled } from "@mui/material/styles"
 import Box, { BoxProps } from "@mui/material/Box"
 
-interface MorphingButtonBoxProperties extends BoxProps {
+interface MorphingButtonTextFieldBoxProperties extends BoxProps {
   isExpanded: boolean
 }
 
-const MorphingButtonTextFieldBox = styled(Box)<MorphingButtonBoxProperties>(
-  ({ isExpanded }) => ({
-    position: "absolute",
-    inset: 0,
-    display: "flex",
-    alignItems: "center",
-    pointerEvents: isExpanded ? "auto" : "none",
-  }),
-)
+const MorphingButtonTextFieldBox = styled(
+  Box,
+)<MorphingButtonTextFieldBoxProperties>(({ isExpanded }) => ({
+  position: "absolute",
+  inset: 0,
+  display: "flex",
+  alignItems: "center",
+  pointerEvents: isExpanded ? "auto" : "none",
+}))
 
 export { MorphingButtonTextFieldBox }
