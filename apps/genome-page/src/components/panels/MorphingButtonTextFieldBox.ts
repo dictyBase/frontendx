634054c8e667a7ccaebe049a0e29a52ab389fcd8
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-12 12:52:31 -0600

    refactor(MorphingButtonTextFieldBox): remove shouldForwardProps
    The shouldForwardProp property was removed from the styled component as it is no longer needed.

--- apps/genome-page/src/components/panels/MorphingButtonTextFieldBox.ts
+++ apps/genome-page/src/components/panels/MorphingButtonTextFieldBox.ts
@@ -5,9 +5,9 @@ interface MorphingButtonTextFieldBoxProperties extends BoxProps {
   isExpanded: boolean
 }
 
-const MorphingButtonTextFieldBox = styled(Box, {
-  shouldForwardProp: (properties) => properties !== "isExpanded",
-})<MorphingButtonTextFieldBoxProperties>(({ isExpanded }) => ({
+const MorphingButtonTextFieldBox = styled(
+  Box,
+)<MorphingButtonTextFieldBoxProperties>(({ isExpanded }) => ({
   position: "absolute",
   inset: 0,
   display: "flex",
