df488f67f588cd9ec9661baade5f3000af0ef7aa
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-17 08:48:39 -0600

    feat(EditingInfoText): enhance gene description editing with loading state and error handling
    The gene description editing component is enhanced by adding a loading state to indicate when the update is in progress. Error handling is implemented to display an error message if the update fails. The save button is disabled when the text is the same as the initial text. The component now trims the input text before saving and allows setting the description to null if the trimmed text is empty.

--- apps/genome-page/src/components/panels/EditingInfoText.tsx
+++ apps/genome-page/src/components/panels/EditingInfoText.tsx
@@ -5,11 +5,26 @@ import {
   ChangeEventHandler,
   useState,
 } from "react"
-import { isEmpty as SisEmpty } from "fp-ts/string"
+import { pipe } from "fp-ts/function"
+import { isEmpty as SisEmpty, trim } from "fp-ts/string"
+import { match as Bmatch } from "fp-ts/boolean"
+import {
+  flatMap as TEflatMap,
+  of as TEof,
+  tapIO,
+  match as TEmatch,
+} from "fp-ts/TaskEither"
+import { of as IOof } from "fp-ts/IO"
+import { Option, none, some, match as Omatch } from "fp-ts/Option"
 import Stack from "@mui/material/Stack"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
-import { useAuthorizedUpdateGeneGeneralInfo } from "common/hooks/useAuthorizedUpdateGeneGeneralInfo"
+import CircularProgress from "@mui/material/CircularProgress"
+import {
+  useAuthorizedUpdateGeneGeneralInfo,
+  UpdateGeneGeneralInfoError,
+} from "common/hooks/useAuthorizedUpdateGeneGeneralInfo"
+import { SummaryPageErrorAlert } from "./SummaryPageErrorAlert"
 
 const EditingInfoText: FunctionComponent<{
   id: string
@@ -17,13 +32,42 @@ const EditingInfoText: FunctionComponent<{
   setIsEditing: Dispatch<SetStateAction<boolean>>
 }> = ({ id, initialText, setIsEditing }) => {
   const [text, setText] = useState(initialText)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Option<UpdateGeneGeneralInfoError>>(none)
   const update = useAuthorizedUpdateGeneGeneralInfo()
 
   const handleSave = () => {
-    if (text !== initialText) update(id, { description: text })
-    setIsEditing(false)
+    if (text === initialText) return
+    pipe(
+      text,
+      trim,
+      TEof,
+      tapIO(() => IOof(setLoading(true))),
+      TEflatMap(() =>
+        update(id, {
+          description: pipe(
+            text,
+            SisEmpty,
+            Bmatch(
+              () => text,
+              // eslint-disable-next-line unicorn/no-null
+              () => null,
+            ),
+          ),
+        }),
+      ),
+      TEmatch(
+        (errorValue) => {
+          setError(some(errorValue))
+          setLoading(false)
+        },
+        () => {
+          setLoading(false)
+          setIsEditing(false)
+        },
+      ),
+    )()
   }
-
   const handleChange: ChangeEventHandler<HTMLTextAreaElement> = ({
     currentTarget: { value },
   }) => {
@@ -34,31 +78,63 @@ const EditingInfoText: FunctionComponent<{
     setIsEditing(false)
   }
 
+  const handleCloseSnackbar = () => {
+    setError(none)
+  }
+
   return (
-    <Stack direction="column" spacing={1}>
-      <TextField
-        autoFocus
-        multiline
-        fullWidth
-        maxRows={4}
-        variant="outlined"
-        value={text}
-        onChange={handleChange}
-        InputProps={{ sx: { fontSize: ".8165rem" } }}
-      />
-      <Stack direction="row" spacing={1} justifyContent="flex-end">
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleSave}
-          disabled={SisEmpty(text)}>
-          Save
-        </Button>
-        <Button variant="contained" color="inherit" onClick={handleCancel}>
-          Cancel
-        </Button>
+    <>
+      <Stack direction="column" spacing={1}>
+        <TextField
+          autoFocus
+          multiline
+          fullWidth
+          maxRows={4}
+          variant="outlined"
+          value={text}
+          onChange={handleChange}
+          InputProps={{ sx: { fontSize: ".8165rem" } }}
+        />
+        <Stack
+          direction="row"
+          spacing={1}
+          justifyContent="flex-end"
+          alignItems="center">
+          {pipe(
+            loading,
+            Bmatch(
+              () => <></>,
+              () => (
+                <CircularProgress size={20} sx={{ color: "primary.main" }} />
+              ),
+            ),
+          )}
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSave}
+            disabled={text === initialText}>
+            Save
+          </Button>
+          <Button variant="contained" color="inherit" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </Stack>
       </Stack>
-    </Stack>
+      {pipe(
+        error,
+        Omatch(
+          () => <></>,
+          ({ message }) => (
+            <SummaryPageErrorAlert
+              open
+              message={message}
+              handleClose={handleCloseSnackbar}
+            />
+          ),
+        ),
+      )}
+    </>
   )
 }
 
