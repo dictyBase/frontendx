17eb23c9d3a0860f0385cdfebb50128b6b934827
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-21 12:33:02 -0600

    feat(ui-dsc): add CatalogListWrapper component tests and functionality
    This commit introduces a new test file for the CatalogListWrapper component, ensuring that it correctly renders children and passes the RefObject to the Paper component. The CatalogListWrapper component now includes a data-testid attribute on the Paper component to facilitate testing.

---
+++ packages/ui-dsc/src/__tests__/CatalogListWrapper.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react"
+import { createRef } from "react"
+import { CatalogListWrapper } from "../catalog/CatalogListWrapper"
+
+test("should render children", () => {
+  const mockReference = createRef<HTMLDivElement>()
+  render(
+    <CatalogListWrapper root={mockReference}>
+      <div>Test Child Content</div>
+    </CatalogListWrapper>,
+  )
+  expect(screen.getByText(/Test Child Content/)).toBeInTheDocument()
+})
+
+test("should pass the RefObject to the Paper component", () => {
+  const mockReference = createRef<HTMLDivElement>()
+  render(
+    <CatalogListWrapper root={mockReference}>
+      <div>Test Content</div>
+    </CatalogListWrapper>,
+  )
+
+  console.log(mockReference.current)
+  // The ref should be attached to the Paper component
+  expect(mockReference.current).toBeInTheDocument()
+  expect(mockReference.current).toBe(
+    screen.getByTestId("catalog-list-wrapper-root"),
+  )
+})
