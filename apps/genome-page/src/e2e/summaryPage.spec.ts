be5ce2e0416502f625e168e371de9b218f12cd5a
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-21 13:15:07 -0600

    fix(e2e): update expected GOA IDs in end-to-end tests
    The expected GOA IDs in the end-to-end tests for the genome and summary pages have been updated to reflect the current state of the data. This ensures that the tests pass with the latest data.

--- apps/genome-page/src/e2e/summaryPage.spec.ts
+++ apps/genome-page/src/e2e/summaryPage.spec.ts
@@ -28,7 +28,7 @@ const EXPECTED_GOA = {
   evidence_code: "IMP",
   extensions: null,
   go_term: "regulation of mitotic nuclear division",
-  id: "UniProtKB:Q9U7C9!615778289",
+  id: "UniProtKB:Q9U7C9!613751099",
   type: "biological_process",
   with: null,
 }
