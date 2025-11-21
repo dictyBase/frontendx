b9256061c0e3be27d8c36ea19611e4b68bfa9dd7
Kevin Tun <kevin.tun@northwestern.edu>
2025-11-21 12:44:57 -0600

    feat(ErrorDisplay.test.tsx): add retry button test case for ErrorDisplay
    A test case was added to verify that the retry button in the ErrorDisplay component calls the refetch function when clicked. This ensures that the retry functionality is working as expected.

--- packages/ui-dsc/src/__tests__/ErrorDisplay.test.tsx
+++ packages/ui-dsc/src/__tests__/ErrorDisplay.test.tsx
@@ -1,9 +1,11 @@
-import { test, expect } from "vitest"
+import { test, expect, vi } from "vitest"
 import { ApolloError } from "@apollo/client"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { ErrorDisplay } from "../ErrorDisplay"
 
 const errorMessage = "Test Error"
+const mockRefetch = vi.fn()
 
 const mockError = {
   graphQLErrors: [{ message: errorMessage }],
@@ -15,4 +17,11 @@ test("renders an error message", () => {
   expect(
     screen.getByText("An unexpected error has occurred."),
   ).toBeInTheDocument()
+})
+
+test("Clicking the retry button calls the passed `refetch` function", async () => {
+  userEvent.setup()
+  render(<ErrorDisplay refetch={mockRefetch} error={mockError} />)
+  await userEvent.click(screen.getByRole("button", { name: "Retry" }))
+  expect(mockRefetch).toHaveBeenCalledOnce()
 })
