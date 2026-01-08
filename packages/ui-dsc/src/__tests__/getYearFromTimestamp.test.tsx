c1a7fee1e3a62287bdcd84197b074035e4afa90a
Kevin Tun <kevin.tun@northwestern.edu>
2026-01-08 14:55:10 -0600

    fix(getYearFromTimestamp.test.tsx): change expected type to string
    The expected type in the test was changed to a string to match the return type of the function.

--- packages/ui-dsc/src/__tests__/getYearFromTimestamp.test.tsx
+++ packages/ui-dsc/src/__tests__/getYearFromTimestamp.test.tsx
@@ -2,6 +2,6 @@ import { getYearFromTimestamp } from "../utils/getYearFromTimeStamp"
 
 describe("getYearFromTimestamp", () => {
   test("should extract year from timestamp", () => {
-    expect(getYearFromTimestamp("2004-06-11T00:00:00.000Z")).toBe(2004)
+    expect(getYearFromTimestamp("2004-06-11T00:00:00.000Z")).toBe("2004")
   })
 })
