466f9953e65d897274b4e87e8b362d90bd8e3d6d
Kevin Tun <kevin.tun@northwestern.edu>
2026-01-29 16:56:16 -0600

    feat(ui-common): enhance route builder to inject mock properties into components
    The route builder now injects mock properties into components based on the `mockComponentProperties` object. This allows for more realistic and comprehensive testing of components in different states. The `Amap` function from `fp-ts/Array` is used to map over the properties array and render the component with each set of properties.

--- packages/ui-common/src/mocks/routeBuilder.tsx
+++ packages/ui-common/src/mocks/routeBuilder.tsx
@@ -1,5 +1,5 @@
 import { pipe } from "fp-ts/function"
-import { reduce as Areduce } from "fp-ts/Array"
+import { map as Amap, reduce as Areduce } from "fp-ts/Array"
 import { RouteObject } from "react-router-dom"
 import { union as Runion, collect as Rcollect } from "fp-ts/Record"
 import { Ord as SOrd } from "fp-ts/string"
@@ -19,10 +19,27 @@ const errorComponentRoutes = import.meta.glob<ComponentModuleImports>(
   },
 )
 
-const mapToRouteObject = (name: string, Component: FC): RouteObject => ({
-  path: name,
-  element: <Component />,
-})
+const mockComponentProperties = pipe(await import("./index")) as Record<
+  string,
+  Array<any>
+>
+
+const mapToRouteObject = (name: string, Component: FC): RouteObject => {
+  const propertiesArray = mockComponentProperties[`mock${name}PropsArray`]
+  if (!propertiesArray)
+    return {
+      path: name,
+      element: <Component />,
+    }
+
+  return {
+    path: name,
+    element: pipe(
+      propertiesArray,
+      Amap((properties) => <Component {...properties} />),
+    ),
+  }
+}
 
 const m1: Magma<ComponentModuleImports> = {
   concat: (x: ComponentModuleImports, _: ComponentModuleImports) => x,
@@ -34,11 +51,11 @@ const m2: Magma<FC> = {
 const collector = (_: string, a: ComponentModuleImports) => a
 // objects -> toEntries
 
-const a = pipe(
+const routes = pipe(
   Runion(m1)(componentRoutes)(errorComponentRoutes), // Combines component file paths and error component routes into a single record.
   Rcollect(SOrd)(collector), //  Gets an array of each file's import object.
   Areduce({}, (accumulator, next) => Runion(m2)(accumulator)(next)), // Combines all imported components into a single object
   Rcollect(SOrd)(mapToRouteObject), // Maps each imported component to a RouteObject
 )
 
-console.log(a)
+export { routes }
