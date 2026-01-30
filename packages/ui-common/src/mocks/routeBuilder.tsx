e4cf884417af64b1d9331ce624f304a1ee218707
Kevin Tun <kevin.tun@northwestern.edu>
2026-01-30 02:20:03 -0600

    fix(routeBuilder.tsx): update import path for mock component properties
    The import path for mock component properties was updated to reflect the new location of the index file. This ensures that the route builder correctly imports and utilizes the mock component properties.

--- packages/ui-common/src/mocks/routeBuilder.tsx
+++ packages/ui-common/src/mocks/routeBuilder.tsx
@@ -19,7 +19,7 @@ const errorComponentRoutes = import.meta.glob<ComponentModuleImports>(
   },
 )
 
-const mockComponentProperties = pipe(await import("./index")) as Record<
+const mockComponentProperties = pipe(await import("./props/index")) as Record<
   string,
   Array<any>
 >
@@ -49,13 +49,12 @@ const m2: Magma<FC> = {
   concat: (x: FC, _: FC) => x,
 }
 const collector = (_: string, a: ComponentModuleImports) => a
-// objects -> toEntries
 
 const routes = pipe(
   Runion(m1)(componentRoutes)(errorComponentRoutes), // Combines component file paths and error component routes into a single record.
   Rcollect(SOrd)(collector), //  Gets an array of each file's import object.
-  Areduce({}, (accumulator, next) => Runion(m2)(accumulator)(next)), // Combines all imported components into a single object
-  Rcollect(SOrd)(mapToRouteObject), // Maps each imported component to a RouteObject
+  Areduce({}, (accumulator, next) => Runion(m2)(accumulator)(next)), // Combines all imported components into a single object.
+  Rcollect(SOrd)(mapToRouteObject), // Takes the object and turns it into Array<RouteObject>.
 )
 
 export { routes }
