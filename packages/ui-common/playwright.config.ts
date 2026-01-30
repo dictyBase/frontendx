45f0af44f141ec1ae386327ebfb214fe3ffe6e06
Kevin Tun <kevin.tun@northwestern.edu>
2026-01-30 02:20:13 -0600

    feat(ui-common): add playwright config for e2e testing
    This commit introduces a Playwright configuration file for end-to-end testing in the ui-common package. It sets up configurations for running tests in parallel, retries on CI, reporters, timeouts, and projects for Firefox, Microsoft Edge, and Google Chrome. It also configures a web server and sets the base URL and trace options.

---
+++ packages/ui-common/playwright.config.ts
@@ -0,0 +1,72 @@
+import { defineConfig, devices } from "@playwright/test"
+
+// eslint-disable-next-line import/no-default-export
+export default defineConfig({
+  testDir: "./src/e2e",
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : 4,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: process.env.CI ? "github" : "list",
+  timeout: 60_000,
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  expect: {
+    timeout: 30_000,
+    toHaveScreenshot: { maxDiffPixelRatio: 0.05 },
+  },
+  /* Configure projects for major browsers */
+  projects: [
+    // {
+    //  name: "chromium",
+    //  use: { ...devices["Desktop Chrome"] },
+    // },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    // {
+    //  name: "webkit",
+    //  use: { ...devices["Desktop Safari"] },
+    // },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    {
+      name: "Microsoft Edge",
+      use: { ...devices["Desktop Edge"], channel: "msedge" },
+    },
+    {
+      name: "Google Chrome",
+      use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    timeout: 120 * 1000,
+    command: "yarn turbo dev --filter=@dictybase/ui-common",
+    url: process.env.BASE_URL,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: `${process.env.BASE_URL}`,
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+  },
+})
