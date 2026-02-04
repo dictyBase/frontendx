02d8d4d15d5dba669db59477ac6be8268dd99ce1
Kevin Tun <kevin.tun@northwestern.edu>
2026-02-04 10:43:46 -0600

    feat(callback.tsx): replace Loader with FullPageLoadingDisplay
    The Loader component was replaced with FullPageLoadingDisplay to provide a full-page loading experience during the sign-in callback process. This change improves the user experience by providing a more visually clear indication that the application is loading.

--- apps/dicty-frontpage/src/pages/callback.tsx
+++ apps/dicty-frontpage/src/pages/callback.tsx
@@ -1,13 +1,13 @@
 import { useHandleSignInCallback } from "@logto/react"
 import { useNavigate } from "react-router-dom"
-import { Loader } from "../common/components/Loader"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
 
 const Callback = () => {
   const navigate = useNavigate()
   const { isLoading } = useHandleSignInCallback(() => {
     navigate("/")
   })
-  return isLoading ? <Loader /> : <></>
+  return isLoading ? <FullPageLoadingDisplay /> : <></>
 }
 
 // eslint-disable-next-line import/no-default-export
