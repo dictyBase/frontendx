8375e20a8af0cea42762fdc64e8214d13f40afac
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-06 16:26:08 -0500

    fix(e2e): update GOA ID to match the expected value in summaryPage.spec.ts
    The GOA ID in the e2e test was incorrect and did not match the expected value. This commit updates the ID to ensure the test passes correctly.

--- apps/genome-page/src/e2e/summaryPage.spec.ts
+++ apps/genome-page/src/e2e/summaryPage.spec.ts
@@ -28,7 +28,7 @@ const EXPECTED_GOA = {
   evidence_code: "IMP",
   extensions: null,
   go_term: "regulation of mitotic nuclear division",
-  id: "UniProtKB:Q9U7C9!636364692",
+  id: "UniProtKB:Q9U7C9!507606576",
   type: "biological_process",
   with: null,
 }
