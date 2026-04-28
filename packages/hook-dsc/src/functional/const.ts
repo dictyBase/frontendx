8b9cb9ef1ed4fd7704e45e92e5e1d575d2fcf61e
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-28 17:13:17 -0500

    feat(hook-dsc): create const file for default and golden braid groups
    This commit introduces a new file containing constant objects for default and golden braid groups, enhancing code maintainability and reusability by centralizing these commonly used configurations.

---
+++ packages/hook-dsc/src/functional/const.ts
@@ -0,0 +1,6 @@
+import { PlasmidType } from "dicty-graphql-schema"
+
+const DEFAULT_GROUP = { plasmid_type: PlasmidType.Regular }
+const GOLDEN_BRAID_GROUP = { plasmid_type: PlasmidType.GoldenBraid }
+
+export { DEFAULT_GROUP, GOLDEN_BRAID_GROUP }
