dabee1945feaa7bcbac2a942211ff3f5140787a4
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-29 09:54:53 -0500

    feat(hook-dsc): build plasmid list filter from URL search parameters
    The buildPlasmidListFilter function now constructs a PlasmidListFilter from URLSearchParams, enabling filtering of plasmid lists based on URL parameters like group and name. A get function was added to retrieve values from URLSearchParams.

--- packages/hook-dsc/src/functional/buildPlasmidFilter.ts
+++ packages/hook-dsc/src/functional/buildPlasmidFilter.ts
@@ -2,8 +2,6 @@ import { pipe } from "fp-ts/function"
 import { Eq as SEq } from "fp-ts/string"
 import { lookup as Mlookup } from "fp-ts/Map"
 import {
-  type Option,
-  fromNullable as OfromNullable,
   Do as ODo,
   bind as Obind,
   let as Olet,
@@ -13,30 +11,29 @@ import {
 } from "fp-ts/Option"
 import { PlasmidListFilter } from "dicty-graphql-schema"
 import { DEFAULT_GROUP, GOLDEN_BRAID_GROUP } from "../const"
+import { get } from "./URLSearchParams"
 
 const plasmidGroupFilterEntries = new Map([
   ["regular", DEFAULT_GROUP],
   ["goldenbraid", GOLDEN_BRAID_GROUP],
 ])
 
-const get: (k: string) => (fa: URLSearchParams) => Option<string> =
-  (k) => (fa) =>
-    pipe(fa.get(k), OfromNullable)
-
+/**
+ * buildPlasmidListFilter is used in the Plasmid Catalog. It takes the `URLSearchParams` of the current URL
+ * and returns a `PlasmidListFilter` that can be used in `PlasmidListQuery` variables.
+ */
 const buildPlasmidListFilter = (
   parameters: URLSearchParams,
 ): PlasmidListFilter =>
   pipe(
     ODo,
     Olet("searchParameters", () => parameters),
-    Obind(
-      "init",
-      ({ searchParameters }): Option<PlasmidListFilter> =>
-        pipe(
-          searchParameters,
-          get("group"),
-          OflatMap((v) => pipe(plasmidGroupFilterEntries, Mlookup(SEq)(v))),
-        ),
+    Obind("init", ({ searchParameters }) =>
+      pipe(
+        searchParameters,
+        get("group"),
+        OflatMap((v) => pipe(plasmidGroupFilterEntries, Mlookup(SEq)(v))),
+      ),
     ),
     Olet("withName", ({ searchParameters, init }) =>
       pipe(
