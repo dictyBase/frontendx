8581e9e9cb6ffc47b9f315b4eca9c61a6aea2fbe
Kevin Tun <kevin.tun@northwestern.edu>
2026-04-29 19:36:11 -0500

    feat(hook-dsc): remove GOLDEN_BRAID_GROUP constant from const.ts
    The GOLDEN_BRAID_GROUP constant was removed as it is no longer needed.

--- packages/hook-dsc/src/const.ts
+++ packages/hook-dsc/src/const.ts
@@ -1,6 +1,5 @@
 import { PlasmidType } from "dicty-graphql-schema"
 
 const DEFAULT_GROUP = { plasmid_type: PlasmidType.Regular }
-const GOLDEN_BRAID_GROUP = { plasmid_type: PlasmidType.GoldenBraid }
 
-export { DEFAULT_GROUP, GOLDEN_BRAID_GROUP }
+export { DEFAULT_GROUP }
