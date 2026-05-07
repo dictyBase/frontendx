7392088066252018b8940773a4b21d25270596ee
Kevin Tun <kevin.tun@northwestern.edu>
2026-05-06 23:14:11 -0500

    feat(ui-dsc): add utility function to check for GraphQL NotFound error
    This commit introduces a utility function, `hasNotFoundError`, that checks if an ApolloError contains a GraphQL error with the code "NotFound" within its extensions. This function enhances error handling by providing a specific check for not found errors.

---
+++ packages/ui-dsc/src/utils/hasNotFoundError.ts
@@ -0,0 +1,21 @@
+import { ApolloError } from "@apollo/client"
+import { pipe } from "fp-ts/function"
+import { fromNullable as OfromNullable, map as Omap } from "fp-ts/Option"
+import {
+  map as ROAmap,
+  compact as ROAcompact,
+  exists as ROAexists,
+} from "fp-ts/ReadonlyArray"
+
+const hasNotFoundError = (error: ApolloError | undefined) =>
+  pipe(
+    error,
+    OfromNullable,
+    Omap(({ graphQLErrors }: ApolloError) => graphQLErrors),
+    Omap(ROAmap(({ extensions }) => OfromNullable(extensions))),
+    Omap(ROAcompact),
+    // eslint-disable-next-line dot-notation
+    Omap(ROAexists((extension) => extension["code"] === "NotFound")),
+  )
+
+export { hasNotFoundError }
