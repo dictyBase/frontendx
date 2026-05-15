0dee128f1d98efd6136169148c7a9ea94c59cb39
Kevin Tun <kevin.tun@northwestern.edu>
2026-05-15 01:51:05 -0500

    feat(atomWithBroadcast): enhance atomWithBroadcast with generics for actions
    The atomWithBroadcast function is now enhanced with generics to provide better type safety for actions. This change ensures that the actions dispatched to the atom are correctly typed, improving the overall reliability and maintainability of the code.

--- apps/stock-center/src/atomWithBroadcast.ts
+++ apps/stock-center/src/atomWithBroadcast.ts
@@ -1,27 +1,34 @@
 import { atom, WritableAtom } from "jotai"
+import type { SetStateAction } from "jotai/vanilla"
 import { SetStateActionWithReset } from "./types"
 
-type MessageEventWithInit<T> = MessageEvent<{ init?: boolean; value: T }>
-type UpdateWithBroadcast<T> = {
+type MessageEventWithInit<Action> = MessageEvent<{
+  init?: boolean
+  value: Action
+}>
+type UpdateWithBroadcast<Action> = {
   isEvent: boolean
-  value?: SetStateActionWithReset<T>
+  value?: Action
 }
 
-const atomWithBroadcast = <T>(
-  baseAtom: WritableAtom<T, [SetStateActionWithReset<T>], void>,
+const atomWithBroadcast = <
+  T,
+  Action extends SetStateAction<T> | SetStateActionWithReset<T>,
+>(
+  baseAtom: WritableAtom<T, [Action], void>,
   key: string,
-) => {
-  const listeners = new Set<(event: MessageEventWithInit<T>) => void>()
+): WritableAtom<T, [Action], void> => {
+  const listeners = new Set<(event: MessageEventWithInit<Action>) => void>()
   const channel = new BroadcastChannel(key)
 
   // eslint-disable-next-line unicorn/prefer-add-event-listener
-  channel.onmessage = (event: MessageEventWithInit<T>) => {
+  channel.onmessage = (event: MessageEventWithInit<Action>) => {
     listeners.forEach((l) => l(event))
   }
 
   const broadcastAtom = atom(
     (get) => get(baseAtom),
-    (get, set, update: UpdateWithBroadcast<T>) => {
+    (get, set, update: UpdateWithBroadcast<Action>) => {
       if (update.value) set(baseAtom, update.value)
       if (!update.isEvent) channel.postMessage({ value: get(baseAtom) })
     },
@@ -30,7 +37,7 @@ const atomWithBroadcast = <T>(
   broadcastAtom.onMount = (setAtom) => {
     channel.postMessage({ init: true })
 
-    const listener = (event: MessageEventWithInit<T>) => {
+    const listener = (event: MessageEventWithInit<Action>) => {
       if (event.data.init) setAtom({ isEvent: false })
       setAtom({ isEvent: true, value: event.data.value })
     }
@@ -44,7 +51,7 @@ const atomWithBroadcast = <T>(
 
   return atom(
     (get) => get(broadcastAtom),
-    (_get, set, update: SetStateActionWithReset<T>) => {
+    (_get, set, update: Action) => {
       set(broadcastAtom, { isEvent: false, value: update })
     },
   )
