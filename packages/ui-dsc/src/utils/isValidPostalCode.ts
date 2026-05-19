4a10a4366e9218c1705c072e69ba2e1ff02baa57
Kevin Tun <kevin.tun@northwestern.edu>
2026-05-19 11:53:08 -0500

    feat(ui-dsc): improve postal code validation with country code check
    The postal code validation logic is improved by checking if a validator exists for the country code before attempting validation. If there isn't a validator or a matching code in our `countryList`, the input should be considered valid.

--- packages/ui-dsc/src/utils/isValidPostalCode.ts
+++ packages/ui-dsc/src/utils/isValidPostalCode.ts
@@ -1,12 +1,6 @@
 import { pipe } from "fp-ts/function"
-import { MonoidAll as BMonoidAll } from "fp-ts/boolean"
-import {
-  some,
-  none,
-  map as Omap,
-  getOrElse as OgetOrElse,
-  fromPredicate as OfromPredicate,
-} from "fp-ts/Option"
+import { MonoidAll as BMonoidAll, match as Bmatch } from "fp-ts/boolean"
+import { some, none, map as Omap, getOrElse as OgetOrElse } from "fp-ts/Option"
 import { findFirstMap as AfindFirstMap } from "fp-ts/Array"
 import {
   postcodeValidator,
@@ -17,8 +11,24 @@ import { countryList } from "./countryList"
 const isValidPostalCode = (postalCode: string, country: string) =>
   pipe(
     countryList,
-    AfindFirstMap(({ label, code }) => (label === country ? some(code) : none)),
+    AfindFirstMap(({ label, code }) =>
+      pipe(
+        // If there is a corresponding country code for `country` in `countryList`,
+        BMonoidAll.concat(
+          label === country,
+          // ...and a validator exists for that country code,
+          postcodeValidatorExistsForCountry(code),
+        ),
+        // Return that country code.
+        Bmatch(
+          () => none,
+          () => some(code),
+        ),
+      ),
+    ),
+    // The country code is then validated against the postal code.
     Omap((countryCode) => postcodeValidator(postalCode, countryCode)),
+    // If there wasn't a valiadator or a matching code in our `countryList`, the input should be considered valid.
     OgetOrElse(() => true),
   )
 
