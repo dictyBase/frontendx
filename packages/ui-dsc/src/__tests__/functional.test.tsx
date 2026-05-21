9f831ed95e58e59976ee08630df9ab96f40b42b9
Kevin Tun <kevin.tun@northwestern.edu>
2026-05-21 11:11:39 -0500

    feat(functional.test.tsx): add renderPlasmidTotal tests and update renderStrainTotal tests to accept plasmidItems
    Added tests for rendering plasmid totals and updated strain total tests to accept plasmid items, ensuring comprehensive test coverage for both strain and plasmid rendering functionalities.

--- packages/ui-dsc/src/__tests__/functional.test.tsx
+++ packages/ui-dsc/src/__tests__/functional.test.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, test } from "vitest"
 import { render, screen } from "@testing-library/react"
-import { renderStrainTotal } from "../functional"
-import { StrainCartItem } from "../types"
+import { renderStrainTotal, renderPlasmidTotal } from "../functional"
+import { StrainCartItem, PlasmidCartItem } from "../types"
 
 describe("renderStrainTotal", () => {
   test("should render the leftValue, numItems, and total correctly", () => {
@@ -22,9 +22,40 @@ describe("renderStrainTotal", () => {
       },
     ]
 
-    render(renderStrainTotal({ strainItems: mockStrainItems }))
+    render(
+      renderStrainTotal({ strainItems: mockStrainItems, plasmidItems: [] }),
+    )
 
     expect(screen.getByText("Strains")).toBeInTheDocument()
+    expect(screen.getByText("2 items:")).toBeInTheDocument()
+    expect(screen.getByText("$30.00")).toBeInTheDocument()
+  })
+})
+
+describe("renderPlasmidTotal", () => {
+  test("should render the leftValue, numItems, and total correctly", () => {
+    const mockPlasmidItems: Array<PlasmidCartItem> = [
+      {
+        id: "DBS0236123",
+        name: "aarA-",
+        summary: "mock strain 1",
+        in_stock: true,
+        fee: 15,
+      },
+      {
+        id: "DBS0236123",
+        name: "aarA",
+        summary: "mock strain 2",
+        in_stock: true,
+        fee: 15,
+      },
+    ]
+
+    render(
+      renderPlasmidTotal({ plasmidItems: mockPlasmidItems, strainItems: [] }),
+    )
+
+    expect(screen.getByText("Plasmids")).toBeInTheDocument()
     expect(screen.getByText("2 items:")).toBeInTheDocument()
     expect(screen.getByText("$30.00")).toBeInTheDocument()
   })
