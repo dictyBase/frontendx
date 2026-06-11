1ffc1c94edf08ab8ca9d6f3d6dbbc59474754eef
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-10 22:49:49 -0500

    feat(wrapLazyComponent): add a utility function to wrap lazy-loaded components with access control
    A new utility function, `wrapLazyComponent`, is introduced to facilitate wrapping lazy-loaded components with access control. This enhancement allows components to be rendered based on their access level (public, protected, or private), improving security and modularity in handling component rendering based on user roles and permissions.

---
+++ packages/auth-mui5/src/functional/wrapLazyComponent.tsx
@@ -0,0 +1,45 @@
+import { lazy, LazyExoticComponent, FunctionComponent } from "react"
+import { pipe } from "fp-ts/function"
+import { match } from "ts-pattern"
+import { Task, map as Tmap } from "fp-ts/Task"
+import { PageComponentData, ACCESS } from "./routeManager"
+import { LazyProtected } from "../LazyProtected"
+import { LazyPrivate } from "../LazyPrivate"
+
+const wrapLazyComponent = (
+  value: Task<PageComponentData>,
+): LazyExoticComponent<FunctionComponent> =>
+  pipe(
+    value,
+    Tmap(({ default: Component, access, roles }) =>
+      match(access)
+        .with(ACCESS.public, () => Component)
+        .with(
+          ACCESS.protected,
+          () =>
+            function () {
+              return (
+                <LazyProtected>
+                  <Component />
+                </LazyProtected>
+              )
+            },
+        )
+        .with(
+          ACCESS.private,
+          () =>
+            function () {
+              return (
+                <LazyPrivate roles={roles || []}>
+                  <Component />
+                </LazyPrivate>
+              )
+            },
+        )
+        .exhaustive(),
+    ),
+    Tmap((component) => ({ default: component })),
+    lazy,
+  )
+
+export { wrapLazyComponent }
