c493dcc20a08e1627be0d7eb5e1a056e99a5b4a2
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-12 10:31:36 -0500

    fix(wrapLazyComponent): enhance access matching by adding roles support and unify authorization logic
    The changes improve the authorization logic in the wrapLazyComponent function by unifying the access checks for public, protected, and private components. By including role validation and simplifying the matching process, the code now handles user access more effectively and clearly. Additionally, unauthorized access is now handled more appropriately by returning the UnAuthorized component.

--- packages/auth-mui5/src/functional/wrapLazyComponent.tsx
+++ packages/auth-mui5/src/functional/wrapLazyComponent.tsx
@@ -1,11 +1,12 @@
 import { lazy, LazyExoticComponent, FunctionComponent } from "react"
 import { pipe } from "fp-ts/function"
-import { match } from "ts-pattern"
+import { match, P } from "ts-pattern"
 import { Task, map as Tmap } from "fp-ts/Task"
 import { PageComponentData } from "./routeManager"
 import { ACCESS } from "../const"
 import { LazyProtected } from "../LazyProtected"
 import { LazyPrivate } from "../LazyPrivate"
+import { UnAuthorized } from "../UnAuthorized"
 
 const wrapLazyComponent = (
   value: Task<PageComponentData>,
@@ -13,19 +14,22 @@ const wrapLazyComponent = (
   pipe(
     value,
     Tmap(({ default: Component, access, roles }) =>
-      match(access)
-        .with(ACCESS.public, () => Component)
-        .with(ACCESS.protected, () => () => (
+      match({ access, roles })
+        .with({ access: ACCESS.public }, () => Component)
+        .with({ access: ACCESS.protected }, () => () => (
           <LazyProtected>
             <Component />
           </LazyProtected>
         ))
-        .with(ACCESS.private, () => () => (
-          <LazyPrivate roles={roles || []}>
-            <Component />
-          </LazyPrivate>
-        ))
-        .exhaustive(),
+        .with(
+          { acess: ACCESS.private, roles: P.select(P.array(P.string)) },
+          (r) => () => (
+            <LazyPrivate roles={r}>
+              <Component />
+            </LazyPrivate>
+          ),
+        )
+        .otherwise(() => UnAuthorized),
     ),
     Tmap((component) => ({ default: component })),
     lazy,
