7052f3931598ce9344eb7afef9abc195a0920a1b
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-12 09:53:04 -0500

    fix(Callback.tsx): replace LoadingDisplay with FullPageLoadingDisplay for improved user experience during loading
    The component now uses FullPageLoadingDisplay instead of LoadingDisplay to provide a more cohesive and comprehensive loading experience for users during the sign-in callback process.

--- packages/auth-mui5/src/Callback.tsx
+++ packages/auth-mui5/src/Callback.tsx
@@ -1,6 +1,6 @@
 import { useHandleSignInCallback } from "@logto/react"
 import { useNavigate } from "react-router-dom"
-import { LoadingDisplay } from "./LoadingDisplay"
+import { FullPageLoadingDisplay } from "@dictybase/ui-common"
 
 /**
  * Callback component
@@ -8,12 +8,13 @@ import { LoadingDisplay } from "./LoadingDisplay"
  * This component is responsible for handling the callback after a successful sign-in.
  * It renders a loading display until the sign-in process is completed.
  */
+
 const Callback = () => {
   const navigate = useNavigate()
   const { isLoading } = useHandleSignInCallback(() => {
     navigate("/")
   })
-  return isLoading ? <LoadingDisplay rows={5} /> : <></>
+  return isLoading ? <FullPageLoadingDisplay /> : <></>
 }
 
 export { Callback }
