07dc7137007a8cbdc636c801e42e345d90e04ab7
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-15 08:13:06 -0500

    feat(downloads): add new pages for editing, showing, and error handling in downloads section to enhance user experience
    The addition of these new components improves the downloads section by providing functionalities for editing and displaying content, as well as handling authorization and error scenarios, which enhances overall usability and accessibility.

---
+++ apps/dicty-frontpage/src/pages/downloads/notfound.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "@dictybase/auth-mui5"
+import { NotFoundError } from "@dictybase/ui-common"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NotFoundError
+export const access = ACCESS.public
