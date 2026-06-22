19e746f169b9dd56bacd066f84ec9c96d524d70d
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-22 08:59:45 -0500

    feat: refactor homepage and strain details components for improved structure and performance
    The changes involve removing the outdated ShowHomepage component and replacing it with DSCHomepage for better organization of the homepage layout. In the strain details components, the implementation shifts to a more functional approach using 'ts-pattern' and 'fp-ts' libraries for improved pattern matching and error handling, while providing a consistent loading and error state display. This refactoring enhances maintainability and user experience by organizing the code more effectively.

--- packages/ui-dsc/src/catalog/StrainDetailsContainer.tsx
+++ packages/ui-dsc/src/catalog/StrainDetailsContainer.tsx
@@ -2,6 +2,13 @@ import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { useStrainQuery } from "dicty-graphql-schema"
 import { match, P } from "ts-pattern"
+import { pipe } from "fp-ts/function"
+import {
+  fromNullable as OfromNullable,
+  getOrElse as OgetOrElse,
+  map as Omap,
+} from "fp-ts/Option"
+import { match as Amatch } from "fp-ts/Array"
 import { FullPageLoadingDisplay } from "@dictybase/ui-common"
 import { ErrorPageWrapper } from "../ErrorPageWrapper"
 import { characterConverter } from "../utils/characterConverter"
@@ -24,10 +31,17 @@ const StrainDetailsContainer = () => {
   return match(result)
     .with({ data: { strain: P.select(P.not(P.nullish)) } }, (strain) => {
       const label = characterConverter(strain.label)
-      let title = `Strain Details for ${label}`
-      if (strain?.phenotypes && strain.phenotypes.length > 0) {
-        title = `Phenotype and Strain Details for ${label}`
-      }
+      const title = pipe(
+        strain.phenotypes,
+        OfromNullable,
+        Omap(
+          Amatch(
+            () => `Strain Details for ${label}`,
+            () => `Phenotype and Strain Details for ${label}`,
+          ),
+        ),
+        OgetOrElse(() => `Strain Details for ${label}`),
+      )
       return (
         <CatalogItemDetailsLayout label={label} title={title}>
           <DetailsHeader id={strain.id} name={strain.label} />
