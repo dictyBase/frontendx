c9f1621f4169e3a49b815b416e9485bba447448e
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-23 10:45:22 -0500

    feat(FullPageLoadingDisplay): add data-testid attribute to Container for better testing support
    The addition of the data-testid attribute allows for improved targeting in tests, making it easier to validate the presence and functionality of the loading component in automated tests.

--- packages/ui-common/src/FullPageLoadingDisplay.tsx
+++ packages/ui-common/src/FullPageLoadingDisplay.tsx
@@ -5,7 +5,7 @@ import { LoadingDisplay } from "./LoadingDisplay"
  * Loading component intended for use in editable content pages.
  */
 const FullPageLoadingDisplay = () => (
-  <Container>
+  <Container data-testid="skeleton-loader">
     <LoadingDisplay rows={1} height={100} />
     <br />
     <LoadingDisplay rows={7} height={50} />
