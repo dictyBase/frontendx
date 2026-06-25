1a960b2a057df8701c2f3e7ca0780a7cde88ed74
Kevin Tun <kevin.tun@northwestern.edu>
2026-06-25 12:07:30 -0500

    feat(GridLayout): refactor grid layout component to use MUI Grid for responsive design
    Changes enhance the responsiveness of the layout by replacing the custom CSS grid with Material-UI's Grid component, leveraging its built-in capabilities for better handling of spacing and item layout across different screen sizes.

--- packages/ui-dsc/src/home/GridLayout.tsx
+++ packages/ui-dsc/src/home/GridLayout.tsx
@@ -1,29 +1,25 @@
 import type { ReactNode } from "react"
+import { pipe } from "fp-ts/function"
+import { map as Amap } from "fp-ts/Array"
 import type { SxProps, Theme } from "@mui/material"
-import { Box } from "@mui/material"
+import { Grid } from "@mui/material"
 
 type GridLayoutProperties = {
-  children: ReactNode
-  minColumnWidth: string
+  children: Array<ReactNode>
   gap?: number
-  sx?: SxProps<Theme>
 }
 
-const GridLayout = ({
-  children,
-  minColumnWidth,
-  gap = 3,
-  sx,
-}: GridLayoutProperties) => (
-  <Box
-    sx={{
-      display: "grid",
-      gridTemplateColumns: `repeat(auto-fit, minmax(${minColumnWidth}, 1fr))`,
-      gap,
-      ...sx,
-    }}>
-    {children}
-  </Box>
+const GridLayout = ({ children, gap = 3 }: GridLayoutProperties) => (
+  <Grid container spacing={gap}>
+    {pipe(
+      children,
+      Amap((child) => (
+        <Grid item lg={3} sm={12} xs={12} sx={{ "> div": { height: "100%" } }}>
+          {child}
+        </Grid>
+      )),
+    )}
+  </Grid>
 )
 
 export { GridLayout }
