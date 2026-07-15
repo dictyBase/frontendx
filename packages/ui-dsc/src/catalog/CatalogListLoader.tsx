b1a28433e86c19bb3b9d4d7a02357b05c3379a19
Kevin Tun <kevin.tun@northwestern.edu>
2026-07-15 16:38:06 -0500

    refactor(graphql_config.ts, CatalogListLoader.tsx): reorganize strainGroupFilterOptions and simplify CatalogListLoader component for improved readability
    The 'All Strains' option was repositioned in the strainGroupFilterOptions for better logical grouping while the CatalogListLoader component is simplified by removing the useStyles and using inline styles, enhancing the component's clarity and performance.

--- packages/ui-dsc/src/catalog/CatalogListLoader.tsx
+++ packages/ui-dsc/src/catalog/CatalogListLoader.tsx
@@ -1,22 +1,10 @@
 import { Paper } from "@mui/material"
-import { makeStyles } from "tss-react/mui"
 import { LoadingDisplay } from "../LoadingDisplay"
 
-const useStyles = makeStyles()((theme) => ({
-  root: {
-    height: "100%",
-    paddingTop: theme.spacing(6),
-    padding: theme.spacing(1),
-  },
-}))
-
-const CatalogListLoader = () => {
-  const { classes } = useStyles()
-  return (
-    <Paper className={classes.root}>
-      <LoadingDisplay rows={12} height={40} />
-    </Paper>
-  )
-}
+const CatalogListLoader = () => (
+  <Paper sx={{ height: "100%", p: 7 }}>
+    <LoadingDisplay rows={12} height={40} />
+  </Paper>
+)
 
 export { CatalogListLoader }
