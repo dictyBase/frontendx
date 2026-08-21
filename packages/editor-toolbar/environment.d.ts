953137467bbcd02d2c3d34f446d15c669e51a0d5
Kevin Tun <kevin.tun@northwestern.edu>
2026-08-21 11:31:58 -0500

    chore(editor-toolbar): remove unused environment.d.ts and migrate its content to vite.environment.d.ts for better organization
    The unused environment.d.ts file is deleted, and its relevant content is added to vite.environment.d.ts. This improves project organization by consolidating environment type definitions in a single file.

--- packages/editor-toolbar/environment.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable unicorn/prevent-abbreviations */
-interface ImportMetaEnv {
-  VITE_APP_LOGTO_API_SECOND_RESOURCE: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- packages/editor-toolbar/vite.environment.d.ts
+++ packages/editor-toolbar/vite.environment.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+/* eslint-disable unicorn/prevent-abbreviations */
+interface ImportMetaEnv {
+  VITE_APP_LOGTO_API_SECOND_RESOURCE: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
