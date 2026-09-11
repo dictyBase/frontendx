3e3e5389b485f89ccc073c0c8876327b0beeb38f
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-11 10:56:49 -0500

    feat(stock-center): create NewHome component for enhanced search functionality
    A new component, NewHome, is introduced to improve the user experience by allowing users to search for strains and plasmids seamlessly. It incorporates real-time search capabilities and better organization of results, enhancing overall accessibility to the Dicty Stock Center's resources.

---
+++ apps/stock-center/src/pages/home.tsx
@@ -0,0 +1,6 @@
+import { ACCESS } from "@dictybase/auth-mui5"
+import { NewHome } from "../NewHome"
+
+// eslint-disable-next-line unicorn/prefer-export-from, import/no-default-export
+export default NewHome
+export const access = ACCESS.public
