5c0e1da765898b7b247fd3c23d20201d1bec30bf
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-16 08:21:39 -0500

    style(editor): remove background color from editor area styles for better adaptability
    The background color has been removed from the editor area styles to allow for more flexible theming and to improve the overall adaptability of the component to various design contexts.

--- packages/editor/src/useEditorStyles.ts
+++ packages/editor/src/useEditorStyles.ts
@@ -13,7 +13,6 @@ const useEditorAreaStyles = makeStyles<{ editable: boolean }>()(
       outline: "0",
       padding: "15px 10px",
       caretColor: "#444",
-      backgroundColor: "white",
     },
     root: {
       position: "relative",
