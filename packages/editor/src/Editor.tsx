3577e41f92eb4fda7724ec442a6adc2c91ae3c41
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-16 08:22:33 -0500

    feat(editor): enhance editor component with initial state and onChange support
    The editor component now supports an initial state and a handleChange prop, allowing better state management and facilitating the detection of content changes for improved functionality and user experience.

--- packages/editor/src/Editor.tsx
+++ packages/editor/src/Editor.tsx
@@ -1,5 +1,7 @@
+import { EditorState } from "lexical"
 import {
   InitialConfigType,
+  InitialEditorStateType,
   LexicalComposer,
 } from "@lexical/react/LexicalComposer"
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
@@ -7,6 +9,7 @@ import { ContentEditable } from "@lexical/react/LexicalContentEditable"
 import { ListPlugin } from "@lexical/react/LexicalListPlugin"
 import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin"
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin"
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary"
 import { Stack } from "@mui/material"
 import { pipe } from "fp-ts/function"
@@ -14,6 +17,7 @@ import { match as Bmatch } from "fp-ts/boolean"
 import {
   getOrElse as OgetOrElse,
   fromNullable as OfromNullable,
+  map as Omap,
 } from "fp-ts/Option"
 import { ImagePlugin } from "@dictybase/image-plugin"
 import { DictybaseToolbar } from "@dictybase/editor-toolbar"
@@ -25,19 +29,24 @@ import "./editor.css"
 import { WidthTablePlugin } from "@dictybase/width-table-plugin"
 import { TableActionPlugin } from "@dictybase/table-action-plugin"
 import { dictyEditorConfig } from "./editorConfig"
+import { flexLayoutStateString } from "./initialStates"
 
 type EditorProperties = {
-  config: InitialConfigType
+  config?: InitialConfigType
+  initialState?: InitialEditorStateType
   plugins?: Array<JSX.Element>
   editable?: boolean
   toolbar?: JSX.Element
+  handleChange?: (editorState: EditorState) => void
 }
 
 const Editor = ({
   config = dictyEditorConfig,
+  initialState = flexLayoutStateString,
   editable = false,
   toolbar,
   plugins,
+  handleChange,
 }: EditorProperties) => {
   const { classes: placeholderClasses } = useEditorPlaceholderStyles()
   const { classes: editorAreaClasses } = useEditorAreaStyles({ editable })
@@ -46,6 +55,7 @@ const Editor = ({
     <LexicalComposer
       initialConfig={{
         ...config,
+        editorState: initialState,
         editable,
       }}>
       <>{plugins}</>
@@ -60,32 +70,36 @@ const Editor = ({
         OfromNullable,
         OgetOrElse(() => <></>),
       )}
-      <Stack direction="row">
-        <Stack spacing={1} flexBasis="70%">
-          {pipe(
-            editable,
-            Bmatch(
-              () => <></>,
-              () => <DictybaseToolbar />,
-            ),
-          )}
-          <div style={{ position: "relative" }}>
-            <RichTextPlugin
-              ErrorBoundary={LexicalErrorBoundary}
-              contentEditable={
-                <ContentEditable
-                  id="content-editor"
-                  className={editorAreaClasses.container}
-                />
-              }
-              placeholder={
-                <div className={placeholderClasses.root}>
-                  Enter some text...
-                </div>
-              }
-            />
-          </div>
-        </Stack>
+      <Stack spacing={1}>
+        {pipe(
+          handleChange,
+          OfromNullable,
+          Omap((handler) => (
+            <OnChangePlugin ignoreSelectionChange onChange={handler} />
+          )),
+          OgetOrElse(() => <></>),
+        )}
+        {pipe(
+          editable,
+          Bmatch(
+            () => <></>,
+            () => <DictybaseToolbar />,
+          ),
+        )}
+        <div style={{ position: "relative" }}>
+          <RichTextPlugin
+            ErrorBoundary={LexicalErrorBoundary}
+            contentEditable={
+              <ContentEditable
+                id="content-editor"
+                className={editorAreaClasses.container}
+              />
+            }
+            placeholder={
+              <div className={placeholderClasses.root}>Enter some text...</div>
+            }
+          />
+        </div>
       </Stack>
     </LexicalComposer>
   )
