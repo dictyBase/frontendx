a4e6167abae3122015f47f38b314258a65539a30
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-18 01:15:24 -0500

    test(editor-playground/setup): add mock for @logto/react to facilitate testing without actual implementation
    Introducing a mock for the @logto/react library enhances the testing environment by preventing dependency on real implementations, allowing for more reliable and isolated tests.

--- apps/editor-playground/setup.ts
+++ apps/editor-playground/setup.ts
@@ -1,8 +1,15 @@
-import { expect, afterEach } from "vitest"
+import { vi, expect, afterEach } from "vitest"
 import { cleanup } from "@testing-library/react"
 import matchers from "@testing-library/jest-dom/matchers"
 
 expect.extend(matchers)
+
+// mock @logto
+vi.mock("@logto/react", async () => ({
+  useLogto: () => ({
+    getAccessToken: () => {},
+  }),
+}))
 
 afterEach(() => {
   cleanup()
