d42909158d62d92ddb0e59f2f6535a4e2cc55729
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-18 00:45:42 -0500

    feat(width-table-plugin): enhance WidthTableNode with static importJSON method for better serialization support
    Adding the static importJSON method improves the serialization and deserialization process for WidthTableNode, enabling more robust handling of the node's width during import, ultimately enhancing the plugin's functionality.

--- packages/width-table-plugin/src/WidthTableNode.ts
+++ packages/width-table-plugin/src/WidthTableNode.ts
@@ -1,6 +1,6 @@
-import { TableNode, type SerializedTableNode } from "@lexical/table"
+import { TableNode, SerializedTableNode } from "@lexical/table"
 import { addClassNamesToElement } from "@lexical/utils"
-import type { NodeKey, EditorConfig } from "lexical"
+import { NodeKey, EditorConfig } from "lexical"
 
 export interface SerializedWidthTableNode extends SerializedTableNode {
   width: number
@@ -27,6 +27,16 @@ class WidthTableNode extends TableNode {
    */
   static override clone({ __width, __key }: WidthTableNode): WidthTableNode {
     return new WidthTableNode(__width, __key)
+  }
+
+  /**
+   * Creates a new instance of WidthTableNode from the serialized data.
+   *
+   * @static
+   * @param serializedNode - Serialized data of the node.
+   */
+  static override importJSON(serializedNode: SerializedWidthTableNode) {
+    return new WidthTableNode(serializedNode.width)
   }
 
   /**
