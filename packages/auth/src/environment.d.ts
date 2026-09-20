060ac44518ee1970c5a87558724e6a9e663b51a8
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-20 09:53:03 -0500

    chore(auth, navbar): remove obsolete packages and source files due to project restructuring
    Removing unused files and configurations helps maintain a cleaner codebase and reduces complexity as the project's structure evolves, ensuring better maintainability moving forward.

--- packages/auth/src/environment.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable unicorn/prevent-abbreviations */
-interface ImportMetaEnv {
-  VITE_LOGTO_APPID: string
-  VITE_APP_BASENAME: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
