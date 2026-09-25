d92d577fd588558e9b93f5b8dc7cdd9181363e94
Kevin Tun <kevin.tun@northwestern.edu>
2026-09-25 09:13:40 -0500

    refactor(SearchPhenotypeListItem): enhance gene filtering with additional utilities for improved readability and functionality
    New functions and imports streamline the process of filtering genes by name, ensuring that only non-empty names are retained, which enhances code clarity and maintainability.

--- packages/ui-dsc/src/catalog/SearchPhenotypeListItem.tsx
+++ packages/ui-dsc/src/catalog/SearchPhenotypeListItem.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
-import { pipe } from "fp-ts/function"
+import { pipe, flow } from "fp-ts/function"
 import { match as Bmatch } from "fp-ts/boolean"
 import {
   fromNullable as OfromNullable,
   map as Omap,
   getOrElse as OgetOrElse,
 } from "fp-ts/Option"
-import { isNonEmpty as AisNonEmpty } from "fp-ts/Array"
+import { isNonEmpty as AisNonEmpty, filter as Afilter } from "fp-ts/Array"
+import { not } from "fp-ts/Predicate"
+import { isEmpty as SisEmpty } from "fp-ts/string"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import ListItem from "@mui/material/ListItem"
@@ -59,6 +61,10 @@ type SearchPhenotypeListItemProperties = {
     ListStrainsWithPhenotypeQuery["listStrainsWithAnnotation"]
   >["strains"][number]
 }
+const hasName = flow(
+  ({ name }: Pick<Gene, "name" | "__typename">) => name,
+  not(SisEmpty),
+)
 
 const SearchPhenotypeListItem = ({
   strain,
@@ -66,7 +72,12 @@ const SearchPhenotypeListItem = ({
   const { classes } = useStyles()
 
   const publications = strain?.publications
-  const genes = (strain?.genes as Gene[]) ?? []
+  const genes = pipe(
+    strain.genes,
+    OfromNullable,
+    Omap(Afilter(hasName)),
+    OgetOrElse(() => [] as Array<Pick<Gene, "name" | "__typename">>),
+  )
 
   return (
     <ListItem className={classes.row}>
